7e4a62aa483fbadc7089144191e48948f419903b
Sugu Sougoumarane <sougou@google.com>
2012-06-04 16:22:02 -0700

    bug: add setup.py

---
+++ py/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf8 shiftwidth=4 tabstop=4 textwidth=80 foldmethod=marker :
+# Copyright (c) 2010, Kou Man Tong. All rights reserved.
+# For licensing, see LICENSE file included in the package.
+
+from distutils.core import setup
+
+setup(name = "vtdb",
+		packages=["vtdb", "net"],
+		platforms = "Any",
+		)
