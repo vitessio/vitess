02115e7048f2ab63b9b8cb9ceb183ba589e06a71
Ric Szopa <szopa@google.com>
2012-06-07 11:55:06 -0700

    Change the license notice to be shorter and refer to the license file.

--- py/setup.py
+++ py/setup.py
@@ -1,11 +1,10 @@
-#!/usr/bin/env python
-# vim: set fileencoding=utf8 shiftwidth=4 tabstop=4 textwidth=80 foldmethod=marker :
-# Copyright (c) 2010, Kou Man Tong. All rights reserved.
-# For licensing, see LICENSE file included in the package.
+# Copyright 2012, Google Inc. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can
+# be found in the LICENSE file.
 
 from distutils.core import setup
 
 setup(name = "vtdb",
-		packages=["vtdb", "net"],
-		platforms = "Any",
-		)
+      packages=["vtdb", "net"],
+      platforms = "Any",
+      )
