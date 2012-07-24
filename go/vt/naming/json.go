105dafc613082a55a7ca4732216ca2de0a0637d3
Mike Solomon <msolomon@google.com>
2012-07-24 01:19:31 -0700

    migrate naming from experimental repo

---
+++ go/vt/naming/json.go
@@ -0,0 +1,17 @@
+// Copyright 2012, Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package naming
+
+import (
+	"encoding/json"
+)
+
+func toJson(x interface{}) string {
+	data, err := json.MarshalIndent(x, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	return string(data)
+}
