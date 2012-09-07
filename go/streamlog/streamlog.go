2e870a8cfd35671dcb6a1904069c1f0b5b7031a4
Ric Szopa <szopa@google.com>
2012-09-07 14:01:08 -0700

    Fix bugs in streamlog, resurrect streamlog.Stringer.

--- go/streamlog/streamlog.go
+++ go/streamlog/streamlog.go
@@ -12,17 +12,20 @@ var droppedMessages = expvar.NewMap("streamlog-dropped-messages")
 
 // A StreamLogger makes messages sent to it available through HTTP.
 type StreamLogger struct {
-	dataQueue  chan string
+	dataQueue  chan Stringer
 	subscribed map[io.Writer]chan bool
 	url        string
 	mu         sync.Mutex
 	// size is used to check if there are any subscriptions. Keep
-	// it atomically in sync (atomically) with the size of
-	// subscribed.
+	// it atomically in sync with the size of subscribed.
 	size uint32
 	// seq is a guard for modifications of subscribed - increment
 	// it atomically whenever you modify it.
 	seq uint32
+}
+
+type Stringer interface {
+	String() string
 }
 
 func (logger *StreamLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -44,7 +47,7 @@ func (logger *StreamLogger) subscribe(w io.Writer) chan bool {
 // messages. Any messages sent to it will be available at url.
 func New(url string, size int) *StreamLogger {
 	logger := &StreamLogger{
-		dataQueue:  make(chan string, size),
+		dataQueue:  make(chan Stringer, size),
 		subscribed: make(map[io.Writer]chan bool),
 		url:        url,
 	}
@@ -67,7 +70,7 @@ func (logger *StreamLogger) stream() {
 			for subscription, done := range logger.subscribed {
 				subscribed[subscription] = done
 			}
-			seq = s
+			seq = atomic.LoadUint32(&(logger.seq))
 			logger.mu.Unlock()
 		}
 
@@ -75,9 +78,9 @@ func (logger *StreamLogger) stream() {
 			continue
 		}
 
-		message += "\n"
-		for subscription, done := range logger.subscribed {
-			if _, err := io.WriteString(subscription, message); err != nil {
+		messageString := message.String() + "\n"
+		for subscription, done := range subscribed {
+			if _, err := io.WriteString(subscription, messageString); err != nil {
 				done <- true
 
 				logger.mu.Lock()
@@ -94,7 +97,7 @@ func (logger *StreamLogger) stream() {
 
 // Send sends message to all the writers subscribed to logger. Calling
 // Send does not block.
-func (logger *StreamLogger) Send(message string) {
+func (logger *StreamLogger) Send(message Stringer) {
 	if atomic.LoadUint32(&logger.size) == 0 {
 		// There are no subscribers, do nothing.
 		return
