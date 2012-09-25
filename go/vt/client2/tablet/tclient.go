456e7a5c693877927e8f7a0d267f3a86f6c2155d
Mike Solomon <msolomon@google.com>
2012-09-25 13:54:53 -0700

    refactor tablet client,  adding reliability layer and increasing compatibility with Go database driver interface

---
+++ go/vt/client2/tablet/tclient.go
@@ -0,0 +1,365 @@
+// Copyright 2012, Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// API compliant to the requirements of database/sql
+// Open expects name to be "hostname:port/dbname"
+// For query arguments, we assume place-holders in the query string
+// in the form of :v0, :v1, etc.
+package tablet
+
+import (
+	"database/sql/driver"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"strings"
+
+	mproto "code.google.com/p/vitess/go/mysql/proto"
+	"code.google.com/p/vitess/go/rpcplus"
+	"code.google.com/p/vitess/go/rpcwrap/bsonrpc"
+	tproto "code.google.com/p/vitess/go/vt/tabletserver/proto"
+)
+
+var (
+	ErrNoNestedTxn = errors.New("vt: no nested transactions")
+	ErrBadCommit   = errors.New("vt: commit without corresponding begin")
+	ErrBadRollback = errors.New("vt: rollback without corresponding begin")
+)
+
+type TabletError struct {
+	err  error
+	addr string
+}
+
+func (te TabletError) Error() string {
+	return fmt.Sprintf("vt: client error on %v %v", te.addr, te.err)
+}
+
+// Not thread safe, as per sql package.
+type Conn struct {
+	dbi       string
+	stream    bool
+	rpcClient *rpcplus.Client
+	tproto.Session
+
+	user   string
+	passwd string
+	addr   string
+	dbName string
+}
+
+type Stmt struct {
+	conn  *Conn
+	query string
+}
+
+type Tx struct {
+	conn *Conn
+}
+
+type StreamResult struct {
+	call    *rpcplus.Call
+	sr      chan *mproto.QueryResult
+	columns *mproto.QueryResult
+	// current result and index on it
+	qr    *mproto.QueryResult
+	index int
+}
+
+func DialTablet(dbi string, stream bool) (*Conn, error) {
+	user, passwd, addr, dbName, err := parseDbi(dbi)
+	if err != nil {
+		return nil, err
+	}
+	conn := &Conn{dbi: dbi, stream: stream, user: user, passwd: passwd, addr: addr, dbName: dbName}
+	if err := conn.dial(); err != nil {
+		return nil, conn.fmtErr(err)
+	}
+	return conn, nil
+}
+
+// FIXME(msolomon) Update protocol, add SASL.
+// dbi: host:port/dbname (v1)
+// dbi: vttp://user:passwd@host:port/dbname (v2)
+func parseDbi(dbi string) (user string, passwd string, addr string, dbName string, err error) {
+	dbiParts := strings.Split(dbi, "/")
+	if len(dbiParts) != 2 {
+		err = fmt.Errorf("vt: incorrectly formatted dbi %v", dbi)
+		return
+	}
+	addr = dbiParts[0]
+	dbName = dbiParts[1]
+	return
+}
+
+// Format error for exported methods to give callers more information.
+func (conn *Conn) fmtErr(err error) error {
+	if err == nil {
+		return nil
+	}
+	return TabletError{err, conn.addr}
+}
+
+func (conn *Conn) dial() error {
+	var err error
+	if conn.rpcClient, err = bsonrpc.DialHTTP("tcp", conn.addr); err != nil {
+		return err
+	}
+	sessionParams := tproto.SessionParams{DbName: conn.dbName}
+	var sessionInfo tproto.SessionInfo
+	if err = conn.rpcClient.Call("SqlQuery.GetSessionId", sessionParams, &sessionInfo); err != nil {
+		return err
+	}
+	conn.SessionId = sessionInfo.SessionId
+	return nil
+}
+
+// driver.Conn interface
+func (conn *Conn) Prepare(query string) (driver.Stmt, error) {
+	return &Stmt{conn, query}, nil
+}
+
+func (conn *Conn) Close() error {
+	conn.Session = tproto.Session{0, 0, 0}
+	return conn.rpcClient.Close()
+}
+
+// ExecBind can be used to execute queries with explicitly named bind vars.
+// You will need to bypass go's database api to use this function.
+func (conn *Conn) ExecBind(query string, bindVars map[string]interface{}) (driver.Result, error) {
+	req := &tproto.Query{
+		Sql:           query,
+		BindVariables: bindVars,
+		TransactionId: conn.TransactionId,
+		ConnectionId:  conn.ConnectionId,
+		SessionId:     conn.SessionId,
+	}
+	if conn.stream {
+		sr := make(chan *mproto.QueryResult, 10)
+		c := conn.rpcClient.StreamGo("SqlQuery.StreamExecute", req, sr)
+
+		// read the columns, or grab the error
+		cols, ok := <-sr
+		if !ok {
+			return nil, conn.fmtErr(c.Error)
+		}
+		return &StreamResult{c, sr, cols, nil, 0}, nil
+	}
+
+	qr := new(mproto.QueryResult)
+	if err := conn.rpcClient.Call("SqlQuery.Execute", req, qr); err != nil {
+		return nil, conn.fmtErr(err)
+	}
+	return &Result{qr, 0}, nil
+}
+
+// driver.Execer interface
+func (conn *Conn) Exec(query string, args []driver.Value) (driver.Result, error) {
+	bindVars := make(map[string]interface{})
+	for i, v := range args {
+		bindVars[fmt.Sprintf("v%d", i)] = v
+	}
+
+	return conn.ExecBind(query, bindVars)
+}
+
+func (conn *Conn) Begin() (driver.Tx, error) {
+	if conn.TransactionId != 0 {
+		return &Tx{}, ErrNoNestedTxn
+	}
+	if err := conn.rpcClient.Call("SqlQuery.Begin", &conn.Session, &conn.TransactionId); err != nil {
+		return &Tx{}, conn.fmtErr(err)
+	}
+	return &Tx{conn}, nil
+}
+
+func (conn *Conn) Commit() error {
+	if conn.TransactionId == 0 {
+		return ErrBadCommit
+	}
+	// NOTE(msolomon) Unset the transaction_id irrespective of the RPC's
+	// response. The intent of commit is that no more statements can be
+	// made on this transaction, so we guarantee that. Transient errors
+	// between the db and the client shouldn't affect this part of the
+	// bookkeeping.  According to the Go Driver API, this will not be
+	// called concurrently.  Defer this because we this affects the
+	// session referenced in the request.
+	defer func() { conn.TransactionId = 0 }()
+	var noOutput string
+	return conn.fmtErr(conn.rpcClient.Call("SqlQuery.Commit", &conn.Session, &noOutput))
+}
+
+func (conn *Conn) Rollback() error {
+	if conn.TransactionId == 0 {
+		return ErrBadRollback
+	}
+	// See note in Commit about the behavior of TransactionId.
+	defer func() { conn.TransactionId = 0 }()
+	var noOutput string
+	return conn.fmtErr(conn.rpcClient.Call("SqlQuery.Rollback", &conn.Session, &noOutput))
+}
+
+// driver.Stmt interface
+func (stmt *Stmt) Close() error {
+	stmt.query = ""
+	return nil
+}
+
+func (*Stmt) NumInput() int {
+	return -1
+}
+
+func (stmt *Stmt) Exec(args []driver.Value) (driver.Result, error) {
+	return stmt.conn.Exec(stmt.query, args)
+}
+
+func (stmt *Stmt) Query(args []driver.Value) (driver.Rows, error) {
+	// we use driver.Execer interface, we know it's a Result return,
+	// and our Result implements driver.Rows
+	// (or a StreamResult that does too)
+	res, err := stmt.conn.Exec(stmt.query, args)
+	if err != nil {
+		return nil, err
+	}
+	return res.(driver.Rows), nil
+}
+
+// driver.Tx interface (forwarded to Conn)
+func (tx *Tx) Commit() error {
+	return tx.conn.Commit()
+}
+
+func (tx *Tx) Rollback() error {
+	return tx.conn.Rollback()
+}
+
+type Result struct {
+	qr    *mproto.QueryResult
+	index int
+}
+
+func NewResult(rowCount, rowsAffected, insertId int64, fields []mproto.Field) *Result {
+	return &Result{
+		qr: &mproto.QueryResult{
+			Rows:         make([][]interface{}, int(rowCount)),
+			Fields:       fields,
+			RowsAffected: uint64(rowsAffected),
+			InsertId:     uint64(insertId),
+		},
+	}
+}
+
+func (result *Result) RowsRetrieved() int64 {
+	return int64(len(result.qr.Rows))
+}
+
+// driver.Result interface
+func (result *Result) LastInsertId() (int64, error) {
+	return int64(result.qr.InsertId), nil
+}
+
+func (result *Result) RowsAffected() (int64, error) {
+	return int64(result.qr.RowsAffected), nil
+}
+
+// driver.Rows interface
+func (result *Result) Columns() []string {
+	cols := make([]string, len(result.qr.Fields))
+	for i, f := range result.qr.Fields {
+		cols[i] = f.Name
+	}
+	return cols
+}
+
+func (result *Result) Rows() [][]interface{} {
+	return result.qr.Rows
+}
+
+// FIXME(msolomon) This should be intependent of the mysql module.
+func (result *Result) Fields() []mproto.Field {
+	return result.qr.Fields
+}
+
+func (result *Result) Close() error {
+	result.index = 0
+	return nil
+}
+
+func (result *Result) Next(dest []driver.Value) error {
+	if len(dest) != len(result.qr.Fields) {
+		return errors.New("vt: field length mismatch")
+	}
+	if result.index >= len(result.qr.Rows) {
+		return io.EOF
+	}
+	defer func() { result.index++ }()
+	for i, v := range result.qr.Rows[result.index] {
+		if v != nil {
+			dest[i] = convert(int(result.qr.Fields[i].Type), v.(string))
+		}
+	}
+	return nil
+}
+
+// driver.Result interface
+func (*StreamResult) LastInsertId() (int64, error) {
+	return 0, errors.New("vt: no LastInsertId available after streaming statement")
+}
+
+func (*StreamResult) RowsAffected() (int64, error) {
+	return 0, errors.New("vt: no RowsAffected available after streaming statement")
+}
+
+// driver.Rows interface
+func (sr *StreamResult) Columns() (cols []string) {
+	cols = make([]string, len(sr.columns.Fields))
+	for i, f := range sr.columns.Fields {
+		cols[i] = f.Name
+	}
+	return cols
+}
+
+func (*StreamResult) Close() error {
+	return nil
+}
+
+func (sr *StreamResult) Next(dest []driver.Value) error {
+	if len(dest) != len(sr.columns.Fields) {
+		return errors.New("vt: field length mismatch")
+	}
+
+	if sr.qr == nil {
+		// we need to read the next record that may contain
+		// multiple rows
+		qr, ok := <-sr.sr
+		if !ok {
+			// if there was an error, we return that, otherwise
+			// we return EOF
+			if sr.call.Error != nil {
+				log.Printf("vt: error reading the next value %v", sr.call.Error.Error())
+				return sr.call.Error
+			}
+			return io.EOF
+		}
+		sr.qr = qr
+		sr.index = 0
+	}
+
+	row := sr.qr.Rows[sr.index]
+	for i, v := range row {
+		if v != nil {
+			dest[i] = convert(int(sr.columns.Fields[i].Type), v.(string))
+		}
+	}
+
+	sr.index++
+	if sr.index == len(sr.qr.Rows) {
+		// we reached the end of our rows, nil it so next run
+		// will fetch the next one
+		sr.qr = nil
+	}
+
+	return nil
+}
---
+++ go/vt/client2/tablet/vclient.go
@@ -0,0 +1,183 @@
+// Copyright 2012, Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This implements some additional error handling logic to make the client
+// more robust in the face of transient problems with easy solutions.
+package tablet
+
+import (
+	"database/sql"
+	"database/sql/driver"
+	"fmt"
+	"net"
+	"strings"
+	"time"
+
+	"code.google.com/p/vitess/go/relog"
+)
+
+const (
+	ErrTypeFatal = 1 //errors.New("vt: fatal: reresolve endpoint")
+	ErrTypeRetry = 2 //errors.New("vt: retry: reconnect endpoint")
+	ErrTypeApp   = 3 //errors.New("vt: app level error")
+)
+
+const (
+	DefaultReconnectDelay = 2 * time.Millisecond
+	DefaultMaxAttempts    = 2
+	DefaultTimeout        = 30 * time.Second
+)
+
+var zeroTime time.Time
+
+// Layer some logic on top of the basic tablet protocol to support
+// fast retry when we can.
+
+type VtConn struct {
+	Conn
+	maxAttempts    int           // How many times should try each retriable operation?
+	timeout        time.Duration // How long should we wait for a given operation?
+	timeFailed     time.Time     // This is the time a client transitioned from presumable health to failure.
+	reconnectDelay time.Duration
+}
+
+// How long should we wait to try to recover?
+// FIXME(msolomon) not sure if maxAttempts is still useful
+func (vtc *VtConn) recoveryTimeout() time.Duration {
+	return vtc.timeout * 2
+}
+
+func (vtc *VtConn) handleErr(err error) (int, error) {
+	now := time.Now()
+	if vtc.timeFailed.IsZero() {
+		vtc.timeFailed = now
+	} else if now.Sub(vtc.timeFailed) > vtc.recoveryTimeout() {
+		vtc.Close()
+		return ErrTypeFatal, fmt.Errorf("vt: max recovery time exceeded: %v", err)
+	}
+
+	errType := ErrTypeApp
+	if tabletErr, ok := err.(TabletError); ok {
+		msg := strings.ToLower(tabletErr.err.Error())
+		if strings.HasPrefix(msg, "fatal") {
+			errType = ErrTypeFatal
+		} else if strings.HasPrefix(msg, "retry") {
+			errType = ErrTypeRetry
+		}
+	} else if netErr, ok := err.(net.Error); ok && netErr.Temporary() {
+		errType = ErrTypeRetry
+	}
+
+	if errType == ErrTypeRetry && vtc.TransactionId != 0 {
+		errType = ErrTypeApp
+		err = fmt.Errorf("vt: cannot retry within a transaction: %v", err)
+		time.Sleep(vtc.reconnectDelay)
+		vtc.Close()
+		dialErr := vtc.dial()
+		relog.Warning("vt: redial error %v", dialErr)
+	}
+
+	return errType, err
+}
+
+// ExecBind can be used to execute queries with explicitly named bind vars.
+// You will need to bypass go's database api to use this function.
+func (vtc *VtConn) ExecBind(query string, bindVars map[string]interface{}) (driver.Result, error) {
+	attempt := 0
+	for {
+		result, err := vtc.Conn.ExecBind(query, bindVars)
+		if err == nil {
+			vtc.timeFailed = zeroTime
+			return result, nil
+		}
+
+		errType, err := vtc.handleErr(err)
+		if errType != ErrTypeRetry {
+			return nil, err
+		}
+		for {
+			attempt++
+			if attempt > vtc.maxAttempts {
+				return nil, fmt.Errorf("vt: max recovery attempts exceeded: %v", err)
+			}
+			vtc.Close()
+			time.Sleep(vtc.reconnectDelay)
+			if err := vtc.dial(); err == nil {
+				break
+			}
+			relog.Warning("vt: error dialing on exec %v", vtc.Conn.addr)
+		}
+	}
+
+	panic("unreachable")
+}
+
+func (vtc *VtConn) Begin() (driver.Tx, error) {
+	attempt := 0
+	for {
+		tx, err := vtc.Conn.Begin()
+		if err == nil {
+			vtc.timeFailed = zeroTime
+			return tx, nil
+		}
+
+		errType, err := vtc.handleErr(err)
+		if errType != ErrTypeRetry {
+			return nil, err
+		}
+		for {
+			attempt++
+			if attempt > vtc.maxAttempts {
+				return nil, fmt.Errorf("vt: max recovery attempts exceeded: %v", err)
+			}
+			vtc.Close()
+			time.Sleep(vtc.reconnectDelay)
+			if err := vtc.dial(); err == nil {
+				break
+			}
+			relog.Warning("vt: error dialing on begin %v", vtc.Conn.addr)
+		}
+	}
+	panic("unreachable")
+}
+
+func (vtc *VtConn) Commit() (err error) {
+	if err = vtc.Conn.Commit(); err == nil {
+		vtc.timeFailed = zeroTime
+		return nil
+	}
+
+	// Not much we can do at this point, just annotate the error and return.
+	_, err = vtc.handleErr(err)
+	return err
+}
+
+func DialVtdb(dbi string, stream bool, timeout time.Duration) (*VtConn, error) {
+	user, passwd, addr, dbName, err := parseDbi(dbi)
+	if err != nil {
+		return nil, err
+	}
+	conn := &VtConn{
+		Conn:        Conn{dbi: dbi, stream: stream, user: user, passwd: passwd, addr: addr, dbName: dbName},
+		maxAttempts: DefaultMaxAttempts, timeout: timeout, reconnectDelay: DefaultReconnectDelay,
+	}
+
+	if err := conn.dial(); err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+type vDriver struct {
+	stream bool
+}
+
+func (driver *vDriver) Open(name string) (driver.Conn, error) {
+	return DialVtdb(name, driver.stream, DefaultTimeout)
+}
+
+func init() {
+	sql.Register("vttablet", &vDriver{})
+	sql.Register("vttablet-streaming", &vDriver{true})
+}
