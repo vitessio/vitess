7dcd8ae013ace0d2f67e20205d3067a7b6691dcc
Mike Solomon <msolomon@google.com>
2012-11-27 12:01:53 -0800

    improve zkns purging of stale addresses

--- go/vt/wrangler/zkns.go
+++ go/vt/wrangler/zkns.go
@@ -3,8 +3,10 @@ package zkwrangler
 import (
 	"fmt"
 	"path"
+	"sort"
 
 	"code.google.com/p/vitess/go/jscfg"
+	"code.google.com/p/vitess/go/relog"
 	"code.google.com/p/vitess/go/vt/naming"
 	tm "code.google.com/p/vitess/go/vt/tabletmanager"
 	"code.google.com/p/vitess/go/zk"
@@ -35,7 +37,7 @@ func (wr *Wrangler) ExportZkns(zkVtRoot string) error {
 			continue
 		}
 
-		if err = wr.exportVtnsToZkns(addrPath, zknsAddrPath); err != nil {
+		if _, err = wr.exportVtnsToZkns(addrPath, zknsAddrPath); err != nil {
 			return err
 		}
 	}
@@ -67,12 +69,22 @@ func (wr *Wrangler) ExportZknsForKeyspace(zkKeyspacePath string) error {
 		vtnsRootPath := fmt.Sprintf("/zk/%v/vt/ns/%v", cell, keyspace)
 		zknsRootPath := fmt.Sprintf("/zk/%v/zkns/vt/%v", cell, keyspace)
 
-		children, err := zk.ChildrenRecursive(wr.zconn, vtnsRootPath)
+		// Get the existing list of zkns children. If they don't get rewritten,
+		// delete them as stale entries.
+		zknsChildren, err := zk.ChildrenRecursive(wr.zconn, zknsRootPath)
 		if err != nil {
 			return err
 		}
+		staleZknsPaths := make(map[string]bool)
+		for _, child := range zknsChildren {
+			staleZknsPaths[path.Join(zknsRootPath, child)] = true
+		}
 
-		for _, child := range children {
+		vtnsChildren, err := zk.ChildrenRecursive(wr.zconn, vtnsRootPath)
+		if err != nil {
+			return err
+		}
+		for _, child := range vtnsChildren {
 			vtnsAddrPath := path.Join(vtnsRootPath, child)
 			zknsAddrPath := path.Join(zknsRootPath, child)
 
@@ -84,7 +96,25 @@ func (wr *Wrangler) ExportZknsForKeyspace(zkKeyspacePath string) error {
 			if stat.NumChildren() > 0 {
 				continue
 			}
-			if err = wr.exportVtnsToZkns(vtnsAddrPath, zknsAddrPath); err != nil {
+			zknsPathsWritten, err := wr.exportVtnsToZkns(vtnsAddrPath, zknsAddrPath)
+			if err != nil {
+				return err
+			}
+			relog.Debug("zknsPathsWritten: %v", zknsPathsWritten)
+			for _, zkPath := range zknsPathsWritten {
+				delete(staleZknsPaths, zkPath)
+			}
+		}
+		relog.Debug("staleZknsPaths: %v", staleZknsPaths)
+		prunePaths := make([]string, 0, len(staleZknsPaths))
+		for prunePath, _ := range staleZknsPaths {
+			prunePaths = append(prunePaths, prunePath)
+		}
+		sort.Strings(prunePaths)
+		// Prune paths in reverse order so we remove children first
+		for i := len(prunePaths) - 1; i >= 0; i-- {
+			relog.Info("prune stale zkns path %v", prunePaths[i])
+			if err := wr.zconn.Delete(prunePaths[i], -1); err != nil && !zookeeper.IsError(err, zookeeper.ZNOTEMPTY) {
 				return err
 			}
 		}
@@ -92,10 +122,11 @@ func (wr *Wrangler) ExportZknsForKeyspace(zkKeyspacePath string) error {
 	return nil
 }
 
-func (wr *Wrangler) exportVtnsToZkns(vtnsAddrPath, zknsAddrPath string) error {
+func (wr *Wrangler) exportVtnsToZkns(vtnsAddrPath, zknsAddrPath string) ([]string, error) {
+	zknsPaths := make([]string, 0, 32)
 	addrs, err := naming.ReadAddrs(wr.zconn, vtnsAddrPath)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// Write the individual endpoints and compute the SRV entries.
@@ -103,10 +134,11 @@ func (wr *Wrangler) exportVtnsToZkns(vtnsAddrPath, zknsAddrPath string) error {
 	defaultAddrs := LegacyZknsAddrs{make([]string, 0, 8)}
 	for i, entry := range addrs.Entries {
 		zknsAddrPath := fmt.Sprintf("%v/%v", zknsAddrPath, i)
+		zknsPaths = append(zknsPaths, zknsAddrPath)
 		zknsAddr := zkns.ZknsAddr{Host: entry.Host, Port: entry.NamedPortMap["_mysql"], NamedPortMap: entry.NamedPortMap}
 		err := WriteAddr(wr.zconn, zknsAddrPath, &zknsAddr)
 		if err != nil {
-			return err
+			return nil, err
 		}
 		defaultAddrs.Endpoints = append(defaultAddrs.Endpoints, zknsAddrPath)
 		vtoccAddrs.Endpoints = append(vtoccAddrs.Endpoints, zknsAddrPath+":_vtocc")
@@ -122,22 +154,24 @@ func (wr *Wrangler) exportVtnsToZkns(vtnsAddrPath, zknsAddrPath string) error {
 			break
 		}
 		if err != nil {
-			return err
+			return nil, err
 		}
 		deleteIdx++
 	}
 
 	// Write the VDNS entries for both vtocc and mysql
 	vtoccVdnsPath := fmt.Sprintf("%v/_vtocc.vdns", zknsAddrPath)
+	zknsPaths = append(zknsPaths, vtoccVdnsPath)
 	if err = WriteAddrs(wr.zconn, vtoccVdnsPath, &vtoccAddrs); err != nil {
-		return err
+		return nil, err
 	}
 
 	defaultVdnsPath := fmt.Sprintf("%v.vdns", zknsAddrPath)
+	zknsPaths = append(zknsPaths, defaultVdnsPath)
 	if err = WriteAddrs(wr.zconn, defaultVdnsPath, &defaultAddrs); err != nil {
-		return err
+		return nil, err
 	}
-	return nil
+	return zknsPaths, nil
 }
 
 type LegacyZknsAddrs struct {
