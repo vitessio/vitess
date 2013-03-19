aab12139d1ddce078f6bc5fae0dc538222985ef2
Alain Jobart <alainjobart@google.com>
2013-03-19 13:20:17 -0700

    Fixing package comments.

--- go/cmd/vt_binlog_player/vt_binlog_player.go
+++ go/cmd/vt_binlog_player/vt_binlog_player.go
@@ -2,12 +2,11 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package main
-
 /*
-This tool reads data from the a remote host via vt_binlog_server
+The vt_binlog_player reads data from the a remote host via vt_binlog_server.
 This is mostly intended for online data migrations.
 */
+package main
 
 import (
 	"bufio"
