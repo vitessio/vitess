bc9aa43e68cbdf19f573e037346d6475d3f5142d
Ryszard Szopa <szopa@google.com>
2013-07-24 15:14:07 -0700

    vtctl UpdateTabletAddrs command.

--- test/tablet.py
+++ test/tablet.py
@@ -186,6 +186,27 @@ class Tablet(object):
     finally:
       conn.close()
 
+  def update_addrs(self, addr=None, secure_addr=None, mysql_addr=None, mysql_ip_addr=None):
+    args = ['UpdateTabletAddrs']
+    if addr:
+      args.extend(['-addr', addr])
+
+    if secure_addr:
+      args.extend(['-secure-addr', secure_addr])
+
+    if mysql_addr:
+      args.extend(['-mysql-addr', mysql_addr])
+
+    if mysql_ip_addr:
+      args.extend(['-mysql-ip-addr', mysql_ip_addr])
+
+    args.append(self.tablet_alias)
+
+    if len(args) < 3:
+      return None, None
+
+    return utils.run_vtctl(args)
+
   def scrap(self, force=False, skip_rebuild=False):
     args = ['ScrapTablet']
     if force:
