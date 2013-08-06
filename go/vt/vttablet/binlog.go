89a9c6c5f73a1f3dc60c4aac97ddb952ad49579a
Alain Jobart <alainjobart@google.com>
2013-08-05 22:23:41 -0700

    Adding states, fixing mutex access.

--- go/vt/vttablet/binlog.go
+++ go/vt/vttablet/binlog.go
@@ -10,6 +10,7 @@ package vttablet
 import (
 	"bytes"
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/youtube/vitess/go/mysql"
@@ -19,6 +20,12 @@ import (
 	"github.com/youtube/vitess/go/vt/topo"
 )
 
+const (
+	BINLOG_PLAYER_CONNECTING = iota
+	BINLOG_PLAYER_PLAYING
+	BINLOG_PLAYER_SLEEPING
+)
+
 // BinlogPlayerController controls one player
 type BinlogPlayerController struct {
 	ts       topo.Server
@@ -26,31 +33,42 @@ type BinlogPlayerController struct {
 	keyspace string
 	source   topo.SourceShard
 
-	// Player is the BinlogPlayer when we have one
+	// Player is the BinlogPlayer when we have one, protected by mu
 	player *mysqlctl.BinlogPlayer
+	mu     sync.Mutex
+
+	// states is our state accounting variable, for stats
+	states *stats.States
 
 	// interrupted is the channel to close to stop the playback
 	interrupted chan struct{}
-
-	// TODO(alainjobart): add state
-	// TODO(alainjobart): add statsJson, include stats and player stats if any
-	// TODO(alainjobart): figure out if we need a lock on structure (for stats)
 }
 
 func NewBinlogController(ts topo.Server, dbConfig *mysql.ConnectionParams, keyspace string, source topo.SourceShard) *BinlogPlayerController {
-	return &BinlogPlayerController{
+	blc := &BinlogPlayerController{
 		ts:          ts,
 		dbConfig:    dbConfig,
 		keyspace:    keyspace,
 		source:      source,
 		interrupted: make(chan struct{}, 1),
 	}
+	blc.states = stats.NewStates("", []string{
+		"Connecting",
+		"Playing",
+		"Sleeping",
+	}, time.Now(), BINLOG_PLAYER_CONNECTING)
+	return blc
 }
 
 func (blc *BinlogPlayerController) statsJSON() string {
 	buf := bytes.NewBuffer(make([]byte, 0, 128))
 	fmt.Fprintf(buf, "{")
-	fmt.Fprintf(buf, "\n \"Player\": %v", blc.player.StatsJSON())
+	blc.mu.Lock()
+	if blc.player != nil {
+		fmt.Fprintf(buf, "\n \"Player\": %v", blc.player.StatsJSON())
+	}
+	blc.mu.Unlock()
+	fmt.Fprintf(buf, "\n \"States\": %v", blc.states.String())
 	fmt.Fprintf(buf, "\n}")
 	return buf.String()
 }
@@ -67,45 +85,64 @@ func (bpc *BinlogPlayerController) Stop() {
 
 func (bpc *BinlogPlayerController) Loop() {
 	for {
-		// create the db connection, connect it
-		vtClient := mysqlctl.NewDbClient(bpc.dbConfig)
-		if err := vtClient.Connect(); err != nil {
-			relog.Error("BinlogPlayerMap: can't connect to database: %v", err)
-			continue
-		}
-
-		// Read the start position
-		startPosition, err := mysqlctl.ReadStartPosition(vtClient, string(bpc.source.KeyRange.Start.Hex()), string(bpc.source.KeyRange.End.Hex()))
-		if err != nil {
-			relog.Warning("BinlogPlayerController: can't read startPosition: %v", err)
-			time.Sleep(5)
-			continue
-		}
-
-		// TODO(alainjobart): Find the server list
-
-		// TODO(alainjobart): Pick a server (same if it's available,
-		// if not clear master file / pos and keep only group id)
-
-		// Create the player.
-		bpc.player, err = mysqlctl.NewBinlogPlayer(vtClient, startPosition, nil /*tables*/, 1 /*txnBatch*/, 30*time.Second /*maxTxnInterval*/, false /*execDdl*/)
-		if err != nil {
-			relog.Warning("BinlogPlayerController: can't create player: %v", err)
-			time.Sleep(5)
-			continue
-		}
-
-		// Run player loop until it's done.
-		err = bpc.player.ApplyBinlogEvents(bpc.interrupted)
-		if err != nil {
-			relog.Error("Error in applying binlog events, err %v", err)
-			time.Sleep(5)
-		} else {
-			// We were interrupted.
+		err := bpc.Iteration()
+		if err == nil {
+			// this happens when we get interrupted
 			break
 		}
+		relog.Warning("BinlogPlayerController(%v-%v): %v", bpc.source.KeyRange.Start, bpc.source.KeyRange.End, err)
+
+		// sleep for a bit before retrying to connect
+		bpc.states.SetState(BINLOG_PLAYER_SLEEPING)
+		time.Sleep(5)
 	}
+
 	relog.Info("Exited main binlog player loop for %v", bpc.source)
+}
+
+func (bpc *BinlogPlayerController) Iteration() (err error) {
+	defer func() {
+		if x := recover(); x != nil {
+			relog.Error("BinlogPlayerController caught panic: %v", x)
+			err = fmt.Errorf("panic: %v", x)
+		}
+	}()
+	bpc.states.SetState(BINLOG_PLAYER_CONNECTING)
+
+	// create the db connection, connect it
+	vtClient := mysqlctl.NewDbClient(bpc.dbConfig)
+	if err := vtClient.Connect(); err != nil {
+		return fmt.Errorf("can't connect to database: %v", err)
+	}
+
+	// Read the start position
+	startPosition, err := mysqlctl.ReadStartPosition(vtClient, string(bpc.source.KeyRange.Start.Hex()), string(bpc.source.KeyRange.End.Hex()))
+	if err != nil {
+		return fmt.Errorf("can't read startPosition: %v", err)
+	}
+
+	// TODO(alainjobart): Find the server list
+
+	// TODO(alainjobart): Pick a server (same if it's available,
+	// if not clear master file / pos and keep only group id)
+
+	// Create the player.
+	bpc.mu.Lock()
+	bpc.player, err = mysqlctl.NewBinlogPlayer(vtClient, startPosition, nil /*tables*/, 1 /*txnBatch*/, 30*time.Second /*maxTxnInterval*/, false /*execDdl*/)
+	bpc.mu.Unlock()
+	if err != nil {
+		return fmt.Errorf("can't create player: %v", err)
+	}
+
+	// Run player loop until it's done.
+	bpc.states.SetState(BINLOG_PLAYER_PLAYING)
+	err = bpc.player.ApplyBinlogEvents(bpc.interrupted)
+
+	bpc.mu.Lock()
+	bpc.player = nil
+	bpc.mu.Unlock()
+
+	return err
 }
 
 // BinlogPlayerMap controls all the players
