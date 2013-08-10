9471cfdf62e4cc0e51d592e887cd18f9087d98aa
Sugu Sougoumarane <sougou@google.com>
2013-08-10 02:15:48 -0700

    proc: new feature: counting listener

--- go/proc/listen_test.go
+++ go/proc/listen_test.go
@@ -4,6 +4,14 @@
 
 package proc
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
+
 func TestListener(t *testing.T) {
 	l, err := Listen(testPort)
 	if err != nil {
@@ -11,10 +19,27 @@ func TestListener(t *testing.T) {
 	}
 	go http.Serve(l, nil)
 
-	resp, err := http.Get(fmt.Sprintf("http://localhost:%s/debug/vars", testPort))
-	if err != nil {
-		t.Fatal(err)
+	for i := 1; i <= 3; i++ {
+		resp, err := http.Get(fmt.Sprintf("http://localhost:%s/debug/vars", testPort))
+		if err != nil {
+			t.Fatal(err)
+		}
+		val, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		http.DefaultTransport.(*http.Transport).CloseIdleConnections()
+		vars := make(map[string]interface{})
+		err = json.Unmarshal(val, &vars)
+		if err != nil {
+			t.Error(err)
+		}
+		if vars["connection-count"].(float64) != 1 {
+			t.Errorf("want 1, got %v", vars["connection-count"])
+		}
+		if vars["connection-accepted"].(float64) != float64(i) {
+			t.Errorf("want %d, got %v", i, vars["connection-count"])
+		}
 	}
-	t.Logf("%#v", string(resp))
 	l.Close()
 }
