71fe01cfeceac0a6d35a27d415d6a63db59e84e3
Alain Jobart <alainjobart@google.com>
2013-08-27 13:05:36 -0700

    Splitting cell.go into its parts.

---
+++ go/vt/zktopo/serving_graph.go
@@ -0,0 +1,176 @@
+// Copyright 2013, Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package zktopo
+
+import (
+	"fmt"
+	"path"
+
+	"github.com/youtube/vitess/go/jscfg"
+	"github.com/youtube/vitess/go/vt/topo"
+	"github.com/youtube/vitess/go/zk"
+	"launchpad.net/gozk/zookeeper"
+)
+
+/*
+This file contains the serving graph management code of zktopo.Server
+*/
+
+func zkPathForVtKeyspace(cell, keyspace string) string {
+	return fmt.Sprintf("/zk/%v/vt/ns/%v", cell, keyspace)
+}
+
+func zkPathForVtShard(cell, keyspace, shard string) string {
+	return path.Join(zkPathForVtKeyspace(cell, keyspace), shard)
+}
+
+func zkPathForVtName(cell, keyspace, shard string, tabletType topo.TabletType) string {
+	return path.Join(zkPathForVtShard(cell, keyspace, shard), string(tabletType))
+}
+
+func (zkts *Server) GetSrvTabletTypesPerShard(cell, keyspace, shard string) ([]topo.TabletType, error) {
+	zkSgShardPath := zkPathForVtShard(cell, keyspace, shard)
+	children, _, err := zkts.zconn.Children(zkSgShardPath)
+	if err != nil {
+		if zookeeper.IsError(err, zookeeper.ZNONODE) {
+			err = topo.ErrNoNode
+		}
+		return nil, err
+	}
+	result := make([]topo.TabletType, len(children))
+	for i, tt := range children {
+		result[i] = topo.TabletType(tt)
+	}
+	return result, nil
+}
+
+func (zkts *Server) UpdateSrvTabletType(cell, keyspace, shard string, tabletType topo.TabletType, addrs *topo.VtnsAddrs) error {
+	path := zkPathForVtName(cell, keyspace, shard, tabletType)
+	data := jscfg.ToJson(addrs)
+	_, err := zk.CreateRecursive(zkts.zconn, path, data, 0, zookeeper.WorldACL(zookeeper.PERM_ALL))
+	if err != nil {
+		if zookeeper.IsError(err, zookeeper.ZNODEEXISTS) {
+			// Node already exists - just stomp away. Multiple writers shouldn't be here.
+			// We use RetryChange here because it won't update the node unnecessarily.
+			f := func(oldValue string, oldStat zk.Stat) (string, error) {
+				return data, nil
+			}
+			err = zkts.zconn.RetryChange(path, 0, zookeeper.WorldACL(zookeeper.PERM_ALL), f)
+		}
+	}
+	return err
+}
+
+func (zkts *Server) GetSrvTabletType(cell, keyspace, shard string, tabletType topo.TabletType) (*topo.VtnsAddrs, error) {
+	path := zkPathForVtName(cell, keyspace, shard, tabletType)
+	data, stat, err := zkts.zconn.Get(path)
+	if err != nil {
+		if zookeeper.IsError(err, zookeeper.ZNONODE) {
+			err = topo.ErrNoNode
+		}
+		return nil, err
+	}
+	return topo.NewVtnsAddrs(data, stat.Version())
+}
+
+func (zkts *Server) DeleteSrvTabletType(cell, keyspace, shard string, tabletType topo.TabletType) error {
+	path := zkPathForVtName(cell, keyspace, shard, tabletType)
+	err := zkts.zconn.Delete(path, -1)
+	if err != nil {
+		if zookeeper.IsError(err, zookeeper.ZNONODE) {
+			err = topo.ErrNoNode
+		}
+		return err
+	}
+	return nil
+}
+
+func (zkts *Server) UpdateSrvShard(cell, keyspace, shard string, srvShard *topo.SrvShard) error {
+	path := zkPathForVtShard(cell, keyspace, shard)
+	data := jscfg.ToJson(srvShard)
+	_, err := zkts.zconn.Set(path, data, -1)
+	return err
+}
+
+func (zkts *Server) GetSrvShard(cell, keyspace, shard string) (*topo.SrvShard, error) {
+	path := zkPathForVtShard(cell, keyspace, shard)
+	data, stat, err := zkts.zconn.Get(path)
+	if err != nil {
+		if zookeeper.IsError(err, zookeeper.ZNONODE) {
+			err = topo.ErrNoNode
+		}
+		return nil, err
+	}
+	return topo.NewSrvShard(data, stat.Version())
+}
+
+func (zkts *Server) UpdateSrvKeyspace(cell, keyspace string, srvKeyspace *topo.SrvKeyspace) error {
+	path := zkPathForVtKeyspace(cell, keyspace)
+	data := jscfg.ToJson(srvKeyspace)
+	_, err := zkts.zconn.Set(path, data, -1)
+	return err
+}
+
+func (zkts *Server) GetSrvKeyspace(cell, keyspace string) (*topo.SrvKeyspace, error) {
+	path := zkPathForVtKeyspace(cell, keyspace)
+	data, stat, err := zkts.zconn.Get(path)
+	if err != nil {
+		if zookeeper.IsError(err, zookeeper.ZNONODE) {
+			err = topo.ErrNoNode
+		}
+		return nil, err
+	}
+	return topo.NewSrvKeyspace(data, stat.Version())
+}
+
+var skipUpdateErr = fmt.Errorf("skip update")
+
+func (zkts *Server) updateTabletEndpoint(oldValue string, oldStat zk.Stat, addr *topo.VtnsAddr) (newValue string, err error) {
+	if oldStat == nil {
+		// The incoming object doesn't exist - we haven't been placed in the serving
+		// graph yet, so don't update. Assume the next process that rebuilds the graph
+		// will get the updated tablet location.
+		return "", skipUpdateErr
+	}
+
+	var addrs *topo.VtnsAddrs
+	if oldValue != "" {
+		addrs, err = topo.NewVtnsAddrs(oldValue, oldStat.Version())
+		if err != nil {
+			return
+		}
+
+		foundTablet := false
+		for i, entry := range addrs.Entries {
+			if entry.Uid == addr.Uid {
+				foundTablet = true
+				if !topo.VtnsAddrEquality(&entry, addr) {
+					addrs.Entries[i] = *addr
+				}
+				break
+			}
+		}
+
+		if !foundTablet {
+			addrs.Entries = append(addrs.Entries, *addr)
+		}
+	} else {
+		addrs = topo.NewAddrs()
+		addrs.Entries = append(addrs.Entries, *addr)
+	}
+	return jscfg.ToJson(addrs), nil
+}
+
+func (zkts *Server) UpdateTabletEndpoint(cell, keyspace, shard string, tabletType topo.TabletType, addr *topo.VtnsAddr) error {
+	path := zkPathForVtName(cell, keyspace, shard, tabletType)
+	f := func(oldValue string, oldStat zk.Stat) (string, error) {
+		return zkts.updateTabletEndpoint(oldValue, oldStat, addr)
+	}
+	err := zkts.zconn.RetryChange(path, 0, zookeeper.WorldACL(zookeeper.PERM_ALL), f)
+	if err == skipUpdateErr {
+		err = nil
+	}
+	return err
+}
