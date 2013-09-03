fe8c0008c44e59822b8d4d34a7b1bd60b398f5b7
Alain Jobart <alainjobart@google.com>
2013-09-03 14:44:10 -0700

    Fixing this test.

--- go/vt/topo/test/testing.go
+++ go/vt/topo/test/testing.go
@@ -314,12 +314,13 @@ func CheckReplicationPaths(t *testing.T, ts topo.Server) {
 }
 
 func CheckServingGraph(t *testing.T, ts topo.Server) {
+	cell := getLocalCell(t, ts)
 
 	// test individual cell/keyspace/shard/type entries
-	if _, err := ts.GetSrvTabletTypesPerShard("cell", "test_keyspace", "-10"); err != topo.ErrNoNode {
+	if _, err := ts.GetSrvTabletTypesPerShard(cell, "test_keyspace", "-10"); err != topo.ErrNoNode {
 		t.Errorf("GetSrvTabletTypesPerShard(invalid): %v", err)
 	}
-	if _, err := ts.GetSrvTabletType("cell", "test_keyspace", "-10", topo.TYPE_MASTER); err != topo.ErrNoNode {
+	if _, err := ts.GetSrvTabletType(cell, "test_keyspace", "-10", topo.TYPE_MASTER); err != topo.ErrNoNode {
 		t.Errorf("GetSrvTabletType(invalid): %v", err)
 	}
 
@@ -333,36 +334,36 @@ func CheckServingGraph(t *testing.T, ts topo.Server) {
 		},
 	}
 
-	if err := ts.UpdateSrvTabletType("cell", "test_keyspace", "-10", topo.TYPE_MASTER, &vtnsAddrs); err != nil {
+	if err := ts.UpdateSrvTabletType(cell, "test_keyspace", "-10", topo.TYPE_MASTER, &vtnsAddrs); err != nil {
 		t.Errorf("UpdateSrvTabletType(master): %v", err)
 	}
-	if types, err := ts.GetSrvTabletTypesPerShard("cell", "test_keyspace", "-10"); err != nil || len(types) != 1 || types[0] != topo.TYPE_MASTER {
+	if types, err := ts.GetSrvTabletTypesPerShard(cell, "test_keyspace", "-10"); err != nil || len(types) != 1 || types[0] != topo.TYPE_MASTER {
 		t.Errorf("GetSrvTabletTypesPerShard(1): %v %v", err, types)
 	}
-	if addrs, err := ts.GetSrvTabletType("cell", "test_keyspace", "-10", topo.TYPE_MASTER); err != nil || len(addrs.Entries) != 1 || addrs.Entries[0].Uid != 1 {
+	if addrs, err := ts.GetSrvTabletType(cell, "test_keyspace", "-10", topo.TYPE_MASTER); err != nil || len(addrs.Entries) != 1 || addrs.Entries[0].Uid != 1 {
 		t.Errorf("GetSrvTabletType(1): %v %v", err, addrs)
 	}
 
-	if err := ts.UpdateTabletEndpoint("cell", "test_keyspace", "-10", topo.TYPE_REPLICA, &topo.VtnsAddr{Uid: 2, Host: "host2", Port: 2}); err != nil {
+	if err := ts.UpdateTabletEndpoint(cell, "test_keyspace", "-10", topo.TYPE_REPLICA, &topo.VtnsAddr{Uid: 2, Host: "host2", Port: 2}); err != nil {
 		t.Errorf("UpdateTabletEndpoint(invalid): %v", err)
 	}
-	if err := ts.UpdateTabletEndpoint("cell", "test_keyspace", "-10", topo.TYPE_MASTER, &topo.VtnsAddr{Uid: 1, Host: "host2", Port: 2}); err != nil {
+	if err := ts.UpdateTabletEndpoint(cell, "test_keyspace", "-10", topo.TYPE_MASTER, &topo.VtnsAddr{Uid: 1, Host: "host2", Port: 2}); err != nil {
 		t.Errorf("UpdateTabletEndpoint(master): %v", err)
 	}
-	if addrs, err := ts.GetSrvTabletType("cell", "test_keyspace", "-10", topo.TYPE_MASTER); err != nil || len(addrs.Entries) != 1 || addrs.Entries[0].Uid != 1 {
+	if addrs, err := ts.GetSrvTabletType(cell, "test_keyspace", "-10", topo.TYPE_MASTER); err != nil || len(addrs.Entries) != 1 || addrs.Entries[0].Uid != 1 {
 		t.Errorf("GetSrvTabletType(2): %v %v", err, addrs)
 	}
-	if err := ts.UpdateTabletEndpoint("cell", "test_keyspace", "-10", topo.TYPE_MASTER, &topo.VtnsAddr{Uid: 3, Host: "host3", Port: 3}); err != nil {
+	if err := ts.UpdateTabletEndpoint(cell, "test_keyspace", "-10", topo.TYPE_MASTER, &topo.VtnsAddr{Uid: 3, Host: "host3", Port: 3}); err != nil {
 		t.Errorf("UpdateTabletEndpoint(master): %v", err)
 	}
-	if addrs, err := ts.GetSrvTabletType("cell", "test_keyspace", "-10", topo.TYPE_MASTER); err != nil || len(addrs.Entries) != 2 {
+	if addrs, err := ts.GetSrvTabletType(cell, "test_keyspace", "-10", topo.TYPE_MASTER); err != nil || len(addrs.Entries) != 2 {
 		t.Errorf("GetSrvTabletType(2): %v %v", err, addrs)
 	}
 
-	if err := ts.DeleteSrvTabletType("cell", "test_keyspace", "-10", topo.TYPE_REPLICA); err != topo.ErrNoNode {
+	if err := ts.DeleteSrvTabletType(cell, "test_keyspace", "-10", topo.TYPE_REPLICA); err != topo.ErrNoNode {
 		t.Errorf("DeleteSrvTabletType(unknown): %v", err)
 	}
-	if err := ts.DeleteSrvTabletType("cell", "test_keyspace", "-10", topo.TYPE_MASTER); err != nil {
+	if err := ts.DeleteSrvTabletType(cell, "test_keyspace", "-10", topo.TYPE_MASTER); err != nil {
 		t.Errorf("DeleteSrvTabletType(master): %v", err)
 	}
 
@@ -370,13 +371,13 @@ func CheckServingGraph(t *testing.T, ts topo.Server) {
 	srvShard := topo.SrvShard{
 		ServedTypes: []topo.TabletType{topo.TYPE_MASTER},
 	}
-	if err := ts.UpdateSrvShard("cell", "test_keyspace", "-10", &srvShard); err != nil {
+	if err := ts.UpdateSrvShard(cell, "test_keyspace", "-10", &srvShard); err != nil {
 		t.Errorf("UpdateSrvShard(1): %v", err)
 	}
-	if _, err := ts.GetSrvShard("cell", "test_keyspace", "666"); err != topo.ErrNoNode {
+	if _, err := ts.GetSrvShard(cell, "test_keyspace", "666"); err != topo.ErrNoNode {
 		t.Errorf("GetSrvShard(invalid): %v", err)
 	}
-	if s, err := ts.GetSrvShard("cell", "test_keyspace", "-10"); err != nil || len(s.ServedTypes) != 1 || s.ServedTypes[0] != topo.TYPE_MASTER {
+	if s, err := ts.GetSrvShard(cell, "test_keyspace", "-10"); err != nil || len(s.ServedTypes) != 1 || s.ServedTypes[0] != topo.TYPE_MASTER {
 		t.Errorf("GetSrvShard(valid): %v", err)
 	}
 
@@ -384,13 +385,13 @@ func CheckServingGraph(t *testing.T, ts topo.Server) {
 	srvKeyspace := topo.SrvKeyspace{
 		TabletTypes: []topo.TabletType{topo.TYPE_MASTER},
 	}
-	if err := ts.UpdateSrvKeyspace("cell", "test_keyspace", &srvKeyspace); err != nil {
+	if err := ts.UpdateSrvKeyspace(cell, "test_keyspace", &srvKeyspace); err != nil {
 		t.Errorf("UpdateSrvKeyspace(1): %v", err)
 	}
-	if _, err := ts.GetSrvKeyspace("cell", "test_keyspace666"); err != topo.ErrNoNode {
+	if _, err := ts.GetSrvKeyspace(cell, "test_keyspace666"); err != topo.ErrNoNode {
 		t.Errorf("GetSrvKeyspace(invalid): %v", err)
 	}
-	if s, err := ts.GetSrvKeyspace("cell", "test_keyspace"); err != nil || len(s.TabletTypes) != 1 || s.TabletTypes[0] != topo.TYPE_MASTER {
+	if s, err := ts.GetSrvKeyspace(cell, "test_keyspace"); err != nil || len(s.TabletTypes) != 1 || s.TabletTypes[0] != topo.TYPE_MASTER {
 		t.Errorf("GetSrvKeyspace(valid): %v", err)
 	}
 
