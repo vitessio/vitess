59feb9dc053201eb85b7af05c8a1ea475435d866
Ryszard Szopa <szopa@google.com>
2013-09-04 16:28:23 -0700

    Add OnRun hook to servenv.

--- go/vt/servenv/run.go
+++ go/vt/servenv/run.go
@@ -3,15 +3,19 @@ package servenv
 import (
 	"fmt"
 	"net/http"
-	"sync"
 
 	log "github.com/golang/glog"
 	"github.com/youtube/vitess/go/proc"
 )
 
+var (
+	onCloseHooks hooks
+)
+
 // Run starts listening for RPC and HTTP requests on the given port,
 // and blocks until it the process gets a signal.
 func Run(port int) {
+	onRunHooks.Fire()
 	RunSecure(port, 0, "", "", "")
 }
 
@@ -19,6 +23,7 @@ func Run(port int) {
 // requests using TLS on securePort, using the passed certificate,
 // key, and CA certificate.
 func RunSecure(port int, securePort int, cert, key, caCert string) {
+	onRunHooks.Fire()
 	ServeRPC()
 
 	l, err := proc.Listen(fmt.Sprintf("%v", port))
@@ -38,25 +43,11 @@ func RunSecure(port int, securePort int, cert, key, caCert string) {
 
 // Close runs any registered exit hooks in parallel.
 func Close() {
-	mu.Lock()
-	defer mu.Unlock()
-
-	wg := sync.WaitGroup{}
-
-	for _, f := range onCloseHooks {
-		wg.Add(1)
-		go func(f func()) {
-			f()
-			wg.Done()
-		}(f)
-	}
-	wg.Wait()
+	onCloseHooks.Fire()
 }
 
 // OnClose registers f to be run at the end of the app lifecycle. All
 // hooks are run in parallel.
 func OnClose(f func()) {
-	mu.Lock()
-	defer mu.Unlock()
-	onCloseHooks = append(onCloseHooks, f)
+	onCloseHooks.Add(f)
 }
