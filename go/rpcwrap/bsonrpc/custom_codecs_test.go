c30d11050ced7948e98cdeb0631f34322fad2d04
Sugu Sougoumarane <sougou@google.com>
2013-10-22 16:07:08 -0700

    add one more missing test

--- go/rpcwrap/bsonrpc/custom_codecs_test.go
+++ go/rpcwrap/bsonrpc/custom_codecs_test.go
@@ -69,3 +69,69 @@ func TestRequestBson(t *testing.T) {
 		t.Errorf("want %v, got %v", want, err)
 	}
 }
+
+type reflectResponseBson struct {
+	ServiceMethod string
+	Seq           int64
+	Error         string
+}
+
+type badResponseBson struct {
+	Extra         int
+	ServiceMethod string
+	Seq           int64
+	Error         string
+}
+
+func TestResponseBson(t *testing.T) {
+	reflected, err := bson.Marshal(&reflectResponseBson{
+		ServiceMethod: "aa",
+		Seq:           1,
+		Error:         "err",
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	want := string(reflected)
+
+	custom := ResponseBson{
+		&rpc.Response{
+			ServiceMethod: "aa",
+			Seq:           1,
+			Error:         "err",
+		},
+	}
+	encoded, err := bson.Marshal(&custom)
+	if err != nil {
+		t.Error(err)
+	}
+	got := string(encoded)
+	if want != got {
+		t.Errorf("want\n%#v, got\n%#v", want, got)
+	}
+
+	unmarshalled := ResponseBson{Response: new(rpc.Response)}
+	err = bson.Unmarshal(encoded, &unmarshalled)
+	if err != nil {
+		t.Error(err)
+	}
+	if custom.ServiceMethod != unmarshalled.ServiceMethod {
+		t.Errorf("want %v, got %#v", custom.ServiceMethod, unmarshalled.ServiceMethod)
+	}
+	if custom.Seq != unmarshalled.Seq {
+		t.Errorf("want %v, got %#v", custom.Seq, unmarshalled.Seq)
+	}
+	if custom.Error != unmarshalled.Error {
+		t.Errorf("want %v, got %#v", custom.Error, unmarshalled.Error)
+	}
+
+	unexpected, err := bson.Marshal(&badResponseBson{})
+	if err != nil {
+		t.Error(err)
+	}
+	err = bson.Unmarshal(unexpected, &unmarshalled)
+	want = "Unrecognized tag Extra"
+	if err == nil || want != err.Error() {
+		t.Errorf("want %v, got %v", want, err)
+	}
+}
