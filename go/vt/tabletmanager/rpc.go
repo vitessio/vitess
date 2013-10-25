2dfe7c8a96003d8bae910a1cc00cdc4e871225a4
Alain Jobart <alainjobart@google.com>
2013-10-25 11:19:19 -0700

    Refactoring tabletmanager RPC system.
    It makes the actions type explicit. And also catches panics
    properly.

--- go/vt/tabletmanager/rpc.go
+++ go/vt/tabletmanager/rpc.go
@@ -17,14 +17,6 @@ import (
 )
 
 // This file contains the RPC methods for the tablet manager.
-// There are multiple kinds of actions:
-// 1 - read-only actions that can be executed in parallel.
-// 2 - read-write actions that change something, and need to take the
-//     action lock (they should Lock and Unlock agent.actionMutex)
-// 3 - read-write actions that also need to reload the tablet state.
-//     (they should use both actionMutex lock and run
-//     agent.afterAction() if the action is successful, using wrapErrForAction)
-// All actions are type 1 unless otherwise noted.
 
 // rpcTimeout is used for timing out the queries on the server in a
 // reasonable amount of time. The actions are stored in the
@@ -57,23 +49,61 @@ func (agent *ActionAgent) RegisterQueryService(mysqld *mysqlctl.Mysqld) {
 	rpcwrap.RegisterAuthenticated(TabletManagerRpcService)
 }
 
-// wrapErr is to use with Type 1 and Type 2 actions.
-func (tm *TabletManager) wrapErr(context *rpcproto.Context, name string, args interface{}, reply interface{}, err error) error {
-	if err != nil {
+func (tm *TabletManager) rpcWrapper(context *rpcproto.Context, name string, args, reply interface{}, f func() error, lock, runAfterAction, reloadSchema bool) (err error) {
+	defer func() {
+		if x := recover(); x != nil {
+			log.Errorf("TabletManager.%v(%v) panic: %v", name, args, x)
+			err = fmt.Errorf("caught panic during %v: %v", name, x)
+		}
+	}()
+
+	if lock {
+		beforeLock := time.Now()
+		tm.agent.actionMutex.Lock()
+		defer tm.agent.actionMutex.Unlock()
+		if time.Now().Sub(beforeLock) > rpcTimeout {
+			return fmt.Errorf("server timeout for " + name)
+		}
+	}
+
+	if err = f(); err != nil {
 		log.Warningf("TabletManager.%v(%v)(from %v) error: %v", name, args, context.RemoteAddr, err.Error())
-		return fmt.Errorf("%v (on %v)", err, tm.agent.tabletAlias)
+		return fmt.Errorf("TabletManager.%v on %v error: %v", name, tm.agent.tabletAlias, err)
 	}
 	log.Infof("TabletManager.%v(%v)(from %v): %v", name, args, context.RemoteAddr, reply)
-	return nil
-}
-
-// wrapErrForAction is to use with type 3 actions.
-func (tm *TabletManager) wrapErrForAction(context *rpcproto.Context, name string, args interface{}, reply interface{}, reloadSchema bool, err error) error {
-	err = tm.wrapErr(context, name, args, reply, err)
-	if err == nil {
+	if runAfterAction {
 		tm.agent.afterAction("RPC("+name+")", reloadSchema)
 	}
-	return err
+	return
+}
+
+// There are multiple kinds of actions:
+// 1 - read-only actions that can be executed in parallel.
+// 2 - read-write actions that change something, and need to take the
+//     action lock.
+// 3 - read-write actions that need to take the action lock, and also
+//     need to reload the tablet state.
+// 4 - read-write actions that need to take the action lock, need to
+//     reload the tablet state, and reload the schema afterwards.
+
+func (tm *TabletManager) type1Wrapper(context *rpcproto.Context, name string, args, reply interface{}, f func() error) error {
+	return tm.rpcWrapper(context, name, args, reply, f,
+		false /*lock*/, false /*runAfterAction*/, false /*reloadSchema*/)
+}
+
+func (tm *TabletManager) type2Wrapper(context *rpcproto.Context, name string, args, reply interface{}, f func() error) error {
+	return tm.rpcWrapper(context, name, args, reply, f,
+		true /*lock*/, false /*runAfterAction*/, false /*reloadSchema*/)
+}
+
+func (tm *TabletManager) type3Wrapper(context *rpcproto.Context, name string, args, reply interface{}, f func() error) error {
+	return tm.rpcWrapper(context, name, args, reply, f,
+		true /*lock*/, true /*runAfterAction*/, false /*reloadSchema*/)
+}
+
+func (tm *TabletManager) type4Wrapper(context *rpcproto.Context, name string, args, reply interface{}, f func() error) error {
+	return tm.rpcWrapper(context, name, args, reply, f,
+		true /*lock*/, true /*runAfterAction*/, true /*reloadSchema*/)
 }
 
 //
@@ -81,48 +111,47 @@ func (tm *TabletManager) wrapErrForAction(context *rpcproto.Context, name string
 //
 
 func (tm *TabletManager) Ping(context *rpcproto.Context, args, reply *string) error {
-	*reply = *args
-	return tm.wrapErr(context, TABLET_ACTION_PING, *args, *reply, nil)
+	return tm.type1Wrapper(context, TABLET_ACTION_PING, args, reply, func() error {
+		*reply = *args
+		return nil
+	})
 }
 
 func (tm *TabletManager) GetSchema(context *rpcproto.Context, args *GetSchemaArgs, reply *mysqlctl.SchemaDefinition) error {
-	// read the tablet to get the dbname
-	tablet, err := tm.agent.ts.GetTablet(tm.agent.tabletAlias)
-	if err != nil {
-		return tm.wrapErr(context, TABLET_ACTION_GET_SCHEMA, args, reply, err)
-	}
+	return tm.type1Wrapper(context, TABLET_ACTION_GET_SCHEMA, args, reply, func() error {
+		// read the tablet to get the dbname
+		tablet, err := tm.agent.ts.GetTablet(tm.agent.tabletAlias)
+		if err != nil {
+			return err
+		}
 
-	// and get the schema
-	sd, err := tm.mysqld.GetSchema(tablet.DbName(), args.Tables, args.IncludeViews)
-	if err == nil {
-		*reply = *sd
-	}
-	return tm.wrapErr(context, TABLET_ACTION_GET_SCHEMA, args, reply, err)
+		// and get the schema
+		sd, err := tm.mysqld.GetSchema(tablet.DbName(), args.Tables, args.IncludeViews)
+		if err == nil {
+			*reply = *sd
+		}
+		return err
+	})
 }
 
 func (tm *TabletManager) GetPermissions(context *rpcproto.Context, args *rpc.UnusedRequest, reply *mysqlctl.Permissions) error {
-	p, err := tm.mysqld.GetPermissions()
-	if err == nil {
-		*reply = *p
-	}
-	return tm.wrapErr(context, TABLET_ACTION_GET_PERMISSIONS, args, reply, err)
+	return tm.type1Wrapper(context, TABLET_ACTION_GET_PERMISSIONS, args, reply, func() error {
+		p, err := tm.mysqld.GetPermissions()
+		if err == nil {
+			*reply = *p
+		}
+		return err
+	})
 }
 
 //
 // Various read-write methods
 //
 
-// ChangeType is a Type 3 action: takes the action lock, and
-// update tablet state
 func (tm *TabletManager) ChangeType(context *rpcproto.Context, args *topo.TabletType, reply *rpc.UnusedResponse) error {
-	beforeLock := time.Now()
-	tm.agent.actionMutex.Lock()
-	defer tm.agent.actionMutex.Unlock()
-	if time.Now().Sub(beforeLock) > rpcTimeout {
-		return fmt.Errorf("server timeout for ChangeType")
-	}
-
-	return tm.wrapErrForAction(context, TABLET_ACTION_CHANGE_TYPE, args, reply, false /*reloadSchema*/, ChangeType(tm.agent.ts, tm.agent.tabletAlias, *args, true /*runHooks*/))
+	return tm.type3Wrapper(context, TABLET_ACTION_CHANGE_TYPE, args, reply, func() error {
+		return ChangeType(tm.agent.ts, tm.agent.tabletAlias, *args, true /*runHooks*/)
+	})
 }
 
 //
@@ -130,50 +159,51 @@ func (tm *TabletManager) ChangeType(context *rpcproto.Context, args *topo.Tablet
 //
 
 func (tm *TabletManager) SlavePosition(context *rpcproto.Context, args *rpc.UnusedRequest, reply *mysqlctl.ReplicationPosition) error {
-	position, err := tm.mysqld.SlaveStatus()
-	if err == nil {
-		*reply = *position
-	}
-	return tm.wrapErr(context, TABLET_ACTION_SLAVE_POSITION, args, reply, err)
+	return tm.type1Wrapper(context, TABLET_ACTION_SLAVE_POSITION, args, reply, func() error {
+		position, err := tm.mysqld.SlaveStatus()
+		if err == nil {
+			*reply = *position
+		}
+		return err
+	})
 }
 
 func (tm *TabletManager) WaitSlavePosition(context *rpcproto.Context, args *SlavePositionReq, reply *mysqlctl.ReplicationPosition) error {
-	if err := tm.mysqld.WaitMasterPos(&args.ReplicationPosition, args.WaitTimeout); err != nil {
-		return tm.wrapErr(context, TABLET_ACTION_WAIT_SLAVE_POSITION, args, reply, err)
-	}
+	return tm.type1Wrapper(context, TABLET_ACTION_WAIT_SLAVE_POSITION, args, reply, func() error {
+		if err := tm.mysqld.WaitMasterPos(&args.ReplicationPosition, args.WaitTimeout); err != nil {
+			return err
+		}
 
-	position, err := tm.mysqld.SlaveStatus()
-	if err != nil {
-		return tm.wrapErr(context, TABLET_ACTION_WAIT_SLAVE_POSITION, args, reply, err)
-	}
-	*reply = *position
-	return tm.wrapErr(context, TABLET_ACTION_WAIT_SLAVE_POSITION, args, reply, nil)
+		position, err := tm.mysqld.SlaveStatus()
+		if err == nil {
+			*reply = *position
+		}
+		return err
+	})
 }
 
 func (tm *TabletManager) MasterPosition(context *rpcproto.Context, args *rpc.UnusedRequest, reply *mysqlctl.ReplicationPosition) error {
-	position, err := tm.mysqld.MasterStatus()
-	if err == nil {
-		*reply = *position
-	}
-	return tm.wrapErr(context, TABLET_ACTION_MASTER_POSITION, args, reply, err)
+	return tm.type1Wrapper(context, TABLET_ACTION_MASTER_POSITION, args, reply, func() error {
+		position, err := tm.mysqld.MasterStatus()
+		if err == nil {
+			*reply = *position
+		}
+		return err
+	})
 }
 
-// StopSlave is a Type 2 action: takes the action lock, but doesn't
-// update tablet state
 func (tm *TabletManager) StopSlave(context *rpcproto.Context, args *rpc.UnusedRequest, reply *rpc.UnusedResponse) error {
-	beforeLock := time.Now()
-	tm.agent.actionMutex.Lock()
-	defer tm.agent.actionMutex.Unlock()
-	if time.Now().Sub(beforeLock) > rpcTimeout {
-		return fmt.Errorf("server timeout for StopSlave")
-	}
-
-	return tm.wrapErr(context, TABLET_ACTION_STOP_SLAVE, args, reply, tm.mysqld.StopSlave(map[string]string{"TABLET_ALIAS": tm.agent.tabletAlias.String()}))
+	return tm.type2Wrapper(context, TABLET_ACTION_STOP_SLAVE, args, reply, func() error {
+		return tm.mysqld.StopSlave(map[string]string{"TABLET_ALIAS": tm.agent.tabletAlias.String()})
+	})
 }
 
-func (tm *TabletManager) GetSlaves(context *rpcproto.Context, args *rpc.UnusedRequest, reply *SlaveList) (err error) {
-	reply.Addrs, err = tm.mysqld.FindSlaves()
-	return tm.wrapErr(context, TABLET_ACTION_GET_SLAVES, args, reply, err)
+func (tm *TabletManager) GetSlaves(context *rpcproto.Context, args *rpc.UnusedRequest, reply *SlaveList) error {
+	return tm.type1Wrapper(context, TABLET_ACTION_GET_SLAVES, args, reply, func() error {
+		var err error
+		reply.Addrs, err = tm.mysqld.FindSlaves()
+		return err
+	})
 }
 
 type WaitBlpPositionArgs struct {
@@ -182,35 +212,23 @@ type WaitBlpPositionArgs struct {
 }
 
 func (tm *TabletManager) WaitBlpPosition(context *rpcproto.Context, args *WaitBlpPositionArgs, reply *rpc.UnusedResponse) error {
-	return tm.wrapErr(context, TABLET_ACTION_WAIT_BLP_POSITION, args, reply, tm.mysqld.WaitBlpPos(&args.BlpPosition, args.WaitTimeout))
+	return tm.type1Wrapper(context, TABLET_ACTION_WAIT_BLP_POSITION, args, reply, func() error {
+		return tm.mysqld.WaitBlpPos(&args.BlpPosition, args.WaitTimeout)
+	})
 }
 
 //
 // Reparenting related functions
 //
 
-// SlaveWasPromoted is a Type 3 action: takes the action lock, and
-// update tablet state
 func (tm *TabletManager) SlaveWasPromoted(context *rpcproto.Context, args *rpc.UnusedRequest, reply *rpc.UnusedResponse) error {
-	beforeLock := time.Now()
-	tm.agent.actionMutex.Lock()
-	defer tm.agent.actionMutex.Unlock()
-	if time.Now().Sub(beforeLock) > rpcTimeout {
-		return fmt.Errorf("server timeout for SlaveWasPromoted")
-	}
-
-	return tm.wrapErrForAction(context, TABLET_ACTION_SLAVE_WAS_PROMOTED, args, reply, false /*reloadSchema*/, slaveWasPromoted(tm.agent.ts, tm.agent.tabletAlias))
+	return tm.type3Wrapper(context, TABLET_ACTION_SLAVE_WAS_PROMOTED, args, reply, func() error {
+		return slaveWasPromoted(tm.agent.ts, tm.agent.tabletAlias)
+	})
 }
 
-// SlaveWasRestarted is a Type 3 action: takes the action lock, and
-// update tablet state
 func (tm *TabletManager) SlaveWasRestarted(context *rpcproto.Context, args *SlaveWasRestartedData, reply *rpc.UnusedResponse) error {
-	beforeLock := time.Now()
-	tm.agent.actionMutex.Lock()
-	defer tm.agent.actionMutex.Unlock()
-	if time.Now().Sub(beforeLock) > rpcTimeout {
-		return fmt.Errorf("server timeout for SlaveWasRestarted")
-	}
-
-	return tm.wrapErrForAction(context, TABLET_ACTION_SLAVE_WAS_RESTARTED, args, reply, false /*reloadSchema*/, slaveWasRestarted(tm.agent.ts, tm.mysqld, tm.agent.tabletAlias, args))
+	return tm.type3Wrapper(context, TABLET_ACTION_SLAVE_WAS_RESTARTED, args, reply, func() error {
+		return slaveWasRestarted(tm.agent.ts, tm.mysqld, tm.agent.tabletAlias, args)
+	})
 }
