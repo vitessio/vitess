33d2bf32e42aa530d6516b13930b85fc9d6814b5
shrutip <shrutip@google.com>
2013-10-29 00:18:14 -0700

    Topo integration with vtgate. Also added mechanism to use topo with zkocc.

---
+++ go/cmd/vtgate/plugin_zkocctopo.go
@@ -0,0 +1,20 @@
+// Copyright 2013, Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+// Imports and register the Zookeeper TopologyServer with Zkocc Connection
+
+import (
+	"github.com/youtube/vitess/go/stats"
+	"github.com/youtube/vitess/go/vt/topo"
+	"github.com/youtube/vitess/go/zk"
+	"github.com/youtube/vitess/go/vt/zktopo"
+)
+
+func init() {
+	zkoccconn := zk.NewMetaConn(true)
+	stats.Publish("ZkOccConn", zkoccconn)
+	topo.RegisterServer("zkocc", zktopo.NewServer(zkoccconn))
+}
