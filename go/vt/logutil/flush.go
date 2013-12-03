41e83e615f88049bbfe60e3bf80215249e9c2a84
Alain Jobart <alainjobart@google.com>
2013-12-02 17:10:19 -0800

    Adding an abstraction for log flush.

---
+++ go/vt/logutil/flush.go
@@ -0,0 +1,14 @@
+// Copyright 2013, Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package logutil
+
+import (
+	log "github.com/golang/glog"
+)
+
+// Flush flushes all open log files
+func Flush() {
+	log.Flush()
+}
