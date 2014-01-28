11690e9a6754a116a3cde40f4852288a148340fe
Alain Jobart <alainjobart@google.com>
2014-01-28 11:14:58 -0800

    refactoring this test a bit.

--- test/update_stream.py
+++ test/update_stream.py
@@ -23,21 +23,41 @@ from vtdb import vtclient
 from zk import zkocc
 
 
-master_tablet = tablet.Tablet(62344)
-replica_tablet = tablet.Tablet(62345)
+master_tablet = tablet.Tablet()
+replica_tablet = tablet.Tablet()
 master_host = "localhost:%u" % master_tablet.port
-replica_host = "localhost:%u" % replica_tablet.port
-zkocc_client = zkocc.ZkOccConnection("localhost:%u" % environment.zkocc_port_base,
-                                     "test_nj", 30.0)
+
+vtgate_server = None
+vtgate_port = None
+
 master_start_position = None
 
+_create_vt_insert_test = '''create table if not exists vt_insert_test (
+id bigint auto_increment,
+msg varchar(64),
+primary key (id)
+) Engine=InnoDB'''
+
+_create_vt_a = '''create table if not exists vt_a (
+eid bigint,
+id int,
+primary key(eid, id)
+) Engine=InnoDB'''
+
+_create_vt_b = '''create table if not exists vt_b (
+eid bigint,
+name varchar(128),
+foo varbinary(128),
+primary key(eid, name)
+) Engine=InnoDB'''
+
 def _get_master_current_position():
-  return utils.mysql_query(62344, 'vt_test_keyspace', 'show master status')[0][4]
+  return utils.mysql_query(master_tablet.tablet_uid, 'vt_test_keyspace', 'show master status')[0][4]
 
 
 def _get_repl_current_position():
   conn = MySQLdb.Connect(user='vt_dba',
-                         unix_socket=os.path.join(environment.vtdataroot, 'vt_%010d/mysql.sock' % 62345),
+                         unix_socket=os.path.join(environment.vtdataroot, 'vt_%010d/mysql.sock' % replica_tablet.tablet_uid),
                          db='vt_test_keyspace')
   cursor = MySQLdb.cursors.DictCursor(conn)
   cursor.execute('show master status')
@@ -46,88 +66,110 @@ def _get_repl_current_position():
 
 
 def setUpModule():
-  try:
-    utils.zk_setup()
+  global vtgate_server
+  global vtgate_port
+  global master_start_position
 
-  # start mysql instance external to the test
+  try:
+    environment.topo_server_setup()
+
+    # start mysql instance external to the test
     setup_procs = [master_tablet.init_mysql(),
                    replica_tablet.init_mysql()
                    ]
     utils.wait_procs(setup_procs)
-    setup_tablets()
+
+    # Start up a master mysql and vttablet
+    logging.debug("Setting up tablets")
+    utils.run_vtctl(['CreateKeyspace', 'test_keyspace'])
+    master_tablet.init_tablet('master', 'test_keyspace', '0')
+    replica_tablet.init_tablet('replica', 'test_keyspace', '0')
+    utils.run_vtctl(['RebuildShardGraph', 'test_keyspace/0'])
+    utils.validate_topology()
+    master_tablet.create_db('vt_test_keyspace')
+    replica_tablet.create_db('vt_test_keyspace')
+
+    utils.run_vtctl(['RebuildKeyspaceGraph', 'test_keyspace'])
+
+    vtgate_server, vtgate_port = utils.vtgate_start()
+
+    master_tablet.start_vttablet()
+    replica_tablet.start_vttablet()
+    utils.run_vtctl(['SetReadWrite', master_tablet.tablet_alias])
+    utils.check_db_read_write(master_tablet.tablet_uid)
+
+    for t in [master_tablet, replica_tablet]:
+      t.reset_replication()
+    utils.run_vtctl(['ReparentShard', '-force', 'test_keyspace/0',
+                     master_tablet.tablet_alias], auto_log=True)
+
+    # reset counter so tests don't assert
+    tablet.Tablet.tablets_running = 0
+
+    master_start_position = _get_master_current_position()
+    master_tablet.mquery('vt_test_keyspace', _create_vt_insert_test)
+    master_tablet.mquery('vt_test_keyspace', _create_vt_a)
+    master_tablet.mquery('vt_test_keyspace', _create_vt_b)
+
+    master_tablet.vquery("set vt_schema_reload_time=86400",
+                         path="test_keyspace/0")
+    replica_tablet.vquery("set vt_schema_reload_time=86400",
+                          path="test_keyspace/0")
+
   except:
     tearDownModule()
     raise
-
 
 def tearDownModule():
   if utils.options.skip_teardown:
     return
   logging.debug("Tearing down the servers and setup")
+  tablet.Tablet.tablets_running = 2
+  tablet.kill_tablets([master_tablet, replica_tablet])
   teardown_procs = [master_tablet.teardown_mysql(),
                     replica_tablet.teardown_mysql()]
   utils.wait_procs(teardown_procs, raise_on_error=False)
 
-  utils.zk_teardown()
-  tablet.Tablet.tablets_running = 2
-  tablet.kill_tablets([master_tablet, replica_tablet])
+  utils.vtgate_kill(vtgate_server)
+  environment.topo_server_teardown()
   utils.kill_sub_processes()
   utils.remove_tmp_files()
   master_tablet.remove_tree()
   replica_tablet.remove_tree()
 
-def setup_tablets():
-  # Start up a master mysql and vttablet
-  logging.debug("Setting up tablets")
-  utils.run_vtctl('CreateKeyspace test_keyspace')
-  master_tablet.init_tablet('master', 'test_keyspace', '0')
-  replica_tablet.init_tablet('replica', 'test_keyspace', '0')
-  utils.run_vtctl('RebuildShardGraph test_keyspace/0')
-  utils.validate_topology()
-  master_tablet.create_db('vt_test_keyspace')
-  replica_tablet.create_db('vt_test_keyspace')
-
-  utils.run_vtctl('RebuildKeyspaceGraph test_keyspace')
-  zkocc_server = utils.zkocc_start()
-
-  master_tablet.start_vttablet()
-  replica_tablet.start_vttablet()
-  utils.run_vtctl('SetReadWrite ' + master_tablet.tablet_alias)
-  utils.check_db_read_write(62344)
-
-  for t in [master_tablet, replica_tablet]:
-    t.reset_replication()
-  utils.run_vtctl('ReparentShard -force test_keyspace/0 ' + master_tablet.tablet_alias, auto_log=True)
-
-  # reset counter so tests don't assert
-  tablet.Tablet.tablets_running = 0
-  setup_schema()
-  master_tablet.vquery("set vt_schema_reload_time=86400", path="test_keyspace/0")
-  replica_tablet.vquery("set vt_schema_reload_time=86400", path="test_keyspace/0")
-
-def setup_schema():
-  global master_start_position
-  master_start_position = _get_master_current_position()
-  master_tablet.mquery('vt_test_keyspace', _create_vt_insert_test)
-  master_tablet.mquery('vt_test_keyspace', _create_vt_a)
-  master_tablet.mquery('vt_test_keyspace', _create_vt_b)
-
 class TestUpdateStream(unittest.TestCase):
+  _populate_vt_insert_test = [
+      "insert into vt_insert_test (msg) values ('test %s')" % x
+      for x in xrange(4)]
+
+  def _populate_vt_a(self, count):
+    return ["insert into vt_a (eid, id) values (%d, %d)" % (x, x)
+            for x in xrange(count+1) if x >0]
+
+  def _populate_vt_b(self, count):
+    return ["insert into vt_b (eid, name, foo) values (%d, 'name %s', 'foo %s')" % (x, x, x)
+            for x in xrange(count)]
+
+  def setUp(self):
+    self.vtgate_client = zkocc.ZkOccConnection("localhost:%u" % vtgate_port,
+                                               "test_nj", 30.0)
+
+  def tearDown(self):
+    self.vtgate_client.close()
 
   def _get_master_stream_conn(self):
-    #return update_stream_service.UpdateStreamConnection(master_host, 30, user="ala", password="ma kota")
     return update_stream_service.UpdateStreamConnection(master_host, 30)
 
   def _get_replica_stream_conn(self):
-    #return update_stream_service.UpdateStreamConnection(replica_host, 30, user="ala", password="ma kota")
-    return update_stream_service.UpdateStreamConnection(replica_host, 30)
+    return update_stream_service.UpdateStreamConnection("localhost:%u" %
+                                                        replica_tablet.port, 30)
 
 
   def _test_service_disabled(self):
     start_position = _get_repl_current_position()
     logging.debug("_test_service_disabled starting @ %s" % start_position)
-    self._exec_vt_txn(master_host, _populate_vt_insert_test)
-    self._exec_vt_txn(master_host, ['delete from vt_insert_test',])
+    self._exec_vt_txn(self._populate_vt_insert_test)
+    self._exec_vt_txn(['delete from vt_insert_test',])
     utils.run_vtctl(['ChangeSlaveType', replica_tablet.tablet_alias, 'spare'])
     #  time.sleep(20)
     replica_conn = self._get_replica_stream_conn()
@@ -144,12 +186,13 @@ class TestUpdateStream(unittest.TestCase):
 
     v = utils.get_vars(replica_tablet.port)
     if v['UpdateStreamState'] != 'Disabled':
-      self.fail("Update stream service should be 'Disabled' but is '%s'" % v['UpdateStreamState'])
+      self.fail("Update stream service should be 'Disabled' but is '%s'" %
+                v['UpdateStreamState'])
 
   def perform_writes(self, count):
     for i in xrange(count):
-      self._exec_vt_txn(master_host, _populate_vt_insert_test)
-      self._exec_vt_txn(master_host, ['delete from vt_insert_test',])
+      self._exec_vt_txn(self._populate_vt_insert_test)
+      self._exec_vt_txn(['delete from vt_insert_test',])
 
 
   def _test_service_enabled(self):
@@ -158,7 +201,8 @@ class TestUpdateStream(unittest.TestCase):
     utils.run_vtctl(['ChangeSlaveType', replica_tablet.tablet_alias, 'replica'])
     logging.debug("sleeping a bit for the replica action to complete")
     time.sleep(10)
-    thd = threading.Thread(target=self.perform_writes, name='write_thd', args=(400,))
+    thd = threading.Thread(target=self.perform_writes, name='write_thd',
+                           args=(400,))
     thd.daemon = True
     thd.start()
     replica_conn = self._get_replica_stream_conn()
@@ -172,16 +216,19 @@ class TestUpdateStream(unittest.TestCase):
           logging.debug("Test Service Enabled: Pass")
           break
     except Exception, e:
-      self.fail("Exception in getting stream from replica: %s\n Traceback %s" % (str(e), traceback.print_exc()))
+      self.fail("Exception in getting stream from replica: %s\n Traceback %s" %
+                (str(e), traceback.print_exc()))
     thd.join(timeout=30)
 
     v = utils.get_vars(replica_tablet.port)
     if v['UpdateStreamState'] != 'Enabled':
-      self.fail("Update stream service should be 'Enabled' but is '%s'" % v['UpdateStreamState'])
+      self.fail("Update stream service should be 'Enabled' but is '%s'" %
+                v['UpdateStreamState'])
     self.assertTrue('DML' in v['UpdateStreamEvents'])
     self.assertTrue('POS' in v['UpdateStreamEvents'])
 
-    logging.debug("Testing enable -> disable switch starting @ %s" % start_position)
+    logging.debug("Testing enable -> disable switch starting @ %s" %
+                  start_position)
     replica_conn = self._get_replica_stream_conn()
     replica_conn.dial()
     disabled_err = False
@@ -206,14 +253,15 @@ class TestUpdateStream(unittest.TestCase):
     logging.debug("Streamed %d transactions before exiting" % txn_count)
 
   def _vtdb_conn(self, host):
-    conn = vtclient.VtOCCConnection(zkocc_client, 'test_keyspace', '0', "master", 30)
+    conn = vtclient.VtOCCConnection(self.vtgate_client, 'test_keyspace', '0',
+                                    'master', 30)
     conn.connect()
     return conn
 
-  def _exec_vt_txn(self, host, query_list=None):
+  def _exec_vt_txn(self, query_list=None):
     if not query_list:
       return
-    vtdb_conn = self._vtdb_conn(host)
+    vtdb_conn = self._vtdb_conn("localhost:%u" % master_tablet.port)
     vtdb_cursor = vtdb_conn.cursor()
     vtdb_conn.begin()
     for q in query_list:
@@ -226,13 +274,14 @@ class TestUpdateStream(unittest.TestCase):
   def test_stream_parity(self):
     master_start_position = _get_master_current_position()
     replica_start_position = _get_repl_current_position()
-    logging.debug("run_test_stream_parity starting @ %s" % master_start_position)
+    logging.debug("run_test_stream_parity starting @ %s" %
+                  master_start_position)
     master_txn_count = 0
     replica_txn_count = 0
-    self._exec_vt_txn(master_host, _populate_vt_a(15))
-    self._exec_vt_txn(master_host, _populate_vt_b(14))
-    self._exec_vt_txn(master_host, ['delete from vt_a',])
-    self._exec_vt_txn(master_host, ['delete from vt_b',])
+    self._exec_vt_txn(self._populate_vt_a(15))
+    self._exec_vt_txn(self._populate_vt_b(14))
+    self._exec_vt_txn(['delete from vt_a',])
+    self._exec_vt_txn(['delete from vt_b',])
     master_conn = self._get_master_stream_conn()
     master_conn.dial()
     master_events = []
@@ -271,7 +320,8 @@ class TestUpdateStream(unittest.TestCase):
     master_conn = self._get_master_stream_conn()
     master_conn.dial()
     data = master_conn.stream_start(start_position)
-    self.assertEqual(data['Sql'], _create_vt_insert_test, "DDL didn't match original")
+    self.assertEqual(data['Sql'], _create_vt_insert_test,
+                     "DDL didn't match original")
 
   #This tests the service switch from disable -> enable -> disable
   def test_service_switch(self):
@@ -282,9 +332,9 @@ class TestUpdateStream(unittest.TestCase):
 
   def test_log_rotation(self):
     start_position = _get_master_current_position()
-    master_tablet.mquery('vt_test_keyspace', "flush logs")
-    self._exec_vt_txn(master_host, _populate_vt_a(15))
-    self._exec_vt_txn(master_host, ['delete from vt_a',])
+    master_tablet.mquery('vt_test_keyspace', 'flush logs')
+    self._exec_vt_txn(self._populate_vt_a(15))
+    self._exec_vt_txn(['delete from vt_a',])
     master_conn = self._get_master_stream_conn()
     master_conn.dial()
     data = master_conn.stream_start(start_position)
@@ -300,45 +350,6 @@ class TestUpdateStream(unittest.TestCase):
           break
     if not logs_correct:
       self.fail("Flush logs didn't get properly interpreted")
-
-_create_vt_insert_test = '''create table if not exists vt_insert_test (
-id bigint auto_increment,
-msg varchar(64),
-primary key (id)
-) Engine=InnoDB'''
-
-_populate_vt_insert_test = [
-    "insert into vt_insert_test (msg) values ('test %s')" % x
-    for x in xrange(4)]
-
-_create_vt_a = '''create table if not exists vt_a (
-eid bigint,
-id int,
-primary key(eid, id)
-) Engine=InnoDB'''
-
-def _populate_vt_a(count):
-  return ["insert into vt_a (eid, id) values (%d, %d)" % (x, x)
-          for x in xrange(count+1) if x >0]
-
-_create_vt_b = '''create table if not exists vt_b (
-eid bigint,
-name varchar(128),
-foo varbinary(128),
-primary key(eid, name)
-) Engine=InnoDB'''
-
-def _populate_vt_b(count):
-  return ["insert into vt_b (eid, name, foo) values (%d, 'name %s', 'foo %s')" % (x, x, x)
-          for x in xrange(count)]
-
-_create_vt_c = '''create table vt_c (
-eid bigint auto_increment,
-id int default 1,
-name varchar(128) default 'name',
-foo varchar(128),
-primary key(eid, id, name)
-) Engine=InnoDB'''
 
 if __name__ == '__main__':
   utils.main()
