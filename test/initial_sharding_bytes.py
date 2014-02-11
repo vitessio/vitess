6b60ca724ec0e78d9b9ced0ec680d3985803034a
Alain Jobart <alainjobart@google.com>
2014-02-11 11:11:32 -0800

    Adding a test for initial split of a shard.

---
+++ test/initial_sharding_bytes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#
+# Copyright 2013, Google Inc. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can
+# be found in the LICENSE file.
+
+import initial_sharding
+import utils
+
+# this test is just re-running an entire initial_sharding.py with a
+# varbinary keyspace_id
+if __name__ == '__main__':
+  initial_sharding.keyspace_id_type = "bytes"
+  utils.main(initial_sharding)
