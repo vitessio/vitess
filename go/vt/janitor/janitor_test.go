162cd5700e440974f20512da6eac341ba2ee3946
Ryszard Szopa <szopa@google.com>
2014-02-28 19:23:02 -0800

    Initial version of the vtjanitor framework.

---
+++ go/vt/janitor/janitor_test.go
@@ -0,0 +1,78 @@
+package janitor
+
+import (
+	"errors"
+	"math"
+	"testing"
+	"time"
+
+	"github.com/youtube/vitess/go/vt/topo"
+	"github.com/youtube/vitess/go/vt/wrangler"
+)
+
+//var testCells = []string{"oe", "wj"}
+
+type testJanitor struct {
+	run       func(bool) error
+	configure func(ts topo.Server, wr *wrangler.Wrangler, keyspace, shard string) error
+}
+
+func (janitor *testJanitor) Configure(ts topo.Server, wr *wrangler.Wrangler, keyspace, shard string) error {
+	return janitor.configure(ts, wr, keyspace, shard)
+}
+
+func (janitor *testJanitor) Run(active bool) error {
+	return janitor.run(active)
+}
+
+func newTestJanitor() *testJanitor {
+	return &testJanitor{
+		run: func(bool) error { return nil },
+		configure: func(ts topo.Server, wr *wrangler.Wrangler, keyspace, shard string) error {
+			return nil
+		},
+	}
+}
+
+func aroundSameDuration(d1, d2, delta time.Duration) bool {
+	return math.Abs(float64(d1-d2)) < float64(delta)
+}
+
+func TestJanitorInfo(t *testing.T) {
+	jan := newJanitorInfo(nil)
+	jan.RecordSuccess(time.Now().Add(-10 * time.Second))
+	jan.RecordSuccess(time.Now().Add(-20 * time.Second))
+	jan.RecordSuccess(time.Now().Add(-30 * time.Second))
+
+	if got, want := jan.AverageRuntime(), 20*time.Second; !aroundSameDuration(want, got, 500*time.Millisecond) {
+		t.Errorf("jan.AverageRuntime: want %v, got %v", want, got)
+	}
+}
+
+func TestRunJanitor(t *testing.T) {
+	scheduler, _ := New("a", "a", nil, nil, 0)
+	jan := newTestJanitor()
+	ji := newJanitorInfo(jan)
+	scheduler.janitors["test"] = ji
+
+	scheduler.runJanitor("test")
+
+	if ji.Runs() != 1 {
+		t.Errorf("jan.Runs: want 1, got %v", ji.Runs())
+	}
+
+	if ji.ErrorCount() != 0 {
+		t.Errorf("ji.ErrorCount: want 0, got %v", ji.ErrorCount())
+	}
+
+	jan.run = func(bool) error {
+		return errors.New("error")
+	}
+
+	scheduler.runJanitor("test")
+
+	if ji.ErrorCount() != 1 {
+		t.Errorf("ji.ErrorCount: want 0, got %v", ji.ErrorCount())
+	}
+
+}
