4fdfca084f1c64ded9738a3a362de43222088934
Alain Jobart <alainjobart@google.com>
2014-05-22 13:46:21 -0700

    Moving jquery includes to own file.

---
+++ go/vt/servenv/jquery.go
@@ -0,0 +1,8 @@
+package servenv
+
+// JQueryIncludes is the include to use to be able to use jquery and jquery-ui
+const JQueryIncludes = `
+<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+`
