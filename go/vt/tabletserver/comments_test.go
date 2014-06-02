eed7b2233a60f36a0a27c6e4f414ff9a5d661b84
Sugu Sougoumarane <sougou@google.com>
2014-06-02 09:45:42 -0700

    fix typos in comments test.

--- go/vt/tabletserver/comments_test.go
+++ go/vt/tabletserver/comments_test.go
@@ -1,4 +1,4 @@
-// Copyright 2012, Google Inc. All rights reserved.
+// Copyright 2014, Google Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -18,10 +18,12 @@ func TestComments(t *testing.T) {
 	}
 	stripTrailing(&query)
 	want := proto.Query{
-		Sql:           "foo",
-		BindVariables: map[string]interface{}{"_trailingComment": " /*** bar */"},
+		Sql: "foo",
+		BindVariables: map[string]interface{}{
+			"_trailingComment": " /*** bar */",
+		},
 	}
 	if !reflect.DeepEqual(query, want) {
-		t.Errorf("got \n%+v, want \n%+v", query, want)
+		t.Errorf("got\n%+v, want\n%+v", query, want)
 	}
 }
