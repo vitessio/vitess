df462879c8596eaf6777d2fb03a686c99aa7368b
Sugu Sougoumarane <sougou@google.com>
2014-06-02 00:54:58 -0700

    tabletserver: work around go 1.3 bug
    Also added a simple test for comments.go.

---
+++ go/vt/tabletserver/comments_test.go
@@ -0,0 +1,27 @@
+// Copyright 2012, Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package tabletserver
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/youtube/vitess/go/vt/tabletserver/proto"
+)
+
+func TestComments(t *testing.T) {
+	query := proto.Query{
+		Sql:           "foo /*** bar */",
+		BindVariables: make(map[string]interface{}),
+	}
+	stripTrailing(&query)
+	want := proto.Query{
+		Sql:           "foo",
+		BindVariables: map[string]interface{}{"_trailingComment": " /*** bar */"},
+	}
+	if !reflect.DeepEqual(query, want) {
+		t.Errorf("got \n%+v, want \n%+v", query, want)
+	}
+}
