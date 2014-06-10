cfbb0faac5bc8fd81c05dfc62d5a5165dbd070c8
Alain Jobart <alainjobart@google.com>
2014-06-10 12:27:56 -0700

    Using MapCounters for vtgate topo counts.

--- test/vertical_split_vtgate.py
+++ test/vertical_split_vtgate.py
@@ -55,6 +55,8 @@ class TestVerticalSplitVTGate(vertical_split.TestVerticalSplit):
     self.assertEqual(v['VttabletCall']['Histograms']['Execute.source_keyspace.0.replica']['Count'], 2, "unexpected value for VttabletCall(Execute.source_keyspace.0.replica) inside %s" % str(v))
     self.assertEqual(v['VtgateApi']['Histograms']['ExecuteKeyRanges.destination_keyspace.master']['Count'], 6, "unexpected value for VtgateApi(ExecuteKeyRanges.destination_keyspace.master) inside %s" % str(v))
     self.assertEqual(len(v['VtgateApiErrorCounts']), 0, "unexpected errors for VtgateApiErrorCounts inside %s" % str(v))
+    self.assertEqual(v['EndpointCount']['test_nj.source_keyspace.0.master'], 1, "unexpected EndpointCount inside %s" % str(v))
+    self.assertEqual(v['DegradedEndpointCount']['test_nj.source_keyspace.0.master'], 0, "unexpected DegradedEndpointCount inside %s" % str(v))
 
 if __name__ == '__main__':
   vertical_split.vtgate_protocol = vertical_split.VTGATE_PROTOCOL_V1BSON
