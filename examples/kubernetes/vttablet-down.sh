3555cdd8ac48e669fafd1f236324441532c6783d
Anthony Yeh <enisoc@google.com>
2014-10-30 14:34:50 -0700

    First pass at vttablet config for Kubernetes.

---
+++ examples/kubernetes/vttablet-down.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This is an example script that tears down the vttablet pods started by
+# vttablet-up.sh. It assumes that kubernetes/cluster/kubecfg.sh is in the path.
+
+# Create the pods for shard-0
+cell=test_cell
+keyspace=test_keyspace
+shard=0
+uid_base=100
+
+for uid_index in 0 1 2; do
+  uid=$[$uid_base + $uid_index]
+  printf -v alias '%s-%010d' $cell $uid
+
+  echo "Deleting pod for tablet $alias..."
+  kubecfg.sh delete pods/vttablet-$uid
+done
