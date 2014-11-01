666dcaaf4d2ad4cc81a61322c89f5ffdff14a90f
Sugu Sougoumarane <sougou@google.com>
2014-10-31 21:46:44 -0700

    vtgate v3: Improved input JSON file structure

--- go/vt/vtgate/planbuilder/vtgate_schema.go
+++ go/vt/vtgate/planbuilder/vtgate_schema.go
@@ -4,6 +4,12 @@
 
 package planbuilder
 
+import (
+	"fmt"
+
+	"github.com/youtube/vitess/go/jscfg"
+)
+
 const (
 	ShardKey = iota
 	Lookup
@@ -18,8 +24,41 @@ type VTGateSchema struct {
 	Tables map[string]*VTGateTable
 }
 
+func BuildSchema(source *VTGateSchemaNormalized) (schema *VTGateSchema, err error) {
+	schema = &VTGateSchema{Tables: make(map[string]*VTGateTable)}
+	for ksname, ks := range source.Keyspaces {
+		keyspace := &VTGateKeyspace{
+			Name:           ksname,
+			ShardingScheme: ks.ShardingScheme,
+			Lookupdb:       ks.Lookupdb,
+		}
+		for tname, table := range ks.Tables {
+			t := &VTGateTable{
+				Keyspace: keyspace,
+				Indexes:  make([]*VTGateIndex, 0, len(table.IndexColumns)),
+			}
+			for _, ind := range table.IndexColumns {
+				idx, ok := ks.Indexes[ind.IndexName]
+				if !ok {
+					return nil, fmt.Errorf("index %s not found for table %s", ind.IndexName, tname)
+				}
+				t.Indexes = append(t.Indexes, &VTGateIndex{
+					Column:    ind.Column,
+					Name:      ind.IndexName,
+					From:      idx.From,
+					To:        idx.To,
+					Owner:     idx.Owner,
+					IsAutoInc: idx.IsAutoInc,
+				})
+			}
+			schema.Tables[tname] = t
+		}
+	}
+	return schema, nil
+}
+
 type VTGateTable struct {
-	Keyspace *Keyspace
+	Keyspace *VTGateKeyspace
 	Indexes  []*VTGateIndex
 }
 
@@ -31,13 +70,13 @@ type VTGateIndex struct {
 	IsAutoInc bool
 }
 
-type Keyspace struct {
+type VTGateKeyspace struct {
 	Name           string
 	ShardingScheme int
 	Lookupdb       string
 }
 
-type VTGateSchemaMetadata struct {
+type VTGateSchemaNormalized struct {
 	Keyspaces map[string]struct {
 		ShardingScheme int
 		Lookupdb       string
@@ -55,53 +94,10 @@ type VTGateSchemaMetadata struct {
 	}
 }
 
-/*
-var user = &Keyspace{
-	Name:           "user",
-	ShardingScheme: HashSharded,
+func LoadSchemaJSON(filename string) (schema *VTGateSchema, err error) {
+	var source VTGateSchemaNormalized
+	if err := jscfg.ReadJson(filename, &source); err != nil {
+		return nil, err
+	}
+	return BuildSchema(&source)
 }
-
-var musicUserLookup = &VTGateLookup{
-	Name: "music_user_map",
-	From: "music_id",
-	To:   "user_id",
-}
-
-var vtgateSchema = &VTGateSchema{
-	Table: map[string]*VTGateTable{
-		"user": {
-			Keyspace:      user,
-			Indexes: []*VTGateIndex{{
-				Type:   ShardKey,
-				Column: "id",
-			}},
-		},
-		"user_extra": {
-			Keyspace:      user,
-			Indexes: []*VTGateIndex{{
-				Type:   ShardKey,
-				Column: "user_id",
-			}},
-		},
-		"music": {
-			Keyspace:      user,
-			Indexes: []*VTGateIndex{{
-				Type:   ShardKey,
-				Column: "user_id",
-			}, {
-				Type:   Lookup,
-				Column: "id",
-				Lookup: musicUserLookup,
-			}},
-		},
-		"music_extra": {
-			Keyspace:      user,
-			Indexes: []*VTGateIndex{{
-				Type:   Lookup,
-				Column: "music_id",
-				Lookup: musicUserLookup,
-			}},
-		},
-	},
-}
-*/
