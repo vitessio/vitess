a65c25effb13619bf9050e23a8d055f6c41233f4
Peter Lobsinger <pitr@google.com>
2014-11-10 20:46:59 -0800

    Remove methods on DummyContext not required by go.net Context.

--- go/vt/context/context.go
+++ go/vt/context/context.go
@@ -1,20 +1,11 @@
 package context
 
-import (
-	"html/template"
-	"time"
-)
+import "time"
 
 // DummyContext is a dummy implementation of Context.
 type DummyContext struct {
 }
 
-func (dc *DummyContext) GetRemoteAddr() string {
-	return "DummyRemoteAddr"
-}
-func (dc *DummyContext) GetUsername() string                     { return "DummyUsername" }
-func (dc *DummyContext) HTML() template.HTML                     { return template.HTML("DummyContext") }
-func (dc *DummyContext) String() string                          { return "DummyContext" }
 func (dc *DummyContext) Deadline() (deadline time.Time, ok bool) { return time.Time{}, false }
 func (dc *DummyContext) Done() <-chan struct{}                   { return nil }
 func (dc *DummyContext) Err() error                              { return nil }
