ac11ee2326c6adde6a2d04103a89d5966df80f8b
Shruti Patil <shrutip@google.com>
2014-12-21 11:14:22 -0800

    Core library and integration test framework.

---
+++ py/vtdb/db_object.py
@@ -0,0 +1,356 @@
+"""Module containing base classes and helper methods for database objects.
+
+The base classes represent different sharding schemes like
+unsharded, range-sharded and custom-sharded tables.
+This abstracts sharding details and provides methods
+for common database access patterns.
+"""
+
+import functools
+import logging
+
+from vtdb import database_context
+from vtdb import dbexceptions
+from vtdb import keyrange
+from vtdb import keyrange_constants
+from vtdb import shard_constants
+from vtdb import sql_builder
+from vtdb import vtgate_cursor
+
+
+class Unimplemented(Exception):
+    pass
+
+class ShardRouting(object):
+  """VTGate Shard Routing Class."""
+
+  keyspace = None
+  sharding_key = None
+  keyrange = None
+  entity_id_sharding_key_map = None
+  shard_name = None # For custom sharding
+
+  def __init__(self, keyspace):
+    self.keyspace = keyspace
+
+
+def _is_iterable_container(x):
+  return hasattr(x, '__iter__')
+
+
+def db_wrapper(method):
+  @functools.wraps(method)
+  def _db_wrapper(*pargs, **kargs):
+    table_class = pargs[0]
+    cursor_method = pargs[1]
+    cursor = cursor_method(table_class, **kargs)
+    if pargs[2:]:
+      return method(table_class, cursor, *pargs[2:], **kargs)
+    else:
+      return method(table_class, cursor, **kargs)
+  return _db_wrapper
+
+
+def db_class_method(*pargs, **kargs):
+  """This function calls db_wrapper to create the appropriate cursor."""
+  return classmethod(db_wrapper(*pargs, **kargs))
+
+
+class DBObjectBase(object):
+  """Base class for db classes.
+
+  This abstracts sharding information and provides helper methods
+  for common database access operations.
+  """
+  keyspace = None
+  sharding = None
+
+  table_name = None
+
+  id_column_name = 'id'
+  sharding_key_column_name = None
+  entity_id_columns = None
+  cursor = None
+
+  @classmethod
+  def create_shard_routing(class_, is_dml, *pargs, **kwargs):
+    """This method is used to create ShardRouting object which is
+    used for determining routing attributes for the vtgate cursor.
+
+    Args:
+    is_dml: This is used to santiy check required params for writes.
+
+    Returns:
+    ShardRouting object.
+    """
+    raise Unimplemented
+
+  @classmethod
+  def create_vtgate_cursor(class_, vtgate_conn, tablet_type, is_dml,
+                           *pargs, **kwargs):
+    """This creates the VTGateCursor object which is used to make
+    all the rpc calls to VTGate.
+
+    Args:
+    vtgate_conn: connection to vtgate.
+    tablet_type: tablet type to connect to.
+    is_dml: Makes the cursor writable, enforces appropriate constraints.
+
+    Returns:
+    VTGateCursor for the query.
+    """
+    raise Unimplemented
+
+  @db_class_method
+  def select_by_columns(class_, cursor, where_column_value_pairs,
+                        columns_list = None,order_by=None, group_by=None,
+                        limit=None, **kwargs):
+    if columns_list is None:
+      columns_list = class_.columns_list
+    query, bind_vars = sql_builder.select_by_columns_query(columns_list,
+                                                           class_.table_name,
+                                                           where_column_value_pairs,
+                                                           order_by=order_by,
+                                                           group_by=group_by,
+                                                           limit=limit,
+                                                           **kwargs)
+
+    rowcount = cursor.execute(query, bind_vars)
+    rows = cursor.fetchall()
+    return [sql_builder.DBRow(columns_list, row) for row in rows]
+
+  @db_class_method
+  def insert(class_, cursor, **bind_variables):
+    values_clause, bind_list = sql_builder.build_values_clause(
+        class_.columns_list,
+        bind_variables)
+    query = 'INSERT INTO %s (%s) VALUES (%s)' % (class_.table_name,
+                                                 sql_builder.colstr(
+                                                     class_.columns_list,
+                                                     bind=bind_list),
+                                                 values_clause)
+    cursor.execute(query, bind_variables)
+    return cursor.lastrowid
+
+  @db_class_method
+  def update_columns(class_, cursor, where_column_value_pairs,
+                     **update_columns):
+    query, bind_variables = sql_builder.update_columns_query(
+        class_.table_name, where_column_value_pairs, **update_columns)
+
+    return cursor.execute(query, bind_variables)
+
+  @db_class_method
+  def delete_by_columns(class_, cursor, where_column_value_pairs, limit=None,
+                        **columns):
+    if not where_column_value_pairs:
+      where_column_value_pairs = columns.items()
+      where_column_value_pairs.sort()
+
+    if not where_column_value_pairs:
+      raise dbexceptions.ProgrammingError("deleting the whole table is not allowed")
+
+    query, bind_variables = sql_builder.delete_by_columns_query(class_.table_name,
+                                                              where_column_value_pairs,
+                                                              limit=limit)
+    cursor.execute(query, bind_variables)
+    if cursor.rowcount == 0:
+      raise dbexceptions.DatabaseError("DB Row not found")
+    return cursor.rowcount
+
+
+class DBObjectUnsharded(DBObjectBase):
+  """Base class for unsharded db classes.
+
+  This provides default implementation of routing helper methods, cursor
+  creation and common database access operations.
+  """
+  keyspace = None
+  sharding = shard_constants.UNSHARDED
+
+  table_name = None
+  columns_list = None
+
+  id_column_name = 'id'
+  sharding_key_column_name = None
+  entity_id_columns = None
+
+  @classmethod
+  def create_shard_routing(class_, is_dml, *pargs, **kwargs):
+    routing = ShardRouting(class_.keyspace)
+    routing.keyrange = keyrange.KeyRange(keyrange_constants.NON_PARTIAL_KEYRANGE)
+    return routing
+
+  @classmethod
+  def create_vtgate_cursor(class_, vtgate_conn, tablet_type, is_dml, **kargs):
+    routing = class_.create_shard_routing(is_dml)
+
+    if routing.keyrange is not None:
+      keyranges = [routing.keyrange,]
+    else:
+      dbexceptions.ProgrammingError("Empty Keyrange")
+
+    cursor = vtgate_cursor.VTGateCursor(vtgate_conn,
+                                        class_.keyspace,
+                                        tablet_type,
+                                        keyranges=keyranges,
+                                        writable=is_dml)
+    return cursor
+
+class DBObjectRangeSharded(DBObjectBase):
+  """Base class for range-sharded db classes.
+
+  This provides default implementation of routing helper methods, cursor
+  creation and common database access operations.
+  This abstracts sharding information and provides helper methods
+  for common database access operations.
+  """
+  keyspace = None
+  sharding = shard_constants.RANGE_SHARDED
+
+  table_name = None
+  columns_list = None
+
+  id_column_name = 'id'
+  sharding_key_column_name = None
+  entity_id_columns = None
+
+  @classmethod
+  def create_shard_routing(class_, is_dml,  **kargs):
+    routing = ShardRouting(class_.keyspace)
+
+    keyrange = kargs.get("keyrange", None)
+    if keyrange is not None:
+      if is_dml:
+        dbexceptions.InternalError(
+            "Writes require unique sharding_key and not keyrange.")
+      routing.keyrange = keyrange
+      return routing
+
+    routing.sharding_key = kargs.get('sharding_key', None)
+    if routing.sharding_key is None:
+      try:
+        entity_id_column = kargs['entity_id_column']
+        entity_id = kargs['entity_id']
+        # this may involve a lookup of the index from db.
+        # consider caching it at the app layer for performance.
+        entity_id_sharding_key_map = class_.map_entity_id_sharding_key(
+            entity_id_column, entity_id)
+        routing.entity_id_sharding_key_map = entity_id_sharding_key_map
+        routing = entity_id_sharding_key_map.values()
+      except KeyError, e:
+        raise dbexceptions.ProgrammingError(
+            "For sharded table, sharding_key and entity_id cannot both be empty.")
+
+    if not class_.is_sharding_key_valid(routing.sharding_key):
+      raise dbexceptions.InternalError("Invalid sharding_key %s" % sharding_key)
+
+    if (_is_iterable_container(routing.sharding_key)
+        and is_dml):
+      raise dbexceptions.InternalError(
+          "Writes are not allowed on multiple sharding_keys.")
+    return routing
+
+  @classmethod
+  def create_vtgate_cursor(class_, vtgate_conn, tablet_type, is_dml, **kargs):
+    routing = class_.create_shard_routing(is_dml, kargs)
+
+    keyspace_ids = None
+    keyranges = None
+    if routing.sharding_key is not None:
+      keysapce_ids = [class_.sharding_key_to_keyspace_id(routing.sharding_key),]
+    elif routing.entity_id_sharding_key_map is not None:
+      keyspace_ids = []
+      for sharding_key in routing.entity_id_sharding_key_map.values():
+        keysapce_ids.append(class_.sharding_key_to_keyspace_id(sharding_key))
+    elif routing.keyrange:
+      keyranges = [routing.keyrange,]
+
+    cursor = vtgate_cursor.VTGateCursor(vtgate_conn,
+                                        class_.keyspace,
+                                        tablet_type,
+                                        keyspace_ids=keyspace_ids,
+                                        keyranges=keyranges,
+                                        writable=is_dml)
+    return cursor
+
+
+  @classmethod
+  def map_entity_id_sharding_key(class_, entity_id_column, entity_id):
+    """This method is used to map any entity id to sharding key.
+
+    Args:
+      entity_id_column: Non-sharding key indexes that can be used for query routing.
+      entity_id: entity id value.
+
+    Returns:
+      sharding key to be used for routing.
+    """
+    raise Unimplemented
+
+  @classmethod
+  def is_sharding_key_valid(class_, sharding_key):
+    """Method to check the validity of sharding key for the table.
+
+    Args:
+      sharding_key: sharding_key to be validated.
+
+    Returns:
+      bool
+    """
+    raise Unimplemented
+
+  @classmethod
+  def sharding_key_to_keyspace_id(class_, sharding_key):
+    """Method to check the validity of sharding key for the table.
+
+    Args:
+      sharding_key: sharding_key
+
+    Returns:
+      keyspace_id
+    """
+    raise Unimplemented
+
+
+class DBObjectCustomSharded(DBObjectBase):
+  """Base class for custom-sharded db classes.
+
+  This class is intended to support a custom sharding scheme, where the user
+  controls the routing of their queries by passing in the shard_name
+  explicitly.This provides helper methods for common database access operations.
+  """
+  keyspace = None
+  sharding = shard_constants.CUSTOM_SHARDED
+
+  table_name = None
+  columns_list = None
+
+  id_column_name = 'id'
+  sharding_key_column_name = None
+  entity_id_columns = None
+
+  @classmethod
+  def create_shard_routing(class_, is_dml, **kargs):
+    routing = shard_routing.ShardRouting(keyspace)
+    routing.shard_name = kargs.get('shard_name')
+    if routing.shard_name is None:
+      dbexceptions.InternalError("For custom sharding, shard_name cannot be None.")
+
+    if (_is_iterable_container(routing.shard_name)
+        and is_dml):
+      raise dbexceptions.InternalError(
+          "Writes are not allowed on multiple shards.")
+    return routing
+
+  @classmethod
+  def create_vtgate_cursor(class_, vtgate_conn, tablet_type, is_dml, **kargs):
+    routing = class_.create_shard_routing(is_dml, kargs)
+
+    # FIXME:extend VTGateCursor's api to accept shard_names
+    # and allow queries based on that.
+    cursor = vtgate_cursor.VTGateCursor(vtgate_conn, class_.keyspace,
+                                        tablet_type,
+                                        keyranges=[routing.shard_name,],
+                                        writable=is_dml)
+    return cursor
---
+++ test/client_test.py
@@ -0,0 +1,262 @@
+"""Test environment for client library tests.
+
+This module has functions for creating keyspaces, tablets for the client
+library test.
+"""
+#!/usr/bin/env python
+# coding: utf-8
+
+import logging
+import struct
+import threading
+import time
+import traceback
+import unittest
+
+import environment
+import tablet
+import utils
+from clientlib_tests import topo_schema
+from clientlib_tests import db_class_unsharded
+
+from vtdb import database_context
+from vtdb import keyrange
+from vtdb import keyrange_constants
+from vtdb import keyspace
+from vtdb import dbexceptions
+from vtdb import shard_constants
+from vtdb import topology
+from vtdb import vtdb_logger
+from vtdb import vtgatev2
+from vtdb import vtgate_cursor
+from zk import zkocc
+
+conn_class = vtgatev2
+__tablets = None
+
+vtgate_server = None
+vtgate_port = None
+
+shard_names = ['-80', '80-']
+shard_kid_map = {'-80': [527875958493693904, 626750931627689502,
+                         345387386794260318, 332484755310826578,
+                         1842642426274125671, 1326307661227634652,
+                         1761124146422844620, 1661669973250483744,
+                         3361397649937244239, 2444880764308344533],
+                 '80-': [9767889778372766922, 9742070682920810358,
+                         10296850775085416642, 9537430901666854108,
+                         10440455099304929791, 11454183276974683945,
+                         11185910247776122031, 10460396697869122981,
+                         13379616110062597001, 12826553979133932576],
+                 }
+
+pack_kid = struct.Struct('!Q').pack
+
+def setUpModule():
+  global vtgate_server, vtgate_port
+  logging.debug("in setUpModule")
+  try:
+    environment.topo_server().setup()
+    setup_topology()
+
+    # start mysql instance external to the test
+    global __tablets
+    setup_procs = []
+    for tablet in __tablets:
+      setup_procs.append(tablet.init_mysql())
+    utils.wait_procs(setup_procs)
+    create_db()
+    start_tablets()
+    vtgate_server, vtgate_port = utils.vtgate_start()
+    # FIXME(shrutip): this should be removed once vtgate_cursor's
+    # dependency on topology goes away.
+    vtgate_client = zkocc.ZkOccConnection("localhost:%u" % vtgate_port,
+                                          "test_nj", 30.0)
+    topology.read_topology(vtgate_client)
+  except:
+    tearDownModule()
+    raise
+
+def tearDownModule():
+  global vtgate_server
+  global __tablets
+  logging.debug("in tearDownModule")
+  if utils.options.skip_teardown:
+    return
+  logging.debug("Tearing down the servers and setup")
+  utils.vtgate_kill(vtgate_server)
+  if __tablets is not None:
+    tablet.kill_tablets(__tablets)
+    teardown_procs = []
+    for t in __tablets:
+      teardown_procs.append(t.teardown_mysql())
+    utils.wait_procs(teardown_procs, raise_on_error=False)
+
+  environment.topo_server().teardown()
+
+  utils.kill_sub_processes()
+  utils.remove_tmp_files()
+
+  if __tablets is not None:
+    for t in __tablets:
+      t.remove_tree()
+
+def setup_topology():
+  global __tablets
+  if __tablets is None:
+    __tablets = []
+
+  keyspaces = topo_schema.keyspaces
+  for ks in keyspaces:
+    ks_name = ks[0]
+    ks_type = ks[1]
+    utils.run_vtctl(['CreateKeyspace', ks_name])
+    if ks_type == shard_constants.UNSHARDED:
+      shard_master = tablet.Tablet()
+      shard_replica = tablet.Tablet()
+      shard_master.init_tablet('master', keyspace=ks_name, shard='0')
+      __tablets.append(shard_master)
+      shard_replica.init_tablet('replica', keyspace=ks_name, shard='0')
+      __tablets.append(shard_replica)
+    elif ks_type == shard_constants.RANGE_SHARDED:
+      utils.run_vtctl(['SetKeyspaceShardingInfo', '-force', ks_name,
+                       'keyspace_id', 'uint64'])
+      for shard_name in shard_names:
+        shard_master = tablet.Tablet()
+        shard_replica = tablet.Tablet()
+        shard_master.init_tablet('master', keyspace=ks_name, shard=shard_name)
+        __tablets.append(shard_master)
+        shard_replica.init_tablet('replica', keyspace=ks_name, shard=shard_name)
+        __tablets.append(shard_replica)
+    utils.run_vtctl(['RebuildKeyspaceGraph', ks_name], auto_log=True)
+
+
+def create_db():
+  global __tablets
+  for t in __tablets:
+    t.create_db(t.dbname)
+    ks_name = t.keyspace
+    for table_tuple in topo_schema.keyspace_table_map[ks_name]:
+      t.mquery(t.dbname, table_tuple[1])
+
+def start_tablets():
+  global __tablets
+  # start tablets
+  for t in __tablets:
+    t.start_vttablet(wait_for_state=None)
+
+  # wait for them to come in serving state
+  for t in __tablets:
+    t.wait_for_vttablet_state('SERVING')
+
+  # ReparentShard for master tablets
+  for t in __tablets:
+    if t.tablet_type == 'master':
+      utils.run_vtctl(['ReparentShard', '-force', t.keyspace+'/'+t.shard,
+                       t.tablet_alias], auto_log=True)
+
+  for ks in topo_schema.keyspaces:
+    ks_name = ks[0]
+    ks_type = ks[1]
+    utils.run_vtctl(['RebuildKeyspaceGraph', ks_name],
+                     auto_log=True)
+    if ks_type == shard_constants.RANGE_SHARDED:
+      utils.check_srv_keyspace('test_nj', ks_name,
+                               'Partitions(master): -80 80-\n' +
+                               'Partitions(replica): -80 80-\n' +
+                               'TabletTypes: master,replica')
+
+
+def get_connection(user=None, password=None):
+  global vtgate_port
+  timeout = 10.0
+  conn = None
+  vtgate_addrs = {"_vt": ["localhost:%s" % (vtgate_port),]}
+  conn = conn_class.connect(vtgate_addrs, timeout,
+                            user=user, password=password)
+  return conn
+
+def get_keyrange(shard_name):
+  kr = None
+  if shard_name == keyrange_constants.SHARD_ZERO:
+    kr = keyrange.KeyRange(keyrange_constants.NON_PARTIAL_KEYRANGE)
+  else:
+    kr = keyrange.KeyRange(shard_name)
+  return kr
+
+
+def _delete_all(keyspace, shard_name, table_name):
+  vtgate_conn = get_connection()
+  # This write is to set up the test with fresh insert
+  # and hence performing it directly on the connection.
+  vtgate_conn.begin()
+  vtgate_conn._execute("delete from %s" % table_name, {},
+                       keyspace, 'master',
+                       keyranges=[get_keyrange(shard_name)])
+  vtgate_conn.commit()
+
+
+def restart_vtgate(extra_args={}):
+  global vtgate_server, vtgate_port
+  utils.vtgate_kill(vtgate_server)
+  vtgate_server, vtgate_port = utils.vtgate_start(vtgate_port, extra_args=extra_args)
+
+def populate_table():
+  keyspace = "KS_UNSHARDED"
+  _delete_all(keyspace, keyrange_constants.SHARD_ZERO, 'vt_unsharded')
+  vtgate_conn = get_connection()
+  cursor = vtgate_conn.cursor(keyspace, 'master', keyranges=[get_keyrange(keyrange_constants.SHARD_ZERO),],writable=True)
+  cursor.begin()
+  for x in xrange(10):
+    cursor.execute('insert into vt_unsharded (id, msg) values (%s, %s)' % (str(x), 'msg'), {})
+  cursor.commit()
+
+class TestUnshardedTable(unittest.TestCase):
+
+  def setUp(self):
+    self.vtgate_addrs = {"_vt": ["localhost:%s" % (vtgate_port),]}
+    self.dc = database_context.DatabaseContext(self.vtgate_addrs)
+    with database_context.WriteTransaction(self.dc) as context:
+      for x in xrange(10):
+        db_class_unsharded.VtUnsharded.insert(context.get_cursor(),
+                                              id=x, msg=str(x))
+
+  def tearDown(self):
+    _delete_all("KS_UNSHARDED", "0", 'vt_unsharded')
+
+  def test_read(self):
+    with database_context.ReadFromMaster(self.dc) as context:
+      rows = db_class_unsharded.VtUnsharded.select_by_id(
+          context.get_cursor(), 2)
+      self.assertEqual(len(rows), 1, "wrong number of rows fetched")
+      for row in rows:
+        logging.info("ROW: %s" % row)
+      self.assertEqual(rows[0].id, 2, "wrong row fetched")
+
+  def test_update_and_read(self):
+    where_column_value_pairs = [('id', 2)]
+    with database_context.WriteTransaction(self.dc) as context:
+      db_class_unsharded.VtUnsharded.update_columns(context.get_cursor(),
+                                                    where_column_value_pairs,
+                                                    msg="test update")
+
+    with database_context.ReadFromMaster(self.dc) as context:
+      rows = db_class_unsharded.VtUnsharded.select_by_id(context.get_cursor(), 2)
+      self.assertEqual(len(rows), 1, "wrong number of rows fetched")
+      self.assertEqual(rows[0].msg, "test update", "wrong row fetched")
+
+  def test_delete_and_read(self):
+    where_column_value_pairs = [('id', 2)]
+    with database_context.WriteTransaction(self.dc) as context:
+      db_class_unsharded.VtUnsharded.delete_by_columns(context.get_cursor(),
+                                                    where_column_value_pairs)
+
+    with database_context.ReadFromMaster(self.dc) as context:
+      rows = db_class_unsharded.VtUnsharded.select_by_id(context.get_cursor(), 2)
+      self.assertEqual(len(rows), 0, "wrong number of rows fetched")
+
+
+
+
+if __name__ == '__main__':
+  utils.main()
