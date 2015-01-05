f89ed0cf051020a14fca7200d2f869f0c81258e0
Anthony Yeh <enisoc@google.com>
2015-01-05 13:47:43 -0800

    Resolve CNAME in Python ZkTopoServer to match Go's net.LookupCNAME().
    If your /etc/hosts file contains something like this:
    
    127.0.0.1 localhost localhost.localdomain.
    
    Then both Python and Go will resolve the FQDN as localhost.localdomain.
    However, in Go we additionally call net.LookupCNAME(), which converts
    localhost.localdomain back to just localhost, since it comes first in
    the /etc/hosts line. In Python, we need to also do the equivalent CNAME
    lookup, or else we won't match the hostname that Vitess sees.

--- test/topo_flavor/zookeeper.py
+++ test/topo_flavor/zookeeper.py
@@ -26,7 +26,7 @@ class ZkTopoServer(server.TopoServer):
     from environment import reserve_ports
 
     self.zk_port_base = reserve_ports(3)
-    self.hostname = socket.getfqdn()
+    self.hostname = socket.getaddrinfo(socket.getfqdn(), None, 0, 0, 0, socket.AI_CANONNAME)[0][3]
     self.zk_ports = ':'.join(str(self.zk_port_base + i) for i in range(3))
     self.zk_client_port = self.zk_port_base + 2
     self.ports_assigned = True
