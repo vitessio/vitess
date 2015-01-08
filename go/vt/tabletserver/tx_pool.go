c6bd4ee8f1f13c60ee49a1f00359c3ff2d083d64
Bradley Heilbrun <nurblieh@gmail.com>
2015-01-07 21:48:46 -0800

    Fix my last log msg change to use the default formatter.

--- go/vt/tabletserver/tx_pool.go
+++ go/vt/tabletserver/tx_pool.go
@@ -103,7 +103,7 @@ func (axp *TxPool) TransactionKiller() {
 	defer logError()
 	for _, v := range axp.activePool.GetOutdated(time.Duration(axp.Timeout()), "for rollback") {
 		conn := v.(*TxConnection)
-		log.Warningf("killing transaction (exceeded timeout: %s): %s", axp.Timeout().String(), conn.Format(nil))
+		log.Warningf("killing transaction (exceeded timeout: %v): %s", axp.Timeout(), conn.Format(nil))
 		killStats.Add("Transactions", 1)
 		conn.Close()
 		conn.discard(TX_KILL)
