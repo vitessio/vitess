718c70cb4de2aa1e8150cc46fa19cc98c590b319
Sugu Sougoumarane <sougou@google.com>
2015-01-10 18:11:15 -0800

    v3: vschema support for etcd

---
+++ go/vt/etcdtopo/vschema.go
@@ -0,0 +1,44 @@
+package etcdtopo
+
+import (
+	"github.com/youtube/vitess/go/vt/topo"
+	"github.com/youtube/vitess/go/vt/vtgate/planbuilder"
+	// vindexes needs to be imported so that they register
+	// themeselves against vtgate/planbuilder. This will allow
+	// us to sanity check the schema being uploaded.
+	_ "github.com/youtube/vitess/go/vt/vtgate/vindexes"
+)
+
+/*
+This file contains the vschema management code for etcdtopo.Server
+*/
+
+// SaveVSchema saves the JSON vschema into the topo.
+func (s *Server) SaveVSchema(vschema string) error {
+	_, err := planbuilder.NewSchema([]byte(vschema))
+	if err != nil {
+		return err
+	}
+
+	_, err = s.getGlobal().Set(vschemaPath, vschema, 0 /* ttl */)
+	if err != nil {
+		return convertError(err)
+	}
+	return nil
+}
+
+// GetVSchema fetches the JSON vschema from the topo.
+func (s *Server) GetVSchema() (string, error) {
+	resp, err := s.getGlobal().Get(vschemaPath, false /* sort */, false /* recursive */)
+	if err != nil {
+		err = convertError(err)
+		if err == topo.ErrNoNode {
+			return "{}", nil
+		}
+		return "", err
+	}
+	if resp.Node == nil {
+		return "", ErrBadResponse
+	}
+	return resp.Node.Value, nil
+}
