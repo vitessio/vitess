b6834199a25bdae578f2a59073dfa6785b22aba3
Shengzhe Yao <yaoshengzhe@gmail.com>
2015-02-04 18:39:54 -0800

    add test case and two params for txlogz
    1. timeout: txlogz will keep dumping transactions until timeout,
       default value: 10s
    2. limit: txlogz will keep dumping transactions until it hits the limit,
       default value: 300

--- go/vt/tabletserver/txlogz.go
+++ go/vt/tabletserver/txlogz.go
@@ -9,6 +9,7 @@ import (
 	"html/template"
 	"io"
 	"net/http"
+	"strconv"
 	"time"
 
 	log "github.com/golang/glog"
@@ -50,22 +51,52 @@ func init() {
 	http.HandleFunc("/txlogz", txlogzHandler)
 }
 
+func adjustValue(val int, lower int, upper int) int {
+	if val < lower {
+		return lower
+	} else if val > upper {
+		return upper
+	}
+	return val
+}
+
+func parseReqParam(req *http.Request) (time.Duration, int) {
+	timeout := 10
+	limit := 300
+	if ts, ok := req.URL.Query()["timeout"]; ok {
+		if t, err := strconv.Atoi(ts[0]); err == nil {
+			timeout = adjustValue(t, 0, 60)
+		}
+	}
+	if l, ok := req.URL.Query()["limit"]; ok {
+		if lim, err := strconv.Atoi(l[0]); err == nil {
+			limit = adjustValue(lim, 1, 200000)
+		}
+	}
+	return time.Duration(timeout) * time.Second, limit
+}
+
 // txlogzHandler serves a human readable snapshot of the
 // current transaction log.
-func txlogzHandler(w http.ResponseWriter, r *http.Request) {
-	if err := acl.CheckAccessHTTP(r, acl.DEBUGGING); err != nil {
+// Endpoint: /txlogz?timeout=%d&limit=%d
+// timeout: the txlogz will keep dumping transactions until timeout
+// limit: txlogz will keep dumping transcations until it hits the limit
+func txlogzHandler(w http.ResponseWriter, req *http.Request) {
+	if err := acl.CheckAccessHTTP(req, acl.DEBUGGING); err != nil {
 		acl.SendError(w, err)
 		return
 	}
+
+	timeout, limit := parseReqParam(req)
 	ch := TxLogger.Subscribe("txlogz")
 	defer TxLogger.Unsubscribe(ch)
 	startHTMLTable(w)
 	defer endHTMLTable(w)
 	w.Write(txlogzHeader)
 
-	tmr := time.NewTimer(10 * time.Second)
+	tmr := time.NewTimer(timeout)
 	defer tmr.Stop()
-	for i := 0; i < 300; i++ {
+	for i := 0; i < limit; i++ {
 		select {
 		case out := <-ch:
 			txc, ok := out.(*TxConnection)
---
+++ go/vt/tabletserver/txlogz_test.go
@@ -0,0 +1,59 @@
+// Copyright 2015, Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package tabletserver
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/youtube/vitess/go/sync2"
+)
+
+func testHandler(req *http.Request, t *testing.T) {
+	response := httptest.NewRecorder()
+	TxLogger.Send("test msg")
+	txlogzHandler(response, req)
+	if !strings.Contains(response.Body.String(), "error") {
+		t.Fatalf("should show an error page since transaction log format is invalid.")
+	}
+	txConn := &TxConnection{
+		TransactionID: 123456,
+		StartTime:     time.Now(),
+		Queries:       []string{"select * from test"},
+		Conclusion:    "unknown",
+		LogToFile:     sync2.AtomicInt32(0),
+	}
+	txConn.EndTime = txConn.StartTime
+	response = httptest.NewRecorder()
+	TxLogger.Send(txConn)
+	txlogzHandler(response, req)
+	txConn.EndTime = txConn.StartTime.Add(time.Duration(2) * time.Second)
+	response = httptest.NewRecorder()
+	TxLogger.Send(txConn)
+	txlogzHandler(response, req)
+	txConn.EndTime = txConn.StartTime.Add(time.Duration(500) * time.Millisecond)
+	response = httptest.NewRecorder()
+	TxLogger.Send(txConn)
+	txlogzHandler(response, req)
+
+}
+
+func TestTxlogzHandler(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/txlogz?timeout=0&limit=10", nil)
+	testHandler(req, t)
+}
+
+func TestTxlogzHandlerWithNegativeTimeout(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/txlogz?timeout=-1&limit=10", nil)
+	testHandler(req, t)
+}
+
+func TestTxlogzHandlerWithLargeLimit(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/txlogz?timeout=0&limit=10000000", nil)
+	testHandler(req, t)
+}
