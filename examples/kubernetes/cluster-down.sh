b6ad069fd355a144405dd89ef4ad88b2188fbd01
Joshua Thompson <thompsonja@google.com>
2015-02-20 14:31:25 -0800

    /s/CLUSTER_NAME/GCE_CLUSTER_NAME in cluster-down.sh

--- examples/kubernetes/cluster-down.sh
+++ examples/kubernetes/cluster-down.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Tears down the gce cluster, removes rules/pools
-CLUSTER_NAME=${CLUSTER_NAME:-'example'}
-gcloud preview container clusters delete $CLUSTER_NAME
+GCE_CLUSTER_NAME=${GCE_CLUSTER_NAME:-'example'}
+gcloud preview container clusters delete $GCE_CLUSTER_NAME
 gcloud compute forwarding-rules delete vtctld -q --region=us-central1
 gcloud compute forwarding-rules delete vtgate -q --region=us-central1
 gcloud compute firewall-rules delete vtctld -q
