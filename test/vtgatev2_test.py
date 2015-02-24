f77c5baa90a248b4f3a9bc1616cb7ae34e826815
Shruti Patil <shrutip@google.com>
2015-02-24 14:55:38 -0800

    Make the mysql failure checks more explicit.

--- test/vtgatev2_test.py
+++ test/vtgatev2_test.py
@@ -837,11 +837,17 @@ class TestFailures(unittest.TestCase):
     except Exception, e:
       self.fail("Connection to vtgate failed with error %s" % str(e))
     utils.wait_procs([self.replica_tablet.shutdown_mysql(),])
-    with self.assertRaises(dbexceptions.DatabaseError):
+    try:
       vtgate_conn._execute(
           "select 1 from vt_insert_test", {},
           KEYSPACE_NAME, 'replica',
           keyranges=[self.keyrange])
+    except Exception, e:
+      self.assertIsInstance(e, dbexceptions.DatabaseError)
+      self.assertNotIsInstance(e, dbexceptions.IntegrityError)
+      self.assertNotIsInstance(e, dbexceptions.OperationalError)
+      self.assertNotIsInstance(e, dbexceptions.TimeoutError)
+
     utils.wait_procs([self.replica_tablet.start_mysql(),])
     self.replica_tablet.kill_vttablet()
     self.replica_tablet.start_vttablet()
