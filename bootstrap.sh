7af27670ba3eb518c1628e07f238083e6dc1469c
Alain Jobart <alainjobart@google.com>
2015-03-03 13:17:42 -0800

    Trying to fix travis build.

--- bootstrap.sh
+++ bootstrap.sh
@@ -50,6 +50,9 @@ protobuf_dist=$VTROOT/dist/protobuf
 if [ -d $protobuf_dist ]; then
   echo "skipping protobuf build"
 else
+  # The directory doesn't exist, so it wasn't picked up by dev.env yet,
+  # but the install needs it to exist first, and be in PYTHONPATH.
+  export PYTHONPATH=$(prepend_path $PYTHONPATH $protobuf_dist/lib/python2.7/site-packages)
   (mkdir -p $protobuf_dist/lib/python2.7/site-packages && \
     cd $protobuf_dist && \
     wget https://github.com/google/protobuf/archive/v3.0.0-alpha-2.zip && \
