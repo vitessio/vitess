0e26627fb142fc3b65a97de75cb25ea876609f6e
Sugu Sougoumarane <sougou@google.com>
2015-03-07 21:23:37 -0800

    vtgate go client: non-tx tests

--- go/vt/vtgate/vtgateconntest/client.go
+++ go/vt/vtgate/vtgateconntest/client.go
@@ -7,6 +7,7 @@
 package vtgateconntest
 
 import (
+	"errors"
 	"fmt"
 	"reflect"
 	"testing"
@@ -27,80 +28,29 @@ type fakeVTGateService struct {
 
 // Execute is part of the VTGateService interface
 func (f *fakeVTGateService) Execute(ctx context.Context, query *proto.Query, reply *proto.QueryResult) error {
-	execCase, ok := resultsMap[query.Sql]
+	execCase, ok := execMap[query.Sql]
 	if !ok {
 		return fmt.Errorf("no match for: %s", query.Sql)
 	}
-	if !reflect.DeepEqual(query, execCase.query) {
-		f.t.Errorf("Execute: %+v, want %+v", query, execCase.query)
+	if !reflect.DeepEqual(query, execCase.execQuery) {
+		f.t.Errorf("Execute: %+v, want %+v", query, execCase.execQuery)
 		return nil
 	}
 	*reply = *execCase.reply
 	return nil
 }
 
-var resultsMap = map[string]struct {
-	query *proto.Query
-	reply *proto.QueryResult
-	err   error
-}{
-	"request1": {
-		query: &proto.Query{
-			Sql: "request1",
-			BindVariables: map[string]interface{}{
-				"bind1": int64(0),
-			},
-			TabletType: topo.TYPE_RDONLY,
-			Session:    nil,
-		},
-		reply: &proto.QueryResult{
-			Result:  &result1,
-			Session: nil,
-			Error:   "",
-		},
-	},
-}
-
-var result1 = mproto.QueryResult{
-	Fields: []mproto.Field{
-		mproto.Field{
-			Name: "field1",
-			Type: 42,
-		},
-		mproto.Field{
-			Name: "field2",
-			Type: 73,
-		},
-	},
-	RowsAffected: 123,
-	InsertId:     72,
-	Rows: [][]sqltypes.Value{
-		[]sqltypes.Value{
-			sqltypes.MakeString([]byte("row1 value1")),
-			sqltypes.MakeString([]byte("row1 value2")),
-		},
-		[]sqltypes.Value{
-			sqltypes.MakeString([]byte("row2 value1")),
-			sqltypes.MakeString([]byte("row2 value2")),
-		},
-	},
-}
-
-func testExecute(t *testing.T, conn vtgateconn.VTGateConn) {
-	ctx := context.Background()
-	execCase := resultsMap["request1"]
-	qr, err := conn.Execute(ctx, execCase.query.Sql, execCase.query.BindVariables, execCase.query.TabletType)
-	if err != nil {
-		t.Fatalf("Execute failed: %v", err)
-	}
-	t.Logf("qr: %#v", qr)
-	if !reflect.DeepEqual(qr, execCase.reply.Result) {
-		t.Errorf("Unexpected result from Execute: got %+v wanted %+v", qr, execCase.reply.Result)
-	}
-}
-
 // ExecuteShard is part of the VTGateService interface
 func (f *fakeVTGateService) ExecuteShard(ctx context.Context, query *proto.QueryShard, reply *proto.QueryResult) error {
+	execCase, ok := execMap[query.Sql]
+	if !ok {
+		return fmt.Errorf("no match for: %s", query.Sql)
+	}
+	if !reflect.DeepEqual(query, execCase.shardQuery) {
+		f.t.Errorf("Execute: %+v, want %+v", query, execCase.shardQuery)
+		return nil
+	}
+	*reply = *execCase.reply
 	return nil
 }
 
@@ -131,6 +81,31 @@ func (f *fakeVTGateService) ExecuteBatchKeyspaceIds(ctx context.Context, batchQu
 
 // StreamExecute is part of the VTGateService interface
 func (f *fakeVTGateService) StreamExecute(ctx context.Context, query *proto.Query, sendReply func(*proto.QueryResult) error) error {
+	execCase, ok := execMap[query.Sql]
+	if !ok {
+		return fmt.Errorf("no match for: %s", query.Sql)
+	}
+	if !reflect.DeepEqual(query, execCase.execQuery) {
+		f.t.Errorf("Execute: %+v, want %+v", query, execCase.execQuery)
+		return nil
+	}
+	if execCase.reply.Result != nil {
+		result := proto.QueryResult{Result: &mproto.QueryResult{}}
+		result.Result.Fields = execCase.reply.Result.Fields
+		if err := sendReply(&result); err != nil {
+			return err
+		}
+		for _, row := range execCase.reply.Result.Rows {
+			result := proto.QueryResult{Result: &mproto.QueryResult{}}
+			result.Result.Rows = [][]sqltypes.Value{row}
+			if err := sendReply(&result); err != nil {
+				return err
+			}
+		}
+	}
+	if execCase.reply.Error != "" {
+		return errors.New(execCase.reply.Error)
+	}
 	return nil
 }
 
@@ -177,4 +152,178 @@ func CreateFakeServer(t *testing.T) vtgateservice.VTGateService {
 // TestSuite runs all the tests
 func TestSuite(t *testing.T, conn vtgateconn.VTGateConn) {
 	testExecute(t, conn)
+	testExecuteShard(t, conn)
+	testStreamExecute(t, conn)
+}
+
+func testExecute(t *testing.T, conn vtgateconn.VTGateConn) {
+	ctx := context.Background()
+	execCase := execMap["request1"]
+	qr, err := conn.Execute(ctx, execCase.execQuery.Sql, execCase.execQuery.BindVariables, execCase.execQuery.TabletType)
+	if err != nil {
+		t.Fatalf("Execute failed: %v", err)
+	}
+	if !reflect.DeepEqual(qr, execCase.reply.Result) {
+		t.Errorf("Unexpected result from Execute: got %+v want %+v", qr, execCase.reply.Result)
+	}
+
+	_, err = conn.Execute(ctx, "none", nil, "")
+	want := "no match for: none"
+	if err == nil || err.Error() != want {
+		t.Errorf("none request: %v, want %v", err, want)
+	}
+
+	_, err = conn.Execute(ctx, "request2", nil, "")
+	want = "app error"
+	if err == nil || err.Error() != want {
+		t.Errorf("request2: %v, want %v", err, want)
+	}
+}
+
+func testExecuteShard(t *testing.T, conn vtgateconn.VTGateConn) {
+	ctx := context.Background()
+	execCase := execMap["request1"]
+	qr, err := conn.ExecuteShard(ctx, execCase.execQuery.Sql, "ks", []string{"1", "2"}, execCase.execQuery.BindVariables, execCase.execQuery.TabletType)
+	if err != nil {
+		t.Fatalf("Execute failed: %v", err)
+	}
+	if !reflect.DeepEqual(qr, execCase.reply.Result) {
+		t.Errorf("Unexpected result from Execute: got %+v want %+v", qr, execCase.reply.Result)
+	}
+
+	_, err = conn.ExecuteShard(ctx, "none", "", []string{}, nil, "")
+	want := "no match for: none"
+	if err == nil || err.Error() != want {
+		t.Errorf("none request: %v, want %v", err, want)
+	}
+
+	_, err = conn.ExecuteShard(ctx, "request2", "", []string{}, nil, "")
+	want = "app error"
+	if err == nil || err.Error() != want {
+		t.Errorf("request2: %v, want %v", err, want)
+	}
+}
+
+func testStreamExecute(t *testing.T, conn vtgateconn.VTGateConn) {
+	ctx := context.Background()
+	execCase := execMap["request1"]
+	packets, errFunc := conn.StreamExecute(ctx, execCase.execQuery.Sql, execCase.execQuery.BindVariables, execCase.execQuery.TabletType)
+	var qr mproto.QueryResult
+	for packet := range packets {
+		if len(packet.Fields) != 0 {
+			qr.Fields = packet.Fields
+		}
+		if len(packet.Rows) != 0 {
+			qr.Rows = append(qr.Rows, packet.Rows...)
+		}
+	}
+	wantResult := *execCase.reply.Result
+	wantResult.RowsAffected = 0
+	wantResult.InsertId = 0
+	if !reflect.DeepEqual(qr, wantResult) {
+		t.Errorf("Unexpected result from Execute: got %+v want %+v", qr, wantResult)
+	}
+	err := errFunc()
+	if err != nil {
+		t.Fatalf("Execute failed: %v", err)
+	}
+
+	packets, errFunc = conn.StreamExecute(ctx, "none", nil, "")
+	for packet := range packets {
+		t.Errorf("packet: %+v, want none", packet)
+	}
+	err = errFunc()
+	want := "no match for: none"
+	if err == nil || err.Error() != want {
+		t.Errorf("none request: %v, want %v", err, want)
+	}
+
+	packets, errFunc = conn.StreamExecute(ctx, "request2", nil, "")
+	for packet := range packets {
+		t.Errorf("packet: %+v, want none", packet)
+	}
+	err = errFunc()
+	want = "app error"
+	if err == nil || err.Error() != want {
+		t.Errorf("request2: %v, want %v", err, want)
+	}
+}
+
+var execMap = map[string]struct {
+	execQuery  *proto.Query
+	shardQuery *proto.QueryShard
+	reply      *proto.QueryResult
+	err        error
+}{
+	"request1": {
+		execQuery: &proto.Query{
+			Sql: "request1",
+			BindVariables: map[string]interface{}{
+				"bind1": int64(0),
+			},
+			TabletType: topo.TYPE_RDONLY,
+			Session:    nil,
+		},
+		shardQuery: &proto.QueryShard{
+			Sql: "request1",
+			BindVariables: map[string]interface{}{
+				"bind1": int64(0),
+			},
+			Keyspace:   "ks",
+			Shards:     []string{"1", "2"},
+			TabletType: topo.TYPE_RDONLY,
+			Session:    nil,
+		},
+		reply: &proto.QueryResult{
+			Result:  &result1,
+			Session: nil,
+			Error:   "",
+		},
+	},
+	"request2": {
+		execQuery: &proto.Query{
+			Sql:           "request2",
+			BindVariables: map[string]interface{}{},
+			TabletType:    "",
+			Session:       nil,
+		},
+		shardQuery: &proto.QueryShard{
+			Sql:           "request2",
+			BindVariables: map[string]interface{}{},
+			TabletType:    "",
+			Keyspace:      "",
+			Shards:        []string{},
+			Session:       nil,
+		},
+		reply: &proto.QueryResult{
+			Result:  nil,
+			Session: nil,
+			Error:   "app error",
+		},
+	},
+}
+
+var result1 = mproto.QueryResult{
+	Fields: []mproto.Field{
+		mproto.Field{
+			Name: "field1",
+			Type: 42,
+		},
+		mproto.Field{
+			Name: "field2",
+			Type: 73,
+		},
+	},
+	RowsAffected: 123,
+	InsertId:     72,
+	Rows: [][]sqltypes.Value{
+		[]sqltypes.Value{
+			sqltypes.MakeString([]byte("row1 value1")),
+			sqltypes.MakeString([]byte("row1 value2")),
+		},
+		[]sqltypes.Value{
+			sqltypes.MakeString([]byte("row2 value1")),
+			sqltypes.MakeString([]byte("row2 value2")),
+		},
+	},
 }
