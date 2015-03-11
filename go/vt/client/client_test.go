5d61cabfd513cd4cdcf3dff6da489edee2a47972
Sugu Sougoumarane <sougou@google.com>
2015-03-10 20:22:44 -0700

    vtgate: go client driver

---
+++ go/vt/client/client_test.go
@@ -0,0 +1,293 @@
+// Copyright 2015, Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package client
+
+import (
+	"database/sql"
+	"database/sql/driver"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"os"
+	"reflect"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/youtube/vitess/go/rpcplus"
+	"github.com/youtube/vitess/go/rpcwrap/bsonrpc"
+	_ "github.com/youtube/vitess/go/vt/vtgate/gorpcvtgateconn"
+	"github.com/youtube/vitess/go/vt/vtgate/gorpcvtgateservice"
+)
+
+var (
+	testAddress string
+)
+
+func TestMain(m *testing.M) {
+	// fake service
+	service := CreateFakeServer()
+
+	// listen on a random port
+	listener, err := net.Listen("tcp", ":0")
+	if err != nil {
+		panic(fmt.Sprintf("Cannot listen: %v", err))
+	}
+
+	// Create a Go Rpc server and listen on the port
+	server := rpcplus.NewServer()
+	server.Register(gorpcvtgateservice.New(service))
+
+	// create the HTTP server, serve the server from it
+	handler := http.NewServeMux()
+	bsonrpc.ServeCustomRPC(handler, server, false)
+	httpServer := http.Server{
+		Handler: handler,
+	}
+	go httpServer.Serve(listener)
+
+	testAddress = listener.Addr().String()
+	os.Exit(m.Run())
+}
+
+func TestDriver(t *testing.T) {
+	connStr := fmt.Sprintf(`{"protocol": "gorpc", "address": "%s", "tablet_type": "rdonly", "timeout": %d}`, testAddress, int64(30*time.Second))
+	db, err := sql.Open("vitess", connStr)
+	if err != nil {
+		t.Error(err)
+	}
+	r, err := db.Query("request1", int64(0))
+	if err != nil {
+		t.Fatal(err)
+	}
+	count := 0
+	for r.Next() {
+		count++
+	}
+	if count != 2 {
+		t.Errorf("count: %d, want 2", count)
+	}
+	_ = db.Close()
+}
+
+func TestDial(t *testing.T) {
+	connStr := fmt.Sprintf(`{"protocol": "gorpc", "address": "%s", "tablet_type": "foo", "timeout": %d}`, testAddress, int64(30*time.Second))
+	c, err := drv{}.Open(connStr)
+	if err != nil {
+		t.Error(err)
+	}
+	wantc := &conn{
+		Protocol:   "gorpc",
+		TabletType: "foo",
+		Streaming:  false,
+		Timeout:    30 * time.Second,
+	}
+	newc := *(c.(*conn))
+	newc.Address = ""
+	newc.vtgateConn = nil
+	if !reflect.DeepEqual(&newc, wantc) {
+		t.Errorf("conn: %+v, want %+v", &newc, wantc)
+	}
+	_ = c.Close()
+
+	_, err = drv{}.Open(`{"protocol": "none"}`)
+	want := "could not find dialer for protocol none"
+	if err == nil || !strings.Contains(err.Error(), want) {
+		t.Errorf("err: %v, want %s", err, want)
+	}
+
+	_, err = drv{}.Open(`{`)
+	want = "unexpected end of JSON input"
+	if err == nil || !strings.Contains(err.Error(), want) {
+		t.Errorf("err: %v, want %s", err, want)
+	}
+}
+
+func TestExec(t *testing.T) {
+	connStr := fmt.Sprintf(`{"protocol": "gorpc", "address": "%s", "tablet_type": "rdonly", "timeout": %d}`, testAddress, int64(30*time.Second))
+	c, err := drv{}.Open(connStr)
+	if err != nil {
+		t.Error(err)
+	}
+	s, _ := c.Prepare("request1")
+	if ni := s.NumInput(); ni != -1 {
+		t.Errorf("got %d, want -1", ni)
+	}
+	r, err := s.Exec([]driver.Value{int64(0)})
+	if err != nil {
+		t.Error(err)
+	}
+	if v, _ := r.LastInsertId(); v != 72 {
+		t.Errorf("insert id: %d, want 72", v)
+	}
+	if v, _ := r.RowsAffected(); v != 123 {
+		t.Errorf("rows affected: %d, want 123", v)
+	}
+	_ = s.Close()
+
+	s, _ = c.Prepare("none")
+	_, err = s.Exec(nil)
+	want := "no match for: none"
+	if err == nil || !strings.Contains(err.Error(), want) {
+		t.Errorf("err: %v, does not contain %s", err, want)
+	}
+	_ = c.Close()
+
+	connStr = fmt.Sprintf(`{"protocol": "gorpc", "address": "%s", "tablet_type": "rdonly", "streaming": true, "timeout": %d}`, testAddress, int64(30*time.Second))
+	c, err = drv{}.Open(connStr)
+	if err != nil {
+		t.Error(err)
+	}
+	s, _ = c.Prepare("request1")
+	r, err = s.Exec([]driver.Value{int64(0)})
+	want = "Exec not allowed for streaming connections"
+	if err == nil || !strings.Contains(err.Error(), want) {
+		t.Errorf("err: %v, does not contain %s", err, want)
+	}
+}
+
+func TestQuery(t *testing.T) {
+	connStr := fmt.Sprintf(`{"protocol": "gorpc", "address": "%s", "tablet_type": "rdonly", "timeout": %d}`, testAddress, int64(30*time.Second))
+	c, err := drv{}.Open(connStr)
+	if err != nil {
+		t.Error(err)
+	}
+	s, _ := c.Prepare("request1")
+	r, err := s.Query([]driver.Value{int64(0)})
+	if err != nil {
+		t.Error(err)
+	}
+	cols := r.Columns()
+	wantCols := []string{
+		"field1",
+		"field2",
+	}
+	if !reflect.DeepEqual(cols, wantCols) {
+		t.Errorf("cols: %v, want %v", cols, wantCols)
+	}
+	row := make([]driver.Value, 2)
+	count := 0
+	for {
+		err = r.Next(row)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			t.Error(err)
+		}
+		count++
+	}
+	if count != 2 {
+		t.Errorf("count: %d, want 2", count)
+	}
+	_ = s.Close()
+
+	s, _ = c.Prepare("none")
+	_, err = s.Query(nil)
+	want := "no match for: none"
+	if err == nil || !strings.Contains(err.Error(), want) {
+		t.Errorf("err: %v, does not contain %s", err, want)
+	}
+	_ = c.Close()
+
+	connStr = fmt.Sprintf(`{"protocol": "gorpc", "address": "%s", "tablet_type": "rdonly", "streaming": true, "timeout": %d}`, testAddress, int64(30*time.Second))
+	c, err = drv{}.Open(connStr)
+	if err != nil {
+		t.Error(err)
+	}
+	s, _ = c.Prepare("request1")
+	r, err = s.Query([]driver.Value{int64(0)})
+	if err != nil {
+		t.Error(err)
+	}
+	cols = r.Columns()
+	wantCols = []string{
+		"field1",
+		"field2",
+	}
+	if !reflect.DeepEqual(cols, wantCols) {
+		t.Errorf("cols: %v, want %v", cols, wantCols)
+	}
+	row = make([]driver.Value, 2)
+	count = 0
+	for {
+		err = r.Next(row)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			t.Error(err)
+		}
+		count++
+	}
+	if count != 2 {
+		t.Errorf("count: %d, want 2", count)
+	}
+	_ = s.Close()
+	_ = c.Close()
+}
+
+func TestTx(t *testing.T) {
+	connStr := fmt.Sprintf(`{"protocol": "gorpc", "address": "%s", "tablet_type": "", "timeout": %d}`, testAddress, int64(30*time.Second))
+	c, err := drv{}.Open(connStr)
+	if err != nil {
+		t.Error(err)
+	}
+	tx, err := c.Begin()
+	if err != nil {
+		t.Error(err)
+	}
+	s, _ := c.Prepare("txRequest")
+	_, err = s.Exec(nil)
+	if err != nil {
+		t.Error(err)
+	}
+	err = tx.Commit()
+	if err != nil {
+		t.Error(err)
+	}
+	err = tx.Commit()
+	want := "commit: not in transaction"
+	if err == nil || !strings.Contains(err.Error(), want) {
+		t.Errorf("err: %v, does not contain %s", err, want)
+	}
+	_ = c.Close()
+
+	c, err = drv{}.Open(connStr)
+	if err != nil {
+		t.Error(err)
+	}
+	tx, err = c.Begin()
+	if err != nil {
+		t.Error(err)
+	}
+	s, _ = c.Prepare("txRequest")
+	_, err = s.Query(nil)
+	if err != nil {
+		t.Error(err)
+	}
+	err = tx.Rollback()
+	if err != nil {
+		t.Error(err)
+	}
+	err = tx.Rollback()
+	if err != nil {
+		t.Error(err)
+	}
+	_ = c.Close()
+
+	connStr = fmt.Sprintf(`{"protocol": "gorpc", "address": "%s", "tablet_type": "rdonly", "streaming": true, "timeout": %d}`, testAddress, int64(30*time.Second))
+	c, err = drv{}.Open(connStr)
+	if err != nil {
+		t.Error(err)
+	}
+	_, err = c.Begin()
+	want = "transaction not allowed for streaming connection"
+	if err == nil || !strings.Contains(err.Error(), want) {
+		t.Errorf("err: %v, does not contain %s", err, want)
+	}
+	_ = c.Close()
+}
