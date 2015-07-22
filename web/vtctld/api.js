47aa42e990fbd17cbe224c8f6aa1945d180c358f
Anthony Yeh <enisoc@google.com>
2015-07-22 14:42:59 -0700

    web/vtctld: Split app.js into separate files.

---
+++ web/vtctld/api.js
@@ -0,0 +1,33 @@
+app.factory('cells', function($resource) {
+  return $resource('/api/cells/');
+});
+
+app.factory('keyspaces', function($resource) {
+  return $resource('/api/keyspaces/:keyspace', {}, {
+      'action': {method: 'POST'}
+  });
+});
+
+app.factory('shards', function($resource) {
+  return $resource('/api/shards/:keyspace/:shard', {}, {
+      'action': {method: 'POST'}
+  });
+});
+
+app.factory('tablets', function($resource) {
+  return $resource('/api/tablets/:tablet', {}, {
+      'action': {method: 'POST'}
+  });
+});
+
+app.factory('tabletinfo', function($resource) {
+  return $resource('/api/tablets/:tablet/:info');
+});
+
+app.factory('endpoints', function($resource) {
+  return $resource('/api/endpoints/:cell/:keyspace/:shard/:tabletType');
+});
+
+app.factory('topodata', function($resource) {
+  return $resource('/api/topodata/:path');
+});
