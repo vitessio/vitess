2b9975f2d1a6425c51f996ebf3d94b13fb89c665
Michael Berlin <mberlin@google.com>
2015-07-30 17:58:40 -0700

    Fix linter warnings.

--- test/automation_horizontal_resharding.py
+++ test/automation_horizontal_resharding.py
@@ -3,13 +3,12 @@
 # Copyright 2015, Google Inc. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can
 # be found in the LICENSE file.
-"""
-End-to-end test for horizontal resharding automation.
-"""
+"""End-to-end test for horizontal resharding automation."""
 
 import environment
 import utils
 import worker
+
 
 def setUpModule():
   worker.setUpModule()
@@ -17,6 +16,7 @@ def setUpModule():
 
 def tearDownModule():
   worker.tearDownModule()
+
 
 class TestAutomationHorizontalResharding(worker.TestBaseSplitClone):
   """This test reuses worker.py because worker.py also covers the happy path
@@ -30,26 +30,32 @@ class TestAutomationHorizontalResharding(worker.TestBaseSplitClone):
     worker_proc, _, worker_rpc_port = utils.run_vtworker_bg(
         ['--cell', 'test_nj'],
         auto_log=True)
+    vtworker_endpoint = "localhost:" + str(worker_rpc_port)
 
     automation_server_proc, automation_server_port = utils.run_automation_server()
 
     keyspace = 'test_keyspace'
-    source_shard_list = "0"
-    dest_shard_list = "-80,80-"
-    _protocol, endpoint = utils.vtctld.rpc_endpoint()
+    source_shard_list = '0'
+    dest_shard_list = '-80,80-'
+    _, vtctld_endpoint = utils.vtctld.rpc_endpoint()
     utils.run(environment.binary_argstr('automation_client') +
               ' --server localhost:' + str(automation_server_port) +
               ' --task HorizontalReshardingTask' +
               ' --param keyspace=' + keyspace +
               ' --param source_shard_list=' + source_shard_list +
               ' --param dest_shard_list=' + dest_shard_list +
-              ' --param source_shard_rdonly_list=' + worker.shard_rdonly1.tablet_alias +
-              ' --param dest_shard_rdonly_list=' + worker.shard_0_rdonly1.tablet_alias + ',' + worker.shard_1_rdonly1.tablet_alias +
-              ' --param vtctld_endpoint=' + endpoint +
-              ' --param vtworker_endpoint=localhost:' + str(worker_rpc_port))
+              ' --param source_shard_rdonly_list=' +
+              worker.shard_rdonly1.tablet_alias +
+              ' --param dest_shard_rdonly_list=' +
+              worker.shard_0_rdonly1.tablet_alias + ',' +
+              worker.shard_1_rdonly1.tablet_alias +
+              ' --param vtctld_endpoint=' + vtctld_endpoint +
+              ' --param vtworker_endpoint=' + vtworker_endpoint)
 
-    self.assert_shard_data_equal(0, worker.shard_master, worker.shard_0_tablets.replica)
-    self.assert_shard_data_equal(1, worker.shard_master, worker.shard_1_tablets.replica)
+    self.assert_shard_data_equal(0, worker.shard_master,
+                                 worker.shard_0_tablets.replica)
+    self.assert_shard_data_equal(1, worker.shard_master,
+                                 worker.shard_1_tablets.replica)
 
     utils.kill_sub_process(automation_server_proc, soft=True)
     utils.kill_sub_process(worker_proc, soft=True)
