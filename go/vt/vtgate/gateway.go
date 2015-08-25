5e54a48c43e049243417a21a1a3a2c909fce1fa3
Liang Guo <liguo@google.com>
2015-08-25 13:58:42 -0700

    Refactor ScatterConn to separate scatter logic and connection map with an interface.

---
+++ go/vt/vtgate/gateway.go
@@ -0,0 +1,74 @@
+// Copyright 2015, Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package vtgate
+
+import (
+	"flag"
+	"time"
+
+	log "github.com/golang/glog"
+	"golang.org/x/net/context"
+
+	mproto "github.com/youtube/vitess/go/mysql/proto"
+	"github.com/youtube/vitess/go/stats"
+	pb "github.com/youtube/vitess/go/vt/proto/topodata"
+	tproto "github.com/youtube/vitess/go/vt/tabletserver/proto"
+	"github.com/youtube/vitess/go/vt/tabletserver/tabletconn"
+)
+
+var (
+	GatewayImplementation = flag.String("gateway_implementation", "shardconn", "The implementation of gateway")
+)
+
+// A Gateway is the query processing module for each shard,
+// which is used by ScatterConn.
+type Gateway interface {
+	// Dial creates the connection for the specified keyspace, shard, and tablet type.
+	Dial(ctx context.Context, keyspace, shard string, tabletType pb.TabletType) error
+
+	// Execute executes the non-streaming query for the specified keyspace, shard, and tablet type.
+	Execute(ctx context.Context, keyspace, shard string, tabletType pb.TabletType, query string, bindVars map[string]interface{}, transactionID int64) (*mproto.QueryResult, error)
+
+	// ExecuteBatch executes a group of queries for the specified keyspace, shard, and tablet type.
+	ExecuteBatch(ctx context.Context, keyspace, shard string, tabletType pb.TabletType, queries []tproto.BoundQuery, asTransaction bool, transactionID int64) (*tproto.QueryResultList, error)
+
+	// StreamExecute executes a streaming query for the specified keyspace, shard, and tablet type.
+	StreamExecute(ctx context.Context, keyspace, shard string, tabletType pb.TabletType, query string, bindVars map[string]interface{}, transactionID int64) (<-chan *mproto.QueryResult, tabletconn.ErrFunc)
+
+	// Begin starts a transaction for the specified keyspace, shard, and tablet type.
+	// It returns the transaction ID.
+	Begin(ctx context.Context, keyspace string, shard string, tabletType pb.TabletType) (int64, error)
+
+	// Commit commits the current transaction for the specified keyspace, shard, and tablet type.
+	Commit(ctx context.Context, keyspace, shard string, tabletType pb.TabletType, transactionID int64) error
+
+	// Rollback rolls back the current transaction for the specified keyspace, shard, and tablet type.
+	Rollback(ctx context.Context, keyspace, shard string, tabletType pb.TabletType, transactionID int64) error
+
+	// SplitQuery splits a query into sub-queries for the specified keyspace, shard, and tablet type.
+	SplitQuery(ctx context.Context, keyspace, shard string, tabletType pb.TabletType, sql string, bindVariables map[string]interface{}, splitColumn string, splitCount int) ([]tproto.QuerySplit, error)
+
+	// Close closes the underlying connections.
+	Close() error
+}
+
+type GatewayCreator func(serv SrvTopoServer, cell string, retryDelay time.Duration, retryCount int, connTimeoutTotal, connTimeoutPerConn, connLife time.Duration, connTimings *stats.MultiTimings) Gateway
+
+var gatewayCreators = make(map[string]GatewayCreator)
+
+func RegisterGatewayCreator(name string, gc GatewayCreator) {
+	if _, ok := gatewayCreators[name]; ok {
+		log.Fatalf("Gateway %s already exists", name)
+	}
+	gatewayCreators[name] = gc
+}
+
+func GetGatewayCreator() GatewayCreator {
+	gc, ok := gatewayCreators[*GatewayImplementation]
+	if !ok {
+		log.Fatalf("No gateway registered as %s", *GatewayImplementation)
+	}
+	return gc
+}
---
+++ go/vt/vtgate/shardgateway.go
@@ -0,0 +1,122 @@
+// Copyright 2015, Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package vtgate
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+	"time"
+
+	"golang.org/x/net/context"
+
+	mproto "github.com/youtube/vitess/go/mysql/proto"
+	"github.com/youtube/vitess/go/stats"
+	pb "github.com/youtube/vitess/go/vt/proto/topodata"
+	tproto "github.com/youtube/vitess/go/vt/tabletserver/proto"
+	"github.com/youtube/vitess/go/vt/tabletserver/tabletconn"
+)
+
+const (
+	gatewayImplementation = "shardconn"
+)
+
+func init() {
+	RegisterGatewayCreator(gatewayImplementation, createShardGateway)
+}
+
+func createShardGateway(serv SrvTopoServer, cell string, retryDelay time.Duration, retryCount int, connTimeoutTotal, connTimeoutPerConn, connLife time.Duration, connTimings *stats.MultiTimings) Gateway {
+	return &shardGateway{
+		toposerv:           serv,
+		cell:               cell,
+		retryDelay:         retryDelay,
+		retryCount:         retryCount,
+		connTimeoutTotal:   connTimeoutTotal,
+		connTimeoutPerConn: connTimeoutPerConn,
+		connLife:           connLife,
+		connTimings:        connTimings,
+		shardConns:         make(map[string]*ShardConn),
+	}
+}
+
+// A Gateway is the query processing module for each shard.
+type shardGateway struct {
+	toposerv           SrvTopoServer
+	cell               string
+	retryDelay         time.Duration
+	retryCount         int
+	connTimeoutTotal   time.Duration
+	connTimeoutPerConn time.Duration
+	connLife           time.Duration
+	connTimings        *stats.MultiTimings
+
+	mu         sync.Mutex
+	shardConns map[string]*ShardConn
+}
+
+// Dial creates the connection for the specified keyspace, shard, and tablet type.
+func (sg *shardGateway) Dial(ctx context.Context, keyspace string, shard string, tabletType pb.TabletType) error {
+	return sg.getConnection(ctx, keyspace, shard, tabletType).Dial(ctx)
+}
+
+// Execute executes the non-streaming query for the specified keyspace, shard, and tablet type.
+func (sg *shardGateway) Execute(ctx context.Context, keyspace string, shard string, tabletType pb.TabletType, query string, bindVars map[string]interface{}, transactionID int64) (*mproto.QueryResult, error) {
+	return sg.getConnection(ctx, keyspace, shard, tabletType).Execute(ctx, query, bindVars, transactionID)
+}
+
+// ExecuteBatch executes a group of queries for the specified keyspace, shard, and tablet type.
+func (sg *shardGateway) ExecuteBatch(ctx context.Context, keyspace string, shard string, tabletType pb.TabletType, queries []tproto.BoundQuery, asTransaction bool, transactionID int64) (*tproto.QueryResultList, error) {
+	return sg.getConnection(ctx, keyspace, shard, tabletType).ExecuteBatch(ctx, queries, asTransaction, transactionID)
+}
+
+// StreamExecute executes a streaming query for the specified keyspace, shard, and tablet type.
+func (sg *shardGateway) StreamExecute(ctx context.Context, keyspace string, shard string, tabletType pb.TabletType, query string, bindVars map[string]interface{}, transactionID int64) (<-chan *mproto.QueryResult, tabletconn.ErrFunc) {
+	return sg.getConnection(ctx, keyspace, shard, tabletType).StreamExecute(ctx, query, bindVars, transactionID)
+}
+
+// Begin starts a transaction for the specified keyspace, shard, and tablet type.
+// It returns the transaction ID.
+func (sg *shardGateway) Begin(ctx context.Context, keyspace string, shard string, tabletType pb.TabletType) (int64, error) {
+	return sg.getConnection(ctx, keyspace, shard, tabletType).Begin(ctx)
+}
+
+// Commit commits the current transaction for the specified keyspace, shard, and tablet type.
+func (sg *shardGateway) Commit(ctx context.Context, keyspace string, shard string, tabletType pb.TabletType, transactionID int64) error {
+	return sg.getConnection(ctx, keyspace, shard, tabletType).Commit(ctx, transactionID)
+}
+
+// Rollback rolls back the current transaction for the specified keyspace, shard, and tablet type.
+func (sg *shardGateway) Rollback(ctx context.Context, keyspace string, shard string, tabletType pb.TabletType, transactionID int64) error {
+	return sg.getConnection(ctx, keyspace, shard, tabletType).Rollback(ctx, transactionID)
+}
+
+// SplitQuery splits a query into sub-queries for the specified keyspace, shard, and tablet type.
+func (sg *shardGateway) SplitQuery(ctx context.Context, keyspace string, shard string, tabletType pb.TabletType, sql string, bindVars map[string]interface{}, splitColumn string, splitCount int) ([]tproto.QuerySplit, error) {
+	return sg.getConnection(ctx, keyspace, shard, tabletType).SplitQuery(ctx, sql, bindVars, splitColumn, splitCount)
+}
+
+// Close closes the underlying connections.
+func (sg *shardGateway) Close() error {
+	sg.mu.Lock()
+	defer sg.mu.Unlock()
+	for _, v := range sg.shardConns {
+		v.Close()
+	}
+	sg.shardConns = make(map[string]*ShardConn)
+	return nil
+}
+
+func (sg *shardGateway) getConnection(ctx context.Context, keyspace, shard string, tabletType pb.TabletType) *ShardConn {
+	sg.mu.Lock()
+	defer sg.mu.Unlock()
+
+	key := fmt.Sprintf("%s.%s.%s", keyspace, shard, strings.ToLower(tabletType.String()))
+	sdc, ok := sg.shardConns[key]
+	if !ok {
+		sdc = NewShardConn(ctx, sg.toposerv, sg.cell, keyspace, shard, tabletType, sg.retryDelay, sg.retryCount, sg.connTimeoutTotal, sg.connTimeoutPerConn, sg.connLife, sg.connTimings)
+		sg.shardConns[key] = sdc
+	}
+	return sdc
+}
