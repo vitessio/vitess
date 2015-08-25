f9e68df8a5748c68e67d3ad726a4db943b2aa1aa
Michael Berlin <mberlin@google.com>
2015-08-25 16:32:09 -0700

    automation: Add task for vtctl MigrateServedTypes.
    Extended end-to-end test to verify the new task.

---
+++ go/vt/automation/migrate_served_types_task.go
@@ -0,0 +1,35 @@
+// Copyright 2015, Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package automation
+
+import (
+	"fmt"
+
+	pb "github.com/youtube/vitess/go/vt/proto/automation"
+	"golang.org/x/net/context"
+)
+
+// MigrateServedTypesTask runs vtctl MigrateServedTypes to migrate a serving
+// type from the source shard to the shards that it replicates to.
+type MigrateServedTypesTask struct {
+}
+
+// Run is part of the Task interface.
+func (t *MigrateServedTypesTask) Run(parameters map[string]string) ([]*pb.TaskContainer, string, error) {
+	keyspaceAndShard := fmt.Sprintf("%v/%v", parameters["keyspace"], parameters["source_shard"])
+	output, err := ExecuteVtctl(context.TODO(), parameters["vtctld_endpoint"],
+		[]string{"MigrateServedTypes", keyspaceAndShard, parameters["type"]})
+	return nil, output, err
+}
+
+// RequiredParameters is part of the Task interface.
+func (t *MigrateServedTypesTask) RequiredParameters() []string {
+	return []string{"keyspace", "source_shard", "type", "vtctld_endpoint"}
+}
+
+// OptionalParameters is part of the Task interface.
+func (t *MigrateServedTypesTask) OptionalParameters() []string {
+	return nil
+}
