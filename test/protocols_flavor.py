86215c7b2da534cd7a812294cb6c17ca18363ff7
Dean Yasuda <dumbunny@google.com>
2015-08-30 20:22:16 -0700

    Fix docstrings and var names in test/protocols_flavor.py.

--- test/protocols_flavor.py
+++ test/protocols_flavor.py
@@ -1,59 +1,67 @@
 #!/usr/bin/env python
+"""Base class for protocols flavor.
+
+Each set of protocols has a flavor name. Derived classes should call
+register_flavor when they are imported.
+"""
 
 import logging
 
 
 class ProtocolsFlavor(object):
-  """Base class for protocols"""
+  """Base class for protocols flavor."""
 
   def binlog_player_protocol(self):
-    """Returns the name of the binlog player protocol to use
-    between vttablets, in go."""
+    """Tdthe binlog player protocol between vttablets, in go."""
     raise NotImplementedError('Not implemented in the base class')
 
   def binlog_player_python_protocol(self):
-    """Returns the name of the binlog player protocol to use
-    for python connections to update_stream service."""
+    """The binlog player protocol in for python connections.
+
+    This is for python connections to update_stream service.
+    """
     raise NotImplementedError('Not implemented in the base class')
 
   def vtctl_client_protocol(self):
-    """Returns the protocol to use for vtctl connections.
-    This is just for the go client."""
+    """The protocol to use for vtctl connections.
+
+    This is just for the go client.
+    """
     raise NotImplementedError('Not implemented in the base class')
 
   def vtctl_python_client_protocol(self):
-    """Returns the protocol to use for vtctl connections.
-    This is just for the python client."""
+    """The protocol to use for vtctl connections.
+
+    This is just for the python client.
+    """
     raise NotImplementedError('Not implemented in the base class')
 
   def vtworker_client_protocol(self):
-    """Returns the protocol to use for vtworker connections."""
+    """The protocol to use for vtworker connections."""
     raise NotImplementedError('Not implemented in the base class')
 
   def tablet_manager_protocol(self):
-    """Returns the protocol to use for the tablet manager protocol."""
+    """The protocol to use for the tablet manager protocol."""
     raise NotImplementedError('Not implemented in the base class')
 
   def tabletconn_protocol(self):
-    """Returns the protocol to use for connections from vtctl/vtgate to
-    vttablet."""
+    """The protocol to use for connections from vtctl/vtgate to vttablet."""
     raise NotImplementedError('Not implemented in the base class')
 
   def vtgate_protocol(self):
-    """Returns the protocol to use to talk to vtgate, in go."""
+    """The protocol to use to talk to vtgate, in go."""
     raise NotImplementedError('Not implemented in the base class')
 
   def vtgate_python_protocol(self):
-    """Returns the protocol to use to talk to vtgate with python clients."""
+    """The protocol to use to talk to vtgate with python clients."""
     raise NotImplementedError('Not implemented in the base class')
 
   def rpc_timeout_message(self):
-    """Returns the error message used by the protocol to indicate a timeout."""
+    """The error message used by the protocol to indicate a timeout."""
     raise NotImplementedError('Not implemented in the base class')
 
   def service_map(self):
-    """Returns a list of entries for the service map to enable all
-    relevant protocols in all servers."""
+    """A list of entries to enable all relevant protocols in all servers."""
     raise NotImplementedError('Not implemented in the base class')
 
 
@@ -66,16 +74,17 @@ def protocols_flavor():
 
 
 def set_protocols_flavor(flavor):
+  """Set the protocols flavor by flavor name."""
   global __protocols_flavor
 
   if not flavor:
     flavor = 'gorpc'
 
-  klass = __knows_protocols_flavor_map.get(flavor, None)
-  if not klass:
+  cls = __knows_protocols_flavor_map.get(flavor, None)
+  if not cls:
     logging.error('Unknown protocols flavor %s', flavor)
     exit(1)
-  __protocols_flavor = klass()
+  __protocols_flavor = cls()
 
   logging.debug('Using protocols flavor %s', flavor)
 
