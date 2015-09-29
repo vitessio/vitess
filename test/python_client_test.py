a7916b9d286bd9be8151b0936a43cac179ba50ef
Ammar Aijazi <aaijazi@google.com>
2015-09-28 18:25:59 -0700

    Refactor python_client_tests.py to split error test cases into their own class.

--- test/python_client_test.py
+++ test/python_client_test.py
@@ -60,11 +60,15 @@ def tearDownModule():
   environment.topo_server().teardown()
 
 
-class TestPythonClient(unittest.TestCase):
+class TestPythonClientBase(unittest.TestCase):
+  """Base class for Python client tests"""
   CONNECT_TIMEOUT = 10.0
 
+  # A packed keyspace_id from the middle of the full keyrange.
+  KEYSPACE_ID_0X80 = struct.Struct('!Q').pack(0x80 << 56)
+
   def setUp(self):
-    super(TestPythonClient, self).setUp()
+    super(TestPythonClientBase, self).setUp()
     addr = 'localhost:%d' % vtgateclienttest_port
     protocol = protocols_flavor().vtgate_python_protocol()
     self.conn = vtgate_client.connect(protocol, addr, 30.0)
@@ -74,46 +78,6 @@ class TestPythonClient(unittest.TestCase):
 
   def tearDown(self):
     self.conn.close()
-
-  def test_success_get_srv_keyspace(self):
-    """Test we get the right results from get_srv_keyspace.
-
-    We only test the successful cases.
-    """
-
-    # big has one big shard
-    big = self.conn.get_srv_keyspace('big')
-    self.assertEquals(big.name, 'big')
-    self.assertEquals(big.sharding_col_name, 'sharding_column_name')
-    self.assertEquals(big.sharding_col_type, keyrange_constants.KIT_UINT64)
-    self.assertEquals(big.served_from, {'master': 'other_keyspace'})
-    self.assertEquals(big.get_shards('replica'),
-                      [{'Name': 'shard0',
-                        'KeyRange': {
-                            'Start': '\x40\x00\x00\x00\x00\x00\x00\x00',
-                            'End': '\x80\x00\x00\x00\x00\x00\x00\x00',
-                            }}])
-    self.assertEquals(big.get_shard_count('replica'), 1)
-    self.assertEquals(big.get_shard_count('rdonly'), 0)
-    self.assertEquals(big.get_shard_names('replica'), ['shard0'])
-    self.assertEquals(big.keyspace_id_to_shard_name_for_db_type(
-        0x6000000000000000, 'replica'), 'shard0')
-    with self.assertRaises(ValueError):
-      big.keyspace_id_to_shard_name_for_db_type(0x2000000000000000, 'replica')
-
-    # small has no shards
-    small = self.conn.get_srv_keyspace('small')
-    self.assertEquals(small.name, 'small')
-    self.assertEquals(small.sharding_col_name, '')
-    self.assertEquals(small.sharding_col_type, keyrange_constants.KIT_UNSET)
-    self.assertEquals(small.served_from, {})
-    self.assertEquals(small.get_shards('replica'), [])
-    self.assertEquals(small.get_shard_count('replica'), 0)
-    with self.assertRaises(ValueError):
-      small.keyspace_id_to_shard_name_for_db_type(0x6000000000000000, 'replica')
-
-  # A packed keyspace_id from the middle of the full keyrange.
-  KEYSPACE_ID_0X80 = struct.Struct('!Q').pack(0x80 << 56)
 
   def _open_keyspace_ids_cursor(self):
     return self.conn.cursor(
@@ -136,6 +100,21 @@ class TestPythonClient(unittest.TestCase):
     return self.conn.cursor(
         'keyspace', 'master', keyspace_ids=[self.KEYSPACE_ID_0X80],
         cursorclass=vtgate_cursor.StreamVTGateCursor)
+
+
+class TestPythonClientErrors(TestPythonClientBase):
+  """Test cases to verify that the Python client can handle errors correctly"""
+
+  def test_integrity_error(self):
+    """Test we raise dbexceptions.IntegrityError for Execute calls."""
+    # Special query that makes vtgateclienttest return an IntegrityError.
+    self._test_integrity_error_for_execute('error://integrity error')
+
+  def test_partial_integrity_error(self):
+    """Test we raise dbexceptions.IntegrityError when Execute calls
+    return a partial error."""
+    # Special query that makes vtgateclienttest return a partial error.
+    self._test_integrity_error_for_execute('partialerror://integrity error')
 
   def _test_integrity_error_for_execute(self, query):
     """Helper for testing that we raise dbexceptions.IntegrityError if an
@@ -191,17 +170,6 @@ class TestPythonClient(unittest.TestCase):
                   shards=[keyrange_constants.SHARD_ZERO])])
     cursor.close()
 
-  def test_integrity_error(self):
-    """Test we raise dbexceptions.IntegrityError for Execute calls."""
-    # Special query that makes vtgateclienttest return an IntegrityError.
-    self._test_integrity_error_for_execute('error://integrity error')
-
-  def test_partial_integrity_error(self):
-    """Test we raise dbexceptions.IntegrityError when Execute calls
-    return a partial error."""
-    # Special query that makes vtgateclienttest return a partial error.
-    self._test_integrity_error_for_execute('partialerror://integrity error')
-
   def test_streaming_integrity_error(self):
     """Test we raise dbexceptions.IntegrityError for StreamExecute calls."""
     # Special query that makes vtgateclienttest return an IntegrityError.
@@ -221,7 +189,8 @@ class TestPythonClient(unittest.TestCase):
 
   def test_error(self):
     """Test a regular server error raises the right exception."""
-    error_caller_id = vtgate_client.CallerID(principal='error://unknown error')
+    error_request = 'error://unknown error'
+    error_caller_id = vtgate_client.CallerID(principal=error_request)
 
     # Begin test
     with self.assertRaisesRegexp(dbexceptions.DatabaseError, "forced error"):
@@ -237,7 +206,48 @@ class TestPythonClient(unittest.TestCase):
 
     # GetSrvKeyspace test
     with self.assertRaisesRegexp(dbexceptions.DatabaseError, "forced error"):
-      self.conn.get_srv_keyspace('error://unknown error')
+      self.conn.get_srv_keyspace(error_request)
+
+
+class TestPythonClient(TestPythonClientBase):
+  """Non-error test cases for the Python client."""
+
+  def test_success_get_srv_keyspace(self):
+    """Test we get the right results from get_srv_keyspace.
+
+    We only test the successful cases.
+    """
+
+    # big has one big shard
+    big = self.conn.get_srv_keyspace('big')
+    self.assertEquals(big.name, 'big')
+    self.assertEquals(big.sharding_col_name, 'sharding_column_name')
+    self.assertEquals(big.sharding_col_type, keyrange_constants.KIT_UINT64)
+    self.assertEquals(big.served_from, {'master': 'other_keyspace'})
+    self.assertEquals(big.get_shards('replica'),
+                      [{'Name': 'shard0',
+                        'KeyRange': {
+                            'Start': '\x40\x00\x00\x00\x00\x00\x00\x00',
+                            'End': '\x80\x00\x00\x00\x00\x00\x00\x00',
+                            }}])
+    self.assertEquals(big.get_shard_count('replica'), 1)
+    self.assertEquals(big.get_shard_count('rdonly'), 0)
+    self.assertEquals(big.get_shard_names('replica'), ['shard0'])
+    self.assertEquals(big.keyspace_id_to_shard_name_for_db_type(
+        0x6000000000000000, 'replica'), 'shard0')
+    with self.assertRaises(ValueError):
+      big.keyspace_id_to_shard_name_for_db_type(0x2000000000000000, 'replica')
+
+    # small has no shards
+    small = self.conn.get_srv_keyspace('small')
+    self.assertEquals(small.name, 'small')
+    self.assertEquals(small.sharding_col_name, '')
+    self.assertEquals(small.sharding_col_type, keyrange_constants.KIT_UNSET)
+    self.assertEquals(small.served_from, {})
+    self.assertEquals(small.get_shards('replica'), [])
+    self.assertEquals(small.get_shard_count('replica'), 0)
+    with self.assertRaises(ValueError):
+      small.keyspace_id_to_shard_name_for_db_type(0x6000000000000000, 'replica')
 
   def test_effective_caller_id(self):
     """Test that the passed in effective_caller_id is parsed correctly.
@@ -324,6 +334,7 @@ class TestPythonClient(unittest.TestCase):
     check_good_and_bad_effective_caller_ids(
         self._open_stream_keyranges_cursor(),
         cursor_stream_execute_keyranges_method)
+
 
 if __name__ == '__main__':
   utils.main()
