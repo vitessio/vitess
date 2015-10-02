8062499471015221e0cf5926c3dbc476d311e5b8
Ammar Aijazi <aaijazi@google.com>
2015-10-02 07:44:21 -0700

    Revert "Merge branch 'suguwork'"
    This reverts commit bf185cb9dbfac57a4624a982888b671f6743f42b, reversing
    changes made to 347c76ff509a376551ec0e0c963a87530026df02.

--- go/vt/tabletserver/endtoend/framework/logtracker.go
+++ go/vt/tabletserver/endtoend/framework/logtracker.go
@@ -5,13 +5,12 @@
 package framework
 
 import (
-	"errors"
 	"time"
 
 	"github.com/youtube/vitess/go/vt/tabletserver"
 )
 
-// TxFetcher allows you to capture and fetch info on the next transaction.
+// TxFetcher alows you to capture and fetch info on the next transaction.
 type TxFetcher struct {
 	start time.Time
 	ch    chan interface{}
@@ -26,72 +25,13 @@ func NewTxFetcher() *TxFetcher {
 }
 
 // Fetch fetches the last captured transaction. It must be called only once.
-// If the wait is longer than one second, it returns an error.
-func (fetcher *TxFetcher) Fetch() (*tabletserver.TxConnection, error) {
-	tmr := time.NewTimer(1 * time.Second)
-	defer tmr.Stop()
+func (fetcher *TxFetcher) Fetch() *tabletserver.TxConnection {
 	for {
-		select {
-		case itx := <-fetcher.ch:
-			tx := itx.(*tabletserver.TxConnection)
-			// Skip any events that pre-date start time.
-			if tx.EndTime.Before(fetcher.start) {
-				continue
-			}
+		tx := (<-fetcher.ch).(*tabletserver.TxConnection)
+		// Skip any events that pre-date start time.
+		if tx.EndTime.After(fetcher.start) {
 			tabletserver.TxLogger.Unsubscribe(fetcher.ch)
-			return tx, nil
-		case <-tmr.C:
-			return nil, errors.New("error waiting for transaction event")
-		}
-	}
-}
-
-// QueryFetcher allows you to capture and fetch queries that are being
-// executed by TabletServer.
-type QueryFetcher struct {
-	start   time.Time
-	ch      chan interface{}
-	queries chan *tabletserver.LogStats
-}
-
-// NewQueryFetcher sets up the capture and retuns a QueryFetcher.
-// It has a buffer size of 20. You must call Close once done.
-func NewQueryFetcher() *QueryFetcher {
-	fetcher := &QueryFetcher{
-		start:   time.Now(),
-		ch:      tabletserver.StatsLogger.Subscribe("endtoend"),
-		queries: make(chan *tabletserver.LogStats, 20),
-	}
-	go func() {
-		for log := range fetcher.ch {
-			fetcher.queries <- log.(*tabletserver.LogStats)
-		}
-		close(fetcher.queries)
-	}()
-	return fetcher
-}
-
-// Close closes the QueryFetcher.
-func (fetcher *QueryFetcher) Close() {
-	tabletserver.StatsLogger.Unsubscribe(fetcher.ch)
-	close(fetcher.ch)
-}
-
-// Next fetches the next captured query.
-// If the wait is longer than one second, it returns an error.
-func (fetcher *QueryFetcher) Next() (*tabletserver.LogStats, error) {
-	tmr := time.NewTimer(1 * time.Second)
-	defer tmr.Stop()
-	for {
-		select {
-		case query := <-fetcher.queries:
-			// Skip any events that pre-date start time.
-			if query.EndTime.Before(fetcher.start) {
-				continue
-			}
-			return query, nil
-		case <-tmr.C:
-			return nil, errors.New("error waiting for query event")
+			return tx
 		}
 	}
 }
