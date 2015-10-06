a9bc7b9867702eae5444ab5da0a8e6931bb2a837
Sugu Sougoumarane <sougou@google.com>
2015-10-05 21:04:19 -0700

    tabletserver endtoend: regroup tests

--- go/vt/tabletserver/endtoend/acl_test.go
+++ go/vt/tabletserver/endtoend/acl_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 
 	mproto "github.com/youtube/vitess/go/mysql/proto"
+	"github.com/youtube/vitess/go/vt/tabletserver"
 	"github.com/youtube/vitess/go/vt/tabletserver/endtoend/framework"
 )
 
@@ -106,5 +107,57 @@ func TestTableACLNoAccess(t *testing.T) {
 		if err == nil || !strings.HasPrefix(err.Error(), tcase.err) {
 			t.Errorf("Error: %v, must start with %s", err, tcase.err)
 		}
+	}
+}
+
+var rulesJSON = []byte(`[{
+	"Name": "r1",
+	"Description": "disallow bindvar 'asdfg'",
+	"BindVarConds":[{
+		"Name": "asdfg",
+		"OnAbsent": false,
+		"Operator": "NOOP"
+	}]
+}]`)
+
+func TestQueryRules(t *testing.T) {
+	rules := tabletserver.NewQueryRules()
+	err := rules.UnmarshalJSON(rulesJSON)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	err = framework.DefaultServer.SetQueryRules("endtoend", rules)
+	want := "Rule source identifier endtoend is not valid"
+	if err == nil || err.Error() != want {
+		t.Errorf("Error: %v, want %s", err, want)
+	}
+
+	framework.DefaultServer.RegisterQueryRuleSource("endtoend")
+	defer framework.DefaultServer.UnRegisterQueryRuleSource("endtoend")
+	err = framework.DefaultServer.SetQueryRules("endtoend", rules)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	client := framework.NewDefaultClient()
+	query := "select * from vtocc_test where intval=:asdfg"
+	bv := map[string]interface{}{"asdfg": 1}
+	_, err = client.Execute(query, bv)
+	want = "error: Query disallowed due to rule: disallow bindvar 'asdfg'"
+	if err == nil || err.Error() != want {
+		t.Errorf("Error: %v, want %s", err, want)
+	}
+
+	err = framework.DefaultServer.SetQueryRules("endtoend", nil)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, err = client.Execute(query, bv)
+	if err != nil {
+		t.Error(err)
+		return
 	}
 }
--- go/vt/tabletserver/endtoend/nocache_test.go
+++ go/vt/tabletserver/endtoend/nocache_test.go
@@ -17,9 +17,7 @@ import (
 	"github.com/youtube/vitess/go/mysql"
 	mproto "github.com/youtube/vitess/go/mysql/proto"
 	"github.com/youtube/vitess/go/sqltypes"
-	"github.com/youtube/vitess/go/vt/tabletserver"
 	"github.com/youtube/vitess/go/vt/tabletserver/endtoend/framework"
-	"github.com/youtube/vitess/go/vt/tabletserver/proto"
 
 	pb "github.com/youtube/vitess/go/vt/proto/query"
 )
@@ -195,60 +193,6 @@ func TestTrailingComment(t *testing.T) {
 	}
 }
 
-func TestStrictMode(t *testing.T) {
-	queries := []string{
-		"insert into vtocc_a(eid, id, name, foo) values (7, 1+1, '', '')",
-		"insert into vtocc_d(eid, id) values (1, 1)",
-		"update vtocc_a set eid = 1+1 where eid = 1 and id = 1",
-		"insert into vtocc_d(eid, id) values (1, 1)",
-		"insert into upsert_test(id1, id2) values " +
-			"(1, 1), (2, 2) on duplicate key update id1 = 1",
-		"insert into upsert_test(id1, id2) select eid, id " +
-			"from vtocc_a limit 1 on duplicate key update id2 = id1",
-		"insert into upsert_test(id1, id2) values " +
-			"(1, 1) on duplicate key update id1 = 2+1",
-	}
-
-	// Strict mode on.
-	func() {
-		client := framework.NewDefaultClient()
-		err := client.Begin()
-		if err != nil {
-			t.Error(err)
-			return
-		}
-		defer client.Rollback()
-
-		want := "error: DML too complex"
-		for _, query := range queries {
-			_, err = client.Execute(query, nil)
-			if err == nil || err.Error() != want {
-				t.Errorf("Execute(%s): %v, want %s", query, err, want)
-			}
-		}
-	}()
-
-	// Strict mode off.
-	func() {
-		framework.DefaultServer.SetStrictMode(false)
-		defer framework.DefaultServer.SetStrictMode(true)
-
-		for _, query := range queries {
-			client := framework.NewDefaultClient()
-			err := client.Begin()
-			if err != nil {
-				t.Error(err)
-				return
-			}
-			_, err = client.Execute(query, nil)
-			if err != nil {
-				t.Error(err)
-			}
-			client.Rollback()
-		}
-	}()
-}
-
 func TestUpsertNonPKHit(t *testing.T) {
 	client := framework.NewDefaultClient()
 	err := client.Begin()
@@ -334,148 +278,6 @@ func TestConsolidation(t *testing.T) {
 	}
 }
 
-func TestBatchRead(t *testing.T) {
-	client := framework.NewDefaultClient()
-	queries := []proto.BoundQuery{{
-		Sql:           "select * from vtocc_a where id = :a",
-		BindVariables: map[string]interface{}{"a": 2},
-	}, {
-		Sql:           "select * from vtocc_b where id = :b",
-		BindVariables: map[string]interface{}{"b": 2},
-	}}
-	qr1 := mproto.QueryResult{
-		Fields: []mproto.Field{{
-			Name:  "eid",
-			Type:  mysql.TypeLonglong,
-			Flags: 0,
-		}, {
-			Name:  "id",
-			Type:  mysql.TypeLong,
-			Flags: 0,
-		}, {
-			Name:  "name",
-			Type:  mysql.TypeVarString,
-			Flags: 0,
-		}, {
-			Name:  "foo",
-			Type:  mysql.TypeVarString,
-			Flags: mysql.FlagBinary,
-		}},
-		RowsAffected: 1,
-		Rows: [][]sqltypes.Value{
-			[]sqltypes.Value{
-				sqltypes.Value{Inner: sqltypes.Numeric("1")},
-				sqltypes.Value{Inner: sqltypes.Numeric("2")},
-				sqltypes.Value{Inner: sqltypes.String("bcde")},
-				sqltypes.Value{Inner: sqltypes.String("fghi")},
-			},
-		},
-	}
-	qr2 := mproto.QueryResult{
-		Fields: []mproto.Field{{
-			Name:  "eid",
-			Type:  mysql.TypeLonglong,
-			Flags: 0,
-		}, {
-			Name:  "id",
-			Type:  mysql.TypeLong,
-			Flags: 0,
-		}},
-		RowsAffected: 1,
-		Rows: [][]sqltypes.Value{
-			[]sqltypes.Value{
-				sqltypes.Value{Inner: sqltypes.Numeric("1")},
-				sqltypes.Value{Inner: sqltypes.Numeric("2")},
-			},
-		},
-	}
-	want := &proto.QueryResultList{
-		List: []mproto.QueryResult{qr1, qr2},
-	}
-
-	qrl, err := client.ExecuteBatch(queries, false)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	if !reflect.DeepEqual(qrl, want) {
-		t.Errorf("ExecueBatch: \n%#v, want \n%#v", qrl, want)
-	}
-}
-
-func TestBatchTransaction(t *testing.T) {
-	client := framework.NewDefaultClient()
-	queries := []proto.BoundQuery{{
-		Sql: "insert into vtocc_test values(4, null, null, null)",
-	}, {
-		Sql: "select * from vtocc_test where intval = 4",
-	}, {
-		Sql: "delete from vtocc_test where intval = 4",
-	}}
-
-	wantRows := [][]sqltypes.Value{
-		[]sqltypes.Value{
-			sqltypes.Value{Inner: sqltypes.Numeric("4")},
-			sqltypes.Value{},
-			sqltypes.Value{},
-			sqltypes.Value{},
-		},
-	}
-
-	// Not in transaction, AsTransaction false
-	qrl, err := client.ExecuteBatch(queries, false)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	if !reflect.DeepEqual(qrl.List[1].Rows, wantRows) {
-		t.Errorf("Rows: \n%#v, want \n%#v", qrl.List[1].Rows, wantRows)
-	}
-
-	// Not in transaction, AsTransaction true
-	qrl, err = client.ExecuteBatch(queries, true)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	if !reflect.DeepEqual(qrl.List[1].Rows, wantRows) {
-		t.Errorf("Rows: \n%#v, want \n%#v", qrl.List[1].Rows, wantRows)
-	}
-
-	// In transaction, AsTransaction false
-	func() {
-		err = client.Begin()
-		if err != nil {
-			t.Error(err)
-			return
-		}
-		defer client.Commit()
-		qrl, err = client.ExecuteBatch(queries, false)
-		if err != nil {
-			t.Error(err)
-			return
-		}
-		if !reflect.DeepEqual(qrl.List[1].Rows, wantRows) {
-			t.Errorf("Rows: \n%#v, want \n%#v", qrl.List[1].Rows, wantRows)
-		}
-	}()
-
-	// In transaction, AsTransaction true
-	func() {
-		err = client.Begin()
-		if err != nil {
-			t.Error(err)
-			return
-		}
-		defer client.Rollback()
-		qrl, err = client.ExecuteBatch(queries, true)
-		want := "error: cannot start a new transaction in the scope of an existing one"
-		if err == nil || err.Error() != want {
-			t.Errorf("Error: %v, want %s", err, want)
-		}
-	}()
-}
-
 func TestBindInSelect(t *testing.T) {
 	client := framework.NewDefaultClient()
 
@@ -555,58 +357,6 @@ func TestBindInSelect(t *testing.T) {
 	}
 	if !reflect.DeepEqual(qr, want) {
 		t.Errorf("Execute: \n%#v, want \n%#v", qr, want)
-	}
-}
-
-var rulesJSON = []byte(`[{
-	"Name": "r1",
-	"Description": "disallow bindvar 'asdfg'",
-	"BindVarConds":[{
-		"Name": "asdfg",
-		"OnAbsent": false,
-		"Operator": "NOOP"
-	}]
-}]`)
-
-func TestQueryRules(t *testing.T) {
-	rules := tabletserver.NewQueryRules()
-	err := rules.UnmarshalJSON(rulesJSON)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	err = framework.DefaultServer.SetQueryRules("endtoend", rules)
-	want := "Rule source identifier endtoend is not valid"
-	if err == nil || err.Error() != want {
-		t.Errorf("Error: %v, want %s", err, want)
-	}
-
-	framework.DefaultServer.RegisterQueryRuleSource("endtoend")
-	defer framework.DefaultServer.UnRegisterQueryRuleSource("endtoend")
-	err = framework.DefaultServer.SetQueryRules("endtoend", rules)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	client := framework.NewDefaultClient()
-	query := "select * from vtocc_test where intval=:asdfg"
-	bv := map[string]interface{}{"asdfg": 1}
-	_, err = client.Execute(query, bv)
-	want = "error: Query disallowed due to rule: disallow bindvar 'asdfg'"
-	if err == nil || err.Error() != want {
-		t.Errorf("Error: %v, want %s", err, want)
-	}
-
-	err = framework.DefaultServer.SetQueryRules("endtoend", nil)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	_, err = client.Execute(query, bv)
-	if err != nil {
-		t.Error(err)
-		return
 	}
 }
 
