84d9ab36b6e4c13a8566190dd348cc4c80cde5fa
Michael Berlin <mberlin@google.com>
2015-10-06 20:14:17 -0700

    examples/kubernetes: Let sharded-vtworker.sh return an error when vtworker failed.

--- examples/kubernetes/sharded-vtworker.sh
+++ examples/kubernetes/sharded-vtworker.sh
@@ -32,5 +32,19 @@ done
 echo "Following vtworker logs until termination..."
 $KUBECTL logs -f vtworker
 
+# Get vtworker exit code. Wait for complete shutdown.
+# (Although logs -f exited, the pod isn't fully shutdown yet and the exit code is not available yet.)
+until [ $($KUBECTL get pod -o template -t '{{.status.phase}}' vtworker 2> /dev/null) != "Running" ]; do
+  echo "Waiting for vtworker pod to shutdown completely..."
+  sleep 1
+done
+exit_code=$($KUBECTL get -o template -t '{{(index .status.containerStatuses 0).state.terminated.exitCode}}' pods vtworker)
+
 echo "Deleting vtworker pod..."
 $KUBECTL delete pod vtworker
+
+if [ "$exit_code" != "0" ]; then
+  echo
+  echo "ERROR: vtworker did not run successfully (return value: $exit_code). Please check the error log above and try to run it again."
+  exit 1
+fi
