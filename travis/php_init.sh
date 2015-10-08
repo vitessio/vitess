e490edacbbeb4b2e8205e34de9974eec6f6264d9
Anthony Yeh <enisoc@google.com>
2015-10-08 01:04:33 -0700

    travis: Switch to PHP 5.5.9

--- travis/php_init.sh
+++ travis/php_init.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
+version=5.5.9
+
 set -e
 
-mkdir -p $HOME/php
-
-if [ -f $HOME/php/phpunit ]; then
-	echo "Using cached phpunit"
+if [ -f $HOME/.phpenv/bin/phpenv ]; then
+	echo "Using cached phpenv"
 else
-	curl -sL https://phar.phpunit.de/phpunit-4.8.9.phar > $HOME/php/phpunit
-	chmod +x $HOME/php/phpunit
+	curl -s https://raw.githubusercontent.com/CHH/phpenv/master/bin/phpenv-install.sh | bash
+	git clone git://github.com/michael-berlin/php-build.git $HOME/.phpenv/plugins/php-build
 fi
 
-if [ -f $HOME/php/mongo.so ]; then
+eval "$(phpenv init -)"
+
+if [ -d $HOME/.phpenv/versions/$version ]; then
+	echo "Using cached php"
+else
+	PHP_BUILD_CONFIGURE_OPTS="--without-xmlrpc --without-xsl --without-curl --disable-dom --without-gd --without-mcrypt --without-readline --disable-soap --without-tidy" phpenv install $version
+fi
+
+phpenv global $version
+phpenv rehash
+
+if [ -f $HOME/.phpenv/bin/phpunit ]; then
+	echo "Using cached phpunit"
+else
+	curl -sL https://phar.phpunit.de/phpunit-4.8.9.phar > $HOME/.phpenv/bin/phpunit
+	chmod +x $HOME/.phpenv/bin/phpunit
+fi
+
+if [ -f $HOME/.phpenv/bin/mongo.so ]; then
 	echo "Using cached mongo.so"
 else
 	mkdir -p $HOME/mongo
@@ -20,5 +38,6 @@ else
 	phpize
 	./configure
 	make
-	mv modules/mongo.so $HOME/php/
+	mv modules/mongo.so $HOME/.phpenv/bin/
+	echo "extension=$HOME/.phpenv/bin/mongo.so" > ~/.phpenv/versions/$version/etc/conf.d/mongo.ini
 fi
