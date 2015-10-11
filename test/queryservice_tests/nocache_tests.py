7403a556b8623d6ec45a94c49591563b7d76ff89
Sugu Sougoumarane <sougou@google.com>
2015-10-11 14:26:30 -0700

    RIP queryservice_test

--- test/queryservice_tests/nocache_tests.py
+++
@@ -1,9 +0,0 @@
-import framework
-import nocache_cases
-
-
-class TestNocache(framework.TestCase):
-  def test_sqls(self):
-    error_count = self.env.run_cases(nocache_cases.cases)
-    if error_count != 0:
-      self.fail("test_execution errors: %d"%(error_count))
