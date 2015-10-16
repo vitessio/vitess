c03a1a2eed86647be364b7d4e74ff8ccd8a6a1b9
Ammar Aijazi <aaijazi@google.com>
2015-10-15 17:39:26 -0700

    Delete old Java client

--- java/vtgate-client/src/main/java/com/youtube/vitess/vtgate/rpcclient/RpcClientFactory.java
+++
@@ -1,7 +0,0 @@
-package com.youtube.vitess.vtgate.rpcclient;
-
-import com.youtube.vitess.vtgate.Exceptions.ConnectionException;
-
-public interface RpcClientFactory {
-  RpcClient create(String address, int timeoutMs) throws ConnectionException;
-}
