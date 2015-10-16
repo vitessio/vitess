be83675c6a6cf42205391bab1d0a5ce5b5d430df
Michael Berlin <mberlin@google.com>
2015-10-15 18:18:01 -0700

    Make vtctl_client_protocol private again and use flag.Set() instead.

--- go/vt/vtctl/vtctlclient/interface.go
+++ go/vt/vtctl/vtctlclient/interface.go
@@ -15,8 +15,8 @@ import (
 	"golang.org/x/net/context"
 )
 
-// VtctlClientProtocol specifices which RPC client implementation should be used.
-var VtctlClientProtocol = flag.String("vtctl_client_protocol", "grpc", "the protocol to use to talk to the vtctl server")
+// vtctlClientProtocol specifices which RPC client implementation should be used.
+var vtctlClientProtocol = flag.String("vtctl_client_protocol", "grpc", "the protocol to use to talk to the vtctl server")
 
 // ErrFunc is returned by streaming queries to get the error
 type ErrFunc func() error
@@ -58,9 +58,9 @@ func UnregisterFactoryForTest(name string) {
 
 // New allows a user of the client library to get its implementation.
 func New(addr string, connectTimeout time.Duration) (VtctlClient, error) {
-	factory, ok := factories[*VtctlClientProtocol]
+	factory, ok := factories[*vtctlClientProtocol]
 	if !ok {
-		return nil, fmt.Errorf("unknown vtctl client protocol: %v", *VtctlClientProtocol)
+		return nil, fmt.Errorf("unknown vtctl client protocol: %v", *vtctlClientProtocol)
 	}
 	return factory(addr, connectTimeout)
 }
