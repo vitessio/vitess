be0b8cdcca0f6424edf7281903bb765dd9f7032e
Jeff (Zhefu) Jiang <jeffjiang@google.com>
2015-10-16 19:53:36 -0700

    EWMA implementation

---
+++ go/ewma/ewma.go
@@ -0,0 +1,63 @@
+// Copyright 2015, Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file
+
+// Package ewma implements exponentially weighted moving averages(EWMA).
+// See https://en.wikipedia.org/wiki/Moving_average#Exponential_moving_average
+// for formal definitions.
+package ewma
+
+import (
+	"math"
+
+	log "github.com/golang/glog"
+)
+
+const (
+	// The default smoothing factor α
+	defaultWeightingFactor = 0.8
+)
+
+type EWMA struct {
+	// The weighting factor α
+	weightingFactor float64
+	// The current value of the average
+	currAverage float64
+}
+
+// NewEWMA prepares a EWMA object which can calculate
+// EWMA of a series of data gradually added to it
+func NewEWMA(wf float64) *EWMA {
+	if wf < 0 || wf > 1.0 {
+		log.Infof(
+			"Invalid weighting factor: %v, using default value(%v) instead.",
+			wf,
+			defaultWeightingFactor,
+		)
+		wf = defaultWeightingFactor
+	}
+	return &EWMA{
+		weightingFactor: wf,
+		currAverage:     math.NaN(),
+	}
+}
+
+// AddValue adds a new data point into the EWMA calculation,
+// by which the EWMA is automatically updated
+func (e *EWMA) AddValue(value float64) {
+	// the first value added
+	if math.IsNaN(e.currAverage) {
+		e.currAverage = value
+	} else {
+		e.currAverage = e.weightingFactor*e.currAverage + (1.0-e.weightingFactor)*value
+	}
+}
+
+// GetAverage returns the EWMA calculated from historical data
+func (e *EWMA) GetEWMA() float64 {
+	if math.IsNaN(e.currAverage) {
+		// No value has been added yet, returning 0
+		return 0
+	}
+	return e.currAverage
+}
---
+++ go/ewma/ewma_test.go
@@ -0,0 +1,33 @@
+package ewma
+
+import (
+	"testing"
+)
+
+func TestEWMACalc(t *testing.T) {
+	input := []float64{200000.0, 210000.0, 190000.0, 201000.0}
+	// EWMA result of input when weighting factor = 0.8
+	output := 199880.0
+
+	// Test EWMA calculation with weighting factor = 0.8
+	e := NewEWMA(0.8)
+	if v := e.GetEWMA(); v != 0 {
+		t.Errorf("Expect zero EWMA when no input has been give, but got %v", v)
+	}
+	for _, in := range input {
+		e.AddValue(in)
+	}
+	if r := e.GetEWMA(); r != output {
+		t.Errorf("Expect EWMA result to be %v, but got %v", output, r)
+	}
+
+	// Test invalid weighting factor
+	e = NewEWMA(-0.1)
+	if e.weightingFactor != defaultWeightingFactor {
+		t.Errorf("Weighting factor should fall back to defult(%v), but got %v", defaultWeightingFactor, e.weightingFactor)
+	}
+	e = NewEWMA(1.01)
+	if e.weightingFactor != defaultWeightingFactor {
+		t.Errorf("Weighting factor should fall back to defult(%v), but got %v", defaultWeightingFactor, e.weightingFactor)
+	}
+}
