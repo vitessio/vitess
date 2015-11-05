ca109103dd45239e0950b3f85ad8e122edac6695
Dean Yasuda <dumbunny@google.com>
2015-11-04 19:47:34 -0800

    Make pylint pre-commit hook less strict.
    Ignore some google-specific warnings that don't apply.
    
    When displaying errors, print path name on each error line instead of as
    a header.
    
    Clean up a couple of python files modified while testing.

--- py/vtdb/topology.py
+++ py/vtdb/topology.py
@@ -1,3 +1,5 @@
+"""Deprecated module that holds keyspace / sharding methods."""
+
 # Copyright 2013, Google Inc. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can
 # be found in the LICENSE file.
@@ -9,4 +11,5 @@ from vtdb import keyrange_constants
 
 
 def get_sharding_col(keyspace_name):
+  _ = keyspace_name
   return 'keyspace_id', keyrange_constants.KIT_UINT64
