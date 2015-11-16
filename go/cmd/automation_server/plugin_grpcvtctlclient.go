ea7b031269215c0b9427e694260643a22c0a7181
Michael Berlin <mberlin@google.com>
2015-11-16 15:26:03 -0800

    automation: server: Enable gRPC plugin for vtctlclient.

---
+++ go/cmd/automation_server/plugin_grpcvtctlclient.go
@@ -0,0 +1,11 @@
+// Copyright 2015, Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+// Imports and register the gRPC vtctl client.
+
+import (
+	_ "github.com/youtube/vitess/go/vt/vtctl/grpcvtctlclient"
+)
