de3e59415b7ab326d4c59c3adf14a1aae8bd084d
Alain Jobart <alainjobart@google.com>
2015-12-09 14:10:14 -0800

    Further refactoring of vtctld to prepare the move.

--- go/cmd/vtctld/vtctld.go
+++ go/cmd/vtctld/vtctld.go
@@ -6,16 +6,12 @@ import (
 	"net/http"
 	"path"
 	"strings"
-	"time"
 
 	"golang.org/x/net/context"
 
 	log "github.com/golang/glog"
 	"github.com/youtube/vitess/go/acl"
-	"github.com/youtube/vitess/go/timer"
-	"github.com/youtube/vitess/go/vt/schemamanager"
 	"github.com/youtube/vitess/go/vt/servenv"
-	"github.com/youtube/vitess/go/vt/tabletmanager/tmclient"
 	"github.com/youtube/vitess/go/vt/topo"
 	"github.com/youtube/vitess/go/vt/wrangler"
 
@@ -23,11 +19,7 @@ import (
 )
 
 var (
-	webDir                    = flag.String("web_dir", "", "directory from which to serve vtctld web interface resources")
-	schemaChangeDir           = flag.String("schema_change_dir", "", "directory contains schema changes for all keyspaces. Each keyspace has its own directory and schema changes are expected to live in '$KEYSPACE/input' dir. e.g. test_keyspace/input/*sql, each sql file represents a schema change")
-	schemaChangeController    = flag.String("schema_change_controller", "", "schema change controller is responsible for finding schema changes and responsing schema change events")
-	schemaChangeCheckInterval = flag.Int("schema_change_check_interval", 60, "this value decides how often we check schema change dir, in seconds")
-	schemaChangeUser          = flag.String("schema_change_user", "", "The user who submits this schema change.")
+	webDir = flag.String("web_dir", "", "directory from which to serve vtctld web interface resources")
 
 	appPrefix = "/app/"
 )
@@ -155,40 +147,8 @@ func main() {
 	initExplorer()
 
 	// Start schema manager service.
-	if *schemaChangeDir != "" {
-		interval := 60
-		if *schemaChangeCheckInterval > 0 {
-			interval = *schemaChangeCheckInterval
-		}
-		timer := timer.NewTimer(time.Duration(interval) * time.Second)
-		controllerFactory, err :=
-			schemamanager.GetControllerFactory(*schemaChangeController)
-		if err != nil {
-			log.Fatalf("unable to get a controller factory, error: %v", err)
-		}
+	initSchema()
 
-		timer.Start(func() {
-			controller, err := controllerFactory(map[string]string{
-				schemamanager.SchemaChangeDirName: *schemaChangeDir,
-				schemamanager.SchemaChangeUser:    *schemaChangeUser,
-			})
-			if err != nil {
-				log.Errorf("failed to get controller, error: %v", err)
-				return
-			}
-			ctx := context.Background()
-			err = schemamanager.Run(
-				ctx,
-				controller,
-				schemamanager.NewTabletExecutor(
-					tmclient.NewTabletManagerClient(), ts),
-			)
-			if err != nil {
-				log.Errorf("Schema change failed, error: %v", err)
-			}
-		})
-		servenv.OnClose(func() { timer.Stop() })
-	}
-
+	// And run the server.
 	servenv.RunDefault()
 }
