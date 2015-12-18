0600fd9c4aa34d17f502a6b12b3a744c6231d05d
Michael Berlin <mberlin@google.com>
2015-12-17 20:25:34 -0800

    automation: Add task for vtctl RebuildKeyspaceGraph.
    Fix issues in MigrateServedTypes actuator/task ("cell" -> "cells",
    reordered parameters such that the vtctld address is the last one).
    
    automation unit tests: Check that all fake results were consumed.
    
    fake vtctl client: Added support for registering the same result for the
    same command multiple times. The reference count is decreased every time
    the result is streamed.
    
    NOTE: This is an automated export. Changes were already LGTM'd internally.

--- go/vt/automation/rebuild_keyspace_graph_task_test.go
+++ go/vt/automation/rebuild_keyspace_graph_task_test.go
@@ -12,28 +12,25 @@ import (
 	"github.com/youtube/vitess/go/vt/vtctl/vtctlclient"
 )
 
-func TestMigrateServedTypesTask(t *testing.T) {
+func TestRebuildKeyspaceGraphTask(t *testing.T) {
 	fake := fakevtctlclient.NewFakeVtctlClient()
 	vtctlclient.RegisterFactory("fake", fake.FakeVtctlClientFactory)
 	defer vtctlclient.UnregisterFactoryForTest("fake")
 	flag.Set("vtctl_client_protocol", "fake")
-	task := &MigrateServedTypesTask{}
+	task := &RebuildKeyspaceGraphTask{}
 
-	fake.RegisterResult([]string{"MigrateServedTypes", "test_keyspace/0", "rdonly"},
+	fake.RegisterResult([]string{"RebuildKeyspaceGraph", "test_keyspace"},
 		"",  // No output.
 		nil) // No error.
 	parameters := map[string]string{
 		"keyspace":        "test_keyspace",
-		"source_shard":    "0",
-		"type":            "rdonly",
 		"vtctld_endpoint": "localhost:15000",
 	}
-	testTask(t, "MigrateServedTypes", task, parameters, fake)
+	testTask(t, "RebuildKeyspaceGraph", task, parameters, fake)
 
-	fake.RegisterResult([]string{"MigrateServedTypes", "--cells=cell1", "--reverse=true", "test_keyspace/0", "rdonly"},
+	fake.RegisterResult([]string{"RebuildKeyspaceGraph", "--cells=cell1", "test_keyspace"},
 		"",  // No output.
 		nil) // No error.
 	parameters["cells"] = "cell1"
-	parameters["reverse"] = "true"
-	testTask(t, "MigrateServedTypes", task, parameters, fake)
+	testTask(t, "RebuildKeyspaceGraph", task, parameters, fake)
 }
--- go/vt/vtctl/fakevtctlclient/fake_loggerevent_streamingclient.go
+++ go/vt/vtctl/fakevtctlclient/fake_loggerevent_streamingclient.go
@@ -18,14 +18,14 @@ import (
 // FakeLoggerEventStreamingClient is the base for the fakes for the vtctlclient and vtworkerclient.
 // It allows to register a (multi-)line string for a given command and return the result as channel which streams it back.
 type FakeLoggerEventStreamingClient struct {
-	results map[string]result
+	results map[string]*result
 	// mu guards all fields of the structs.
 	mu sync.Mutex
 }
 
 // NewFakeLoggerEventStreamingClient creates a new fake.
 func NewFakeLoggerEventStreamingClient() *FakeLoggerEventStreamingClient {
-	return &FakeLoggerEventStreamingClient{results: make(map[string]result)}
+	return &FakeLoggerEventStreamingClient{results: make(map[string]*result)}
 }
 
 // generateKey returns a map key for a []string.
@@ -38,6 +38,15 @@ func generateKey(args []string) string {
 type result struct {
 	output string
 	err    error
+	// count is the number of times this result is registered for the same
+	// command. With each stream of this result, count will be decreased by one.
+	count int
+}
+
+func (r1 result) Equals(r2 result) bool {
+	return r1.output == r2.output &&
+		((r1.err == nil && r2.err == nil) ||
+			(r1.err != nil && r2.err != nil && r1.err.Error() == r2.err.Error()))
 }
 
 // RegisterResult registers for a given command (args) the result which the fake should return.
@@ -47,10 +56,15 @@ func (f *FakeLoggerEventStreamingClient) RegisterResult(args []string, output st
 	defer f.mu.Unlock()
 
 	k := generateKey(args)
+	v := result{output, err, 1}
 	if result, ok := f.results[k]; ok {
-		return fmt.Errorf("Result is already registered as: %v", result)
+		if result.Equals(v) {
+			result.count++
+			return nil
+		}
+		return fmt.Errorf("A different result (%v) is already registered for command: %v", result, args)
 	}
-	f.results[k] = result{output, err}
+	f.results[k] = &v
 	return nil
 }
 
@@ -77,7 +91,10 @@ func (f *FakeLoggerEventStreamingClient) StreamResult(args []string) (<-chan *lo
 	if !ok {
 		return nil, nil, fmt.Errorf("No response was registered for args: %v", args)
 	}
-	delete(f.results, k)
+	result.count--
+	if result.count == 0 {
+		delete(f.results, k)
+	}
 
 	stream := make(chan *logutilpb.Event)
 	go func() {
