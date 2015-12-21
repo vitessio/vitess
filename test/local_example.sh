4ef928840042062fbe94357aecd0f2a0ecaf624c
Michael Berlin <mberlin@google.com>
2015-12-21 05:26:03 -0800

    test/local_example.sh: During mvn install, skip running the tests but not compiling them.
    Hopefully, this fixes the travis error that the artifact "com.youtube.vitess:client:jar:tests:1.0-SNAPSHOT" cannot be found.

--- test/local_example.sh
+++ test/local_example.sh
@@ -72,7 +72,7 @@ echo "Run Java client script..."
 # We have to install the "example" module first because Maven cannot resolve
 # them when we run "exec:java". See also: http://stackoverflow.com/questions/11091311/maven-execjava-goal-on-a-multi-module-project
 # Install only "example". See also: http://stackoverflow.com/questions/1114026/maven-modules-building-a-single-specific-module
-mvn -f ../../java/pom.xml -pl example -am install -Dmaven.test.skip=true
+mvn -f ../../java/pom.xml -pl example -am install -DskipTests
 mvn -f ../../java/example/pom.xml exec:java -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="com.youtube.vitess.example.VitessClientExample" -Dexec.args="localhost:15991" || teardown
 
 exitcode=0
