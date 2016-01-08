c0ad4584bbb6c00768560b6c8605ebe775f995f1
Michael Berlin <mberlin@google.com>
2016-01-08 13:49:54 +0100

    java/example: Use try-resource blocks instead of explicit close() statements.

--- java/example/src/main/java/com/youtube/vitess/example/VitessClientExample.java
+++ java/example/src/main/java/com/youtube/vitess/example/VitessClientExample.java
@@ -1,8 +1,13 @@
 package com.youtube.vitess.example;
 
+import java.net.InetSocketAddress;
+import java.util.Arrays;
+import java.util.Map;
+
+import org.joda.time.Duration;
+
 import com.google.common.collect.ImmutableMap;
 import com.google.common.net.HostAndPort;
-
 import com.youtube.vitess.client.Context;
 import com.youtube.vitess.client.RpcClient;
 import com.youtube.vitess.client.VTGateConn;
@@ -11,12 +16,6 @@ import com.youtube.vitess.client.cursor.Cursor;
 import com.youtube.vitess.client.cursor.Row;
 import com.youtube.vitess.client.grpc.GrpcClientFactory;
 import com.youtube.vitess.proto.Topodata.TabletType;
-
-import org.joda.time.Duration;
-
-import java.net.InetSocketAddress;
-import java.util.Arrays;
-import java.util.Map;
 
 /**
  * VitessClientExample.java is a sample for using the Vitess Java Client with an unsharded keyspace.
@@ -40,38 +39,35 @@ public class VitessClientExample {
 
     // Connect to vtgate.
     HostAndPort hostAndPort = HostAndPort.fromString(args[0]);
+    InetSocketAddress addr =
+        new InetSocketAddress(hostAndPort.getHostText(), hostAndPort.getPort());
     Context ctx = Context.getDefault().withDeadlineAfter(Duration.millis(5 * 1000));
-    RpcClient client =
-        new GrpcClientFactory().create(ctx, new InetSocketAddress(hostAndPort.getHostText(),
-            hostAndPort.getPort()));
-    VTGateConn conn = new VTGateConn(client);
+    try (RpcClient client = new GrpcClientFactory().create(ctx, addr);
+        VTGateConn conn = new VTGateConn(client);) {
+      String keyspace = "test_keyspace";
+      Iterable<String> shards = Arrays.asList("0");
+      Map<String, Object> bindVars =
+          new ImmutableMap.Builder<String, Object>().put("msg", "V is for speed").build();
 
-    String keyspace = "test_keyspace";
-    Iterable<String> shards = Arrays.asList("0");
-    Map<String, Object> bindVars =
-        new ImmutableMap.Builder<String, Object>().put("msg", "V is for speed").build();
+      // Insert something.
+      System.out.println("Inserting into master...");
+      VTGateTx tx = conn.begin(ctx);
+      tx.executeShards(ctx, "INSERT INTO test_table (msg) VALUES (:msg)", keyspace, shards,
+          bindVars, TabletType.MASTER, false /* notInTransaction (ignore this because we will soon remove it) */);
+      tx.commit(ctx);
 
-    // Insert something.
-    System.out.println("Inserting into master...");
-    VTGateTx tx = conn.begin(ctx);
-    tx.executeShards(ctx, "INSERT INTO test_table (msg) VALUES (:msg)", keyspace, shards, bindVars,
-        TabletType.MASTER, false /* notInTransaction (ignore this because we will soon remove it) */);
-    tx.commit(ctx);
-
-    // Read it back from the master.
-    System.out.println("Reading from master...");
-    Cursor cursor =
-        conn.executeShards(ctx, "SELECT id, msg FROM test_table", keyspace, shards,
-            null /* bindVars */, TabletType.MASTER);
-    Row row;
-    while ((row = cursor.next()) != null) {
-      long id = row.getLong("id");
-      byte[] msg = row.getBytes("msg");
-      System.out.format("(%d, %s)\n", id, new String(msg));
+      // Read it back from the master.
+      System.out.println("Reading from master...");
+      try (Cursor cursor =
+          conn.executeShards(ctx, "SELECT id, msg FROM test_table", keyspace, shards,
+              null /* bindVars */, TabletType.MASTER);) {
+        Row row;
+        while ((row = cursor.next()) != null) {
+          long id = row.getLong("id");
+          byte[] msg = row.getBytes("msg");
+          System.out.format("(%d, %s)\n", id, new String(msg));
+        }
+      }
     }
-    cursor.close();
-
-    conn.close();
-    client.close();
   }
 }
