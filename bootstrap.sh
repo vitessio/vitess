a3ca40bdba623a0351045a7aa91ef6242f8d461a
Michael Berlin <mberlin@google.com>
2016-01-13 21:24:17 -0800

    bootstrap: Fix pkg-config work-around for MariaDB 10.1.
    It looks like with 10.1 MariaDB libmysqlclient is reentrant by default and they got rid of the special library "libmysqlclient_r". Therefore, mysql_config --libs_r now returns "libmysqlclient" instead of "libmysqlclient_r". Replace any of these two values with the static library.
    
    Fixes issue https://github.com/youtube/vitess/issues/1437

--- bootstrap.sh
+++ bootstrap.sh
@@ -160,7 +160,7 @@ echo "Cflags:" "$($VT_MYSQL_ROOT/bin/mysql_config --cflags) -ggdb -fPIC" >> $VTR
 if [ "$MYSQL_FLAVOR" == "MariaDB" ]; then
   # Use static linking because the shared library doesn't export
   # some internal functions we use, like cli_safe_read.
-  echo "Libs:" "$($VT_MYSQL_ROOT/bin/mysql_config --libs_r | sed 's,-lmysqlclient_r,-l:libmysqlclient.a -lstdc++,')" >> $VTROOT/lib/gomysql.pc
+  echo "Libs:" "$($VT_MYSQL_ROOT/bin/mysql_config --libs_r | sed -r 's,-lmysqlclient(_r)?,-l:libmysqlclient.a -lstdc++,')" >> $VTROOT/lib/gomysql.pc
 else
   echo "Libs:" "$($VT_MYSQL_ROOT/bin/mysql_config --libs_r)" >> $VTROOT/lib/gomysql.pc
 fi
