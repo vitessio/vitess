0b9139d8bd663e0e04b6e44b47a607ea71b78732
Rasta <rasta@lj.sk>
2016-01-23 09:43:37 +0100

    examples/local - vttablet-up.sh - use hostname -i only in travis build (fix2)

--- examples/local/vttablet-up.sh
+++ examples/local/vttablet-up.sh
@@ -86,7 +86,7 @@ for uid_index in $uids; do
   $VTROOT/bin/vttablet \
     -log_dir $VTDATAROOT/tmp \
     -tablet-path $alias \
-    -tablet_hostname $hostname
+    -tablet_hostname $hostname \
     -init_keyspace $keyspace \
     -init_shard $shard \
     -target_tablet_type $tablet_type \
