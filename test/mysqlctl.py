a8cad681913c2b06f80fcd796b008c0a83de7c73
Anthony Yeh <enisoc@google.com>
2016-02-17 19:58:51 -0800

    Fix some pylint issues to try to trigger Travis.

--- test/mysqlctl.py
+++ test/mysqlctl.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 
 import warnings
-# Dropping a table inexplicably produces a warning despite
-# the "IF EXISTS" clause. Squelch these warnings.
-warnings.simplefilter('ignore')
-
 import unittest
-
 import environment
 import tablet
 import utils
+
+# Dropping a table inexplicably produces a warning despite
+# the "IF EXISTS" clause. Squelch these warnings.
+warnings.simplefilter('ignore')
 
 master_tablet = tablet.Tablet()
 replica_tablet = tablet.Tablet()
