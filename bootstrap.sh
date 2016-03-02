vitessio/vitess

commit 938f31743ddef04cc932f40ed9133aca73149705

Update to gRPC 0.13.

bootstrap.sh
@@ -65,7 +65,7 @@ fi
 # install gRPC C++ base, so we can install the python adapters.
 # this also installs protobufs
 grpc_dist=$VTROOT/dist/grpc
-grpc_ver=release-0_12_0
+grpc_ver=release-0_13_0
 if [ $SKIP_ROOT_INSTALLS == "True" ]; then
   echo "skipping grpc build, as root version was already installed."
 elif [[ -f $grpc_dist/.build_finished && "$(cat $grpc_dist/.build_finished)" == "$grpc_ver" ]]; then

travis/install_grpc.sh
@@ -7,24 +7,24 @@ set -ex
 
 # grpc_dist can be empty, in which case we just install to the default paths
 grpc_dist="$1"
-if [ "$grpc_dist" != "" ]; then
+if [ -n "$grpc_dist" ]; then
   cd $grpc_dist
 fi
 
 # for python, we'll need the latest virtualenv and tox.
-# running gRPC requires the six package, any version will do.
-if [ "$grpc_dist" != "" ]; then
-  pip install --upgrade --root $grpc_dist --ignore-installed virtualenv tox
-  pip install --root $grpc_dist --ignore-installed six
+# running gRPC requires the six package, version >=1.10.
+if [ -n "$grpc_dist" ]; then
+  # Create a virtualenv, which also creates a virualenv-boxed pip.
+  virtualenv $grpc_dist/usr/local
+  $grpc_dist/usr/local/bin/pip install --upgrade --ignore-installed virtualenv tox six
 else
-  pip install --upgrade virtualenv tox
-  pip install six
+  pip install --upgrade --ignore-installed virtualenv tox six
 fi
 
 # clone the repository, setup the submodules
 git clone https://github.com/grpc/grpc.git
 cd grpc
-git checkout release-0_12_0
+git checkout release-0_13_0
 git submodule update --init
 
 # on OSX beta-1 doesn't work, it has to be built in version beta-2
@@ -42,7 +42,7 @@ make
 
 # install protobuf side (it was already built by the 'make' earlier)
 cd third_party/protobuf
-if [ "$grpc_dist" != "" ]; then
+if [ -n "$grpc_dist" ]; then
   make install prefix=$grpc_dist/usr/local
 else
   make install
@@ -50,9 +50,9 @@ fi
 
 # build and install python protobuf side
 cd python
-if [ "$grpc_dist" != "" ]; then
+if [ -n "$grpc_dist" ]; then
   python setup.py build --cpp_implementation
-  python setup.py install --cpp_implementation --root=$grpc_dist
+  python setup.py install --cpp_implementation --prefix=$grpc_dist/usr/local
 else
   python setup.py build --cpp_implementation
   python setup.py install --cpp_implementation
@@ -60,21 +60,17 @@ fi
 
 # now install grpc itself
 cd ../../..
-if [ "$grpc_dist" != "" ]; then
+if [ -n "$grpc_dist" ]; then
   make install prefix=$grpc_dist/usr/local
 else
   make install
 fi
 
 # and now build and install gRPC python libraries
-# Note: running this twice as the first run exists
-# with 'build_data' not found error. Seems the python
-# libraries still work though.
-CONFIG=opt ./tools/run_tests/build_python.sh || CONFIG=opt ./tools/run_tests/build_python.sh
-if [ "$grpc_dist" != "" ]; then
-  CFLAGS=-I$grpc_dist/include LDFLAGS=-L$grpc_dist/lib pip install src/python/grpcio --root $grpc_dist
+if [ -n "$grpc_dist" ]; then
+  $grpc_dist/usr/local/bin/pip install .
 else
-  pip install src/python/grpcio
+  pip install .
 fi
 
 # Build PHP extension, only in Travis.

.travis.yml
@@ -22,6 +22,7 @@ addons:
     - gcc-4.8
     - git
     - pkg-config
+    - python-virtualenv
     # installs libaio1 which is required by MariaDB 10.0 server package
     - libaio-dev
     # required by travis script below to measure CPU and memory usage

examples/kubernetes/guestbook/extract.sh
@@ -9,4 +9,4 @@ cp -R /usr/local/lib/python2.7/dist-packages /out/pkg/
 cp -R /vt/dist/py-* /out/pkg/
 
 # We also need the grpc libraries.
-cp /usr/local/lib/libgrpc.so.0 /usr/local/lib/libgpr.so.0 /out/lib/
+cp /usr/local/lib/libgrpc.so.0 /out/lib/

go/vt/proto/automation/automation.pb.go
@@ -91,7 +91,7 @@ func (TaskState) EnumDescriptor() ([]byte, []int) { return fileDescriptor0, []in
 type ClusterOperation struct {
 	Id string `protobuf:"bytes,1,opt,name=id" json:"id,omitempty"`
 	// TaskContainer are processed sequentially, one at a time.
-	SerialTasks []*TaskContainer `protobuf:"bytes,2,rep,name=serial_tasks" json:"serial_tasks,omitempty"`
+	SerialTasks []*TaskContainer `protobuf:"bytes,2,rep,name=serial_tasks,json=serialTasks" json:"serial_tasks,omitempty"`
 	// Cached value. This has to be re-evaluated e.g. after a checkpoint load because running tasks may have already finished.
 	State ClusterOperationState `protobuf:"varint,3,opt,name=state,enum=automation.ClusterOperationState" json:"state,omitempty"`
 	// Error of the first task which failed. Set after state advanced to CLUSTER_OPERATION_DONE. If empty, all tasks succeeded. Cached value, see state above.
@@ -113,7 +113,7 @@ func (m *ClusterOperation) GetSerialTasks() []*TaskContainer {
 // TaskContainer holds one or more task which may be executed in parallel.
 // "concurrency", if > 0, limits the amount of concurrently executed tasks.
 type TaskContainer struct {
-	ParallelTasks []*Task `protobuf:"bytes,1,rep,name=parallel_tasks" json:"parallel_tasks,omitempty"`
+	ParallelTasks []*Task `protobuf:"bytes,1,rep,name=parallel_tasks,json=parallelTasks" json:"parallel_tasks,omitempty"`
 	Concurrency   int32   `protobuf:"varint,2,opt,name=concurrency" json:"concurrency,omitempty"`
 }
 
@@ -214,7 +214,7 @@ func (*GetClusterOperationDetailsRequest) Descriptor() ([]byte, []int) {
 
 type GetClusterOperationDetailsResponse struct {
 	// Full snapshot of the execution e.g. including output of each task.
-	ClusterOp *ClusterOperation `protobuf:"bytes,2,opt,name=cluster_op" json:"cluster_op,omitempty"`
+	ClusterOp *ClusterOperation `protobuf:"bytes,2,opt,name=cluster_op,json=clusterOp" json:"cluster_op,omitempty"`
 }
 
 func (m *GetClusterOperationDetailsResponse) Reset()         { *m = GetClusterOperationDetailsResponse{} }
@@ -246,37 +246,40 @@ func init() {
 }
 
 var fileDescriptor0 = []byte{
-	// 511 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0x9c, 0x94, 0xc1, 0x6e, 0xd3, 0x40,
-	0x10, 0x86, 0xd9, 0xc4, 0x09, 0x64, 0xdc, 0xa6, 0xd6, 0xa2, 0x56, 0x6e, 0x45, 0x69, 0x62, 0x38,
-	0x44, 0x95, 0x48, 0x21, 0x70, 0x40, 0xbd, 0x45, 0x89, 0x55, 0x55, 0x41, 0xeb, 0xca, 0x71, 0xe0,
-	0x68, 0x2d, 0x61, 0x0f, 0x51, 0x1d, 0xaf, 0x59, 0xaf, 0x91, 0xf2, 0x16, 0x9c, 0x79, 0x0b, 0x5e,
-	0x84, 0x67, 0xc2, 0x76, 0x62, 0xc7, 0x75, 0xdc, 0x08, 0x71, 0xcc, 0xce, 0x3f, 0xff, 0x7c, 0xff,
-	0x68, 0x62, 0xd0, 0x68, 0x24, 0xf9, 0x92, 0xca, 0x05, 0xf7, 0xfb, 0x81, 0xe0, 0x92, 0x63, 0xd8,
-	0xbe, 0x18, 0x3f, 0x11, 0x68, 0x23, 0x2f, 0x0a, 0x25, 0x13, 0x56, 0xc0, 0x44, 0xfa, 0x88, 0x01,
-	0x6a, 0x8b, 0x6f, 0x3a, 0xea, 0xa0, 0x5e, 0x0b, 0x5f, 0xc1, 0x41, 0xc8, 0xc4, 0x82, 0x7a, 0xae,
-	0xa4, 0xe1, 0x7d, 0xa8, 0xd7, 0x3a, 0xf5, 0x9e, 0x3a, 0x38, 0xed, 0x17, 0x5c, 0x9d, 0xb8, 0x30,
-	0xe2, 0xbe, 0xa4, 0x0b, 0x9f, 0x09, 0xfc, 0x16, 0x1a, 0xa1, 0xa4, 0x92, 0xe9, 0xf5, 0xb8, 0xbf,
-	0x3d, 0xe8, 0x16, 0x95, 0xe5, 0x49, 0xd3, 0x44, 0x88, 0x0f, 0xa1, 0xc1, 0x84, 0xe0, 0x42, 0x57,
-	0x92, 0x89, 0x06, 0x81, 0xc3, 0x87, 0x8e, 0x3d, 0x68, 0x07, 0x54, 0x50, 0xcf, 0x63, 0x19, 0x04,
-	0x4a, 0x21, 0xb4, 0x32, 0x04, 0x7e, 0x0e, 0xea, 0x9c, 0xfb, 0xf3, 0x48, 0x08, 0xe6, 0xcf, 0x57,
-	0x31, 0x2b, 0xea, 0x35, 0x8c, 0x3f, 0x08, 0x94, 0xb4, 0x7a, 0x00, 0x8a, 0x4f, 0x97, 0x6c, 0x13,
-	0xec, 0x03, 0x40, 0xe2, 0xba, 0x64, 0x31, 0x50, 0x16, 0xab, 0x53, 0x76, 0xec, 0xdf, 0xe5, 0x12,
-	0xd3, 0x97, 0x62, 0xb5, 0x59, 0x4d, 0x3d, 0x75, 0x78, 0x9d, 0x25, 0x55, 0xd2, 0xa4, 0xc7, 0xe5,
-	0xe6, 0x75, 0xba, 0x36, 0x34, 0x79, 0x24, 0x83, 0x48, 0xea, 0x8d, 0xb4, 0x2b, 0x4f, 0xdb, 0x4c,
-	0x7e, 0x9e, 0xbd, 0x83, 0xa3, 0xf2, 0x0c, 0x15, 0xea, 0xf7, 0x6c, 0xb5, 0xc1, 0x8c, 0xe5, 0x3f,
-	0xa8, 0x17, 0xb1, 0x34, 0x4c, 0xeb, 0xba, 0xf6, 0x11, 0x19, 0xbf, 0x11, 0xbc, 0x34, 0xfd, 0xef,
-	0x11, 0x8b, 0x58, 0x79, 0xa1, 0x36, 0x8b, 0x9f, 0x43, 0x59, 0x8a, 0x4a, 0x2a, 0xa2, 0x5e, 0x17,
-	0x69, 0xf7, 0xbb, 0x95, 0x97, 0xf0, 0x3f, 0xcc, 0x6f, 0xe0, 0xe2, 0xd1, 0x21, 0x61, 0xc0, 0xfd,
-	0x90, 0x15, 0xaf, 0x2e, 0x91, 0xdf, 0x30, 0x59, 0x79, 0x2e, 0x59, 0xc4, 0xa2, 0xdc, 0x81, 0xce,
-	0xe3, 0xf2, 0x8d, 0x7d, 0x7e, 0x97, 0xe8, 0x1f, 0xef, 0xd2, 0xb8, 0x82, 0x6e, 0x85, 0xeb, 0x98,
-	0xc5, 0x67, 0xe9, 0x85, 0x55, 0x18, 0x9f, 0xc1, 0xd8, 0xd7, 0x90, 0x83, 0xc0, 0x7c, 0x2d, 0x71,
-	0x79, 0x90, 0xae, 0x48, 0x1d, 0xbc, 0xd8, 0x47, 0x73, 0xf9, 0x0b, 0xc1, 0x71, 0xf5, 0x5f, 0xe7,
-	0x15, 0x5c, 0xcc, 0xc8, 0x84, 0x58, 0x5f, 0x88, 0x3b, 0xfa, 0x34, 0x9b, 0x3a, 0xa6, 0xed, 0x5a,
-	0x77, 0xa6, 0x3d, 0x74, 0x6e, 0x2d, 0xe2, 0x4e, 0x9d, 0xa1, 0x63, 0x6a, 0x4f, 0x70, 0x17, 0xce,
-	0x77, 0x8b, 0xc4, 0x72, 0x12, 0x81, 0xed, 0x98, 0x63, 0x0d, 0xe1, 0x73, 0x38, 0xdd, 0x95, 0xd8,
-	0x33, 0x42, 0x6e, 0xc9, 0x8d, 0x56, 0xc3, 0x67, 0x70, 0xb2, 0x5b, 0x1e, 0x5b, 0xc4, 0xd4, 0xea,
-	0x97, 0x13, 0x68, 0x6d, 0x8f, 0xfd, 0x04, 0x70, 0xc6, 0xe3, 0x0c, 0xa7, 0x93, 0x1c, 0xe1, 0x08,
-	0xd4, 0x87, 0x03, 0x55, 0x78, 0xba, 0xb5, 0x7f, 0x06, 0xca, 0xda, 0xec, 0x6b, 0x33, 0xfd, 0x42,
-	0xbd, 0xff, 0x1b, 0x00, 0x00, 0xff, 0xff, 0x81, 0x18, 0xbf, 0x1a, 0xb5, 0x04, 0x00, 0x00,
+	// 559 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xa4, 0x54, 0xdd, 0x6e, 0xd3, 0x30,
+	0x14, 0x26, 0xfd, 0x19, 0xf4, 0x84, 0x75, 0x91, 0xc5, 0xa6, 0x6e, 0x62, 0xac, 0x0d, 0x37, 0xd5,
+	0x90, 0x2a, 0xd1, 0x5d, 0x80, 0x06, 0x48, 0x54, 0x6d, 0x34, 0x4d, 0x45, 0xc9, 0xe4, 0xa6, 0x42,
+	0x82, 0x8b, 0xca, 0x14, 0x5f, 0x94, 0xa5, 0x49, 0xb0, 0x1d, 0xa4, 0xbe, 0x00, 0x0f, 0xc1, 0x4b,
+	0xf0, 0x28, 0xbc, 0x12, 0x8e, 0x93, 0xb4, 0x69, 0xfa, 0x23, 0x21, 0xee, 0xec, 0xe3, 0xef, 0xfb,
+	0xce, 0x77, 0xbe, 0xf6, 0x04, 0x0c, 0x12, 0x89, 0x60, 0x4e, 0xc4, 0x2c, 0xf0, 0x3b, 0x21, 0x0b,
+	0x44, 0x80, 0x60, 0x55, 0x31, 0x7f, 0x6b, 0x60, 0xf4, 0xbd, 0x88, 0x0b, 0xca, 0x9c, 0x90, 0x32,
+	0x55, 0x44, 0x75, 0x28, 0xcd, 0xbe, 0x36, 0xb4, 0xa6, 0xd6, 0xae, 0x61, 0x79, 0x42, 0x6f, 0xe1,
+	0x31, 0xa7, 0x6c, 0x46, 0xbc, 0x89, 0x20, 0xfc, 0x9e, 0x37, 0x4a, 0xcd, 0x72, 0x5b, 0xef, 0x9e,
+	0x76, 0x72, 0xca, 0xae, 0x7c, 0xe8, 0x07, 0xbe, 0x20, 0x33, 0x9f, 0x32, 0xac, 0x27, 0xf0, 0xb8,
+	0xc8, 0xd1, 0x2b, 0xa8, 0x72, 0x41, 0x04, 0x6d, 0x94, 0xa5, 0x60, 0xbd, 0xdb, 0xca, 0xd3, 0x8a,
+	0xad, 0x47, 0x31, 0x10, 0x27, 0x78, 0xf4, 0x04, 0xaa, 0x94, 0xb1, 0x80, 0x35, 0x2a, 0xca, 0x49,
+	0x72, 0x31, 0xbf, 0xc1, 0xe1, 0x5a, 0x33, 0xa9, 0x5f, 0x0f, 0x09, 0x23, 0x9e, 0x47, 0x33, 0x7f,
+	0x9a, 0xf2, 0x67, 0x14, 0xfd, 0xe1, 0xc3, 0x0c, 0x97, 0x18, 0x6b, 0x82, 0x3e, 0x0d, 0xfc, 0x69,
+	0xc4, 0x18, 0xf5, 0xa7, 0x0b, 0x39, 0x95, 0xd6, 0xae, 0xe2, 0x7c, 0xc9, 0xfc, 0x59, 0x82, 0x4a,
+	0x8c, 0x45, 0x08, 0x2a, 0x3e, 0x99, 0xd3, 0x34, 0x13, 0x75, 0x46, 0xef, 0x01, 0x62, 0xbd, 0x39,
+	0x95, 0x03, 0x64, 0x99, 0x34, 0x8b, 0x3d, 0x3b, 0x77, 0x4b, 0x88, 0xe5, 0x0b, 0xb6, 0xc0, 0x39,
+	0x4e, 0x9a, 0x73, 0x79, 0x99, 0xf3, 0x8b, 0x2c, 0xa9, 0x8a, 0x4a, 0xea, 0xb8, 0x28, 0xb6, 0x96,
+	0xce, 0x09, 0x1c, 0x04, 0x91, 0x08, 0x23, 0xd1, 0xa8, 0x2a, 0x81, 0xf4, 0xb6, 0x4a, 0xed, 0x20,
+	0x97, 0xda, 0xd9, 0x3b, 0x38, 0x2a, 0x38, 0x41, 0x06, 0x94, 0xef, 0xe9, 0x22, 0x1d, 0x29, 0x3e,
+	0xc6, 0xd4, 0x1f, 0xc4, 0x8b, 0xa8, 0x8a, 0x42, 0x52, 0xd5, 0xe5, 0xba, 0xf4, 0x5a, 0x33, 0xff,
+	0x68, 0xf0, 0xcc, 0xf2, 0xbf, 0x47, 0x34, 0xa2, 0xc5, 0x9f, 0x0c, 0x53, 0x59, 0xe6, 0x62, 0x6b,
+	0x44, 0x9f, 0xb6, 0x44, 0x74, 0x9d, 0x9f, 0x6a, 0xbf, 0xe6, 0xbe, 0xf0, 0xfe, 0x77, 0xa2, 0x97,
+	0x70, 0xb1, 0xb3, 0x39, 0x0f, 0x03, 0x9f, 0xd3, 0xe2, 0x1a, 0xc4, 0x94, 0x1b, 0x2a, 0xb6, 0xff,
+	0x65, 0xd3, 0x10, 0x8a, 0x94, 0xcf, 0xd0, 0xdc, 0x4d, 0x49, 0xdb, 0x2c, 0xf7, 0x43, 0xfb, 0xb7,
+	0xfd, 0x30, 0xaf, 0xa0, 0xb5, 0x45, 0x7c, 0x40, 0xe5, 0x5e, 0x78, 0x7c, 0x97, 0x23, 0x02, 0xe6,
+	0x3e, 0x52, 0xea, 0xe9, 0x0d, 0xc0, 0x34, 0x81, 0x4c, 0x82, 0x50, 0x85, 0xa7, 0x77, 0x9f, 0xee,
+	0x33, 0x86, 0x6b, 0xd3, 0xac, 0x72, 0xf9, 0x4b, 0x83, 0xe3, 0xad, 0xc6, 0xd1, 0x73, 0xb8, 0x18,
+	0xdb, 0x43, 0xdb, 0xf9, 0x68, 0x4f, 0xfa, 0x1f, 0xc6, 0x23, 0xd7, 0xc2, 0x13, 0xe7, 0xce, 0xc2,
+	0x3d, 0xf7, 0xd6, 0xb1, 0x27, 0x23, 0xb7, 0xe7, 0x5a, 0xc6, 0x03, 0xd4, 0x82, 0xf3, 0xcd, 0x47,
+	0xdb, 0x71, 0x63, 0x00, 0x76, 0xad, 0x81, 0xa1, 0xa1, 0x73, 0x38, 0xdd, 0x84, 0xe0, 0xb1, 0x6d,
+	0xdf, 0xda, 0x37, 0x46, 0x09, 0x9d, 0xc1, 0xc9, 0xe6, 0xf3, 0xc0, 0xb1, 0x2d, 0xa3, 0x7c, 0x39,
+	0x84, 0xda, 0x72, 0x95, 0xe4, 0x0e, 0xa1, 0xcc, 0x8f, 0xdb, 0x1b, 0x0d, 0x97, 0x16, 0x8e, 0x40,
+	0x5f, 0x6f, 0xa8, 0xc3, 0xc3, 0x95, 0xfc, 0x23, 0xa8, 0x24, 0x62, 0x5f, 0x0e, 0xd4, 0x07, 0xf5,
+	0xea, 0x6f, 0x00, 0x00, 0x00, 0xff, 0xff, 0x8f, 0x42, 0xc7, 0x06, 0x64, 0x05, 0x00, 0x00,
 }

go/vt/proto/binlogdata/binlogdata.pb.go
@@ -124,7 +124,7 @@ type BinlogTransaction struct {
 	// the timestamp of the statements
 	Timestamp int64 `protobuf:"varint,2,opt,name=timestamp" json:"timestamp,omitempty"`
 	// the Transaction ID after this statement was applied
-	TransactionId string `protobuf:"bytes,3,opt,name=transaction_id" json:"transaction_id,omitempty"`
+	TransactionId string `protobuf:"bytes,3,opt,name=transaction_id,json=transactionId" json:"transaction_id,omitempty"`
 }
 
 func (m *BinlogTransaction) Reset()                    { *m = BinlogTransaction{} }
@@ -164,15 +164,15 @@ func (m *BinlogTransaction_Statement) GetCharset() *Charset {
 type StreamEvent struct {
 	Category StreamEvent_Category `protobuf:"varint,1,opt,name=category,enum=binlogdata.StreamEvent_Category" json:"category,omitempty"`
 	// table_name, primary_key_fields and primary_key_values are set for SE_DML
-	TableName        string         `protobuf:"bytes,2,opt,name=table_name" json:"table_name,omitempty"`
-	PrimaryKeyFields []*query.Field `protobuf:"bytes,3,rep,name=primary_key_fields" json:"primary_key_fields,omitempty"`
-	PrimaryKeyValues []*query.Row   `protobuf:"bytes,4,rep,name=primary_key_values" json:"primary_key_values,omitempty"`
+	TableName        string         `protobuf:"bytes,2,opt,name=table_name,json=tableName" json:"table_name,omitempty"`
+	PrimaryKeyFields []*query.Field `protobuf:"bytes,3,rep,name=primary_key_fields,json=primaryKeyFields" json:"primary_key_fields,omitempty"`
+	PrimaryKeyValues []*query.Row   `protobuf:"bytes,4,rep,name=primary_key_values,json=primaryKeyValues" json:"primary_key_values,omitempty"`
 	// sql is set for SE_DDL or SE_ERR
 	Sql string `protobuf:"bytes,5,opt,name=sql" json:"sql,omitempty"`
 	// timestamp is set for SE_DML, SE_DDL or SE_ERR
 	Timestamp int64 `protobuf:"varint,6,opt,name=timestamp" json:"timestamp,omitempty"`
 	// the Transaction ID after this statement was applied
-	TransactionId string `protobuf:"bytes,7,opt,name=transaction_id" json:"transaction_id,omitempty"`
+	TransactionId string `protobuf:"bytes,7,opt,name=transaction_id,json=transactionId" json:"transaction_id,omitempty"`
 }
 
 func (m *StreamEvent) Reset()                    { *m = StreamEvent{} }
@@ -207,7 +207,7 @@ func (*StreamUpdateRequest) Descriptor() ([]byte, []int) { return fileDescriptor
 
 // StreamUpdateResponse is the response from StreamUpdate
 type StreamUpdateResponse struct {
-	StreamEvent *StreamEvent `protobuf:"bytes,1,opt,name=stream_event" json:"stream_event,omitempty"`
+	StreamEvent *StreamEvent `protobuf:"bytes,1,opt,name=stream_event,json=streamEvent" json:"stream_event,omitempty"`
 }
 
 func (m *StreamUpdateResponse) Reset()                    { *m = StreamUpdateResponse{} }
@@ -227,7 +227,7 @@ type StreamKeyRangeRequest struct {
 	// where to start
 	Position string `protobuf:"bytes,1,opt,name=position" json:"position,omitempty"`
 	// what to get
-	KeyRange *topodata.KeyRange `protobuf:"bytes,2,opt,name=key_range" json:"key_range,omitempty"`
+	KeyRange *topodata.KeyRange `protobuf:"bytes,2,opt,name=key_range,json=keyRange" json:"key_range,omitempty"`
 	// default charset on the player side
 	Charset *Charset `protobuf:"bytes,3,opt,name=charset" json:"charset,omitempty"`
 }
@@ -253,7 +253,7 @@ func (m *StreamKeyRangeRequest) GetCharset() *Charset {
 
 // StreamKeyRangeResponse is the response from StreamKeyRange
 type StreamKeyRangeResponse struct {
-	BinlogTransaction *BinlogTransaction `protobuf:"bytes,1,opt,name=binlog_transaction" json:"binlog_transaction,omitempty"`
+	BinlogTransaction *BinlogTransaction `protobuf:"bytes,1,opt,name=binlog_transaction,json=binlogTransaction" json:"binlog_transaction,omitempty"`
 }
 
 func (m *StreamKeyRangeResponse) Reset()                    { *m = StreamKeyRangeResponse{} }
@@ -292,7 +292,7 @@ func (m *StreamTablesRequest) GetCharset() *Charset {
 
 // StreamTablesResponse is the response from StreamTables
 type StreamTablesResponse struct {
-	BinlogTransaction *BinlogTransaction `protobuf:"bytes,1,opt,name=binlog_transaction" json:"binlog_transaction,omitempty"`
+	BinlogTransaction *BinlogTransaction `protobuf:"bytes,1,opt,name=binlog_transaction,json=binlogTransaction" json:"binlog_transaction,omitempty"`
 }
 
 func (m *StreamTablesResponse) Reset()                    { *m = StreamTablesResponse{} }
@@ -323,43 +323,47 @@ func init() {
 }
 
 var fileDescriptor0 = []byte{
-	// 594 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xa4, 0x54, 0x5d, 0x8f, 0xd2, 0x4c,
-	0x14, 0x7e, 0xa1, 0xbb, 0x40, 0x4f, 0x79, 0xd9, 0xee, 0xa0, 0x2b, 0x21, 0x31, 0xd9, 0x34, 0xea,
-	0x72, 0x63, 0x63, 0xf0, 0xc2, 0xa8, 0x57, 0x02, 0x75, 0x43, 0x96, 0x0f, 0x6d, 0xd9, 0x1b, 0x13,
-	0xd3, 0x0c, 0x30, 0x62, 0x63, 0xbf, 0x98, 0x99, 0xc5, 0xf0, 0x5b, 0xfc, 0x99, 0xc6, 0x7b, 0xa7,
-	0xd3, 0x76, 0x29, 0x6e, 0x30, 0x9b, 0x78, 0x77, 0xce, 0x99, 0xe7, 0x7c, 0x3c, 0xcf, 0x9c, 0x19,
-	0xd0, 0xe7, 0x5e, 0xe8, 0x47, 0xab, 0x25, 0xe6, 0xd8, 0x8c, 0x69, 0xc4, 0x23, 0x04, 0xbb, 0x48,
-	0x5b, 0x5b, 0xdf, 0x10, 0xba, 0x4d, 0x0f, 0xda, 0x0d, 0x1e, 0xc5, 0xd1, 0x0e, 0x68, 0xbc, 0x82,
-	0x6a, 0xff, 0x2b, 0xa6, 0x8c, 0x70, 0xd4, 0x80, 0xca, 0xc2, 0xf7, 0x48, 0xc8, 0x5b, 0xa5, 0xf3,
-	0x52, 0xe7, 0x18, 0xd5, 0xe1, 0x68, 0x11, 0x85, 0x61, 0xab, 0x2c, 0x3d, 0x71, 0xca, 0x08, 0xdd,
-	0x10, 0xda, 0x52, 0x12, 0xdf, 0xf8, 0x59, 0x86, 0xd3, 0x9e, 0x6c, 0x32, 0xa3, 0x38, 0x64, 0x78,
-	0xc1, 0xbd, 0x28, 0x44, 0x6f, 0x01, 0x18, 0xc7, 0x9c, 0x04, 0xa2, 0x0c, 0x13, 0x75, 0x94, 0x8e,
-	0xd6, 0xbd, 0x30, 0x0b, 0xe3, 0xdd, 0x49, 0x31, 0x9d, 0x1c, 0x8f, 0x4e, 0x41, 0xe5, 0x5e, 0x40,
-	0x44, 0x81, 0x20, 0x96, 0x5d, 0x15, 0x74, 0x06, 0x0d, 0xbe, 0xc3, 0xba, 0xde, 0x52, 0x76, 0x57,
-	0xdb, 0xbf, 0x4a, 0xa0, 0xee, 0x12, 0x7b, 0x50, 0x5b, 0x08, 0x7b, 0x15, 0xd1, 0xad, 0x9c, 0xbd,
-	0xd1, 0x7d, 0x71, 0xcf, 0x9e, 0x66, 0x3f, 0xcb, 0x43, 0x4f, 0xa0, 0xba, 0x48, 0x85, 0x90, 0xad,
-	0xb5, 0x6e, 0xb3, 0x58, 0x22, 0xd7, 0x48, 0x03, 0x85, 0xad, 0xfd, 0x74, 0x08, 0x63, 0x0d, 0xb5,
-	0xdb, 0xf4, 0x26, 0x9c, 0xf4, 0x46, 0xee, 0xf5, 0xc4, 0xb6, 0xfa, 0xd3, 0xcb, 0xc9, 0xf0, 0x93,
-	0x35, 0xd0, 0xff, 0x13, 0x0a, 0xd6, 0x44, 0xb0, 0x67, 0x5d, 0x0e, 0x27, 0x7a, 0x09, 0xfd, 0x0f,
-	0xaa, 0xf0, 0xfa, 0xd3, 0xf1, 0x78, 0x38, 0xd3, 0xcb, 0xe8, 0x04, 0x34, 0xe1, 0xda, 0xd3, 0xd1,
-	0xa8, 0xf7, 0xae, 0x7f, 0xa5, 0x2b, 0x08, 0xa0, 0x22, 0x02, 0x83, 0xf1, 0x48, 0x3f, 0xca, 0xed,
-	0xc1, 0x48, 0x3f, 0xce, 0x6c, 0xc7, 0x9a, 0xe9, 0x15, 0xe3, 0x47, 0x19, 0x34, 0x87, 0x53, 0x82,
-	0x03, 0x6b, 0x93, 0x30, 0xef, 0xde, 0x61, 0x7e, 0x5e, 0x1c, 0xbb, 0x00, 0xdd, 0x31, 0x15, 0x05,
-	0x39, 0x9e, 0xfb, 0xc4, 0x0d, 0x71, 0x40, 0x24, 0x59, 0x15, 0x75, 0x00, 0xc5, 0xd4, 0x0b, 0x30,
-	0xdd, 0xba, 0xdf, 0xc8, 0xd6, 0xfd, 0xe2, 0x11, 0x7f, 0xc9, 0x04, 0xcd, 0xe4, 0xfe, 0xea, 0x66,
-	0xba, 0x40, 0xef, 0x93, 0x20, 0x7a, 0xb6, 0x8f, 0xdc, 0x60, 0xff, 0x86, 0xb0, 0xd6, 0x91, 0x44,
-	0x42, 0x86, 0xb4, 0xa3, 0xef, 0xb9, 0x52, 0xc7, 0xb2, 0xfc, 0xde, 0xcd, 0x56, 0x0e, 0xdc, 0x6c,
-	0x55, 0x8a, 0xfa, 0xa6, 0x20, 0xaa, 0x60, 0xee, 0x58, 0xae, 0x65, 0xdb, 0x42, 0xcb, 0xd4, 0x4e,
-	0xd4, 0x29, 0xe5, 0xb6, 0x50, 0xa7, 0x9c, 0xd9, 0x1f, 0xa6, 0x8e, 0xae, 0x18, 0x17, 0xd0, 0x4c,
-	0x19, 0x5f, 0xc7, 0x82, 0x3e, 0xb1, 0x89, 0x18, 0x87, 0x71, 0xa4, 0x43, 0x2d, 0x8e, 0x98, 0x97,
-	0xf4, 0x91, 0x22, 0xa9, 0x86, 0x05, 0x0f, 0xf6, 0x81, 0x2c, 0x8e, 0x42, 0x46, 0xd0, 0x73, 0xa8,
-	0x33, 0x19, 0x77, 0xc9, 0x26, 0x7f, 0x08, 0x5a, 0xf7, 0xd1, 0x01, 0x49, 0x8d, 0x0d, 0x3c, 0x4c,
-	0xdd, 0x2b, 0xb2, 0xb5, 0x71, 0xb8, 0x3a, 0xdc, 0x11, 0x3d, 0x05, 0x35, 0x91, 0x8b, 0x26, 0xa8,
-	0x6c, 0xc1, 0x90, 0x79, 0xfb, 0x16, 0xf3, 0xfc, 0xe2, 0x16, 0x2a, 0x07, 0xb7, 0xd0, 0x70, 0xe0,
-	0xec, 0xcf, 0xbe, 0x19, 0x81, 0xd7, 0x80, 0x52, 0xbc, 0x5b, 0x10, 0x37, 0xa3, 0xf1, 0xf8, 0xaf,
-	0x6f, 0xc2, 0xf8, 0x9c, 0x8b, 0x37, 0x4b, 0x96, 0x83, 0x1d, 0xa6, 0x22, 0x7e, 0x02, 0xb9, 0x3f,
-	0x4c, 0xf0, 0x50, 0x84, 0x7f, 0xbf, 0x99, 0x3f, 0xe6, 0x92, 0xe7, 0xe5, 0xff, 0x79, 0xe2, 0x79,
-	0x45, 0x7e, 0x61, 0x2f, 0x7f, 0x07, 0x00, 0x00, 0xff, 0xff, 0x6c, 0x15, 0x73, 0xe9, 0xff, 0x04,
-	0x00, 0x00,
+	// 664 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xb4, 0x54, 0xcf, 0x6e, 0xd3, 0x4e,
+	0x10, 0xfe, 0x25, 0x6e, 0xd3, 0x78, 0xdc, 0x3f, 0xee, 0xf6, 0x47, 0x89, 0x22, 0x2a, 0x55, 0x96,
+	0x10, 0xbd, 0x10, 0x20, 0x5c, 0x50, 0xc5, 0x85, 0xa4, 0xa6, 0x8a, 0xea, 0x24, 0x68, 0x93, 0x72,
+	0xe0, 0x62, 0x6d, 0x92, 0x6d, 0xb1, 0x9a, 0xd8, 0xae, 0x77, 0x1b, 0xc8, 0x43, 0x70, 0xe2, 0x49,
+	0x78, 0x36, 0x5e, 0x80, 0xdd, 0xf5, 0xda, 0x71, 0xff, 0x40, 0xcb, 0x81, 0xdb, 0xcc, 0xb7, 0x33,
+	0xb3, 0xdf, 0x7c, 0x33, 0xbb, 0x60, 0x8f, 0x82, 0x70, 0x1a, 0x9d, 0x4f, 0x08, 0x27, 0x8d, 0x38,
+	0x89, 0x78, 0x84, 0x60, 0x89, 0xd4, 0xad, 0xcb, 0x2b, 0x9a, 0x2c, 0xd2, 0x83, 0xfa, 0x26, 0x8f,
+	0xe2, 0x68, 0x19, 0xe8, 0x74, 0x61, 0xad, 0xfd, 0x99, 0x24, 0x8c, 0x72, 0xb4, 0x0b, 0x95, 0xf1,
+	0x34, 0xa0, 0x21, 0xaf, 0x95, 0xf6, 0x4b, 0x07, 0xab, 0x58, 0x7b, 0x08, 0xc1, 0xca, 0x38, 0x0a,
+	0xc3, 0x5a, 0x59, 0xa1, 0xca, 0x96, 0xb1, 0x8c, 0x26, 0x73, 0x9a, 0xd4, 0x8c, 0x34, 0x36, 0xf5,
+	0x9c, 0x1f, 0x06, 0x6c, 0xb7, 0xd4, 0xd5, 0xc3, 0x84, 0x84, 0x8c, 0x8c, 0x79, 0x10, 0x85, 0xe8,
+	0x18, 0x80, 0x71, 0xc2, 0xe9, 0x4c, 0x94, 0x63, 0xa2, 0xba, 0x71, 0x60, 0x35, 0x9f, 0x35, 0x0a,
+	0xa4, 0x6f, 0xa5, 0x34, 0x06, 0x59, 0x3c, 0x2e, 0xa4, 0xa2, 0x27, 0x60, 0xf2, 0x60, 0x46, 0x05,
+	0x32, 0x8b, 0x15, 0x1f, 0x03, 0x2f, 0x01, 0xf4, 0x14, 0x36, 0xf9, 0xb2, 0x84, 0x1f, 0x4c, 0x14,
+	0x39, 0x13, 0x6f, 0x14, 0xd0, 0xce, 0xa4, 0xfe, 0xad, 0x0c, 0x66, 0x5e, 0x1e, 0x79, 0x50, 0x1d,
+	0x0b, 0xfb, 0x3c, 0x4a, 0x16, 0xaa, 0xef, 0xcd, 0xe6, 0xcb, 0x07, 0x32, 0x6b, 0xb4, 0x75, 0x1e,
+	0xce, 0x2b, 0xa0, 0xe7, 0xb0, 0x36, 0x4e, 0xe5, 0x54, 0xf4, 0xac, 0xe6, 0x4e, 0xb1, 0x98, 0x56,
+	0x1a, 0x67, 0x31, 0xc8, 0x06, 0x83, 0x5d, 0x4e, 0x35, 0x4d, 0x69, 0x3a, 0x97, 0x50, 0xcd, 0xca,
+	0xa2, 0x1d, 0xd8, 0x6a, 0x79, 0xfe, 0x69, 0x0f, 0xbb, 0xed, 0xfe, 0x71, 0xaf, 0xf3, 0xc9, 0x3d,
+	0xb2, 0xff, 0x43, 0xeb, 0x50, 0x15, 0x60, 0xcb, 0x3d, 0xee, 0xf4, 0xec, 0x12, 0xda, 0x00, 0x53,
+	0x78, 0xed, 0x7e, 0xb7, 0xdb, 0x19, 0xda, 0x65, 0xb4, 0x05, 0x96, 0x70, 0x71, 0xdf, 0xf3, 0x5a,
+	0xef, 0xda, 0x27, 0xb6, 0x81, 0x00, 0x2a, 0x02, 0x38, 0xea, 0x7a, 0xf6, 0x4a, 0x66, 0x1f, 0x79,
+	0xf6, 0xaa, 0xb6, 0x07, 0xee, 0xd0, 0xae, 0x38, 0x3f, 0xcb, 0x60, 0x0d, 0x78, 0x42, 0xc9, 0xcc,
+	0x9d, 0x4b, 0x45, 0xde, 0xde, 0x52, 0x64, 0xbf, 0xd8, 0x44, 0x21, 0xf4, 0x2e, 0x05, 0xf6, 0x00,
+	0x38, 0x19, 0x4d, 0xa9, 0x1f, 0x92, 0x19, 0x55, 0x22, 0x98, 0x62, 0x46, 0x12, 0xe9, 0x09, 0x00,
+	0x1d, 0x02, 0x8a, 0x93, 0x60, 0x46, 0x92, 0x85, 0x7f, 0x41, 0x17, 0xfe, 0x59, 0x40, 0xa7, 0x13,
+	0x26, 0x04, 0x90, 0x2b, 0xb1, 0xde, 0x48, 0x37, 0xf5, 0xbd, 0x04, 0xb1, 0xad, 0xe3, 0x4e, 0xe8,
+	0x42, 0x01, 0x0c, 0xbd, 0xb9, 0x9e, 0x3b, 0x27, 0xd3, 0x2b, 0xca, 0x6a, 0x2b, 0x2a, 0x17, 0x74,
+	0x2e, 0x8e, 0xbe, 0x14, 0x33, 0x3f, 0xaa, 0x98, 0x4c, 0xe7, 0xd5, 0x5c, 0xe7, 0xeb, 0x9b, 0x54,
+	0xb9, 0x7f, 0x93, 0xd6, 0xee, 0xd8, 0x24, 0xe7, 0xb0, 0x30, 0x2c, 0xa1, 0xe8, 0xc0, 0xf5, 0x5d,
+	0x8c, 0xc5, 0x8c, 0x52, 0x5b, 0xaa, 0x5e, 0xca, 0x6c, 0xa1, 0x7a, 0x59, 0xdb, 0x1f, 0xfa, 0x03,
+	0xdb, 0x70, 0x5e, 0xc1, 0x4e, 0xaa, 0xe4, 0x69, 0x2c, 0x64, 0xa5, 0x98, 0x0a, 0xfe, 0x8c, 0xa3,
+	0x3a, 0x54, 0xe3, 0x88, 0x05, 0xf2, 0x02, 0x25, 0xbe, 0x89, 0x73, 0xdf, 0xc1, 0xf0, 0xff, 0xf5,
+	0x14, 0x16, 0x47, 0x21, 0x93, 0x9a, 0xae, 0x33, 0x85, 0xfb, 0x74, 0x9e, 0x3d, 0x5f, 0xab, 0xf9,
+	0xf8, 0x37, 0x43, 0xc3, 0x16, 0x5b, 0x3a, 0xce, 0xf7, 0x12, 0x3c, 0x4a, 0x0f, 0x85, 0x5a, 0x98,
+	0x84, 0xe7, 0x0f, 0x61, 0x82, 0x5e, 0x80, 0x29, 0x27, 0x90, 0xc8, 0x78, 0xbd, 0xe8, 0xa8, 0x91,
+	0xff, 0x2c, 0x79, 0xa5, 0xea, 0x85, 0xb6, 0x8a, 0xef, 0xc2, 0xb8, 0xff, 0x5d, 0x38, 0x67, 0xb0,
+	0x7b, 0x93, 0x94, 0xee, 0xd5, 0x03, 0x94, 0x26, 0xfa, 0x85, 0x51, 0xe8, 0x8e, 0xf7, 0xfe, 0xf8,
+	0x70, 0xf1, 0xf6, 0xe8, 0x26, 0xe4, 0x7c, 0xcd, 0x86, 0x30, 0x94, 0x0b, 0xca, 0x1e, 0xd2, 0xba,
+	0xf8, 0xf9, 0xd4, 0x36, 0x33, 0xd1, 0xb7, 0x21, 0x4e, 0xb4, 0xf7, 0xb7, 0x1d, 0x4e, 0xb2, 0x59,
+	0x66, 0x37, 0xff, 0x8b, 0xfe, 0x46, 0x15, 0xf5, 0xc9, 0xbf, 0xfe, 0x15, 0x00, 0x00, 0xff, 0xff,
+	0x46, 0xa2, 0xfc, 0xd1, 0x21, 0x06, 0x00, 0x00,
 }

commit 58e72c1a9c99f9a3fca9be2f8938881f13c86686

bootstrap: Do no longer install gRPC Python dependencies manually.
Since gRPC 0.13, the gRPC Python install process takes care of downloading python modules like "six" or "google.protobuf".

The new install process also no longer requires "tox", hence we dropped that dependency as well.

This commit also fixes the issue which was introduced in commit 938f31743ddef04cc932f40ed9133aca73149705.

When we changed the install command flag for protobuf python from --root to --prefix, a system wide python package would always trump the locally installed version. Due to that, we could no longer run the e2e tests on our desktops. Since we no longer manually install protobuf python, this issue goes away. Before I had that solution, I did some analysis why the system wide version gets imported instead of the local version.

If --prefix is used, protobuf python is installed as an .egg file in the "site-packages" directory.

If --root is used, the .py files are copied to the "dist-packages" directory instead.

For unknown reasons, the .egg file in "site-packages" was not used when calling "import google.protobuf".

Here's what it looked like:

--prefix: (not working)

$ python -v
>>> import google.protobuf
[...]
import google # loaded from Zip /home/mberlin/workspace/vitess/dist/grpc/usr/local/lib/python2.7/site-packages/protobuf-3.0.0b2-py2.7-linux-x86_64.egg/google/__init__.pyc
import google.protobuf # directory /usr/lib/python2.7/dist-packages/google/protobuf
import google.protobuf # precompiled from /usr/lib/python2.7/dist-packages/google/protobuf/__init__.pyc
>>>

--root: (working)

$ python -v
>>> import google.protobuf
import google.protobuf # directory /home/mberlin/workspace/vitess/dist/grpc/usr/local/lib/python2.7/dist-packages/google/protobuf
import google.protobuf # precompiled from /home/mberlin/workspace/vitess/dist/grpc/usr/local/lib/python2.7/dist-packages/google/protobuf/__init__.pyc
>>>

bootstrap.sh
@@ -87,11 +87,6 @@ else
   export PATH=$(prepend_path $PATH $grpc_dist/usr/local/bin)
   export LD_LIBRARY_PATH=$(prepend_path $LD_LIBRARY_PATH $grpc_dist/usr/local/lib)
 
-  if [ `uname -s` == "Darwin" ]; then
-    # on OSX tox is installed in the following path
-    export PATH=$(prepend_path $PATH /usr/local/Cellar/python/2.7.11/Frameworks/Python.framework/Versions/2.7/bin)
-  fi
-
   ./travis/install_grpc.sh $grpc_dist || fail "gRPC build failed"
   echo "$grpc_ver" > $grpc_dist/.build_finished
 

travis/install_grpc.sh
@@ -11,14 +11,13 @@ if [ -n "$grpc_dist" ]; then
   cd $grpc_dist
 fi
 
-# for python, we'll need the latest virtualenv and tox.
-# running gRPC requires the six package, version >=1.10.
+# Python requires a very recent version of virtualenv.
 if [ -n "$grpc_dist" ]; then
   # Create a virtualenv, which also creates a virualenv-boxed pip.
   virtualenv $grpc_dist/usr/local
-  $grpc_dist/usr/local/bin/pip install --upgrade --ignore-installed virtualenv tox six
+  $grpc_dist/usr/local/bin/pip install --upgrade --ignore-installed virtualenv
 else
-  pip install --upgrade --ignore-installed virtualenv tox six
+  pip install --upgrade --ignore-installed virtualenv
 fi
 
 # clone the repository, setup the submodules
@@ -48,18 +47,8 @@ else
   make install
 fi
 
-# build and install python protobuf side
-cd python
-if [ -n "$grpc_dist" ]; then
-  python setup.py build --cpp_implementation
-  python setup.py install --cpp_implementation --prefix=$grpc_dist/usr/local
-else
-  python setup.py build --cpp_implementation
-  python setup.py install --cpp_implementation
-fi
-
 # now install grpc itself
-cd ../../..
+cd ../..
 if [ -n "$grpc_dist" ]; then
   make install prefix=$grpc_dist/usr/local
 else