bbe2ab1fd846f921ae907bc5f2dcf5a0a3e83991
Alain Jobart <alainjobart@google.com>
2016-04-06 08:31:19 -0700

    Adding topo.Impl.WatchVSchema.
    And unit tests, implementing it for etcd and zk.

--- go/vt/etcdtopo/vschema.go
+++ go/vt/etcdtopo/vschema.go
@@ -1,9 +1,14 @@
 package etcdtopo
 
 import (
+	"time"
+
+	"github.com/coreos/go-etcd/etcd"
+	log "github.com/golang/glog"
+	"golang.org/x/net/context"
+
 	"github.com/youtube/vitess/go/vt/topo"
 	"github.com/youtube/vitess/go/vt/vtgate/vindexes"
-	"golang.org/x/net/context"
 )
 
 /*
@@ -38,4 +43,74 @@ func (s *Server) GetVSchema(ctx context.Context, keyspace string) (string, error
 		return "", ErrBadResponse
 	}
 	return resp.Node.Value, nil
+}
+
+// WatchVSchema is part of the topo.Server interface
+func (s *Server) WatchVSchema(ctx context.Context, keyspace string) (<-chan string, error) {
+	filePath := vschemaFilePath(keyspace)
+
+	notifications := make(chan string, 10)
+
+	// The watch go routine will stop if the 'stop' channel is closed.
+	// Otherwise it will try to watch everything in a loop, and send events
+	// to the 'watch' channel.
+	watch := make(chan *etcd.Response)
+	stop := make(chan bool)
+	go func() {
+		vschema := "{}"
+		var modifiedVersion int64
+
+		resp, err := s.getGlobal().Get(filePath, false /* sort */, false /* recursive */)
+		if err != nil || resp.Node == nil {
+			// node doesn't exist
+		} else {
+			if resp.Node.Value != "" {
+				vschema = resp.Node.Value
+				modifiedVersion = int64(resp.Node.ModifiedIndex)
+			}
+		}
+
+		// re-check for stop here to be safe, in case the
+		// Get took a long time
+		select {
+		case <-stop:
+			return
+		case notifications <- vschema:
+		}
+
+		for {
+			if _, err := s.getGlobal().Watch(filePath, uint64(modifiedVersion+1), false /* recursive */, watch, stop); err != nil {
+				log.Errorf("Watch on %v failed, waiting for %v to retry: %v", filePath, WatchSleepDuration, err)
+				timer := time.After(WatchSleepDuration)
+				select {
+				case <-stop:
+					return
+				case <-timer:
+				}
+			}
+		}
+	}()
+
+	// This go routine is the main event handling routine:
+	// - it will stop if ctx.Done() is closed.
+	// - if it receives a notification from the watch, it will forward it
+	// to the notifications channel.
+	go func() {
+		for {
+			select {
+			case resp := <-watch:
+				vschema := "{}"
+				if resp.Node != nil && resp.Node.Value != "" {
+					vschema = resp.Node.Value
+				}
+				notifications <- vschema
+			case <-ctx.Done():
+				close(stop)
+				close(notifications)
+				return
+			}
+		}
+	}()
+
+	return notifications, nil
 }
