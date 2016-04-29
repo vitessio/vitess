c6495557ee5d53cf0a8ac14274e0f55658ba9a47
Andy Diamondstein <adiamondstein@google.com>
2016-04-29 16:28:47 -0400

    Remove blog post templates since blog was moved to blog.vitess.io, served by blogspot or blogger or something. Revert changes to preview-site.sh.

--- preview-site.sh
+++ preview-site.sh
@@ -14,19 +14,29 @@ mkdir $PREVIEW_DIR
 cp -rf vitess.io/* $PREVIEW_DIR
 
 mkdir $PREVIEW_DIR/_includes/doc
-mkdir -p $PREVIEW_DIR/posts/
 mkdir -p $PREVIEW_DIR/_posts/doc
+
+# create ref files for each doc
+for d in doc/*.md
+do
+  name=${d:4}
+  title=${name%%.*}
+  docpath="$PREVIEW_DIR/_posts/doc/2015-01-01-${name}"
+  touch $docpath
+  echo "---" >> $docpath
+  echo "layout: doc" >> $docpath
+  echo "title: \"$title\"" >> $docpath
+  echo "categories: doc" >> $docpath
+  echo "toc: true" >> $docpath
+  echo "---" >> $docpath
+  echo "{% include $d %}" >> $docpath
+done
 
 # preserve links between docs
 for d in `ls doc/*.md` README.md index.md
 do
   python replace_doc_link.py doc $d > $PREVIEW_DIR/_includes/$d
 done
-
-# copy blog posts
-cp doc/blog/index.md $PREVIEW_DIR/posts/
-cp doc/blog/posts/* $PREVIEW_DIR/_posts/
-cp doc/blog/posts/* $PREVIEW_DIR/posts/
 
 # launch web site locally
 cd $PREVIEW_DIR
