3d20aefac6f4f6882dd373d90b8d4cb867f5f10c
Anthony Yeh <enisoc@google.com>
2016-05-25 20:14:46 -0700

    java: Add back Hadoop MapReduce integration test.
    This is based on an old test that got left behind in the conversion to
    the new proto3-based Java client. I've updated it to use VSchema, which
    removes the need for the test to know about keyspace IDs.

--- java/hadoop/src/main/java/com/youtube/vitess/hadoop/RowWritable.java
+++ java/hadoop/src/main/java/com/youtube/vitess/hadoop/RowWritable.java
@@ -2,6 +2,7 @@ package com.youtube.vitess.hadoop;
 
 import com.google.common.collect.Lists;
 import com.google.common.io.BaseEncoding;
+import com.google.gson.Gson;
 
 import com.youtube.vitess.client.cursor.Row;
 import com.youtube.vitess.proto.Query;
@@ -12,10 +13,15 @@ import org.apache.hadoop.io.Writable;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RowWritable implements Writable {
   private Row row;
+
+  public RowWritable() {}
 
   public RowWritable(Row row) {
     this.row = row;
@@ -36,12 +42,33 @@ public class RowWritable implements Writable {
 
   @Override
   public void readFields(DataInput in) throws IOException {
-    int fieldLen = in.readInt();
-    List<Field> fields = Lists.newArrayListWithCapacity(fieldLen);
-    for (int i = 0; i < fieldLen; i++) {
+    int numFields = in.readInt();
+    List<Field> fields = Lists.newArrayListWithCapacity(numFields);
+    for (int i = 0; i < numFields; i++) {
       fields.add(Field.parseFrom(BaseEncoding.base64().decode(in.readUTF())));
     }
     Query.Row rowProto = Query.Row.parseFrom(BaseEncoding.base64().decode(in.readUTF()));
     row = new Row(fields, rowProto);
+  }
+
+  @Override
+  public String toString() {
+    List<Field> fields = row.getFields();
+
+    Map<String, String> map = new HashMap<>();
+    for (int i = 0; i < fields.size(); i++) {
+      String key = fields.get(i).getName();
+      // Prefer the first column if there are duplicate names.
+      // This matches JDBC ResultSet behavior.
+      if (!map.containsKey(key)) {
+        try {
+          map.put(key, row.getRawValue(i + 1).toStringUtf8());
+        } catch (SQLException e) {
+          map.put(key, e.toString());
+        }
+      }
+    }
+
+    return new Gson().toJson(map);
   }
 }
