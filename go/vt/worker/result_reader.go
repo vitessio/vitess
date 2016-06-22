7920e882f809345d8c4fa27e47a5ae2646d05958
Michael Berlin <mberlin@google.com>
2016-06-22 16:04:56 -0700

    worker: Clarify that ResultReader implementations must be referred by their pointer type.
    Otherwise, the equal comparisons in result_merger.go will fail or produce unexpected results.

--- go/vt/worker/result_reader.go
+++ go/vt/worker/result_reader.go
@@ -9,6 +9,12 @@ import (
 // ResultReader is an advanced version of sqltypes.ResultStream.
 // In addition to the streamed Result messages (which contain a set of rows),
 // it will expose the Fields (columns information) of the result separately.
+//
+// Note that some code in the worker package checks if instances of ResultReader
+// are equal. In consequence, any ResultReader implementation must always use
+// pointer receivers. This way, implementations are always referred by their
+// pointer type and the equal comparison of ResultReader instances behaves as
+// expected.
 type ResultReader interface {
 	// Fields returns the field information for the columns in the result.
 	Fields() []*querypb.Field
