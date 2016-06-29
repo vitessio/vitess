77f5070ed79e3db8cc33e198a9b5ae50d4a242c9
Anthony Yeh <enisoc@google.com>
2016-06-28 18:32:34 -0700

    vtctld2: Rename TopoComponent to TopoBrowserComponent.

---
+++ web/vtctld2/src/app/topo/topo-browser.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'vt-topo',
+  templateUrl: './topo-browser.component.html',
+  styleUrls: ['./topo-browser.component.css'],
+})
+export class TopoBrowserComponent {
+  title = 'Topology Browser';
+}
