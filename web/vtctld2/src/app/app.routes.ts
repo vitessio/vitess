f1bed4bd3406dbc86fc916e3e82b1f153b2cc25f
David Slater <dsslater@google.com>
2016-06-30 16:47:58 -0700

    Added dsahboard view and created keyspace service

--- web/vtctld2/src/app/app.routes.ts
+++ web/vtctld2/src/app/app.routes.ts
@@ -1,15 +1,22 @@
 import { provideRouter, RouterConfig } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { StatusComponent } from './status/status.component';
-import { SchemaComponent } from './schema/schema.component';
+import { DashboardComponent } from './dashboard/dashboard.component'
+import { StatusComponent } from './status/status.component'
+import { SchemaComponent } from './schema/schema.component'
 import { TopoBrowserComponent } from './topo/topo-browser.component';
+import { WorkqueueComponent } from './workqueue/workqueue.component'
+import { KeyspaceComponent } from './keyspace/keyspace.component'
+
+
 
 export const routes: RouterConfig = [
-  { path: '', component: DashboardComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'status', component: StatusComponent },
-  { path: 'schema', component: SchemaComponent },
-  { path: 'topo', component: TopoBrowserComponent },
+	{ path: '', component: DashboardComponent},
+	{ path: 'dashboard', component: DashboardComponent},
+	{ path: 'status', component: StatusComponent},
+	{ path: 'schema', component: SchemaComponent},
+	{ path: 'topo', component: TopoBrowserComponent },
+        { path: 'queue', component: WorkqueueComponent},
+        { path: 'keyspace/:keyspaceName', component: KeyspaceComponent},
+
 ];
 
 export const APP_ROUTER_PROVIDERS = [
