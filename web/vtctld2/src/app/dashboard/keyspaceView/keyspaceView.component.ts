be8b2f73998ce8494ed59ab30f66da948efb9827
David Slater <dsslater@google.com>
2016-07-11 13:49:57 -0700

    Finished adding mock up views to dashboard

--- web/vtctld2/src/app/dashboard/keyspaceView/keyspaceView.component.ts
+++ web/vtctld2/src/app/dashboard/keyspaceView/keyspaceView.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { ROUTER_DIRECTIVES, ActivatedRoute, Router } from '@angular/router';
 import { KeyspaceService } from '../../shared/keyspaceService/keyspace.service';
 import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
-import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+import { PolymerElement } from '@vaadin/angular2-polymer';
 
 @Component({
   moduleId: module.id,
@@ -10,15 +10,32 @@ import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
   templateUrl: './keyspaceView.component.html',
   styleUrls: ['./keyspaceView.component.css'],
   directives: [
-            ROUTER_DIRECTIVES,
-            MD_CARD_DIRECTIVES,
-            MD_BUTTON_DIRECTIVES],
+              ROUTER_DIRECTIVES,
+              MD_CARD_DIRECTIVES,
+              PolymerElement('paper-dialog'),
+              PolymerElement('paper-button'),
+              PolymerElement('paper-input'),
+              PolymerElement('paper-item'),
+              PolymerElement('paper-toast')],
   providers: [KeyspaceService],
 })
 export class KeyspaceViewComponent implements OnInit, OnDestroy{
   private routeSub: any;
   keyspaceName: string;
+  shardsReady = false;
   keyspace = {};
+  openForm = false;
+  actionWord: string;
+  actionWordPast: string;
+  actionFunction: any;
+  dialogTitle: string;
+  toastText: string;
+  //New/Edited Shard Information
+  NSH = {
+    name : "",
+    lowerBound : "",
+    upperBound : ""
+  };
   constructor(
     private route: ActivatedRoute,
     private router: Router,
@@ -39,12 +56,68 @@ export class KeyspaceViewComponent implements OnInit, OnDestroy{
 
   getKeyspace(keyspaceName) {
     this.keyspaceService.getKeyspace(keyspaceName).subscribe((keyspace) => {
-      console.log(keyspace[0]);
       this.keyspace = keyspace[0];
+      this.shardsReady =true;
     });
   }
 
   getShardParams(shardName: string) {
     return {keyspace: this.keyspaceName, shard: shardName};
+  }
+
+  toggleForm() {
+    this.openForm = !this.openForm;
+  }
+
+  submitForm(){
+    /*Temporary Function, will be replaced with CRUD interface*/
+    console.log("SUBMIT: ", this.NSH);
+  }
+
+  populateNSH(shard){
+    this.actionWord = "Edit";
+    this.actionWordPast = "Edited";
+    this.actionFunction = this.submitForm;
+    this.dialogTitle = "Edit " + shard;
+    this.NSH.name = shard;
+    this.NSH.lowerBound = this.getLower(shard);
+    this.NSH.upperBound = this.getUpper(shard);
+    this.toastText = "Edited Shard";
+  }
+  getLower(shard) {
+    let index = shard.indexOf('-');
+    if (index <= 0) {
+      return "";
+    } else {
+      return shard.substring(0,index);
+    }
+  }
+  getUpper(shard){
+    let index = shard.indexOf('-');
+    return shard.substring(index + 1);
+  }
+  clearNSH(){
+    this.actionWord = "Create";
+    this.actionWordPast = "Created";
+    this.actionFunction = this.submitForm;
+    this.dialogTitle = "Create a New Shard";
+    this.NSH.name = "";
+    this.NSH.lowerBound = "";
+    this.NSH.upperBound = "";
+    this.toastText = "Created a New Shard"
+  }
+  blockClicks(event){
+    event.stopPropagation();
+  }
+  nav(keyspaceName, shardName) {
+    this.router.navigateByUrl("/shard?keyspace=" + keyspaceName + "&shard="+shardName);
+  }
+
+  getName(lowerBound, upperBound) {
+    let LB = lowerBound == "0" ? "": lowerBound;
+    let UB = upperBound == "0" ? "": upperBound;
+    let full = LB + "-" + UB;
+    this.NSH.name = full == "-" ? "0": full;
+    return this.NSH.name;
   }
 }
