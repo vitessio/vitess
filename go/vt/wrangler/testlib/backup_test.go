f8628fc2ec306aaaf95795959877e8c7067c0ffd
Pavel Ivanov <pivanof@google.com>
2016-07-25 14:02:41 -0700

    Keep _vt.local_metadata table across backup-restore operations.
    So far local_metadata contained only data needed for Orchestrator and we didn't
    need to preserve that data across restores because it was about the vttablet
    running near MySQL. With the plan to store some data about the database itself
    (e.g. id of a schema swap already executed on the database) we need to save
    the table's contents in a backup and preserve it after restore.
    This change achieves that. The data used by Orchestrator gets repopulated
    immediately after restore, so that functionality is not changed.

--- go/vt/wrangler/testlib/backup_test.go
+++ go/vt/wrangler/testlib/backup_test.go
@@ -38,9 +38,10 @@ func TestBackupRestore(t *testing.T) {
 
 	// Set up mock query results.
 	db.AddQuery("CREATE DATABASE IF NOT EXISTS _vt", &sqltypes.Result{})
-	db.AddQuery("DROP TABLE IF EXISTS _vt.local_metadata", &sqltypes.Result{})
+	db.AddQuery("BEGIN", &sqltypes.Result{})
+	db.AddQuery("COMMIT", &sqltypes.Result{})
 	db.AddQueryPattern(`SET @@session\.sql_log_bin = .*`, &sqltypes.Result{})
-	db.AddQueryPattern(`CREATE TABLE _vt\.local_metadata .*`, &sqltypes.Result{})
+	db.AddQueryPattern(`CREATE TABLE IF NOT EXISTS _vt\.local_metadata .*`, &sqltypes.Result{})
 	db.AddQueryPattern(`INSERT INTO _vt\.local_metadata .*`, &sqltypes.Result{})
 
 	// Initialize our temp dirs
