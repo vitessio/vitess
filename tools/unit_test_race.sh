22d46b73db755b3a819f8e3456109cf156512511
Michael Berlin <mberlin@google.com>
2016-08-04 19:42:08 -0700

    tools: Go race executor: Remove workaround for Go binaries <1.5.
    We require at least Go 1.5 and do not need this workaround anymore.

--- tools/unit_test_race.sh
+++ tools/unit_test_race.sh
@@ -14,12 +14,7 @@ trap '[ -f "$temp_log_file" ] && rm $temp_log_file' EXIT
 # Although Go 1.5 says 'exit status 66' in case of a race, it exits with 1.
 # Therefore, we manually check the output of 'go test' for data races and
 # exit with an error if one was found.
-
-# NOTE: Go binaries <1.5 had a bug which prevented go test -race from exiting
-# with a non-zero code when a race is found.
-# The fix for the bug seems to be: https://go-review.googlesource.com/#/c/4371/
-# To work-around bugged go (<1.5) binaries, we enable "halt_on_error".
-GORACE=halt_on_error=1 go test $VT_GO_PARALLEL -race ./go/vt/... 2>&1 | tee $temp_log_file
+go test $VT_GO_PARALLEL -race ./go/vt/... 2>&1 | tee $temp_log_file
 if [ ${PIPESTATUS[0]} -ne 0 ]; then
   if grep "WARNING: DATA RACE" -q $temp_log_file; then
     echo
