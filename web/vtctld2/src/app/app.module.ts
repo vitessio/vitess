93253a9b61f267e16c4175458afb2c91cbced4e5
Anthony Yeh <enisoc@google.com>
2016-08-12 17:29:20 -0700

    vtctld2: Add own components to module declarations.

--- web/vtctld2/src/app/app.module.ts
+++ web/vtctld2/src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_PROVIDERS } from '@angular/http';
 
 import { AppComponent }  from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { KeyspaceComponent } from './dashboard/keyspace.component';
+import { SchemaComponent } from './schema/schema.component';
+import { ShardComponent } from './dashboard/shard.component';
+import { StatusComponent } from './status/status.component';
+import { TopoBrowserComponent } from './topo/topo-browser.component';
+import { TasksComponent } from './tasks/tasks.component';
 import { APP_ROUTER_PROVIDERS, routing } from './app.routes';
-import { HTTP_PROVIDERS } from '@angular/http';
 
 import { FormsModule } from '@angular/forms';
 import { MdButtonModule } from '@angular2-material/button';
@@ -11,7 +18,16 @@ import { MdRippleModule } from '@angular2-material/core/ripple/ripple';
 
 @NgModule({
   imports:      [ BrowserModule, routing, FormsModule, MdButtonModule, MdRippleModule],
-  declarations: [ AppComponent ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    KeyspaceComponent,
+    SchemaComponent,
+    ShardComponent,
+    StatusComponent,
+    TopoBrowserComponent,
+    TasksComponent,
+  ],
   providers:    [ APP_ROUTER_PROVIDERS, HTTP_PROVIDERS ],
   bootstrap:    [ AppComponent ]
 })
