235b489f05add0f4d9803ad2b5b19dcc0001addb
Alain Jobart <alainjobart@google.com>
2016-08-15 09:50:44 -0700

    Lower level topo server interface: Watch.
    Starting a lower level topo server interface, called topo.Backend.
    The idea is to switch to a path-based interface, with files being
    represented by []byte. All in all simpler interface, easier to test,
    and to maintain. The backward compatibility layer for existing
    implementations will be annoying to maintain, but we'll eventually
    migrate.
    
    For now, only support a Watch method, with slightly different semantics:
    if the node doesn't exist (which is reported as topo.ErrNoNode), or if
    there is an error in the watch, returns the error and closes the watch.

---
+++ go/vt/etcdtopo/convert.go
@@ -0,0 +1,76 @@
+package etcdtopo
+
+import (
+	"encoding/json"
+	"strings"
+
+	"github.com/golang/protobuf/proto"
+
+	topodatapb "github.com/youtube/vitess/go/vt/proto/topodata"
+	vschemapb "github.com/youtube/vitess/go/vt/proto/vschema"
+)
+
+// This file contains utility functions to maintain backward
+// compatibility with old-style non-Backend etcd topologies. The old
+// implementations (before 2016-08-17) used to deal with explicit data
+// types. We converted them to a generic []byte and path
+// interface. But the etcd implementation was not compatible with
+// this.
+
+// dataType is an enum for possible known data types, used for
+// backward compatibility.
+type dataType int
+
+// Constants for type conversion
+const (
+	// newType is used to indicate a topology object type of
+	// anything that is added after the topo.Backend refactor,
+	// i.e. anything that doesn't require conversion between old
+	// style topologies and the new style ones. The list of enum
+	// values after this contain all types that exist at the
+	// moment (2016-08-17) and doesn't need to be expanded when
+	// something new is saved in the topology because it will be
+	// saved in the new style, not in the old one.
+	newType dataType = iota
+	srvKeyspaceType
+	srvVSchemaType
+)
+
+// rawDataFromNodeValue convert the data of the given type into an []byte.
+// It is mindful of the backward compatibility, i.e. for newer objects
+// it doesn't do anything, but for old object types that were stored in JSON
+// format in converts them to proto3 binary encoding.
+func rawDataFromNodeValue(valueType dataType, value string) ([]byte, error) {
+	var p proto.Message
+	switch valueType {
+	case srvKeyspaceType:
+		p = &topodatapb.SrvKeyspace{}
+	case srvVSchemaType:
+		p = &vschemapb.SrvVSchema{}
+	default:
+		return []byte(value), nil
+	}
+
+	if err := json.Unmarshal([]byte(value), p); err != nil {
+		return nil, err
+	}
+
+	return proto.Marshal(p)
+}
+
+// oldTypeAndFilePath returns the data type and old file path for a given path.
+func oldTypeAndFilePath(filePath string) (dataType, string) {
+	parts := strings.Split(filePath, "/")
+
+	// SrvKeyspace: local cell, /keyspaces/<keyspace>/SrvKeyspace
+	if len(parts) == 4 && parts[0] == "" && parts[1] == "keyspaces" && parts[3] == "SrvKeyspace" {
+		return srvKeyspaceType, srvKeyspaceFilePath(parts[2])
+	}
+
+	// SrvVSchema: local cell, /SrvVSchema
+	if len(parts) == 2 && parts[1] == "SrvVSchema" {
+		return srvVSchemaType, srvVSchemaFilePath()
+	}
+
+	return newType, filePath
+}
