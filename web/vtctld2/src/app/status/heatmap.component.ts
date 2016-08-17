899f8ebe14e04403b847288089f531282488f4ba
Priyanka Kulshreshtha <pkulshre@google.com>
2016-08-16 22:29:58 -0700

    Implemented routing to different views + basic popup template

--- web/vtctld2/src/app/status/heatmap.component.ts
+++ web/vtctld2/src/app/status/heatmap.component.ts
@@ -1,4 +1,8 @@
-import { Component, Input, AfterViewInit, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, AfterViewInit, NgZone, OnInit  } from '@angular/core';
+import { CORE_DIRECTIVES } from '@angular/common';
+
+import { TabletStatusService } from '../api/tablet-status.service';
+import { TabletComponent } from './tablet.component';
 
 declare var Plotly: any;
 
@@ -6,36 +10,67 @@ declare var Plotly: any;
   selector: 'vt-heatmap',
     templateUrl: './heatmap.component.html',
     styleUrls: ['./heatmap.component.css'],
+    directives: [
+      CORE_DIRECTIVES,
+      TabletComponent,
+    ],
+    providers: [
+      TabletStatusService
+    ]
 })
 
-export class HeatmapComponent implements AfterViewInit, OnInit {
+export class HeatmapComponent implements AfterViewInit, OnInit, OnChanges {
+  // Needed for the heatmap.
   @Input() data: number[][];
   @Input() aliases: any[][];
-  // yLabels is an array of objects with 2 properties: the cell and array of tabletTypes.
+  // yLabels is an array of objects each with label and nestedLabels
+  // each of which have a name and rowspan.
   @Input() yLabels: Array<any>;
   @Input() xLabels: Array<string>;
-  name: string;
-
+  // metric needed to set the proper colorscale.
+  @Input() metric: string;
   plotlyMap: any;
+  name: string;
+  first = true;
+  heatmapHeight = 0;
+  dataMin = 0;
+  dataMax = 0;
 
   // colorscaleValue defines the gradient for the heatmap.
-  private colorscaleValue = [
-    [0.0, '#424141'],
-    [0.5, '#17A234'],
-    [1.0, '#A22417'],
-  ];
+  private colorscaleValue;
+
+  // Needed for the popup.
+  showPopup = false;
+  popupReady = false;
+  popupTitle: string;
+  popupData: Array<any>;
   private getRowHeight() { return 50; }
   private getXLabelsRowHeight() { return 25; }
 
+  static rowHeight = 50;
+  constructor(private zone: NgZone, private tabletService: TabletStatusService) { }
   constructor() {}
 
+  // getTotalRows returns the number of rows the heatmap should span.
   // getTotalRows returns the number of rows the entire heatmap should span.
   getTotalRows() {
     if (this.yLabels == null) {
+    for (let yLabel of this.yLabels) {
+      height += yLabel.tabletTypes.length;
+      return this.data.length;
+    }
+    return this.yLabels.reduce((a, b) => (a + b.Label.Rowspan), 0);
+  }
+
+  // ngOnChanges is triggered when any input values are changed in status.component
+  // forcing the heatmap to be redrawn.
+  ngOnChanges(changes) {
+    // If this is the first time it's being rendered then no need to redraw.
+    if (this.first === true) {
+      return;
       // TODO(pkulshre): fix this when backend is generalized.
       return 1;
     }
-    return this.yLabels.reduce((a, b) => a.Label.Rowspan + b.Label.Rowspan);
   }
 
   ngOnInit() {
@@ -43,50 +78,104 @@ export class HeatmapComponent implements AfterViewInit, OnInit {
   }
 
   ngAfterViewInit() {
-    this.drawHeatmap();
+    this.drawHeatmap(this.metric);
 
     let elem = <any>(document.getElementById(this.name));
     elem.on('plotly_click', function(data){
-      // TODO(pkulshre): get tabletInfo from service.
+      let y: number = data.points[0].x;
+      let x: number = data.points[0].y;
+      let alias = this.aliases[y][x];
+      this.tabletService.getTabletHealth(alias.cell, alias.uid).subscribe( health => {
+        this.popupTitle = '' + alias.Cell + '-' + alias.Uid;
+        this.popupData = health;
+        this.popupReady = true;
+        this.zone.run( () => { this.showPopup = true; } );
+      });
     }.bind(this));
+    this.first = false;
   }
 
   drawHeatmap() {
+  closePopup() {
+    this.zone.run( () => { this.showPopup = false; } );
+    this.popupReady = false;
+  }
+  drawHeatmap() {
      // Settings for the Plotly heatmap.
-     let chartInfo = [{
-       z: this.data,
        zmin: -10,
        zmax: 10,
-       x: this.xLabels,
-       colorscale: this.colorscaleValue,
-       type: 'heatmap',
-       showscale: false
-     }];
 
-     let xAxisTemplate = {
-       type: 'category',
-       showgrid: false,
-       zeroline: false,
-       rangemode: 'nonnegative',
-       side: 'top',
-       ticks: '',
-     };
-     let yAxisTemplate = {
-       showticklabels: false,
-       ticks: '',
-       fixedrange: true
-     };
-     let chartLayout = {
-       xaxis: xAxisTemplate,
-       yaxis: yAxisTemplate,
-       height: (this.getTotalRows() * this.getRowHeight() + this.getXLabelsRowHeight()),
-       margin: {
-         t: 25,
-         b: 0,
-         r: 0,
-         l: 0
-       },
-       showlegend: false,
+  // setupColorscale sets the right scale based on what metric the heatmap is displaying.
+  setupColorscale(metric) {
+    if (metric === 'healthy') {
+      this.colorscaleValue = [
+        [0.0, '#000000'],
+        [0.33, '#F7EEDE'],
+        [0.66, '#EA109A'],
+        [1.0, '#A22417'],
+      ];
+      this.dataMin = 0;
+      this.dataMax = 3;
+    } else {
+      let max = this.data.reduce(function(a, b) { return a.concat(b); })
+                     .reduce(function(a, b) {
+                        if (a > b) { return a; }
+                        return b;
+                      });
+      let percent = 0;
+      if (max === 0) {
+        percent = 1.0;
+      } else {
+        percent = 1 / max;
+      }
+      this.colorscaleValue = [
+        [0.0, '#000000'],
+        [percent, '#F7EEDE'],
+        [1.0, '#A22417'],
+      ];
+
+      this.dataMin = -1;
+      this.dataMax = max;
+    }
+  }
+
+  drawHeatmap(metric) {
+    this.setupColorscale(metric);
+
+    // Settings for the Plotly heatmap.
+    let chartInfo = [{
+      z: this.data,
+      zmin: this.dataMin,
+      zmax: this.dataMax,
+      x: this.xLabels,
+      colorscale: this.colorscaleValue,
+      type: 'heatmap',
+      showscale: false
+    }];
+
+    let xAxisTemplate = {
+      type: 'category',
+      showgrid: false,
+      zeroline: false,
+      rangemode: 'nonnegative',
+      side: 'top',
+      ticks: '',
+    };
+    let yAxisTemplate = {
+      showticklabels: false,
+      ticks: '',
+      fixedrange: true
+    };
+    let chartLayout = {
+      xaxis: xAxisTemplate,
+      yaxis: yAxisTemplate,
+      margin: {
+        t: 25,
+        b: 0,
+        r: 0,
+        l: 0
+      },
+      showlegend: false,
     };
 
     this.plotlyMap = Plotly.newPlot(this.name, chartInfo, chartLayout, {scrollZoom: true, displayModeBar: false});
--- web/vtctld2/src/app/status/status.component.ts
+++ web/vtctld2/src/app/status/status.component.ts
@@ -1,7 +1,17 @@
+import { Component, OnInit, ComponentResolver, ViewContainerRef } from '@angular/core';
+import { CORE_DIRECTIVES } from '@angular/common';
+import { Component, OnInit, ComponentResolver, ViewChild, NgZone } from '@angular/core';
+import { CORE_DIRECTIVES } from '@angular/common';
 import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { HeatmapComponent } from './heatmap.component';
 import { TabletStatusService } from '../api/tablet-status.service';
+import { TopologyInfoService } from '../api/topology-info.service';
+
+import { Router } from '@angular/router';
+
+import { Dropdown } from 'primeng/primeng';
+import { SelectItem } from 'primeng/primeng';
 
 @Component({
   selector: 'vt-status',
@@ -12,40 +22,163 @@ import { TabletStatusService } from '../api/tablet-status.service';
 export class StatusComponent implements OnInit {
   @ViewChild(HeatmapComponent) heatmap: HeatmapComponent;
 
-  // Used for the heatmap component.
+  // Needed for the router.
+  private sub: any;
+  metricKey = 'metric';
+  keyspaceKey = 'keyspace';
+  cellKey = 'cell';
+  typeKey = 'type';
+
+  // Needed for the construction of the heatmap.
   private data: number[][];
   private aliases: any[][];
   // yLabels is an array of structs with the cell and array of tabletTypes.
   private yLabels: Array<any>;
   private xLabels: Array<string>;
+  private metric: string;
   private heatmapDataReady: boolean = false;
 
-  constructor (private tabletService: TabletStatusService) {}
+  // Needed for the dropdowns.
+  keyspaces: SelectItem[] = [];
+  cells: SelectItem[] = [];
+  tabletTypes: SelectItem[] = [];
+  metrics: SelectItem[] = [];
+  selectedKeyspace: string;
+  selectedCell: string;
+  selectedType: string;
+  selectedMetric: string;
+
+  // Needed to keep track of which data is being polled.
+  statusService: any;
+
+  constructor(private componentResolver: ComponentResolver, private tabletService: TabletStatusService,
+              private router: Router, private zone: NgZone, private topoInfoService: TopologyInfoService) {}
 
   ngOnInit() {
-     this.getHeatmapData();
+    this.getBasicInfo();
   }
 
-  getHeatmapData() {
-    // Subscribe to get updates every second.
-    this.tabletService.getTabletStats('lag', 'test', 'test_keyspace', 'REPLICA').subscribe(stats => {
-      this.data = stats.Data;
-      this.aliases = stats.Aliases;
-      this.yLabels = stats.Labels;
-      this.xLabels = [];
-      for (let i = 0; i < stats.Data[0].length; i++) {
-        this.xLabels.push('' + i);
-      }
-      this.heatmapDataReady = true;
+  // getTopologyInfo gets the keyspace, cell, tabletType, and metric information from the service. 
+  getTopologyInfo() {
+    this.topoInfoService.getKeyspacesAndCell().subscribe( stream => {
+       let keyspacesReturned = stream[0];
+       this.keyspaces = [];
+       for (let i = 0; i < keyspacesReturned.length; i++) {
+         let keyspace = keyspacesReturned[i];
+         this.keyspaces.push({label: keyspace, value: keyspace});
+       }
+       this.keyspaces.push({label: 'all', value: 'all'});
 
-      // The heatmap has already been instantiated so it needs to be redrawn.
-      if (this.heatmap != null) {
-        this.heatmap.data = this.data;
-        this.heatmap.aliases = this.aliases;
-        this.heatmap.yLabels = this.yLabels;
-        this.heatmap.xLabels = this.xLabels;
-        this.heatmap.drawHeatmap();
-      }
+       let cellsReturned = stream[1];
+       this.cells = [];
+       for (let i = 0; i < cellsReturned.length; i++) {
+         let cell = cellsReturned[i];
+         this.cells.push({label: cell, value: cell});
+       }
+       this.cells.push({label: 'all', value: 'all'});
     });
+
+    this.tabletTypes = [];
+    let tabletTypesReturned = this.topoInfoService.getTypes();
+    for (let i = 0; i < tabletTypesReturned.length; i++) {
+      let tabletType = tabletTypesReturned[i];
+      this.tabletTypes.push({label: tabletType, value: tabletType});
+    }
+    this.tabletTypes.push({label: 'all', value: 'all'});
+
+    this.metrics = [];
+    let metricsReturned = this.topoInfoService.getMetrics();
+    for (let i = 0; i < metricsReturned.length; i++) {
+      let metric = metricsReturned[i];
+      this.metrics.push({label: metric, value: metric});
+    }
+  }
+
+  // getBasicInfo is responsible for populating the dropdowns based on the URL.
+  getBasicInfo() {
+    this.sub = this.router
+      .routerState
+      .queryParams
+      .subscribe(params => {
+        this.getTopologyInfo();
+
+        // Setting the beginning values of each category. 
+        this.selectedKeyspace = params[this.keyspaceKey];
+        this.selectedCell = params[this.cellKey];
+        this.selectedType = params[this.typeKey];
+        this.selectedMetric = params[this.metricKey];
+
+        // Show deafult view if path was 'status/'
+        if (this.selectedKeyspace == null && this.selectedCell == null && this.selectedType == null) {
+          this.selectedKeyspace = 'all';
+          this.selectedCell = 'all';
+          this.selectedType = 'all';
+          this.selectedMetric = 'healthy';
+          this.router.navigate(['/status'], this.getExtras());
+        }
+
+        this.zone.run( () => {
+          this.getHeatmapData();
+        });
+    });
+  }
+
+  getExtras() {
+    return {
+      queryParams: { 'keyspace': this.selectedKeyspace,
+                     'cell': this.selectedCell,
+                     'type': this.selectedType,
+                     'metric': this.selectedMetric},
+    };
+  }
+
+  // The next four functions route to a new page once the dropdown is changed.
+  handleKeyspaceChange(e) {
+   this.zone.run( () => {
+     this.statusService.unsubscribe();
+     this.router.navigate(['/status'], this.getExtras());
+   });
+  }
+
+  handleCellChange(e) {
+   this.zone.run( () => {
+     this.statusService.unsubscribe();
+     this.router.navigate(['/status'], this.getExtras());
+   });
+  }
+
+  handleTypeChange(e) {
+   this.zone.run( () => {
+     this.statusService.unsubscribe();
+     this.router.navigate(['/status'], this.getExtras());
+   });
+  }
+
+  handleMetricChange(e) {
+   this.zone.run( () => {
+     this.statusService.unsubscribe();
+     this.router.navigate(['/status'], this.getExtras());
+   });
+  }
+
+  // Resets the heatmap data to new values when polling or when dropdown changes
+  getHeatmapData() {
+     // Subscribe to get updates every second.
+     if  (this.statusService !=  null) {
+      this.statusService.unsubscribe();
+     }
+     this.statusService = this.tabletService.getTabletStats(this.selectedKeyspace,
+       this.selectedCell, this.selectedType, this.selectedMetric)
+       .subscribe(stats => {
+       this.data = stats.Data;
+       this.aliases = stats.Aliases;
+       this.yLabels = stats.Labels;
+       this.xLabels = [];
+       this.metric = this.selectedMetric;
+       for (let i = 0; i < stats.Data[0].length; i++) {
+         this.xLabels.push('' + i);
+       }
+       this.heatmapDataReady = true;
+     });
   }
 }
