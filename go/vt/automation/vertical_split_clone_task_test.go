e04432fb940e6ca9da2de4e0ba7ad3981253f3d0
Michael Berlin <mberlin@google.com>
2016-08-16 22:42:20 -0700

    automation: Update parameters for VerticalSplitCloneTask.
    Removed:
    - destination_pack_count
    
    Added:
    - online
    - offline
    - chunk_count
    - min_rows_per_chunk
    - write_query_max_rows
    - write_query_max_size

---
+++ go/vt/automation/vertical_split_clone_task_test.go
@@ -0,0 +1,51 @@
+// Copyright 2016, Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package automation
+
+import (
+	"flag"
+	"testing"
+
+	"github.com/youtube/vitess/go/vt/worker/fakevtworkerclient"
+	"github.com/youtube/vitess/go/vt/worker/vtworkerclient"
+)
+
+func TestVerticalSplitCloneTask(t *testing.T) {
+	fake := fakevtworkerclient.NewFakeVtworkerClient()
+	vtworkerclient.RegisterFactory("fake", fake.FakeVtworkerClientFactory)
+	defer vtworkerclient.UnregisterFactoryForTest("fake")
+	flag.Set("vtworker_client_protocol", "fake")
+	fake.RegisterResult([]string{"VerticalSplitClone", "--tables=moving1", "--online=false", "--offline=true", "--chunk_count=2", "--min_rows_per_chunk=4", "--write_query_max_rows=1", "--write_query_max_size=1024", "--min_healthy_rdonly_tablets=1", "dest_keyspace/0"},
+		"",  // No output.
+		nil) // No error.
+
+	task := &VerticalSplitCloneTask{}
+	parameters := map[string]string{
+		"dest_keyspace":              "dest_keyspace",
+		"shard":                      "0",
+		"tables":                     "moving1",
+		"vtworker_endpoint":          "localhost:15001",
+		"online":                     "false",
+		"offline":                    "true",
+		"chunk_count":                "2",
+		"min_rows_per_chunk":         "4",
+		"write_query_max_rows":       "1",
+		"write_query_max_size":       "1024",
+		"min_healthy_rdonly_tablets": "1",
+	}
+
+	err := validateParameters(task, parameters)
+	if err != nil {
+		t.Fatalf("Not all required parameters were specified: %v", err)
+	}
+
+	newTasks, _ /* output */, err := task.Run(parameters)
+	if newTasks != nil {
+		t.Errorf("Task should not emit new tasks: %v", newTasks)
+	}
+	if err != nil {
+		t.Errorf("Task should not fail: %v", err)
+	}
+}
