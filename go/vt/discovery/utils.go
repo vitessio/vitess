b4517f8602f34ed3b472a78093a657e402b9c4c8
Pavel Ivanov <pivanof@google.com>
2016-08-30 11:10:40 -0700

    Add code to propagate schema swap to one shard.
    This code is not fully functional and not tested yet, it's just a logically
    complete part. And it's committed separately to make it easier to review and to
    validate the overall approach.
    
    The idea is that once schema change is applied to one tablet and backup is
    taken the function shardSchemaSwap.propagateToNonMasterTablets() will be called
    and it will propagate the new schema to all non-master tablets. The part that
    propagates the new schema to the master and the part that applies the schema
    change and takes the backup will be added later.

--- go/vt/discovery/utils.go
+++ go/vt/discovery/utils.go
@@ -14,7 +14,7 @@ func RemoveUnhealthyTablets(tabletStatsList []TabletStats) []TabletStats {
 		// source and destination, and the source is not serving (disabled by
 		// TabletControl). When we switch the tablet to 'worker', it will
 		// go back to serving state.
-		if ts.Stats == nil || ts.Stats.HealthError != "" || float64(ts.Stats.SecondsBehindMaster) > lowReplicationLag.Seconds() {
+		if ts.Stats == nil || ts.Stats.HealthError != "" || IsReplicationLagHigh(&ts) {
 			continue
 		}
 		result = append(result, ts)
