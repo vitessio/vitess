vitessio/vitess

commit f8bd508d6147b5e9b62850aa9b5b248aba5c2e37

worker: Restart streaming queries on a different tablet if the first retry failed. (#1984)
This is necessary when a tablet is temporary taken out of service.

I've added two integration tests in split_clone_test.go to cover the new functionality.

Other changes:
- Added stat variables for started and errored streaming queries.
- Fake query service uses the alias instead of the UID to make it easier to access the new stat vars by alias.
- StreamHealthQueryService has a new method to inject a health response message where serving is set to false.
- The fake query service now supports an error callback to run code before an error is returned.
- TestSplitCloneV2_Offline_RestartStreamingQuery: Tightened the test by using only one source tablet and verifying the stat vars.
- Unified the clone code to always use a common prefix "table=%v chunk=%v" within the chunk pipeline.
- Clarified when to use TabletStatsCache.GetHealthyTablets() and when not.

go/vt/worker/split_clone.go
@@ -796,7 +796,7 @@ func (scw *SplitCloneWorker) clone(ctx context.Context, state StatusWorkerState)
 	} else {
 		// Pick any healthy serving source tablet.
 		si := scw.sourceShards[0]
-		tablets := discovery.RemoveUnhealthyTablets(scw.tsc.GetTabletStats(si.Keyspace(), si.ShardName(), topodatapb.TabletType_RDONLY))
+		tablets := scw.tsc.GetTabletStats(si.Keyspace(), si.ShardName(), topodatapb.TabletType_RDONLY)
 		if len(tablets) == 0 {
 			// We fail fast on this problem and don't retry because at the start all tablets should be healthy.
 			return fmt.Errorf("no healthy RDONLY tablet in source shard (%v) available (required to find out the schema)", topoproto.KeyspaceShardString(si.Keyspace(), si.ShardName()))
@@ -893,6 +893,7 @@ func (scw *SplitCloneWorker) clone(ctx context.Context, state StatusWorkerState)
 			sourceWaitGroup.Add(1)
 			go func(td *tabletmanagerdatapb.TableDefinition, tableIndex int, chunk chunk) {
 				defer sourceWaitGroup.Done()
+				errPrefix := fmt.Sprintf("table=%v chunk=%v", td.Name, chunk)
 
 				// We need our own error per Go routine to avoid races.
 				var err error
@@ -906,7 +907,7 @@ func (scw *SplitCloneWorker) clone(ctx context.Context, state StatusWorkerState)
 					// Wait for enough healthy tablets (they might have become unhealthy
 					// and their replication lag might have increased since we started.)
 					if err := scw.waitForTablets(ctx, scw.sourceShards, *retryDuration); err != nil {
-						processError("table=%v chunk=%v: No healthy source tablets found (gave up after %v): ", td.Name, chunk, *retryDuration, err)
+						processError("%v: No healthy source tablets found (gave up after %v): ", errPrefix, *retryDuration, err)
 						return
 					}
 				}
@@ -916,49 +917,34 @@ func (scw *SplitCloneWorker) clone(ctx context.Context, state StatusWorkerState)
 				sourceReaders := make([]ResultReader, len(scw.sourceShards))
 				destReaders := make([]ResultReader, len(scw.destinationShards))
 				for shardIndex, si := range scw.sourceShards {
-					var sourceAlias *topodatapb.TabletAlias
+					var tp tabletProvider
 					if state == WorkerStateCloneOffline {
-						// Use the source tablet which we took offline for this phase.
-						sourceAlias = scw.offlineSourceAliases[shardIndex]
+						tp = newSingleTabletProvider(ctx, scw.wr.TopoServer(), scw.offlineSourceAliases[shardIndex])
 					} else {
-						// Pick any healthy serving source tablet.
-						tablets := discovery.RemoveUnhealthyTablets(scw.tsc.GetTabletStats(si.Keyspace(), si.ShardName(), topodatapb.TabletType_RDONLY))
-						if len(tablets) == 0 {
-							processError("no healthy RDONLY tablets in source shard (%v) available", topoproto.KeyspaceShardString(si.Keyspace(), si.ShardName()))
-							return
-						}
-						sourceAlias = scw.tabletTracker.Track(tablets)
-						defer scw.tabletTracker.Untrack(sourceAlias)
+						tp = newShardTabletProvider(scw.tsc, scw.tabletTracker, si.Keyspace(), si.ShardName())
 					}
-
-					sourceResultReader, err := NewRestartableResultReader(ctx, scw.wr.Logger(), scw.wr.TopoServer(), sourceAlias, td, chunk)
+					sourceResultReader, err := NewRestartableResultReader(ctx, scw.wr.Logger(), tp, td, chunk)
 					if err != nil {
-						processError("NewRestartableResultReader for source tablet: %v failed: %v", sourceAlias, err)
+						processError("%v: NewRestartableResultReader for source: %v failed", errPrefix, tp.description())
 						return
 					}
 					defer sourceResultReader.Close()
 					sourceReaders[shardIndex] = sourceResultReader
 				}
+
 				// Wait for enough healthy tablets (they might have become unhealthy
 				// and their replication lag might have increased due to a previous
 				// chunk pipeline.)
 				if err := scw.waitForTablets(ctx, scw.destinationShards, *retryDuration); err != nil {
-					processError("table=%v chunk=%v: No healthy destination tablets found (gave up after %v): ", td.Name, chunk, *retryDuration, err)
+					processError("%v: No healthy destination tablets found (gave up after %v): ", errPrefix, *retryDuration, err)
 					return
 				}
-				for shardIndex, si := range scw.destinationShards {
-					// Pick any healthy serving destination tablet.
-					tablets := discovery.RemoveUnhealthyTablets(scw.tsc.GetTabletStats(si.Keyspace(), si.ShardName(), topodatapb.TabletType_RDONLY))
-					if len(tablets) == 0 {
-						processError("no healthy RDONLY tablets in destination shard (%v) available", topoproto.KeyspaceShardString(si.Keyspace(), si.ShardName()))
-						return
-					}
-					destAlias := scw.tabletTracker.Track(tablets)
-					defer scw.tabletTracker.Untrack(destAlias)
 
-					destResultReader, err := NewRestartableResultReader(ctx, scw.wr.Logger(), scw.wr.TopoServer(), destAlias, td, chunk)
+				for shardIndex, si := range scw.destinationShards {
+					tp := newShardTabletProvider(scw.tsc, scw.tabletTracker, si.Keyspace(), si.ShardName())
+					destResultReader, err := NewRestartableResultReader(ctx, scw.wr.Logger(), tp, td, chunk)
 					if err != nil {
-						processError("NewQueryResultReaderForTablet for destination tablet: %v failed: %v", destAlias, err)
+						processError("%v: NewRestartableResultReader for destination: %v failed: %v", errPrefix, tp.description(), err)
 						return
 					}
 					defer destResultReader.Close()
@@ -970,7 +956,7 @@ func (scw *SplitCloneWorker) clone(ctx context.Context, state StatusWorkerState)
 				if len(sourceReaders) >= 2 {
 					sourceReader, err = NewResultMerger(sourceReaders, len(td.PrimaryKeyColumns))
 					if err != nil {
-						processError("NewResultMerger for table: %v for source tablets failed: %v", td.Name, err)
+						processError("%v: NewResultMerger for source tablets failed: %v", errPrefix, err)
 						return
 					}
 				} else {
@@ -979,7 +965,7 @@ func (scw *SplitCloneWorker) clone(ctx context.Context, state StatusWorkerState)
 				if len(destReaders) >= 2 {
 					destReader, err = NewResultMerger(destReaders, len(td.PrimaryKeyColumns))
 					if err != nil {
-						processError("NewResultMerger for table: %v for destination tablets failed: %v", td.Name, err)
+						processError("%v: NewResultMerger for destination tablets failed: %v", errPrefix, err)
 						return
 					}
 				} else {
@@ -996,13 +982,13 @@ func (scw *SplitCloneWorker) clone(ctx context.Context, state StatusWorkerState)
 					scw.destinationShards, keyResolver,
 					insertChannels, ctx.Done(), dbNames, scw.writeQueryMaxRows, scw.writeQueryMaxSize, scw.writeQueryMaxRowsDelete, statsCounters)
 				if err != nil {
-					processError("NewRowDiffer2 failed: %v", err)
+					processError("%v: NewRowDiffer2 failed: %v", errPrefix, err)
 					return
 				}
 				// Ignore the diff report because all diffs should get reconciled.
 				_ /* DiffReport */, err = differ.Diff()
 				if err != nil {
-					processError("RowDiffer2 failed for table: %v, Error: %v", td.Name, err)
+					processError("%v: RowDiffer2 failed: %v", errPrefix, err)
 					return
 				}
 

go/vt/tabletmanager/binlog_players.go
@@ -305,8 +305,9 @@ func (bpc *BinlogPlayerController) Iteration() (err error) {
 
 	// Find the server list from the health check.
 	// Note: We cannot use tsc.GetHealthyTabletStats() here because it does
-	// not return non-serving tablets. However, we might replicate from
-	// these as well.
+	// not return non-serving tablets. We must include non-serving tablets because
+	// REPLICA source tablets may not be serving anymore because their traffic was
+	// already migrated to the destination shards.
 	addrs := discovery.RemoveUnhealthyTablets(bpc.tabletStatsCache.GetTabletStats(bpc.sourceShard.Keyspace, bpc.sourceShard.Shard, topodatapb.TabletType_REPLICA))
 	if len(addrs) == 0 {
 		return fmt.Errorf("can't find any healthy source tablet for %v %v %v", bpc.cell, bpc.sourceShard.String(), topodatapb.TabletType_REPLICA)

go/vt/tabletserver/queryservice/fakes/stream_health_query_service.go
@@ -83,6 +83,18 @@ func (q *StreamHealthQueryService) AddHealthResponseWithSecondsBehindMaster(repl
 	}
 }
 
+// AddHealthResponseWithNotServing adds a faked health response to the
+// buffer channel. Only "Serving" is different in this message.
+func (q *StreamHealthQueryService) AddHealthResponseWithNotServing() {
+	q.healthResponses <- &querypb.StreamHealthResponse{
+		Target:  proto.Clone(&q.target).(*querypb.Target),
+		Serving: false,
+		RealtimeStats: &querypb.RealtimeStats{
+			SecondsBehindMaster: DefaultSecondsBehindMaster,
+		},
+	}
+}
+
 // UpdateType changes the type of the query service.
 // Only newly sent health messages will use the new type.
 func (q *StreamHealthQueryService) UpdateType(tabletType topodatapb.TabletType) {

go/vt/worker/legacy_split_clone.go
@@ -564,7 +564,8 @@ func (scw *LegacySplitCloneWorker) copy(ctx context.Context) error {
 					scw.tableStatusList.threadStarted(tableIndex)
 
 					// Start streaming from the source tablets.
-					rr, err := NewRestartableResultReader(ctx, scw.wr.Logger(), scw.wr.TopoServer(), scw.sourceAliases[shardIndex], td, chunk)
+					tp := newSingleTabletProvider(ctx, scw.wr.TopoServer(), scw.sourceAliases[shardIndex])
+					rr, err := NewRestartableResultReader(ctx, scw.wr.Logger(), tp, td, chunk)
 					if err != nil {
 						processError("NewRestartableResultReader failed: %v", err)
 						return

go/vt/worker/restartable_result_reader.go
@@ -9,13 +9,13 @@ import (
 	"fmt"
 	"io"
 	"strings"
+	"time"
 
 	"golang.org/x/net/context"
 
 	"github.com/youtube/vitess/go/sqltypes"
 	"github.com/youtube/vitess/go/vt/logutil"
 	"github.com/youtube/vitess/go/vt/tabletserver/tabletconn"
-	"github.com/youtube/vitess/go/vt/topo"
 	"github.com/youtube/vitess/go/vt/topo/topoproto"
 
 	querypb "github.com/youtube/vitess/go/vt/proto/query"
@@ -29,16 +29,20 @@ import (
 type RestartableResultReader struct {
 	ctx    context.Context
 	logger logutil.Logger
-	tablet *topodatapb.Tablet
+	tp     tabletProvider
 	// td is used to get the list of primary key columns at a restart.
 	td    *tabletmanagerdatapb.TableDefinition
 	chunk chunk
 
 	query string
 
-	conn    tabletconn.TabletConn
-	fields  []*querypb.Field
-	output  sqltypes.ResultStream
+	tablet *topodatapb.Tablet
+	conn   tabletconn.TabletConn
+	fields []*querypb.Field
+	output sqltypes.ResultStream
+
+	// lastRow is used during a restart to determine after which row the restart
+	// should start.
 	lastRow []sqltypes.Value
 }
 
@@ -48,62 +52,176 @@ type RestartableResultReader struct {
 // the chunk.
 // NOTE: We assume that the Columns field in "td" was ordered by a preceding
 // call to reorderColumnsPrimaryKeyFirst().
-func NewRestartableResultReader(ctx context.Context, logger logutil.Logger, ts topo.Server, tabletAlias *topodatapb.TabletAlias, td *tabletmanagerdatapb.TableDefinition, chunk chunk) (*RestartableResultReader, error) {
-	shortCtx, cancel := context.WithTimeout(ctx, *remoteActionsTimeout)
-	tablet, err := ts.GetTablet(shortCtx, tabletAlias)
-	cancel()
-	if err != nil {
-		return nil, fmt.Errorf("tablet=%v table=%v chunk=%v: Failed to resolve tablet alias: %v", topoproto.TabletAliasString(tabletAlias), td.Name, chunk, err)
-	}
-
-	conn, err := tabletconn.GetDialer()(tablet.Tablet, *remoteActionsTimeout)
-	if err != nil {
-		return nil, fmt.Errorf("tablet=%v table=%v chunk=%v: Failed to get dialer for tablet: %v", topoproto.TabletAliasString(tabletAlias), td.Name, chunk, err)
-	}
-
+func NewRestartableResultReader(ctx context.Context, logger logutil.Logger, tp tabletProvider, td *tabletmanagerdatapb.TableDefinition, chunk chunk) (*RestartableResultReader, error) {
 	r := &RestartableResultReader{
 		ctx:    ctx,
 		logger: logger,
-		tablet: tablet.Tablet,
+		tp:     tp,
 		td:     td,
 		chunk:  chunk,
-		conn:   conn,
 	}
 
-	if err := r.startStream(); err != nil {
-		return nil, err
+	// If the initial connection fails, we do not restart.
+	if _ /* retryable */, err := r.getTablet(); err != nil {
+		return nil, fmt.Errorf("tablet=%v: %v", topoproto.TabletAliasString(r.tablet.Alias), err)
 	}
-	logger.Infof("tablet=%v table=%v chunk=%v: Starting to stream rows using query '%v'.", topoproto.TabletAliasString(tabletAlias), td.Name, chunk, r.query)
+	if _ /* retryable */, err := r.startStream(); err != nil {
+		return nil, fmt.Errorf("tablet=%v: %v", topoproto.TabletAliasString(r.tablet.Alias), err)
+	}
 	return r, nil
+}
+
+// getTablet (re)sets the tablet which is used for the streaming query.
+// If the method returns an error, the first return value specifies if it is
+// okay to retry.
+func (r *RestartableResultReader) getTablet() (bool, error) {
+	if r.tablet != nil {
+		// If there was a tablet before, return it to the tabletProvider.
+		r.Close()
+
+		r.tablet = nil
+		r.conn = nil
+		r.fields = nil
+		r.output = nil
+	}
+
+	// Get a tablet from the tablet provider.
+	tablet, err := r.tp.getTablet()
+	if err != nil {
+		return true /* retryable */, fmt.Errorf("failed get tablet for streaming query: %v", err)
+	}
+
+	// Get the dialer for it.
+	conn, err := tabletconn.GetDialer()(tablet, *remoteActionsTimeout)
+	if err != nil {
+		return false /* retryable */, fmt.Errorf("failed to get dialer for tablet: %v", err)
+	}
+	r.tablet = tablet
+	r.conn = conn
+	return false /* retryable */, nil
+}
+
+// startStream assumes that getTablet() was succesfully called before and now
+// tries to connect to the set tablet and start the streaming query.
+// If the method returns an error, the first return value specifies if it is
+// okay to retry.
+func (r *RestartableResultReader) startStream() (bool, error) {
+	// Start the streaming query.
+	r.generateQuery()
+	stream, err := r.conn.StreamExecute(r.ctx, &querypb.Target{
+		Keyspace:   r.tablet.Keyspace,
+		Shard:      r.tablet.Shard,
+		TabletType: r.tablet.Type,
+	}, r.query, make(map[string]interface{}))
+	if err != nil {
+		return true /* retryable */, fmt.Errorf("failed to call StreamExecute() for query '%v': %v", r.query, err)
+	}
+
+	// Read the fields information.
+	cols, err := stream.Recv()
+	if err != nil {
+		return true /* retryable */, fmt.Errorf("cannot read Fields for query '%v': %v", r.query, err)
+	}
+	r.fields = cols.Fields
+	r.output = stream
+
+	alias := topoproto.TabletAliasString(r.tablet.Alias)
+	statsStreamingQueryCounters.Add(alias, 1)
+	r.logger.Infof("tablet=%v table=%v chunk=%v: Starting to stream rows using query '%v'.", alias, r.td.Name, r.chunk, r.query)
+	return false, nil
 }
 
 // Next returns the next result on the stream. It implements ResultReader.
 func (r *RestartableResultReader) Next() (*sqltypes.Result, error) {
 	result, err := r.output.Recv()
 	if err != nil && err != io.EOF {
-		r.logger.Infof("tablet=%v table=%v chunk=%v: Failed to read next rows from active streaming query. Trying to restart stream. Original Error: %v", topoproto.TabletAliasString(r.tablet.Alias), r.td.Name, r.chunk, err)
-		// Restart streaming query.
-		// Note that we intentionally don't reset "r.conn" here. This restart
-		// mechanism is only meant to fix transient problems which go away at the
-		// next retry. For example, when MySQL killed the vttablet connection due
-		// to net_write_timeout being reached.
-		if err := r.startStream(); err != nil {
-			return nil, err
-		}
-		result, err = r.output.Recv()
-		if err == nil || err == io.EOF {
-			r.logger.Infof("tablet=%v table=%v chunk=%v: Successfully restarted streaming query with query '%v'.", topoproto.TabletAliasString(r.tablet.Alias), r.td.Name, r.chunk, r.query)
-		} else {
-			// We won't retry a second time and fail for good.
-			// TODO(mberlin): When we have a chunk pipeline restart mechanism,
-			// mention that restart mechanism in the log here.
-			r.logger.Infof("tablet=%v table=%v chunk=%v: Failed to restart streaming query with query '%v'. Error: %v", topoproto.TabletAliasString(r.tablet.Alias), r.td.Name, r.chunk, r.query, err)
-		}
+		// We start the retries only on the second attempt to avoid the cost
+		// of starting a timer (for the retry timeout) for every Next() call
+		// when no error occurs.
+		alias := topoproto.TabletAliasString(r.tablet.Alias)
+		statsStreamingQueryErrorsCounters.Add(alias, 1)
+		r.logger.Infof("tablet=%v table=%v chunk=%v: Failed to read next rows from active streaming query. Trying to restart stream on the same tablet. Original Error: %v", alias, r.td.Name, r.chunk, err)
+		result, err = r.nextWithRetries()
 	}
 	if result != nil && len(result.Rows) > 0 {
 		r.lastRow = result.Rows[len(result.Rows)-1]
 	}
 	return result, err
+}
+
+func (r *RestartableResultReader) nextWithRetries() (*sqltypes.Result, error) {
+	// In case of errors we will keep retrying until retryCtx is done.
+	retryCtx, retryCancel := context.WithTimeout(r.ctx, *retryDuration)
+	defer retryCancel()
+
+	// Note: The first retry will be the second attempt.
+	attempt := 1
+	start := time.Now()
+	for {
+		attempt++
+
+		var result *sqltypes.Result
+		var retryable bool
+		var err error
+
+		if attempt > 2 {
+			// Do not failover to a different tablet at the 2nd attempt, but always
+			// after that.
+			// That's because the first restart is meant to fix transient problems
+			// which go away at the next retry. For example, when MySQL killed the
+			// vttablet connection due to net_write_timeout being reached.
+			retryable, err = r.getTablet()
+			if err != nil {
+				if !retryable {
+					r.logger.Infof("table=%v chunk=%v: Failed to restart streaming query (attempt %d) and failover to a different tablet (%v) due to a non-retryable error: %v", r.td.Name, r.chunk, attempt, r.tablet, err)
+					return nil, err
+				}
+				goto retry
+			}
+		}
+
+		if attempt > 1 {
+			// Restart streaming query.
+			retryable, err = r.startStream()
+			if err != nil {
+				if !retryable {
+					r.logger.Infof("tablet=%v table=%v chunk=%v: Failed to restart streaming query (attempt %d) with query '%v' and stopped due to a non-retryable error: %v", topoproto.TabletAliasString(r.tablet.Alias), r.td.Name, r.chunk, attempt, r.query, err)
+					return nil, err
+				}
+				goto retry
+			}
+		}
+
+		result, err = r.output.Recv()
+		if err == nil || err == io.EOF {
+			r.logger.Infof("tablet=%v table=%v chunk=%v: Successfully restarted streaming query with query '%v' after %.1f seconds.", topoproto.TabletAliasString(r.tablet.Alias), r.td.Name, r.chunk, r.query, time.Now().Sub(start).Seconds())
+
+			// Recv() was successful.
+			return result, err
+		}
+
+	retry:
+		alias := "unknown"
+		if r.tablet != nil {
+			alias = topoproto.TabletAliasString(r.tablet.Alias)
+			// tablet may be nil if e.g. the HealthCheck module currently does not
+			// return a tablet.
+			statsStreamingQueryErrorsCounters.Add(alias, 1)
+		}
+
+		deadline, _ := retryCtx.Deadline()
+		r.logger.Infof("tablet=%v table=%v chunk=%v: Failed to restart streaming query (attempt %d) with query '%v'. Retrying to restart stream on a different tablet (for up to %.1f minutes). Next retry is in %.1f seconds. Error: %v", alias, r.td.Name, r.chunk, attempt, r.query, deadline.Sub(time.Now()).Minutes(), executeFetchRetryTime.Seconds(), err)
+
+		select {
+		case <-retryCtx.Done():
+			if retryCtx.Err() == context.DeadlineExceeded {
+				return nil, fmt.Errorf("%v: failed to restart the streaming connection after retrying for %v", r.tp.description(), *retryDuration)
+			}
+			return nil, fmt.Errorf("%v: interrupted while trying to restart the streaming connection (%.1f minutes elapsed so far)", r.tp.description(), time.Now().Sub(start).Minutes())
+		case <-time.After(*executeFetchRetryTime):
+			// Make a pause between the retries to avoid hammering the servers.
+		}
+	}
 }
 
 // Fields returns the field data. It implements ResultReader.
@@ -113,29 +231,10 @@ func (r *RestartableResultReader) Fields() []*querypb.Field {
 
 // Close closes the connection to the tablet.
 func (r *RestartableResultReader) Close() {
-	r.conn.Close()
-}
-
-func (r *RestartableResultReader) startStream() error {
-	r.generateQuery()
-	stream, err := r.conn.StreamExecute(r.ctx, &querypb.Target{
-		Keyspace:   r.tablet.Keyspace,
-		Shard:      r.tablet.Shard,
-		TabletType: r.tablet.Type,
-	}, r.query, make(map[string]interface{}))
-	if err != nil {
-		return fmt.Errorf("tablet=%v table=%v chunk=%v: failed to call StreamExecute() for query '%v': %v", topoproto.TabletAliasString(r.tablet.Alias), r.td.Name, r.chunk, r.query, err)
+	if r.conn != nil {
+		r.conn.Close()
+		r.tp.returnTablet(r.tablet)
 	}
-
-	// Read the fields information. Fail and do not restart if there is an error.
-	cols, err := stream.Recv()
-	if err != nil {
-		return fmt.Errorf("tablet=%v table=%v chunk=%v: cannot read Fields for query '%v': %v", topoproto.TabletAliasString(r.tablet.Alias), r.td.Name, r.chunk, r.query, err)
-	}
-
-	r.fields = cols.Fields
-	r.output = stream
-	return nil
 }
 
 func (r *RestartableResultReader) generateQuery() {

commit 81da880f97a8be9a8fa039379d0f3457c7b7acb1

worker: Use only healthy tablets to get the schema.
This was the case before until I've introduced a regression in commit f8bd508d6147b5e9b62850aa9b5b248aba5c2e37.

go/vt/worker/split_clone.go
@@ -800,7 +800,7 @@ func (scw *SplitCloneWorker) clone(ctx context.Context, state StatusWorkerState)
 	} else {
 		// Pick any healthy serving source tablet.
 		si := scw.sourceShards[0]
-		tablets := scw.tsc.GetTabletStats(si.Keyspace(), si.ShardName(), topodatapb.TabletType_RDONLY)
+		tablets := scw.tsc.GetHealthyTabletStats(si.Keyspace(), si.ShardName(), topodatapb.TabletType_RDONLY)
 		if len(tablets) == 0 {
 			// We fail fast on this problem and don't retry because at the start all tablets should be healthy.
 			return fmt.Errorf("no healthy RDONLY tablet in source shard (%v) available (required to find out the schema)", topoproto.KeyspaceShardString(si.Keyspace(), si.ShardName()))