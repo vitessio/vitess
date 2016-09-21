3a9981926b234995baffcef852ccd213cb1f5941
Alain Jobart <alainjobart@google.com>
2016-09-21 08:52:16 -0700

    Implementing more GTID comparison for event token.
    In go, adding both MariaDB and MySQL 5.6.
    In python, just adding MariaDB for now.

--- go/vt/binlog/eventtoken/compare.go
+++ go/vt/binlog/eventtoken/compare.go
@@ -1,11 +1,9 @@
 // Package eventtoken includes utility methods for event token
 // handling.
-//
-// FIXME(alainjobart): it will be used to compare event tokens in
-// other ways later, but for now it's a bit small.
 package eventtoken
 
 import (
+	"github.com/youtube/vitess/go/vt/mysqlctl/replication"
 	querypb "github.com/youtube/vitess/go/vt/proto/query"
 )
 
@@ -32,17 +30,47 @@ func Minimum(ev1, ev2 *querypb.EventToken) *querypb.EventToken {
 	}
 }
 
-// Fresher returns true if ev1 is fresher than ev2. In case of doubt,
-// it returns false.
-//
-// FIXME(alainjobart): if both events come from the same shard, we can
-// compare replication position (after parsing it). Need to implement that.
-// FIMXE(alainjobart): when it's more than just a simple compare, add
-// unit tests too.
-func Fresher(ev1, ev2 *querypb.EventToken) bool {
+// Fresher compares two event tokens.  It returns a negative number if
+// ev1<ev2, zero if they're equal, and a positive number if
+// ev1>ev2. In case of doubt (we don't have enough information to know
+// for sure), it returns a negative number.
+func Fresher(ev1, ev2 *querypb.EventToken) int {
 	if ev1 == nil || ev2 == nil {
-		return false
+		// Either one is nil, we don't know.
+		return -1
 	}
 
-	return ev1.Timestamp > ev2.Timestamp
+	if ev1.Timestamp != ev2.Timestamp {
+		// The timestamp is enough to set them apart.
+		return int(ev1.Timestamp - ev2.Timestamp)
+	}
+
+	if ev1.Shard != "" && ev1.Shard == ev2.Shard {
+		// They come from the same shard. See if we have positions.
+		if ev1.Position == "" || ev2.Position == "" {
+			return -1
+		}
+
+		// We can parse them.
+		pos1, err := replication.DecodePosition(ev1.Position)
+		if err != nil {
+			return -1
+		}
+		pos2, err := replication.DecodePosition(ev2.Position)
+		if err != nil {
+			return -1
+		}
+
+		// Then compare.
+		if pos1.Equal(pos2) {
+			return 0
+		}
+		if pos1.AtLeast(pos2) {
+			return 1
+		}
+		return -1
+	}
+
+	// We do not know.
+	return -1
 }
