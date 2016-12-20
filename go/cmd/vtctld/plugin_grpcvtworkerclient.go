933075b872f79ccdfc1ef754094ad73a44fa0cfc
Yipei Wang <yipeiw@google.com>
2016-12-19 19:24:03 -0800

    workflow: Add horizontal resharding workflow.
    This is a beta version implementation, which simply supports creating the horizontal workflow through UI and updating progress of each step.
    
    In the end-to-end test (test/workflow_horizontal_resharding.py), only happy path is tested. To simplify the test, rather than fetching information from front-end and checking explicitly, I manually interacting with the UI after setting up the environments.
    
    Unit test will be added later.
    
    Possible Optimizations (wait for feedback from reviewers, the listed
    points might be useless):
    - make UI more friendly, s.t. the user input doesn't have to input the shard
      list
    - supports action for the workflow, which allows the user to stop and
      restart the workflow
    - adding possible corner case end-to-end test

---
+++ go/cmd/vtctld/plugin_grpcvtworkerclient.go
@@ -0,0 +1,11 @@
+// Copyright 2015, Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+// Imports and registers the gRPC vtworker client.
+
+import (
+	_ "github.com/youtube/vitess/go/vt/worker/grpcvtworkerclient"
+)
--- web/vtctld2/src/app/shared/flags/workflow.flags.ts
+++ web/vtctld2/src/app/shared/flags/workflow.flags.ts
@@ -22,6 +22,20 @@ export class NewWorkflowFlags {
     this.flags['schema_swap_sql'] = new SchemaSwapSQLFlag(4, 'schema_swap_sql');
     this.flags['schema_swap_sql'].positional = true;
     this.flags['schema_swap_sql'].namedPositional = 'sql';
+
+    // Flags for Horizontal Resharding workflow
+    this.flags['horizontal_resharding_keyspace'] = new HorizontalReshardKeyspaceFlag(5, 'horizontal_resharding_keyspace');
+    this.flags['horizontal_resharding_keyspace'].positional = true;
+    this.flags['horizontal_resharding_keyspace'].namedPositional = 'keyspace';
+    this.flags['horizontal_resharding_sources'] = new HorizontalReshardSourceFlag(6, 'horizontal_resharding_sources');
+    this.flags['horizontal_resharding_sources'].positional = true;
+    this.flags['horizontal_resharding_sources'].namedPositional = 'source_shard_list';
+    this.flags['horizontal_resharding_destinations'] = new HorizontalReshardDestinationFlag(7, 'horizontal_resharding_destinations');
+    this.flags['horizontal_resharding_destinations'].positional = true;
+    this.flags['horizontal_resharding_destinations'].namedPositional = 'destination_shard_list';
+    this.flags['horizontal_resharding_vtworker'] = new HorizontalReshardVTWorkerFlag(8, 'horizontal_resharding_vtworker');
+    this.flags['horizontal_resharding_vtworker'].positional = true
+    this.flags['horizontal_resharding_vtworker'].namedPositional = 'vtworker_server_address'
   }
 }
 
@@ -29,6 +43,13 @@ export class FactoryNameFlag extends DropDownFlag {
   constructor(position: number, id: string, workflows) {
     super(position, id, 'Factory Name', 'Specifies the type of workflow to create.', '');
     let options = [];
+    // tested the UI; TO DO: load workflow.horizontal_resharding component
+    if (workflows.horizontal_resharding) {
+      options.push({
+        label: 'Horizontal Resharding',
+        value: 'horizontal_resharding'
+      });
+    }   
     if (workflows.schema_swap) {
       options.push({
         label: 'Schema Swap',
@@ -78,6 +99,34 @@ export class SchemaSwapSQLFlag extends InputFlag {
     this.setDisplayOn('factory_name', 'schema_swap');
   }
 }
+
+export class HorizontalReshardKeyspaceFlag extends InputFlag {
+  constructor(position: number, id: string, value= '') {
+    super(position, id, 'Keyspace', 'Name of a keyspace.', value);
+    this.setDisplayOn('factory_name', 'horizontal_resharding');
+  }
+}
+
+export class HorizontalReshardDestinationFlag extends InputFlag {
+  constructor(position: number, id: string, value= '') {
+    super(position, id, 'Destination Shards', 'List of destination shards.', value);
+    this.setDisplayOn('factory_name', 'horizontal_resharding');
+  }
+} 
+
+export class HorizontalReshardSourceFlag extends InputFlag {
+  constructor(position: number, id: string, value= '') {
+    super(position, id, 'Source Shards', 'List of source shards.', value);
+    this.setDisplayOn('factory_name', 'horizontal_resharding');
+  }
+} 
+
+export class HorizontalReshardVTWorkerFlag extends InputFlag {
+  constructor(position: number, id: string, value= '') {
+    super(position, id, 'VTWorker Server', 'server address of vtworker.', value);
+    this.setDisplayOn('factory_name', 'horizontal_resharding');
+  }
+} 
 
 // WorkflowFlags is used by the Start / Stop / Delete dialogs.
 export class WorkflowFlags {
