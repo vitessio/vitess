03e78c914185e8c238a2d744f6ee4b51d2bc3ffa
Yipei Wang <yipeiw@google.com>
2016-12-22 16:31:01 -0800

    workflow: Add supports for multiple source shards for horizontal resharding workflow. Create Tree Structure UI to report the progress. Tested the happy path by end-to-end test where single source shard is horizontally resharded.

---
+++ test/horizontal_resharding_workflow.py
@@ -0,0 +1,101 @@
+# Copyright 2016 Google Inc. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can
+# be found in the LICENSE file.
+"""End-to-end test for horizontal resharding workflow."""
+
+# pylint: disable=unused-import
+import re
+import unittest
+
+import utils
+import worker
+
+
+def setUpModule():
+  try:
+    worker.setUpModule()
+    utils.Vtctld().start()
+  except:
+    tearDownModule()
+    raise
+
+
+def tearDownModule():
+  worker.tearDownModule()
+
+
+class TestHorizontalReshardingWorkflow(worker.TestBaseSplitClone):
+  """End-to-end test for horizontal resharding workflow.
+
+  This test reuses worker.py, which sets up the environment.
+  """
+  KEYSPACE = 'test_keyspace'
+
+  def test_successful_resharding(self):
+    """Normal horizontal resharding, "happy path test"."""
+    worker_proc, _, worker_rpc_port = utils.run_vtworker_bg(
+        ['--cell', 'test_nj'], auto_log=True)
+    vtworker_endpoint = 'localhost:' + str(worker_rpc_port)
+
+    self._error = None
+
+    vtctl_res = utils.run_vtctl(
+        [
+            'WorkflowCreate', 'horizontal_resharding',
+            '-keyspace=test_keyspace',
+            '-vtworkers=%s' % vtworker_endpoint
+        ],
+        auto_log=True)
+    hw_uuid = re.match(r'^uuid: (.*)$', vtctl_res[0]).group(1)
+
+    utils.pause('Now is a good time to look at vtctld UI at: '
+                '%s, workflow uuid=%s' % (utils.vtctld.port, hw_uuid))
+
+    utils.run_vtctl(['WorkflowWait', hw_uuid])
+
+    self.verify()
+    utils.kill_sub_process(worker_proc, soft=True)
+
+  def verify(self):
+    self.assert_shard_data_equal(0, worker.shard_master,
+                                 worker.shard_0_tablets.replica)
+    self.assert_shard_data_equal(1, worker.shard_master,
+                                 worker.shard_1_tablets.replica)
+
+    # Verify effect of MigrateServedTypes. Dest shards are serving now.
+    utils.check_srv_keyspace('test_nj', self.KEYSPACE,
+                             'Partitions(master): -80 80-\n'
+                             'Partitions(rdonly): -80 80-\n'
+                             'Partitions(replica): -80 80-\n')
+    
+    all_tablets = [worker.shard_rdonly1.tablet_alias, 
+                   worker.shard_0_rdonly1.tablet_alias, 
+                   worker.shard_1_rdonly1.tablet_alias]
+    for tablet_alias in all_tablets:
+      utils.run_vtctl(['RunHealthCheck', tablet_alias], auto_log=True)
+
+    # source shard: query service must be disabled after MigrateServedTypes.
+    source_shards = [worker.shard_rdonly1, 
+                     worker.shard_replica, 
+                     worker.shard_master]
+    for shard in source_shards:
+      utils.check_tablet_query_service(
+        self, shard, serving=False, tablet_control_disabled=True)
+
+    # dest shard -80, 80-: query service must be disabled after MigrateServedTypes.
+    # Run explicit healthcheck because 'rdonly' tablet may still be 'spare'.
+    dest_shards = [worker.shard_0_rdonly1, 
+                   worker.shard_0_replica, 
+                   worker.shard_0_master,
+                   worker.shard_1_rdonly1, 
+                   worker.shard_1_replica, 
+                   worker.shard_1_master]
+    for shard in dest_shards:
+      utils.check_tablet_query_service(
+        self, 
+        shard, 
+        serving=True, 
+        tablet_control_disabled=False)
+
+if __name__ == '__main__':
+  utils.main(test_options=worker.add_test_options)
