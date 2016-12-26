fc05887af8819011a7a0b6115395a572e55dbaff
Harshit Gangal <harshit.gangal@flipkart.com>
2016-12-26 23:45:15 +0530

    added proto conversion test

--- go/sqltypes/proto3_test.go
+++ go/sqltypes/proto3_test.go
@@ -176,3 +176,117 @@ func TestResults(t *testing.T) {
 		t.Errorf("reverse:\n%#v, want\n%#v", reverse, sqlResults)
 	}
 }
+
+func TestQueryReponses(t *testing.T) {
+	fields1 := []*querypb.Field{{
+		Name: "col1",
+		Type: VarChar,
+	}, {
+		Name: "col2",
+		Type: Int64,
+	}, {
+		Name: "col3",
+		Type: Float64,
+	}}
+	fields2 := []*querypb.Field{{
+		Name: "col11",
+		Type: VarChar,
+	}, {
+		Name: "col12",
+		Type: Int64,
+	}, {
+		Name: "col13",
+		Type: Float64,
+	}}
+
+	queryResponses := []QueryResponse{
+		{
+			QueryResult: &Result{
+				Fields:       fields1,
+				InsertID:     1,
+				RowsAffected: 2,
+				Rows: [][]Value{{
+					testVal(VarChar, "aa"),
+					testVal(Int64, "1"),
+					testVal(Float64, "2"),
+				}},
+				Extras: &querypb.ResultExtras{
+					EventToken: &querypb.EventToken{
+						Timestamp: 123,
+						Shard:     "shard0",
+						Position:  "position0",
+					},
+				},
+			},
+			QueryError: nil,
+		}, {
+			QueryResult: &Result{
+				Fields:       fields2,
+				InsertID:     3,
+				RowsAffected: 4,
+				Rows: [][]Value{{
+					testVal(VarChar, "bb"),
+					testVal(Int64, "3"),
+					testVal(Float64, "4"),
+				}},
+				Extras: &querypb.ResultExtras{
+					EventToken: &querypb.EventToken{
+						Timestamp: 123,
+						Shard:     "shard1",
+						Position:  "position1",
+					},
+				},
+			},
+			QueryError: nil,
+		},
+	}
+
+	p3ResultWithError := []*querypb.ResultWithError{
+		{
+			Error: nil,
+			Result: &querypb.QueryResult{
+				Fields:       fields1,
+				InsertId:     1,
+				RowsAffected: 2,
+				Rows: []*querypb.Row{{
+					Lengths: []int64{2, 1, 1},
+					Values:  []byte("aa12"),
+				}},
+				Extras: &querypb.ResultExtras{
+					EventToken: &querypb.EventToken{
+						Timestamp: 123,
+						Shard:     "shard0",
+						Position:  "position0",
+					},
+				},
+			},
+		}, {
+			Error: nil,
+			Result: &querypb.QueryResult{
+				Fields:       fields2,
+				InsertId:     3,
+				RowsAffected: 4,
+				Rows: []*querypb.Row{{
+					Lengths: []int64{2, 1, 1},
+					Values:  []byte("bb34"),
+				}},
+				Extras: &querypb.ResultExtras{
+					EventToken: &querypb.EventToken{
+						Timestamp: 123,
+						Shard:     "shard1",
+						Position:  "position1",
+					},
+				},
+			},
+		},
+	}
+	p3converted := QueryResponsesToProto3(queryResponses)
+	if !reflect.DeepEqual(p3converted, p3ResultWithError) {
+		t.Errorf("P3:\n%v, want\n%v", p3converted, p3ResultWithError)
+	}
+
+	reverse := Proto3ToQueryReponses(p3ResultWithError)
+	if !reflect.DeepEqual(reverse, queryResponses) {
+		t.Errorf("reverse:\n%#v, want\n%#v", reverse, queryResponses)
+	}
+}
