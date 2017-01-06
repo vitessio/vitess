vitessio/vitess

commit 0d0c736fc14273cca4a0929f2c5adce3da5ca3b5

MultiRow Insert Optimization (#2422)
* MultiRow Insert Optimization

* Rewriting lkp.ver query using and/or clauses

* Rewriting lkp.ver query using and/or clauses

*  Incorporating comments and Adding tests to improve the code-coverage

*  Fixing Lint Errors

go/vt/vtgate/vindexes/numeric_static_map_test.go
@@ -8,33 +8,33 @@ import (
 	"reflect"
 	"testing"
 
+	"strings"
+
 	"github.com/youtube/vitess/go/sqltypes"
 	"github.com/youtube/vitess/go/testfiles"
 )
 
-func createVindex() (Vindex, error) {
+var numericStaticMap Vindex
+
+func init() {
 	m := make(map[string]string)
 	m["json_path"] = testfiles.Locate("vtgate/numeric_static_map_test.json")
-	return CreateVindex("numeric_static_map", "numericStaticMap", m)
+	numericStaticMap, _ = CreateVindex("numeric_static_map", "numericStaticMap", m)
 }
 
 func TestNumericStaticMapCost(t *testing.T) {
-	numericStaticMap, err := createVindex()
-	if err != nil {
-		t.Fatalf("failed to create vindex: %v", err)
-	}
-
 	if numericStaticMap.Cost() != 1 {
 		t.Errorf("Cost(): %d, want 1", numericStaticMap.Cost())
 	}
 }
 
-func TestNumericStaticMapMap(t *testing.T) {
-	numericStaticMap, err := createVindex()
-	if err != nil {
-		t.Fatalf("failed to create vindex: %v", err)
+func TestNumericStaticMapString(t *testing.T) {
+	if strings.Compare("numericStaticMap", numericStaticMap.String()) != 0 {
+		t.Errorf("String(): %s, want num", numericStaticMap.String())
 	}
+}
 
+func TestNumericStaticMapMap(t *testing.T) {
 	sqlVal, _ := sqltypes.BuildIntegral("8")
 	got, err := numericStaticMap.(Unique).Map(nil, []interface{}{
 		1,
@@ -68,12 +68,7 @@ func TestNumericStaticMapMap(t *testing.T) {
 }
 
 func TestNumericStaticMapMapBadData(t *testing.T) {
-	numericStaticMap, err := createVindex()
-	if err != nil {
-		t.Fatalf("failed to create vindex: %v", err)
-	}
-
-	_, err = numericStaticMap.(Unique).Map(nil, []interface{}{1.1})
+	_, err := numericStaticMap.(Unique).Map(nil, []interface{}{1.1})
 	want := `NumericStaticMap.Map: getNumber: unexpected type for 1.1: float64`
 	if err == nil || err.Error() != want {
 		t.Errorf("NumericStaticMap.Map: %v, want %v", err, want)
@@ -81,12 +76,7 @@ func TestNumericStaticMapMapBadData(t *testing.T) {
 }
 
 func TestNumericStaticMapVerify(t *testing.T) {
-	numericStaticMap, err := createVindex()
-	if err != nil {
-		t.Fatalf("failed to create vindex: %v", err)
-	}
-
-	success, err := numericStaticMap.Verify(nil, 1, []byte("\x00\x00\x00\x00\x00\x00\x00\x01"))
+	success, err := numericStaticMap.Verify(nil, []interface{}{1}, [][]byte{[]byte("\x00\x00\x00\x00\x00\x00\x00\x01")})
 	if err != nil {
 		t.Error(err)
 	}
@@ -95,15 +85,25 @@ func TestNumericStaticMapVerify(t *testing.T) {
 	}
 }
 
-func TestNumericStaticMapVerifyBadData(t *testing.T) {
-	numericStaticMap, err := createVindex()
-	if err != nil {
-		t.Fatalf("failed to create vindex: %v", err)
+func TestNumericStaticMapVerifyNeg(t *testing.T) {
+	_, err := numericStaticMap.Verify(nil, []interface{}{1, 2}, [][]byte{[]byte("\x16k@\xb4J\xbaK\xd6")})
+	want := "NumericStaticMap.Verify: length of ids 2 doesn't match length of ksids 1"
+	if err.Error() != want {
+		t.Error(err.Error())
 	}
 
-	_, err = numericStaticMap.Verify(nil, 1.1, []byte("\x00\x00\x00\x00\x00\x00\x00\x01"))
-	want := `NumericStaticMap.Verify: getNumber: unexpected type for 1.1: float64`
+	_, err = numericStaticMap.Verify(nil, []interface{}{1.1}, [][]byte{[]byte("\x00\x00\x00\x00\x00\x00\x00\x01")})
+	want = `NumericStaticMap.Verify: getNumber: unexpected type for 1.1: float64`
 	if err == nil || err.Error() != want {
 		t.Errorf("numericStaticMap.Map: %v, want %v", err, want)
 	}
+
+	success, err := numericStaticMap.Verify(nil, []interface{}{1}, [][]byte{[]byte("\x00\x00\x00\x00\x00\x00\x00\x02")})
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	if success {
+		t.Errorf("Numeric.Verify(): %+v, want false", success)
+	}
+
 }

go/vt/sqlparser/tracked_buffer.go
@@ -30,7 +30,7 @@ func NewTrackedBuffer(nodeFormatter func(buf *TrackedBuffer, node SQLNode)) *Tra
 	}
 }
 
-// Convenience function, initiates the writing of a single SQLNode tree by passing
+// WriteNode function, initiates the writing of a single SQLNode tree by passing
 // through to Myprintf with a default format string
 func (buf *TrackedBuffer) WriteNode(node SQLNode) *TrackedBuffer {
 	buf.Myprintf("%v", node)

go/vt/vtgate/engine/route.go
@@ -8,7 +8,6 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"encoding/hex"
 	"strconv"
 	"strings"
 
@@ -492,25 +491,29 @@ func (route *Route) getInsertShardedRoute(vcursor VCursor, queryConstruct *query
 	}
 
 	inputs := route.Values.([]interface{})
-	for rowNum, input := range inputs {
+	allKeys := make([][]interface{}, len(inputs[0].([]interface{})))
+	for _, input := range inputs {
 		keys, err := route.resolveKeys(input.([]interface{}), queryConstruct.BindVars)
 		if err != nil {
 			return "", nil, fmt.Errorf("getInsertShardedRoute: %v", err)
 		}
 		for colNum := 0; colNum < len(keys); colNum++ {
-			if colNum == 0 {
-				ksid, err := route.handlePrimary(vcursor, queryConstruct, keys[colNum], route.Table.ColumnVindexes[colNum], queryConstruct.BindVars, rowNum)
-				if err != nil {
-					return "", nil, fmt.Errorf("getInsertShardedRoute: %v", err)
-				}
-				keyspaceIDs = append(keyspaceIDs, ksid)
-			} else {
-				err := route.handleNonPrimary(vcursor, queryConstruct, keys[colNum], route.Table.ColumnVindexes[colNum], queryConstruct.BindVars, keyspaceIDs[rowNum], rowNum)
-				if err != nil {
-					return "", nil, fmt.Errorf("getInsertShardedRoute: %v", err)
-				}
-			}
+			allKeys[colNum] = append(allKeys[colNum], keys[colNum])
 		}
+	}
+
+	keyspaceIDs, err = route.handlePrimary(vcursor, allKeys[0], route.Table.ColumnVindexes[0], queryConstruct.BindVars)
+	if err != nil {
+		return "", nil, fmt.Errorf("getInsertShardedRoute: %v", err)
+	}
+
+	for colNum := 1; colNum < len(allKeys); colNum++ {
+		err := route.handleNonPrimary(vcursor, allKeys[colNum], route.Table.ColumnVindexes[colNum], queryConstruct.BindVars, keyspaceIDs)
+		if err != nil {
+			return "", nil, fmt.Errorf("getInsertShardedRoute: %v", err)
+		}
+	}
+	for rowNum := range keyspaceIDs {
 		shard, err := vcursor.GetShardForKeyspaceID(allShards, keyspaceIDs[rowNum])
 		routing[shard] = append(routing[shard], route.Mid[rowNum])
 		if err != nil {
@@ -744,57 +747,77 @@ func (route *Route) handleGenerate(vcursor VCursor, queryConstruct *queryinfo.Qu
 	return insertid, nil
 }
 
-func (route *Route) handlePrimary(vcursor VCursor, queryConstruct *queryinfo.QueryConstruct, vindexKey interface{}, colVindex *vindexes.ColumnVindex, bv map[string]interface{}, rowNum int) (ksid []byte, err error) {
-	if vindexKey == nil {
-		return nil, fmt.Errorf("value must be supplied for column %v", colVindex.Column)
+func (route *Route) handlePrimary(vcursor VCursor, vindexKeys []interface{}, colVindex *vindexes.ColumnVindex, bv map[string]interface{}) (keyspaceIDs [][]byte, err error) {
+	for _, vindexkey := range vindexKeys {
+		if vindexkey == nil {
+			return nil, fmt.Errorf("value must be supplied for column %v", colVindex.Column)
+		}
 	}
 	mapper := colVindex.Vindex.(vindexes.Unique)
-	ksids, err := mapper.Map(vcursor, []interface{}{vindexKey})
+	keyspaceIDs, err = mapper.Map(vcursor, vindexKeys)
 	if err != nil {
 		return nil, err
 	}
-	ksid = ksids[0]
-	if len(ksid) == 0 {
-		return nil, fmt.Errorf("could not map %v to a keyspace id", vindexKey)
+	if len(keyspaceIDs) != len(vindexKeys) {
+		return nil, fmt.Errorf("could not map %v to a keyspaceids", vindexKeys)
 	}
-	bv["_"+colVindex.Column.CompliantName()+strconv.Itoa(rowNum)] = vindexKey
-	return ksid, nil
+	for rowNum, vindexKey := range vindexKeys {
+		if len(keyspaceIDs[rowNum]) == 0 {
+			return nil, fmt.Errorf("could not map %v to a keyspace id", vindexKey)
+		}
+		bv["_"+colVindex.Column.CompliantName()+strconv.Itoa(rowNum)] = vindexKey
+	}
+	return keyspaceIDs, nil
 }
 
-func (route *Route) handleNonPrimary(vcursor VCursor, queryConstruct *queryinfo.QueryConstruct, vindexKey interface{}, colVindex *vindexes.ColumnVindex, bv map[string]interface{}, ksid []byte, rowNum int) error {
+func (route *Route) handleNonPrimary(vcursor VCursor, vindexKeys []interface{}, colVindex *vindexes.ColumnVindex, bv map[string]interface{}, ksids [][]byte) error {
 	if colVindex.Owned {
-		if vindexKey == nil {
-			return fmt.Errorf("value must be supplied for column %v", colVindex.Column)
+		for rowNum, vindexKey := range vindexKeys {
+			if vindexKey == nil {
+				return fmt.Errorf("value must be supplied for column %v", colVindex.Column)
+			}
+			bv["_"+colVindex.Column.CompliantName()+strconv.Itoa(rowNum)] = vindexKey
 		}
-		err := colVindex.Vindex.(vindexes.Lookup).Create(vcursor, vindexKey, ksid)
+		err := colVindex.Vindex.(vindexes.Lookup).Create(vcursor, vindexKeys, ksids)
 		if err != nil {
 			return err
 		}
 	} else {
-		if vindexKey == nil {
+		var reverseKsids [][]byte
+		var verifyKsids [][]byte
+		for rowNum, vindexKey := range vindexKeys {
+			if vindexKey == nil {
+				reverseKsids = append(reverseKsids, ksids[rowNum])
+			} else {
+				verifyKsids = append(verifyKsids, ksids[rowNum])
+			}
+			bv["_"+colVindex.Column.CompliantName()+strconv.Itoa(rowNum)] = vindexKey
+		}
+		var err error
+		if reverseKsids != nil {
 			reversible, ok := colVindex.Vindex.(vindexes.Reversible)
 			if !ok {
 				return fmt.Errorf("value must be supplied for column %v", colVindex.Column)
 			}
-			var err error
-			vindexKey, err = reversible.ReverseMap(vcursor, ksid)
+			vindexKeys, err = reversible.ReverseMap(vcursor, reverseKsids)
 			if err != nil {
 				return err
 			}
-			if vindexKey == nil {
-				return fmt.Errorf("could not compute value for column %v", colVindex.Column)
+			for rowNum, vindexKey := range vindexKeys {
+				bv["_"+colVindex.Column.CompliantName()+strconv.Itoa(rowNum)] = vindexKey
 			}
-		} else {
-			ok, err := colVindex.Vindex.Verify(vcursor, vindexKey, ksid)
+		}
+
+		if verifyKsids != nil {
+			ok, err := colVindex.Vindex.Verify(vcursor, vindexKeys, verifyKsids)
 			if err != nil {
 				return err
 			}
 			if !ok {
-				return fmt.Errorf("value %v for column %v does not map to keyspace id %v", vindexKey, colVindex.Column, hex.EncodeToString(ksid))
+				return fmt.Errorf("values %v for column %v does not map to keyspaceids", vindexKeys, colVindex.Column)
 			}
 		}
 	}
-	bv["_"+colVindex.Column.CompliantName()+strconv.Itoa(rowNum)] = vindexKey
 	return nil
 }
 

go/vt/vtgate/planbuilder/plan_test.go
@@ -25,11 +25,11 @@ type hashIndex struct{ name string }
 
 func (v *hashIndex) String() string { return v.name }
 func (*hashIndex) Cost() int        { return 1 }
-func (*hashIndex) Verify(vindexes.VCursor, interface{}, []byte) (bool, error) {
+func (*hashIndex) Verify(vindexes.VCursor, []interface{}, [][]byte) (bool, error) {
 	return false, nil
 }
 func (*hashIndex) Map(vindexes.VCursor, []interface{}) ([][]byte, error) { return nil, nil }
-func (*hashIndex) Create(vindexes.VCursor, interface{}) error            { return nil }
+func (*hashIndex) Create(vindexes.VCursor, []interface{}) error          { return nil }
 func (*hashIndex) Delete(vindexes.VCursor, []interface{}, []byte) error  { return nil }
 
 func newHashIndex(name string, _ map[string]string) (vindexes.Vindex, error) {
@@ -41,12 +41,12 @@ type lookupIndex struct{ name string }
 
 func (v *lookupIndex) String() string { return v.name }
 func (*lookupIndex) Cost() int        { return 2 }
-func (*lookupIndex) Verify(vindexes.VCursor, interface{}, []byte) (bool, error) {
+func (*lookupIndex) Verify(vindexes.VCursor, []interface{}, [][]byte) (bool, error) {
 	return false, nil
 }
-func (*lookupIndex) Map(vindexes.VCursor, []interface{}) ([][]byte, error) { return nil, nil }
-func (*lookupIndex) Create(vindexes.VCursor, interface{}, []byte) error    { return nil }
-func (*lookupIndex) Delete(vindexes.VCursor, []interface{}, []byte) error  { return nil }
+func (*lookupIndex) Map(vindexes.VCursor, []interface{}) ([][]byte, error)  { return nil, nil }
+func (*lookupIndex) Create(vindexes.VCursor, []interface{}, [][]byte) error { return nil }
+func (*lookupIndex) Delete(vindexes.VCursor, []interface{}, []byte) error   { return nil }
 
 func newLookupIndex(name string, _ map[string]string) (vindexes.Vindex, error) {
 	return &lookupIndex{name: name}, nil
@@ -57,11 +57,11 @@ type multiIndex struct{ name string }
 
 func (v *multiIndex) String() string { return v.name }
 func (*multiIndex) Cost() int        { return 3 }
-func (*multiIndex) Verify(vindexes.VCursor, interface{}, []byte) (bool, error) {
+func (*multiIndex) Verify(vindexes.VCursor, []interface{}, [][]byte) (bool, error) {
 	return false, nil
 }
 func (*multiIndex) Map(vindexes.VCursor, []interface{}) ([][][]byte, error) { return nil, nil }
-func (*multiIndex) Create(vindexes.VCursor, interface{}, []byte) error      { return nil }
+func (*multiIndex) Create(vindexes.VCursor, []interface{}, [][]byte) error  { return nil }
 func (*multiIndex) Delete(vindexes.VCursor, []interface{}, []byte) error    { return nil }
 
 func newMultiIndex(name string, _ map[string]string) (vindexes.Vindex, error) {
@@ -73,11 +73,11 @@ type costlyIndex struct{ name string }
 
 func (v *costlyIndex) String() string { return v.name }
 func (*costlyIndex) Cost() int        { return 10 }
-func (*costlyIndex) Verify(vindexes.VCursor, interface{}, []byte) (bool, error) {
+func (*costlyIndex) Verify(vindexes.VCursor, []interface{}, [][]byte) (bool, error) {
 	return false, nil
 }
 func (*costlyIndex) Map(vindexes.VCursor, []interface{}) ([][][]byte, error) { return nil, nil }
-func (*costlyIndex) Create(vindexes.VCursor, interface{}, []byte) error      { return nil }
+func (*costlyIndex) Create(vindexes.VCursor, []interface{}, [][]byte) error  { return nil }
 func (*costlyIndex) Delete(vindexes.VCursor, []interface{}, []byte) error    { return nil }
 
 func newCostlyIndex(name string, _ map[string]string) (vindexes.Vindex, error) {

go/vt/vtgate/router_dml_test.go
@@ -313,10 +313,10 @@ func TestInsertSharded(t *testing.T) {
 		t.Errorf("sbc2.Queries: %+v, want nil\n", sbc2.Queries)
 	}
 	wantQueries = []querytypes.BoundQuery{{
-		Sql: "insert into name_user_map(name, user_id) values (:name, :user_id)",
+		Sql: "insert into name_user_map(name, user_id) values (:name0, :user_id0)",
 		BindVariables: map[string]interface{}{
-			"name":    []byte("myname"),
-			"user_id": int64(1),
+			"name0":    []byte("myname"),
+			"user_id0": int64(1),
 		},
 	}}
 	if !reflect.DeepEqual(sbclookup.Queries, wantQueries) {
@@ -344,10 +344,10 @@ func TestInsertSharded(t *testing.T) {
 		t.Errorf("sbc1.Queries: %+v, want nil\n", sbc1.Queries)
 	}
 	wantQueries = []querytypes.BoundQuery{{
-		Sql: "insert into name_user_map(name, user_id) values (:name, :user_id)",
+		Sql: "insert into name_user_map(name, user_id) values (:name0, :user_id0)",
 		BindVariables: map[string]interface{}{
-			"name":    []byte("myname2"),
-			"user_id": int64(3),
+			"name0":    []byte("myname2"),
+			"user_id0": int64(3),
 		},
 	}}
 	if !reflect.DeepEqual(sbclookup.Queries, wantQueries) {
@@ -377,10 +377,10 @@ func TestInsertComments(t *testing.T) {
 		t.Errorf("sbc2.Queries: %+v, want nil\n", sbc2.Queries)
 	}
 	wantQueries = []querytypes.BoundQuery{{
-		Sql: "insert into name_user_map(name, user_id) values (:name, :user_id)",
+		Sql: "insert into name_user_map(name, user_id) values (:name0, :user_id0)",
 		BindVariables: map[string]interface{}{
-			"name":    []byte("myname"),
-			"user_id": int64(1),
+			"name0":    []byte("myname"),
+			"user_id0": int64(1),
 		},
 	}}
 	if !reflect.DeepEqual(sbclookup.Queries, wantQueries) {
@@ -417,10 +417,10 @@ func TestInsertGeneratorSharded(t *testing.T) {
 		Sql:           "select next :n values from user_seq",
 		BindVariables: map[string]interface{}{"n": int64(1)},
 	}, {
-		Sql: "insert into name_user_map(name, user_id) values (:name, :user_id)",
+		Sql: "insert into name_user_map(name, user_id) values (:name0, :user_id0)",
 		BindVariables: map[string]interface{}{
-			"name":    []byte("myname"),
-			"user_id": int64(1),
+			"name0":    []byte("myname"),
+			"user_id0": int64(1),
 		},
 	}}
 	if !reflect.DeepEqual(sbclookup.Queries, wantQueries) {
@@ -477,10 +477,10 @@ func TestInsertLookupOwned(t *testing.T) {
 		t.Errorf("sbc.Queries:\n%+v, want\n%+v\n", sbc.Queries, wantQueries)
 	}
 	wantQueries = []querytypes.BoundQuery{{
-		Sql: "insert into music_user_map(music_id, user_id) values (:music_id, :user_id)",
+		Sql: "insert into music_user_map(music_id, user_id) values (:music_id0, :user_id0)",
 		BindVariables: map[string]interface{}{
-			"music_id": int64(3),
-			"user_id":  int64(2),
+			"music_id0": int64(3),
+			"user_id0":  int64(2),
 		},
 	}}
 	if !reflect.DeepEqual(sbclookup.Queries, wantQueries) {
@@ -517,10 +517,10 @@ func TestInsertLookupOwnedGenerator(t *testing.T) {
 		Sql:           "select next :n values from user_seq",
 		BindVariables: map[string]interface{}{"n": int64(1)},
 	}, {
-		Sql: "insert into music_user_map(music_id, user_id) values (:music_id, :user_id)",
+		Sql: "insert into music_user_map(music_id, user_id) values (:music_id0, :user_id0)",
 		BindVariables: map[string]interface{}{
-			"music_id": int64(4),
-			"user_id":  int64(2),
+			"music_id0": int64(4),
+			"user_id0":  int64(2),
 		},
 	}}
 	if !reflect.DeepEqual(sbclookup.Queries, wantQueries) {
@@ -551,10 +551,10 @@ func TestInsertLookupUnowned(t *testing.T) {
 		t.Errorf("sbc.Queries: %+v, want %+v\n", sbc.Queries, wantQueries)
 	}
 	wantQueries = []querytypes.BoundQuery{{
-		Sql: "select music_id from music_user_map where music_id = :music_id and user_id = :user_id",
+		Sql: "select music_id from music_user_map where ((music_id = :music_id0 and user_id = :user_id0))",
 		BindVariables: map[string]interface{}{
-			"music_id": int64(3),
-			"user_id":  int64(2),
+			"music_id0": int64(3),
+			"user_id0":  int64(2),
 		},
 	}}
 	if !reflect.DeepEqual(sbclookup.Queries, wantQueries) {
@@ -681,7 +681,7 @@ func TestInsertFail(t *testing.T) {
 	}
 
 	_, err = routerExec(router, "insert into music_extra_reversed(music_id, user_id) values (1, 3)", nil)
-	want = "execInsertSharded: getInsertShardedRoute: value 3 for column user_id does not map to keyspace id 166b40b44aba4bd6"
+	want = "execInsertSharded: getInsertShardedRoute: values [3] for column user_id does not map to keyspaceids"
 	if err == nil || err.Error() != want {
 		t.Errorf("routerExec: %v, want %v", err, want)
 	}
@@ -774,16 +774,12 @@ func TestMultiInsertSharded(t *testing.T) {
 	}
 
 	wantQueries1 = []querytypes.BoundQuery{{
-		Sql: "insert into name_user_map(name, user_id) values (:name, :user_id)",
+		Sql: "insert into name_user_map(name, user_id) values (:name0, :user_id0), (:name1, :user_id1)",
 		BindVariables: map[string]interface{}{
-			"name":    []byte("myname1"),
-			"user_id": int64(1),
-		},
-	}, {
-		Sql: "insert into name_user_map(name, user_id) values (:name, :user_id)",
-		BindVariables: map[string]interface{}{
-			"name":    []byte("myname3"),
-			"user_id": int64(3),
+			"name0":    []byte("myname1"),
+			"user_id0": int64(1),
+			"name1":    []byte("myname3"),
+			"user_id1": int64(3),
 		},
 	}}
 	if !reflect.DeepEqual(sbclookup.Queries, wantQueries1) {
@@ -816,16 +812,12 @@ func TestMultiInsertSharded(t *testing.T) {
 		t.Errorf("sbc2.Queries: %+v, want nil\n", sbc2.Queries)
 	}
 	wantQueries = []querytypes.BoundQuery{{
-		Sql: "insert into name_user_map(name, user_id) values (:name, :user_id)",
+		Sql: "insert into name_user_map(name, user_id) values (:name0, :user_id0), (:name1, :user_id1)",
 		BindVariables: map[string]interface{}{
-			"name":    []byte("myname1"),
-			"user_id": int64(1),
-		},
-	}, {
-		Sql: "insert into name_user_map(name, user_id) values (:name, :user_id)",
-		BindVariables: map[string]interface{}{
-			"name":    []byte("myname2"),
-			"user_id": int64(2),
+			"name0":    []byte("myname1"),
+			"user_id0": int64(1),
+			"name1":    []byte("myname2"),
+			"user_id1": int64(2),
 		},
 	}}
 	if !reflect.DeepEqual(sbclookup.Queries, wantQueries) {
@@ -866,16 +858,12 @@ func TestMultiInsertGenerator(t *testing.T) {
 		Sql:           "select next :n values from user_seq",
 		BindVariables: map[string]interface{}{"n": int64(2)},
 	}, {
-		Sql: "insert into music_user_map(music_id, user_id) values (:music_id, :user_id)",
+		Sql: "insert into music_user_map(music_id, user_id) values (:music_id0, :user_id0), (:music_id1, :user_id1)",
 		BindVariables: map[string]interface{}{
-			"user_id":  int64(2),
-			"music_id": int64(1),
-		},
-	}, {
-		Sql: "insert into music_user_map(music_id, user_id) values (:music_id, :user_id)",
-		BindVariables: map[string]interface{}{
-			"user_id":  int64(2),
-			"music_id": int64(2),
+			"user_id0":  int64(2),
+			"music_id0": int64(1),
+			"user_id1":  int64(2),
+			"music_id1": int64(2),
 		},
 	}}
 	if !reflect.DeepEqual(sbclookup.Queries, wantQueries) {
@@ -924,22 +912,14 @@ func TestMultiInsertGeneratorSparse(t *testing.T) {
 		Sql:           "select next :n values from user_seq",
 		BindVariables: map[string]interface{}{"n": int64(2)},
 	}, {
-		Sql: "insert into music_user_map(music_id, user_id) values (:music_id, :user_id)",
+		Sql: "insert into music_user_map(music_id, user_id) values (:music_id0, :user_id0), (:music_id1, :user_id1), (:music_id2, :user_id2)",
 		BindVariables: map[string]interface{}{
-			"user_id":  int64(2),
-			"music_id": int64(1),
-		},
-	}, {
-		Sql: "insert into music_user_map(music_id, user_id) values (:music_id, :user_id)",
-		BindVariables: map[string]interface{}{
-			"user_id":  int64(2),
-			"music_id": int64(2),
-		},
-	}, {
-		Sql: "insert into music_user_map(music_id, user_id) values (:music_id, :user_id)",
-		BindVariables: map[string]interface{}{
-			"user_id":  int64(2),
-			"music_id": int64(2),
+			"user_id0":  int64(2),
+			"music_id0": int64(1),
+			"user_id1":  int64(2),
+			"music_id1": int64(2),
+			"user_id2":  int64(2),
+			"music_id2": int64(2),
 		},
 	}}
 	if !reflect.DeepEqual(sbclookup.Queries, wantQueries) {

go/vt/vtgate/vindexes/binary.go
@@ -22,16 +22,24 @@ func (vind *Binary) String() string {
 
 // Cost returns the cost as 1.
 func (vind *Binary) Cost() int {
-	return 0
+	return 1
 }
 
-// Verify returns true if id maps to ksid.
-func (vind *Binary) Verify(_ VCursor, id interface{}, ksid []byte) (bool, error) {
-	data, err := getBytes(id)
-	if err != nil {
-		return false, fmt.Errorf("Binary.Verify: %v", err)
+// Verify returns true if ids maps to ksids.
+func (vind *Binary) Verify(_ VCursor, ids []interface{}, ksids [][]byte) (bool, error) {
+	if len(ids) != len(ksids) {
+		return false, fmt.Errorf("Binary.Verify: length of ids %v doesn't match length of ksids %v", len(ids), len(ksids))
 	}
-	return bytes.Compare(data, ksid) == 0, nil
+	for rowNum := range ids {
+		data, err := getBytes(ids[rowNum])
+		if err != nil {
+			return false, fmt.Errorf("Binary.Verify: %v", err)
+		}
+		if bytes.Compare(data, ksids[rowNum]) != 0 {
+			return false, nil
+		}
+	}
+	return true, nil
 }
 
 // Map returns the corresponding keyspace id values for the given ids.
@@ -47,12 +55,16 @@ func (vind *Binary) Map(_ VCursor, ids []interface{}) ([][]byte, error) {
 	return out, nil
 }
 
-// ReverseMap returns the associated id for the ksid.
-func (*Binary) ReverseMap(_ VCursor, ksid []byte) (interface{}, error) {
-	if ksid == nil {
-		return nil, fmt.Errorf("Binary.ReverseMap: is nil")
+// ReverseMap returns the associated ids for the ksids.
+func (*Binary) ReverseMap(_ VCursor, ksids [][]byte) ([]interface{}, error) {
+	var reverseIds = make([]interface{}, len(ksids))
+	for rownum, keyspaceID := range ksids {
+		if keyspaceID == nil {
+			return nil, fmt.Errorf("Binary.ReverseMap: keyspaceId is nil")
+		}
+		reverseIds[rownum] = []byte(keyspaceID)
 	}
-	return []byte(ksid), nil
+	return reverseIds, nil
 }
 
 func init() {

commit ca499fb6405480657f570159c59d80e2e432c85c

vtgate/vindexes: Do not run testfiles.Locate() from init() (2nd time).
Our internal implementation of Locate() does not support to be called from init().

Therefore, I've changed the test to create a new vindex in each test instance.

I've also added error checking for the creation of the vindex to make sure we don't swallow any errors.

I've fixed this code before in commit 1bcbff7f324bf86ff253292816b4efc1a7147bce but I forgot to add a proper warning in the code to make sure that it doesn't happen again. Therefore, the same issue was introduced in https://github.com/youtube/vitess/pull/2422. With the new warning, people should no longer eagerly revert it back to a global variable :)

go/vt/vtgate/vindexes/numeric_static_map_test.go
@@ -14,27 +14,43 @@ import (
 	"github.com/youtube/vitess/go/testfiles"
 )
 
-var numericStaticMap Vindex
-
-func init() {
+// createVindex creates the "numeric_static_map" vindex object which is used by
+// each test.
+//
+// IMPORTANT: This code is called per test and must not be called from init()
+// because our internal implementation of testfiles.Locate() does not support to
+// be called from init().
+func createVindex() (Vindex, error) {
 	m := make(map[string]string)
 	m["json_path"] = testfiles.Locate("vtgate/numeric_static_map_test.json")
-	numericStaticMap, _ = CreateVindex("numeric_static_map", "numericStaticMap", m)
+	return CreateVindex("numeric_static_map", "numericStaticMap", m)
 }
 
 func TestNumericStaticMapCost(t *testing.T) {
+	numericStaticMap, err := createVindex()
+	if err != nil {
+		t.Fatalf("failed to create vindex: %v", err)
+	}
 	if numericStaticMap.Cost() != 1 {
 		t.Errorf("Cost(): %d, want 1", numericStaticMap.Cost())
 	}
 }
 
 func TestNumericStaticMapString(t *testing.T) {
+	numericStaticMap, err := createVindex()
+	if err != nil {
+		t.Fatalf("failed to create vindex: %v", err)
+	}
 	if strings.Compare("numericStaticMap", numericStaticMap.String()) != 0 {
 		t.Errorf("String(): %s, want num", numericStaticMap.String())
 	}
 }
 
 func TestNumericStaticMapMap(t *testing.T) {
+	numericStaticMap, err := createVindex()
+	if err != nil {
+		t.Fatalf("failed to create vindex: %v", err)
+	}
 	sqlVal, _ := sqltypes.BuildIntegral("8")
 	got, err := numericStaticMap.(Unique).Map(nil, []interface{}{
 		1,
@@ -68,7 +84,11 @@ func TestNumericStaticMapMap(t *testing.T) {
 }
 
 func TestNumericStaticMapMapBadData(t *testing.T) {
-	_, err := numericStaticMap.(Unique).Map(nil, []interface{}{1.1})
+	numericStaticMap, err := createVindex()
+	if err != nil {
+		t.Fatalf("failed to create vindex: %v", err)
+	}
+	_, err = numericStaticMap.(Unique).Map(nil, []interface{}{1.1})
 	want := `NumericStaticMap.Map: getNumber: unexpected type for 1.1: float64`
 	if err == nil || err.Error() != want {
 		t.Errorf("NumericStaticMap.Map: %v, want %v", err, want)
@@ -76,6 +96,10 @@ func TestNumericStaticMapMapBadData(t *testing.T) {
 }
 
 func TestNumericStaticMapVerify(t *testing.T) {
+	numericStaticMap, err := createVindex()
+	if err != nil {
+		t.Fatalf("failed to create vindex: %v", err)
+	}
 	success, err := numericStaticMap.Verify(nil, []interface{}{1}, [][]byte{[]byte("\x00\x00\x00\x00\x00\x00\x00\x01")})
 	if err != nil {
 		t.Error(err)
@@ -86,7 +110,11 @@ func TestNumericStaticMapVerify(t *testing.T) {
 }
 
 func TestNumericStaticMapVerifyNeg(t *testing.T) {
-	_, err := numericStaticMap.Verify(nil, []interface{}{1, 2}, [][]byte{[]byte("\x16k@\xb4J\xbaK\xd6")})
+	numericStaticMap, err := createVindex()
+	if err != nil {
+		t.Fatalf("failed to create vindex: %v", err)
+	}
+	_, err = numericStaticMap.Verify(nil, []interface{}{1, 2}, [][]byte{[]byte("\x16k@\xb4J\xbaK\xd6")})
 	want := "NumericStaticMap.Verify: length of ids 2 doesn't match length of ksids 1"
 	if err.Error() != want {
 		t.Error(err.Error())