ea1db6eb7d4590b2428748e439ce873f45a5b6bb
Joshua Thompson <thompsonja@google.com>
2017-01-17 12:10:56 -0800

    Update repo in docker orchestrator build.sh file

--- docker/orchestrator/build.sh
+++ docker/orchestrator/build.sh
@@ -5,7 +5,7 @@ set -e
 tmpdir=`mktemp -d`
 
 script="go get github.com/youtube/vitess/go/cmd/vtctlclient && \
-  git clone https://github.com/enisoc/orchestrator.git src/github.com/github/orchestrator && \
+  git clone https://github.com/github/orchestrator.git src/github.com/github/orchestrator && \
   go install github.com/github/orchestrator/go/cmd/orchestrator"
 
 echo "Building orchestrator..."
