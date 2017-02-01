de1eda70c459978d9040168fb36a6fde210efc74
Jon Tirsen <jontirsen@squareup.com>
2017-02-01 14:09:41 +0100

    Implement setClob

--- java/jdbc/src/main/java/io/vitess/jdbc/VitessPreparedStatement.java
+++ java/jdbc/src/main/java/io/vitess/jdbc/VitessPreparedStatement.java
@@ -752,8 +752,14 @@ public class VitessPreparedStatement extends VitessStatement implements Prepared
     }
 
     public void setClob(int parameterIndex, Clob x) throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-            Constants.SQLExceptionMessages.SQL_FEATURE_NOT_SUPPORTED);
+        checkOpen();
+        if (x.length() > Integer.MAX_VALUE) {
+            throw new SQLFeatureNotSupportedException(
+                Constants.SQLExceptionMessages.SQL_FEATURE_NOT_SUPPORTED);
+        }
+        // Clob uses 1-based indexing!
+        this.bindVariables.put(Constants.LITERAL_V + parameterIndex,
+            x.getSubString(1, (int) x.length()));
     }
 
     public void setArray(int parameterIndex, Array x) throws SQLException {
