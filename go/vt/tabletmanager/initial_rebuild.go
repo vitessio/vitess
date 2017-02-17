2fb92cff0cd7da9fce3d36c823291eef547ef891
Bryan Beaudreault <bbeaudreault@hubspot.com>
2017-02-17 11:29:26 -0500

    rebuild vschema too

--- go/vt/tabletmanager/initial_rebuild.go
+++ go/vt/tabletmanager/initial_rebuild.go
@@ -33,5 +33,10 @@ func (agent *ActionAgent) maybeRebuildKeyspace(cell, keyspace string) {
 
 	if err := topotools.RebuildKeyspace(agent.batchCtx, logutil.NewConsoleLogger(), agent.TopoServer, keyspace, []string{cell}); err != nil {
 		log.Warningf("RebuildKeyspace(%v,%v) failed: %v, may need to run 'vtctl RebuildKeyspaceGraph %v')", cell, keyspace, err, keyspace)
+		return
+	}
+
+	if err := topotools.RebuildVSchema(agent.batchCtx, logutil.NewConsoleLogger(), agent.TopoServer, []string{cell}); err != nil {
+		log.Warningf("RebuildVSchema(%v) failed: %v, may need to run 'vtctl RebuildVSchemaGraph --cell %v", cell, err, cell)
 	}
 }
