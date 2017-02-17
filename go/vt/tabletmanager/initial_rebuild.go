15f8c81d42dc2c471497d85d1ee36cc5a5d35fc7
Bryan Beaudreault <bbeaudreault@hubspot.com>
2017-02-17 13:36:33 -0500

    typo

--- go/vt/tabletmanager/initial_rebuild.go
+++ go/vt/tabletmanager/initial_rebuild.go
@@ -37,6 +37,6 @@ func (agent *ActionAgent) maybeRebuildKeyspace(cell, keyspace string) {
 	}
 
 	if err := topotools.RebuildVSchema(agent.batchCtx, logutil.NewConsoleLogger(), agent.TopoServer, []string{cell}); err != nil {
-		log.Warningf("RebuildVSchema(%v) failed: %v, may need to run 'vtctl RebuildVSchemaGraph --cell %v", cell, err, cell)
+		log.Warningf("RebuildVSchema(%v) failed: %v, may need to run 'vtctl RebuildVSchemaGraph --cells %v", cell, err, cell)
 	}
 }
