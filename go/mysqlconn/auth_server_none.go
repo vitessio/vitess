66d470cd45141506ee8ce7813d6bff42c41d0cba
Alex Charis <acharis@hubspot.com>
2017-03-24 10:22:05 -0400

    fixup for auth_server_none pulled in from rebasing onto master

--- go/mysqlconn/auth_server_none.go
+++ go/mysqlconn/auth_server_none.go
@@ -1,5 +1,9 @@
 package mysqlconn
 
+import (
+	querypb "github.com/youtube/vitess/go/vt/proto/query"
+)
+
 // authServerNone accepts any username/password as valid.
 // It's meant to be used for testing and prototyping.
 // With this config, you can connect to a local vtgate using
@@ -15,14 +19,22 @@ func (a *authServerNone) Salt() ([]byte, error) {
 	return make([]byte, 20), nil
 }
 
-func (a *authServerNone) ValidateHash(salt []byte, user string, authResponse []byte) (string, error) {
-	return "", nil
+func (a *authServerNone) ValidateHash(salt []byte, user string, authResponse []byte) (Getter, error) {
+	return &NoneGetter{}, nil
 }
 
-func (a *authServerNone) ValidateClearText(user, password string) (string, error) {
+func (a *authServerNone) ValidateClearText(user, password string) (Getter, error) {
 	panic("unimplemented")
 }
 
 func init() {
 	RegisterAuthServerImpl("none", &authServerNone{})
+}
+
+// NoneGetter holds the empty string
+type NoneGetter struct{}
+
+// Get returns the empty string
+func (ng *NoneGetter) Get() *querypb.VTGateCallerID {
+	return &querypb.VTGateCallerID{Username: ""}
 }
