99e35fe41889ebfeec45a22ea639c8c904c7289e
Bryan Beaudreault <bbeaudreault@hubspot.com>
2017-05-04 14:40:01 -0400

    docs

--- java/grpc-client/src/main/java/io/vitess/client/grpc/RetryingInterceptorConfig.java
+++ java/grpc-client/src/main/java/io/vitess/client/grpc/RetryingInterceptorConfig.java
@@ -1,5 +1,9 @@
 package io.vitess.client.grpc;
 
+/**
+ * This class defines what level of exponential backoff to apply in the {@link RetryingInterceptor}. It can be
+ * disabled with the {@link #noopConfig()}.
+ */
 public class RetryingInterceptorConfig {
 
   private static final long DISABLED = -1;
@@ -14,10 +18,22 @@ public class RetryingInterceptorConfig {
     this.backoffMultiplier = backoffMultiplier;
   }
 
+  /**
+   * Returns a no-op config which will not do any retries.
+   */
   public static RetryingInterceptorConfig noopConfig() {
     return new RetryingInterceptorConfig(DISABLED, DISABLED, 0);
   }
 
+  /**
+   * Returns an exponential config with the given values to determine how aggressive of a backoff is needed
+   * @param initialBackoffMillis
+   *    how long in millis to backoff off for the first attempt
+   * @param maxBackoffMillis
+   *    the maximum value the backoff time can grow to, at which point all future retries will backoff at this amount
+   * @param backoffMultiplier
+   *    how quickly the backoff time should grow
+   */
   public static RetryingInterceptorConfig exponentialConfig(long initialBackoffMillis, long maxBackoffMillis, double backoffMultiplier) {
     return new RetryingInterceptorConfig(initialBackoffMillis, maxBackoffMillis, backoffMultiplier);
   }
