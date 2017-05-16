a6e5e2743162eacaa6281acc6916394ba88936d6
Alain Jobart <alainjobart@google.com>
2017-05-16 10:29:11 -0700

    Reverting test to pre-fix version.
    StrayTransactions is actually 0 if no other test leaks them.

--- go/vt/vttablet/tabletserver/tx_pool_test.go
+++ go/vt/vttablet/tabletserver/tx_pool_test.go
@@ -433,8 +433,6 @@ func TestTxPoolExecFailDueToConnFail_Errno2013(t *testing.T) {
 }
 
 func TestTxPoolCloseKillsStrayTransactions(t *testing.T) {
-	before := tabletenv.InternalErrors.Counts()["StrayTransactions"]
-
 	db := fakesqldb.New(t)
 	defer db.Close()
 	db.AddQuery("begin", &sqltypes.Result{})
@@ -450,7 +448,7 @@ func TestTxPoolCloseKillsStrayTransactions(t *testing.T) {
 
 	// Close kills stray transaction.
 	txPool.Close()
-	if got, want := tabletenv.InternalErrors.Counts()["StrayTransactions"]-before, int64(1); got != want {
+	if got, want := tabletenv.InternalErrors.Counts()["StrayTransactions"], int64(1); got != want {
 		t.Fatalf("internal error count for stray transactions not increased: got = %v, want = %v", got, want)
 	}
 	if got, want := txPool.conns.Capacity(), int64(0); got != want {
