b1c1ba0606cafecc0be6c00b79aed8cc05152695
Alex Charis <acharis@hubspot.com>
2017-05-18 19:45:05 -0400

    fix copy/paste errors, making comment accurate

--- go/vt/vtgate/grpcvtgateservice/no_test.go
+++ go/vt/vtgate/grpcvtgateservice/no_test.go
@@ -20,11 +20,11 @@ import (
 	"testing"
 )
 
-// This package is mostly tested from VTGate. This place-holder test
+// This package is mostly tested from ../grpcvtgatesconn . This place-holder test
 // is to document this fact. In order to make sure all code paths are
-// covered, you need to run the coverage tool from the vtgate directory
+// covered, you need to run the coverage tool from the ../grpcvtgatesconn directory
 // using the following command:
-//	go test -coverprofile=c.out -coverpkg ./engine,.
+//	go test -coverprofile=c.out -coverpkg ../grpcvtgateservice,.
 // You can then follow it with:
 //	go tool cover -html=c.out -o=c.html
 // to see the html output.
