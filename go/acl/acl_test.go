c845a2255517f96a6e0aaf447e74850333cbe310
Yang Luo <hsluoyz@qq.com>
2017-07-02 18:38:44 +0800

    Support Casbin as the authorization backend.

--- go/acl/acl_test.go
+++ go/acl/acl_test.go
@@ -17,70 +17,118 @@ limitations under the License.
 package acl
 
 import (
-	"errors"
+	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-type TestPolicy struct{}
-
-func (tp TestPolicy) CheckAccessActor(actor, role string) error {
-	if role == ADMIN {
-		return errors.New("not allowed")
-	}
-	return nil
+type TestHandler struct {
 }
 
-func (tp TestPolicy) CheckAccessHTTP(req *http.Request, role string) error {
-	if role == ADMIN {
-		return errors.New("not allowed")
+func (th *TestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	role := GetUserName(r)
+	if err := CheckAccessHTTP(r, role); err != nil {
+		SendError(w, err)
+		return
 	}
-	return nil
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Access OK.\n")
 }
 
-func init() {
-	RegisterPolicy("test", TestPolicy{})
-}
+func testRequest(t *testing.T, th *TestHandler, role string, path string, method string, code int) {
+	r, _ := http.NewRequest(method, path, nil)
+	r.SetBasicAuth(role, "123")
+	w := httptest.NewRecorder()
+	th.ServeHTTP(w, r)
 
-func TestSimplePolicy(t *testing.T) {
-	currentPolicy = policies["test"]
-	err := CheckAccessActor("", ADMIN)
-	want := "not allowed"
-	if err == nil || err.Error() != want {
-		t.Errorf("got %v, want %s", err, want)
-	}
-	err = CheckAccessActor("", DEBUGGING)
-	if err != nil {
-		t.Errorf("got %v, want no error", err)
-	}
-
-	err = CheckAccessHTTP(nil, ADMIN)
-	if err == nil || err.Error() != want {
-		t.Errorf("got %v, want %s", err, want)
-	}
-	err = CheckAccessHTTP(nil, DEBUGGING)
-	if err != nil {
-		t.Errorf("got %v, want no error", err)
+	if w.Code != code {
+		t.Errorf("%s, %s, %s: %d, supposed to be %d", role, path, method, w.Code, code)
 	}
 }
 
-func TestEmptyPolicy(t *testing.T) {
-	currentPolicy = nil
-	err := CheckAccessActor("", ADMIN)
-	if err != nil {
-		t.Errorf("got %v, want no error", err)
-	}
-	err = CheckAccessActor("", DEBUGGING)
-	if err != nil {
-		t.Errorf("got %v, want no error", err)
-	}
+func initExamplePolicy() {
+	enforcer.EnableLog(true)
 
-	err = CheckAccessHTTP(nil, ADMIN)
-	if err != nil {
-		t.Errorf("got %v, want no error", err)
-	}
-	err = CheckAccessHTTP(nil, DEBUGGING)
-	if err != nil {
-		t.Errorf("got %v, want no error", err)
-	}
+	enforcer.AddPermissionForUser("alice", "/dataset1/*", "GET")
+	enforcer.AddPermissionForUser("alice", "/dataset1/resource1", "POST")
+
+	enforcer.AddPermissionForUser("bob", "/dataset2/resource1", "*")
+	enforcer.AddPermissionForUser("bob", "/dataset2/resource2", "GET")
+	enforcer.AddPermissionForUser("bob", "/dataset2/folder1/*", "POST")
+
+	enforcer.AddPermissionForUser("dataset1_admin", "/dataset1/*", "*")
+
+	enforcer.AddRoleForUser("cathy", "dataset1_admin")
+}
+
+func TestBasic(t *testing.T) {
+	th := &TestHandler{}
+
+	// Here we use HTTP basic authentication as the way to get the logged-in user name
+	// For simplicity, the credential is not verified, you should implement and use your own
+	// authentication before the authorization.
+	// In this example, we assume "alice:123" is a legal user.
+	initEnforcer()
+	initExamplePolicy()
+
+	testRequest(t, th, "alice", "/dataset1/resource1", "GET", 200)
+	testRequest(t, th, "alice", "/dataset1/resource1", "POST", 200)
+	testRequest(t, th, "alice", "/dataset1/resource2", "GET", 200)
+	testRequest(t, th, "alice", "/dataset1/resource2", "POST", 403)
+}
+
+func TestPathWildcard(t *testing.T) {
+	th := &TestHandler{}
+
+	// Here we use HTTP basic authentication as the way to get the logged-in user name
+	// For simplicity, the credential is not verified, you should implement and use your own
+	// authentication before the authorization.
+	// In this example, we assume "bob:123" is a legal user.
+	initEnforcer()
+	initExamplePolicy()
+
+	testRequest(t, th, "bob", "/dataset2/resource1", "GET", 200)
+	testRequest(t, th, "bob", "/dataset2/resource1", "POST", 200)
+	testRequest(t, th, "bob", "/dataset2/resource1", "DELETE", 200)
+	testRequest(t, th, "bob", "/dataset2/resource2", "GET", 200)
+	testRequest(t, th, "bob", "/dataset2/resource2", "POST", 403)
+	testRequest(t, th, "bob", "/dataset2/resource2", "DELETE", 403)
+
+	testRequest(t, th, "bob", "/dataset2/folder1/item1", "GET", 403)
+	testRequest(t, th, "bob", "/dataset2/folder1/item1", "POST", 200)
+	testRequest(t, th, "bob", "/dataset2/folder1/item1", "DELETE", 403)
+	testRequest(t, th, "bob", "/dataset2/folder1/item2", "GET", 403)
+	testRequest(t, th, "bob", "/dataset2/folder1/item2", "POST", 200)
+	testRequest(t, th, "bob", "/dataset2/folder1/item2", "DELETE", 403)
+}
+
+func TestRBAC(t *testing.T) {
+	th := &TestHandler{}
+
+	// Here we use HTTP basic authentication as the way to get the logged-in user name
+	// For simplicity, the credential is not verified, you should implement and use your own
+	// authentication before the authorization.
+	// In this example, we assume "cathy:123" is a legal user.
+	initEnforcer()
+	initExamplePolicy()
+
+	// cathy can access all /dataset1/* resources via all methods because it has the dataset1_admin role.
+	testRequest(t, th, "cathy", "/dataset1/item", "GET", 200)
+	testRequest(t, th, "cathy", "/dataset1/item", "POST", 200)
+	testRequest(t, th, "cathy", "/dataset1/item", "DELETE", 200)
+	testRequest(t, th, "cathy", "/dataset2/item", "GET", 403)
+	testRequest(t, th, "cathy", "/dataset2/item", "POST", 403)
+	testRequest(t, th, "cathy", "/dataset2/item", "DELETE", 403)
+
+	// delete all roles on user cathy, so cathy cannot access any resources now.
+	enforcer.DeleteRolesForUser("cathy")
+
+	testRequest(t, th, "cathy", "/dataset1/item", "GET", 403)
+	testRequest(t, th, "cathy", "/dataset1/item", "POST", 403)
+	testRequest(t, th, "cathy", "/dataset1/item", "DELETE", 403)
+	testRequest(t, th, "cathy", "/dataset2/item", "GET", 403)
+	testRequest(t, th, "cathy", "/dataset2/item", "POST", 403)
+	testRequest(t, th, "cathy", "/dataset2/item", "DELETE", 403)
 }
