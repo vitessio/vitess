a511eedd6aeca6b4b4c9f3df977fe20509769ae8
Alain Jobart <alainjobart@google.com>
2017-07-11 13:14:01 -0700

    Adding parameter to bind mysql port socket to.
    And using it in vtcombo local usages, so unit tests only bind to
    'localhost', and don't provide an open MySQL port with no user/password
    required.
    
    BUG=63407520

--- py/vttest/vt_processes.py
+++ py/vttest/vt_processes.py
@@ -1,11 +1,11 @@
 # Copyright 2017 Google Inc.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -171,7 +171,8 @@ class VtcomboProcess(VtProcess):
     self.vtcombo_mysql_port = environment.get_port('vtcombo_mysql_port')
     self.extraparams.extend(
         ['-mysql_auth_server_impl', 'none',
-         '-mysql_server_port', str(self.vtcombo_mysql_port)])
+         '-mysql_server_port', str(self.vtcombo_mysql_port),
+         '-mysql_server_bind_address', 'localhost'])
 
 
 vtcombo_process = None
