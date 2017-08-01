69ee888d477c003270d71f53d66c4c38045f38e1
Michael Demmer <mdemmer@slack-corp.com>
2017-08-01 15:16:31 -0700

    make grpc max message size options apply to the client as well
    For both vttablet and vtgate, add  options to the grpc.Dial call site
    that pass through the max message size option and thereby override
    the 4MB default.

---
+++ go/vt/servenv/grpcutils/options.go
@@ -0,0 +1,35 @@
+/*
+Copyright 2017 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreedto in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package grpcutils
+
+import (
+	"flag"
+)
+
+var (
+	defaultMaxMessageSize = 4 * 1024 * 1024
+	// MaxMessageSize is the maximum message size which the gRPC server will
+	// accept. Larger messages will be rejected.
+	MaxMessageSize = &defaultMaxMessageSize
+)
+
+// RegisterFlags registers the command line flags for common grpc options
+func RegisterFlags() {
+	// Note: We're using 4 MiB as default value because that's the default in the
+	// gRPC 1.0.0 Go server.
+	MaxMessageSize = flag.Int("grpc_max_message_size", defaultMaxMessageSize, "Maximum allowed RPC message size. Larger messages will be rejected by gRPC with the error 'exceeding the max size'.")
+}
