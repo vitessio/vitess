6a45ba8ddb09bf047f5d055e7967341be94e30f3
Michael Demmer <mdemmer@slack-corp.com>
2017-10-03 16:18:51 -0700

    add option to keep bind vars and full queries out of debug UIs
    Add a -redact-debug-ui-queries option to vttablet. When enabled, all
    the debug UIs that contain full sql queries are redacted either by
    completely stubbing out the UI or by removing the PII data.
    
    This includes a rework of how the consolidator debug UI works to move
    the http handling out of the base class and into the two places in
    the tabletserver where it is being used.

--- go/sync2/consolidator_test.go
+++ go/sync2/consolidator_test.go
@@ -16,15 +16,27 @@ limitations under the License.
 
 package sync2
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestConsolidator(t *testing.T) {
 	con := NewConsolidator()
 	sql := "select * from SomeTable"
 
+	want := []ConsolidatorCacheItem{}
+	if !reflect.DeepEqual(con.Items(), want) {
+		t.Fatalf("expected consolidator to have no items")
+	}
+
 	orig, added := con.Create(sql)
 	if !added {
 		t.Fatalf("expected consolidator to register a new entry")
+	}
+
+	if !reflect.DeepEqual(con.Items(), want) {
+		t.Fatalf("expected consolidator to still have no items")
 	}
 
 	dup, added := con.Create(sql)
@@ -46,10 +58,27 @@ func TestConsolidator(t *testing.T) {
 		t.Fatalf("failed to share the result")
 	}
 
+	want = []ConsolidatorCacheItem{{Query: sql, Count: 1}}
+	if !reflect.DeepEqual(con.Items(), want) {
+		t.Fatalf("expected consolidator to have one items %v", con.Items())
+	}
+
 	// Running the query again should add a new entry since the original
 	// query execution completed
-	_, added = con.Create(sql)
+	second, added := con.Create(sql)
 	if !added {
 		t.Fatalf("expected consolidator to register a new entry")
 	}
+
+	go func() {
+		second.Result = &result
+		second.Broadcast()
+	}()
+	dup.Wait()
+
+	want = []ConsolidatorCacheItem{{Query: sql, Count: 2}}
+	if !reflect.DeepEqual(con.Items(), want) {
+		t.Fatalf("expected consolidator to have two items %v", con.Items())
+	}
+
 }
--- go/vt/vttablet/tabletserver/txlogz_test.go
+++ go/vt/vttablet/tabletserver/txlogz_test.go
@@ -28,6 +28,18 @@ import (
 	"github.com/youtube/vitess/go/vt/vttablet/tabletserver/tabletenv"
 )
 
+func testNotRedacted(t *testing.T, r *httptest.ResponseRecorder) {
+	if strings.Contains(r.Body.String(), "redacted") {
+		t.Errorf("/debug/txlogz unexpectedly redacted")
+	}
+}
+
+func testRedacted(t *testing.T, r *httptest.ResponseRecorder) {
+	if !strings.Contains(r.Body.String(), "redacted") {
+		t.Errorf("/debug/txlogz unexpectedly not redacted")
+	}
+}
+
 func testHandler(req *http.Request, t *testing.T) {
 	response := httptest.NewRecorder()
 	tabletenv.TxLogger.Send("test msg")
@@ -48,14 +60,21 @@ func testHandler(req *http.Request, t *testing.T) {
 	response = httptest.NewRecorder()
 	tabletenv.TxLogger.Send(txConn)
 	txlogzHandler(response, req)
+	testNotRedacted(t, response)
 	txConn.EndTime = txConn.StartTime.Add(time.Duration(2) * time.Second)
 	response = httptest.NewRecorder()
 	tabletenv.TxLogger.Send(txConn)
 	txlogzHandler(response, req)
+	testNotRedacted(t, response)
 	txConn.EndTime = txConn.StartTime.Add(time.Duration(500) * time.Millisecond)
 	response = httptest.NewRecorder()
 	tabletenv.TxLogger.Send(txConn)
 	txlogzHandler(response, req)
+	testNotRedacted(t, response)
+	*tabletenv.RedactDebugUIQueries = true
+	txlogzHandler(response, req)
+	testRedacted(t, response)
+	*tabletenv.RedactDebugUIQueries = false
 
 }
 
