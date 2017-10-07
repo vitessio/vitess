3ca80efd119859afa066c61aad779f301e742c44
Sugu Sougoumarane <ssougou@gmail.com>
2017-10-06 17:34:28 -0700

    messages: handle auto-inc columns
    BUG=67490100
    There are legitimate use cases for using auto_increment columns
    for id.
    
    In such situations, the insert code uses the last insert id returned
    by the insert to read back the inserted rows.
    
    It's possible for a rogue application to partially supply values
    for the auto-inc column. If so, we cannot predict all the values.
    In such cases, the code does the best it can.
    
    Tests have been written for these use cases.

--- go/vt/vttablet/tabletserver/query_executor.go
+++ go/vt/vttablet/tabletserver/query_executor.go
@@ -528,6 +528,26 @@ func (qre *QueryExecutor) execInsertMessage(conn *TxConnection) (*sqltypes.Resul
 		return nil, err
 	}
 
+	// If an auto-inc value was generated, it could be the id column.
+	// If so, we have to populate it.
+	if qr.InsertID != 0 {
+		id := int64(qr.InsertID)
+		idPKIndex := qre.plan.Table.MessageInfo.IDPKIndex
+		for _, row := range pkRows {
+			if !row[idPKIndex].IsNull() {
+				// If a value was supplied, either it was not the auto-inc column
+				// or values were partially supplied for an auto-inc column.
+				// If it's the former, there is nothing more to do.
+				// If it's the latter, we cannot predict the values for subsequent
+				// rows. So, we still break out of this loop, and will rely on
+				// the poller to eventually pick up the rows from the database.
+				break
+			}
+			row[idPKIndex] = sqltypes.NewInt64(id)
+			id++
+		}
+	}
+
 	// Re-read the inserted rows to prime the cache.
 	extras := map[string]sqlparser.Encodable{
 		"#pk": &sqlparser.TupleEqualityList{
