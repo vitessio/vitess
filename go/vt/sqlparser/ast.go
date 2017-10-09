0716cacaab6ba594058d455b4e6f0bad2f30d0cd
Janne Kulmala <jannek@google.com>
2017-10-09 10:18:13 +0200

    sqlparser: Escape identifiers
    Identifiers with backticks in the middle were not correctly escaped.
    Myprintf() calls Format() interface, which happens to be formatID for
    ColIdent and TableIdent.

--- go/vt/sqlparser/ast.go
+++ go/vt/sqlparser/ast.go
@@ -698,7 +698,7 @@ type ColumnDefinition struct {
 
 // Format formats the node.
 func (col *ColumnDefinition) Format(buf *TrackedBuffer) {
-	buf.Myprintf("`%s` %v", col.Name.String(), &col.Type)
+	buf.Myprintf("%v %v", col.Name, &col.Type)
 }
 
 // WalkSubtree walks the nodes of the subtree.
@@ -920,9 +920,9 @@ func (idx *IndexDefinition) Format(buf *TrackedBuffer) {
 	buf.Myprintf("%v (", idx.Info)
 	for i, col := range idx.Columns {
 		if i != 0 {
-			buf.Myprintf(", `%s`", col.Column.String())
+			buf.Myprintf(", %v", col.Column)
 		} else {
-			buf.Myprintf("`%s`", col.Column.String())
+			buf.Myprintf("%v", col.Column)
 		}
 		if col.Length != nil {
 			buf.Myprintf("(%v)", col.Length)
