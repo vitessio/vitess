6e501daa0cbe9a95b1784ebbca9d1816f8f4424d
Alain Jobart <alainjobart@google.com>
2017-11-19 12:33:26 -0800

    Simplified vtctld.Explorer code.
    No need to have the interface now that we only  have one implementation.
    Moved everything into vtctld/explorer.go.

--- go/cmd/vtctld/plugin_zk2topo.go
+++ go/cmd/vtctld/plugin_zk2topo.go
@@ -16,19 +16,8 @@ limitations under the License.
 
 package main
 
-// Imports and register the 'zk2' topo.Server and its Explorer.
+// Imports and register the 'zk2' topo.Server.
 
 import (
-	"github.com/youtube/vitess/go/vt/servenv"
-	"github.com/youtube/vitess/go/vt/topo/zk2topo"
-	"github.com/youtube/vitess/go/vt/vtctld"
+	_ "github.com/youtube/vitess/go/vt/topo/zk2topo"
 )
-
-func init() {
-	// Wait until flags are parsed, so we can check which topo server is in use.
-	servenv.OnRun(func() {
-		if s, ok := ts.Impl.(*zk2topo.Server); ok {
-			vtctld.HandleExplorer("zk2", vtctld.NewBackendExplorer(s))
-		}
-	})
-}
