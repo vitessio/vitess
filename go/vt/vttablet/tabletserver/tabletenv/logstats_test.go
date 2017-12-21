be2f2e326547bffadd3dbecb6698d67bed75910c
Michael Demmer <mdemmer@slack-corp.com>
2017-12-21 11:10:27 -0800

    remove test flakiness due to bind variable map
    The output ordering when stringifying a map is undefined so change
    the test to only ever put one element in the map and split out the
    int / string formatting into two separate passes.

--- go/vt/vttablet/tabletserver/tabletenv/logstats_test.go
+++ go/vt/vttablet/tabletserver/tabletenv/logstats_test.go
@@ -60,7 +60,7 @@ func TestLogStatsFormat(t *testing.T) {
 	logStats.StartTime = time.Date(2017, time.January, 1, 1, 2, 3, 0, time.UTC)
 	logStats.EndTime = time.Date(2017, time.January, 1, 1, 2, 4, 0, time.UTC)
 	logStats.OriginalSQL = "sql"
-	logStats.BindVariables = map[string]*querypb.BindVariable{"intVal": sqltypes.Int64BindVariable(1), "strVal": sqltypes.StringBindVariable("abc")}
+	logStats.BindVariables = map[string]*querypb.BindVariable{"intVal": sqltypes.Int64BindVariable(1)}
 	logStats.AddRewrittenSQL("sql with pii", time.Now())
 	logStats.MysqlResponseTime = 0
 	logStats.Rows = [][]sqltypes.Value{{sqltypes.NewVarBinary("a")}}
@@ -69,7 +69,7 @@ func TestLogStatsFormat(t *testing.T) {
 	*streamlog.RedactDebugUIQueries = false
 	*streamlog.QueryLogFormat = "text"
 	got := logStats.Format(url.Values(params))
-	want := "test\t\t\t''\t''\tJan  1 01:02:03.000000\tJan  1 01:02:04.000000\t1.000000\t\t\"sql\"\tmap[intVal:type:INT64 value:\"1\"  strVal:type:VARCHAR value:\"abc\" ]\t1\t\"sql with pii\"\tmysql\t0.000000\t0.000000\t0\t1\t\"\"\t\n"
+	want := "test\t\t\t''\t''\tJan  1 01:02:03.000000\tJan  1 01:02:04.000000\t1.000000\t\t\"sql\"\tmap[intVal:type:INT64 value:\"1\" ]\t1\t\"sql with pii\"\tmysql\t0.000000\t0.000000\t0\t1\t\"\"\t\n"
 	if got != want {
 		t.Errorf("logstats format: got:\n%q\nwant:\n%q\n", got, want)
 	}
@@ -94,7 +94,7 @@ func TestLogStatsFormat(t *testing.T) {
 	if err != nil {
 		t.Errorf("logstats format: error marshaling json: %v -- got:\n%v", err, got)
 	}
-	want = "{\n    \"BindVars\": {\n        \"intVal\": {\n            \"type\": \"INT64\",\n            \"value\": 1\n        },\n        \"strVal\": {\n            \"type\": \"VARCHAR\",\n            \"value\": \"abc\"\n        }\n    },\n    \"ConnWaitTime\": 0,\n    \"Effective Caller\": \"\",\n    \"End\": \"Jan  1 01:02:04.000000\",\n    \"Error\": \"\",\n    \"ImmediateCaller\": \"\",\n    \"Method\": \"test\",\n    \"MysqlTime\": 0,\n    \"OriginalSQL\": \"sql\",\n    \"PlanType\": \"\",\n    \"Queries\": 1,\n    \"QuerySources\": \"mysql\",\n    \"RemoteAddr\": \"\",\n    \"ResponseSize\": 1,\n    \"RewrittenSQL\": \"sql with pii\",\n    \"RowsAffected\": 0,\n    \"Start\": \"Jan  1 01:02:03.000000\",\n    \"TotalTime\": 1,\n    \"Username\": \"\"\n}"
+	want = "{\n    \"BindVars\": {\n        \"intVal\": {\n            \"type\": \"INT64\",\n            \"value\": 1\n        }\n    },\n    \"ConnWaitTime\": 0,\n    \"Effective Caller\": \"\",\n    \"End\": \"Jan  1 01:02:04.000000\",\n    \"Error\": \"\",\n    \"ImmediateCaller\": \"\",\n    \"Method\": \"test\",\n    \"MysqlTime\": 0,\n    \"OriginalSQL\": \"sql\",\n    \"PlanType\": \"\",\n    \"Queries\": 1,\n    \"QuerySources\": \"mysql\",\n    \"RemoteAddr\": \"\",\n    \"ResponseSize\": 1,\n    \"RewrittenSQL\": \"sql with pii\",\n    \"RowsAffected\": 0,\n    \"Start\": \"Jan  1 01:02:03.000000\",\n    \"TotalTime\": 1,\n    \"Username\": \"\"\n}"
 	if string(formatted) != want {
 		t.Errorf("logstats format: got:\n%q\nwant:\n%v\n", string(formatted), want)
 	}
@@ -116,6 +116,33 @@ func TestLogStatsFormat(t *testing.T) {
 	}
 
 	*streamlog.RedactDebugUIQueries = false
+
+	// Make sure formatting works for string bind vars. We can't do this as part of a single
+	// map because the output ordering is undefined.
+	logStats.BindVariables = map[string]*querypb.BindVariable{"strVal": sqltypes.StringBindVariable("abc")}
+
+	*streamlog.QueryLogFormat = "text"
+	got = logStats.Format(url.Values(params))
+	want = "test\t\t\t''\t''\tJan  1 01:02:03.000000\tJan  1 01:02:04.000000\t1.000000\t\t\"sql\"\tmap[strVal:type:VARCHAR value:\"abc\" ]\t1\t\"sql with pii\"\tmysql\t0.000000\t0.000000\t0\t1\t\"\"\t\n"
+	if got != want {
+		t.Errorf("logstats format: got:\n%q\nwant:\n%q\n", got, want)
+	}
+
+	*streamlog.QueryLogFormat = "json"
+	got = logStats.Format(url.Values(params))
+	err = json.Unmarshal([]byte(got), &parsed)
+	if err != nil {
+		t.Errorf("logstats format: error unmarshaling json: %v -- got:\n%v", err, got)
+	}
+	formatted, err = json.MarshalIndent(parsed, "", "    ")
+	if err != nil {
+		t.Errorf("logstats format: error marshaling json: %v -- got:\n%v", err, got)
+	}
+	want = "{\n    \"BindVars\": {\n        \"strVal\": {\n            \"type\": \"VARCHAR\",\n            \"value\": \"abc\"\n        }\n    },\n    \"ConnWaitTime\": 0,\n    \"Effective Caller\": \"\",\n    \"End\": \"Jan  1 01:02:04.000000\",\n    \"Error\": \"\",\n    \"ImmediateCaller\": \"\",\n    \"Method\": \"test\",\n    \"MysqlTime\": 0,\n    \"OriginalSQL\": \"sql\",\n    \"PlanType\": \"\",\n    \"Queries\": 1,\n    \"QuerySources\": \"mysql\",\n    \"RemoteAddr\": \"\",\n    \"ResponseSize\": 1,\n    \"RewrittenSQL\": \"sql with pii\",\n    \"RowsAffected\": 0,\n    \"Start\": \"Jan  1 01:02:03.000000\",\n    \"TotalTime\": 1,\n    \"Username\": \"\"\n}"
+	if string(formatted) != want {
+		t.Errorf("logstats format: got:\n%q\nwant:\n%v\n", string(formatted), want)
+	}
+
 	*streamlog.QueryLogFormat = "text"
 }
 
