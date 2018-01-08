217c6010b93dccedc1d6e62ef6dde612aa2ddd0e
Michael Demmer <mdemmer@slack-corp.com>
2018-01-07 21:18:13 -0800

    add a bunch more delete / update vtexplain cases

--- data/test/vtexplain/updatesharded-queries.sql
+++ data/test/vtexplain/updatesharded-queries.sql
@@ -2,5 +2,9 @@ update user set nickname='alice' where id=1;
 update user set nickname='alice' where name='alice';
 update user set pet='fido' where id=1;
 
+/* update secondary vindex value */
+update user set name='alicia' where id=1;
+update user set name='alicia' where name='alice';
+
 /* not supported - multi-shard update */
 -- update user set pet='rover' where name='alice';
