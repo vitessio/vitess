30c6a69c043544353c4de7b2c041a7cfb9ebcf16
Rafael Chacon <rafael@slack-corp.com>
2018-01-31 13:22:19 -0800

    Add support for trailing comments in begin/commit/rollback statements

--- go/vt/sqlparser/analyzer.go
+++ go/vt/sqlparser/analyzer.go
@@ -52,6 +52,7 @@ const (
 // textual comparison to identify the statement type.
 func Preview(sql string) int {
 	trimmed := StripLeadingComments(sql)
+	trimmedNoComments, _ := SplitTrailingComments(trimmed)
 
 	firstWord := trimmed
 	if end := strings.IndexFunc(trimmed, unicode.IsSpace); end != -1 {
@@ -72,7 +73,7 @@ func Preview(sql string) int {
 	case "delete":
 		return StmtDelete
 	}
-	switch strings.ToLower(trimmed) {
+	switch strings.ToLower(trimmedNoComments) {
 	case "begin", "start transaction":
 		return StmtBegin
 	case "commit":
