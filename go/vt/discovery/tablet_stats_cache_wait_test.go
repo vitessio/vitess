79d74d8348ffdb2eed858788583f42b033b2590b
yangxuanjia <yangxuanjia@jd.com>
2018-02-08 16:45:42 +0800

    when etcd all down , user can also connect vtgate

--- go/vt/discovery/tablet_stats_cache_wait_test.go
+++ go/vt/discovery/tablet_stats_cache_wait_test.go
@@ -31,7 +31,6 @@ import (
 
 	querypb "github.com/youtube/vitess/go/vt/proto/query"
 	topodatapb "github.com/youtube/vitess/go/vt/proto/topodata"
-	"github.com/youtube/vitess/go/vt/sqlparser"
 )
 
 // To sort []*querypb.Target for comparison.
