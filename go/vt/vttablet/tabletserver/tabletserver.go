ddcd8eb7f0a08f47546cff5aab6b43e47dbce0b9
Michael Demmer <mdemmer@slack-corp.com>
2018-03-04 10:02:28 -0800

    fix other calls to get a conn pool connection to use the timeout
    The previous change didn't catch every place where the conn pool
    was used to get a connection since QueryExecutor.getConn took the
    pool by reference.
    
    Refactor to split out getConn() from getStreamConn() and use them
    consistently throughout QueryExecutor.
    
    Signed-off-by: Michael Demmer <mdemmer@slack-corp.com>

--- go/vt/vttablet/tabletserver/tabletserver.go
+++ go/vt/vttablet/tabletserver/tabletserver.go
@@ -1583,7 +1583,7 @@ func newSplitQuerySQLExecuter(
 		queryExecutor: queryExecutor,
 	}
 	var err error
-	result.conn, err = queryExecutor.getConn(queryExecutor.tsv.qe.conns)
+	result.conn, err = queryExecutor.getConn()
 	if err != nil {
 		return nil, err
 	}
