a6c477867b35e791e7587b6422f64c3166a3cba1
Sugu Sougoumarane <ssougou@gmail.com>
2018-04-08 22:26:21 -0700

    vtgate planbuilder: name generation for subqueries
    Signed-off-by: Sugu Sougoumarane <ssougou@gmail.com>

---
+++ go/vt/vtgate/planbuilder/jointab_test.go
@@ -0,0 +1,43 @@
+/*
+Copyright 2018 The Vitess Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package planbuilder
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGenerateSubqueryVars(t *testing.T) {
+	jt := newJointab(map[string]struct{}{
+		"__sq1":         {},
+		"__has_values3": {},
+	})
+
+	v1, v2, v3 := jt.GenerateSubqueryVars()
+	combined := []string{v1, v2, v3}
+	want := []string{"__sq2", "__has_values2", "__is_empty2"}
+	if !reflect.DeepEqual(combined, want) {
+		t.Errorf("jt.GenerateSubqueryVars: %v, want %v", combined, want)
+	}
+
+	v1, v2, v3 = jt.GenerateSubqueryVars()
+	combined = []string{v1, v2, v3}
+	want = []string{"__sq4", "__has_values4", "__is_empty4"}
+	if !reflect.DeepEqual(combined, want) {
+		t.Errorf("jt.GenerateSubqueryVars: %v, want %v", combined, want)
+	}
+}
