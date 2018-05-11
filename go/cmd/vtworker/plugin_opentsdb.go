76e01b657af6d2046d58d12f624e338285bbccfb
David Weitzman <dweitzman@pinterest.com>
2018-05-11 13:05:38 -0700

    Basic support for -stats_backend opentsdb
    Signed-off-by: David Weitzman <dweitzman@pinterest.com>

---
+++ go/cmd/vtworker/plugin_opentsdb.go
@@ -0,0 +1,11 @@
+package main
+
+// This plugin imports opentsdb to register the opentsdb stats backend.
+
+import (
+	"vitess.io/vitess/go/stats/opentsdb"
+)
+
+func init() {
+	opentsdb.Init("vtworker")
+}
