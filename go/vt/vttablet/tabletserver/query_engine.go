e3834871a1ca526ef3f396cac978160f2c14177a
Alessandro De Filippo <ale.defilippo@workday.com>
2018-05-17 16:06:44 +0100

    Adding AllowUnsafeDMLs in vttablet configuration
    Signed-off-by: Alessandro De Filippo <alessandro@defilippo.me>

--- go/vt/vttablet/tabletserver/query_engine.go
+++ go/vt/vttablet/tabletserver/query_engine.go
@@ -235,6 +235,7 @@ func NewQueryEngine(checker connpool.MySQLChecker, se *schema.Engine, config tab
 	qe.streamBufferSize = sync2.NewAtomicInt64(int64(config.StreamBufferSize))
 
 	qe.passthroughDMLs = sync2.NewAtomicBool(config.PassthroughDMLs)
+	qe.allowUnsafeDMLs = config.AllowUnsafeDMLs
 	planbuilder.PassthroughDMLs = config.PassthroughDMLs
 
 	qe.accessCheckerLogger = logutil.NewThrottledLogger("accessChecker", 1*time.Second)
