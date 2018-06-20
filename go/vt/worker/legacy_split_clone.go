27e301524665e072ad21a39ea44bbf36d0fd16d3
Rafael Chacon <rafael@slack-corp.com>
2018-06-20 14:02:56 -0700

    Do not require RDONLY tablets for split diff task
    Signed-off-by: Rafael Chacon <rafael@slack-corp.com>

--- go/vt/worker/legacy_split_clone.go
+++ go/vt/worker/legacy_split_clone.go
@@ -344,7 +344,7 @@ func (scw *LegacySplitCloneWorker) findTargets(ctx context.Context) error {
 	// find an appropriate tablet in the source shards
 	scw.sourceAliases = make([]*topodatapb.TabletAlias, len(scw.sourceShards))
 	for i, si := range scw.sourceShards {
-		scw.sourceAliases[i], err = FindWorkerTablet(ctx, scw.wr, scw.cleaner, scw.tsc, scw.cell, si.Keyspace(), si.ShardName(), scw.minHealthyRdonlyTablets)
+		scw.sourceAliases[i], err = FindWorkerTablet(ctx, scw.wr, scw.cleaner, scw.tsc, scw.cell, si.Keyspace(), si.ShardName(), scw.minHealthyRdonlyTablets, topodatapb.TabletType_RDONLY)
 		if err != nil {
 			return fmt.Errorf("FindWorkerTablet() failed for %v/%v/%v: %v", scw.cell, si.Keyspace(), si.ShardName(), err)
 		}
