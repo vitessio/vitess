265138cb721a2d50008370c0608288e26a0cd6b2
cjsheng <liyuansheng21@gmail.com>
2018-07-26 20:27:29 +0800

    sqlparser: Adjust the order of reserved and non-reserved words
    Signed-off-by: cjsheng <liyuansheng21@gmail.com>

--- go/vt/sqlparser/parse_test.go
+++ go/vt/sqlparser/parse_test.go
@@ -1517,7 +1517,7 @@ func TestKeywords(t *testing.T) {
 		input:  "select /* share and mode as cols */ share, mode from t where share = 'foo'",
 		output: "select /* share and mode as cols */ `share`, `mode` from t where `share` = 'foo'",
 	}, {
-		input:  "select /* unused keywords as cols */ write, varying from t where trailing = 'foo'",
+		input:  "select /* unused keywords as cols */ `write`, varying from t where trailing = 'foo'",
 		output: "select /* unused keywords as cols */ `write`, `varying` from t where `trailing` = 'foo'",
 	}, {
 		input:  "select status from t",
