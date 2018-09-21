0666c45f079036aafcbc8c9501e673cceb43c003
Rafael Chacon <rafael@slack-corp.com>
2018-09-21 11:14:07 -0700

    Adds batch time as parameter
    Signed-off-by: Rafael Chacon <rafael@slack-corp.com>

--- go/vt/vtexplain/vtexplain.go
+++ go/vt/vtexplain/vtexplain.go
@@ -21,6 +21,7 @@ package vtexplain
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
 	"sort"
 	"time"
@@ -32,6 +33,12 @@ import (
 	"vitess.io/vitess/go/vt/vtgate/engine"
 
 	querypb "vitess.io/vitess/go/vt/proto/query"
+)
+
+var (
+	// vtexplain uses a logical clock to show order of execution in queries.
+	// Some vtexplain runs can change order depending on the duration of the batch time
+	batchTimeWaitDuration = flag.Duration("batch-time-wait-duration", 10*time.Millisecond, "Duration of a batch time logical clock.")
 )
 
 // ExecutorMode controls the mode of operation for the vtexplain simulator
@@ -238,7 +245,7 @@ func Run(sql string) ([]*Explain, error) {
 
 		if sql != "" {
 			// Reset the global time simulator for each query
-			batchTime = sync2.NewBatcher(time.Duration(10 * time.Millisecond))
+			batchTime = sync2.NewBatcher(*batchTimeWaitDuration)
 			log.V(100).Infof("explain %s", sql)
 			e, err := explain(sql)
 			if err != nil {
