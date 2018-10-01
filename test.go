fc4fd6b976afb8117aa6f17e10a63fb176626148
Rafael Chacon <rafael@slack-corp.com>
2018-10-01 16:17:48 -0700

    Revert test.go change
    Signed-off-by: Rafael Chacon <rafael@slack-corp.com>

--- test.go
+++ test.go
@@ -322,7 +322,7 @@ func main() {
 				wg.Add(1)
 				go func(flavor string) {
 					defer wg.Done()
-					image := "rafael/vitess_bootstrap:" + flavor
+					image := "vitess/bootstrap:" + flavor
 					pullTime := time.Now()
 					log.Printf("Pulling %v...", image)
 					cmd := exec.Command("docker", "pull", image)
