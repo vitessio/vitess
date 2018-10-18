9e0930d185929afa9daee9d90ab0d656a204e037
Michael Demmer <mdemmer@slack-corp.com>
2018-10-17 20:28:43 -0700

    add vtgate engine support to record warnings in the session
    This includes adding a new RecordWarnings() method to the vcursor
    interface so that primitives can store warnings in the session.
    
    Also add support to the route primitive to record warnings when the
    SCATTER_ERRORS_AS_WARNINGS query directive is set.
    
    Signed-off-by: Michael Demmer <mdemmer@slack-corp.com>

--- go/vt/vtgate/engine/route.go
+++ go/vt/vtgate/engine/route.go
@@ -23,6 +23,7 @@ import (
 	"time"
 
 	"vitess.io/vitess/go/jsonutil"
+	"vitess.io/vitess/go/mysql"
 	"vitess.io/vitess/go/sqltypes"
 	"vitess.io/vitess/go/stats"
 	"vitess.io/vitess/go/vt/key"
@@ -224,6 +225,12 @@ func (route *Route) execute(vcursor VCursor, bindVars map[string]*querypb.BindVa
 	if errs != nil {
 		if route.ScatterErrorsAsWarnings {
 			partialSuccessScatterQueries.Add(1)
+
+			for _, err := range errs {
+				if err != nil {
+					vcursor.RecordWarning(&querypb.QueryWarning{Code: mysql.ERVitessShardError, Message: err.Error()})
+				}
+			}
 			// fall through
 		} else {
 			return nil, vterrors.Aggregate(errs)
