eb1b69bdfda7237802d1ccbbbd703949343e76a4
Michael Demmer <mdemmer@slack-corp.com>
2018-10-23 08:15:06 -0700

    Revert "enhance AllErrorRecorder to support positional error recording"
    Since the design for partial scatter evolved to no longer require
    errors be associated with their position in the shards array, remove
    the now unnecessary support in the AllErrorRecorder.
    
    This reverts commit 8ba997f323c56ea1ca676b0be2be8ff1a469fa91.
    
    Signed-off-by: Michael Demmer <mdemmer@slack-corp.com>

--- go/vt/concurrency/error_recorder.go
+++ go/vt/concurrency/error_recorder.go
@@ -73,21 +73,7 @@ func (fer *FirstErrorRecorder) Error() error {
 	return fer.firstError
 }
 
-// AllErrorRecorder records all the errors either by appending
-// or by a reference position.
-//
-// It enhances the ErrorRecorder interface to support two methods for
-// storing errors:
-//
-//   RecordError(err)      appends the error to the list
-//   RecordErrorAt(err, i) adds the error at the given position
-//
-// In either case:
-//   HasErrors()           returns true if there are any errors in the list
-//   Error()       		   returns an aggregated error composed of any non-nil entries
-//   AggrError(aggr)       runs the given aggregation to coalesce errors
-//   GetErrors(N)          returns an array of size N with either an error or nil for each position
-//
+// AllErrorRecorder records all the errors.
 type AllErrorRecorder struct {
 	mu     sync.Mutex
 	Errors []error
@@ -104,51 +90,11 @@ func (aer *AllErrorRecorder) RecordError(err error) {
 	aer.mu.Unlock()
 }
 
-// RecordErrorAt records a possible error at the given position
-// - does nothing if err is nil
-func (aer *AllErrorRecorder) RecordErrorAt(i int, err error) {
-	if err == nil {
-		return
-	}
-	aer.mu.Lock()
-	aer.ensureSize(i + 1)
-	aer.Errors[i] = err
-	aer.mu.Unlock()
-}
-
-// grow the internal array to the given size if needed
-func (aer *AllErrorRecorder) ensureSize(n int) {
-	if len(aer.Errors) < n {
-		errs := make([]error, n)
-		if aer.Errors != nil {
-			copy(errs, aer.Errors)
-		}
-		aer.Errors = errs
-	}
-}
-
 // HasErrors returns true if we ever recorded an error
 func (aer *AllErrorRecorder) HasErrors() bool {
 	aer.mu.Lock()
 	defer aer.mu.Unlock()
 	return len(aer.Errors) > 0
-}
-
-// GetErrors returns nil if there are no errors or
-// an array of size N with any errors.
-//
-// Note that this will grow the internal storage to the
-// given capacity if required, so any future calls to
-// RecordError will append to the array _after_ any
-// nil entries created by growing the representation.
-func (aer *AllErrorRecorder) GetErrors(n int) []error {
-	aer.mu.Lock()
-	defer aer.mu.Unlock()
-	if len(aer.Errors) == 0 {
-		return nil
-	}
-	aer.ensureSize(n)
-	return aer.Errors[:n]
 }
 
 // AllErrorAggregator aggregates errors.
@@ -170,9 +116,7 @@ func (aer *AllErrorRecorder) Error() error {
 	return aer.AggrError(func(errors []error) error {
 		errs := make([]string, 0, len(errors))
 		for _, e := range errors {
-			if e != nil {
-				errs = append(errs, e.Error())
-			}
+			errs = append(errs, e.Error())
 		}
 		return fmt.Errorf("%v", strings.Join(errs, ";"))
 	})
@@ -187,9 +131,7 @@ func (aer *AllErrorRecorder) ErrorStrings() []string {
 	}
 	errs := make([]string, 0, len(aer.Errors))
 	for _, e := range aer.Errors {
-		if e != nil {
-			errs = append(errs, e.Error())
-		}
+		errs = append(errs, e.Error())
 	}
 	return errs
 }
