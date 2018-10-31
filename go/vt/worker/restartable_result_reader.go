0dedd5e08b4c342eaf5d027e5834d7ddd9c9549b
deepthi <deepthi@planetscale.com>
2018-10-30 17:57:19 -0700

    rebase PR #3450, fix unit test and worker test
    Signed-off-by: deepthi <deepthi@planetscale.com>

--- go/vt/worker/restartable_result_reader.go
+++ go/vt/worker/restartable_result_reader.go
@@ -82,14 +82,30 @@ func NewRestartableResultReader(ctx context.Context, logger logutil.Logger, tp t
 		allowMultipleRetries: allowMultipleRetries,
 	}
 
-	// If the initial connection fails, we do not restart.
-	if _ /* retryable */, err := r.getTablet(); err != nil {
-		return nil, vterrors.Wrap(err, "tablet=unknown")
+	// If the initial connection fails we retry once.
+	// Note: The first retry will be the second attempt.
+	attempt := 0
+	for {
+		attempt++
+		var err error
+		var retryable bool
+		if retryable, err = r.getTablet(); err != nil {
+			err = fmt.Errorf("tablet=unknown: %v", err)
+			goto retry
+		}
+		if retryable, err = r.startStream(); err != nil {
+			err = fmt.Errorf("tablet=%v: %v", topoproto.TabletAliasString(r.tablet.Alias), err)
+			goto retry
+		}
+		return r, nil
+
+	retry:
+		if !retryable || attempt > 1 {
+			return nil, fmt.Errorf("failed to initialize tablet connection: retryable %v, %v", retryable, err)
+		}
+		statsRetryCount.Add(1)
+		logger.Infof("retrying after error: %v", err)
 	}
-	if _ /* retryable */, err := r.startStream(); err != nil {
-		return nil, vterrors.Wrapf(err, "tablet=%v", topoproto.TabletAliasString(r.tablet.Alias))
-	}
-	return r, nil
 }
 
 // getTablet (re)sets the tablet which is used for the streaming query.
@@ -172,7 +188,7 @@ func (r *RestartableResultReader) nextWithRetries() (*sqltypes.Result, error) {
 	retryCtx, retryCancel := context.WithTimeout(r.ctx, *retryDuration)
 	defer retryCancel()
 
-	// Note: The first retry will be the second attempt.
+	// The first retry is the second attempt because we already tried once in Next()
 	attempt := 1
 	start := time.Now()
 	for {
