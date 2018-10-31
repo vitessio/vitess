9bbfaf68fe0289003c1f7de64509b0abe88062ce
kuba-- <kuba@sourced.tech>
2018-10-31 11:16:02 +0100

    No panic when could not find glog flag.
    Signed-off-by: kuba-- <kuba@sourced.tech>

--- go/vt/logutil/level.go
+++ go/vt/logutil/level.go
@@ -23,11 +23,13 @@ import (
 func init() {
 	threshold := flag.Lookup("stderrthreshold")
 	if threshold == nil {
-		panic("the logging module doesn't specify a stderrthreshold flag")
+		// the logging module doesn't specify a stderrthreshold flag
+		return
 	}
+
 	const warningLevel = "1"
 	if err := threshold.Value.Set(warningLevel); err != nil {
-		panic(err)
+		return
 	}
 	threshold.DefValue = warningLevel
 }
