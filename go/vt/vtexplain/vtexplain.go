af73dec01e07486fb8a3f158382d4a928a96787c
Rafael Chacon <rafael@slack-corp.com>
2018-12-01 17:20:30 -0800

    Make sure tabletserver is shutdown before cleaning up DB
    Signed-off-by: Rafael Chacon <rafael@slack-corp.com>

--- go/vt/vtexplain/vtexplain.go
+++ go/vt/vtexplain/vtexplain.go
@@ -174,6 +174,7 @@ func Stop() {
 	// Cleanup all created fake dbs.
 	if explainTopo != nil {
 		for _, conn := range explainTopo.TabletConns {
+			conn.tsv.StopService()
 			conn.db.Close()
 		}
 	}
