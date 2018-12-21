de85143a18cdf4e07e7e71a1d9c8f4656c878209
Rafael Chacon <rafael@slack-corp.com>
2018-12-21 09:39:29 -0800

    Revert "Adds tests for discovery gateway aggregate stats"
    This reverts commit 6e95f149ca751a7d177970c60045c7d8bcd61d42.

--- go/vt/discovery/tablet_stats_cache.go
+++ go/vt/discovery/tablet_stats_cache.go
@@ -498,7 +498,6 @@ func (tc *TabletStatsCache) GetAggregateStats(target *querypb.Target) (*querypb.
 			return agg, nil
 		}
 	}
-
 	targetRegion := tc.getRegionByCell(target.Cell)
 	regionAggregateStats := &querypb.AggregateStats{SecondsBehindMasterMin: math.MaxUint32}
 	regionAggrFound := false
