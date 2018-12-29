d106ba0d5d5eccedbe25f9cca78815412a8d0d77
Sugu Sougoumarane <ssougou@gmail.com>
2018-12-28 22:51:25 -0800

    vreplication: vstreamer, plan and filtering
    Signed-off-by: Sugu Sougoumarane <ssougou@gmail.com>

--- go/vt/vttablet/tabletserver/vstreamer/planbuilder.go
+++ go/vt/vttablet/tabletserver/vstreamer/planbuilder.go
@@ -20,11 +20,14 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
+	"time"
 
 	"vitess.io/vitess/go/mysql"
+	"vitess.io/vitess/go/sqltypes"
 	"vitess.io/vitess/go/vt/key"
 	"vitess.io/vitess/go/vt/sqlparser"
 	"vitess.io/vitess/go/vt/vtgate/vindexes"
+	"vitess.io/vitess/go/vt/vttablet/tabletserver/schema"
 
 	binlogdatapb "vitess.io/vitess/go/vt/proto/binlogdata"
 	topodatapb "vitess.io/vitess/go/vt/proto/topodata"
@@ -32,7 +35,7 @@ import (
 
 // Plan represents the streaming plan for a table.
 type Plan struct {
-	Table        string
+	Table        *Table
 	ColExprs     []ColExpr
 	VindexColumn int
 	Vindex       vindexes.Vindex
@@ -42,6 +45,7 @@ type Plan struct {
 // ColExpr represents a column expression.
 type ColExpr struct {
 	ColNum    int
+	Alias     sqlparser.ColIdent
 	Operation Operation
 }
 
@@ -56,79 +60,126 @@ const (
 	OpHour
 )
 
-// TableMap contains the metadata for a table. The
+// Table contains the metadata for a table. The
 // name is dervied from mysql's Table_map_log_event.
-type TableMap struct {
+type Table struct {
 	*mysql.TableMap
-	ColumnNames []sqlparser.ColIdent
+	Columns []schema.TableColumn
 }
 
-func buildPlan(tm *TableMap, kschema *vindexes.KeyspaceSchema, filter *binlogdatapb.Filter) (*Plan, error) {
+func (plan *Plan) filter(values []sqltypes.Value) (bool, []sqltypes.Value, error) {
+	result := make([]sqltypes.Value, len(plan.ColExprs))
+	for i, colExpr := range plan.ColExprs {
+		switch colExpr.Operation {
+		case OpYearMonth:
+			v, _ := sqltypes.ToInt64(values[colExpr.ColNum])
+			t := time.Unix(v, 0)
+			s := fmt.Sprintf("%d%02d", t.Year(), t.Month())
+			result[i] = sqltypes.NewVarBinary(s)
+		case OpDay:
+			v, _ := sqltypes.ToInt64(values[colExpr.ColNum])
+			t := time.Unix(v, 0)
+			s := fmt.Sprintf("%d%02d%02d", t.Year(), t.Month(), t.Day())
+			result[i] = sqltypes.NewVarBinary(s)
+		case OpHour:
+			v, _ := sqltypes.ToInt64(values[colExpr.ColNum])
+			t := time.Unix(v, 0)
+			s := fmt.Sprintf("%d%02d%02d%02d", t.Year(), t.Month(), t.Day(), t.Hour())
+			result[i] = sqltypes.NewVarBinary(s)
+		default:
+			result[i] = values[colExpr.ColNum]
+		}
+	}
+	if plan.Vindex == nil {
+		return true, result, nil
+	}
+	destinations, err := plan.Vindex.Map(nil, []sqltypes.Value{result[plan.VindexColumn]})
+	if err != nil {
+		return false, nil, err
+	}
+	if len(destinations) != 1 {
+		return false, nil, fmt.Errorf("mapping row to keyspace id returned an invalid array of destinations: %v", key.DestinationsString(destinations))
+	}
+	ksid, ok := destinations[0].(key.DestinationKeyspaceID)
+	if !ok || len(ksid) == 0 {
+		return false, nil, fmt.Errorf("could not map %v to a keyspace id, got destination %v", result[plan.VindexColumn], destinations[0])
+	}
+	if !key.KeyRangeContains(plan.KeyRange, ksid) {
+		return false, nil, nil
+	}
+	return true, result, nil
+}
+
+func buildPlan(ti *Table, kschema *vindexes.KeyspaceSchema, filter *binlogdatapb.Filter) (*Plan, error) {
 	for _, rule := range filter.Rules {
 		switch {
 		case strings.HasPrefix(rule.Match, "/"):
 			expr := strings.Trim(rule.Match, "/")
-			result, err := regexp.MatchString(expr, tm.Name)
+			result, err := regexp.MatchString(expr, ti.Name)
 			if err != nil {
 				return nil, err
 			}
 			if !result {
 				continue
 			}
-			return buildREPlan(tm, kschema, rule.Filter)
-		case rule.Match == tm.Name:
+			return buildREPlan(ti, kschema, rule.Filter)
+		case rule.Match == ti.Name:
+			return buildTablePlan(ti, kschema, rule.Filter)
 		}
 	}
 	return nil, nil
 }
 
-func buildREPlan(tm *TableMap, kschema *vindexes.KeyspaceSchema, filter string) (*Plan, error) {
+func buildREPlan(ti *Table, kschema *vindexes.KeyspaceSchema, filter string) (*Plan, error) {
 	plan := &Plan{
-		Table: tm.Name,
+		Table: ti,
 	}
-	plan.ColExprs = make([]ColExpr, len(tm.ColumnNames))
-	for i := range tm.ColumnNames {
+	plan.ColExprs = make([]ColExpr, len(ti.Columns))
+	for i, col := range ti.Columns {
 		plan.ColExprs[i].ColNum = i
+		plan.ColExprs[i].Alias = col.Name
 	}
-	if filter != "" {
-		// We need to additionally set VindexColumn, Vindex and KeyRange
-		// based on the Primary Vindex of the table.
-		// Find table in kschema.
-		table := kschema.Tables[tm.Name]
-		if table == nil {
-			return nil, fmt.Errorf("no vschema definition for table %s", tm.Name)
-		}
-		// Get Primary Vindex.
-		if len(table.ColumnVindexes) == 0 {
-			return nil, fmt.Errorf("table %s has no primary vindex", tm.Name)
-		}
-		colnum, err := findColumn(tm, table.ColumnVindexes[0].Columns[0])
-		if err != nil {
-			return nil, err
-		}
-		plan.VindexColumn = colnum
-		plan.Vindex = table.ColumnVindexes[0].Vindex
+	if filter == "" {
+		return plan, nil
+	}
 
-		// Parse keyrange.
-		keyranges, err := key.ParseShardingSpec(filter)
-		if err != nil {
-			return nil, err
-		}
-		if len(keyranges) != 1 {
-			return nil, fmt.Errorf("error parsing keyrange: %v", filter)
-		}
-		plan.KeyRange = keyranges[0]
+	// We need to additionally set VindexColumn, Vindex and KeyRange
+	// based on the Primary Vindex of the table.
+	// Find table in kschema.
+	table := kschema.Tables[ti.Name]
+	if table == nil {
+		return nil, fmt.Errorf("no vschema definition for table %s", ti.Name)
 	}
+	// Get Primary Vindex.
+	if len(table.ColumnVindexes) == 0 {
+		return nil, fmt.Errorf("table %s has no primary vindex", ti.Name)
+	}
+	colnum, err := findColumn(ti, table.ColumnVindexes[0].Columns[0])
+	if err != nil {
+		return nil, err
+	}
+	plan.VindexColumn = colnum
+	plan.Vindex = table.ColumnVindexes[0].Vindex
+
+	// Parse keyrange.
+	keyranges, err := key.ParseShardingSpec(filter)
+	if err != nil {
+		return nil, err
+	}
+	if len(keyranges) != 1 {
+		return nil, fmt.Errorf("error parsing keyrange: %v", filter)
+	}
+	plan.KeyRange = keyranges[0]
 	return plan, nil
 }
 
-func buildTablePlan(tm *TableMap, kschema *vindexes.KeyspaceSchema, query string) (*Plan, error) {
+func buildTablePlan(ti *Table, kschema *vindexes.KeyspaceSchema, query string) (*Plan, error) {
 	statement, err := sqlparser.Parse(query)
 	if err != nil {
 		return nil, err
 	}
 	plan := &Plan{
-		Table: tm.Name,
+		Table: ti,
 	}
 	sel, ok := statement.(*sqlparser.Select)
 	if !ok {
@@ -145,13 +196,13 @@ func buildTablePlan(tm *TableMap, kschema *vindexes.KeyspaceSchema, query string
 	if fromTable.IsEmpty() {
 		return nil, fmt.Errorf("unexpected: %v", sqlparser.String(sel))
 	}
-	if fromTable.String() == tm.Name {
-		return nil, fmt.Errorf("unexpected: select expression table %v does not match the table entry name %s", sqlparser.String(fromTable), tm.Name)
+	if fromTable.String() == ti.Name {
+		return nil, fmt.Errorf("unexpected: select expression table %v does not match the table entry name %s", sqlparser.String(fromTable), ti.Name)
 	}
 
 	if _, ok := sel.SelectExprs[0].(*sqlparser.StarExpr); !ok {
 		for _, expr := range sel.SelectExprs {
-			cExpr, err := analyzeExpr(tm, expr)
+			cExpr, err := analyzeExpr(ti, expr)
 			if err != nil {
 				return nil, err
 			}
@@ -172,14 +223,25 @@ func buildTablePlan(tm *TableMap, kschema *vindexes.KeyspaceSchema, query string
 	if len(funcExpr.Exprs) != 3 {
 		return nil, fmt.Errorf("unexpected where clause: %v", sqlparser.String(sel.Where))
 	}
-	cExpr, err := analyzeExpr(tm, funcExpr.Exprs[0])
-	if err != nil {
-		return nil, err
+	aexpr, ok := funcExpr.Exprs[0].(*sqlparser.AliasedExpr)
+	if !ok {
+		return nil, fmt.Errorf("unexpected: %v", sqlparser.String(funcExpr))
 	}
-	if cExpr.Operation != OpNone {
-		return nil, fmt.Errorf("unexpected operaion on vindex column: %v", funcExpr.Exprs[0])
+	colname, ok := aexpr.Expr.(*sqlparser.ColName)
+	if !ok {
+		return nil, fmt.Errorf("unsupported: %v", sqlparser.String(funcExpr))
 	}
-	plan.VindexColumn = cExpr.ColNum
+	found := false
+	for i, cExpr := range plan.ColExprs {
+		if cExpr.Alias.Equal(colname.Name) {
+			found = true
+			plan.VindexColumn = i
+			break
+		}
+	}
+	if !found {
+		return nil, fmt.Errorf("keyrange expression does not reference a column in the select list: %v", sqlparser.String(colname))
+	}
 	vtype, err := selString(funcExpr.Exprs[1])
 	if err != nil {
 		return nil, err
@@ -203,18 +265,18 @@ func buildTablePlan(tm *TableMap, kschema *vindexes.KeyspaceSchema, query string
 	return plan, nil
 }
 
-func analyzeExpr(tm *TableMap, expr sqlparser.SelectExpr) (cExpr ColExpr, err error) {
+func analyzeExpr(ti *Table, expr sqlparser.SelectExpr) (cExpr ColExpr, err error) {
 	aexpr, ok := expr.(*sqlparser.AliasedExpr)
 	if !ok {
 		return ColExpr{}, fmt.Errorf("unexpected: %v", sqlparser.String(expr))
 	}
 	switch expr := aexpr.Expr.(type) {
 	case *sqlparser.ColName:
-		colnum, err := findColumn(tm, expr.Name)
+		colnum, err := findColumn(ti, expr.Name)
 		if err != nil {
 			return ColExpr{}, err
 		}
-		return ColExpr{ColNum: colnum}, nil
+		return ColExpr{ColNum: colnum, Alias: expr.Name}, nil
 	case *sqlparser.FuncExpr:
 		if expr.Distinct || len(expr.Exprs) != 1 {
 			return ColExpr{}, fmt.Errorf("unsupported: %v", sqlparser.String(expr))
@@ -229,17 +291,20 @@ func analyzeExpr(tm *TableMap, expr sqlparser.SelectExpr) (cExpr ColExpr, err er
 			if !ok {
 				return ColExpr{}, fmt.Errorf("unsupported: %v", sqlparser.String(expr))
 			}
-			colnum, err := findColumn(tm, innerCol.Name)
+			if aInner.As.IsEmpty() {
+				return ColExpr{}, fmt.Errorf("need alias for expression: %v", sqlparser.String(expr))
+			}
+			colnum, err := findColumn(ti, innerCol.Name)
 			if err != nil {
 				return ColExpr{}, err
 			}
 			switch fname {
 			case "yearmonth":
-				return ColExpr{ColNum: colnum, Operation: OpYearMonth}, nil
+				return ColExpr{ColNum: colnum, Alias: aInner.As, Operation: OpYearMonth}, nil
 			case "day":
-				return ColExpr{ColNum: colnum, Operation: OpDay}, nil
+				return ColExpr{ColNum: colnum, Alias: aInner.As, Operation: OpDay}, nil
 			case "hour":
-				return ColExpr{ColNum: colnum, Operation: OpHour}, nil
+				return ColExpr{ColNum: colnum, Alias: aInner.As, Operation: OpHour}, nil
 			default:
 				panic("unreachable")
 			}
@@ -263,11 +328,11 @@ func selString(expr sqlparser.SelectExpr) (string, error) {
 	return string(val.Val), nil
 }
 
-func findColumn(tm *TableMap, name sqlparser.ColIdent) (int, error) {
-	for i, cname := range tm.ColumnNames {
-		if name.Equal(cname) {
+func findColumn(ti *Table, name sqlparser.ColIdent) (int, error) {
+	for i, col := range ti.Columns {
+		if name.Equal(col.Name) {
 			return i, nil
 		}
 	}
-	return 0, fmt.Errorf("column %s not found in table %s", sqlparser.String(name), tm.Name)
+	return 0, fmt.Errorf("column %s not found in table %s", sqlparser.String(name), ti.Name)
 }
--- go/vt/vttablet/tabletserver/vstreamer/streamer.go
+++ go/vt/vttablet/tabletserver/vstreamer/streamer.go
@@ -18,37 +18,389 @@ package vstreamer
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"vitess.io/vitess/go/mysql"
+	"vitess.io/vitess/go/sqltypes"
 	"vitess.io/vitess/go/vt/binlog"
+	"vitess.io/vitess/go/vt/log"
+	"vitess.io/vitess/go/vt/sqlparser"
 	"vitess.io/vitess/go/vt/vtgate/vindexes"
+	"vitess.io/vitess/go/vt/vttablet/tabletserver/schema"
 
 	binlogdatapb "vitess.io/vitess/go/vt/proto/binlogdata"
 )
 
-type vstreamer struct {
-	cp       *mysql.ConnParams
-	startPos mysql.Position
-	filter   *binlogdatapb.Filter
-	f        func()
-	conn     *binlog.SlaveConnection
+// statementPrefixes are normal sql statement prefixes.
+var statementPrefixes = map[string]binlogdatapb.VEventType{
+	"begin":    binlogdatapb.VEventType_BEGIN,
+	"commit":   binlogdatapb.VEventType_COMMIT,
+	"rollback": binlogdatapb.VEventType_ROLLBACK,
+	"insert":   binlogdatapb.VEventType_INSERT,
+	"update":   binlogdatapb.VEventType_UPDATE,
+	"delete":   binlogdatapb.VEventType_DELETE,
+	"create":   binlogdatapb.VEventType_DDL,
+	"alter":    binlogdatapb.VEventType_DDL,
+	"drop":     binlogdatapb.VEventType_DDL,
+	"truncate": binlogdatapb.VEventType_DDL,
+	"rename":   binlogdatapb.VEventType_DDL,
+	"set":      binlogdatapb.VEventType_SET,
 }
 
-func newVStreamer(cp *mysql.ConnParams, startPos mysql.Position, filter *binlogdatapb.Filter, f func()) (*vstreamer, error) {
+// getStatementCategory returns the binlogdatapb.BL_* category for a SQL statement.
+func getStatementCategory(sql string) binlogdatapb.VEventType {
+	if i := strings.IndexByte(sql, byte(' ')); i >= 0 {
+		sql = sql[:i]
+	}
+	return statementPrefixes[strings.ToLower(sql)]
+}
+
+type vstreamer struct {
+	ctx    context.Context
+	cancel func()
+
+	cp       *mysql.ConnParams
+	se       *schema.Engine
+	startPos mysql.Position
+	filter   *binlogdatapb.Filter
+	send     func(*binlogdatapb.VEvent) error
+
+	kevents chan *vindexes.KeyspaceSchema
+	kschema *vindexes.KeyspaceSchema
+	plans   map[uint64]*Plan
+
+	// format and pos are updated by parseEvent.
+	format mysql.BinlogFormat
+	pos    mysql.Position
+}
+
+func newVStreamer(ctx context.Context, cp *mysql.ConnParams, se *schema.Engine, startPos mysql.Position, filter *binlogdatapb.Filter, kschema *vindexes.KeyspaceSchema, send func(*binlogdatapb.VEvent) error) *vstreamer {
+	ctx, cancel := context.WithCancel(ctx)
 	return &vstreamer{
+		ctx:      ctx,
+		cancel:   cancel,
 		cp:       cp,
+		se:       se,
 		startPos: startPos,
 		filter:   filter,
-		f:        f,
-	}, nil
+		send:     send,
+		kevents:  make(chan *vindexes.KeyspaceSchema, 1),
+		kschema:  kschema,
+		plans:    make(map[uint64]*Plan),
+	}
 }
 
 func (vs *vstreamer) SetKSchema(kschema *vindexes.KeyspaceSchema) {
-}
-
-func (vs *vstreamer) Stream(ctx context.Context) error {
-	return nil
+	// Since vs.Stream is a single-threaded loop. We just send an event to
+	// that thread, which helps us avoid mutexes to update the plans.
+	select {
+	case vs.kevents <- kschema:
+	case <-vs.ctx.Done():
+	}
 }
 
 func (vs *vstreamer) Cancel() {
+	vs.cancel()
+}
+
+// Stream runs a single-threaded loop.
+func (vs *vstreamer) Stream() error {
+	defer vs.cancel()
+	vs.pos = vs.startPos
+
+	// Ensure se is Open. If vttablet came up in a non_serving role,
+	// the schema engine may not have been initialized.
+	if err := vs.se.Open(); err != nil {
+		return wrapError(err, vs.pos)
+	}
+
+	conn, err := binlog.NewSlaveConnection(vs.cp)
+	if err != nil {
+		return wrapError(err, vs.pos)
+	}
+	defer conn.Close()
+
+	events, err := conn.StartBinlogDumpFromPosition(vs.ctx, vs.pos)
+	if err != nil {
+		return wrapError(err, vs.pos)
+	}
+	err = vs.parseEvents(vs.ctx, events)
+	return wrapError(err, vs.pos)
+}
+
+func (vs *vstreamer) parseEvents(ctx context.Context, events <-chan mysql.BinlogEvent) error {
+	for {
+		select {
+		case ev, ok := <-events:
+			if !ok {
+				return fmt.Errorf("server EOF")
+			}
+			if err := vs.parseEvent(ev); err != nil {
+				return err
+			}
+		case vs.kschema = <-vs.kevents:
+			if err := vs.updatePlans(); err != nil {
+				return err
+			}
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+}
+
+func (vs *vstreamer) parseEvent(ev mysql.BinlogEvent) error {
+	// Validate the buffer before reading fields from it.
+	if !ev.IsValid() {
+		return fmt.Errorf("can't parse binlog event: invalid data: %#v", ev)
+	}
+
+	// We need to keep checking for FORMAT_DESCRIPTION_EVENT even after we've
+	// seen one, because another one might come along (e.g. on log rotate due to
+	// binlog settings change) that changes the format.
+	if ev.IsFormatDescription() {
+		var err error
+		vs.format, err = ev.Format()
+		if err != nil {
+			return fmt.Errorf("can't parse FORMAT_DESCRIPTION_EVENT: %v, event data: %#v", err, ev)
+		}
+		return nil
+	}
+
+	// We can't parse anything until we get a FORMAT_DESCRIPTION_EVENT that
+	// tells us the size of the event header.
+	if vs.format.IsZero() {
+		// The only thing that should come before the FORMAT_DESCRIPTION_EVENT
+		// is a fake ROTATE_EVENT, which the master sends to tell us the name
+		// of the current log file.
+		if ev.IsRotate() {
+			return nil
+		}
+		return fmt.Errorf("got a real event before FORMAT_DESCRIPTION_EVENT: %#v", ev)
+	}
+
+	// Strip the checksum, if any. We don't actually verify the checksum, so discard it.
+	ev, _, err := ev.StripChecksum(vs.format)
+	if err != nil {
+		return fmt.Errorf("can't strip checksum from binlog event: %v, event data: %#v", err, ev)
+	}
+	switch {
+	case ev.IsPseudo() || ev.IsGTID():
+		gtid, hasBegin, err := ev.GTID(vs.format)
+		if err != nil {
+			return fmt.Errorf("can't get GTID from binlog event: %v, event data: %#v", err, ev)
+		}
+		vs.pos = mysql.AppendGTID(vs.pos, gtid)
+		vevent := &binlogdatapb.VEvent{
+			Type: binlogdatapb.VEventType_GTID,
+			Gtid: mysql.EncodePosition(vs.pos),
+		}
+		if err := vs.send(vevent); err != nil {
+			return fmt.Errorf("error sending GTID: %v", err)
+		}
+		if hasBegin {
+			vevent := &binlogdatapb.VEvent{
+				Type: binlogdatapb.VEventType_BEGIN,
+			}
+			if err := vs.send(vevent); err != nil {
+				return fmt.Errorf("error sending BEGIN: %v", err)
+			}
+		}
+	case ev.IsXID():
+		vevent := &binlogdatapb.VEvent{
+			Type: binlogdatapb.VEventType_COMMIT,
+		}
+		if err := vs.send(vevent); err != nil {
+			return fmt.Errorf("error sending COMMIT: %v", err)
+		}
+	case ev.IsQuery():
+		q, err := ev.Query(vs.format)
+		if err != nil {
+			return fmt.Errorf("can't get query from binlog event: %v, event data: %#v", err, ev)
+		}
+		var vevent *binlogdatapb.VEvent
+		switch cat := getStatementCategory(q.SQL); cat {
+		case binlogdatapb.VEventType_BEGIN, binlogdatapb.VEventType_COMMIT, binlogdatapb.VEventType_ROLLBACK:
+			vevent = &binlogdatapb.VEvent{
+				Type: cat,
+			}
+		case binlogdatapb.VEventType_DDL:
+			vevent = &binlogdatapb.VEvent{
+				Type: cat,
+				Ddl:  q.SQL,
+			}
+		default:
+			return fmt.Errorf("unexpected event type %v in row-based replication: %#v", cat, ev)
+		}
+		if err := vs.send(vevent); err != nil {
+			return fmt.Errorf("error sending COMMIT: %v", err)
+		}
+	case ev.IsTableMap():
+		id := ev.TableID(vs.format)
+		tm, err := ev.TableMap(vs.format)
+		if err != nil {
+			return err
+		}
+		if tm.Database != "" && tm.Database != vs.cp.DbName {
+			return nil
+		}
+		ti := vs.se.GetTable(sqlparser.NewTableIdent(tm.Name))
+		if ti == nil {
+			return fmt.Errorf("unknown table %v in schema", tm.Name)
+		}
+		if len(ti.Columns) < len(tm.Types) {
+			return fmt.Errorf("cannot determine table columns for %s: event has %d columns, current schema has %d: %#v", tm.Name, len(tm.Types), len(ti.Columns), ev)
+		}
+		table := &Table{
+			TableMap: tm,
+			Columns:  ti.Columns,
+		}
+		plan, err := buildPlan(table, vs.kschema, vs.filter)
+		if err != nil {
+			return err
+		}
+		if plan != nil {
+			vs.plans[id] = plan
+		}
+	case ev.IsWriteRows():
+		id := ev.TableID(vs.format)
+		plan, ok := vs.plans[id]
+		if !ok {
+			return nil
+		}
+		rows, err := ev.Rows(vs.format, plan.Table.TableMap)
+		if err != nil {
+			return err
+		}
+		rowEvents := make([]*binlogdatapb.RowEvent, 0, len(rows.Rows))
+		for _, row := range rows.Rows {
+			ok, values, err := vs.extractRowAndFilter(plan, row.Data, rows.DataColumns, row.NullColumns)
+			if err != nil {
+				return err
+			}
+			if !ok {
+				continue
+			}
+			encoded := sqltypes.RowToProto3(values)
+			rowEvents = append(rowEvents, &binlogdatapb.RowEvent{After: encoded})
+		}
+		vevent := &binlogdatapb.VEvent{
+			Type:      binlogdatapb.VEventType_INSERT,
+			RowEvents: rowEvents,
+		}
+		if err := vs.send(vevent); err != nil {
+			return fmt.Errorf("error sending INSERT: %v", err)
+		}
+	case ev.IsDeleteRows():
+		id := ev.TableID(vs.format)
+		plan, ok := vs.plans[id]
+		if !ok {
+			return nil
+		}
+		rows, err := ev.Rows(vs.format, plan.Table.TableMap)
+		if err != nil {
+			return err
+		}
+		rowEvents := make([]*binlogdatapb.RowEvent, 0, len(rows.Rows))
+		for _, row := range rows.Rows {
+			ok, values, err := vs.extractRowAndFilter(plan, row.Identify, rows.IdentifyColumns, row.NullIdentifyColumns)
+			if err != nil {
+				return err
+			}
+			if !ok {
+				continue
+			}
+			encoded := sqltypes.RowToProto3(values)
+			rowEvents = append(rowEvents, &binlogdatapb.RowEvent{Before: encoded})
+		}
+		vevent := &binlogdatapb.VEvent{
+			Type:      binlogdatapb.VEventType_DELETE,
+			RowEvents: rowEvents,
+		}
+		if err := vs.send(vevent); err != nil {
+			return fmt.Errorf("error sending DELETE: %v", err)
+		}
+	case ev.IsUpdateRows():
+		id := ev.TableID(vs.format)
+		plan, ok := vs.plans[id]
+		if !ok {
+			return nil
+		}
+		rows, err := ev.Rows(vs.format, plan.Table.TableMap)
+		if err != nil {
+			return err
+		}
+		rowEvents := make([]*binlogdatapb.RowEvent, 0, len(rows.Rows))
+		for _, row := range rows.Rows {
+			beforeOK, beforeValues, err := vs.extractRowAndFilter(plan, row.Identify, rows.IdentifyColumns, row.NullIdentifyColumns)
+			if err != nil {
+				return err
+			}
+			afterOK, afterValues, err := vs.extractRowAndFilter(plan, row.Data, rows.DataColumns, row.NullColumns)
+			if err != nil {
+				return err
+			}
+			if !beforeOK && !afterOK {
+				continue
+			}
+			rowEvent := &binlogdatapb.RowEvent{}
+			if beforeOK {
+				rowEvent.Before = sqltypes.RowToProto3(beforeValues)
+			}
+			if afterOK {
+				rowEvent.After = sqltypes.RowToProto3(afterValues)
+			}
+			rowEvents = append(rowEvents, rowEvent)
+		}
+		vevent := &binlogdatapb.VEvent{
+			Type:      binlogdatapb.VEventType_UPDATE,
+			RowEvents: rowEvents,
+		}
+		if err := vs.send(vevent); err != nil {
+			return fmt.Errorf("error sending UPDATE: %v", err)
+		}
+	}
+	return nil
+}
+
+func (vs *vstreamer) updatePlans() error {
+	for id, plan := range vs.plans {
+		newPlan, err := buildPlan(plan.Table, vs.kschema, vs.filter)
+		if err != nil {
+			return err
+		}
+		vs.plans[id] = newPlan
+	}
+	return nil
+}
+
+func (vs *vstreamer) extractRowAndFilter(plan *Plan, data []byte, dataColumns, nullColumns mysql.Bitmap) (bool, []sqltypes.Value, error) {
+	values := make([]sqltypes.Value, dataColumns.Count())
+	valueIndex := 0
+	pos := 0
+	for colNum := 0; colNum < dataColumns.Count(); colNum++ {
+		if !dataColumns.Bit(colNum) {
+			continue
+		}
+		if nullColumns.Bit(valueIndex) {
+			valueIndex++
+			continue
+		}
+		value, l, err := mysql.CellValue(data, pos, plan.Table.Types[colNum], plan.Table.Metadata[colNum], plan.Table.Columns[colNum].Type)
+		if err != nil {
+			return false, nil, err
+		}
+		pos += l
+		values[colNum] = value
+		valueIndex++
+	}
+	return plan.filter(values)
+}
+
+func wrapError(err error, stopPos mysql.Position) error {
+	if err != nil {
+		err = fmt.Errorf("stream error @ %v: %v", stopPos, err)
+	}
+	log.Infof("stream ended @ %v, err: %v", stopPos, err)
+	return err
 }
