76b1a6b6f95c7ae94e15e72369729de6ac091bcc
xichengliudui <liuduidui@beyondcent.com>
2019-01-29 21:02:35 -0500

    Update func
    Signed-off-by: xichengliudui <liuduidui@beyondcent.com>

--- go/vt/vtgate/executor_select_test.go
+++ go/vt/vtgate/executor_select_test.go
@@ -596,7 +596,7 @@ func TestSelectKeyRangeUnique(t *testing.T) {
 func TestSelectIN(t *testing.T) {
 	executor, sbc1, sbc2, sbclookup := createExecutorEnv()
 
-	// Constant in IN is just a number, not a bind variable.
+	// Constant IN is just a number, not a bind variable.
 	_, err := executorExec(executor, "select id from user where id in (1)", nil)
 	if err != nil {
 		t.Error(err)
@@ -614,7 +614,7 @@ func TestSelectIN(t *testing.T) {
 		t.Errorf("sbc2.Queries: %+v, want nil\n", sbc2.Queries)
 	}
 
-	// Constant in IN is just a couple numbers, not bind variables.
+	// Constant IN is just a couple numbers, not bind variables.
 	// They result in two different queries on two shards.
 	sbc1.Queries = nil
 	sbc2.Queries = nil
