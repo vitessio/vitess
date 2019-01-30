1972948c3285684cce99a8a39500ce3e50489ca7
xichengliudui <liuduidui@beyondcent.com>
2019-01-30 01:10:51 -0500

    Clean up the commented code
    Signed-off-by: xichengliudui <liuduidui@beyondcent.com>

--- go/event/event.go
+++ go/event/event.go
@@ -14,66 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-/*
-Package event provides a reflect-based framework for low-frequency global
-dispatching of events, which are values of any arbitrary type, to a set of
-listener functions, which are usually registered by plugin packages during init().
-
-Listeners should do work in a separate goroutine if it might block. Dispatch
-should be called synchronously to make sure work enters the listener's work
-queue before moving on. After Dispatch returns, the listener is responsible for
-arranging to flush its work queue before program termination if desired.
-
-For example, any package can define an event type:
-
-	package mypackage
-
-	type MyEvent struct {
-		field1, field2 string
-	}
-
-Then, any other package (e.g. a plugin) can listen for those events:
-
-	package myplugin
-
-	import (
-		"event"
-		"mypackage"
-	)
-
-	func onMyEvent(ev mypackage.MyEvent) {
-		// do something with ev
-	}
-
-	func init() {
-		event.AddListener(onMyEvent)
-	}
-
-Any registered listeners that accept a single argument of type MyEvent will
-be called when a value of type MyEvent is dispatched:
-
-	package myotherpackage
-
-	import (
-		"event"
-		"mypackage"
-	)
-
-	func InMediasRes() {
-		ev := mypackage.MyEvent{
-			field1: "foo",
-			field2: "bar",
-		}
-
-		event.Dispatch(ev)
-	}
-
-In addition, listener functions that accept an interface type will be called
-for any dispatched value that implements the specified interface. A listener
-that accepts interface{} will be called for every event type. Listeners can also
-accept pointer types, but they will only be called if the dispatch site calls
-Dispatch() on a pointer.
-*/
 package event
 
 import (
--- go/event/syslogger/syslogger.go
+++ go/event/syslogger/syslogger.go
@@ -14,38 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-/*
-Package syslogger uses the event package to listen for any event that
-implements the Syslogger interface. The listener calls the Syslog method on the
-event, which should return a severity and a message.
-
-The tag for messages sent to syslog will be the program name (os.Args[0]),
-and the facility number will be 1 (user-level).
-
-For example, to declare that your event type MyEvent should be sent to syslog,
-implement the Syslog() method to define how the message should be formatted and
-which severity it should have (see package "log/syslog" for details).
-
-	import (
-		"fmt"
-		"log/syslog"
-		"syslogger"
-	)
-
-	type MyEvent struct {
-		field1, field2 string
-	}
-
-	func (ev *MyEvent) Syslog() (syslog.Priority, string) {
-		return syslog.LOG_INFO, fmt.Sprintf("event: %v, %v", ev.field1, ev.field2)
-	}
-	var _ syslogger.Syslogger = (*MyEvent)(nil) // compile-time interface check
-
-The compile-time interface check is optional but recommended because usually
-there is no other static conversion in these cases. See:
-
-http://golang.org/doc/effective_go.html#blank_implements
-*/
 package syslogger
 
 import (
