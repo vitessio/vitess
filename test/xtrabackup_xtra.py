39c859b1c8493eeea9e86958e38b3921902c7801
deepthi <deepthi@planetscale.com>
2019-03-28 14:33:30 -0700

    better option documentation, make xtrabackupUser a required param for using xtrabackup, loop through existing backups to find the most recent good backup
    Signed-off-by: deepthi <deepthi@planetscale.com>

--- test/xtrabackup_xtra.py
+++ test/xtrabackup_xtra.py
@@ -32,7 +32,10 @@ stream_mode = 'tar'
 xtrabackup_args = ['-backup_engine_implementation',
                    'xtrabackup',
                    '-xtrabackup_stream_mode',
-                   stream_mode]
+                   stream_mode,
+                   '-xtrabackup_user=vt_dba',
+                   '-xtrabackup_backup_flags',
+                   '--password=VtDbaPass']
 
 tablet_master = None
 tablet_replica1 = None
