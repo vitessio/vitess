6235187379f522469125d8e87aa30500556be353
Miriam Lauter <lauter.miriam@gmail.com>
2019-04-09 15:50:51 -0400

    Add vttablet option to set super_read_only during planned reparent
    Signed-off-by: Miriam Lauter <lauter.miriam@gmail.com>

--- go/vt/mysqlctl/replication.go
+++ go/vt/mysqlctl/replication.go
@@ -161,6 +161,17 @@ var (
 	ErrNotMaster = errors.New("no master status")
 )
 
+// SetSuperReadOnly set/unset the super_read_only flag
+func (mysqld *Mysqld) SetSuperReadOnly(on bool) error {
+	query := "SET GLOBAL super_read_only = "
+	if on {
+		query += "ON"
+	} else {
+		query += "OFF"
+	}
+	return mysqld.ExecuteSuperQuery(context.TODO(), query)
+}
+
 // WaitMasterPos lets slaves wait to given replication position
 func (mysqld *Mysqld) WaitMasterPos(ctx context.Context, targetPos mysql.Position) error {
 	// Get a connection.
