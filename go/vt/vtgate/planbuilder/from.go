90e765e65f51158d206fda32a5b5db6d4faf3e1f
Sugu Sougoumarane <ssougou@gmail.com>
2019-04-20 12:49:32 -0700

    vreplication: planbuilder for reference tables
    Signed-off-by: Sugu Sougoumarane <ssougou@gmail.com>

--- go/vt/vtgate/planbuilder/from.go
+++ go/vt/vtgate/planbuilder/from.go
@@ -227,16 +227,12 @@ func (pb *primitiveBuilder) buildTablePrimitive(tableExpr *sqlparser.AliasedTabl
 		switch {
 		case vst.Type == vindexes.TypeSequence:
 			eroute = engine.NewSimpleRoute(engine.SelectNext, vst.Keyspace)
+		case vst.Type == vindexes.TypeReference:
+			eroute = engine.NewSimpleRoute(engine.SelectReference, vst.Keyspace)
 		case !vst.Keyspace.Sharded:
 			eroute = engine.NewSimpleRoute(engine.SelectUnsharded, vst.Keyspace)
-		case vst.Type == vindexes.TypeSequence:
-			eroute = engine.NewSimpleRoute(engine.SelectNext, vst.Keyspace)
 		case vst.Pinned == nil:
-			if vst.Type == vindexes.TypeReference {
-				eroute = engine.NewSimpleRoute(engine.SelectReference, vst.Keyspace)
-			} else {
-				eroute = engine.NewSimpleRoute(engine.SelectScatter, vst.Keyspace)
-			}
+			eroute = engine.NewSimpleRoute(engine.SelectScatter, vst.Keyspace)
 			eroute.TargetDestination = destTarget
 			eroute.TargetTabletType = destTableType
 		default:
