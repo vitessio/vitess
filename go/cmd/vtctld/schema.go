c8508f0721c64d9ef923d5767db1b62cb4f27c00
SataQiu <qiushida@beyondcent.com>
2019-04-24 18:21:07 +0800

    fix some spelling mistakes
    Signed-off-by: SataQiu <qiushida@beyondcent.com>

--- go/cmd/vtctld/schema.go
+++ go/cmd/vtctld/schema.go
@@ -33,7 +33,7 @@ import (
 
 var (
 	schemaChangeDir           = flag.String("schema_change_dir", "", "directory contains schema changes for all keyspaces. Each keyspace has its own directory and schema changes are expected to live in '$KEYSPACE/input' dir. e.g. test_keyspace/input/*sql, each sql file represents a schema change")
-	schemaChangeController    = flag.String("schema_change_controller", "", "schema change controller is responsible for finding schema changes and responsing schema change events")
+	schemaChangeController    = flag.String("schema_change_controller", "", "schema change controller is responsible for finding schema changes and responding to schema change events")
 	schemaChangeCheckInterval = flag.Int("schema_change_check_interval", 60, "this value decides how often we check schema change dir, in seconds")
 	schemaChangeUser          = flag.String("schema_change_user", "", "The user who submits this schema change.")
 	schemaChangeSlaveTimeout  = flag.Duration("schema_change_slave_timeout", 10*time.Second, "how long to wait for slaves to receive the schema change")
