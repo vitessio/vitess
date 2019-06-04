af81ebc10703ed69096bc09c5bca37be4a4344b1
Anthony Yeh <enisoc@planetscale.com>
2019-06-04 16:14:50 -0700

    vtbackup: Document and clean up.
    Signed-off-by: Anthony Yeh <enisoc@planetscale.com>

--- go/cmd/vtbackup/vtbackup.go
+++ go/cmd/vtbackup/vtbackup.go
@@ -14,16 +14,54 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// vt backup job: Restore a Backup and Takes a new backup
+/*
+vtbackup is a batch command to perform a single pass of backup maintenance for a shard.
+
+When run periodically for each shard, vtbackup can ensure these configurable policies:
+* There is always a recent backup for the shard.
+* Old backups for the shard are removed.
+
+Whatever system launches vtbackup is responsible for the following:
+* Running vtbackup with similar flags that would be used for a vttablet in the
+  target shard to be backed up.
+* Running mysqlctld alongside vtbackup, as it would be alongside vttablet.
+* Provisioning as much disk space for vtbackup as would be given to vttablet.
+  The data directory MUST be empty at startup. Do NOT reuse a persistent disk.
+* Running vtbackup periodically for each shard, for each backup storage location.
+* Ensuring that at most one instance runs at a time for a given pair of shard
+  and backup storage location.
+* Retrying vtbackup if it fails.
+* Alerting human operators if the failure is persistent.
+
+The process vtbackup follows to take a new backup is as follows:
+1. Restore from the most recent backup.
+2. Start a mysqld instance (but no vttablet) from the restored data.
+3. Instruct mysqld to connect to the current shard master and replicate any
+   transactions that are new since the last backup.
+4. Wait until replication is caught up to the master.
+5. Stop mysqld and take a new backup.
+
+Aside from additional replication load while vtbackup's mysqld catches up on
+new transactions, the shard should be otherwise unaffected. Existing tablets
+will continue to serve, and no new tablets will appear in topology, meaning no
+query traffic will ever be routed to vtbackup's mysqld. This silent operation
+mode helps make backups minimally disruptive to serving capacity and orthogonal
+to the handling of the query path.
+
+The command-line parameters to vtbackup specify a policy for when a new backup
+is needed, and when old backups should be removed. If the existing backups
+already satisfy the policy, then vtbackup will do nothing and return success
+immediately.
+*/
 package main
 
 import (
 	"context"
 	"flag"
 	"fmt"
-	"strconv"
 	"time"
 
+	"vitess.io/vitess/go/exit"
 	"vitess.io/vitess/go/mysql"
 	"vitess.io/vitess/go/vt/dbconfigs"
 	"vitess.io/vitess/go/vt/log"
@@ -41,18 +79,25 @@ var (
 	initShard                = flag.String("init_shard", "", "(init parameter) shard to use for this tablet")
 	tabletPath               = flag.String("tablet-path", "", "tablet alias")
 	concurrency              = flag.Int("concurrency", 4, "(init restore parameter) how many concurrent files to restore at once")
-	acceptableReplicationLag = flag.Duration("acceptable_replication_lag", 0, "set what the is the acceptable replication lag to wait for before a backup is taken")
+	acceptableReplicationLag = flag.Duration("acceptable_replication_lag", 1*time.Second, "Wait until replication lag is less than or equal to this value before taking a new backup")
+	timeout                  = flag.Duration("timeout", 1*time.Hour, "Overall timeout for this vtbackup run")
 )
 
 func main() {
+	defer exit.Recover()
+
 	dbconfigs.RegisterFlags(dbconfigs.All...)
 	mysqlctl.RegisterFlags()
 
 	servenv.ParseFlags("vtbackup")
 
+	ctx, cancel := context.WithTimeout(context.Background(), *timeout)
+	defer cancel()
+
 	tabletAlias, err := topoproto.ParseTabletAlias(*tabletPath)
 	if err != nil {
-		log.Exitf("failed to parse -tablet-path: %v", err)
+		log.Errorf("failed to parse -tablet-path: %v", err)
+		exit.Return(1)
 	}
 
 	dbName := *initDbNameOverride
@@ -62,71 +107,91 @@ func main() {
 
 	var mycnf *mysqlctl.Mycnf
 	var socketFile string
-	extraEnv := map[string]string{"TABLET_ALIAS": strconv.FormatUint(uint64(tabletAlias.Uid), 10)}
+	extraEnv := map[string]string{
+		"TABLET_ALIAS": topoproto.TabletAliasString(tabletAlias),
+	}
 
-	if !dbconfigs.HasConnectionParams() {
+	if dbconfigs.HasConnectionParams() {
+		log.Info("connection parameters were specified. Not loading my.cnf.")
+	} else {
 		var err error
 		if mycnf, err = mysqlctl.NewMycnfFromFlags(tabletAlias.Uid); err != nil {
-			log.Exitf("mycnf read failed: %v", err)
+			log.Errorf("mycnf read failed: %v", err)
+			exit.Return(1)
 		}
 		socketFile = mycnf.SocketFile
-	} else {
-		log.Info("connection parameters were specified. Not loading my.cnf.")
 	}
 
 	dbcfgs, err := dbconfigs.Init(socketFile)
 	if err != nil {
-		log.Warning(err)
+		log.Errorf("can't initialize dbconfigs: %v", err)
+		exit.Return(1)
 	}
 
 	topoServer := topo.Open()
 	mysqld := mysqlctl.NewMysqld(dbcfgs)
 	dir := fmt.Sprintf("%v/%v", *initKeyspace, *initShard)
 
-	pos, err := mysqlctl.Restore(context.Background(), mycnf, mysqld, dir, *concurrency, extraEnv, map[string]string{}, logutil.NewConsoleLogger(), true, dbName)
+	log.Infof("Restoring latest backup from directory %v", dir)
+	pos, err := mysqlctl.Restore(ctx, mycnf, mysqld, dir, *concurrency, extraEnv, map[string]string{}, logutil.NewConsoleLogger(), true, dbName)
 	switch err {
 	case nil:
-		// Horray Evenything worked
-		// We have restored a backup ( If one existed, now make sure replicatoin is started
-		if err := resetReplication(context.Background(), pos, mysqld); err != nil {
-			log.Fatalf("Error Starting Replication %v", err)
+		log.Info("Successfully restored from backup at replication position %v", pos)
+	case mysqlctl.ErrNoBackup:
+		log.Error("No backup found. Not starting up empty since -initial_backup flag was not enabled.")
+		exit.Return(1)
+	case mysqlctl.ErrExistingDB:
+		log.Error("Can't run vtbackup because data directory is not empty.")
+		exit.Return(1)
+	default:
+		log.Errorf("Error restoring from backup: %v", err)
+		exit.Return(1)
+	}
+
+	// We have restored a backup. Now start replication.
+	if err := resetReplication(ctx, pos, mysqld); err != nil {
+		log.Errorf("Error resetting replication %v", err)
+		exit.Return(1)
+	}
+	if err := startReplication(ctx, pos, mysqld, topoServer); err != nil {
+		log.Errorf("Error starting replication %v", err)
+		exit.Return(1)
+	}
+
+	// Wait for replication to catch up.
+	waitStartTime := time.Now()
+	for {
+		time.Sleep(time.Second)
+
+		// Check if the context is still good.
+		if err := ctx.Err(); err != nil {
+			log.Errorf("Timed out waiting for replication to catch up to within %v.", *acceptableReplicationLag)
+			exit.Return(1)
 		}
 
-	case mysqlctl.ErrNoBackup:
-		// No-op, starting with empty database.
-	case mysqlctl.ErrExistingDB:
-		// No-op, assuming we've just restarted.  Note the
-		// replication reporter may restart replication at the
-		// next health check if it thinks it should. We do not
-		// alter replication here.
-	default:
-		log.Fatalf("Error restoring backup: %v", err)
-	}
-
-	// We have restored a backup ( If one existed, now make sure replicatoin is started
-	if err := startReplication(context.Background(), pos, mysqld, topoServer); err != nil {
-		log.Fatalf("Error Starting Replication %v", err)
-	}
-
-	for {
 		status, statusErr := mysqld.SlaveStatus()
 		if statusErr != nil {
-			log.Warning("Error getting Slave Status (%v)", statusErr)
-		} else if time.Duration(status.SecondsBehindMaster)*time.Second <= *acceptableReplicationLag {
+			log.Warningf("Error getting replication status: %v", statusErr)
+			continue
+		}
+		if time.Duration(status.SecondsBehindMaster)*time.Second <= *acceptableReplicationLag {
+			// We're caught up on replication.
+			log.Infof("Replication caught up to within %v after %v", *acceptableReplicationLag, time.Since(waitStartTime))
 			break
 		}
 		if !status.SlaveRunning() {
-			log.Warning("Slave has stopped before backup could be taken")
-			startReplication(context.Background(), pos, mysqld, topoServer)
+			log.Warning("Replication has stopped before backup could be taken. Trying to restart replication.")
+			if err := startReplication(ctx, pos, mysqld, topoServer); err != nil {
+				log.Warningf("Failed to restart replication: %v", err)
+			}
 		}
-		time.Sleep(time.Second)
 	}
 
-	// now we can run the backup
+	// Now we can take a new backup.
 	name := fmt.Sprintf("%v.%v", time.Now().UTC().Format("2006-01-02.150405"), topoproto.TabletAliasString(tabletAlias))
-	returnErr := mysqlctl.Backup(context.Background(), mycnf, mysqld, logutil.NewConsoleLogger(), dir, name, *concurrency, extraEnv)
-	if returnErr != nil {
-		log.Fatalf("Error taking backup: %v", returnErr)
+	if err := mysqlctl.Backup(ctx, mycnf, mysqld, logutil.NewConsoleLogger(), dir, name, *concurrency, extraEnv); err != nil {
+		log.Errorf("Error taking backup: %v", err)
+		exit.Return(1)
 	}
 }
 
