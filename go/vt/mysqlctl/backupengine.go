d08a9e9a26234f03b03d4c69571fa4667ab359dd
deepthi <deepthi@planetscale.com>
2019-06-05 14:15:21 -0700

    restore: refactor common code, add functions
    Signed-off-by: deepthi <deepthi@planetscale.com>

--- go/vt/mysqlctl/backupengine.go
+++ go/vt/mysqlctl/backupengine.go
@@ -18,9 +18,12 @@ package mysqlctl
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"flag"
 
 	"vitess.io/vitess/go/mysql"
+	"vitess.io/vitess/go/vt/log"
 	"vitess.io/vitess/go/vt/logutil"
 	"vitess.io/vitess/go/vt/mysqlctl/backupstorage"
 	"vitess.io/vitess/go/vt/proto/vtrpc"
@@ -49,4 +52,55 @@ func GetBackupEngine() (BackupEngine, error) {
 		return nil, vterrors.New(vtrpc.Code_NOT_FOUND, "no registered implementation of BackupEngine")
 	}
 	return be, nil
+}
+
+func findBackupToRestore(ctx context.Context, cnf *Mycnf, mysqld MysqlDaemon, logger logutil.Logger, dir string, bhs []backupstorage.BackupHandle, bm interface{}) (backupstorage.BackupHandle, error) {
+	var bh backupstorage.BackupHandle
+	var index int
+
+	for index = len(bhs) - 1; index >= 0; index-- {
+		bh = bhs[index]
+		rc, err := bh.ReadFile(ctx, backupManifest)
+		if err != nil {
+			log.Warningf("Possibly incomplete backup %v in directory %v on BackupStorage: can't read MANIFEST: %v)", bh.Name(), dir, err)
+			continue
+		}
+
+		err = json.NewDecoder(rc).Decode(&bm)
+		rc.Close()
+		if err != nil {
+			log.Warningf("Possibly incomplete backup %v in directory %v on BackupStorage (cannot JSON decode MANIFEST: %v)", bh.Name(), dir, err)
+			continue
+		}
+
+		logger.Infof("Restore: found backup %v %v to restore", bh.Directory(), bh.Name())
+		break
+	}
+	if index < 0 {
+		// There is at least one attempted backup, but none could be read.
+		// This implies there is data we ought to have, so it's not safe to start
+		// up empty.
+		return nil, errors.New("backup(s) found but none could be read, unsafe to start up empty, restart to retry restore")
+	}
+
+	// Starting from here we won't be able to recover if we get stopped by a cancelled
+	// context. Thus we use the background context to get through to the finish.
+
+	logger.Infof("Restore: shutdown mysqld")
+	err := mysqld.Shutdown(context.Background(), cnf, true)
+	if err != nil {
+		return nil, err
+	}
+
+	logger.Infof("Restore: deleting existing files")
+	if err := removeExistingFiles(cnf); err != nil {
+		return nil, err
+	}
+
+	logger.Infof("Restore: reinit config file")
+	err = mysqld.ReinitConfig(context.Background(), cnf)
+	if err != nil {
+		return nil, err
+	}
+	return bh, nil
 }
