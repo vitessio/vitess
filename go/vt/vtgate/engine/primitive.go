db0c37e1150f1a1a67f486c0463e63bcc5300092
Sugu Sougoumarane <ssougou@gmail.com>
2019-06-15 09:52:04 -0700

    vtgate sql: in-memory sorting, initial cut
    The planbuilder and tests for it are ready. Still need tests for
    the engine primitive.
    
    Signed-off-by: Sugu Sougoumarane <ssougou@gmail.com>

--- go/vt/vtgate/engine/primitive.go
+++ go/vt/vtgate/engine/primitive.go
@@ -85,15 +85,15 @@ type Plan struct {
 	// Mutex to protect the stats
 	mu sync.Mutex
 	// Count of times this plan was executed
-	ExecCount uint64
+	ExecCount uint64 `json:",omitempty"`
 	// Total execution time
-	ExecTime time.Duration
+	ExecTime time.Duration `json:",omitempty"`
 	// Total number of shard queries
-	ShardQueries uint64
+	ShardQueries uint64 `json:",omitempty"`
 	// Total number of rows
-	Rows uint64
+	Rows uint64 `json:",omitempty"`
 	// Total number of errors
-	Errors uint64
+	Errors uint64 `json:",omitempty"`
 }
 
 // AddStats updates the plan execution statistics
