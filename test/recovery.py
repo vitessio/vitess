3f8ad0d1756abaeda22903c979ed43df3989751f
deepthi <deepthi@planetscale.com>
2019-06-24 13:19:39 -0700

    add multi-recovery integration test
    Signed-off-by: deepthi <deepthi@planetscale.com>

--- test/recovery.py
+++ test/recovery.py
@@ -32,19 +32,22 @@ use_mysqlctld = False
 tablet_master = None
 tablet_replica1 = None
 tablet_replica2 = None
+tablet_replica3 = None
 new_init_db = ''
 db_credentials_file = ''
 
 
 def setUpModule():
   global new_init_db, db_credentials_file
-  global tablet_master, tablet_replica1, tablet_replica2
+  global tablet_master, tablet_replica1, tablet_replica2, tablet_replica3
 
   tablet_master = tablet.Tablet(use_mysqlctld=use_mysqlctld,
                                 vt_dba_passwd='VtDbaPass')
   tablet_replica1 = tablet.Tablet(use_mysqlctld=use_mysqlctld,
                                   vt_dba_passwd='VtDbaPass')
   tablet_replica2 = tablet.Tablet(use_mysqlctld=use_mysqlctld,
+                                  vt_dba_passwd='VtDbaPass')
+  tablet_replica3 = tablet.Tablet(use_mysqlctld=use_mysqlctld,
                                   vt_dba_passwd='VtDbaPass')
 
   try:
@@ -89,11 +92,15 @@ FLUSH PRIVILEGES;
         tablet_replica2.init_mysql(init_db=new_init_db,
                                    extra_args=['-db-credentials-file',
                                                db_credentials_file]),
+        tablet_replica3.init_mysql(init_db=new_init_db,
+                                   extra_args=['-db-credentials-file',
+                                               db_credentials_file]),
     ]
     if use_mysqlctld:
       tablet_master.wait_for_mysqlctl_socket()
       tablet_replica1.wait_for_mysqlctl_socket()
       tablet_replica2.wait_for_mysqlctl_socket()
+      tablet_replica3.wait_for_mysqlctl_socket()
     else:
       utils.wait_procs(setup_procs)
   except:
@@ -113,6 +120,8 @@ def tearDownModule():
                                                  db_credentials_file]),
       tablet_replica2.teardown_mysql(extra_args=['-db-credentials-file',
                                                  db_credentials_file]),
+      tablet_replica3.teardown_mysql(extra_args=['-db-credentials-file',
+                                                 db_credentials_file]),
   ]
   utils.wait_procs(teardown_procs, raise_on_error=False)
 
@@ -123,6 +132,7 @@ def tearDownModule():
   tablet_master.remove_tree()
   tablet_replica1.remove_tree()
   tablet_replica2.remove_tree()
+  tablet_replica3.remove_tree()
 
 def get_connection(timeout=15.0):
   protocol, endpoint = utils.vtgate.rpc_endpoint(python=True)
@@ -149,12 +159,12 @@ class TestVttabletRecovery(unittest.TestCase):
                      tablet_master.tablet_alias])
 
   def tearDown(self):
-    for t in tablet_master, tablet_replica1, tablet_replica2:
+    for t in tablet_master, tablet_replica1, tablet_replica2, tablet_replica3:
       t.kill_vttablet()
 
     tablet.Tablet.check_vttablet_count()
     environment.topo_server().wipe()
-    for t in [tablet_master, tablet_replica1, tablet_replica2]:
+    for t in [tablet_master, tablet_replica1, tablet_replica2, tablet_replica3]:
       t.reset_replication()
       t.set_semi_sync_enabled(master=False, slave=False)
       t.clean_dbs()
@@ -198,7 +208,7 @@ class TestVttabletRecovery(unittest.TestCase):
         logging.exception('exception waiting for data to replicate')
       timeout = utils.wait_step(msg, timeout)
 
-  def _restore(self, t):
+  def _restore(self, t, keyspace):
     """Erase mysql/tablet dir, then start tablet with restore enabled."""
     self._reset_tablet_dir(t)
 
@@ -208,7 +218,7 @@ class TestVttabletRecovery(unittest.TestCase):
                      '-base_keyspace=test_keyspace',
                      '-snapshot_time',
                      datetime.datetime.utcnow().isoformat("T")+"Z",
-                     'recovery_keyspace'])
+                     keyspace])
     
     # set disable_active_reparents to true, otherwise replication_reporter will
     # try to restart replication
@@ -220,7 +230,7 @@ class TestVttabletRecovery(unittest.TestCase):
 
     t.start_vttablet(wait_for_state='SERVING',
                      init_tablet_type='replica',
-                     init_keyspace='recovery_keyspace',
+                     init_keyspace=keyspace,
                      init_shard='0',
                      supports_backups=True,
                      extra_args=xtra_args)
@@ -252,7 +262,7 @@ class TestVttabletRecovery(unittest.TestCase):
         ['RemoveBackup', 'test_keyspace/0', backup],
         auto_log=True, mode=utils.VTCTL_VTCTL)
 
-  def test_recovery(self):
+  def test_basic_recovery(self):
     """Test recovery from backup flow.
 
     test_recovery will:
@@ -261,9 +271,10 @@ class TestVttabletRecovery(unittest.TestCase):
     - insert some data
     - take a backup
     - insert more data on the master
-    - bring up tablet_replica2 after the fact, let it restore the backup
-    - check all data is right (before+after backup data)
-    - list the backup, remove it
+    - create a recovery keyspace
+    - bring up tablet_replica2 in the new keyspace
+    - check that new tablet does not have data created after backup
+    - check that vtgate queries work correctly
 
     """
 
@@ -292,16 +303,23 @@ class TestVttabletRecovery(unittest.TestCase):
                      'test_keyspace'],
                     auto_log=True)
 
-    vs, _ = utils.run_vtctl(['GetVSchema', 'test_keyspace'],
-                                 mode=utils.VTCTL_VTCTL, trap_output=True)
-    logging.debug('test_keyspace vschema: %s', vs.splitlines())
+    vs = utils.run_vtctl_json(['GetVSchema', 'test_keyspace'])
+    logging.debug('test_keyspace vschema: %s', str(vs))
+    ks = utils.run_vtctl_json(['GetSrvKeyspace', 'test_nj', 'test_keyspace'])
+    logging.debug('Serving keyspace before: %s', str(ks))
+    vs = utils.run_vtctl_json(['GetSrvVSchema', 'test_nj'])
+    logging.debug('Serving vschema before recovery: %s', str(vs))
 
     # now bring up the other slave, letting it restore from backup.
-    self._restore(tablet_replica2)
+    self._restore(tablet_replica2, 'recovery_keyspace')
 
-    vs, _ = utils.run_vtctl(['GetVSchema', 'recovery_keyspace'],
-                            mode=utils.VTCTL_VTCTL, trap_output=True)
-    logging.debug('recovery_keyspace vschema: %s', vs.splitlines())
+    vs = utils.run_vtctl_json(['GetSrvVSchema', 'test_nj'])
+    logging.debug('Serving vschema after recovery: %s', str(vs))
+    ks = utils.run_vtctl_json(['GetSrvKeyspace', 'test_nj', 'test_keyspace'])
+    logging.debug('Serving keyspace after: %s', str(ks))
+    vs = utils.run_vtctl_json(['GetVSchema', 'recovery_keyspace'])
+    logging.debug('recovery_keyspace vschema: %s', str(vs))
+
     # check the new slave does not have the data
     self._check_data(tablet_replica2, 1, 'replica2 tablet should not have new data')
 
@@ -315,7 +333,8 @@ class TestVttabletRecovery(unittest.TestCase):
     self.assertEqual(metadata['DataCenter'], 'test_nj')
 
     # start vtgate
-    utils.VtGate().start(tablets=[
+    vtgate = utils.VtGate()
+    vtgate.start(tablets=[
       tablet_master, tablet_replica1, tablet_replica2
       ], tablet_types_to_wait='REPLICA')
     utils.vtgate.wait_for_endpoints('test_keyspace.0.master', 1)
@@ -363,6 +382,139 @@ class TestVttabletRecovery(unittest.TestCase):
 
     vtgate_conn.close()
     tablet_replica2.kill_vttablet()
+    vtgate.kill()
+
+  def test_multi_recovery(self):
+    """Test recovery from backup flow.
+
+    test_multi_recovery will:
+    - create a shard with master and replica1 only
+    - run InitShardMaster
+    - insert some data
+    - take a backup
+    - insert more data on the master
+    - take another backup
+    - create a recovery keyspace after first backup
+    - bring up tablet_replica2 in the new keyspace
+    - check that new tablet does not have data created after backup1
+    - create second recovery keyspace after second backup
+    - bring up tablet_replica3 in second keyspace
+    - check that new tablet has data created after backup1 but not data created after backup2
+    - check that vtgate queries work correctly
+
+    """
+
+    # insert data on master, wait for slave to get it
+    utils.run_vtctl(['ApplySchema',
+                     '-sql', self._create_vt_insert_test,
+                     'test_keyspace'],
+                    auto_log=True)
+    self._insert_data(tablet_master, 1)
+    self._check_data(tablet_replica1, 1, 'replica1 tablet getting data')
+
+    # backup the slave
+    utils.run_vtctl(['Backup', tablet_replica1.tablet_alias], auto_log=True)
+
+    # check that the backup shows up in the listing
+    backups = self._list_backups()
+    logging.debug('list of backups: %s', backups)
+    self.assertEqual(len(backups), 1)
+    self.assertTrue(backups[0].endswith(tablet_replica1.tablet_alias))
+
+    # insert more data on the master
+    self._insert_data(tablet_master, 2)
+    # wait for it to replicate
+    self._check_data(tablet_replica1, 2, 'replica1 tablet getting data')
+
+    utils.run_vtctl(['ApplyVSchema',
+                     '-vschema', self._vschema_json,
+                     'test_keyspace'],
+                    auto_log=True)
+
+    vs = utils.run_vtctl_json(['GetVSchema', 'test_keyspace'])
+    logging.debug('test_keyspace vschema: %s', str(vs))
+
+    # now bring up the other slave, letting it restore from backup.
+    self._restore(tablet_replica2, 'recovery_ks1')
+
+    # we are not asserting on the contents of vschema here
+    # because the later part of the test (vtgate) will fail
+    # if the vschema is not copied correctly from the base_keyspace
+    vs = utils.run_vtctl_json(['GetVSchema', 'recovery_ks1'])
+    logging.debug('recovery_ks1 vschema: %s', str(vs))
+
+    # check the new slave does not have the data
+    self._check_data(tablet_replica2, 1, 'replica2 tablet should not have new data')
+
+    # take another backup on the slave
+    utils.run_vtctl(['Backup', tablet_replica1.tablet_alias], auto_log=True)
+
+    # insert more data on the master
+    self._insert_data(tablet_master, 3)
+    # wait for it to replicate
+    self._check_data(tablet_replica1, 3, 'replica1 tablet getting data')
+
+    # now bring up the other slave, letting it restore from backup2.
+    self._restore(tablet_replica3, 'recovery_ks2')
+
+    vs = utils.run_vtctl(['GetVSchema', 'recovery_ks2'])
+    logging.debug('recovery_ks2 vschema: %s', str(vs))
+
+    # check the new slave does not have the latest data
+    self._check_data(tablet_replica3, 2, 'replica3 tablet should not have new data')
+
+    # start vtgate
+    vtgate = utils.VtGate()
+    vtgate.start(tablets=[
+      tablet_master, tablet_replica1, tablet_replica2, tablet_replica3
+      ], tablet_types_to_wait='REPLICA')
+    utils.vtgate.wait_for_endpoints('test_keyspace.0.master', 1)
+    utils.vtgate.wait_for_endpoints('test_keyspace.0.replica', 1)
+    utils.vtgate.wait_for_endpoints('recovery_ks1.0.replica', 1)
+    utils.vtgate.wait_for_endpoints('recovery_ks2.0.replica', 1)
+
+    # check that vtgate doesn't route queries to new tablet
+    vtgate_conn = get_connection()
+    cursor = vtgate_conn.cursor(
+        tablet_type='replica', keyspace=None, writable=True)
+
+    cursor.execute('select count(*) from vt_insert_test', {})
+    result = cursor.fetchall()
+    if not result:
+      self.fail('Result cannot be null')
+    else:
+      self.assertEqual(result[0][0], 3)
+
+    # check that new tablet is accessible by using ks.table
+    cursor.execute('select count(*) from recovery_ks1.vt_insert_test', {})
+    result = cursor.fetchall()
+    if not result:
+      self.fail('Result cannot be null')
+    else:
+      self.assertEqual(result[0][0], 1)
+
+    # check that new tablet is accessible by using ks.table
+    cursor.execute('select count(*) from recovery_ks2.vt_insert_test', {})
+    result = cursor.fetchall()
+    if not result:
+      self.fail('Result cannot be null')
+    else:
+      self.assertEqual(result[0][0], 2)
+
+    # TODO check that new tablet is accessible with 'use ks:shard'
+    # this currently does not work through the python client, though it works from mysql client
+    #cursor.execute('use recovery_ks1:0@replica', {})
+    #cursor.execute('select count(*) from vt_insert_test', {})
+    #result = cursor.fetchall()
+    #if not result:
+      #self.fail('Result cannot be null')
+    #else:
+      #self.assertEqual(result[0][0], 1)
+
+    vtgate_conn.close()
+    tablet_replica2.kill_vttablet()
+    tablet_replica3.kill_vttablet()
+    vtgate.kill()
 
 if __name__ == '__main__':
   utils.main()
