2b9cb6e4eb3a22d04576fb650e86880566f0937a
Sugu Sougoumarane <ssougou@gmail.com>
2019-07-07 12:44:42 -0700

    migrater: more tests and cleanup
    Signed-off-by: Sugu Sougoumarane <ssougou@gmail.com>

--- go/vt/wrangler/fake_dbclient_test.go
+++ go/vt/wrangler/fake_dbclient_test.go
@@ -19,35 +19,46 @@ package wrangler
 import (
 	"fmt"
 	"regexp"
+	"testing"
 
 	"vitess.io/vitess/go/sqltypes"
 )
 
+func verifyQueries(t *testing.T, dcs []*fakeDBClient) {
+	for _, dc := range dcs {
+		dc.verifyQueries(t)
+	}
+}
+
 type dbResult struct {
 	result *sqltypes.Result
 	err    error
+	called bool
 }
 
 // fakeDBClient fakes a binlog_player.DBClient.
 type fakeDBClient struct {
 	queries   map[string]*dbResult
-	queriesRE map[*regexp.Regexp]*dbResult
+	queriesRE map[string]*dbResult
 }
 
 // NewfakeDBClient returns a new DBClientMock.
 func newFakeDBClient() *fakeDBClient {
 	return &fakeDBClient{
-		queries:   make(map[string]*dbResult),
-		queriesRE: make(map[*regexp.Regexp]*dbResult),
+		queries: map[string]*dbResult{
+			"use _vt": {result: &sqltypes.Result{}, called: true},
+			"select * from _vt.vreplication where db_name='db'": {result: &sqltypes.Result{}},
+		},
+		queriesRE: make(map[string]*dbResult),
 	}
 }
 
-func (dc *fakeDBClient) setResult(query string, result *sqltypes.Result, err error) {
+func (dc *fakeDBClient) addQuery(query string, result *sqltypes.Result, err error) {
 	dc.queries[query] = &dbResult{result: result, err: err}
 }
 
-func (dc *fakeDBClient) setResultRE(query string, result *sqltypes.Result, err error) {
-	dc.queriesRE[regexp.MustCompile(query)] = &dbResult{result: result, err: err}
+func (dc *fakeDBClient) addQueryRE(query string, result *sqltypes.Result, err error) {
+	dc.queriesRE[query] = &dbResult{result: result, err: err}
 }
 
 // DBName is part of the DBClient interface
@@ -82,12 +93,28 @@ func (dc *fakeDBClient) Close() {
 // ExecuteFetch is part of the DBClient interface
 func (dc *fakeDBClient) ExecuteFetch(query string, maxrows int) (qr *sqltypes.Result, err error) {
 	if dbr := dc.queries[query]; dbr != nil {
+		dbr.called = true
 		return dbr.result, dbr.err
 	}
 	for re, dbr := range dc.queriesRE {
-		if re.MatchString(query) {
+		if regexp.MustCompile(re).MatchString(query) {
+			dbr.called = true
 			return dbr.result, dbr.err
 		}
 	}
 	return nil, fmt.Errorf("unexpected query: %s", query)
+}
+
+func (dc *fakeDBClient) verifyQueries(t *testing.T) {
+	t.Helper()
+	for query, dbr := range dc.queries {
+		if !dbr.called {
+			t.Errorf("query: %v was not called", query)
+		}
+	}
+	for query, dbr := range dc.queriesRE {
+		if !dbr.called {
+			t.Errorf("query: %v was not called", query)
+		}
+	}
 }
