c4398b3d75830c672193f3b82a0ae05621f13bc0
John Watson <johnw@planetscale.com>
2019-07-11 00:09:22 -0700

    Standard unix directories
    Signed-off-by: John Watson <johnw@planetscale.com>

--- docker/packaging/package_vitess.sh
+++ docker/packaging/package_vitess.sh
@@ -7,18 +7,21 @@ fi
 
 set -eu
 
+PREFIX=${PREFIX:-/usr}
+
 inputs_file="/vt/packaging/inputs"
 cat <<EOF >> "${inputs_file}"
-/vt/bin/mysqlctld
-/vt/bin/vtctlclient
-/vt/bin/vtctld
-/vt/bin/vtgate
-/vt/bin/vttablet
-/vt/bin/vtworker
-/vt/packaging/etc_default_vitess=/etc/default/vitess
-/vt/src/vitess.io/vitess/config=/vt
-/vt/src/vitess.io/vitess/web/vtctld
-/vt/src/vitess.io/vitess/web/vtctld2/app
+/vt/bin/mysqlctld=${PREFIX}/bin/mysqlctld
+/vt/bin/vtbackup=${PREFIX}/bin/vtbackup
+/vt/bin/vtctl=${PREFIX}/bin/vtctl
+/vt/bin/vtctlclient=${PREFIX}/bin/vtctlclient
+/vt/bin/vtctld=${PREFIX}/bin/vtctld
+/vt/bin/vtgate=${PREFIX}/bin/vtgate
+/vt/bin/vttablet=${PREFIX}/bin/vttablet
+/vt/bin/vtworker=${PREFIX}/bin/vtworker
+/vt/src/vitess.io/vitess/config/=/etc/vitess
+/vt/src/vitess.io/vitess/web/vtctld2/app=${PREFIX}/lib/vitess/web/vtcld2
+/vt/src/vitess.io/vitess/web/vtctld=${PREFIX}/lib/vitess/web
 EOF
 
 description='A database clustering system for horizontal scaling of MySQL
@@ -36,14 +39,8 @@ exec /usr/local/bin/fpm \
     --url "https://vitess.io/" \
     --description "${description}" \
     --license "Apache License - Version 2.0, January 2004" \
-    --config-files "/vt/config" \
-    --config-files "/etc/default/vitess" \
-    --directories "/vt/bin" \
-    --directories "/vt/src" \
     --inputs "${inputs_file}" \
+    --config-files "/etc/vitess" \
+    --directories "${PREFIX}/lib/vitess" \
     --before-install "/vt/packaging/preinstall.sh" \
-    --deb-user vitess \
-    --deb-group vitess \
-    --rpm-user vitess \
-    --rpm-group vitess \
     "${@}"
