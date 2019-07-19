32f27d8193056d8942fae0852bdd1866027f533f
Rasika Kale <rasika@planetscale.com>
2019-07-19 13:38:25 -0700

    updated env.sh file to resolve conflict from pull request
    Signed-off-by: Rasika Kale <rasika@planetscale.com>

--- examples/local/env.sh
+++ examples/local/env.sh
@@ -40,44 +40,38 @@ if [ -z "$MYSQL_FLAVOR" ]; then
 fi
 
 if [ "${TOPO}" = "zk2" ]; then
-    
-    TOPOLOGY_FLAGS="-topo_implementation zk2 -topo_global_server_address ${ZK_SERVER} -topo_global_root /vitess/global"
-
-    mkdir -p ${VTDATAROOT}/tmp
-   
-else
-
     # Each ZooKeeper server needs a list of all servers in the quorum.
     # Since we're running them all locally, we need to give them unique ports.
     # In a real deployment, these should be on different machines, and their
     # respective hostnames should be given.
+    echo "enter zk2 env"
+    zkcfg=(\
+        "1@$hostname:28881:38881:21811" \
+        "2@$hostname:28882:38882:21812" \
+        "3@$hostname:28883:38883:21813" \
+        )
+    printf -v zkcfg ",%s" "${zkcfg[@]}"
+    zkcfg=${zkcfg:1}
 
-  echo "enter etcd2 env"
+    zkids='1 2 3'
+
+    # Set topology environment parameters.
+    ZK_SERVER="localhost:21811,localhost:21812,localhost:21813"
+    # shellcheck disable=SC2034
+    TOPOLOGY_FLAGS="-topo_implementation zk2 -topo_global_server_address ${ZK_SERVER} -topo_global_root /vitess/global"
+
+    mkdir -p $VTDATAROOT/tmp
+else
+    echo "enter etcd2 env"
 
     case $(uname) in
       Linux)  etcd_platform=linux;;
       Darwin) etcd_platform=darwin;;
     esac
 
-    
-
-    #echo "enter zk2 env"
-    #zkcfg=(\
-    #    "1@$hostname:28881:38881:21811" \
-    #    "2@$hostname:28882:38882:21812" \
-    #    "3@$hostname:28883:38883:21813" \
-    #    )
-    #printf -v zkcfg ",%s" "${zkcfg[@]}"
-    #zkcfg=${zkcfg:1}
-
-    #zkids='1 2 3'
-
     ETCD_SERVER="localhost:2379"
     ETCD_VERSION=$(cat "${VTROOT}/dist/etcd/.installed_version")
     ETCD_BINDIR="${VTROOT}/dist/etcd/etcd-${ETCD_VERSION}-${etcd_platform}-amd64/"
-    # Set topology environment parameters.
-    #ZK_SERVER="localhost:21811,localhost:21812,localhost:21813"
-    # shellcheck disable=SC2034
     TOPOLOGY_FLAGS="-topo_implementation etcd2 -topo_global_server_address $ETCD_SERVER -topo_global_root /vitess/global"
 
     mkdir -p "${VTDATAROOT}/tmp"
