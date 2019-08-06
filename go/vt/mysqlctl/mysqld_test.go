1ecd822beb2cbc37a94981a1ad9fa68d0db7797b
Morgan Tocker <tocker@gmail.com>
2019-08-06 10:35:31 -0700

    update copyright
    Signed-off-by: Morgan Tocker <tocker@gmail.com>

--- go/vt/mysqlctl/mysqld_test.go
+++ go/vt/mysqlctl/mysqld_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 Google Inc.
+Copyright 2019 The Vitess Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -19,7 +19,6 @@ package mysqlctl
 import (
 	"os"
 	"testing"
-	//	"vitess.io/vitess/go/vt/env"
 )
 
 type testcase struct {
