f8519c29ea304eff9d67361c76d142327bdd88c3
Morgan Tocker <tocker@gmail.com>
2019-08-08 00:41:43 -0600

    Add initial support for capabilities (#4987)
    * Add initial support for capabilities
    
    This eliminates the need to set MYSQL_FLAVOR
    
    Signed-off-by: Morgan Tocker <tocker@gmail.com>
    
    * Removed MYSQL_FLAVOR variable precedence
    
    It will cause backwards compatibility problems,
    since many users set "MySQL56" for any MySQL system.
    
    Signed-off-by: Morgan Tocker <tocker@gmail.com>
    
    * Address PR feedback (WIP)
    
    Signed-off-by: Morgan Tocker <tocker@gmail.com>
    
    * Fixed mysql_safe to not use capabilitities
    
    Reverted small changes to configs
    
    Signed-off-by: Morgan Tocker <tocker@gmail.com>
    
    * fix version detection
    
    Signed-off-by: Morgan Tocker <tocker@gmail.com>
    
    * Fix style, re-add EXTRA_MY_CNF from review
    
    Signed-off-by: Morgan Tocker <tocker@gmail.com>
    
    * Address review feedback
    
    Signed-off-by: Morgan Tocker <tocker@gmail.com>
    
    * address remaining feedback
    
    Signed-off-by: Morgan Tocker <tocker@gmail.com>
    
    * Remove rogue added space
    
    Signed-off-by: Morgan Tocker <tocker@gmail.com>
    
    * Address PR feedback
    
    Signed-off-by: Morgan Tocker <tocker@gmail.com>
    
    * Address PR feedback
    
    Signed-off-by: Morgan Tocker <tocker@gmail.com>
    
    * Fix broken test
    
    Signed-off-by: Morgan Tocker <tocker@gmail.com>
    
    * Fixed tests
    
    Signed-off-by: Morgan Tocker <tocker@gmail.com>
    
    * Addressed PR feedback
    
    Added tests for version detection
    
    Signed-off-by: Morgan Tocker <tocker@gmail.com>
    
    * update copyright
    
    Signed-off-by: Morgan Tocker <tocker@gmail.com>
    
    * Remove unset MYSQL_FLAVOR
    
    I thought about this: it's a bad idea.
    It means that if there was a test that created 2 mysqld's, the usage
    would not be repeatable/idempotent.
    
    Signed-off-by: Morgan Tocker <tocker@gmail.com>
    
    * Make sure error is caught.
    
    Signed-off-by: Morgan Tocker <tocker@gmail.com>
    
    * Address PR Feedback
    
    Signed-off-by: Morgan Tocker <tocker@gmail.com>

---
+++ go/vt/mysqlctl/version.go
@@ -0,0 +1,38 @@
+/*
+Copyright 2019 The Vitess Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*
+ Detect server flavors and capabilities
+*/
+
+package mysqlctl
+
+type serverVersion struct {
+	Major, Minor, Patch int
+}
+
+func (v *serverVersion) atLeast(compare serverVersion) bool {
+	if v.Major > compare.Major {
+		return true
+	}
+	if v.Major == compare.Major && v.Minor > compare.Minor {
+		return true
+	}
+	if v.Major == compare.Major && v.Minor == compare.Minor && v.Patch >= compare.Patch {
+		return true
+	}
+	return false
+}
