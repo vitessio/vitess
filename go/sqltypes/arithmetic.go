2f38e5a104511ea8ed42d73404540b65b34e9398
Rasika Kale <rasika@planetscale.com>
2019-09-03 10:48:49 -0700

    Changed error checking in uintTimesUintWithError() in arithmetic.go
    Signed-off-by: Rasika Kale <rasika@planetscale.com>

--- go/sqltypes/arithmetic.go
+++ go/sqltypes/arithmetic.go
@@ -571,7 +571,7 @@ func uintMinusUintWithError(v1, v2 uint64) (numeric, error) {
 
 func uintTimesUintWithError(v1, v2 uint64) (numeric, error) {
 	result := v1 * v2
-	if v1 >= math.MaxUint64 && v2 > 1 || v2 >= math.MaxUint64 && v1 > 1 {
+	if result < v2 {
 		return numeric{}, vterrors.Errorf(vtrpcpb.Code_INVALID_ARGUMENT, "BIGINT UNSIGNED value is out of range in %v * %v", v1, v2)
 	}
 	return numeric{typ: Uint64, uval: result}, nil
