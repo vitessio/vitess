7c81f4449ea0376f1bf3b445eadddd57fd47266e
deepthi <deepthi@planetscale.com>
2019-09-30 15:13:26 -0700

    Don't abort restore if master is unreachable
    Signed-off-by: deepthi <deepthi@planetscale.com>

--- go/vt/vttablet/tabletmanager/restore.go
+++ go/vt/vttablet/tabletmanager/restore.go
@@ -232,7 +232,12 @@ func (agent *ActionAgent) startReplication(ctx context.Context, pos mysql.Positi
 	defer remoteCancel()
 	posStr, err := tmc.MasterPosition(remoteCtx, ti.Tablet)
 	if err != nil {
-		return vterrors.Wrap(err, "can't get master replication position")
+		// It is possible that though MasterAlias is set, the master tablet is unreachable
+		// Log a warning and let tablet restore in that case
+		// If we had instead considered this fatal, all tablets would crash-loop
+		// until a master appears, which would make it impossible to elect a master.
+		log.Warningf("Can't get master replication position after restore: %v", err)
+		return nil
 	}
 	masterPos, err := mysql.DecodePosition(posStr)
 	if err != nil {
