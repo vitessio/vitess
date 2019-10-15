d0bdde712d5a391d11d732d888805f45c69146a3
JavierR14 <javier@squareup.com>
2019-10-15 14:28:45 -0400

    Restored a sample docker-compose for beginners, added more to the README, and added external db support to vtcompose
    Signed-off-by: JavierR14 <javier@squareup.com>
    Signed-off-by: jawabuu <49921621+jawabuu@users.noreply.github.com>

--- examples/compose/schemaload.sh
+++ examples/compose/schemaload.sh
@@ -5,6 +5,7 @@ targettab=${TARGETTAB:-'test-0000000101'}
 schema_files=${SCHEMA_FILES:-'create_messages.sql create_tokens.sql'}
 vschema_file=${VSCHEMA_FILE:-'base_vschema.json'}
 load_file=${POST_LOAD_FILE:-''}
+external_db=${EXTERNAL_DB:-'0'}
 
 sleep $sleeptime
 
@@ -13,9 +14,11 @@ if [ ! -f schema_run ]; then
     vtctlclient -server vtctld:$GRPC_PORT GetTablet $targettab && break
     sleep 1
   done
-  for schema_file in $schema_files; do
-    vtctlclient -server vtctld:$GRPC_PORT ApplySchema -sql-file /script/tables/${schema_file} $KEYSPACE
-  done
+  if [ "$external_db" = "0" ]; then
+    for schema_file in $schema_files; do
+      vtctlclient -server vtctld:$GRPC_PORT ApplySchema -sql-file /script/tables/${schema_file} $KEYSPACE
+    done
+  fi
   vtctlclient -server vtctld:$GRPC_PORT ApplyVSchema -vschema_file /script/${vschema_file} $KEYSPACE
 
   if [ -n "$load_file" ]; then
