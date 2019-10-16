f94dd1fdd2d26107c7d4b34568fa2f304bd7c340
Rafael Chacon <rafael@slack-corp.com>
2019-10-16 16:33:20 -0700

    Simplifies vtshovel logic. It assumes that it runs directly again
    Signed-off-by: Rafael Chacon <rafael@slack-corp.com>
    Signed-off-by: Arindam Nayak <arindam.nayak@outlook.com>

--- go/vt/vttablet/tabletserver/vstreamer/planbuilder.go
+++ go/vt/vttablet/tabletserver/vstreamer/planbuilder.go
@@ -138,6 +138,13 @@ func mustSendStmt(query mysql.Query, dbname string) bool {
 	return true
 }
 
+func mustSendStmt(query mysql.Query, dbname string) bool {
+	if query.Database != "" && query.Database != dbname {
+		return false
+	}
+	return true
+}
+
 func mustSendDDL(query mysql.Query, dbname string, filter *binlogdatapb.Filter) bool {
 	if query.Database != "" && query.Database != dbname {
 		return false
