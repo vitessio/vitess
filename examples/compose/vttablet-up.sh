7041d1859b89eb121ca696b17aa64f2800d7fc27
jawabuu <49921621+jawabuu@users.noreply.github.com>
2019-10-17 22:54:49 +0300

    Update vttablet script to work with vtcompose and beginners docker-compose
    Signed-off-by: jawabuu <49921621+jawabuu@users.noreply.github.com>

--- examples/compose/vttablet-up.sh
+++ examples/compose/vttablet-up.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-set -uex
+set -u
 
 keyspace=${KEYSPACE:-'test_keyspace'}
 shard=${SHARD:-'0'}
 grpc_port=${GRPC_PORT:-'15999'}
 web_port=${WEB_PORT:-'8080'}
 role=${ROLE:-'replica'}
-vthost=${VTHOST:-'localhost'}
+vthost=${VTHOST:-''}
 sleeptime=${SLEEPTIME:-'0'}
 uid=$1
 external=${EXTERNAL_DB:-0}
-db_name=${DB:-"vt_$keyspace"}
+# db_name=${DB:-"vt_$keyspace"}
+[ $external = 0 ] && db_name=${DB:-"vt_$keyspace"} ||  db_name=${DB:-"$keyspace"}
 db_charset=${DB_CHARSET:-''}
 tablet_hostname=''
 
@@ -25,20 +26,21 @@ printf -v alias '%s-%010d' $CELL $uid
 printf -v tablet_dir 'vt_%010d' $uid
 
 tablet_role=$role
+tablet_type='replica'
 
-init_db_sql_file="$VTROOT/init_db.sql"
-echo "GRANT ALL ON *.* TO 'root'@'%';" > $init_db_sql_file
-echo "GRANT ALL ON *.* TO 'vt_dba'@'%';" >> $init_db_sql_file
-echo "GRANT ALL ON *.* TO 'vt_app'@'%';" >> $init_db_sql_file
-echo "GRANT ALL ON *.* TO 'vt_repl'@'%';" >> $init_db_sql_file
-echo "GRANT ALL ON *.* TO 'vt_filtered'@'%';" >> $init_db_sql_file
+# Make every 3rd tablet rdonly
+if (( $uid % 100 % 3 == 0 )) ; then
+    tablet_type='rdonly'
+fi
+
+init_db_sql_file="$VTROOT/config/init_db.sql"
 
 # Create database on master
-if [ "$tablet_role" = "master" ]; then
+if [ $tablet_role = "master" ]; then
     echo "CREATE DATABASE IF NOT EXISTS $db_name;" >> $init_db_sql_file
 fi
 # Create database on replicas
-if [ "$tablet_role" != "master" ]; then
+if [ $tablet_role != "master" ]; then
     echo "Add create database statement for replicas tablet:  $uid..."
     if [ "$external" = "1" ]; then
         # Add master character set and collation to avoid replication errors
@@ -53,8 +55,9 @@ if [ "$tablet_role" != "master" ]; then
         echo "CREATE DATABASE IF NOT EXISTS $db_name;" >> $init_db_sql_file
     fi
 fi
-
-export EXTRA_MY_CNF=$VTROOT/config/mycnf/master_mysql56.cnf
+# Enforce Row Based Replication
+export EXTRA_MY_CNF=$VTROOT/config/mycnf/default-fast.cnf:$VTROOT/config/mycnf/rbr.cnf
+export EXTRA_MY_CNF=$EXTRA_MY_CNF:$VTROOT/config/mycnf/master_mysql56.cnf
 
 mkdir -p $VTDATAROOT/backups
 
@@ -78,7 +81,7 @@ export DB_NAME=$db_name
 
 # Create mysql instances
 # Do not create mysql instance for master if connecting to external mysql database
-if [[ $uid -gt 1 || $external = 0 ]]; then
+if [[ $role != "master" || $external = 0 ]]; then
   echo "Initing mysql for tablet: $uid.. "
   $VTROOT/bin/mysqlctl \
   -log_dir $VTDATAROOT/tmp \
@@ -91,7 +94,7 @@ fi
 
 sleep $sleeptime
 
-if [ "$role" != "master" ]; then
+if [ $role != "master" ]; then
 
     master_uid=${uid:0:1}01
     master_vttablet=vttablet${master_uid}
@@ -108,12 +111,11 @@ $VTROOT/bin/vtctl $TOPOLOGY_FLAGS AddCellInfo -root vitess/$CELL -server_address
 $VTROOT/bin/vtctl $TOPOLOGY_FLAGS CreateKeyspace $keyspace || true
 $VTROOT/bin/vtctl $TOPOLOGY_FLAGS CreateShard $keyspace/$shard || true
 
-
-$VTROOT/bin/vtctl $TOPOLOGY_FLAGS InitTablet -shard $shard -keyspace $keyspace -hostname $vthost -grpc_port $grpc_port -port $web_port -allow_master_override $alias $tablet_role
+$VTROOT/bin/vtctl $TOPOLOGY_FLAGS InitTablet -shard $shard -keyspace $keyspace -grpc_port $grpc_port -port $web_port -allow_master_override $alias $tablet_role
 
 #Populate external db conditional args
 if [ "$external" = "1" ]; then
-    if [ "$uid" = "1" ]; then
+    if [ $role = "master" ]; then
         echo "Setting external db args for master: $DB_NAME"
         external_db_args="-db_host $DB_HOST \
                           -db_port $DB_PORT \
@@ -151,7 +153,7 @@ exec $VTROOT/bin/vttablet \
   $TOPOLOGY_FLAGS \
   -logtostderr=true \
   -tablet-path $alias \
-  -tablet_hostname $vthost \
+  -tablet_hostname "$vthost" \
   -health_check_interval 5s \
   -enable_semi_sync \
   -enable_replication_reporter \
@@ -160,5 +162,11 @@ exec $VTROOT/bin/vttablet \
   -binlog_use_v3_resharding_mode=true \
   -service_map 'grpc-queryservice,grpc-tabletmanager,grpc-updatestream' \
   -pid_file $VTDATAROOT/$tablet_dir/vttablet.pid \
-  -vtctld_addr "http://vtctld:$WEB_PORT/"
+  -vtctld_addr "http://vtctld:$WEB_PORT/" \
+  -init_keyspace $keyspace \
+  -init_shard $shard \
+  -init_tablet_type $tablet_type \
+  -backup_storage_implementation file \
+  -file_backup_storage_root $VTDATAROOT/backups \
+  -queryserver-config-schema-reload-time 60 \
   $external_db_args
