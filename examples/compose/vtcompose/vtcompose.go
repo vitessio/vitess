105758d0c4d75ea6c353bc9b5a9c7482731a5ce3
jawabuu <49921621+jawabuu@users.noreply.github.com>
2019-10-19 12:27:24 +0300

    Remove workaraound for applying vschema in unsharded keyspace
    Signed-off-by: jawabuu <49921621+jawabuu@users.noreply.github.com>

--- examples/compose/vtcompose/vtcompose.go
+++ examples/compose/vtcompose/vtcompose.go
@@ -113,7 +113,10 @@ func main() {
 
 		// Check if it is an external_db
 		if  _, ok := externalDbInfoMap[k]; ok {
-			vSchemaFile = applyJsonInMemoryPatch(vSchemaFile,`[{"op": "add","path": "/tables/*", "value": {}}]`)
+      //This is no longer necessary, but we'll keep it for reference
+      //https://github.com/vitessio/vitess/pull/4868, https://github.com/vitessio/vitess/pull/5010
+      //vSchemaFile = applyJsonInMemoryPatch(vSchemaFile,`[{"op": "add","path": "/tables/*", "value": {}}]`)
+      vSchemaFile = applyJsonInMemoryPatch(vSchemaFile,`[{"op": "replace","path": "/sharded", "value": false}]`)
 		} else {
 			var primaryTableColumns map[string]string
 			vSchemaFile, primaryTableColumns = addTablesVschemaPatch(vSchemaFile, keyspaceData.schemaFileNames)
