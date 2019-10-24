ba3d1c226ccc58e5ff6ec1d296f295d4a1a37863
Arindam Nayak <arindam.nayak@outlook.com>
2019-10-24 13:14:16 +0530

    Added cluster setup using go processes
    Signed-off-by: Arindam Nayak <arindam.nayak@outlook.com>

---
+++ go/test/endtoend/cluster/vtctlclient_process.go
@@ -0,0 +1,100 @@
+/*
+Copyright 2019 The Vitess Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cluster
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+
+	"vitess.io/vitess/go/vt/log"
+)
+
+// VtctlClientProcess is a generic handle for a running vtctlclient command .
+// It can be spawned manually
+type VtctlClientProcess struct {
+	Name          string
+	Binary        string
+	Server        string
+	TempDirectory string
+	ZoneName      string
+}
+
+// InitShardMaster executes vtctlclient command to make one of tablet as master
+func (vtctlclient *VtctlClientProcess) InitShardMaster(Keyspace string, Shard string, Cell string, TabletUID int) (err error) {
+	return vtctlclient.ExecuteCommand(
+		"InitShardMaster",
+		"-force",
+		fmt.Sprintf("%s/%s", Keyspace, Shard),
+		fmt.Sprintf("%s-%d", Cell, TabletUID))
+}
+
+// ApplySchema applies SQL schema to the keyspace
+func (vtctlclient *VtctlClientProcess) ApplySchema(Keyspace string, SQL string) (err error) {
+	return vtctlclient.ExecuteCommand(
+		"ApplySchema",
+		"-sql", SQL,
+		Keyspace)
+}
+
+// ApplyVSchema applies vitess schema (JSON format) to the keyspace
+func (vtctlclient *VtctlClientProcess) ApplyVSchema(Keyspace string, JSON string) (err error) {
+	return vtctlclient.ExecuteCommand(
+		"ApplyVSchema",
+		"-vschema", JSON,
+		Keyspace,
+	)
+}
+
+// ExecuteCommand executes any vtctlclient command
+func (vtctlclient *VtctlClientProcess) ExecuteCommand(args ...string) (err error) {
+	args = append([]string{"-server", vtctlclient.Server}, args...)
+	tmpProcess := exec.Command(
+		vtctlclient.Binary,
+		args...,
+	)
+	println(fmt.Sprintf("Executing vtctlclient with arguments %v", strings.Join(tmpProcess.Args, " ")))
+	log.Info(fmt.Sprintf("Executing vtctlclient with arguments %v", strings.Join(tmpProcess.Args, " ")))
+	return tmpProcess.Run()
+}
+
+// ExecuteCommandWithOutput executes any vtctlclient command and returns output
+func (vtctlclient *VtctlClientProcess) ExecuteCommandWithOutput(args ...string) (result string, err error) {
+	args = append([]string{"-server", vtctlclient.Server}, args...)
+	tmpProcess := exec.Command(
+		vtctlclient.Binary,
+		args...,
+	)
+	println(fmt.Sprintf("Executing vtctlclient with arguments %v", strings.Join(tmpProcess.Args, " ")))
+	log.Info(fmt.Sprintf("Executing vtctlclient with arguments %v", strings.Join(tmpProcess.Args, " ")))
+	resultByte, err := tmpProcess.CombinedOutput()
+	return string(resultByte), err
+}
+
+// VtctlClientProcessInstance returns a VtctlProcess handle for vtctlclient process
+// configured with the given Config.
+func VtctlClientProcessInstance(Hostname string, GrpcPort int) *VtctlClientProcess {
+	vtctlclient := &VtctlClientProcess{
+		Name:          "vtctlclient",
+		Binary:        "vtctlclient",
+		Server:        fmt.Sprintf("%s:%d", Hostname, GrpcPort),
+		TempDirectory: path.Join(os.Getenv("VTDATAROOT"), "/tmp"),
+	}
+	return vtctlclient
+}
---
+++ go/test/endtoend/cluster/vtgate_process.go
@@ -0,0 +1,187 @@
+/*
+Copyright 2019 The Vitess Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cluster
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+	"syscall"
+	"time"
+
+	"vitess.io/vitess/go/vt/log"
+)
+
+// VtgateProcess is a generic handle for a running vtgate .
+// It can be spawned manually
+type VtgateProcess struct {
+	Name                  string
+	Binary                string
+	CommonArg             VtctlProcess
+	LogDir                string
+	FileToLogQueries      string
+	Port                  int
+	GrpcPort              int
+	MySQLServerPort       int
+	MySQLServerSocketPath string
+	Cell                  string
+	CellsToWatch          string
+	TabletTypesToWait     string
+	GatewayImplementation string
+	ServiceMap            string
+	PidFile               string
+	MySQLAuthServerImpl   string
+	Directory             string
+	VerifyURL             string
+
+	proc *exec.Cmd
+	exit chan error
+}
+
+// Setup starts Vtgate process with required arguements
+func (vtgate *VtgateProcess) Setup() (err error) {
+
+	vtgate.proc = exec.Command(
+		vtgate.Binary,
+		"-topo_implementation", vtgate.CommonArg.TopoImplementation,
+		"-topo_global_server_address", vtgate.CommonArg.TopoGlobalAddress,
+		"-topo_global_root", vtgate.CommonArg.TopoGlobalRoot,
+		"-log_dir", vtgate.LogDir,
+		"-log_queries_to_file", vtgate.FileToLogQueries,
+		"-port", fmt.Sprintf("%d", vtgate.Port),
+		"-grpc_port", fmt.Sprintf("%d", vtgate.GrpcPort),
+		"-mysql_server_port", fmt.Sprintf("%d", vtgate.MySQLServerPort),
+		"-mysql_server_socket_path", vtgate.MySQLServerSocketPath,
+		"-cell", vtgate.Cell,
+		"-cells_to_watch", vtgate.CellsToWatch,
+		"-tablet_types_to_wait", vtgate.TabletTypesToWait,
+		"-gateway_implementation", vtgate.GatewayImplementation,
+		"-service_map", vtgate.ServiceMap,
+		"-mysql_auth_server_impl", vtgate.MySQLAuthServerImpl,
+		"-pid_file", vtgate.PidFile,
+	)
+
+	vtgate.proc.Stderr = os.Stderr
+	vtgate.proc.Stdout = os.Stdout
+
+	vtgate.proc.Env = append(vtgate.proc.Env, os.Environ()...)
+
+	log.Infof("%v %v", strings.Join(vtgate.proc.Args, " "))
+
+	err = vtgate.proc.Start()
+	if err != nil {
+		return
+	}
+
+	vtgate.exit = make(chan error)
+	go func() {
+		vtgate.exit <- vtgate.proc.Wait()
+	}()
+
+	timeout := time.Now().Add(60 * time.Second)
+	for time.Now().Before(timeout) {
+		if vtgate.WaitForStatus() {
+			return nil
+		}
+		select {
+		case err := <-vtgate.exit:
+			return fmt.Errorf("process '%s' exited prematurely (err: %s)", vtgate.Name, err)
+		default:
+			time.Sleep(300 * time.Millisecond)
+		}
+	}
+
+	return fmt.Errorf("process '%s' timed out after 60s (err: %s)", vtgate.Name, <-vtgate.exit)
+}
+
+// WaitForStatus function checks if vtgate process is up and running
+func (vtgate *VtgateProcess) WaitForStatus() bool {
+	resp, err := http.Get(vtgate.VerifyURL)
+	if err != nil {
+		return false
+	}
+	if resp.StatusCode == 200 {
+		resultMap := make(map[string]interface{})
+		respByte, _ := ioutil.ReadAll(resp.Body)
+		err := json.Unmarshal(respByte, &resultMap)
+		if err != nil {
+			panic(err)
+		}
+		//for key, value := range resultMap {
+		//	println("VTGate API Response: Key = " + key + ", value = " + fmt.Sprintf("%v", value))
+		//}
+		//println(string(respByte))
+		//return resultMap["TabletStateName"] == "NOT_SERVING"
+		return true
+	}
+	return false
+}
+
+// TearDown shuts down the running vtgate service
+func (vtgate *VtgateProcess) TearDown() error {
+	if vtgate.proc == nil || vtgate.exit == nil {
+		return nil
+	}
+	// Attempt graceful shutdown with SIGTERM first
+	vtgate.proc.Process.Signal(syscall.SIGTERM)
+
+	select {
+	case err := <-vtgate.exit:
+		vtgate.proc = nil
+		return err
+
+	case <-time.After(10 * time.Second):
+		vtgate.proc.Process.Kill()
+		vtgate.proc = nil
+		return <-vtgate.exit
+	}
+}
+
+// VtgateProcessInstance returns a Vtgate handle for vtgate process
+// configured with the given Config.
+// The process must be manually started by calling setup()
+func VtgateProcessInstance(Port int, GrpcPort int, MySQLServerPort int, Cell string, CellsToWatch string, Hostname string, TabletTypesToWait string, topoPort int, hostname string) *VtgateProcess {
+	vtctl := VtctlProcessInstance(topoPort, hostname)
+	vtgate := &VtgateProcess{
+		Name:                  "vtgate",
+		Binary:                "vtgate",
+		FileToLogQueries:      path.Join(os.Getenv("VTDATAROOT"), "/tmp/vtgate_querylog.txt"),
+		Directory:             os.Getenv("VTDATAROOT"),
+		ServiceMap:            "grpc-vtgateservice",
+		LogDir:                path.Join(os.Getenv("VTDATAROOT"), "/tmp"),
+		Port:                  Port,
+		GrpcPort:              GrpcPort,
+		MySQLServerPort:       MySQLServerPort,
+		MySQLServerSocketPath: "/tmp/mysql.sock",
+		Cell:                  Cell,
+		CellsToWatch:          CellsToWatch,
+		TabletTypesToWait:     TabletTypesToWait,
+		GatewayImplementation: "discoverygateway",
+		CommonArg:             *vtctl,
+		PidFile:               path.Join(os.Getenv("VTDATAROOT"), "/tmp/vtgate.pid"),
+		MySQLAuthServerImpl:   "none",
+	}
+
+	vtgate.VerifyURL = fmt.Sprintf("http://%s:%d/debug/vars", Hostname, Port)
+
+	return vtgate
+}
