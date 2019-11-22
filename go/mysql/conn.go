a6ac2a4b25edb90909d91b04b2fee47fa02cfe47
Peter Farr <Peter@PrismaPhonic.com>
2019-11-22 15:47:17 -0800

    Bug Fix: Prepared Statements Bind Variables Nil Error (#5441)
    * Fixed nil bind variables error by resetting bindvars to an empty map.  Also added a recover to catch panics that occur inside Logf due to FormatBindVariables call potentially panicking.
    
    Signed-off-by: Peter Farr <Peter@PrismaPhonic.com>
    
    * Changed import order back.  Order was auto-changed by IDE on save
    
    Signed-off-by: Peter Farr <Peter@PrismaPhonic.com>
    
    * Using stored ParamsCount for capacity per review suggestion.
    
    Signed-off-by: Peter Farr <Peter@PrismaPhonic.com>
    
    * Added explanation comment per review suggestion.
    
    Signed-off-by: Peter Farr <Peter@PrismaPhonic.com>
    
    * Removed explicit panic after offline talk with sougou and deepthi
    
    Signed-off-by: Peter Farr <Peter@PrismaPhonic.com>

--- go/mysql/conn.go
+++ go/mysql/conn.go
@@ -897,12 +897,9 @@ func (c *Conn) handleNextCommand(handler Handler) error {
 
 		if stmtID != uint32(0) {
 			defer func() {
+				// Allocate a new bindvar map every time since VTGate.Execute() mutates it.
 				prepare := c.PrepareData[stmtID]
-				if prepare.BindVars != nil {
-					for k := range prepare.BindVars {
-						prepare.BindVars[k] = nil
-					}
-				}
+				prepare.BindVars = make(map[string]*querypb.BindVariable, prepare.ParamsCount)
 			}()
 		}
 
