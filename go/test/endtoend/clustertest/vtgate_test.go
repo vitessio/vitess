862d2289fb4439181d3ba47cc7356036c694afc3
Arindam Nayak <arindam.nayak@outlook.com>
2019-12-05 12:41:54 +0530

    added testcase for vtgate vars
    Signed-off-by: Arindam Nayak <arindam.nayak@outlook.com>

--- go/test/endtoend/clustertest/vtgate_test.go
+++ go/test/endtoend/clustertest/vtgate_test.go
@@ -19,7 +19,12 @@ package clustertest
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"reflect"
+	"strings"
 	"testing"
 
 	"vitess.io/vitess/go/mysql"
@@ -27,6 +32,7 @@ import (
 )
 
 func TestVtgateProcess(t *testing.T) {
+	verifyVtgateVariables(t, clusterInstance.VtgateProcess.VerifyURL)
 	ctx := context.Background()
 	conn, err := mysql.Connect(ctx, &vtParams)
 	if err != nil {
@@ -40,6 +46,68 @@ func TestVtgateProcess(t *testing.T) {
 	if got, want := fmt.Sprintf("%v", qr.Rows), `[[INT64(1) VARCHAR("email1")]]`; got != want {
 		t.Errorf("select:\n%v want\n%v", got, want)
 	}
+}
+
+func verifyVtgateVariables(t *testing.T, url string) {
+	resp, _ := http.Get(url)
+	if resp != nil && resp.StatusCode == 200 {
+		resultMap := make(map[string]interface{})
+		respByte, _ := ioutil.ReadAll(resp.Body)
+		err := json.Unmarshal(respByte, &resultMap)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if resultMap["VtgateVSchemaCounts"] == nil {
+			t.Error("Vschema count should be present in variables")
+		}
+		vschemaCountMap := getMapFromJSON(resultMap, "VtgateVSchemaCounts")
+		if _, present := vschemaCountMap["Reload"]; !present {
+			t.Error("Reload count should be present in vschemacount")
+		} else if object := reflect.ValueOf(vschemaCountMap["Reload"]); object.NumField() <= 0 {
+			t.Error("Reload count should be greater than 0")
+		}
+		if _, present := vschemaCountMap["WatchError"]; present {
+			t.Error("There should not be any WatchError in VschemaCount")
+		}
+		if _, present := vschemaCountMap["Parsing"]; present {
+			t.Error("There should not be any Parsing in VschemaCount")
+		}
+
+		if resultMap["HealthcheckConnections"] == nil {
+			t.Error("HealthcheckConnections count should be present in variables")
+		}
+
+		healthCheckConnection := getMapFromJSON(resultMap, "HealthcheckConnections")
+		if len(healthCheckConnection) <= 0 {
+			t.Error("Atleast one healthy tablet needs to be present")
+		}
+		if !isMasterTabletPresent(healthCheckConnection) {
+			t.Error("Atleast one master tablet needs to be present")
+		}
+	} else {
+		t.Error("Vtgate api url response not found")
+	}
+}
+
+func getMapFromJSON(JSON map[string]interface{}, key string) map[string]interface{} {
+	result := make(map[string]interface{})
+	object := reflect.ValueOf(JSON[key])
+	if object.Kind() == reflect.Map {
+		for _, key := range object.MapKeys() {
+			value := object.MapIndex(key)
+			result[key.String()] = value
+		}
+	}
+	return result
+}
+
+func isMasterTabletPresent(tablets map[string]interface{}) bool {
+	for key := range tablets {
+		if strings.Contains(key, "master") {
+			return true
+		}
+	}
+	return false
 }
 
 func exec(t *testing.T, conn *mysql.Conn, query string) *sqltypes.Result {
