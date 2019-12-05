596862674bb6d10bb9eec15dc2363e13889dd0ae
Arindam Nayak <arindamnayak@users.noreply.github.com>
2019-12-05 12:26:27 +0530

    moved sql start to non-blocking mode (#35)
    * moved sql start to non-blocking mode
    
    Signed-off-by: Arindam Nayak <arindam.nayak@outlook.com>
    Signed-off-by: Arindam Nayak <arindam.nayak@outlook.com>

--- go/test/endtoend/cluster/mysqlctl_process.go
+++ go/test/endtoend/cluster/mysqlctl_process.go
@@ -54,6 +54,15 @@ func (mysqlctl *MysqlctlProcess) InitDb() (err error) {
 
 // Start executes mysqlctl command to start mysql instance
 func (mysqlctl *MysqlctlProcess) Start() (err error) {
+	if tmpProcess, err := mysqlctl.StartProcess(); err != nil {
+		return err
+	} else {
+		return tmpProcess.Wait()
+	}
+}
+
+// StartProcess starts the mysqlctl and returns the process reference
+func (mysqlctl *MysqlctlProcess) StartProcess() (*exec.Cmd, error) {
 	tmpProcess := exec.Command(
 		mysqlctl.Binary,
 		"-log_dir", mysqlctl.LogDirectory,
@@ -62,17 +71,26 @@ func (mysqlctl *MysqlctlProcess) Start() (err error) {
 		"init",
 		"-init_db_sql_file", mysqlctl.InitDBFile,
 	)
-	return tmpProcess.Run()
+	return tmpProcess, tmpProcess.Start()
 }
 
 // Stop executes mysqlctl command to stop mysql instance
 func (mysqlctl *MysqlctlProcess) Stop() (err error) {
+	if tmpProcess, err := mysqlctl.StopProcess(); err != nil {
+		return err
+	} else {
+		return tmpProcess.Wait()
+	}
+}
+
+// StopProcess executes mysqlctl command to stop mysql instance and returns process reference
+func (mysqlctl *MysqlctlProcess) StopProcess() (*exec.Cmd, error) {
 	tmpProcess := exec.Command(
 		mysqlctl.Binary,
 		"-tablet_uid", fmt.Sprintf("%d", mysqlctl.TabletUID),
 		"shutdown",
 	)
-	return tmpProcess.Start()
+	return tmpProcess, tmpProcess.Start()
 }
 
 // CleanupFiles clean the mysql files to make sure we can start the same process again
