f18b6ce57a8f9e4054512c69f57ac3dac575b72f
Sugu Sougoumarane <ssougou@gmail.com>
2019-12-28 17:44:52 -0800

    vrepl: improve failure handling
    Tablet dialers were set to not fail fast. This was causing grpc
    to redial the same tablet indefinitely eventhough it was permanently
    down. This has now been changed to fail fast.
    
    The tablet picker was only taking health errors into account, which
    happens when tablets report themseleves as unhealthy. But if a tablet
    is unreachable, we have to check LastError.
    
    Also fixed some staticcheck errors.
    
    Signed-off-by: Sugu Sougoumarane <ssougou@gmail.com>

--- go/vt/discovery/utils.go
+++ go/vt/discovery/utils.go
@@ -30,7 +30,7 @@ func RemoveUnhealthyTablets(tabletStatsList []TabletStats) []TabletStats {
 		// source and destination, and the source is not serving (disabled by
 		// TabletControl). When we switch the tablet to 'worker', it will
 		// go back to serving state.
-		if ts.Stats == nil || ts.Stats.HealthError != "" || IsReplicationLagHigh(&ts) {
+		if ts.Stats == nil || ts.Stats.HealthError != "" || ts.LastError != nil || IsReplicationLagHigh(&ts) {
 			continue
 		}
 		result = append(result, ts)
