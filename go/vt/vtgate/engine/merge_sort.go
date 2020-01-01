44ddea8d518d70eeea9cd1269d45449e6e0c41be
Sugu Sougoumarane <ssougou@gmail.com>
2020-01-01 14:00:35 -0800

    vdiff: document vdiff.go
    Signed-off-by: Sugu Sougoumarane <ssougou@gmail.com>

--- go/vt/vtgate/engine/merge_sort.go
+++ go/vt/vtgate/engine/merge_sort.go
@@ -14,8 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// This file has the logic for performing merge-sorts of scatter queries.
-
 package engine
 
 import (
