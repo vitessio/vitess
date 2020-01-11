65a57ef38436f7f995775b85ce83f409668a3b65
Andres Taylor <andres@planetscale.com>
2020-01-10 16:33:57 -0800

    Add the visitor generator
    Signed-off-by: Andres Taylor <andres@planetscale.com>

---
+++ go/visitorgen/transformer.go
@@ -0,0 +1,79 @@
+package visitorgen
+
+import "fmt"
+
+// Transform takes an input file and collects the information into an easier to consume format
+func Transform(input *SourceFile) *SourceInformation {
+	interestingTypes := make(map[string]Type)
+	interfaces := make(map[string]bool)
+	structs := make(map[string]*StructDeclaration)
+	typeAliases := make(map[string]*TypeAlias)
+
+	for _, l := range input.lines {
+		switch line := l.(type) {
+		case *FuncDeclaration:
+			interestingTypes[line.receiver.typ.toTypString()] = line.receiver.typ
+		case *StructDeclaration:
+			structs[line.name] = line
+		case *TypeAlias:
+			typeAliases[line.name] = line
+		case *InterfaceDeclaration:
+			interfaces[line.name] = true
+		}
+	}
+
+	return &SourceInformation{
+		interfaces:       interfaces,
+		interestingTypes: interestingTypes,
+		structs:          structs,
+		typeAliases:      typeAliases,
+	}
+}
+
+// SourceInformation contains the information from the ast.go file, but in a format that is easier to consume
+type SourceInformation struct {
+	interestingTypes map[string]Type
+	interfaces       map[string]bool
+	structs          map[string]*StructDeclaration
+	typeAliases      map[string]*TypeAlias
+}
+
+func (v *SourceInformation) String() string {
+	var types string
+	for _, k := range v.interestingTypes {
+		types += k.toTypString() + "\n"
+	}
+	var structs string
+	for _, k := range v.structs {
+		structs += k.toSastString() + "\n"
+	}
+	var typeAliases string
+	for _, k := range v.typeAliases {
+		typeAliases += k.toSastString() + "\n"
+	}
+
+	return fmt.Sprintf("Types to build visitor for:\n%s\nStructs with fields: \n%s\nTypeAliases with type: \n%s\n", types, structs, typeAliases)
+}
+
+// getItemTypeOfArray will return nil if the given type is not pointing to a array type.
+// If it is an array type, the type of it's items will be returned
+func (v *SourceInformation) getItemTypeOfArray(typ Type) Type {
+	alias := v.typeAliases[typ.rawTypeName()]
+	if alias == nil {
+		return nil
+	}
+	arrTyp, isArray := alias.typ.(*Array)
+	if !isArray {
+		return v.getItemTypeOfArray(alias.typ)
+	}
+	return arrTyp.inner
+}
+
+func (v *SourceInformation) isSQLNode(typ Type) bool {
+	_, isInteresting := v.interestingTypes[typ.toTypString()]
+	if isInteresting {
+		return true
+	}
+	_, isInterface := v.interfaces[typ.toTypString()]
+	return isInterface
+}
