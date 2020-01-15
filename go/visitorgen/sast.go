00eaa27f42f36b1886f68f927b4c4c556aaf216a
Andres Taylor <andres@planetscale.com>
2020-01-15 11:19:23 -0800

    License headers and more comments
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/visitorgen/sast.go
+++ go/visitorgen/sast.go
@@ -1,6 +1,23 @@
+/*
+Copyright 2019 The Vitess Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package visitorgen
 
-// simplified ast
+// simplified ast - when reading the golang ast of the ast.go file, we translate the golang ast objects
+// to this much simpler format, that contains only the necessary information and no more
 type (
 	// SourceFile contains all important lines from an ast.go file
 	SourceFile struct {
