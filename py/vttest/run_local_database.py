d66b93d14b010f43fc5f0935eb9b37f9208c58bb
Arindam Nayak <arindam.nayak@outlook.com>
2020-03-13 17:32:38 +0530

    make java test case, as re-adding files back to required location, not fixing the error
    Signed-off-by: Arindam Nayak <arindam.nayak@outlook.com>

--- py/vttest/run_local_database.py
+++ py/vttest/run_local_database.py
@@ -53,7 +53,6 @@ import sys
 from google.protobuf import text_format
 
 from vtproto import vttest_pb2
-from vtdb import prefer_vtroot_imports  # pylint: disable=unused-import
 from vttest import environment
 from vttest import init_data_options
 from vttest import local_database
@@ -124,8 +123,29 @@ def main(cmdline_options):
           ' the local cluster. For example, did you forget the shutdown in'
           ' your test\'s tearDown()?\n' % os.path.basename(__file__))
 
-if __name__ == '__main__':
+def _prefer_vtroot_imports():
+  """Reorder sys.path to put $VTROOT/dist before others."""
 
+  vtroot = os.environ.get('VTROOT')
+  if not vtroot:
+      # VTROOT is not set. Don't try anything.
+      return
+  dist = os.path.join(vtroot, 'dist')
+
+  dist_paths = []
+  other_paths = []
+
+  for path in sys.path:
+      if path:
+          if path.startswith(dist):
+              dist_paths.append(path)
+          else:
+              other_paths.append(path)
+
+  sys.path = [''] + dist_paths + other_paths
+
+if __name__ == '__main__':
+  _prefer_vtroot_imports()
   parser = optparse.OptionParser()
   parser.add_option(
       '-p', '--port', type='int',
