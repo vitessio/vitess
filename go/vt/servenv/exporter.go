f80aab09b331b78657f91e9deacdf1ce909494d2
Sugu Sougoumarane <ssougou@gmail.com>
2020-03-29 22:14:45 -0700

    exporter: implement TODO functions
    Signed-off-by: Sugu Sougoumarane <ssougou@gmail.com>

--- go/vt/servenv/exporter.go
+++ go/vt/servenv/exporter.go
@@ -14,25 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package servenv allows you to remap http and stats end-points
-// to distinct names thereby allowing you to create multiple instances
-// of the same object within a single process.
-//
-// Unnamed instances are treated as unscoped, and requests are passed
-// through to the underlying functions.
-//
-// For named instances, http handle requests of the form /path will
-// be remapped to /name/path. In the case of stats variables, a new
-// dimension will be added. For example, a Counter of value 1
-// will be changed to a map {"name": 1}. A multi-counter like
-// { "a.b": 1, "c.d": 2} will be mapped to {"name.a.b": 1, "name.c.d": 2}.
-// Stats vars of the same name are merged onto a single map. For example,
-// if instances name1 and name2 independently create a stats Counter
-// named foo and export values 1 and 2, the result is a merged stats var
-// named foo with the following content: {"name1": 1, "name2": 2}.
-// This approach works for counters and gauges, but does not work for
-// the more complex vars like Timings. In those cases, no remapping is
-// done. The embedder returns unexported variables instead.
 package servenv
 
 import (
@@ -45,81 +26,41 @@ import (
 )
 
 var (
-	// embedmu protects embeds, members of Instance and globalStatVars.
-	// However, varMap and handlFunc have their own mutexes. This is
-	// because their handler functions directly access them.
-	embedmu sync.Mutex
+	// exporterMu protects exporters and globalStatVars, timingsVars and otherStatsVars.
+	// varMap and handleFunc have their own mutexes.
+	exporterMu sync.Mutex
 
-	// embeds contains the full list of instances. Entries can only be
-	// added. The creation of a new instance with a previously existing
-	// name causes that instance to be reused.
-	embeds = make(map[string]*Exporter)
+	// exporters contains the full list of exporters. Entries can only be
+	// added. The creation of a new Exporter with a previously existing
+	// name causes that Exporter to be reused.
+	exporters = make(map[string]*Exporter)
 
-	// globalStatVars contains the merged stats vars created for the instances.
+	// globalStatVars contains the merged stats vars created for the exporters.
 	globalStatVars = make(map[string]*varMap)
+
+	// timingsVars contains all the timings vars.
+	timingsVars = make(map[string]*stats.MultiTimings)
+
+	// otherStatsVars contains Rates, Histograms and Publish vars.
+	otherStatsVars = make(map[string]*expvar.Map)
 )
 
 //-----------------------------------------------------------------
 
-// varMap contains the metadata for a merged stats var. It supports
-// only gauges and counters. For every Instance, it stores a function
-// that yields the counter or gauge values for that Instance.
-type varMap struct {
-	mu   sync.Mutex
-	vars map[string]func() map[string]int64
-}
-
-// Set adds or updates the func for an Instance.
-func (vmap *varMap) Set(name string, f func() map[string]int64) {
-	vmap.mu.Lock()
-	defer vmap.mu.Unlock()
-	vmap.vars[name] = f
-}
-
-// Fetch returns the consolidated stats value for all Instances.
-func (vmap *varMap) Fetch() map[string]int64 {
-	result := make(map[string]int64)
-	vmap.mu.Lock()
-	defer vmap.mu.Unlock()
-	for k, f := range vmap.vars {
-		for innerk, innerv := range f() {
-			if innerk == "" {
-				result[k] = innerv
-			} else {
-				result[k+"."+innerk] = innerv
-			}
-		}
-	}
-	return result
-}
-
-//-----------------------------------------------------------------
-
-// handleFunc stores the http Handler for an Instance. This function can
-// be replaced as needed.
-type handleFunc struct {
-	mu sync.Mutex
-	f  func(w http.ResponseWriter, r *http.Request)
-}
-
-// Set replaces the existing handler with a new one.
-func (hf *handleFunc) Set(f func(w http.ResponseWriter, r *http.Request)) {
-	hf.mu.Lock()
-	defer hf.mu.Unlock()
-	hf.f = f
-}
-
-// Get returns the current handler.
-func (hf *handleFunc) Get() func(w http.ResponseWriter, r *http.Request) {
-	hf.mu.Lock()
-	defer hf.mu.Unlock()
-	return hf.f
-}
-
-//-----------------------------------------------------------------
-
-// Exporter provides the functions needed to embed an object
-// by remapping global endpoints into different namespaces.
+// Exporter remaps http and stats end-points to distinct namespaces.
+//
+// Unnamed exporters are treated as unscoped, and requests are passed
+// through to the underlying functions.
+//
+// For named exporters, http handle requests of the form /path will
+// be remapped to /name/path. In the case of stats variables, a new
+// dimension will be added. For example, a Counter of value 1
+// will be changed to a map {"name": 1}. A multi-counter like
+// { "a.b": 1, "c.d": 2} will be mapped to {"name.a.b": 1, "name.c.d": 2}.
+// Stats vars of the same name are merged onto a single map. For example,
+// if exporters name1 and name2 independently create a stats Counter
+// named foo and export values 1 and 2, the result is a merged stats var
+// named foo with the following content: {"name1": 1, "name2": 2}.
 type Exporter struct {
 	name, label string
 	handleFuncs map[string]*handleFunc
@@ -127,26 +68,26 @@ type Exporter struct {
 }
 
 // NewExporter creates a new Exporter with name as namespace.
-// The label specifies the prefix for the variables, and is also
-// used to label the additonial dimension for the stats vars.
+// label is the name of the additonial dimension for the stats vars.
 func NewExporter(name, label string) *Exporter {
-	embedmu.Lock()
-	defer embedmu.Unlock()
+	if name == "" {
+		return &Exporter{}
+	}
 
-	e, ok := embeds[name]
-	if ok {
+	exporterMu.Lock()
+	defer exporterMu.Unlock()
+
+	if e, ok := exporters[name]; ok {
 		e.resetLocked()
 		return e
 	}
-	e = &Exporter{
+	e := &Exporter{
 		name:        name,
 		label:       label,
 		handleFuncs: make(map[string]*handleFunc),
+		sp:          newStatusPage(name),
 	}
-	if name != "" {
-		e.sp = newStatusPage(name)
-	}
-	embeds[name] = e
+	exporters[name] = e
 	return e
 }
 
@@ -155,16 +96,12 @@ func (e *Exporter) resetLocked() {
 		hf.Set(nil)
 	}
 	for _, vmap := range globalStatVars {
-		vmap.mu.Lock()
-		delete(vmap.vars, e.name)
-		vmap.mu.Unlock()
+		vmap.Unset(e.name)
 	}
-	if e.sp != nil {
-		e.sp.reset()
-	}
+	e.sp.reset()
 }
 
-// URLPrefix returns the URL prefix for all the embedder.
+// URLPrefix returns the URL prefix for the exporter.
 func (e *Exporter) URLPrefix() string {
 	// There are two other places where this logic is duplicated:
 	// status.go and go/vt/vtgate/discovery/healthcheck.go.
@@ -174,7 +111,7 @@ func (e *Exporter) URLPrefix() string {
 	return "/" + e.name
 }
 
-// HandleFunc sets or overwrites the handler for url. If Instance has a name,
+// HandleFunc sets or overwrites the handler for url. If Exporter has a name,
 // url remapped from /path to /name/path. If name is empty, the request
 // is passed through to http.HandleFunc.
 func (e *Exporter) HandleFunc(url string, f func(w http.ResponseWriter, r *http.Request)) {
@@ -183,15 +120,11 @@ func (e *Exporter) HandleFunc(url string, f func(w http.ResponseWriter, r *http.
 		return
 	}
 
-	embedmu.Lock()
-	defer embedmu.Unlock()
-
-	hf, ok := e.handleFuncs[url]
-	if ok {
+	if hf, ok := e.handleFuncs[url]; ok {
 		hf.Set(f)
 		return
 	}
-	hf = &handleFunc{f: f}
+	hf := &handleFunc{f: f}
 	e.handleFuncs[url] = hf
 
 	http.HandleFunc(e.URLPrefix()+url, func(w http.ResponseWriter, r *http.Request) {
@@ -201,16 +134,13 @@ func (e *Exporter) HandleFunc(url string, f func(w http.ResponseWriter, r *http.
 	})
 }
 
-// AddStatusPart adds a status part to the status page. If instance has a name,
+// AddStatusPart adds a status part to the status page. If Exporter has a name,
 // the part is added to a url named /name/debug/status. Otherwise, it's /debug/status.
 func (e *Exporter) AddStatusPart(banner, frag string, f func() interface{}) {
-	if e.sp == nil {
+	if e.name == "" {
 		AddStatusPart(banner, frag, f)
 		return
 	}
-
-	embedmu.Lock()
-	defer embedmu.Unlock()
 	e.sp.addStatusPart(banner, frag, f)
 }
 
@@ -222,8 +152,8 @@ func (e *Exporter) NewCountersFuncWithMultiLabels(name, help string, labels []st
 		return stats.NewCountersFuncWithMultiLabels(name, help, labels, f)
 	}
 
-	embedmu.Lock()
-	defer embedmu.Unlock()
+	exporterMu.Lock()
+	defer exporterMu.Unlock()
 
 	if vmap, ok := globalStatVars[name]; ok {
 		vmap.Set(e.name, f)
@@ -232,10 +162,8 @@ func (e *Exporter) NewCountersFuncWithMultiLabels(name, help string, labels []st
 	vmap := &varMap{vars: map[string]func() map[string]int64{e.name: f}}
 	globalStatVars[name] = vmap
 
-	newlabels := append(append(make([]string, 0, len(labels)+1), e.label), labels...)
-	_ = stats.NewCountersFuncWithMultiLabels(e.label+name, help, newlabels, func() map[string]int64 {
-		return vmap.Fetch()
-	})
+	newlabels := combineLabels(e.label, labels)
+	_ = stats.NewCountersFuncWithMultiLabels(name, help, newlabels, vmap.Fetch)
 	return stats.NewCountersFuncWithMultiLabels("", help, labels, f)
 }
 
@@ -246,8 +174,8 @@ func (e *Exporter) NewGaugesFuncWithMultiLabels(name, help string, labels []stri
 		return stats.NewGaugesFuncWithMultiLabels(name, help, labels, f)
 	}
 
-	embedmu.Lock()
-	defer embedmu.Unlock()
+	exporterMu.Lock()
+	defer exporterMu.Unlock()
 
 	if vmap, ok := globalStatVars[name]; ok {
 		vmap.Set(e.name, f)
@@ -256,10 +184,8 @@ func (e *Exporter) NewGaugesFuncWithMultiLabels(name, help string, labels []stri
 	vmap := &varMap{vars: map[string]func() map[string]int64{e.name: f}}
 	globalStatVars[name] = vmap
 
-	newlabels := append(append(make([]string, 0, len(labels)+1), e.label), labels...)
-	_ = stats.NewGaugesFuncWithMultiLabels(e.label+name, help, newlabels, func() map[string]int64 {
-		return vmap.Fetch()
-	})
+	newlabels := combineLabels(e.label, labels)
+	_ = stats.NewGaugesFuncWithMultiLabels(name, help, newlabels, vmap.Fetch)
 	return stats.NewGaugesFuncWithMultiLabels("", help, labels, f)
 }
 
@@ -324,7 +250,7 @@ func (e *Exporter) NewGaugeDurationFunc(name string, help string, f func() time.
 }
 
 // NewCountersWithSingleLabel creates a name-spaced equivalent for stats.NewCountersWithSingleLabel.
-// Tags are ignored if embedded.
+// Tags are ignored if the exporter is named.
 func (e *Exporter) NewCountersWithSingleLabel(name, help string, label string, tags ...string) *stats.CountersWithSingleLabel {
 	if e.name == "" || name == "" {
 		return stats.NewCountersWithSingleLabel(name, help, label, tags...)
@@ -336,7 +262,7 @@ func (e *Exporter) NewCountersWithSingleLabel(name, help string, label string, t
 }
 
 // NewGaugesWithSingleLabel creates a name-spaced equivalent for stats.NewGaugesWithSingleLabel.
-// Tags are ignored if embedded.
+// Tags are ignored if the exporter is named.
 func (e *Exporter) NewGaugesWithSingleLabel(name, help string, label string, tags ...string) *stats.GaugesWithSingleLabel {
 	if e.name == "" || name == "" {
 		return stats.NewGaugesWithSingleLabel(name, help, label, tags...)
@@ -371,49 +297,245 @@ func (e *Exporter) NewGaugesWithMultiLabels(name, help string, labels []string) 
 
 // NewTimings creates a name-spaced equivalent for stats.NewTimings.
 // The function currently just returns an unexported variable.
-// TODO(sougou): implement.
-func (e *Exporter) NewTimings(name string, help string, label string) *stats.Timings {
+func (e *Exporter) NewTimings(name string, help string, label string) *TimingsWrapper {
 	if e.name == "" || name == "" {
-		return stats.NewTimings(name, help, label)
+		return &TimingsWrapper{
+			timings: stats.NewMultiTimings(name, help, []string{label}),
+		}
 	}
-	return stats.NewTimings("", help, label)
+
+	exporterMu.Lock()
+	defer exporterMu.Unlock()
+
+	if tv, ok := timingsVars[name]; ok {
+		return &TimingsWrapper{
+			name:    e.name,
+			timings: tv,
+		}
+	}
+	mt := stats.NewMultiTimings(name, help, []string{e.label, label})
+	timingsVars[name] = mt
+	return &TimingsWrapper{
+		name:    e.name,
+		timings: mt,
+	}
 }
 
 // NewMultiTimings creates a name-spaced equivalent for stats.NewMultiTimings.
 // The function currently just returns an unexported variable.
-// TODO(sougou): implement.
-func (e *Exporter) NewMultiTimings(name string, help string, labels []string) *stats.MultiTimings {
+func (e *Exporter) NewMultiTimings(name string, help string, labels []string) *MultiTimingsWrapper {
 	if e.name == "" || name == "" {
-		return stats.NewMultiTimings(name, help, labels)
+		return &MultiTimingsWrapper{
+			timings: stats.NewMultiTimings(name, help, labels),
+		}
 	}
-	return stats.NewMultiTimings("", help, labels)
+
+	exporterMu.Lock()
+	defer exporterMu.Unlock()
+
+	if tv, ok := timingsVars[name]; ok {
+		return &MultiTimingsWrapper{
+			name:    e.name,
+			timings: tv,
+		}
+	}
+	mt := stats.NewMultiTimings(name, help, combineLabels(e.label, labels))
+	timingsVars[name] = mt
+	return &MultiTimingsWrapper{
+		name:    e.name,
+		timings: mt,
+	}
 }
 
 // NewRates creates a name-spaced equivalent for stats.NewRates.
 // The function currently just returns an unexported variable.
-// TODO(sougou): implement.
 func (e *Exporter) NewRates(name string, countTracker stats.CountTracker, samples int, interval time.Duration) *stats.Rates {
 	if e.name == "" || name == "" {
 		return stats.NewRates(name, countTracker, samples, interval)
 	}
-	return stats.NewRates("", countTracker, samples, interval)
+
+	exporterMu.Lock()
+	defer exporterMu.Unlock()
+
+	ov, ok := otherStatsVars[name]
+	if !ok {
+		ov = expvar.NewMap(name)
+		otherStatsVars[name] = ov
+	}
+	rates := stats.NewRates("", countTracker, samples, interval)
+	ov.Set(e.name, rates)
+	return rates
 }
 
 // NewHistogram creates a name-spaced equivalent for stats.NewHistogram.
 // The function currently just returns an unexported variable.
-// TODO(sougou): implement.
 func (e *Exporter) NewHistogram(name, help string, cutoffs []int64) *stats.Histogram {
 	if e.name == "" || name == "" {
 		return stats.NewHistogram(name, help, cutoffs)
 	}
-	return stats.NewHistogram("", help, cutoffs)
+
+	exporterMu.Lock()
+	defer exporterMu.Unlock()
+
+	ov, ok := otherStatsVars[name]
+	if !ok {
+		ov = expvar.NewMap(name)
+		otherStatsVars[name] = ov
+	}
+	hist := stats.NewHistogram("", help, cutoffs)
+	ov.Set(e.name, hist)
+	return hist
 }
 
 // Publish creates a name-spaced equivalent for stats.Publish.
-// The function just passes through if the Instance name is empty.
-// TODO(sougou): implement.
+// The function just passes through if the Exporter name is empty.
 func (e *Exporter) Publish(name string, v expvar.Var) {
-	if e.name == "" {
+	if e.name == "" || name == "" {
 		stats.Publish(name, v)
+		return
 	}
+
+	exporterMu.Lock()
+	defer exporterMu.Unlock()
+
+	ov, ok := otherStatsVars[name]
+	if !ok {
+		ov = expvar.NewMap(name)
+		otherStatsVars[name] = ov
+	}
+	ov.Set(e.name, v)
+}
+
+//-----------------------------------------------------------------
+
+// varMap contains the metadata for a merged stats var. It supports
+// gauges and counters. For every Exporter, it stores a function
+// that yields the counter or gauge values for that Exporter.
+type varMap struct {
+	mu   sync.Mutex
+	vars map[string]func() map[string]int64
+}
+
+// Set adds or updates the func for an Exporter.
+func (vmap *varMap) Set(name string, f func() map[string]int64) {
+	vmap.mu.Lock()
+	defer vmap.mu.Unlock()
+	vmap.vars[name] = f
+}
+
+// Unset removes the Exporter's entry from varMap.
+func (vmap *varMap) Unset(name string) {
+	vmap.mu.Lock()
+	defer vmap.mu.Unlock()
+	delete(vmap.vars, name)
+}
+
+// Fetch returns the consolidated stats value for all exporters.
+func (vmap *varMap) Fetch() map[string]int64 {
+	result := make(map[string]int64)
+	vmap.mu.Lock()
+	defer vmap.mu.Unlock()
+	for k, f := range vmap.vars {
+		for innerk, innerv := range f() {
+			if innerk == "" {
+				result[k] = innerv
+			} else {
+				result[k+"."+innerk] = innerv
+			}
+		}
+	}
+	return result
+}
+
+//-----------------------------------------------------------------
+
+// TimingsWrapper provides a namespaced version of stats.Timings.
+type TimingsWrapper struct {
+	name    string
+	timings *stats.MultiTimings
+}
+
+// Add behaves like Timings.Add.
+func (tw *TimingsWrapper) Add(name string, elapsed time.Duration) {
+	if tw.name == "" {
+		tw.timings.Add([]string{name}, elapsed)
+		return
+	}
+	tw.timings.Add([]string{tw.name, name}, elapsed)
+}
+
+// Record behaves like Timings.Record.
+func (tw *TimingsWrapper) Record(name string, startTime time.Time) {
+	if tw.name == "" {
+		tw.timings.Record([]string{name}, startTime)
+		return
+	}
+	tw.timings.Record([]string{tw.name, name}, startTime)
+}
+
+// Counts behaves lie Timings.Counts.
+func (tw *TimingsWrapper) Counts() map[string]int64 {
+	return tw.timings.Counts()
+}
+
+//-----------------------------------------------------------------
+
+// MultiTimingsWrapper provides a namespaced version of stats.MultiTimings.
+type MultiTimingsWrapper struct {
+	name    string
+	timings *stats.MultiTimings
+}
+
+// Add behaves like MultiTimings.Add.
+func (tw *MultiTimingsWrapper) Add(names []string, elapsed time.Duration) {
+	if tw.name == "" {
+		tw.timings.Add(names, elapsed)
+		return
+	}
+	newlabels := combineLabels(tw.name, names)
+	tw.timings.Add(newlabels, elapsed)
+}
+
+// Record behaves like MultiTimings.Record.
+func (tw *MultiTimingsWrapper) Record(names []string, startTime time.Time) {
+	if tw.name == "" {
+		tw.timings.Record(names, startTime)
+		return
+	}
+	newlabels := combineLabels(tw.name, names)
+	tw.timings.Record(newlabels, startTime)
+}
+
+// Counts behaves lie MultiTimings.Counts.
+func (tw *MultiTimingsWrapper) Counts() map[string]int64 {
+	return tw.timings.Counts()
+}
+
+//-----------------------------------------------------------------
+
+// handleFunc stores the http Handler for an Exporter. This function can
+// be replaced as needed.
+type handleFunc struct {
+	mu sync.Mutex
+	f  func(w http.ResponseWriter, r *http.Request)
+}
+
+// Set replaces the existing handler with a new one.
+func (hf *handleFunc) Set(f func(w http.ResponseWriter, r *http.Request)) {
+	hf.mu.Lock()
+	defer hf.mu.Unlock()
+	hf.f = f
+}
+
+// Get returns the current handler.
+func (hf *handleFunc) Get() func(w http.ResponseWriter, r *http.Request) {
+	hf.mu.Lock()
+	defer hf.mu.Unlock()
+	return hf.f
+}
+
+//-----------------------------------------------------------------
+
+func combineLabels(label string, labels []string) []string {
+	return append(append(make([]string, 0, len(labels)+1), label), labels...)
 }
