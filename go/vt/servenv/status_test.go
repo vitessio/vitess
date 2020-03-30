6ea0828875a3a9876dd5606e2774b77911a46fea
Sugu Sougoumarane <ssougou@gmail.com>
2020-03-29 17:10:26 -0700

    exporter: initial cut
    Signed-off-by: Sugu Sougoumarane <ssougou@gmail.com>

--- go/vt/servenv/status_test.go
+++ go/vt/servenv/status_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 The Vitess Authors.
+Copyright 2017 Google Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -7,7 +7,7 @@ You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
+Unless required by applicable law or agreedto in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
@@ -45,6 +45,49 @@ func TestStatus(t *testing.T) {
 	defer server.Close()
 
 	resp, err := http.Get(server.URL + StatusURLPath())
+	if err != nil {
+		t.Fatalf("http.Get: %v", err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("ioutil.ReadAll: %v", err)
+	}
+
+	cases := []string{
+		`h1.*test_part.*/h1`,
+		`THIS SHOULD BE UPPERCASE`,
+		`h1.*test_section.*/h1`,
+	}
+	for _, cas := range cases {
+		if !regexp.MustCompile(cas).Match(body) {
+			t.Errorf("failed matching: %q", cas)
+		}
+	}
+	t.Logf("body: \n%s", body)
+}
+
+func TestNamedStatus(t *testing.T) {
+	server := httptest.NewServer(nil)
+	defer server.Close()
+
+	name := "test"
+	sp := newStatusPage(name)
+	sp.addStatusFuncs(
+		template.FuncMap{
+			"github_com_vitessio_vitess_to_upper": strings.ToUpper,
+		})
+
+	sp.addStatusPart("test_part", `{{github_com_vitessio_vitess_to_upper . }}`, func() interface{} {
+		return "this should be uppercase"
+	})
+	sp.addStatusSection("test_section", func() string {
+		return "this is a section"
+	})
+
+	resp, err := http.Get(server.URL + "/" + name + StatusURLPath())
 	if err != nil {
 		t.Fatalf("http.Get: %v", err)
 	}
