271aad8b602ab93eb966a55f8df74034fc82c03d
Rohit Nayak <rohit@planetscale.com>
2020-04-10 17:36:09 +0200

    Added more dry-run logs
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>

--- go/vt/wrangler/switcher.go
+++ go/vt/wrangler/switcher.go
@@ -6,6 +6,8 @@ import (
 	"strings"
 	"time"
 
+	"vitess.io/vitess/go/mysql"
+	binlogdatapb "vitess.io/vitess/go/vt/proto/binlogdata"
 	topodatapb "vitess.io/vitess/go/vt/proto/topodata"
 
 	"golang.org/x/net/context"
@@ -159,12 +161,51 @@ func (dr *switchDryRun) allowTargetWrites(ctx context.Context) error {
 }
 
 func (dr *switchDryRun) changeRouting(ctx context.Context) error {
+	rules, err := dr.ts.wr.getRoutingRules(ctx)
+	if err != nil {
+		return err
+	}
 	dr.drLog.Log(fmt.Sprintf("Switch routing from keyspace %s to keyspace %s", dr.ts.sourceKeyspace, dr.ts.targetKeyspace))
+	deleteLogs := make([]string, 0)
+	addLogs := make([]string, 0)
+	if dr.ts.migrationType == binlogdatapb.MigrationType_TABLES {
+		for _, table := range dr.ts.tables {
+			for _, tabletType := range []topodatapb.TabletType{topodatapb.TabletType_REPLICA, topodatapb.TabletType_RDONLY} {
+				tt := strings.ToLower(tabletType.String())
+				deleteLogs = append(deleteLogs, fmt.Sprintf("\t%s => %s", table+"@"+tt, strings.Trim(rules[table+"@"+tt][0], "[]")))
+				deleteLogs = append(deleteLogs, fmt.Sprintf("\t%s => %s", dr.ts.targetKeyspace+"."+table+"@"+tt, strings.Trim(rules[dr.ts.targetKeyspace+"."+table+"@"+tt][0], "[]")))
+				deleteLogs = append(deleteLogs, fmt.Sprintf("\t%s => %s", dr.ts.sourceKeyspace+"."+table+"@"+tt, strings.Trim(rules[dr.ts.sourceKeyspace+"."+table+"@"+tt][0], "[]")))
+			}
+			addLogs = append(addLogs, fmt.Sprintf("\t%s => %s", table, dr.ts.targetKeyspace+"."+table))
+			addLogs = append(addLogs, fmt.Sprintf("\t%s => %s", dr.ts.sourceKeyspace+"."+table, dr.ts.targetKeyspace+"."+table))
+		}
+		if len(deleteLogs) > 0 {
+			dr.drLog.Log("Following rules will be deleted:")
+			dr.drLog.LogSlice(deleteLogs)
+			dr.drLog.Log("Following rules will be added:")
+			dr.drLog.LogSlice(addLogs)
+		}
+		return nil
+	}
+	deleteLogs = nil
+	addLogs = nil
+	for _, source := range dr.ts.sources {
+		deleteLogs = append(deleteLogs, fmt.Sprintf("\tShard %s, Tablet %s", source.si.ShardName(), source.si.MasterAlias.String()))
+	}
+	for _, target := range dr.ts.targets {
+		addLogs = append(addLogs, fmt.Sprintf("\tShard %s, Tablet %s", target.si.ShardName(), target.si.MasterAlias.String()))
+	}
+	if len(deleteLogs) > 0 {
+		dr.drLog.Log("IsMasterServing will be set to false for:")
+		dr.drLog.LogSlice(deleteLogs)
+		dr.drLog.Log("IsMasterServing will be set to true for:")
+		dr.drLog.LogSlice(addLogs)
+	}
 	return nil
 }
 
 func (dr *switchDryRun) streamMigraterfinalize(ctx context.Context, ts *trafficSwitcher, workflows []string) error {
-	dr.drLog.Log("SwitchWrites completed, freeze and delete migration streams on:")
+	dr.drLog.Log("SwitchWrites completed, freeze and delete vreplication streams on:")
 	logs := make([]string, 0)
 	for _, t := range ts.targets {
 		logs = append(logs, fmt.Sprintf("\ttablet %s", t.master.Alias))
@@ -184,33 +225,69 @@ func (dr *switchDryRun) startReverseVReplication(ctx context.Context) error {
 }
 
 func (dr *switchDryRun) createReverseVReplication(ctx context.Context) error {
-	dr.drLog.Log(fmt.Sprintf("Crate reverse replication workflow %s", dr.ts.reverseWorkflow))
+	dr.drLog.Log(fmt.Sprintf("Create reverse replication workflow %s", dr.ts.reverseWorkflow))
 	return nil
 }
 
 func (dr *switchDryRun) migrateStreams(ctx context.Context, sm *streamMigrater) error {
-	dr.drLog.Log(fmt.Sprintf("Migrate streams to %s", dr.ts.targetKeyspace))
+	if len(sm.templates) == 0 {
+		return nil
+	}
+	logs := make([]string, 0)
+
+	dr.drLog.Log(fmt.Sprintf("Migrate streams to %s:", dr.ts.targetKeyspace))
+	for key, streams := range sm.streams {
+		for _, stream := range streams {
+			logs = append(logs, fmt.Sprintf("\tShard %s Id %d, Workflow %s, Pos %s, BinLogSource %v", key, stream.id, stream.workflow, mysql.EncodePosition(stream.pos), stream.bls))
+		}
+	}
+	if len(logs) > 0 {
+		dr.drLog.Log("Source streams will be migrated:")
+		dr.drLog.LogSlice(logs)
+		logs = nil
+	}
+	for _, target := range dr.ts.targets {
+		tabletStreams := copyTabletStreams(sm.templates)
+		for _, vrs := range tabletStreams {
+			logs = append(logs, fmt.Sprintf("\t Keyspace %s, Shard %s, Tablet %s, Workflow %s, Id %d, Pos %v, BinLogSource %s",
+				vrs.bls.Keyspace, vrs.bls.Shard, target.master.Alias, vrs.workflow, vrs.id, mysql.EncodePosition(vrs.pos), vrs.bls))
+		}
+	}
+	if len(logs) > 0 {
+		dr.drLog.Log("Target streams will be created (as stopped):")
+		dr.drLog.LogSlice(logs)
+	}
 	return nil
 }
 
 func (dr *switchDryRun) waitForCatchup(ctx context.Context, filteredReplicationWaitTime time.Duration) error {
-	dr.drLog.Log(fmt.Sprintf("Wait for VReplication on all streams to catchup for upto %v", filteredReplicationWaitTime))
+	dr.drLog.Log(fmt.Sprintf("Wait for VReplication on stopped streams to catchup for upto %v", filteredReplicationWaitTime))
 	return nil
 }
 
 func (dr *switchDryRun) stopSourceWrites(ctx context.Context) error {
-	dr.drLog.Log(fmt.Sprintf("Stop writes on keyspace %s, tables %s", dr.ts.sourceKeyspace, strings.Join(dr.ts.tables, ",")))
+	logs := make([]string, 0)
+	for _, source := range dr.ts.sources {
+		position, _ := dr.ts.wr.tmc.MasterPosition(ctx, source.master.Tablet)
+		logs = append(logs, fmt.Sprintf("\tKeyspace %s, Shard %s at Position %s", dr.ts.sourceKeyspace, source.si.ShardName(), position))
+	}
+	if len(logs) > 0 {
+		dr.drLog.Log(fmt.Sprintf("Stop writes on keyspace %s, tables %s:", dr.ts.sourceKeyspace, strings.Join(dr.ts.tables, ",")))
+		dr.drLog.LogSlice(logs)
+	}
 	return nil
 }
 
 func (dr *switchDryRun) stopStreams(ctx context.Context, sm *streamMigrater) ([]string, error) {
-	if len(sm.streams) > 0 {
-		dr.drLog.Log(fmt.Sprintf("Stop streams on keyspace %s", dr.ts.sourceKeyspace))
-		for key, streams := range sm.streams {
-			for _, stream := range streams {
-				dr.drLog.Log(fmt.Sprintf("\tkey %s shard %s stream %+v", key, stream.bls.Shard, stream.bls))
-			}
+	logs := make([]string, 0)
+	for _, streams := range sm.streams {
+		for _, stream := range streams {
+			logs = append(logs, fmt.Sprintf("\tId %d Shard %s at Position %v", stream.id, stream.bls.Shard, mysql.EncodePosition(stream.pos)))
 		}
+	}
+	if len(logs) > 0 {
+		dr.drLog.Log(fmt.Sprintf("Stop streams on keyspace %s", dr.ts.sourceKeyspace))
+		dr.drLog.LogSlice(logs)
 	}
 	return nil, nil
 }
@@ -231,6 +308,14 @@ func (dr *switchDryRun) LockKeyspace(ctx context.Context, keyspace, _ string) (c
 }
 
 func (dr *switchDryRun) deleteTargetVReplication(ctx context.Context) error {
-	dr.drLog.Log("Delete left-over streams")
+	logs := make([]string, 0)
+	for _, target := range dr.ts.targets {
+		logs = append(logs, fmt.Sprintf("\tKeyspace %s, Shard %s, Tablet %d, Workflow %s, DbName %s",
+			target.master.Keyspace, target.master.Shard, target.master.Alias.Uid, dr.ts.workflow, target.master.DbName()))
+	}
+	if len(logs) > 0 {
+		dr.drLog.Log("Deleting vreplication streams on:")
+		dr.drLog.LogSlice(logs)
+	}
 	return nil
 }
