a74bc724e181dc28377d10e8dfc109ff99c0fe7d
ZouYu <zouy.fnst@cn.fujitsu.com>
2020-04-28 16:18:29 +0800

    　Add unit test for function in go/sync2/atomic.go
    Signed-off-by: ZouYu <zouy.fnst@cn.fujitsu.com>

--- go/sync2/atomic_test.go
+++ go/sync2/atomic_test.go
@@ -18,58 +18,93 @@ package sync2
 
 import (
 	"testing"
+	"time"
+
+	"gotest.tools/assert"
 )
+
+func TestAtomicInt32(t *testing.T) {
+	i := NewAtomicInt32(1)
+	assert.Equal(t, int32(1), i.Get())
+
+	i.Set(2)
+	assert.Equal(t, int32(2), i.Get())
+
+	i.Add(1)
+	assert.Equal(t, int32(3), i.Get())
+
+	i.CompareAndSwap(3, 4)
+	assert.Equal(t, int32(4), i.Get())
+
+	i.CompareAndSwap(3, 5)
+	assert.Equal(t, int32(4), i.Get())
+}
+
+func TestAtomicInt64(t *testing.T) {
+	i := NewAtomicInt64(1)
+	assert.Equal(t, int64(1), i.Get())
+
+	i.Set(2)
+	assert.Equal(t, int64(2), i.Get())
+
+	i.Add(1)
+	assert.Equal(t, int64(3), i.Get())
+
+	i.CompareAndSwap(3, 4)
+	assert.Equal(t, int64(4), i.Get())
+
+	i.CompareAndSwap(3, 5)
+	assert.Equal(t, int64(4), i.Get())
+}
+
+func TestAtomicDuration(t *testing.T) {
+	d := NewAtomicDuration(time.Second)
+	assert.Equal(t, time.Second, d.Get())
+
+	d.Set(time.Second * 2)
+	assert.Equal(t, time.Second*2, d.Get())
+
+	d.Add(time.Second)
+	assert.Equal(t, time.Second*3, d.Get())
+
+	d.CompareAndSwap(time.Second*3, time.Second*4)
+	assert.Equal(t, time.Second*4, d.Get())
+
+	d.CompareAndSwap(time.Second*3, time.Second*5)
+	assert.Equal(t, time.Second*4, d.Get())
+}
 
 func TestAtomicString(t *testing.T) {
 	var s AtomicString
-	if s.Get() != "" {
-		t.Errorf("want empty, got %s", s.Get())
-	}
+	assert.Equal(t, "", s.Get())
+
 	s.Set("a")
-	if s.Get() != "a" {
-		t.Errorf("want a, got %s", s.Get())
-	}
-	if s.CompareAndSwap("b", "c") {
-		t.Errorf("want false, got true")
-	}
-	if s.Get() != "a" {
-		t.Errorf("want a, got %s", s.Get())
-	}
-	if !s.CompareAndSwap("a", "c") {
-		t.Errorf("want true, got false")
-	}
-	if s.Get() != "c" {
-		t.Errorf("want c, got %s", s.Get())
-	}
+	assert.Equal(t, "a", s.Get())
+
+	assert.Equal(t, false, s.CompareAndSwap("b", "c"))
+	assert.Equal(t, "a", s.Get())
+
+	assert.Equal(t, true, s.CompareAndSwap("a", "c"))
+	assert.Equal(t, "c", s.Get())
 }
 
 func TestAtomicBool(t *testing.T) {
 	b := NewAtomicBool(true)
-	if !b.Get() {
-		t.Error("b.Get: false, want true")
-	}
-	b.Set(false)
-	if b.Get() {
-		t.Error("b.Get: true, want false")
-	}
-	b.Set(true)
-	if !b.Get() {
-		t.Error("b.Get: false, want true")
-	}
+	assert.Equal(t, true, b.Get())
 
-	if b.CompareAndSwap(false, true) {
-		t.Error("b.CompareAndSwap false, true should fail")
-	}
-	if !b.CompareAndSwap(true, false) {
-		t.Error("b.CompareAndSwap true, false should succeed")
-	}
-	if !b.CompareAndSwap(false, false) {
-		t.Error("b.CompareAndSwap false, false should NOP")
-	}
-	if !b.CompareAndSwap(false, true) {
-		t.Error("b.CompareAndSwap false, true should succeed")
-	}
-	if !b.CompareAndSwap(true, true) {
-		t.Error("b.CompareAndSwap true, true should NOP")
-	}
+	b.Set(false)
+	assert.Equal(t, false, b.Get())
+
+	b.Set(true)
+	assert.Equal(t, true, b.Get())
+
+	assert.Equal(t, false, b.CompareAndSwap(false, true))
+
+	assert.Equal(t, true, b.CompareAndSwap(true, false))
+
+	assert.Equal(t, true, b.CompareAndSwap(false, false))
+
+	assert.Equal(t, true, b.CompareAndSwap(false, true))
+
+	assert.Equal(t, true, b.CompareAndSwap(true, true))
 }
