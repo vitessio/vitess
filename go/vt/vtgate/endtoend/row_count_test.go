dcb74aeff0b2f45e58e326369e3cf6c7777373b4
Andres Taylor <andres@planetscale.com>
2020-05-14 10:44:10 +0200

    Turned test into table driven
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/vt/vtgate/endtoend/row_count_test.go
+++ go/vt/vtgate/endtoend/row_count_test.go
@@ -18,6 +18,7 @@ package endtoend
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -29,28 +30,35 @@ func TestRowCount(t *testing.T) {
 	conn, err := mysql.Connect(ctx, &vtParams)
 	require.NoError(t, err)
 	defer conn.Close()
+	type tc struct {
+		query    string
+		expected int
+	}
+	tests := []tc{{
+		query:    "insert into t1_row_count(id, id1) values(1, 1), (2, 1), (3, 3), (4, 3)",
+		expected: 4,
+	}, {
+		query:    "select * from t1_row_count",
+		expected: -1,
+	}, {
+		query:    "update t1_row_count set id1 = 500 where id in (1,3)",
+		expected: 2,
+	}, {
+		query:    "show tables",
+		expected: -1,
+	}, {
+		query:    "set @x = 24",
+		expected: 0,
+	}, {
+		query:    "delete from t1_row_count",
+		expected: 4,
+	}}
 
-	exec(t, conn, "insert into t1_row_count(id, id1) values(1, 1), (2, 1), (3, 3), (4, 3)")
-	qr := exec(t, conn, "select row_count()")
-	require.Equal(t, "INT64(4)", qr.Rows[0][0].String())
-
-	exec(t, conn, "select * from t1_row_count")
-	qr = exec(t, conn, "select row_count()")
-	require.Equal(t, "INT64(-1)", qr.Rows[0][0].String())
-
-	exec(t, conn, "update t1_row_count set id1 = 500 where id in (1,3)")
-	qr = exec(t, conn, "select row_count()")
-	require.Equal(t, "INT64(2)", qr.Rows[0][0].String())
-
-	exec(t, conn, "show tables")
-	qr = exec(t, conn, "select row_count()")
-	require.Equal(t, "INT64(-1)", qr.Rows[0][0].String())
-
-	exec(t, conn, "set @x = 24")
-	qr = exec(t, conn, "select row_count()")
-	require.Equal(t, "INT64(0)", qr.Rows[0][0].String())
-
-	exec(t, conn, "delete from t1_row_count")
-	qr = exec(t, conn, "select row_count()")
-	require.Equal(t, "INT64(4)", qr.Rows[0][0].String())
+	for _, test := range tests {
+		t.Run(test.query, func(t *testing.T) {
+			exec(t, conn, test.query)
+			qr := exec(t, conn, "select row_count()")
+			require.Equal(t, fmt.Sprintf("INT64(%d)", test.expected), qr.Rows[0][0].String())
+		})
+	}
 }
