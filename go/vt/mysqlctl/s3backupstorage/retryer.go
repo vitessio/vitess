19fb6bc0a472c17f86a0793b98c9d71193bd4d5e
Andrew Mason <amason@slack-corp.com>
2020-05-14 20:07:26 -0400

    Add doc comments
    Signed-off-by: Andrew Mason <amason@slack-corp.com>

--- go/vt/mysqlctl/s3backupstorage/retryer.go
+++ go/vt/mysqlctl/s3backupstorage/retryer.go
@@ -8,14 +8,22 @@ import (
 	"github.com/aws/aws-sdk-go/aws/request"
 )
 
+// ClosedConnectionRetryer implements the aws request.Retryer interface
+// and is used to retry closed connection errors during MultipartUpload
+// operations.
 type ClosedConnectionRetryer struct {
 	awsRetryer request.Retryer
 }
 
+// RetryRules is part of the Retryer interface. It defers to the underlying
+// aws Retryer to compute backoff rules.
 func (retryer *ClosedConnectionRetryer) RetryRules(r *request.Request) time.Duration {
 	return retryer.awsRetryer.RetryRules(r)
 }
 
+// ShouldRetry is part of the Retryer interface. It retries on errors that occur
+// due to a closed network connection, and then falls back to the underlying aws
+// Retryer for checking additional retry conditions.
 func (retryer *ClosedConnectionRetryer) ShouldRetry(r *request.Request) bool {
 	if retryer.MaxRetries() == 0 {
 		return false
@@ -34,6 +42,8 @@ func (retryer *ClosedConnectionRetryer) ShouldRetry(r *request.Request) bool {
 	return retryer.awsRetryer.ShouldRetry(r)
 }
 
+// MaxRetries is part of the Retryer interface. It defers to the
+// underlying aws Retryer for the max number of retries.
 func (retryer *ClosedConnectionRetryer) MaxRetries() int {
 	return retryer.awsRetryer.MaxRetries()
 }
