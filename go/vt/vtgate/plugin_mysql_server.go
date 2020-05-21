6496d1509473b9a043939f110c090e8aa294e787
huiqing <hzhou@pinterest.com>
2020-05-21 15:16:23 -0700

    SIGHUP for tls config update
    Signed-off-by: huiqing <hzhou@pinterest.com>

--- go/vt/vtgate/plugin_mysql_server.go
+++ go/vt/vtgate/plugin_mysql_server.go
@@ -21,6 +21,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"os/signal"
 	"regexp"
 	"strings"
 	"sync"
@@ -330,7 +331,7 @@ func (vh *vtgateHandler) session(c *mysql.Conn) *vtgatepb.Session {
 
 var mysqlListener *mysql.Listener
 var mysqlUnixListener *mysql.Listener
-
+var sigChan chan os.Signal
 var vtgateHandle *vtgateHandler
 
 // initiMySQLProtocol starts the mysql protocol.
@@ -377,12 +378,26 @@ func initMySQLProtocol() {
 			mysqlListener.ServerVersion = *mysqlServerVersion
 		}
 		if *mysqlSslCert != "" && *mysqlSslKey != "" {
-			mysqlListener.TLSConfig, err = vttls.ServerConfig(*mysqlSslCert, *mysqlSslKey, *mysqlSslCa)
+			serverConfig, err := vttls.ServerConfig(*mysqlSslCert, *mysqlSslKey, *mysqlSslCa)
 			if err != nil {
 				log.Exitf("grpcutils.TLSServerConfig failed: %v", err)
 				return
 			}
+			mysqlListener.TLSConfig.Store(serverConfig)
 			mysqlListener.RequireSecureTransport = *mysqlServerRequireSecureTransport
+			sigChan = make(chan os.Signal, 1)
+			signal.Notify(sigChan, syscall.SIGHUP)
+			go func() {
+				for range sigChan {
+					serverConfig, err := vttls.ServerConfig(*mysqlSslCert, *mysqlSslKey, *mysqlSslCa)
+					if err != nil {
+						log.Errorf("grpcutils.TLSServerConfig failed: %v", err)
+					} else {
+						log.Info("grpcutils.TLSServerConfig updated")
+						mysqlListener.TLSConfig.Store(serverConfig)
+					}
+				}
+			}()
 		}
 		mysqlListener.AllowClearTextWithoutTLS.Set(*mysqlAllowClearTextWithoutTLS)
 		// Check for the connection threshold
@@ -448,6 +463,9 @@ func shutdownMysqlProtocolAndDrain() {
 	if mysqlUnixListener != nil {
 		mysqlUnixListener.Close()
 		mysqlUnixListener = nil
+	}
+	if sigChan != nil {
+		signal.Stop(sigChan)
 	}
 
 	if atomic.LoadInt32(&busyConnections) > 0 {
