e9fb3876494727775f6bc063542b0995066eb60e
Andres Taylor <andres@planetscale.com>
2020-06-18 10:58:36 +0200

    Added UNION end to end tests
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/vt/vtexplain/vtexplain_vttablet.go
+++ go/vt/vtexplain/vtexplain_vttablet.go
@@ -454,7 +454,7 @@ func (t *explainTablet) HandleQuery(c *mysql.Conn, query string, callback func(*
 		case *sqlparser.Select:
 			selStmt = stmt
 		case *sqlparser.Union:
-			selStmt = stmt.Statements[1].(*sqlparser.Select)
+			selStmt = stmt.UnionSelects[1].Statement.(*sqlparser.Select)
 		default:
 			return fmt.Errorf("vtexplain: unsupported statement type +%v", reflect.TypeOf(stmt))
 		}
