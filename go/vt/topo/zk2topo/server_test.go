2c71439b00c72e7de0a06e87e8db93d3deee641b
Akilan Selvacoumar <31743758+Akilan1999@users.noreply.github.com>
2020-06-19 19:15:23 +0530

    Update server_test.go
    Signed-off-by: akilan <as251@hw.ac.uk>

--- go/vt/topo/zk2topo/server_test.go
+++ go/vt/topo/zk2topo/server_test.go
@@ -14,299 +14,64 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package etcd2topo
+package zk2topo
 
 import (
 	"fmt"
-	"io/ioutil"
-	"os"
-	"os/exec"
 	"path"
 	"testing"
-	"time"
-
-	"vitess.io/vitess/go/vt/log"
-
-	"vitess.io/vitess/go/vt/tlstest"
-
-	"github.com/coreos/etcd/pkg/transport"
 
 	"golang.org/x/net/context"
 
-	"github.com/coreos/etcd/clientv3"
 	"vitess.io/vitess/go/testfiles"
 	"vitess.io/vitess/go/vt/topo"
 	"vitess.io/vitess/go/vt/topo/test"
+	"vitess.io/vitess/go/vt/zkctl"
 
 	topodatapb "vitess.io/vitess/go/vt/proto/topodata"
 )
 
-// startEtcd starts an etcd subprocess, and waits for it to be ready.
-func startEtcd(t *testing.T) (*exec.Cmd, string, string) {
-	// Create a temporary directory.
-	dataDir, err := ioutil.TempDir("", "etcd")
-	if err != nil {
-		t.Fatalf("cannot create tempdir: %v", err)
-	}
+func TestZk2Topo(t *testing.T) {
+	// Start a real single ZK daemon, and close it after all tests are done.
+	zkd, serverAddr := zkctl.StartLocalZk(testfiles.GoVtTopoZk2topoZkID, testfiles.GoVtTopoZk2topoPort)
+	defer zkd.Teardown()
 
-	// Get our two ports to listen to.
-	port := testfiles.GoVtTopoEtcd2topoPort
-	name := "vitess_unit_test"
-	clientAddr := fmt.Sprintf("http://localhost:%v", port)
-	peerAddr := fmt.Sprintf("http://localhost:%v", port+1)
-	initialCluster := fmt.Sprintf("%v=%v", name, peerAddr)
-
-	cmd := exec.Command("etcd",
-		"-name", name,
-		"-advertise-client-urls", clientAddr,
-		"-initial-advertise-peer-urls", peerAddr,
-		"-listen-client-urls", clientAddr,
-		"-listen-peer-urls", peerAddr,
-		"-initial-cluster", initialCluster,
-		"-data-dir", dataDir)
-	err = cmd.Start()
-	if err != nil {
-		t.Fatalf("failed to start etcd: %v", err)
-	}
-
-	// Create a client to connect to the created etcd.
-	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   []string{clientAddr},
-		DialTimeout: 5 * time.Second,
-	})
-	if err != nil {
-		t.Fatalf("newCellClient(%v) failed: %v", clientAddr, err)
-	}
-
-	// Wait until we can list "/", or timeout.
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	start := time.Now()
-	for {
-		if _, err := cli.Get(ctx, "/"); err == nil {
-			break
-		}
-		if time.Since(start) > 10*time.Second {
-			t.Fatalf("Failed to start etcd daemon in time")
-		}
-		time.Sleep(10 * time.Millisecond)
-	}
-
-	return cmd, dataDir, clientAddr
-}
-
-// startEtcdWithTLS starts an etcd subprocess with TLS setup, and waits for it to be ready.
-func startEtcdWithTLS(t *testing.T) (string, *tlstest.ClientServerKeyPairs, func()) {
-	// Create a temporary directory.
-	dataDir, err := ioutil.TempDir("", "etcd")
-	if err != nil {
-		t.Fatalf("cannot create tempdir: %v", err)
-	}
-
-	// Get our two ports to listen to.
-	port := testfiles.GoVtTopoEtcd2topoPort
-	name := "vitess_unit_test"
-	clientAddr := fmt.Sprintf("https://localhost:%v", port+2)
-	peerAddr := fmt.Sprintf("https://localhost:%v", port+3)
-	initialCluster := fmt.Sprintf("%v=%v", name, peerAddr)
-
-	certs := tlstest.CreateClientServerCertPairs(dataDir)
-
-	cmd := exec.Command("etcd",
-		"-name", name,
-		"-advertise-client-urls", clientAddr,
-		"-initial-advertise-peer-urls", peerAddr,
-		"-listen-client-urls", clientAddr,
-		"-listen-peer-urls", peerAddr,
-		"-initial-cluster", initialCluster,
-		"-cert-file", certs.ServerCert,
-		"-key-file", certs.ServerKey,
-		"-trusted-ca-file", certs.ClientCA,
-		"-peer-trusted-ca-file", certs.ClientCA,
-		"-peer-cert-file", certs.ServerCert,
-		"-peer-key-file", certs.ServerKey,
-		"-client-cert-auth",
-		"-data-dir", dataDir)
-
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	err = cmd.Start()
-	if err != nil {
-		t.Fatalf("failed to start etcd: %v", err)
-	}
-
-	// Safe now to build up TLS info.
-	tlsInfo := transport.TLSInfo{
-		CertFile:      certs.ClientCert,
-		KeyFile:       certs.ClientKey,
-		TrustedCAFile: certs.ServerCA,
-	}
-
-	tlsConfig, err := tlsInfo.ClientConfig()
-	if err != nil {
-		t.Fatalf("failed to get tls.Config: %v", err)
-	}
-
-	var cli *clientv3.Client
-	// Create client
-	start := time.Now()
-	for {
-		// Create a client to connect to the created etcd.
-		cli, err = clientv3.New(clientv3.Config{
-			Endpoints:   []string{clientAddr},
-			TLS:         tlsConfig,
-			DialTimeout: 5 * time.Second,
-		})
-		if err == nil {
-			break
-		}
-		t.Logf("error establishing client for etcd tls test: %v", err)
-		if time.Since(start) > 60*time.Second {
-			t.Fatalf("failed to start client for etcd tls test in time")
-		}
-		time.Sleep(100 * time.Millisecond)
-	}
-	defer cli.Close()
-
-	// Wait until we can list "/", or timeout.
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	start = time.Now()
-	for {
-		if _, err := cli.Get(ctx, "/"); err == nil {
-			break
-		}
-		if time.Since(start) > 60*time.Second {
-			t.Fatalf("failed to start etcd daemon in time")
-		}
-		time.Sleep(100 * time.Millisecond)
-	}
-
-	stopEtcd := func() {
-		err3 := cmd.Process.Kill()
-		// log error
-		if err3 != nil {
-			log.Error("cmd.Process.Kill() failed error : %v", err3)
-		}
-
-		err2 := cmd.Wait()
-		// log error
-		if err2 != nil {
-			log.Error("cmd.wait() failed : %v", err2)
-		}
-		os.RemoveAll(dataDir)
-	}
-
-	return clientAddr, &certs, stopEtcd
-}
-
-func TestEtcd2TLS(t *testing.T) {
-	// Start a single etcd in the background.
-	clientAddr, certs, stopEtcd := startEtcdWithTLS(t)
-	defer stopEtcd()
-
+	// Run the test suite.
 	testIndex := 0
-	testRoot := fmt.Sprintf("/test-%v", testIndex)
-
-	// Create the server on the new root.
-	server, err := NewServerWithOpts(clientAddr, testRoot, certs.ClientCert, certs.ClientKey, certs.ServerCA)
-	if err != nil {
-		t.Fatalf("NewServerWithOpts failed: %v", err)
-	}
-	defer server.Close()
-
-	testCtx := context.Background()
-	testKey := "testkey"
-	testVal := "testval"
-	_, err = server.Create(testCtx, testKey, []byte(testVal))
-	if err != nil {
-		t.Fatalf("Failed to set key value pair: %v", err)
-	}
-	val, _, err := server.Get(testCtx, testKey)
-	if err != nil {
-		t.Fatalf("Failed to retrieve value at key we just set: %v", err)
-	}
-	if string(val) != testVal {
-		t.Fatalf("Value returned doesn't match %s, err: %v", testVal, err)
-	}
-}
-
-func TestEtcd2Topo(t *testing.T) {
-	// Start a single etcd in the background.
-	cmd, dataDir, clientAddr := startEtcd(t)
-	defer func() {
-		cmd.Process.Kill()
-		cmd.Wait()
-		os.RemoveAll(dataDir)
-	}()
-
-	testIndex := 0
-	newServer := func() *topo.Server {
+	test.TopoServerTestSuite(t, func() *topo.Server {
 		// Each test will use its own sub-directories.
+		// The directories will be created when used the first time.
 		testRoot := fmt.Sprintf("/test-%v", testIndex)
 		testIndex++
 
-		// Create the server on the new root.
-		ts, err := topo.OpenServer("etcd2", clientAddr, path.Join(testRoot, topo.GlobalCell))
+		globalRoot := path.Join(testRoot, topo.GlobalCell)
+		cellRoot := path.Join(testRoot, test.LocalCellName)
+
+		// Note we exercise the observer feature here by passing in
+		// the same server twice, with a "|" separator.
+		ts, err := topo.OpenServer("zk2", serverAddr+"|"+serverAddr, globalRoot)
 		if err != nil {
 			t.Fatalf("OpenServer() failed: %v", err)
 		}
-
-		// Create the CellInfo.
 		if err := ts.CreateCellInfo(context.Background(), test.LocalCellName, &topodatapb.CellInfo{
-			ServerAddress: clientAddr,
-			Root:          path.Join(testRoot, test.LocalCellName),
+			ServerAddress: serverAddr,
+			Root:          cellRoot,
 		}); err != nil {
 			t.Fatalf("CreateCellInfo() failed: %v", err)
 		}
 
 		return ts
-	}
-
-	// Run the TopoServerTestSuite tests.
-	test.TopoServerTestSuite(t, func() *topo.Server {
-		return newServer()
 	})
-
-	// Run etcd-specific tests.
-	ts := newServer()
-	testKeyspaceLock(t, ts)
-	ts.Close()
 }
 
-// testKeyspaceLock tests etcd-specific heartbeat (TTL).
-// Note TTL granularity is in seconds, even though the API uses time.Duration.
-// So we have to wait a long time in these tests.
-func testKeyspaceLock(t *testing.T, ts *topo.Server) {
-	ctx := context.Background()
-	keyspacePath := path.Join(topo.KeyspacesPath, "test_keyspace")
-	if err := ts.CreateKeyspace(ctx, "test_keyspace", &topodatapb.Keyspace{}); err != nil {
-		t.Fatalf("CreateKeyspace: %v", err)
+func TestHasObservers(t *testing.T) {
+	s1, s2, ok := hasObservers("s1:p1,s2:p2")
+	if ok {
+		t.Errorf("hasObservers(s1:p1,s2:p2): got unexpected %v %v %v", s1, s2, ok)
 	}
 
-	conn, err := ts.ConnForCell(ctx, topo.GlobalCell)
-	if err != nil {
-		t.Fatalf("ConnForCell failed: %v", err)
-	}
-
-	// Long TTL, unlock before lease runs out.
-	*leaseTTL = 1000
-	lockDescriptor, err := conn.Lock(ctx, keyspacePath, "ttl")
-	if err != nil {
-		t.Fatalf("Lock failed: %v", err)
-	}
-	if err := lockDescriptor.Unlock(ctx); err != nil {
-		t.Fatalf("Unlock failed: %v", err)
-	}
-
-	// Short TTL, make sure it doesn't expire.
-	*leaseTTL = 1
-	lockDescriptor, err = conn.Lock(ctx, keyspacePath, "short ttl")
-	if err != nil {
-		t.Fatalf("Lock failed: %v", err)
-	}
-	time.Sleep(2 * time.Second)
-	if err := lockDescriptor.Unlock(ctx); err != nil {
-		t.Fatalf("Unlock failed: %v", err)
+	s1, s2, ok = hasObservers("s1:p1,s2:p2|o1:p1,o2:p2")
+	if !ok || s1 != "s1:p1,s2:p2" || s2 != "o1:p1,o2:p2" {
+		t.Errorf("hasObservers(s1:p1,s2:p2|o1:p1,o2:p2): got unexpected %v %v %v", s1, s2, ok)
 	}
 }
