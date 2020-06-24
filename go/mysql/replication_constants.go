ed45ced17ee838829da6b02ce6fb5db8916a8a93
akilan <as251@hw.ac.uk>
2020-06-24 23:22:18 +0400

    changed linter to read only go directory
    Signed-off-by: akilan <as251@hw.ac.uk>

--- go/mysql/replication_constants.go
+++ go/mysql/replication_constants.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+//nolint:unparam
 package mysql
 
 // This file contains the constant definitions for this package.
