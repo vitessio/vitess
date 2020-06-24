d4ab57ad9fc2b950da01d9f7cda0b68617b00089
akilan <as251@hw.ac.uk>
2020-06-25 00:07:03 +0400

    Made linter scan only go files
    Signed-off-by: akilan <as251@hw.ac.uk>

--- go/mysql/replication_constants.go
+++ go/mysql/replication_constants.go
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
+//nolint:unparam
 package mysql
 
 // This file contains the constant definitions for this package.
