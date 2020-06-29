11d5b09918ec60bafc679bb60e5902c1b8d562dc
Andrew Mason <amason@slack-corp.com>
2020-06-29 09:13:47 -0400

    Fix segfault in backup retryer
    Not every case has `OrigErr` as non-nil, and getting at the underlying
    error this way can cause segfaults. It's much better to use the
    `Error()` function will which will safely include the error strings of
    any underlying errors.
    
    Signed-off-by: Andrew Mason <amason@slack-corp.com>

--- go/vt/mysqlctl/s3backupstorage/retryer.go
+++ go/vt/mysqlctl/s3backupstorage/retryer.go
@@ -35,7 +35,7 @@ func (retryer *ClosedConnectionRetryer) ShouldRetry(r *request.Request) bool {
 
 	if r.Error != nil {
 		if awsErr, ok := r.Error.(awserr.Error); ok {
-			return strings.Contains(awsErr.OrigErr().Error(), "use of closed network connection")
+			return strings.Contains(awsErr.Error(), "use of closed network connection")
 		}
 	}
 
