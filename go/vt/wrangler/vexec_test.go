d01b80b636a8de649f850bfcca60df709e79697e
Rohit Nayak <rohit@planetscale.com>
2020-07-05 20:33:09 +0200

    vtctl vexec/workflow actions: added more tests
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>

--- go/vt/wrangler/vexec_test.go
+++ go/vt/wrangler/vexec_test.go
@@ -22,6 +22,7 @@ import (
 	"sort"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"vitess.io/vitess/go/sqltypes"
@@ -171,32 +172,211 @@ func TestVExec(t *testing.T) {
 		"+----------------------+----+--------------------------------+---------+-----------+--------------+-------------------+",
 		"|        TABLET        | ID |          BINLOGSOURCE          |  STATE  |  DBNAME   | CURRENT GTID | MAXREPLICATIONLAG |",
 		"+----------------------+----+--------------------------------+---------+-----------+--------------+-------------------+",
-		"| -80/zone1-0000000200 |  1 | keyspace:\"source\" shard:\"0\"    | Running | vt_target | pos          |                 0 |",
+		"| -80/zone1-0000000200 |  1 | keyspace:\"source\" shard:\"0\"    | Copying | vt_target | pos          |                 0 |",
 		"|                      |    | filter:<rules:<match:\"t1\" > >  |         |           |              |                   |",
 		"+----------------------+----+--------------------------------+---------+-----------+--------------+-------------------+",
-		"| 80-/zone1-0000000210 |  1 | keyspace:\"source\" shard:\"0\"    | Running | vt_target | pos          |                 0 |",
+		"| 80-/zone1-0000000210 |  1 | keyspace:\"source\" shard:\"0\"    | Copying | vt_target | pos          |                 0 |",
 		"|                      |    | filter:<rules:<match:\"t1\" > >  |         |           |              |                   |",
 		"+----------------------+----+--------------------------------+---------+-----------+--------------+-------------------+",
 	}
 	require.Equal(t, strings.Join(dryRunResults, "\n")+"\n\n\n\n\n", logger.String())
 }
 
-/*
-func TextVExecValidations(t *testing.T) {
+func TestWorkflowStatusUpdate(t *testing.T) {
+	require.Equal(t, "Error", updateState("master tablet not contactable", "Running", nil, 0))
+	require.Equal(t, "Lagging", updateState("", "Running", nil, int64(time.Now().Second())-100))
+	require.Equal(t, "Copying", updateState("", "Running", []copyState{{Table: "t1", LastPK: "[[INT64(10)]]"}}, int64(time.Now().Second())))
+}
+
+func TestWorkflowListStreams(t *testing.T) {
 	ctx := context.Background()
-	workflow := ""
-	keyspace := ""
+	workflow := "wrWorkflow"
+	keyspace := "target"
+	env := newWranglerTestEnv([]string{"0"}, []string{"-80", "80-"}, "", nil)
+	defer env.close()
+	logger := logutil.NewMemoryLogger()
+	wr := New(logger, env.topoServ, env.tmc)
+
+	err := wr.listStreams(ctx, workflow, keyspace)
+	require.Nil(t, err)
+	want := `{
+	"Workflow": "wrWorkflow",
+	"SourceKeyspace": "source",
+	"TargetKeyspace": "target",
+	"Statuses": {
+		"-80/zone1-0000000200": [
+			{
+				"Shard": "-80",
+				"Tablet": "zone1-0000000200",
+				"ID": 1,
+				"Bls": {
+					"keyspace": "source",
+					"shard": "0",
+					"filter": {
+						"rules": [
+							{
+								"match": "t1"
+							}
+						]
+					}
+				},
+				"Pos": "pos",
+				"StopPos": "",
+				"State": "Copying",
+				"MaxReplicationLag": 0,
+				"DBName": "vt_target",
+				"TimeUpdated": 1234,
+				"Message": "",
+				"CopyState": [
+					{
+						"Table": "t",
+						"LastPK": "1"
+					}
+				]
+			}
+		],
+		"80-/zone1-0000000210": [
+			{
+				"Shard": "80-",
+				"Tablet": "zone1-0000000210",
+				"ID": 1,
+				"Bls": {
+					"keyspace": "source",
+					"shard": "0",
+					"filter": {
+						"rules": [
+							{
+								"match": "t1"
+							}
+						]
+					}
+				},
+				"Pos": "pos",
+				"StopPos": "",
+				"State": "Copying",
+				"MaxReplicationLag": 0,
+				"DBName": "vt_target",
+				"TimeUpdated": 1234,
+				"Message": "",
+				"CopyState": [
+					{
+						"Table": "t",
+						"LastPK": "1"
+					}
+				]
+			}
+		]
+	}
+}
+
+`
+	require.Equal(t, want, logger.String())
+
+	results, err := wr.execWorkflowAction(ctx, workflow, keyspace, "stop", false)
+	require.Nil(t, err)
+	require.Equal(t, "map[Tablet{zone1-0000000200}:rows_affected:1  Tablet{zone1-0000000210}:rows_affected:1 ]", fmt.Sprintf("%v", results))
+
+	logger.Clear()
+	results, err = wr.execWorkflowAction(ctx, workflow, keyspace, "stop", true)
+	require.Nil(t, err)
+	require.Equal(t, "map[]", fmt.Sprintf("%v", results))
+	dryRunResult := `Query: update _vt.vreplication set state = 'Stopped' where db_name = 'vt_target' and workflow = 'wrWorkflow'
+will be run on the following streams in keyspace target for workflow wrWorkflow:
+
+
++----------------------+----+--------------------------------+---------+-----------+--------------+-------------------+
+|        TABLET        | ID |          BINLOGSOURCE          |  STATE  |  DBNAME   | CURRENT GTID | MAXREPLICATIONLAG |
++----------------------+----+--------------------------------+---------+-----------+--------------+-------------------+
+| -80/zone1-0000000200 |  1 | keyspace:"source" shard:"0"    | Copying | vt_target | pos          |                 0 |
+|                      |    | filter:<rules:<match:"t1" > >  |         |           |              |                   |
++----------------------+----+--------------------------------+---------+-----------+--------------+-------------------+
+| 80-/zone1-0000000210 |  1 | keyspace:"source" shard:"0"    | Copying | vt_target | pos          |                 0 |
+|                      |    | filter:<rules:<match:"t1" > >  |         |           |              |                   |
++----------------------+----+--------------------------------+---------+-----------+--------------+-------------------+
+
+
+
+
+`
+	require.Equal(t, dryRunResult, logger.String())
+}
+
+func TestVExecValidations(t *testing.T) {
+	ctx := context.Background()
+	workflow := "wf"
+	keyspace := "ks"
 	query := ""
-	env := newWranglerTestEnv([]string{"0"}, []string{"-80","80-"}, "", nil)
+	env := newWranglerTestEnv([]string{"0"}, []string{"-80", "80-"}, "", nil)
 	defer env.close()
 
 	wr := New(logutil.NewConsoleLogger(), env.topoServ, env.tmc)
 
-	//validations: empty workflow/ks/query
-	//	valid workflow: no masters found? valid keyspace
-	// invalid query
-	// insert query
+	vx := newVExec(ctx, workflow, keyspace, query, wr)
 
+	type badQuery struct {
+		name        string
+		query       string
+		errorString string
+	}
+	badQueries := []badQuery{
+		{
+			name:        "invalid",
+			query:       "bad query",
+			errorString: "syntax error at position 4 near 'bad'",
+		},
+		{
+			name:        "incorrect table",
+			query:       "select * from _vt.vreplication2",
+			errorString: "invalid table name: _vt.vreplication2",
+		},
+		{
+			name:        "unsupported query",
+			query:       "describe _vt.vreplication",
+			errorString: "query not supported by vexec: otherread",
+		},
+	}
+	for _, bq := range badQueries {
+		t.Run(bq.name, func(t *testing.T) {
+			vx.query = bq.query
+			plan, err := vx.buildVExecPlan()
+			require.EqualError(t, err, bq.errorString)
+			require.Nil(t, plan)
+		})
+	}
+
+	type action struct {
+		name          string
+		want          string
+		expectedError error
+	}
+	updateSQL := "update _vt.vreplication set state = %s"
+	actions := []action{
+		{
+			name:          "start",
+			want:          fmt.Sprintf(updateSQL, encodeString("Running")),
+			expectedError: nil,
+		},
+		{
+			name:          "stop",
+			want:          fmt.Sprintf(updateSQL, encodeString("Stopped")),
+			expectedError: nil,
+		},
+		{
+			name:          "delete",
+			want:          "delete from _vt.vreplication",
+			expectedError: nil,
+		},
+		{
+			name:          "other",
+			want:          "",
+			expectedError: fmt.Errorf("invalid action found: other"),
+		}}
+
+	for _, a := range actions {
+		t.Run(a.name, func(t *testing.T) {
+			sql, err := wr.getWorkflowActionQuery(a.name)
+			require.Equal(t, a.expectedError, err)
+			require.Equal(t, a.want, sql)
+		})
+	}
 }
-
-*/
