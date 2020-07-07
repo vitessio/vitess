33687c16803896c33cc5f6bb60d100255b265662
Peter Farr <Peter@PrismaPhonic.com>
2020-07-06 19:47:08 -0700

    Fix incorrect test methodology.
    Signed-off-by: Peter Farr <Peter@PrismaPhonic.com>

--- go/vt/vttablet/tmrpctest/test_tm_rpc.go
+++ go/vt/vttablet/tmrpctest/test_tm_rpc.go
@@ -702,16 +702,6 @@ var testReplicationStatus = &replicationdatapb.Status{
 	MasterConnectRetry:  12,
 }
 
-var testAfterStopIOThreadReplicationStatus = &replicationdatapb.Status{
-	Position:            "MariaDB/1-345-789",
-	IoThreadRunning:     false,
-	SqlThreadRunning:    true,
-	SecondsBehindMaster: 654,
-	MasterHost:          "master.host",
-	MasterPort:          3366,
-	MasterConnectRetry:  12,
-}
-
 func (fra *fakeRPCTM) SlaveStatus(ctx context.Context) (*replicationdatapb.Status, error) {
 	if fra.panics {
 		panic(fmt.Errorf("test-triggered panic"))
@@ -1295,8 +1285,8 @@ func tmRPCTestReplicaWasRestartedPanic(ctx context.Context, t *testing.T, client
 func tmRPCTestStopReplicationAndGetStatus(ctx context.Context, t *testing.T, client tmclient.TabletManagerClient, tablet *topodatapb.Tablet) {
 	rp, _, err := client.StopReplicationAndGetStatus(ctx, tablet, "")
 	compareError(t, "StopReplicationAndGetStatus", err, rp, testReplicationStatus)
-	_, status, err := client.StopReplicationAndGetStatus(ctx, tablet, "IOThreadOnly")
-	compareError(t, "StopReplicationAndGetStatus", err, status.After, testAfterStopIOThreadReplicationStatus)
+	rp, _, err = client.StopReplicationAndGetStatus(ctx, tablet, "")
+	compareError(t, "StopReplicationAndGetStatus", err, rp, testReplicationStatus)
 }
 
 func tmRPCTestStopReplicationAndGetStatusPanic(ctx context.Context, t *testing.T, client tmclient.TabletManagerClient, tablet *topodatapb.Tablet) {
