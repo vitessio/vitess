1a65c0224752c8abfe149165ea722f11d9607df8
Rohit Nayak <rohit@planetscale.com>
2020-07-10 13:59:14 +0200

    WIP
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>

--- go/test/endtoend/vreplication/misc_test.go
+++ go/test/endtoend/vreplication/misc_test.go
@@ -2,11 +2,11 @@ package vreplication
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
-	"vitess.io/vitess/go/test/endtoend/cluster"
 )
 
 const (
@@ -75,7 +75,7 @@ create table mine(mid int, name varchar(128), primary key (mid));
 `
 	initialWalesInserts = `
 insert into warehouse(wid, name) values (1, 'Cardiff'), (2, 'Swansea');
-insert into coal(cid, type) values (2, 'Anthracite');
+insert into coal(cid, type) values (1, 'Lignite'), (2, 'Anthracite'), (3, 'Bituminous'), (4, 'Peat');
 `
 	initialNewcastleInserts = `
 insert into mine(mid, name) values (1, 'Elswick'), (2, 'Winlaton'), (3, 'Heworth'), (4, 'Tyneside'), (5, 'Quayside');
@@ -91,45 +91,45 @@ func TestShardedMoveTables(t *testing.T) {
 	//defer vc.TearDown()
 
 	cell = vc.Cells[cellName]
-	//vc.AddKeyspace(t, cell, "wales", "0", walesVSchema, walesSchema, 1, 0, 100)
-	vc.AddKeyspace(t, cell, "wales", "-80,80-", walesVSchema, walesSchema, 1, 0, 100)
+	sourceKeyspace := "wales"
+	sourceShards := "-80,80-"
+	vc.AddKeyspace(t, cell, sourceKeyspace, sourceShards, walesVSchema, walesSchema, 1, 0, 100)
 	vtgate = cell.Vtgates[0]
 	assert.NotNil(t, vtgate)
-	vtgate.WaitForStatusOfTabletInShard(fmt.Sprintf("%s.%s.master", "wales", "0"), 1)
-	vtgate.WaitForStatusOfTabletInShard(fmt.Sprintf("%s.%s.master", "wales", "80-"), 1)
-	vtgate.WaitForStatusOfTabletInShard(fmt.Sprintf("%s.%s.master", "wales", "-80"), 1)
+	for _, shard := range strings.Split(sourceShards, ",") {
+		vtgate.WaitForStatusOfTabletInShard(fmt.Sprintf("%s.%s.master", sourceKeyspace, shard), 1)
+	}
 
 	vtgateConn = getConnection(t, globalConfig.vtgateMySQLPort)
 	defer vtgateConn.Close()
 
-	vc.AddKeyspace(t, cell, "newcastle", "-80,80-c0,c0-", newcastleVSchema, newcastleSchema, 1, 0, 300)
-	vtgate.WaitForStatusOfTabletInShard(fmt.Sprintf("%s.%s.master", "newcastle", "-80"), 1)
-	vtgate.WaitForStatusOfTabletInShard(fmt.Sprintf("%s.%s.master", "newcastle", "80-c0"), 1)
-	vtgate.WaitForStatusOfTabletInShard(fmt.Sprintf("%s.%s.master", "newcastle", "c0-"), 1)
+	targetKeyspace := "newcastle"
+	targetShards := "-40,40-80,80-c0,c0-"
+	vc.AddKeyspace(t, cell, "newcastle", targetShards, newcastleVSchema, newcastleSchema, 1, 0, 300)
+	for _, shard := range strings.Split(targetShards, ",") {
+		vtgate.WaitForStatusOfTabletInShard(fmt.Sprintf("%s.%s.master", targetKeyspace, shard), 1)
+	}
 
 	execMultipleQueries(t, vtgateConn, "wales", initialWalesInserts)
 	execMultipleQueries(t, vtgateConn, "newcastle", initialNewcastleInserts)
 
 	workflow := "c2n"
-	ksWorkflow := "newcastle." + workflow
+	ksWorkflow := targetKeyspace + "." + workflow
 	if output, err := vc.VtctlClient.ExecuteCommandWithOutput("MoveTables", "-cell="+cell.Name, "-workflow="+workflow,
-		"-tablet_types="+"replica,rdonly", "wales", "newcastle", "coal"); err != nil {
+		"-tablet_types="+"replica,rdonly", sourceKeyspace, targetKeyspace, "coal"); err != nil {
 		t.Fatalf("MoveTables command failed with %s\n %v\n", output, err)
 	}
 
-	tab1 := vc.Cells[cell.Name].Keyspaces["newcastle"].Shards["-80"].Tablets["zone1-300"].Vttablet
-	tab2 := vc.Cells[cell.Name].Keyspaces["newcastle"].Shards["80-c0"].Tablets["zone1-400"].Vttablet
-	tab3 := vc.Cells[cell.Name].Keyspaces["newcastle"].Shards["c0-"].Tablets["zone1-500"].Vttablet
-
-	tabs := []*cluster.VttabletProcess{tab1, tab2, tab3}
-	for _, tab := range tabs {
+	tablets := vc.getVttabletsInKeyspace(t, cell, targetKeyspace, "master")
+	for _, tab := range tablets {
 		if vc.WaitForVReplicationToCatchup(tab, workflow, "vt_newcastle", 1*time.Second) != nil {
 			t.Fatalf("MoveTables timed out for %s:%d", ksWorkflow, tab.TabletUID)
 		}
 	}
-	assert.Empty(t, validateCountInTablet(t, tab1, "newcastle", "coal", 1))
-	assert.Empty(t, validateCountInTablet(t, tab2, "newcastle", "coal", 0))
-	assert.Empty(t, validateCountInTablet(t, tab3, "newcastle", "coal", 0))
-	assert.Empty(t, validateCount(t, vtgateConn, "newcastle", "newcastle.coal", 1))
+	for _, tab := range tablets {
+		assert.Empty(t, validateCountInTablet(t, tab, "newcastle", "coal", 1))
+
+	}
+	assert.Empty(t, validateCount(t, vtgateConn, "newcastle", "newcastle.coal", 4))
 
 }
