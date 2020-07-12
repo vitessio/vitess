1c9fd18b8a429f2b72933dc77493f7d16dd2d269
Arindam Nayak <arindam.nayak@outlook.com>
2020-07-12 17:37:47 +0530

    Added binlog server as process
    Signed-off-by: Arindam Nayak <arindam.nayak@outlook.com>

---
+++ go/test/endtoend/recovery/pitr/main_test.go
@@ -0,0 +1,124 @@
+package pitr
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"testing"
+
+	"vitess.io/vitess/go/test/endtoend/cluster"
+	"vitess.io/vitess/go/vt/log"
+)
+
+var (
+	clusterInstance *cluster.LocalProcessCluster
+	masterTablet    *cluster.Vttablet
+	replicaTablet   *cluster.Vttablet
+
+	cell          = "zone1"
+	hostname      = "localhost"
+	keyspaceName  = "ks"
+	dbName        = "vt_ks"
+	shardName     = "0"
+	mysqlUserName = "vt_dba"
+)
+
+func TestMain(m *testing.M) {
+	defer cluster.PanicHandler(nil)
+	flag.Parse()
+
+	exitCode := func() int {
+		clusterInstance = cluster.NewCluster(cell, hostname)
+		defer clusterInstance.Teardown()
+
+		// Start topo server
+		if err := clusterInstance.StartTopo(); err != nil {
+			log.Error(err)
+			return 1
+		}
+		initDBFile := updateDBInitFile()
+		if err := clusterInstance.VtctlProcess.CreateKeyspace(keyspaceName); err != nil {
+			log.Error(err)
+			return 1
+		}
+		clusterInstance.Keyspaces = append(clusterInstance.Keyspaces, cluster.Keyspace{Name: keyspaceName})
+
+		masterTablet = clusterInstance.NewVttabletInstance("replica", 0, cell)
+		replicaTablet = clusterInstance.NewVttabletInstance("replica", 0, cell)
+
+		err := startTablets([]*cluster.Vttablet{masterTablet, replicaTablet}, initDBFile)
+		if err != nil {
+			return 1
+		}
+		err = clusterInstance.VtctlclientProcess.InitShardMaster(keyspaceName, shardName, cell, masterTablet.TabletUID)
+		return m.Run()
+	}()
+	os.Exit(exitCode)
+
+}
+
+// crates a vt_dba user to login to the mysql without password
+func updateDBInitFile() string {
+	initDb, _ := ioutil.ReadFile(path.Join(os.Getenv("VTROOT"), "/config/init_db.sql"))
+	sql := string(initDb)
+	newInitDBFile := path.Join(clusterInstance.TmpDirectory, "init_db_custom.sql")
+	sql = sql + fmt.Sprintf(`
+CREATE USER '%s'@'%%' ;
+GRANT ALL ON *.* TO '%s'@'%%';
+GRANT GRANT OPTION ON *.* TO '%s'@'%%';
+FLUSH PRIVILEGES;
+create database %s;
+`, mysqlUserName, mysqlUserName, mysqlUserName, dbName)
+	ioutil.WriteFile(newInitDBFile, []byte(sql), 0666)
+	return newInitDBFile
+}
+
+func startTablets(tablets []*cluster.Vttablet, initDBFile string) error {
+	var mysqlProcesses []*exec.Cmd
+	shard := &cluster.Shard{
+		Name: shardName,
+	}
+	for _, tablet := range tablets {
+		//tablet.MysqlctlProcess = cluster.MysqlCtlProcessInstance()
+		tablet.MysqlctlProcess = *cluster.MysqlCtlProcessInstance(tablet.TabletUID, tablet.MySQLPort, clusterInstance.TmpDirectory)
+		tablet.MysqlctlProcess.InitDBFile = initDBFile
+		if proc, err := tablet.MysqlctlProcess.StartProcess(); err != nil {
+			return err
+		} else {
+			// ignore golint warning, we need the else block to use proc
+			mysqlProcesses = append(mysqlProcesses, proc)
+		}
+		tablet.VttabletProcess = cluster.VttabletProcessInstance(tablet.HTTPPort,
+			tablet.GrpcPort,
+			tablet.TabletUID,
+			clusterInstance.Cell,
+			shardName,
+			keyspaceName,
+			clusterInstance.VtctldProcess.Port,
+			tablet.Type,
+			clusterInstance.TopoProcess.Port,
+			clusterInstance.Hostname,
+			clusterInstance.TmpDirectory,
+			clusterInstance.VtTabletExtraArgs,
+			clusterInstance.EnableSemiSync)
+		tablet.Alias = tablet.VttabletProcess.TabletPath
+		tablet.VttabletProcess.SupportsBackup = true
+		shard.Vttablets = append(shard.Vttablets, tablet)
+		shard.Vttablets = append(shard.Vttablets, tablet)
+	}
+	for _, proc := range mysqlProcesses {
+		proc.Wait()
+	}
+	for _, tablet := range tablets {
+		err := tablet.VttabletProcess.Setup()
+		if err != nil {
+			log.Error(err)
+			return err
+		}
+	}
+	clusterInstance.Keyspaces[0].Shards = []cluster.Shard{*shard}
+	return nil
+}
--- go/test/endtoend/recovery/pitr/pitr_test.go
+++ go/test/endtoend/recovery/pitr/pitr_test.go
@@ -1,32 +1,35 @@
 package pitr
 
 import (
-	"flag"
-	"os"
-	"os/exec"
-	"path"
-	"strings"
+	"fmt"
 	"testing"
+	"time"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"vitess.io/vitess/go/test/endtoend/cluster"
 )
 
-const (
-	binlogServer = "rippled"
+var (
+	createTable = `create table product (id bigint(20) primary key AUTO_INCREMENT, name char(10), created bigint(20));`
+	insertTable = `set time_zone='+00:00';insert into product (name, created) values('%s', unix_timestamp());`
 )
 
 func TestPointInTimeRecovery(t *testing.T) {
-	defer cluster.PanicHandler(nil)
-	flag.Parse()
-
-	exePath := path.Join(os.Getenv("EXTRA_BIN"), binlogServer)
-	tmpProcess := exec.Command(
-		exePath,
-		"--version",
-	)
-	output, err := tmpProcess.CombinedOutput()
+	_, err := masterTablet.VttabletProcess.QueryTablet(createTable, keyspaceName, true)
 	require.NoError(t, err)
-	assert.True(t, strings.Contains(string(output), "Debug build"))
+	println(fmt.Sprintf(insertTable, "p1"))
+	_, err = masterTablet.VttabletProcess.QueryTablet(fmt.Sprintf(insertTable, "p1"), keyspaceName, true)
+
+	bs, err := newBinlogServer(hostname, clusterInstance.GetAndReservePort())
+	defer bs.stop()
+	require.NoError(t, err)
+
+	err = bs.start(mysqlMaster{
+		hostname: "127.0.0.1",
+		port:     masterTablet.MysqlctlProcess.MySQLPort,
+		username: mysqlUserName,
+	})
+	fmt.Println("mysql -u vt_dba -h 127.0.0.1 -P", replicaTablet.MysqlctlProcess.MySQLPort, " vt_ks -e \"select * from product\" ")
+	fmt.Println("mysql -h 127.0.0.1 -P ", bs.port, " -u ripple -e \"SELECT @@global.gtid_executed\"")
+	time.Sleep(1 * time.Minute)
+
 }
