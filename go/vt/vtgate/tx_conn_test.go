84f01e31cc4b24c15855e80d12daae5ee3f679e8
Harshit Gangal <harshit@planetscale.com>
2020-07-16 19:33:31 +0530

    call release on commit/rollback failure
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/vt/vtgate/tx_conn_test.go
+++ go/vt/vtgate/tx_conn_test.go
@@ -193,7 +193,7 @@ func TestTxConnReservedCommitSuccess(t *testing.T) {
 	assert.EqualValues(t, 1, sbc1.ReleaseCount.Get(), "sbc1.ReleaseCount")
 }
 
-func TestTxConnReservedAndCommitWithDifferentNumberOfShards(t *testing.T) {
+func TestTxConnReservedOn2ShardTxOn1ShardAndCommit(t *testing.T) {
 	keyspace := "TestTxConn"
 	sc, sbc0, sbc1, rss0, rss1, _ := newTestTxConnEnv(t, keyspace)
 	sc.txConn.mode = vtgatepb.TransactionMode_MULTI
@@ -286,6 +286,99 @@ func TestTxConnReservedAndCommitWithDifferentNumberOfShards(t *testing.T) {
 	assert.EqualValues(t, 0, sbc1.CommitCount.Get(), "sbc1.CommitCount")
 }
 
+func TestTxConnReservedOn2ShardTxOn1ShardAndRollback(t *testing.T) {
+	keyspace := "TestTxConn"
+	sc, sbc0, sbc1, rss0, rss1, _ := newTestTxConnEnv(t, keyspace)
+	sc.txConn.mode = vtgatepb.TransactionMode_MULTI
+
+	// Sequence the executes to ensure shard session order
+	session := NewSafeSession(&vtgatepb.Session{InReservedConn: true})
+
+	// this will create reserved connections against all tablets
+	_, errs := sc.ExecuteMultiShard(ctx, rss1, queries, session, false)
+	require.Empty(t, errs)
+	_, errs = sc.ExecuteMultiShard(ctx, rss0, queries, session, false)
+	require.Empty(t, errs)
+
+	wantSession := vtgatepb.Session{
+		InReservedConn: true,
+		ShardSessions: []*vtgatepb.Session_ShardSession{{
+			Target: &querypb.Target{
+				Keyspace:   keyspace,
+				Shard:      "1",
+				TabletType: topodatapb.TabletType_MASTER,
+			},
+			ReservedId:  1,
+			TabletAlias: sbc0.Tablet().Alias,
+		}, {
+			Target: &querypb.Target{
+				Keyspace:   keyspace,
+				Shard:      "0",
+				TabletType: topodatapb.TabletType_MASTER,
+			},
+			ReservedId:  1,
+			TabletAlias: sbc1.Tablet().Alias,
+		}},
+	}
+	utils.MustMatch(t, &wantSession, session.Session, "Session")
+
+	session.Session.InTransaction = true
+
+	// start a transaction against rss0
+	sc.ExecuteMultiShard(ctx, rss0, queries, session, false)
+	wantSession = vtgatepb.Session{
+		InTransaction:  true,
+		InReservedConn: true,
+		ShardSessions: []*vtgatepb.Session_ShardSession{{
+			Target: &querypb.Target{
+				Keyspace:   keyspace,
+				Shard:      "1",
+				TabletType: topodatapb.TabletType_MASTER,
+			},
+			ReservedId:  1,
+			TabletAlias: sbc1.Tablet().Alias,
+		}, {
+			Target: &querypb.Target{
+				Keyspace:   keyspace,
+				Shard:      "0",
+				TabletType: topodatapb.TabletType_MASTER,
+			},
+			TransactionId: 1,
+			ReservedId:    1,
+			TabletAlias:   sbc0.Tablet().Alias,
+		}},
+	}
+
+	utils.MustMatch(t, &wantSession, session.Session, "Session")
+
+	require.NoError(t,
+		sc.txConn.Rollback(ctx, session))
+
+	wantSession = vtgatepb.Session{
+		InReservedConn: true,
+		ShardSessions: []*vtgatepb.Session_ShardSession{{
+			Target: &querypb.Target{
+				Keyspace:   keyspace,
+				Shard:      "1",
+				TabletType: topodatapb.TabletType_MASTER,
+			},
+			ReservedId:  1,
+			TabletAlias: sbc1.Tablet().Alias,
+		}, {
+			Target: &querypb.Target{
+				Keyspace:   keyspace,
+				Shard:      "0",
+				TabletType: topodatapb.TabletType_MASTER,
+			},
+			ReservedId:  2,
+			TabletAlias: sbc0.Tablet().Alias,
+		}},
+	}
+	utils.MustMatch(t, &wantSession, session.Session, "Session")
+	assert.EqualValues(t, 1, sbc0.RollbackCount.Get(), "sbc0.RollbackCount")
+	assert.EqualValues(t, 0, sbc1.RollbackCount.Get(), "sbc1.RollbackCount")
+}
+
 func TestTxConnCommitOrderFailure1(t *testing.T) {
 	sc, sbc0, sbc1, rss0, rss1, _ := newTestTxConnEnv(t, "TestTxConn")
 	sc.txConn.mode = vtgatepb.TransactionMode_MULTI
@@ -313,8 +406,8 @@ func TestTxConnCommitOrderFailure1(t *testing.T) {
 	// first commit failed so we don't try to commit the second shard
 	assert.EqualValues(t, 0, sbc1.CommitCount.Get(), "sbc1.CommitCount")
 	// When the commit fails, we try to clean up by issuing a rollback
-	assert.EqualValues(t, 2, sbc0.RollbackCount.Get(), "sbc0.RollbackCount")
-	assert.EqualValues(t, 1, sbc1.RollbackCount.Get(), "sbc1.RollbackCount")
+	assert.EqualValues(t, 2, sbc0.ReleaseCount.Get(), "sbc0.ReleaseCount")
+	assert.EqualValues(t, 1, sbc1.ReleaseCount.Get(), "sbc1.ReleaseCount")
 }
 
 func TestTxConnCommitOrderFailure2(t *testing.T) {
@@ -345,8 +438,8 @@ func TestTxConnCommitOrderFailure2(t *testing.T) {
 	assert.EqualValues(t, 1, sbc0.CommitCount.Get(), "sbc0.CommitCount")
 	assert.EqualValues(t, 1, sbc1.CommitCount.Get(), "sbc1.CommitCount")
 	// When the commit fails, we try to clean up by issuing a rollback
-	assert.EqualValues(t, 0, sbc0.RollbackCount.Get(), "sbc0.RollbackCount")
-	assert.EqualValues(t, 2, sbc1.RollbackCount.Get(), "sbc1.RollbackCount")
+	assert.EqualValues(t, 0, sbc0.ReleaseCount.Get(), "sbc0.ReleaseCount")
+	assert.EqualValues(t, 2, sbc1.ReleaseCount.Get(), "sbc1.ReleaseCount")
 }
 
 func TestTxConnCommitOrderFailure3(t *testing.T) {
@@ -794,11 +887,28 @@ func TestTxConnReservedRollback(t *testing.T) {
 	utils.MustMatch(t, &wantSession, session.Session, "Session")
 	assert.EqualValues(t, 1, sbc0.RollbackCount.Get(), "sbc0.RollbackCount")
 	assert.EqualValues(t, 1, sbc1.RollbackCount.Get(), "sbc1.RollbackCount")
+	assert.EqualValues(t, 0, sbc0.ReleaseCount.Get(), "sbc0.ReleaseCount")
+	assert.EqualValues(t, 0, sbc1.ReleaseCount.Get(), "sbc1.ReleaseCount")
+}
 
-	require.NoError(t,
-		sc.txConn.Release(ctx, session))
-	wantSession = vtgatepb.Session{}
+func TestTxConnReservedRollbackFailure(t *testing.T) {
+	sc, sbc0, sbc1, rss0, _, rss01 := newTestTxConnEnv(t, "TxConnReservedRollback")
+
+	session := NewSafeSession(&vtgatepb.Session{InTransaction: true, InReservedConn: true})
+	sc.ExecuteMultiShard(ctx, rss0, queries, session, false)
+	sc.ExecuteMultiShard(ctx, rss01, twoQueries, session, false)
+
+	sbc1.MustFailCodes[vtrpcpb.Code_INVALID_ARGUMENT] = 1
+	require.Error(t,
+		sc.txConn.Rollback(ctx, session))
+	wantSession := vtgatepb.Session{
+		Warnings: []*querypb.QueryWarning{{
+			Message: "rollback encountered an error and connection to all shard for this session is released: Code: INVALID_ARGUMENT\nINVALID_ARGUMENT error\n\ntarget: TxConnReservedRollback.1.master, used tablet: aa-0 (1)",
+		}},
+	}
 	utils.MustMatch(t, &wantSession, session.Session, "Session")
+	assert.EqualValues(t, 1, sbc0.RollbackCount.Get(), "sbc0.RollbackCount")
+	assert.EqualValues(t, 1, sbc1.RollbackCount.Get(), "sbc1.RollbackCount")
 	assert.EqualValues(t, 1, sbc0.ReleaseCount.Get(), "sbc0.ReleaseCount")
 	assert.EqualValues(t, 1, sbc1.ReleaseCount.Get(), "sbc1.ReleaseCount")
 }
