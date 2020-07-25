7cd9ba8f10eca0d6dfa141ad965114e99542a8e4
Sugu Sougoumarane <ssougou@gmail.com>
2020-07-25 13:41:17 -0700

    vttablet: more review comments
    Signed-off-by: Sugu Sougoumarane <ssougou@gmail.com>

--- go/vt/vttablet/endtoend/framework/client.go
+++ go/vt/vttablet/endtoend/framework/client.go
@@ -162,7 +162,7 @@ func (client *QueryClient) ReadTransaction(dtid string) (*querypb.TransactionMet
 // SetServingType is for testing transitions.
 // It currently supports only master->replica and back.
 func (client *QueryClient) SetServingType(tabletType topodatapb.TabletType) error {
-	err := client.server.SetServingType(tabletType, time.Time{}, true, "")
+	err := client.server.SetServingType(tabletType, time.Time{}, true /* serving */, "" /* reason */)
 	return err
 }
 
