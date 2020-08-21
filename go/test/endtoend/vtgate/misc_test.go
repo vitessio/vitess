e7964aa6db5b4ed789421cfa27ac102bae575d0a
Andres Taylor <andres@planetscale.com>
2020-08-21 17:00:32 +0200

    Added failing endtoend test
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/test/endtoend/vtgate/misc_test.go
+++ go/test/endtoend/vtgate/misc_test.go
@@ -276,6 +276,16 @@ func TestXXHash(t *testing.T) {
 	assertMatches(t, conn, "select phone, keyspace_id from t7_xxhash_idx", `[]`)
 }
 
+func TestShowTablesWithWhereClause(t *testing.T) {
+	defer cluster.PanicHandler(t)
+	ctx := context.Background()
+	conn, err := mysql.Connect(ctx, &vtParams)
+	require.Nil(t, err)
+	defer conn.Close()
+
+	assertMatches(t, conn, "show tables from ks where Tables_in_ks='t5'", `[[VARCHAR("t5")]]`)
+}
+
 func assertMatches(t *testing.T, conn *mysql.Conn, query, expected string) {
 	t.Helper()
 	qr := exec(t, conn, query)
