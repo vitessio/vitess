3a563547a96d6ac607edcdd8317f95507325f5d4
Harshit Gangal <harshit@planetscale.com>
2020-08-25 12:54:57 +0530

    lookup vindex to lock the row on select
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/vt/vtgate/vindexes/lookup_internal.go
+++ go/vt/vtgate/vindexes/lookup_internal.go
@@ -61,7 +61,7 @@ func (lkp *lookupInternal) Init(lookupQueryParams map[string]string, autocommit,
 	// TODO @rafael: update sel and ver to support multi column vindexes. This will be done
 	// as part of face 2 of https://github.com/vitessio/vitess/issues/3481
 	// For now multi column behaves as a single column for Map and Verify operations
-	lkp.sel = fmt.Sprintf("select %s, %s from %s where %s in ::%s", lkp.FromColumns[0], lkp.To, lkp.Table, lkp.FromColumns[0], lkp.FromColumns[0])
+	lkp.sel = fmt.Sprintf("select %s, %s from %s where %s in ::%s for update", lkp.FromColumns[0], lkp.To, lkp.Table, lkp.FromColumns[0], lkp.FromColumns[0])
 	lkp.ver = fmt.Sprintf("select %s from %s where %s = :%s and %s = :%s", lkp.FromColumns[0], lkp.Table, lkp.FromColumns[0], lkp.FromColumns[0], lkp.To, lkp.To)
 	lkp.del = lkp.initDelStmt()
 	return nil
