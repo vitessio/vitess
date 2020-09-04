53f43fd23c35c9c53351334da511c511395e7b76
Hu Shuai <hus.fnst@cn.fujitsu.com>
2020-09-04 11:03:54 +0800

    Add unit test case to improve test coverage for go/sqltypes/named_result.go
    Signed-off-by: Hu Shuai <hus.fnst@cn.fujitsu.com>

--- go/sqltypes/named_result_test.go
+++ go/sqltypes/named_result_test.go
@@ -18,9 +18,9 @@ package sqltypes
 
 import (
 	"fmt"
-	"reflect"
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	querypb "vitess.io/vitess/go/vt/proto/query"
 )
 
@@ -32,40 +32,27 @@ func TestToNamedResult(t *testing.T) {
 		}, {
 			Name: "status",
 			Type: VarChar,
+		}, {
+			Name: "uid",
+			Type: Uint64,
 		}},
 		InsertID:     1,
 		RowsAffected: 2,
 		Rows: [][]Value{
-			{TestValue(Int64, "0"), TestValue(VarChar, "s0")},
-			{TestValue(Int64, "1"), TestValue(VarChar, "s1")},
-			{TestValue(Int64, "2"), TestValue(VarChar, "s2")},
+			{TestValue(Int64, "0"), TestValue(VarChar, "s0"), TestValue(Uint64, "0")},
+			{TestValue(Int64, "1"), TestValue(VarChar, "s1"), TestValue(Uint64, "1")},
+			{TestValue(Int64, "2"), TestValue(VarChar, "s2"), TestValue(Uint64, "2")},
 		},
 	}
 	named := in.Named()
 	for i := range in.Rows {
-		{
-			want := in.Rows[i][0]
-			got := named.Rows[i]["id"]
-			if !reflect.DeepEqual(want, got) {
-				t.Errorf("Named:%+v\n, want:%+v\n", got, want)
-			}
-			wantAs := int64(i)
-			gotAs := named.Rows[i].AsInt64("id", 0)
-			if gotAs != wantAs {
-				t.Errorf("Named:%+v\n, want:%+v\n", gotAs, wantAs)
-			}
-		}
-		{
-			want := in.Rows[i][1]
-			got := named.Rows[i]["status"]
-			if !reflect.DeepEqual(want, got) {
-				t.Errorf("Named:%+v\n, want:%+v\n", got, want)
-			}
-			wantAs := fmt.Sprintf("s%d", i)
-			gotAs := named.Rows[i].AsString("status", "notfound")
-			if gotAs != wantAs {
-				t.Errorf("Named:%+v\n, want:%+v\n", gotAs, wantAs)
-			}
-		}
+		require.Equal(t, in.Rows[i][0], named.Rows[i]["id"])
+		require.Equal(t, int64(i), named.Rows[i].AsInt64("id", 0))
+
+		require.Equal(t, in.Rows[i][1], named.Rows[i]["status"])
+		require.Equal(t, fmt.Sprintf("s%d", i), named.Rows[i].AsString("status", "notfound"))
+
+		require.Equal(t, in.Rows[i][2], named.Rows[i]["uid"])
+		require.Equal(t, uint64(i), named.Rows[i].AsUint64("uid", 0))
 	}
 }
