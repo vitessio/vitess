617cd6c60cad6c8726f8a17a4c979ee77a0271dd
Andres Taylor <andres@planetscale.com>
2020-09-14 16:48:05 +0200

    refactoring of expression rewriting
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/vt/sqlparser/expression_rewriting.go
+++ go/vt/sqlparser/expression_rewriting.go
@@ -33,15 +33,6 @@ func PrepareAST(in Statement, bindVars map[string]*querypb.BindVariable, prefix 
 	return RewriteAST(in)
 }
 
-// BindVarNeeds represents the bind vars that need to be provided as the result of expression rewriting.
-type BindVarNeeds struct {
-	NeedLastInsertID         bool
-	NeedDatabase             bool
-	NeedFoundRows            bool
-	NeedRowCount             bool
-	NeedUserDefinedVariables []string
-}
-
 // RewriteAST rewrites the whole AST, replacing function calls and adding column aliases to queries
 func RewriteAST(in Statement) (*RewriteASTResult, error) {
 	er := newExpressionRewriter()
@@ -56,21 +47,8 @@ func RewriteAST(in Statement) (*RewriteASTResult, error) {
 	}
 
 	r := &RewriteASTResult{
-		AST: out,
-	}
-	for k := range er.bindVars {
-		switch k {
-		case LastInsertIDName:
-			r.NeedLastInsertID = true
-		case DBVarName:
-			r.NeedDatabase = true
-		case FoundRowsName:
-			r.NeedFoundRows = true
-		case RowCountName:
-			r.NeedRowCount = true
-		default:
-			r.NeedUserDefinedVariables = append(r.NeedUserDefinedVariables, k)
-		}
+		AST:          out,
+		BindVarNeeds: er.bindVars,
 	}
 	return r, nil
 }
@@ -96,18 +74,18 @@ func shouldRewriteDatabaseFunc(in Statement) bool {
 
 // RewriteASTResult contains the rewritten ast and meta information about it
 type RewriteASTResult struct {
-	BindVarNeeds
+	*BindVarNeeds
 	AST Statement // The rewritten AST
 }
 
 type expressionRewriter struct {
-	bindVars                  map[string]struct{}
+	bindVars                  *BindVarNeeds
 	shouldRewriteDatabaseFunc bool
 	err                       error
 }
 
 func newExpressionRewriter() *expressionRewriter {
-	return &expressionRewriter{bindVars: make(map[string]struct{})}
+	return &expressionRewriter{bindVars: &BindVarNeeds{}}
 }
 
 const (
@@ -148,9 +126,7 @@ func (er *expressionRewriter) goingDown(cursor *Cursor) bool {
 				if inner.didAnythingChange() {
 					aliasedExpr.As = NewColIdent(buf.String())
 				}
-				for k := range inner.bindVars {
-					er.needBindVarFor(k)
-				}
+				er.bindVars.MergeWith(inner.bindVars)
 			}
 		}
 	case *FuncExpr:
@@ -159,7 +135,7 @@ func (er *expressionRewriter) goingDown(cursor *Cursor) bool {
 		if node.Name.at == SingleAt {
 			udv := strings.ToLower(node.Name.CompliantName())
 			cursor.Replace(bindVarExpression(UserDefinedVariableName + udv))
-			er.needBindVarFor(udv)
+			er.bindVars.AddUserDefVar(udv)
 		}
 	}
 	return true
@@ -173,7 +149,7 @@ func (er *expressionRewriter) funcRewrite(cursor *Cursor, node *FuncExpr) {
 			er.err = vterrors.New(vtrpc.Code_UNIMPLEMENTED, "Argument to LAST_INSERT_ID() not supported")
 		} else {
 			cursor.Replace(bindVarExpression(LastInsertIDName))
-			er.needBindVarFor(LastInsertIDName)
+			er.bindVars.AddFuncResult(LastInsertIDName)
 		}
 	// database() -> :__vtdbname
 	case er.shouldRewriteDatabaseFunc &&
@@ -183,7 +159,7 @@ func (er *expressionRewriter) funcRewrite(cursor *Cursor, node *FuncExpr) {
 			er.err = vterrors.Errorf(vtrpc.Code_INVALID_ARGUMENT, "Syntax error. %s() takes no arguments", node.Name.String())
 		} else {
 			cursor.Replace(bindVarExpression(DBVarName))
-			er.needBindVarFor(DBVarName)
+			er.bindVars.AddFuncResult(DBVarName)
 		}
 	// found_rows() -> :__vtfrows
 	case node.Name.EqualString("found_rows"):
@@ -191,7 +167,7 @@ func (er *expressionRewriter) funcRewrite(cursor *Cursor, node *FuncExpr) {
 			er.err = vterrors.New(vtrpc.Code_INVALID_ARGUMENT, "Arguments to FOUND_ROWS() not supported")
 		} else {
 			cursor.Replace(bindVarExpression(FoundRowsName))
-			er.needBindVarFor(FoundRowsName)
+			er.bindVars.AddFuncResult(FoundRowsName)
 		}
 	// row_count() -> :__vtrcount
 	case node.Name.EqualString("row_count"):
@@ -199,20 +175,15 @@ func (er *expressionRewriter) funcRewrite(cursor *Cursor, node *FuncExpr) {
 			er.err = vterrors.New(vtrpc.Code_INVALID_ARGUMENT, "Arguments to ROW_COUNT() not supported")
 		} else {
 			cursor.Replace(bindVarExpression(RowCountName))
-			er.needBindVarFor(RowCountName)
+			er.bindVars.AddFuncResult(RowCountName)
 		}
 	}
 }
 
-// instead of creating new objects, we'll reuse this one
-var token = struct{}{}
-
-func (er *expressionRewriter) needBindVarFor(name string) {
-	er.bindVars[name] = token
-}
-
 func (er *expressionRewriter) didAnythingChange() bool {
-	return len(er.bindVars) > 0
+	return len(er.bindVars.NeedUserDefinedVariables) > 0 ||
+		len(er.bindVars.needSystemVariable) > 0 ||
+		len(er.bindVars.needFunctionResult) > 0
 }
 
 func bindVarExpression(name string) Expr {
