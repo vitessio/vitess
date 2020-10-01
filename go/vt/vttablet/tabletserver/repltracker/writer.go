60a40823c24d4d368a7abf9dc004d8bbaf22f980
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2020-10-01 08:36:58 +0300

    enable/disable heartbeat; fix tests
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/vttablet/tabletserver/repltracker/writer.go
+++ go/vt/vttablet/tabletserver/repltracker/writer.go
@@ -108,7 +108,7 @@ func (w *heartbeatWriter) Open() {
 	log.Info("Hearbeat Writer: opening")
 
 	w.pool.Open(w.env.Config().DB.AppWithDB(), w.env.Config().DB.DbaWithDB(), w.env.Config().DB.AppDebugWithDB())
-	w.ticks.Start(w.writeHeartbeat)
+	w.enableWrites(true)
 	w.isOpen = true
 }
 
@@ -123,7 +123,7 @@ func (w *heartbeatWriter) Close() {
 		return
 	}
 
-	w.ticks.Stop()
+	w.enableWrites(false)
 	w.pool.Close()
 	w.isOpen = false
 	log.Info("Hearbeat Writer: closed")
@@ -178,4 +178,13 @@ func (w *heartbeatWriter) write() error {
 func (w *heartbeatWriter) recordError(err error) {
 	w.errorLog.Errorf("%v", err)
 	writeErrors.Add(1)
+}
+
+// enableWrites actives or deactives heartbeat writes
+func (w *heartbeatWriter) enableWrites(enable bool) {
+	if enable {
+		w.ticks.Start(w.writeHeartbeat)
+	} else {
+		w.ticks.Stop()
+	}
 }
