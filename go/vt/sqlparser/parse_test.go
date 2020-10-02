1b382efbd87bbe184c962c84e73dfc9877a4ee57
GuptaManan100 <manan@planetscale.com>
2020-10-02 16:43:52 +0530

    Added Parsing for Into outfile s3
    Signed-off-by: GuptaManan100 <manan@planetscale.com>

--- go/vt/sqlparser/parse_test.go
+++ go/vt/sqlparser/parse_test.go
@@ -2101,6 +2101,56 @@ func TestConvert(t *testing.T) {
 	}
 }
 
+func TestIntoOutfileS3(t *testing.T) {
+	validSQL := []struct {
+		input  string
+		output string
+	}{{
+		input:  "select * from t order by name limit 100 into outfile s3 'out_file_name'",
+		output: "select * from t order by name asc limit 100 into outfile s3 'out_file_name'",
+	}, {
+		input: "select * from (select * from t union select * from t2) as t3 where t3.name in (select col from t4) into outfile s3 'out_file_name'",
+	}, {
+		// Invalid queries but these are parsed and errors caught in planbuilder
+		input: "select * from t limit 100 into outfile s3 'out_file_name' union select * from t2",
+	}, {
+		input: "select * from (select * from t into outfile s3 'inner_outfile') as t2 into outfile s3 'out_file_name'",
+	}}
+
+	for _, tcase := range validSQL {
+		if tcase.output == "" {
+			tcase.output = tcase.input
+		}
+		tree, err := Parse(tcase.input)
+		if err != nil {
+			t.Errorf("input: %s, err: %v", tcase.input, err)
+			continue
+		}
+		out := String(tree)
+		if out != tcase.output {
+			t.Errorf("out: %s, want %s", out, tcase.output)
+		}
+	}
+
+	invalidSQL := []struct {
+		input  string
+		output string
+	}{{
+		input:  "select convert('abc' as date) from t",
+		output: "syntax error at position 24 near 'as'",
+	}, {
+		input:  "set transaction isolation level 12345",
+		output: "syntax error at position 38 near '12345'",
+	}}
+
+	for _, tcase := range invalidSQL {
+		_, err := Parse(tcase.input)
+		if err == nil || err.Error() != tcase.output {
+			t.Errorf("%s: %v, want %s", tcase.input, err, tcase.output)
+		}
+	}
+}
+
 func TestPositionedErr(t *testing.T) {
 	invalidSQL := []struct {
 		input  string
@@ -2451,7 +2501,7 @@ func TestCreateTable(t *testing.T) {
 			"	f1 float default 1.23,\n" +
 			"	s1 varchar default 'c',\n" +
 			"	s2 varchar default 'this is a string',\n" +
-			"	s3 varchar default null,\n" +
+			"	`s3` varchar default null,\n" +
 			"	s4 timestamp default current_timestamp(),\n" +
 			"	s5 bit(1) default B'0'\n" +
 			")",
