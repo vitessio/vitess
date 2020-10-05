1b2dc0f7d17a163902c2f36bd2c5e26ea26526c4
deepthi <deepthi@planetscale.com>
2020-10-05 12:35:16 -0700

    Add clusters_to_watch flag to orchestrator. Defaults to all clusters
    Signed-off-by: deepthi <deepthi@planetscale.com>

--- go/vt/orchestrator/db/generate_base.go
+++ go/vt/orchestrator/db/generate_base.go
@@ -856,6 +856,8 @@ var generateSQLBase = []string{
 			hostname varchar(128) CHARACTER SET ascii NOT NULL,
 			port smallint(5) unsigned NOT NULL,
 			cell varchar(128) CHARACTER SET ascii NOT NULL,
+			keyspace varchar(128) CHARACTER SET ascii NOT NULL,
+			shard varchar(128) CHARACTER SET ascii NOT NULL,
 			tablet_type smallint(5) NOT NULL,
 			master_timestamp timestamp NOT NULL,
 			info varchar(512) CHARACTER SET ascii NOT NULL,
@@ -864,5 +866,8 @@ var generateSQLBase = []string{
 	`,
 	`
 		CREATE INDEX cell_idx_vitess_tablet ON vitess_tablet (cell)
+	`,
+	`
+		CREATE INDEX ks_idx_vitess_tablet ON vitess_tablet (keyspace, shard)
 	`,
 }
