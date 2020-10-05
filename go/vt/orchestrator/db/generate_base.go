326bf2d0ccd6a3b83ddbbbc6ffc7ab2597239b97
deepthi <deepthi@planetscale.com>
2020-10-05 13:17:39 -0700

    column order in vitess_tablet should be keyspace,shard,cell
    Signed-off-by: deepthi <deepthi@planetscale.com>

--- go/vt/orchestrator/db/generate_base.go
+++ go/vt/orchestrator/db/generate_base.go
@@ -855,9 +855,9 @@ var generateSQLBase = []string{
 		CREATE TABLE IF NOT EXISTS vitess_tablet (
 			hostname varchar(128) CHARACTER SET ascii NOT NULL,
 			port smallint(5) unsigned NOT NULL,
-			cell varchar(128) CHARACTER SET ascii NOT NULL,
 			keyspace varchar(128) CHARACTER SET ascii NOT NULL,
 			shard varchar(128) CHARACTER SET ascii NOT NULL,
+			cell varchar(128) CHARACTER SET ascii NOT NULL,
 			tablet_type smallint(5) NOT NULL,
 			master_timestamp timestamp NOT NULL,
 			info varchar(512) CHARACTER SET ascii NOT NULL,
