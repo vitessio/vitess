7de8c8731d85ef9d24257cdcbe528336b02f4351
Rohit Nayak <rohit@planetscale.com>
2020-10-06 12:54:36 +0200

    Address review comments
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>

--- go/vt/wrangler/materializer_env_test.go
+++ go/vt/wrangler/materializer_env_test.go
@@ -267,7 +267,7 @@ func (tmc *testMaterializerTMClient) verifyQueries(t *testing.T) {
 			for _, qr := range qrs {
 				list = append(list, qr.query)
 			}
-			t.Errorf("tablet %v: found queries that were expected but never got execed by the test: %v", tabletID, list)
+			t.Errorf("tablet %v: found queries that were expected but never got executed by the test: %v", tabletID, list)
 		}
 	}
 }
