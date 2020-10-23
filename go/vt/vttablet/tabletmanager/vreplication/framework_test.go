636c57c689a7a2ab5af5462825ca2a39e0d23589
Rohit Nayak <rohit@planetscale.com>
2020-10-23 07:47:58 +0200

    Update tests
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>

--- go/vt/vttablet/tabletmanager/vreplication/framework_test.go
+++ go/vt/vttablet/tabletmanager/vreplication/framework_test.go
@@ -409,11 +409,9 @@ func (dbc *realDBClient) ExecuteFetch(query string, maxrows int) (*sqltypes.Resu
 
 func expectDeleteQueries(t *testing.T) {
 	t.Helper()
-	expectDBClientQueries(t, []string{
-		"begin",
+	expectNontxQueries(t, []string{
 		"/delete from _vt.vreplication",
 		"/delete from _vt.copy_state",
-		"commit",
 	})
 }
 
@@ -465,7 +463,7 @@ func expectDBClientQueries(t *testing.T, queries []string) {
 			continue
 		}
 		var got string
-		heartbeatRe := regexp.MustCompile(`update _vt.vreplication set time_updated=\d+, transaction_timestamp=\d+ where id=\d+`)
+		heartbeatRe := regexp.MustCompile(`update _vt.vreplication set time_updated=\d+ where id=\d+`)
 	retry:
 		select {
 		case got = <-globalDBQueries:
@@ -508,7 +506,7 @@ func expectDBClientQueries(t *testing.T, queries []string) {
 func expectNontxQueries(t *testing.T, queries []string) {
 	t.Helper()
 	failed := false
-	heartbeatRe := regexp.MustCompile(`update _vt.vreplication set time_updated=\d+, transaction_timestamp=\d+ where id=\d+`)
+	heartbeatRe := regexp.MustCompile(`update _vt.vreplication set time_updated=\d+ where id=\d+`)
 	for i, query := range queries {
 		if failed {
 			t.Errorf("no query received, expecting %s", query)
