aa3e05dbf2d667efa52cd3dcf3141b5cfc7b2399
Michael Demmer <mdemmer@slack-corp.com>
2020-10-22 17:40:35 -0700

    use ephemeral buffer when reading rows
    Signed-off-by: Michael Demmer <mdemmer@slack-corp.com>

--- go/mysql/query.go
+++ go/mysql/query.go
@@ -406,7 +406,7 @@ func (c *Conn) ReadQueryResult(maxrows int, wantfields bool) (*sqltypes.Result, 
 
 	// read each row until EOF or OK packet.
 	for {
-		data, err := c.ReadPacket()
+		data, err := c.readEphemeralPacket()
 		if err != nil {
 			return nil, false, 0, err
 		}
@@ -415,6 +415,8 @@ func (c *Conn) ReadQueryResult(maxrows int, wantfields bool) (*sqltypes.Result, 
 		// https://dev.mysql.com/doc/internals/en/packet-EOF_Packet.html
 		// It will be OK Packet with EOF Header. This needs to change in the code here.
 		if isEOFPacket(data) {
+			defer c.recycleReadPacket()
+
 			// Strip the partial Fields before returning.
 			if !wantfields {
 				result.Fields = nil
@@ -440,12 +442,14 @@ func (c *Conn) ReadQueryResult(maxrows int, wantfields bool) (*sqltypes.Result, 
 			return result, more, warnings, nil
 
 		} else if isErrorPacket(data) {
+			defer c.recycleReadPacket()
 			// Error packet.
 			return nil, false, 0, ParseErrorPacket(data)
 		}
 
 		// Check we're not over the limit before we add more.
 		if len(result.Rows) == maxrows {
+			c.recycleReadPacket()
 			if err := c.drainResults(); err != nil {
 				return nil, false, 0, err
 			}
@@ -455,9 +459,11 @@ func (c *Conn) ReadQueryResult(maxrows int, wantfields bool) (*sqltypes.Result, 
 		// Regular row.
 		row, err := c.parseRow(data, result.Fields)
 		if err != nil {
+			c.recycleReadPacket()
 			return nil, false, 0, err
 		}
 		result.Rows = append(result.Rows, row)
+		c.recycleReadPacket()
 	}
 }
 
