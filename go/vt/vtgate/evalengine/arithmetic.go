1ab8e7ae92d836866ca0fc427f85d6e7045c188f
Rohit Nayak <rohit@planetscale.com>
2020-11-13 14:48:06 +0100

    MoveTables: validations, make json columns vdiff-able
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>

--- go/vt/vtgate/evalengine/arithmetic.go
+++ go/vt/vtgate/evalengine/arithmetic.go
@@ -209,7 +209,7 @@ func isByteComparable(v sqltypes.Value) bool {
 		return true
 	}
 	switch v.Type() {
-	case sqltypes.Timestamp, sqltypes.Date, sqltypes.Time, sqltypes.Datetime, sqltypes.Enum, sqltypes.Set:
+	case sqltypes.Timestamp, sqltypes.Date, sqltypes.Time, sqltypes.Datetime, sqltypes.Enum, sqltypes.Set, sqltypes.TypeJSON:
 		return true
 	}
 	return false
