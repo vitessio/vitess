d1949e61c1f7f85ca4663e438b7c6042fbb3f658
Marla <5854253+hallaroo@users.noreply.github.com>
2020-11-18 14:09:46 -0700

    Add files via upload

---
+++ examples/local/external_db_rds.sh
@@ -0,0 +1,56 @@
+#!/bin/bash    
+    
+# Copyright 2019 The Vitess Authors.    
+#     
+# Licensed under the Apache License, Version 2.0 (the "License");    
+# you may not use this file except in compliance with the License.    
+# You may obtain a copy of the License at    
+#     
+#     http://www.apache.org/licenses/LICENSE-2.0    
+#     
+# Unless required by applicable law or agreed to in writing, software    
+# distributed under the License is distributed on an "AS IS" BASIS,    
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.    
+# See the License for the specific language governing permissions and    
+# limitations under the License.    
+    
+# this script brings up zookeeper and all the vitess components    
+# required for a single shard deployment.    
+
+uid=500    
+printf -v tablet_dir 'vt_%010d' $uid    
+echo calling mkdir ${VTDATAROOT}/${tablet_dir}    
+mkdir -p ${VTDATAROOT}/${tablet_dir}    
+
+source ./env.sh    
+    
+# start topo server    
+if [ "${TOPO}" = "zk2" ]; then    
+    CELL=zone1 ./scripts/zk-up.sh    
+elif [ "${TOPO}" = "k8s" ]; then    
+    CELL=zone1 ./scripts/k3s-up.sh    
+else    
+ CELL=zone1 ./scripts/etcd-up.sh    
+fi    
+    
+# start vtctld    
+CELL=zone1 ./scripts/vtctld-up.sh    
+ 
+# scripts/vttablet-external-up.sh
+echo running ./scripts/vttablet-external-rds-up.sh
+CELL=zone1 KEYSPACE=rdstest  SHARD=0    TABLET_UID=$uid ./scripts/vttablet-external-rds-up.sh    
+
+# reparent for the master
+sleep 2    
+vtctlclient -server localhost:15999 TabletExternallyReparented zone1-$uid
+
+    
+echo creating vschema
+# create the vschema    
+vtctlclient -server localhost:15999 ApplyVSchema -vschema_file vschema_commerce_initial.json rdstest    
+    
+echo starting vtgate
+# start vtgate    
+CELL=zone1 ./scripts/vtgate-up.sh    
+
+
---
+++ examples/local/init_vt_external_db_cloudsql.sql
@@ -0,0 +1,82 @@
+#SET sql_log_bin = 0;
+
+###############################################################################
+# Vitess defaults
+###############################################################################
+
+# application database.
+CREATE DATABASE IF NOT EXISTS cloudsqltest1;
+
+# Vitess-internal database.
+CREATE DATABASE IF NOT EXISTS _vt;
+# Note that definitions of local_metadata and shard_metadata should be the same
+# as in production which is defined in go/vt/mysqlctl/metadata_tables.go.
+CREATE TABLE IF NOT EXISTS _vt.local_metadata (
+  name VARCHAR(255) NOT NULL,
+  value VARCHAR(255) NOT NULL,
+  db_name VARBINARY(255) NOT NULL,
+  PRIMARY KEY (db_name, name)
+  ) ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS _vt.shard_metadata (
+  name VARCHAR(255) NOT NULL,
+  value MEDIUMBLOB NOT NULL,
+  db_name VARBINARY(255) NOT NULL,
+  PRIMARY KEY (db_name, name)
+  ) ENGINE=InnoDB;
+
+# Admin user with all privileges.
+CREATE USER 'vt_dba'@'%';
+GRANT ALL ON _vt.* TO 'vt_dba'@'%';
+GRANT ALL ON cloudsqltest1.* TO 'vt_dba'@'%';
+GRANT GRANT OPTION ON _vt.* TO 'vt_dba'@'%';
+GRANT GRANT OPTION ON cloudsqltest1.* TO 'vt_dba'@'%';
+
+# User for app traffic, with global read-write access.
+CREATE USER 'vt_app'@'%';
+GRANT ALL ON _vt.* TO 'vt_app'@'%';
+GRANT ALL ON cloudsqltest1.* TO 'vt_app'@'%';
+
+
+# User for app debug traffic, with global read access.
+CREATE USER 'vt_appdebug'@'%';
+GRANT SELECT, SHOW DATABASES, PROCESS ON *.* TO 'vt_appdebug'@'%';
+
+# User for administrative operations that need to be executed as non-SUPER.
+# Same permissions as vt_app here.
+CREATE USER 'vt_allprivs'@'%';
+GRANT ALL ON _vt.* TO 'vt_allprivs'@'%';
+GRANT ALL ON cloudsqltest1.* TO 'vt_allprivs'@'%';
+
+
+# User for slave replication connections.
+# ...NOT SURE THIS IS NEEDED OR EVEN WORKS...
+CREATE USER 'vt_repl'@'%';
+GRANT ALL ON cloudsqltest1.* TO 'vt_repl'@'%';
+
+
+# User for Vitess filtered replication (binlog player).
+# Same permissions as vt_app.
+CREATE USER 'vt_filtered'@'%';
+GRANT ALL ON _vt.* TO 'vt_filtered'@'%';
+GRANT ALL ON cloudsqltest1.* TO 'vt_filtered'@'%';
+
+# User for general MySQL monitoring.
+CREATE USER 'vt_monitoring'@'%';
+GRANT ALL ON _vt.* TO 'vt_monitoring'@'%';
+GRANT ALL ON cloudsqltest1.* TO 'vt_monitoring'@'%';
+GRANT SELECT, UPDATE, DELETE, DROP
+  ON performance_schema.* TO 'vt_monitoring'@'%';
+
+# Skipping Orchestrator
+# User for Orchestrator (https://github.com/github/orchestrator).
+#CREATE USER 'orc_client_user'@'%' IDENTIFIED BY 'orc_client_user_password';
+#GRANT SUPER, PROCESS, REPLICATION SLAVE, RELOAD
+#  ON *.* TO 'orc_client_user'@'%';
+#GRANT SELECT
+#  ON _vt.* TO 'orc_client_user'@'%';
+
+FLUSH PRIVILEGES;
+
+RESET SLAVE ALL;
+# RESET MASTER;
+
