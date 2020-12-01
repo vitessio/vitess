246aedad5f1b9157404f2ebc13a3bf900930373a
liortamary <lior.tamary@houzz.com>
2020-12-01 22:20:11 +0200

    add unit tests for s3 server-side encryption with customer provided key
    Signed-off-by: liortamary <lior.tamary@houzz.com>

--- go/vt/mysqlctl/s3backupstorage/s3_test.go
+++ go/vt/mysqlctl/s3backupstorage/s3_test.go
@@ -1,8 +1,13 @@
 package s3backupstorage
 
 import (
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/base64"
 	"errors"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -40,4 +45,98 @@ func TestAddFileError(t *testing.T) {
 	bh.waitGroup.Wait() // wait for the goroutine to finish, at which point it should have recorded an error
 
 	require.Equal(t, bh.HasErrors(), true, "AddFile() expected bh to record async error but did not")
+}
+
+func TestNoSSECustomer(t *testing.T) {
+	sse := S3SSECustomer{}
+	err := sse.Open()
+	require.NoErrorf(t, err, "Open() expected to succeed")
+
+	assert.Nil(t, sse.alg, "S3SSECustomer.alg expected to be nil")
+	assert.Nil(t, sse.key, "S3SSECustomer.key expected to be nil")
+	assert.Nil(t, sse.md5, "S3SSECustomer.md5 expected to be nil")
+
+	sse.Close()
+	require.NoErrorf(t, err, "Close() expected to succeed")
+}
+
+func TestSSECustomerFileNotFound(t *testing.T) {
+	tempFile, err := ioutil.TempFile("", "filename")
+	require.NoErrorf(t, err, "TempFile() expected to succeed")
+	defer os.Remove(tempFile.Name())
+
+	err = tempFile.Close()
+	require.NoErrorf(t, err, "Close() expected to succeed")
+
+	err = os.Remove(tempFile.Name())
+	require.NoErrorf(t, err, "Remove() expected to succeed")
+
+	sseCustomerKeyFile = aws.String(tempFile.Name())
+	sse := S3SSECustomer{}
+	err = sse.Open()
+	require.Errorf(t, err, "Open() expected to fail")
+}
+
+func TestSSECustomerFileBinaryKey(t *testing.T) {
+	tempFile, err := ioutil.TempFile("", "filename")
+	require.NoErrorf(t, err, "TempFile() expected to succeed")
+	defer os.Remove(tempFile.Name())
+
+	randomKey := make([]byte, 32)
+	_, err = rand.Read(randomKey)
+	require.NoErrorf(t, err, "Read() expected to succeed")
+	_, err = tempFile.Write(randomKey)
+	require.NoErrorf(t, err, "Write() expected to succeed")
+	err = tempFile.Close()
+	require.NoErrorf(t, err, "Close() expected to succeed")
+
+	sseCustomerKeyFile = aws.String(tempFile.Name())
+	sse := S3SSECustomer{}
+	err = sse.Open()
+	require.NoErrorf(t, err, "Open() expected to succeed")
+
+	assert.Equal(t, aws.String("AES256"), sse.alg, "S3SSECustomer.alg expected to be AES256")
+	assert.Equal(t, aws.String(string(randomKey)), sse.key, "S3SSECustomer.key expected to be equal to the generated randomKey")
+	md5Hash := md5.Sum(randomKey)
+	assert.Equal(t, aws.String(base64.StdEncoding.EncodeToString(md5Hash[:])), sse.md5, "S3SSECustomer.md5 expected to be equal to the md5 hash of the generated randomKey")
+
+	sse.Close()
+	require.NoErrorf(t, err, "Close() expected to succeed")
+
+	assert.Nil(t, sse.alg, "S3SSECustomer.alg expected to be nil")
+	assert.Nil(t, sse.key, "S3SSECustomer.key expected to be nil")
+	assert.Nil(t, sse.md5, "S3SSECustomer.md5 expected to be nil")
+}
+
+func TestSSECustomerFileBase64Key(t *testing.T) {
+	tempFile, err := ioutil.TempFile("", "filename")
+	require.NoErrorf(t, err, "TempFile() expected to succeed")
+	defer os.Remove(tempFile.Name())
+
+	randomKey := make([]byte, 32)
+	_, err = rand.Read(randomKey)
+	require.NoErrorf(t, err, "Read() expected to succeed")
+
+	base64Key := base64.StdEncoding.EncodeToString(randomKey[:])
+	_, err = tempFile.WriteString(base64Key)
+	require.NoErrorf(t, err, "WriteString() expected to succeed")
+	err = tempFile.Close()
+	require.NoErrorf(t, err, "Close() expected to succeed")
+
+	sseCustomerKeyFile = aws.String(tempFile.Name())
+	sse := S3SSECustomer{}
+	err = sse.Open()
+	require.NoErrorf(t, err, "Open() expected to succeed")
+
+	assert.Equal(t, aws.String("AES256"), sse.alg, "S3SSECustomer.alg expected to be AES256")
+	assert.Equal(t, aws.String(string(randomKey)), sse.key, "S3SSECustomer.key expected to be equal to the generated randomKey")
+	md5Hash := md5.Sum(randomKey)
+	assert.Equal(t, aws.String(base64.StdEncoding.EncodeToString(md5Hash[:])), sse.md5, "S3SSECustomer.md5 expected to be equal to the md5 hash of the generated randomKey")
+
+	sse.Close()
+	require.NoErrorf(t, err, "Close() expected to succeed")
+
+	assert.Nil(t, sse.alg, "S3SSECustomer.alg expected to be nil")
+	assert.Nil(t, sse.key, "S3SSECustomer.key expected to be nil")
+	assert.Nil(t, sse.md5, "S3SSECustomer.md5 expected to be nil")
 }
