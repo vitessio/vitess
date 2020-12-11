0ba1b27fc1b0b4bb4e17b98fc37379d7c18096dc
Jacques Grove <aquarapid@gmail.com>
2020-12-10 21:23:12 -0800

    Initial support for storing db credentials in Vault.
    Signed-off-by: Jacques Grove <aquarapid@gmail.com>

--- go/cmd/vttablet/vttablet.go
+++ go/cmd/vttablet/vttablet.go
@@ -72,7 +72,7 @@ func main() {
 		log.Exitf("failed to parse -tablet-path: %v", err)
 	}
 
-	// config and mycnf intializations are intertwined.
+	// config and mycnf initializations are intertwined.
 	config, mycnf := initConfig(tabletAlias)
 
 	ts := topo.Open()
@@ -105,7 +105,7 @@ func main() {
 		VREngine:            vreplication.NewEngine(config, ts, tabletAlias.Cell, mysqld),
 	}
 	if err := tm.Start(tablet, config.Healthcheck.IntervalSeconds.Get()); err != nil {
-		log.Exitf("failed to parse -tablet-path: %v", err)
+		log.Exitf("failed to parse -tablet-path or initialize DB credentials: %v", err)
 	}
 	servenv.OnClose(func() {
 		// Close the tm so that our topo entry gets pruned properly and any
