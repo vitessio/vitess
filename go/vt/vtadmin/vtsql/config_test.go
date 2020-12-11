3cfc00f162eb438afb55899bbd1f55fedff81a51
Andrew Mason <amason@slack-corp.com>
2020-12-11 17:13:19 -0500

    Add a top-level function for parsing without an existing config
    Signed-off-by: Andrew Mason <amason@slack-corp.com>

--- go/vt/vtadmin/vtsql/config_test.go
+++ go/vt/vtadmin/vtsql/config_test.go
@@ -70,4 +70,57 @@ func TestConfigParse(t *testing.T) {
 		assert.Equal(t, expectedTags, cfg.DiscoveryTags)
 		assert.Equal(t, expectedCreds, cfg.Credentials)
 	})
+
+	t.Run("", func(t *testing.T) {
+		f, err := ioutil.TempFile("", "vtsql-config-test-testcluster-*") // testcluster is going to appear in the template
+		require.NoError(t, err)
+
+		_, err = f.Write([]byte(`{
+	"Username": "vtadmin",
+	"Password": "hunter2"
+}`))
+		require.NoError(t, err)
+
+		path := f.Name()
+		defer os.Remove(path)
+		f.Close()
+
+		dir := filepath.Dir(path)
+		baseParts := strings.Split(filepath.Base(path), "-")
+		tmplParts := append(baseParts[:3], "{{ .ClusterName }}", baseParts[4])
+
+		credsTmplStr := filepath.Join(dir, strings.Join(tmplParts, "-"))
+
+		args := []string{
+			"--discovery-tags=a:1,b:2",
+			"--effective-user=vt_appdebug",
+			"--discovery-tags=c:3",
+			fmt.Sprintf("--credentials-path-tmpl=%s", credsTmplStr),
+		}
+
+		expectedCreds := &StaticAuthCredentials{
+			EffectiveUser: "vt_appdebug",
+			StaticAuthClientCreds: &grpcclient.StaticAuthClientCreds{
+				Username: "vtadmin",
+				Password: "hunter2",
+			},
+		}
+		expectedTags := []string{
+			"a:1",
+			"b:2",
+			"c:3",
+		}
+
+		expected := &Config{
+			ClusterID:       "cid",
+			ClusterName:     "testcluster",
+			DiscoveryTags:   expectedTags,
+			Credentials:     expectedCreds,
+			CredentialsPath: path,
+		}
+
+		cfg, err := Parse("cid", "testcluster", args)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, cfg)
+	})
 }
