0c8c74670bbb6aaf6364c7d9317275d275f9cd89
D.K <dan.kozlowski@gmail.com>
2020-12-17 09:43:01 -0800

    Trying Closest version from Chrome for driver
    Signed-off-by: D.K <dan.kozlowski@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -276,7 +276,7 @@ install_all() {
 
   # chromedriver
   if [ "$BUILD_CHROME" == 1 ] ; then
-    install_dep "chromedriver" "83.0.4103.116" "$VTROOT/dist/chromedriver" install_chromedriver
+    install_dep "chromedriver" "83.0.4103.14" "$VTROOT/dist/chromedriver" install_chromedriver
   fi
 
   echo
