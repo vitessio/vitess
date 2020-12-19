5c26b4394e96dfc049b1f840458bf7e3a7295c00
Sugu Sougoumarane <ssougou@gmail.com>
2020-12-18 21:11:23 -0800

    examples: update demo
    Change the demo to be more specific about what it demonstrates
    and also for easier documentation, which will follow.
    
    Signed-off-by: Sugu Sougoumarane <ssougou@gmail.com>

--- examples/demo/schema/user/user_schema.sql
+++
@@ -1,6 +0,0 @@
-create table user(user_id bigint, name varchar(128), primary key(user_id));
-create table user_extra(user_id bigint, extra varchar(128), primary key(user_id));
-create table music(user_id bigint, music_id bigint, primary key(user_id, music_id));
-create table music_extra(music_id bigint, keyspace_id bigint unsigned, primary key(music_id));
-create table name_info(name varchar(128), info varchar(128), primary key(name));
-create table music_keyspace_idx(music_id bigint not null auto_increment, keyspace_id binary(8), primary key(music_id));
