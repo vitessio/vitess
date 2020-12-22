013d286e552df9c99fe28eb38e2453c65cb8bbdc
Sara Bee <855595+doeg@users.noreply.github.com>
2020-12-22 15:46:57 -0500

    Add a very (veryyy) simple TabletList component
    Signed-off-by: Sara Bee <855595+doeg@users.noreply.github.com>

--- web/vtadmin/src/components/App.tsx
+++ web/vtadmin/src/components/App.tsx
@@ -13,15 +13,89 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import * as React from 'react';
 
 import style from './App.module.scss';
 import logo from '../img/vitess-icon-color.svg';
+import { TabletList } from './TabletList';
+import { vtadmin as pb, topodata } from '../proto/vtadmin';
 
 export const App = () => {
     return (
         <div className={style.container}>
             <img src={logo} alt="logo" height={40} />
             <h1>VTAdmin</h1>
+            <TabletList tablets={fakeTablets} />
         </div>
     );
 };
+
+// Add some fake data solely for this super duper simple hello world example.
+// When we do this for reals, we'll be fetching cluster JSON over HTTP
+// from vtadmin-api and casting/validating the responses to Cluster objects.
+// This requires merging https://github.com/vitessio/vitess/pull/7187 first.
+//
+// Long-term, we can use grpc in the browser with a library like https://github.com/grpc/grpc-web,
+// which will obviate the casting/validation steps, too.
+const fakeTablets = [
+    {
+        cluster: { id: 'prod', name: 'prod' },
+        tablet: {
+            hostname: 'tablet-prod-commerce-00-00-abcd',
+            keyspace: 'commerce',
+            shard: '-',
+            type: topodata.TabletType.MASTER,
+        },
+        state: pb.Tablet.ServingState.SERVING,
+    },
+    {
+        cluster: { id: 'prod', name: 'prod' },
+        tablet: {
+            hostname: 'tablet-prod-commerce-00-00-efgh',
+            keyspace: 'commerce',
+            shard: '-',
+            type: topodata.TabletType.REPLICA,
+        },
+        state: pb.Tablet.ServingState.SERVING,
+    },
+    {
+        cluster: { id: 'prod', name: 'prod' },
+        tablet: {
+            hostname: 'tablet-prod-commerce-00-00-ijkl',
+            keyspace: 'commerce',
+            shard: '-',
+            type: topodata.TabletType.REPLICA,
+        },
+        state: pb.Tablet.ServingState.SERVING,
+    },
+    {
+        cluster: { id: 'dev', name: 'dev' },
+        tablet: {
+            hostname: 'tablet-prod-commerce-00-00-mnop',
+            keyspace: 'commerce',
+            shard: '-',
+            type: topodata.TabletType.MASTER,
+        },
+        state: pb.Tablet.ServingState.SERVING,
+    },
+    {
+        cluster: { id: 'dev', name: 'dev' },
+        tablet: {
+            hostname: 'tablet-prod-commerce-00-00-qrst',
+            keyspace: 'commerce',
+            shard: '-',
+            type: topodata.TabletType.REPLICA,
+        },
+        state: pb.Tablet.ServingState.SERVING,
+    },
+    {
+        cluster: { id: 'dev', name: 'dev' },
+        tablet: {
+            hostname: 'tablet-prod-commerce-00-00-uvwx',
+            keyspace: 'commerce',
+            shard: '-',
+            type: topodata.TabletType.REPLICA,
+        },
+        state: pb.Tablet.ServingState.SERVING,
+    },
+].map((opts) => pb.Tablet.create(opts));
