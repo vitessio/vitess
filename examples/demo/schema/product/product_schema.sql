a8d14f6a0b0ba3f04398a11a49c707330cd8050a
Sugu Sougoumarane <ssougou@gmail.com>
2020-12-25 20:32:13 -0800

    examples: update demo to match guide
    Signed-off-by: Sugu Sougoumarane <ssougou@gmail.com>

--- examples/demo/schema/product/product_schema.sql
+++ examples/demo/schema/product/product_schema.sql
@@ -2,7 +2,7 @@ create table product(product_id bigint auto_increment, pname varchar(128), prima
 create table customer_seq(id bigint, next_id bigint, cache bigint, primary key(id)) comment 'vitess_sequence';
 insert into customer_seq(id, next_id, cache) values(0, 1, 3);
 create table corder_seq(id bigint, next_id bigint, cache bigint, primary key(id)) comment 'vitess_sequence';
-insert into corder_seq(id, next_id, cache) values(0, 1, 2);
+insert into corder_seq(id, next_id, cache) values(0, 1, 3);
 create table corder_event_seq(id bigint, next_id bigint, cache bigint, primary key(id)) comment 'vitess_sequence';
-insert into corder_event_seq(id, next_id, cache) values(0, 1, 2);
+insert into corder_event_seq(id, next_id, cache) values(0, 1, 3);
 create table corder_keyspace_idx(corder_id bigint not null auto_increment, keyspace_id varbinary(10), primary key(corder_id));
