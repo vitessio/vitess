c2db46d4fec587fbae031a8e87b7881eee079741
Sara Bee <855595+doeg@users.noreply.github.com>
2021-01-03 13:43:17 -0500

    Add a bunch of commentary about MSW in http.test.ts
    Signed-off-by: Sara Bee <855595+doeg@users.noreply.github.com>

--- web/vtadmin/src/api/http.test.ts
+++ web/vtadmin/src/api/http.test.ts
@@ -19,9 +19,27 @@ import { setupServer } from 'msw/node';
 import * as api from './http';
 import { vtadmin as pb } from '../proto/vtadmin';
 
-const SERVER_ADDRESS = '';
-process.env.REACT_APP_VTADMIN_API_ADDRESS = SERVER_ADDRESS;
-
+// This test suite uses Mock Service Workers (https://github.com/mswjs/msw)
+// to mock HTTP responses from vtadmin-api.
+//
+// MSW lets us intercept requests at the network level. This decouples the tests from
+// whatever particular HTTP fetcher interface we are using, and obviates the need
+// to mock `fetch` directly (by using a library like jest-fetch-mock, for example).
+//
+// MSW gives us full control over the response, including edge cases like errors,
+// malformed payloads, and timeouts.
+//
+// The big downside to mocking or "faking" APIs like vtadmin is that
+// we end up re-implementing some (or all) of vtadmin-api in our test environment.
+// It is, unfortunately, impossible to completely avoid this kind of duplication
+// unless we solely use e2e tests (which have their own trade-offs).
+//
+// That said, our use of protobufjs to validate and strongly type HTTP responses
+// means our fake is more robust than it would be otherwise. Since we are using
+// the exact same protos in our fake as in our real vtadmin-api server, we're guaranteed
+// to have type parity.
+//
+process.env.REACT_APP_VTADMIN_API_ADDRESS = '';
 const server = setupServer();
 
 const mockServerJson = (endpoint: string, json: object) => {
