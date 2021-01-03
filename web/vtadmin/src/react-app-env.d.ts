1f8f66190bdfa0d3e93d6769aca044d06d0632d5
Sara Bee <855595+doeg@users.noreply.github.com>
2021-01-03 11:23:48 -0500

    Add env variable + some modest documentation
    Signed-off-by: Sara Bee <855595+doeg@users.noreply.github.com>

--- web/vtadmin/src/react-app-env.d.ts
+++ web/vtadmin/src/react-app-env.d.ts
@@ -3,6 +3,7 @@ declare namespace NodeJS {
     interface ProcessEnv {
         NODE_ENV: 'development' | 'production' | 'test';
         PUBLIC_URL: string;
+        REACT_APP_VTADMIN_API_ADDRESS: string;
     }
 }
 
