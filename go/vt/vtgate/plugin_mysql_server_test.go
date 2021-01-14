600b316c9723414c5b598a61cf4254d71daa6ac3
Akilan <me@akilan.io>
2021-01-14 15:26:04 +0400

    fixed unit tests
    Signed-off-by: Akilan <me@akilan.io>

--- go/vt/vtgate/plugin_mysql_server_test.go
+++ go/vt/vtgate/plugin_mysql_server_test.go
@@ -234,7 +234,7 @@ func newTestAuthServerStatic() *mysql.AuthServerStatic {
 func TestDefaultWorkloadEmpty(t *testing.T) {
 	vh := &vtgateHandler{}
 	sess := vh.session(&mysql.Conn{})
-	if sess.Options.Workload != querypb.ExecuteOptions_UNSPECIFIED {
+	if sess.Options.Workload != querypb.ExecuteOptions_OLTP {
 		t.Fatalf("Expected default workload UNSPECIFIED")
 	}
 }
