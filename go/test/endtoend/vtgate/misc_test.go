f2e8dc055f00e0d82a47b122d17563e2957029ac
Akilan <me@akilan.io>
2021-01-15 22:30:29 +0400

    changed varchar to varbinary in end to end test
    Signed-off-by: Akilan <me@akilan.io>

--- go/test/endtoend/vtgate/misc_test.go
+++ go/test/endtoend/vtgate/misc_test.go
@@ -404,15 +404,15 @@ func TestSwitchBetweenOlapAndOltp(t *testing.T) {
 	require.NoError(t, err)
 	defer conn.Close()
 
-	assertMatches(t, conn, "select @@workload", `[[VARCHAR("OLTP")]]`)
+	assertMatches(t, conn, "select @@workload", `[[VARBINARY("OLTP")]]`)
 
 	exec(t, conn, "set workload='olap'")
 
-	assertMatches(t, conn, "select @@workload", `[[VARCHAR("OLAP")]]`)
+	assertMatches(t, conn, "select @@workload", `[[VARBINARY("OLAP")]]`)
 
 	exec(t, conn, "set workload='oltp'")
 
-	assertMatches(t, conn, "select @@workload", `[[VARCHAR("OLTP")]]`)
+	assertMatches(t, conn, "select @@workload", `[[VARBINARY("OLTP")]]`)
 }
 
 func TestFoundRowsOnDualQueries(t *testing.T) {
