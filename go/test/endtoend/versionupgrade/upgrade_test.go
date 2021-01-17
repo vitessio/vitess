38546d67f1f0ab86c2ebf4c2bbca26e994ba9d09
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2021-01-17 11:43:20 +0200

    adding debug info
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/test/endtoend/versionupgrade/upgrade_test.go
+++ go/test/endtoend/versionupgrade/upgrade_test.go
@@ -41,6 +41,7 @@ import (
 	"testing"
 
 	"vitess.io/vitess/go/mysql"
+	"vitess.io/vitess/go/vt/log"
 
 	"vitess.io/vitess/go/test/endtoend/cluster"
 
@@ -151,6 +152,9 @@ func TestDeploySchema(t *testing.T) {
 		{
 			sqlQuery := fmt.Sprintf(createTable, tableName)
 			err := clusterInstance.VtctlclientProcess.ApplySchema(keyspaceName, sqlQuery)
+			if err != nil {
+				log.Infof("ERROR: %+v", err)
+			}
 			require.Nil(t, err)
 		}
 		for i := range clusterInstance.Keyspaces[0].Shards {
