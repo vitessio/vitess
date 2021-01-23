d94dcaef4fc68c246b08ed237662ba2592deb764
Andrew Mason <amason@slack-corp.com>
2021-01-23 14:47:30 -0500

    Add `GetVSchema` rpc and run generators
    Signed-off-by: Andrew Mason <amason@slack-corp.com>

--- go/vt/vtctl/grpcvtctldserver/server.go
+++ go/vt/vtctl/grpcvtctldserver/server.go
@@ -343,6 +343,11 @@ func (s *VtctldServer) GetTablets(ctx context.Context, req *vtctldatapb.GetTable
 	}, nil
 }
 
+// GetVSchema is part of the vtctlservicepb.VtctldServer interface.
+func (s *VtctldServer) GetVSchema(ctx context.Context, req *vtctldatapb.GetVSchemaRequest) (*vtctldatapb.GetVSchemaResponse, error) {
+	panic("unimplemented!")
+}
+
 // InitShardPrimary is part of the vtctlservicepb.VtctldServer interface.
 func (s *VtctldServer) InitShardPrimary(ctx context.Context, req *vtctldatapb.InitShardPrimaryRequest) (*vtctldatapb.InitShardPrimaryResponse, error) {
 	if req.Keyspace == "" {
