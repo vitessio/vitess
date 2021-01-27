c20a1289a4a482b7e2d10d6f0cbced62343aa07c
Andrew Mason <amason@slack-corp.com>
2021-01-26 21:43:06 -0500

    Implement `DeleteShards` with tests
    Signed-off-by: Andrew Mason <amason@slack-corp.com>

--- go/vt/vtctl/grpcvtctldserver/topo.go
+++ go/vt/vtctl/grpcvtctldserver/topo.go
@@ -30,6 +30,131 @@ import (
 	"vitess.io/vitess/go/vt/proto/vtrpc"
 )
 
+func deleteShard(ctx context.Context, ts *topo.Server, keyspace string, shard string, recursive bool, evenIfServing bool) error {
+	// Read the Shard object. If it's not in the topo, try to clean up the topo
+	// anyway.
+	shardInfo, err := ts.GetShard(ctx, keyspace, shard)
+	if err != nil {
+		if topo.IsErrType(err, topo.NoNode) {
+			log.Infof("Shard %v/%v doesn't seem to exist; cleaning up any potential leftover topo data", keyspace, shard)
+
+			return ts.DeleteShard(ctx, keyspace, shard)
+		}
+
+		return err
+	}
+
+	servingCells, err := ts.GetShardServingCells(ctx, shardInfo)
+	if err != nil {
+		return err
+	}
+
+	// We never want to remove a potentially serving shard unless someone
+	// explicitly requested it.
+	if len(servingCells) > 0 && !evenIfServing {
+		return vterrors.Errorf(vtrpc.Code_FAILED_PRECONDITION, "shard %v/%v is still serving; cannot delete it; use EvenIfServing = true to delete anyway", keyspace, shard)
+	}
+
+	cells, err := ts.GetCellInfoNames(ctx)
+	if err != nil {
+		return err
+	}
+
+	for _, cell := range cells {
+		if err := deleteShardCell(ctx, ts, keyspace, shard, cell, recursive); err != nil {
+			return err
+		}
+	}
+
+	// Try to remove the replication and serving graphs from each cell,
+	// regardless of whether they exist.
+	for _, cell := range cells {
+		if err := ts.DeleteShardReplication(ctx, cell, keyspace, shard); err != nil && !topo.IsErrType(err, topo.NoNode) {
+			log.Warningf("Cannot delete ShardReplication in cell %v for %v/%v: %w", cell, keyspace, shard, err)
+		}
+	}
+
+	return ts.DeleteShard(ctx, keyspace, shard)
+}
+
+// deleteShardCell is the per-cell helper function for deleteShard, and is
+// distinct from the RemoveShardCell rpc. Despite having similar names, they are
+// **not** the same!
+func deleteShardCell(ctx context.Context, ts *topo.Server, keyspace string, shard string, cell string, recursive bool) error {
+	var aliases []*topodatapb.TabletAlias
+
+	// Get the ShardReplication object for the cell. Collect all the tablets
+	// that belong to the shard.
+	sri, err := ts.GetShardReplication(ctx, cell, keyspace, shard)
+	switch {
+	case topo.IsErrType(err, topo.NoNode):
+		// No ShardReplication object means that the topo is inconsistent.
+		// Therefore we read all the tablets for that cell, and if we find any
+		// in our shard, we'll either abort or try to delete them, depending on
+		// whether recursive=true.
+		aliases, err = ts.GetTabletsByCell(ctx, cell)
+		if err != nil {
+			return fmt.Errorf("GetTabletsByCell(%v) failed: %w", cell, err)
+		}
+	case err == nil:
+		// If a ShardReplication object exists, we trust it to have all the
+		// tablet records for the shard in that cell.
+		aliases = make([]*topodatapb.TabletAlias, len(sri.Nodes))
+
+		for i, node := range sri.Nodes {
+			aliases[i] = node.TabletAlias
+		}
+	default:
+		return fmt.Errorf("GetShardReplication(%v, %v, %v) failed: %w", cell, keyspace, shard, err)
+	}
+
+	// Get all the tablet records for the aliases we've collected. Note that
+	// GetTabletMap ignores ErrNoNode, which is convenient for our purpose; it
+	// means a tablet was deleted but is still referenced.
+	tabletMap, err := ts.GetTabletMap(ctx, aliases)
+	if err != nil {
+		return fmt.Errorf("GetTabletMap() failed: %w", err)
+	}
+
+	// In the case where no ShardReplication object exists, we collect the
+	// aliases of every tablet in the cell, so we'll need to filter
+	// out anything not in our shard.
+	for alias, ti := range tabletMap {
+		if !(ti.Keyspace == keyspace && ti.Shard == shard) {
+			delete(tabletMap, alias)
+		}
+	}
+
+	// If there are any tablets in the shard in the cell, delete them.
+	if len(tabletMap) > 0 {
+		if !recursive {
+			return vterrors.Errorf(vtrpc.Code_FAILED_PRECONDITION, "Shard %v/%v still hase %v tablets in cell %v; use Recursive = true or remove them manually", keyspace, shard, len(tabletMap), cell)
+		}
+
+		log.Infof("Deleting all %d tablets in shard %v/%v cell %v", len(tabletMap), keyspace, shard, cell)
+		for alias, tablet := range tabletMap {
+			// We don't care about updating the ShardReplication object, because
+			// later we're going to delete the entire object.
+			log.Infof("Deleting tablet %v", alias)
+			if err := ts.DeleteTablet(ctx, tablet.Alias); err != nil && !topo.IsErrType(err, topo.NoNode) {
+				// We don't want to continue if a DeleteTablet fails for any
+				// reason other than a missing tablet (in which case it's just
+				// topo server inconsistency, which we can ignore). If we were
+				// to continue and delete the replication graph, the tablet
+				// record would become orphaned, since we'd no longer know that
+				// it belongs to this shard.
+				//
+				// If the problem is temporary, or resolved externally,
+				// re-running DeleteShard will skip over tablets that were
+				// already deleted.
+				return fmt.Errorf("cannot delete tablet %v: %w", alias, err)
+			}
+		}
+	}
+
+	return nil
+}
+
 func deleteTablet(ctx context.Context, ts *topo.Server, alias *topodatapb.TabletAlias, allowPrimary bool) (err error) {
 	tablet, err := ts.GetTablet(ctx, alias)
 	if err != nil {
