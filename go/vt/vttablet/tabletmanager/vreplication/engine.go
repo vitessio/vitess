965c1750d2d0319f79fd7219c6536833f57c0693
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2021-02-01 09:24:11 +0200

    Throttler: fix to client usage in vreplication and table GC
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/vttablet/tabletmanager/vreplication/engine.go
+++ go/vt/vttablet/tabletmanager/vreplication/engine.go
@@ -134,7 +134,7 @@ func NewEngine(config *tabletenv.TabletConfig, ts *topo.Server, cell string, mys
 		mysqld:          mysqld,
 		journaler:       make(map[string]*journalEvent),
 		ec:              newExternalConnector(config.ExternalConnections),
-		throttlerClient: throttle.NewBackgroundClient(lagThrottler, throttlerAppName, throttle.ThrottleCheckSelf),
+		throttlerClient: throttle.NewBackgroundClient(lagThrottler, throttlerAppName, throttle.ThrottleCheckPrimaryWrite),
 	}
 	return vre
 }
