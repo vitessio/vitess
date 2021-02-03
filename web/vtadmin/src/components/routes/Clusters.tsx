8441734b4be926201a0ffd3aa7540873fe3527e2
Sara Bee <855595+doeg@users.noreply.github.com>
2021-02-03 12:43:10 -0500

    [vtadmin-web] Add routes and simple tables for all entities
    Signed-off-by: Sara Bee <855595+doeg@users.noreply.github.com>

---
+++ web/vtadmin/src/components/routes/Clusters.tsx
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2021 The Vitess Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { orderBy } from 'lodash-es';
+import * as React from 'react';
+import { useClusters } from '../../hooks/api';
+
+export const Clusters = () => {
+    const { data } = useClusters();
+
+    const rows = React.useMemo(() => {
+        return orderBy(data, ['name']);
+    }, [data]);
+
+    return (
+        <div>
+            <h1>Clusters</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Id</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows.map((cluster, idx) => (
+                        <tr key={idx}>
+                            <td>{cluster.name}</td>
+                            <td>{cluster.id}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
