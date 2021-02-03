8ef58783d8aa73865e0b251219eb269296688cbb
Sara Bee <855595+doeg@users.noreply.github.com>
2021-02-03 12:43:10 -0500

    [vtadmin-web] Add routes and simple tables for all entities
    Signed-off-by: Sara Bee <855595+doeg@users.noreply.github.com>

---
+++ web/vtadmin/src/components/routes/Schemas.tsx
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2021 The Vitess Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { orderBy } from 'lodash-es';
+import * as React from 'react';
+import { useTableDefinitions } from '../../hooks/api';
+
+export const Schemas = () => {
+    const { data = [] } = useTableDefinitions();
+
+    const rows = React.useMemo(() => {
+        return orderBy(data, ['cluster.name', 'keyspace', 'tableDefinition.name']);
+    }, [data]);
+
+    return (
+        <div>
+            <h1>Schemas</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Cluster</th>
+                        <th>Keyspace</th>
+                        <th>Table</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows.map((row, idx) => (
+                        <tr key={idx}>
+                            <td>{row.cluster?.name}</td>
+                            <td>{row.keyspace}</td>
+                            <td>{row.tableDefinition?.name}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
--- web/vtadmin/src/components/routes/Tablets.tsx
+++ web/vtadmin/src/components/routes/Tablets.tsx
@@ -16,27 +16,56 @@
 import * as React from 'react';
 
 import { useTablets } from '../../hooks/api';
-import { TabletList } from '../TabletList';
+import { vtadmin as pb, topodata } from '../../proto/vtadmin';
+import { orderBy } from 'lodash-es';
 
 export const Tablets = () => {
-    const { data, error, isError, isSuccess } = useTablets();
+    const { data = [] } = useTablets();
 
-    // Placeholder UI :D
-    let content = <div>Loading...</div>;
-    if (isError) {
-        content = (
-            <div>
-                {error?.name}: {error?.message}
-            </div>
-        );
-    } else if (isSuccess) {
-        content = <TabletList tablets={data || []} />;
-    }
+    const rows = React.useMemo(() => {
+        return orderBy(data, ['cluster.name', 'tablet.keyspace', 'tablet.shard', 'tablet.type']);
+    }, [data]);
 
     return (
         <div>
             <h1>Tablets</h1>
-            {content}
+            <table>
+                <thead>
+                    <tr>
+                        <th>Cluster</th>
+                        <th>Keyspace</th>
+                        <th>Shard</th>
+                        <th>Alias</th>
+                        <th>Hostname</th>
+
+                        <th>Type</th>
+                        <th>State</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows.map((t, tdx) => (
+                        <tr key={tdx}>
+                            <td>{t.cluster?.name}</td>
+                            <td>{t.tablet?.keyspace}</td>
+                            <td>{t.tablet?.shard}</td>
+                            <td>{formatAlias(t)}</td>
+                            <td>{t.tablet?.hostname}</td>
+                            <td>{formatType(t)}</td>
+                            <td>{formatState(t)}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 };
+
+const SERVING_STATES = Object.keys(pb.Tablet.ServingState);
+const TABLET_TYPES = Object.keys(topodata.TabletType);
+
+const formatAlias = (t: pb.Tablet) =>
+    t.tablet?.alias?.cell && t.tablet?.alias?.uid && `${t.tablet.alias.cell}-${t.tablet.alias.uid}`;
+
+const formatType = (t: pb.Tablet) => t.tablet?.type && TABLET_TYPES[t.tablet?.type];
+
+const formatState = (t: pb.Tablet) => t.state && SERVING_STATES[t.state];
