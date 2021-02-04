63329ceb81165f9e1ee24aa86c029bd664462128
Harshit Gangal <harshit@planetscale.com>
2021-02-04 22:25:46 +0530

    plan explain table correctly
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/vt/vtgate/planbuilder/other_read.go
+++ go/vt/vtgate/planbuilder/other_read.go
@@ -35,7 +35,6 @@ func buildOtherReadAndAdmin(sql string, vschema ContextVSchema) (engine.Primitiv
 		Keyspace:          keyspace,
 		TargetDestination: destination,
 		Query:             sql, //This is original sql query to be passed as the parser can provide partial ddl AST.
-		IsDML:             false,
 		SingleShardOnly:   true,
 	}, nil
 }
