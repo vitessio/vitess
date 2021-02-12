8208406056cbfa19ab94ee9b3499ca8e2cf5162a
Andres Taylor <andres@planetscale.com>
2021-02-12 07:49:00 +0100

    it's a start
    Signed-off-by: Andres Taylor <andres@planetscale.com>

---
+++ go/tools/asthelpergen/integration/types.go
@@ -0,0 +1,54 @@
+/*
+Copyright 2021 The Vitess Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+//nolint
+package integration
+
+type (
+	AST interface {
+		i()
+	}
+
+	//Plus struct {
+	//	Left, Right AST
+	//}
+	//
+	//Array struct {
+	//	Values []AST
+	//}
+	//
+	//UnaryMinus struct {
+	//	Val *LiteralInt
+	//}
+	//
+	LiteralInt struct {
+		Val int
+	}
+
+	//String struct {
+	//	Val string
+	//}
+
+	//ArrayDef []AST
+)
+
+//func (*Plus) i()        {}
+//func (*Array) i()       {}
+//func (*UnaryMinus) i()  {}
+func (*LiteralInt) i() {}
+
+//func (String) i()       {}
+//func (ArrayDef) i()     {}
