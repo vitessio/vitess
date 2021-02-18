a537c967f411fc9859b07fee40bfe6ef0676b84d
Andrew Mason <amason@slack-corp.com>
2021-02-17 21:59:01 -0500

    Add testutil vtctld constructor for fake TabletManagerClient impls
    Signed-off-by: Andrew Mason <amason@slack-corp.com>

--- go/vt/vtctl/grpcvtctldserver/testutil/test_tmclient.go
+++ go/vt/vtctl/grpcvtctldserver/testutil/test_tmclient.go
@@ -19,6 +19,8 @@ package testutil
 import (
 	"context"
 	"fmt"
+	"sync"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 
@@ -29,6 +31,7 @@ import (
 
 	tabletmanagerdatapb "vitess.io/vitess/go/vt/proto/tabletmanagerdata"
 	topodatapb "vitess.io/vitess/go/vt/proto/topodata"
+	vtctlservicepb "vitess.io/vitess/go/vt/proto/vtctlservice"
 	"vitess.io/vitess/go/vt/proto/vttime"
 )
 
@@ -60,6 +63,95 @@ func (c *tabletManagerClient) GetSchema(ctx context.Context, tablet *topodatapb.
 	}
 
 	return schema, nil
+}
+
+// SetMaster is part of the tmclient.TabletManagerClient interface.
+func (c *tabletManagerClient) SetMaster(ctx context.Context, tablet *topodatapb.Tablet, parent *topodatapb.TabletAlias, timeCreatedNS int64, waitPosition string, forceStartReplication bool) error {
+	if c.Topo == nil {
+		return assert.AnError
+	}
+
+	return nil
+}
+
+var (
+	tmclientLock        sync.Mutex
+	tmclientFactoryLock sync.Mutex
+	tmclients           = map[string]tmclient.TabletManagerClient{}
+	tmclientFactories   = map[string]func() tmclient.TabletManagerClient{}
+)
+
+// NewVtctldServerWithTabletManagerClient returns a new
+// grpcvtctldserver.VtctldServer configured with the given topo server and
+// tmclient.TabletManagerClient implementation for testing.
+//
+// It synchronizes on private locks to prevent multiple goroutines from stepping
+// on each other during VtctldServer initialization, but still run the rest of
+// the test in parallel.
+//
+// NOTE, THE FIRST: It is only safe to use in parallel with other tests using
+// this method of creating a VtctldServer, or with tests that do not depend on a
+// VtctldServer's tmclient.TabletManagerClient implementation.
+//
+// NOTE, THE SECOND: It needs to register a unique name to the tmclient factory
+// registry, so we keep a shadow map of factories registered for "protocols" by
+// this function. That way, if we happen to have multiple tests with the same
+// name, we can swap out the return value for the factory and allow both tests
+// to run, rather than the second test failing when it attempts to register a
+// second factory for the same "protocol" name.
+//
+// NOTE, THE THIRD: we take a "new" func to produce a valid
+// vtctlservicepb.VtctldServer implementation, rather than constructing directly
+// ourselves with grpcvtctldserver.NewVtctldServer. This is to prevent an import
+// cycle between this package and package grpcvtctldserver. Further, because the
+// return type of NewVtctldServer is the struct type
+// (*grpcvtctldserver.VtctldServer) and not the interface type
+// vtctlservicepb.VtctldServer, tests will need to indirect that call through an
+// extra layer rather than passing the function identifier directly, e.g.:
+//
+//		vtctld := testutil.NewVtctldServerWithTabletManagerClient(t, ts, &testutil.TabletManagerClient{
+//			...
+//		}, func(ts *topo.Server) vtctlservicepb.VtctldServer { return NewVtctldServer(ts) })
+//
+func NewVtctldServerWithTabletManagerClient(t *testing.T, ts *topo.Server, tmc tmclient.TabletManagerClient, newVtctldServerFn func(ts *topo.Server) vtctlservicepb.VtctldServer) vtctlservicepb.VtctldServer {
+	tmclientFactoryLock.Lock()
+	defer tmclientFactoryLock.Unlock()
+
+	protocol := t.Name()
+
+	if _, alreadyRegisteredFactory := tmclientFactories[protocol]; !alreadyRegisteredFactory {
+		factory := func() tmclient.TabletManagerClient {
+			tmclientLock.Lock()
+			defer tmclientLock.Unlock()
+
+			client, ok := tmclients[protocol]
+			if !ok {
+				t.Fatal("Test managed to register a factory for a client value that never got set; this should be impossible")
+			}
+
+			return client
+		}
+
+		tmclient.RegisterTabletManagerClientFactory(protocol, factory)
+		tmclientFactories[protocol] = factory
+	}
+
+	// Always swap in the new client return value for the given protocol name.
+	// We cannot defer the unlock here, because grpcvtctldserver.NewVtctldServer
+	// eventually will call into the factory we registered above, and we will
+	// deadlock ourselves.
+	tmclientLock.Lock()
+	tmclients[protocol] = tmc
+	tmclientLock.Unlock()
+
+	// Be (mostly, we can't help concurrent goroutines not using this function)
+	// atomic with our mutation of the global TabletManagerProtocol pointer.
+	oldProto := *tmclient.TabletManagerProtocol
+	defer func() { *tmclient.TabletManagerProtocol = oldProto }()
+
+	*tmclient.TabletManagerProtocol = protocol
+
+	return newVtctldServerFn(ts)
 }
 
 // TabletManagerClientProtocol is the protocol this package registers its client
