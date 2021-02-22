a71f7e0f4eabe24d0c6d495d18ae325e9908406a
Andrew Mason <amason@slack-corp.com>
2021-02-22 11:50:20 -0500

    Add CLI entrypoint for `EmergencyReparentShard`
    Signed-off-by: Andrew Mason <amason@slack-corp.com>

--- go/cmd/vtctldclient/internal/command/reparents.go
+++ go/cmd/vtctldclient/internal/command/reparents.go
@@ -25,12 +25,22 @@ import (
 	"vitess.io/vitess/go/cmd/vtctldclient/cli"
 	"vitess.io/vitess/go/protoutil"
 	"vitess.io/vitess/go/vt/log"
+	"vitess.io/vitess/go/vt/logutil"
+	"vitess.io/vitess/go/vt/topo"
 	"vitess.io/vitess/go/vt/topo/topoproto"
 
+	topodatapb "vitess.io/vitess/go/vt/proto/topodata"
 	vtctldatapb "vitess.io/vitess/go/vt/proto/vtctldata"
 )
 
 var (
+	// EmergencyReparentShard makes an EmergencyReparent gRPC call to a vtctld.
+	EmergencyReparentShard = &cobra.Command{
+		Use:  "EmergencyReparentShard <keyspace/shard>",
+		Args: cobra.ExactArgs(1),
+		Long: "Reparents the shard to the new primary. Assumes the old primary is dead and not responding",
+		RunE: commandEmergencyReparentShard,
+	}
 	// InitShardPrimary makes an InitShardPrimary gRPC call to a vtctld.
 	InitShardPrimary = &cobra.Command{
 		Use:  "InitShardPrimary",
@@ -53,6 +63,60 @@ var (
 		RunE: commandTabletExternallyReparented,
 	}
 )
+
+var emergencyReparentShardOptions = struct {
+	Force                     bool
+	WaitReplicasTimeout       time.Duration
+	NewPrimaryAliasStr        string
+	IgnoreReplicaAliasStrList []string
+}{}
+
+func commandEmergencyReparentShard(cmd *cobra.Command, args []string) error {
+	keyspace, shard, err := topoproto.ParseKeyspaceShard(cmd.Flags().Arg(0))
+	if err != nil {
+		return err
+	}
+
+	var (
+		newPrimaryAlias      *topodatapb.TabletAlias
+		ignoreReplicaAliases = make([]*topodatapb.TabletAlias, len(emergencyReparentShardOptions.IgnoreReplicaAliasStrList))
+	)
+
+	if emergencyReparentShardOptions.NewPrimaryAliasStr != "" {
+		newPrimaryAlias, err = topoproto.ParseTabletAlias(emergencyReparentShardOptions.NewPrimaryAliasStr)
+		if err != nil {
+			return err
+		}
+	}
+
+	for i, aliasStr := range emergencyReparentShardOptions.IgnoreReplicaAliasStrList {
+		alias, err := topoproto.ParseTabletAlias(aliasStr)
+		if err != nil {
+			return err
+		}
+
+		ignoreReplicaAliases[i] = alias
+	}
+
+	cli.FinishedParsing(cmd)
+
+	resp, err := client.EmergencyReparentShard(commandCtx, &vtctldatapb.EmergencyReparentShardRequest{
+		Keyspace:            keyspace,
+		Shard:               shard,
+		NewPrimary:          newPrimaryAlias,
+		IgnoreReplicas:      ignoreReplicaAliases,
+		WaitReplicasTimeout: ptypes.DurationProto(emergencyReparentShardOptions.WaitReplicasTimeout),
+	})
+	if err != nil {
+		return err
+	}
+
+	for _, event := range resp.Events {
+		fmt.Println(logutil.EventString(event))
+	}
+
+	return nil
+}
 
 var initShardPrimaryOptions = struct {
 	WaitReplicasTimeout time.Duration
@@ -134,6 +198,11 @@ func commandTabletExternallyReparented(cmd *cobra.Command, args []string) error 
 }
 
 func init() {
+	EmergencyReparentShard.Flags().DurationVar(&emergencyReparentShardOptions.WaitReplicasTimeout, "wait-replicas-timeout", *topo.RemoteOperationTimeout, "Time to wait for replicas to catch up in reparenting.")
+	EmergencyReparentShard.Flags().StringVar(&emergencyReparentShardOptions.NewPrimaryAliasStr, "new-primary", "", "Alias of a tablet that should be the new primary. If not specified, the vtctld will select the best candidate to promote.")
+	EmergencyReparentShard.Flags().StringSliceVarP(&emergencyReparentShardOptions.IgnoreReplicaAliasStrList, "ignore-replicas", "i", nil, "Comma-separated, repeated list of replica tablet aliases to ignore during the emergency reparent.")
+	Root.AddCommand(EmergencyReparentShard)
+
 	InitShardPrimary.Flags().DurationVar(&initShardPrimaryOptions.WaitReplicasTimeout, "wait-replicas-timeout", 30*time.Second, "time to wait for replicas to catch up in reparenting")
 	InitShardPrimary.Flags().BoolVar(&initShardPrimaryOptions.Force, "force", false, "will force the reparent even if the provided tablet is not a master or the shard master")
 	Root.AddCommand(InitShardPrimary)
