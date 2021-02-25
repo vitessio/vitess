0b843af7614e0ca69078b0b41b062b6bb470a939
Jacques Grove <aquarapid@gmail.com>
2021-02-24 21:07:41 -0800

    Adjust parameter naming to reflect that it is a threshold, not a limit.
    Signed-off-by: Jacques Grove <aquarapid@gmail.com>

--- go/streamlog/streamlog.go
+++ go/streamlog/streamlog.go
@@ -44,8 +44,8 @@ var (
 	// QueryLogFilterTag contains an optional string that must be present in the query for it to be logged
 	QueryLogFilterTag = flag.String("querylog-filter-tag", "", "string that must be present in the query for it to be logged; if using a value as the tag, you need to disable query normalization")
 
-	// QueryLogRowLimit only log queries returning or affecting this many rows
-	QueryLogRowLimit = flag.Uint64("querylog-row-limit", 0, "Minimum number of rows a query has to return or affect before being logged; not useful for streaming queries")
+	// QueryLogRowThreshold only log queries returning or affecting this many rows
+	QueryLogRowThreshold = flag.Uint64("querylog-row-threshold", 0, "Number of rows a query has to return or affect before being logged; not useful for streaming queries. 0 means all queries will be logged.")
 
 	sendCount      = stats.NewCountersWithSingleLabel("StreamlogSend", "stream log send count", "logger_names")
 	deliveredCount = stats.NewCountersWithMultiLabels(
@@ -212,7 +212,7 @@ func GetFormatter(logger *StreamLogger) LogFormatter {
 // ShouldEmitLog returns whether the log with the given SQL query
 // should be emitted or filtered
 func ShouldEmitLog(sql string, rowsAffected, rowsReturned uint64) bool {
-	if *QueryLogRowLimit > maxUint64(rowsAffected, rowsReturned) && *QueryLogFilterTag == "" {
+	if *QueryLogRowThreshold > maxUint64(rowsAffected, rowsReturned) && *QueryLogFilterTag == "" {
 		return false
 	}
 	if *QueryLogFilterTag != "" {
