5af13c017dd5b82958746a2caf7b1833813e1a89
Sara Bee <855595+doeg@users.noreply.github.com>
2021-02-24 20:25:32 -0500

    Add cluster_test.go
    Signed-off-by: Sara Bee <855595+doeg@users.noreply.github.com>

--- go/vt/vtadmin/api.go
+++ go/vt/vtadmin/api.go
@@ -303,7 +303,7 @@ func (api *API) GetSchemas(ctx context.Context, req *vtadminpb.GetSchemasRequest
 
 			// Since tablets are per-cluster, we can fetch them once
 			// and use them throughout the other waitgroups.
-			tablets, err := api.getTablets(ctx, c)
+			tablets, err := c.GetTablets(ctx)
 			if err != nil {
 				er.RecordError(err)
 				return
@@ -450,7 +450,7 @@ func (api *API) GetTablet(ctx context.Context, req *vtadminpb.GetTabletRequest) 
 		go func(c *cluster.Cluster) {
 			defer wg.Done()
 
-			ts, err := api.getTablets(ctx, c)
+			ts, err := c.GetTablets(ctx)
 			if err != nil {
 				er.RecordError(err)
 				return
@@ -506,7 +506,7 @@ func (api *API) GetTablets(ctx context.Context, req *vtadminpb.GetTabletsRequest
 		go func(c *cluster.Cluster) {
 			defer wg.Done()
 
-			ts, err := api.getTablets(ctx, c)
+			ts, err := c.GetTablets(ctx)
 			if err != nil {
 				er.RecordError(err)
 				return
@@ -527,60 +527,6 @@ func (api *API) GetTablets(ctx context.Context, req *vtadminpb.GetTabletsRequest
 	return &vtadminpb.GetTabletsResponse{
 		Tablets: tablets,
 	}, nil
-}
-
-func (api *API) getTablets(ctx context.Context, c *cluster.Cluster) ([]*vtadminpb.Tablet, error) {
-	if err := c.DB.Dial(ctx, ""); err != nil {
-		return nil, err
-	}
-
-	rows, err := c.DB.ShowTablets(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	return c.ParseTablets(rows)
-}
-
-// findTablet returns the first tablet in a given cluster that satisfies the filter function.
-func (api *API) findTablet(ctx context.Context, cluster *cluster.Cluster, filter func(*vtadminpb.Tablet) bool) (*vtadminpb.Tablet, error) {
-	tablets, err := api.findTablets(ctx, cluster, filter, 1)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(tablets) != 1 {
-		return nil, errors.ErrNoTablet
-	}
-
-	return tablets[0], nil
-}
-
-// findTablets returns the first N tablets in the given cluster that satisfy
-// the filter function. If N = -1, then all matching tablets are returned.
-// Ordering is not guaranteed, and callers should write their filter functions accordingly.
-func (api *API) findTablets(ctx context.Context, cluster *cluster.Cluster, filter func(*vtadminpb.Tablet) bool, n int) ([]*vtadminpb.Tablet, error) {
-	tablets, err := api.getTablets(ctx, cluster)
-	if err != nil {
-		return nil, err
-	}
-
-	if n == -1 {
-		n = len(tablets)
-	}
-
-	results := make([]*vtadminpb.Tablet, 0, n)
-	for _, t := range tablets {
-		if len(results) >= n {
-			break
-		}
-
-		if filter(t) {
-			results = append(results, t)
-		}
-	}
-
-	return results, nil
 }
 
 func (api *API) getClustersForRequest(ids []string) ([]*cluster.Cluster, []string) {
@@ -627,7 +573,7 @@ func (api *API) VTExplain(ctx context.Context, req *vtadminpb.VTExplainRequest) 
 		return nil, errors.ErrUnsupportedCluster
 	}
 
-	tablet, err := api.findTablet(ctx, c, func(t *vtadminpb.Tablet) bool {
+	tablet, err := c.FindTablet(ctx, func(t *vtadminpb.Tablet) bool {
 		return t.Tablet.Keyspace == req.Keyspace && topo.IsInServingGraph(t.Tablet.Type) && t.Tablet.Type != topodatapb.TabletType_MASTER && t.State == vtadminpb.Tablet_SERVING
 	})
 
---
+++ go/vt/vtadmin/cluster/cluster_test.go
@@ -0,0 +1,51 @@
+/*
+Copyright 2021 The Vitess Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cluster
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"vitess.io/vitess/go/vt/vitessdriver"
+	"vitess.io/vitess/go/vt/vtadmin/cluster/discovery/fakediscovery"
+	"vitess.io/vitess/go/vt/vtadmin/vtsql"
+
+	vtadminpb "vitess.io/vitess/go/vt/proto/vtadmin"
+)
+
+func TestGetTablets(t *testing.T) {
+	disco := fakediscovery.New()
+	disco.AddTaggedGates(nil, &vtadminpb.VTGate{Hostname: "gate"})
+
+	db := vtsql.New(&vtsql.Config{
+		Cluster: &vtadminpb.Cluster{
+			Id:   "c1",
+			Name: "one",
+		},
+		Discovery: disco,
+	})
+	db.DialFunc = func(cfg vitessdriver.Configuration) (*sql.DB, error) {
+		return nil, assert.AnError
+	}
+
+	cluster := &Cluster{DB: db}
+	_, err := cluster.GetTablets(context.Background())
+	assert.Error(t, err)
+}
