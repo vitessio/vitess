e5b9db72ba90d942576e19e5cfae7f2448e0ed5c
Sara Bee <855595+doeg@users.noreply.github.com>
2021-02-24 22:08:59 -0500

    Add TestFindTablets
    Signed-off-by: Sara Bee <855595+doeg@users.noreply.github.com>

--- go/vt/vtadmin/cluster/cluster_test.go
+++ go/vt/vtadmin/cluster/cluster_test.go
@@ -26,8 +26,11 @@ import (
 	"vitess.io/vitess/go/vt/vitessdriver"
 	"vitess.io/vitess/go/vt/vtadmin/cluster"
 	"vitess.io/vitess/go/vt/vtadmin/cluster/discovery/fakediscovery"
+	"vitess.io/vitess/go/vt/vtadmin/testutil"
 	"vitess.io/vitess/go/vt/vtadmin/vtsql"
 
+	topodatapb "vitess.io/vitess/go/vt/proto/topodata"
+	"vitess.io/vitess/go/vt/proto/vtadmin"
 	vtadminpb "vitess.io/vitess/go/vt/proto/vtadmin"
 )
 
@@ -49,4 +52,195 @@ func TestGetTablets(t *testing.T) {
 	c := &cluster.Cluster{DB: db}
 	_, err := c.GetTablets(context.Background())
 	assert.Error(t, err)
+}
+
+func TestFindTablets(t *testing.T) {
+	tests := []struct {
+		name     string
+		tablets  []*vtadminpb.Tablet
+		filter   func(*vtadminpb.Tablet) bool
+		n        int
+		expected []*vtadminpb.Tablet
+	}{
+		{
+			name: "returns n filtered tablets",
+			tablets: []*vtadminpb.Tablet{
+				{
+					State: vtadminpb.Tablet_SERVING,
+					Tablet: &topodatapb.Tablet{
+						Alias: &topodatapb.TabletAlias{
+							Cell: "c0_cell1",
+							Uid:  100,
+						},
+						Keyspace: "commerce",
+					},
+				},
+				{
+					State: vtadminpb.Tablet_NOT_SERVING,
+					Tablet: &topodatapb.Tablet{
+						Alias: &topodatapb.TabletAlias{
+							Cell: "c0_cell1",
+							Uid:  101,
+						},
+						Keyspace: "commerce",
+					},
+				},
+				{
+					State: vtadminpb.Tablet_SERVING,
+					Tablet: &topodatapb.Tablet{
+						Alias: &topodatapb.TabletAlias{
+							Cell: "c0_cell1",
+							Uid:  102,
+						},
+						Keyspace: "commerce",
+					},
+				},
+				{
+					State: vtadminpb.Tablet_SERVING,
+					Tablet: &topodatapb.Tablet{
+						Alias: &topodatapb.TabletAlias{
+							Cell: "c0_cell1",
+							Uid:  103,
+						},
+						Keyspace: "commerce",
+					},
+				},
+			},
+			filter: func(t *vtadminpb.Tablet) bool {
+				return t.State == vtadminpb.Tablet_SERVING
+			},
+			n: 2,
+			expected: []*vtadminpb.Tablet{
+				{
+					Cluster: &vtadmin.Cluster{
+						Id:   "c0",
+						Name: "cluster0",
+					},
+					State: vtadminpb.Tablet_SERVING,
+					Tablet: &topodatapb.Tablet{
+						Alias: &topodatapb.TabletAlias{
+							Cell: "c0_cell1",
+							Uid:  100,
+						},
+						Keyspace: "commerce",
+					},
+				},
+				{
+					Cluster: &vtadmin.Cluster{
+						Id:   "c0",
+						Name: "cluster0",
+					},
+					State: vtadminpb.Tablet_SERVING,
+					Tablet: &topodatapb.Tablet{
+						Alias: &topodatapb.TabletAlias{
+							Cell: "c0_cell1",
+							Uid:  102,
+						},
+						Keyspace: "commerce",
+					},
+				},
+			},
+		},
+		{
+			name: "returns all filtered tablets when n == -1",
+			tablets: []*vtadminpb.Tablet{
+				{
+					State: vtadminpb.Tablet_SERVING,
+					Tablet: &topodatapb.Tablet{
+						Alias: &topodatapb.TabletAlias{
+							Cell: "c0_cell1",
+							Uid:  100,
+						},
+						Keyspace: "commerce",
+					},
+				},
+				{
+					State: vtadminpb.Tablet_NOT_SERVING,
+					Tablet: &topodatapb.Tablet{
+						Alias: &topodatapb.TabletAlias{
+							Cell: "c0_cell1",
+							Uid:  101,
+						},
+						Keyspace: "commerce",
+					},
+				},
+				{
+					State: vtadminpb.Tablet_SERVING,
+					Tablet: &topodatapb.Tablet{
+						Alias: &topodatapb.TabletAlias{
+							Cell: "c0_cell1",
+							Uid:  102,
+						},
+						Keyspace: "commerce",
+					},
+				},
+				{
+					State: vtadminpb.Tablet_SERVING,
+					Tablet: &topodatapb.Tablet{
+						Alias: &topodatapb.TabletAlias{
+							Cell: "c0_cell1",
+							Uid:  103,
+						},
+						Keyspace: "commerce",
+					},
+				},
+			},
+			filter: func(t *vtadminpb.Tablet) bool {
+				return t.State == vtadminpb.Tablet_SERVING
+			},
+			n: -1,
+			expected: []*vtadminpb.Tablet{
+				{
+					Cluster: &vtadmin.Cluster{
+						Id:   "c0",
+						Name: "cluster0",
+					},
+					State: vtadminpb.Tablet_SERVING,
+					Tablet: &topodatapb.Tablet{
+						Alias: &topodatapb.TabletAlias{
+							Cell: "c0_cell1",
+							Uid:  100,
+						},
+						Keyspace: "commerce",
+					},
+				},
+				{
+					Cluster: &vtadmin.Cluster{
+						Id:   "c0",
+						Name: "cluster0",
+					},
+					State: vtadminpb.Tablet_SERVING,
+					Tablet: &topodatapb.Tablet{
+						Alias: &topodatapb.TabletAlias{
+							Cell: "c0_cell1",
+							Uid:  102,
+						},
+						Keyspace: "commerce",
+					},
+				},
+				{
+					Cluster: &vtadmin.Cluster{
+						Id:   "c0",
+						Name: "cluster0",
+					},
+					State: vtadminpb.Tablet_SERVING,
+					Tablet: &topodatapb.Tablet{
+						Alias: &topodatapb.TabletAlias{
+							Cell: "c0_cell1",
+							Uid:  103,
+						},
+						Keyspace: "commerce",
+					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		cluster := testutil.BuildCluster(0, nil, tt.tablets, nil)
+		tablets, err := cluster.FindTablets(context.Background(), tt.filter, tt.n)
+
+		assert.NoError(t, err)
+		testutil.AssertTabletSlicesEqual(t, tt.expected, tablets)
+	}
 }
