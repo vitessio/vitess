ce03db3d0e7b9257a2ab7ea0480f316fa80afac4
Harshit Gangal <harshit@planetscale.com>
2021-03-03 18:05:29 +0530

    e2e test fir dbddl plugin
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/test/endtoend/vtgate/createdb_plugin/main_test.go
+++ go/test/endtoend/vtgate/createdb_plugin/main_test.go
@@ -22,11 +22,6 @@ import (
 	"fmt"
 	"os"
 	"testing"
-	"time"
-
-	"vitess.io/vitess/go/vt/vtgate/engine"
-
-	"vitess.io/vitess/go/vt/log"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/assert"
@@ -39,140 +34,15 @@ import (
 
 var (
 	clusterInstance *cluster.LocalProcessCluster
+	vtParams        mysql.ConnParams
+	keyspaceName    = "ks"
 	cell            = "zone1"
 	hostname        = "localhost"
-	KeyspaceName    = "customer"
-	SchemaSQL       = `
-CREATE TABLE t1 (
-    c1 BIGINT NOT NULL,
-    c2 BIGINT NOT NULL,
-    c3 BIGINT,
-    c4 varchar(100),
-    PRIMARY KEY (c1),
-    UNIQUE KEY (c2),
-    UNIQUE KEY (c3),
-    UNIQUE KEY (c4)
-) ENGINE=Innodb;
-
-CREATE TABLE allDefaults (
-  id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255)
-) ENGINE=Innodb;`
-	VSchema = `
-{
-    "sharded": false,
-    "tables": {
-        "t1": {
-            "columns": [
-                {
-                    "name": "c1",
-                    "type": "INT64"
-                },
-                {
-                    "name": "c2",
-                    "type": "INT64"
-                },
-                {
-                    "name": "c3",
-                    "type": "INT64"
-                },
-                {
-                    "name": "c4",
-                    "type": "VARCHAR"
-                }
-            ]
-        },
-        "allDefaults": {
-            "columns": [
-                {
-                    "name": "id",
-                    "type": "INT64"
-                },
-                {
-                    "name": "name",
-                    "type": "VARCHAR"
-                }
-            ]
-        }
-    }
-}
-`
-
-	createProcSQL = `use vt_customer;
-CREATE PROCEDURE sp_insert()
-BEGIN
-	insert into allDefaults () values ();
-END;
-
-CREATE PROCEDURE sp_delete()
-BEGIN
-	delete from allDefaults;
-END;
-
-CREATE PROCEDURE sp_multi_dml()
-BEGIN
-	insert into allDefaults () values ();
-	delete from allDefaults;
-END;
-
-CREATE PROCEDURE sp_variable()
-BEGIN
-	insert into allDefaults () values ();
-	SELECT min(id) INTO @myvar FROM allDefaults;
-	DELETE FROM allDefaults WHERE id = @myvar;
-END;
-
-CREATE PROCEDURE sp_select()
-BEGIN
-	SELECT * FROM allDefaults;
-END;
-
-CREATE PROCEDURE sp_all()
-BEGIN
-	insert into allDefaults () values ();
-    select * from allDefaults;
-	delete from allDefaults;
-    set autocommit = 0;
-END;
-
-CREATE PROCEDURE in_parameter(IN val int)
-BEGIN
-	insert into allDefaults(id) values(val);
-END;
-
-CREATE PROCEDURE out_parameter(OUT val int)
-BEGIN
-	insert into allDefaults(id) values (128);
-	select 128 into val from dual;
-END;
-`
 )
-
-type testPlugin struct {
-	create, drop func(name string, vcursor engine.VCursor) (*sqltypes.Result, error)
-}
-
-func (t *testPlugin) CreateDatabase(name string, vcursor engine.VCursor) (*sqltypes.Result, error) {
-	return t.create(name, vcursor)
-}
-
-func (t *testPlugin) DropDatabase(name string, vcursor engine.VCursor) (*sqltypes.Result, error) {
-	return t.drop(name, vcursor)
-}
-
-var _ engine.DBDDLPlugin = (*testPlugin)(nil)
 
 func TestMain(m *testing.M) {
 	defer cluster.PanicHandler(nil)
 	flag.Parse()
-
-	before := engine.DatabaseCreator
-	engine.DatabaseCreator = &testPlugin{
-		create: func(name string, vcursor engine.VCursor) (*sqltypes.Result, error) {
-
-		},
-		drop: nil,
-	}
 
 	exitCode := func() int {
 		clusterInstance = cluster.NewCluster(cell, hostname)
@@ -184,106 +54,31 @@ func TestMain(m *testing.M) {
 		}
 
 		// Start keyspace
-		Keyspace := &cluster.Keyspace{
-			Name:      KeyspaceName,
-			SchemaSQL: SchemaSQL,
-			VSchema:   VSchema,
+		keyspace := &cluster.Keyspace{
+			Name: keyspaceName,
 		}
-		if err := clusterInstance.StartUnshardedKeyspace(*Keyspace, 0, false); err != nil {
-			log.Fatal(err.Error())
+		if err := clusterInstance.StartKeyspace(*keyspace, []string{"-80", "80-"}, 0, false); err != nil {
 			return 1
 		}
 
 		// Start vtgate
-		if err := clusterInstance.StartVtgate(); err != nil {
-			log.Fatal(err.Error())
+		clusterInstance.VtGateExtraArgs = []string{"-dbddl_plugin", "noop"}
+		vtgateProcess := clusterInstance.NewVtgateInstance()
+		vtgateProcess.SysVarSetEnabled = true
+		if err := vtgateProcess.Setup(); err != nil {
 			return 1
 		}
 
-		masterProcess := clusterInstance.Keyspaces[0].Shards[0].MasterTablet().VttabletProcess
-		if _, err := masterProcess.QueryTablet(createProcSQL, KeyspaceName, false); err != nil {
-			log.Fatal(err.Error())
-			return 1
+		vtParams = mysql.ConnParams{
+			Host: clusterInstance.Hostname,
+			Port: clusterInstance.VtgateMySQLPort,
 		}
-
 		return m.Run()
 	}()
 	os.Exit(exitCode)
 }
 
-func TestSelectIntoAndLoadFrom(t *testing.T) {
-	// Test is skipped because it requires secure-file-priv variable to be set to not NULL or empty.
-	t.Skip()
-	defer cluster.PanicHandler(t)
-	ctx := context.Background()
-	vtParams := mysql.ConnParams{
-		Host: "localhost",
-		Port: clusterInstance.VtgateMySQLPort,
-	}
-	conn, err := mysql.Connect(ctx, &vtParams)
-	require.Nil(t, err)
-	defer conn.Close()
-
-	defer exec(t, conn, `delete from t1`)
-	exec(t, conn, `insert into t1(c1, c2, c3, c4) values (300,100,300,'abc')`)
-	res := exec(t, conn, `select @@secure_file_priv;`)
-	directory := res.Rows[0][0].ToString()
-	query := `select * from t1 into outfile '` + directory + `x.txt'`
-	exec(t, conn, query)
-	defer os.Remove(directory + `x.txt`)
-	query = `load data infile '` + directory + `x.txt' into table t1`
-	execAssertError(t, conn, query, "Duplicate entry '300' for key 'PRIMARY'")
-	exec(t, conn, `delete from t1`)
-	exec(t, conn, query)
-	assertMatches(t, conn, `select c1,c2,c3 from t1`, `[[INT64(300) INT64(100) INT64(300)]]`)
-	query = `select * from t1 into dumpfile '` + directory + `x1.txt'`
-	exec(t, conn, query)
-	defer os.Remove(directory + `x1.txt`)
-	query = `select * from t1 into outfile '` + directory + `x2.txt' Fields terminated by ';' optionally enclosed by '"' escaped by '\t' lines terminated by '\n'`
-	exec(t, conn, query)
-	defer os.Remove(directory + `x2.txt`)
-	query = `load data infile '` + directory + `x2.txt' replace into table t1 Fields terminated by ';' optionally enclosed by '"' escaped by '\t' lines terminated by '\n'`
-	exec(t, conn, query)
-	assertMatches(t, conn, `select c1,c2,c3 from t1`, `[[INT64(300) INT64(100) INT64(300)]]`)
-}
-
-func TestEmptyStatement(t *testing.T) {
-	defer cluster.PanicHandler(t)
-	ctx := context.Background()
-	vtParams := mysql.ConnParams{
-		Host: "localhost",
-		Port: clusterInstance.VtgateMySQLPort,
-	}
-	conn, err := mysql.Connect(ctx, &vtParams)
-	require.Nil(t, err)
-	defer conn.Close()
-	defer exec(t, conn, `delete from t1`)
-	execAssertError(t, conn, " \t;", "Query was empty")
-	execMulti(t, conn, `insert into t1(c1, c2, c3, c4) values (300,100,300,'abc'); ;; insert into t1(c1, c2, c3, c4) values (301,101,301,'abcd');;`)
-	assertMatches(t, conn, `select c1,c2,c3 from t1`, `[[INT64(300) INT64(100) INT64(300)] [INT64(301) INT64(101) INT64(301)]]`)
-}
-
-func TestTopoDownServingQuery(t *testing.T) {
-	defer cluster.PanicHandler(t)
-	ctx := context.Background()
-	vtParams := mysql.ConnParams{
-		Host: "localhost",
-		Port: clusterInstance.VtgateMySQLPort,
-	}
-	conn, err := mysql.Connect(ctx, &vtParams)
-	require.Nil(t, err)
-	defer conn.Close()
-
-	defer exec(t, conn, `delete from t1`)
-
-	execMulti(t, conn, `insert into t1(c1, c2, c3, c4) values (300,100,300,'abc'); ;; insert into t1(c1, c2, c3, c4) values (301,101,301,'abcd');;`)
-	assertMatches(t, conn, `select c1,c2,c3 from t1`, `[[INT64(300) INT64(100) INT64(300)] [INT64(301) INT64(101) INT64(301)]]`)
-	clusterInstance.TopoProcess.TearDown(clusterInstance.Cell, clusterInstance.OriginalVTDATAROOT, clusterInstance.CurrentVTDATAROOT, true, *clusterInstance.TopoFlavorString())
-	time.Sleep(3 * time.Second)
-	assertMatches(t, conn, `select c1,c2,c3 from t1`, `[[INT64(300) INT64(100) INT64(300)] [INT64(301) INT64(101) INT64(301)]]`)
-}
-
-func TestInsertAllDefaults(t *testing.T) {
+func TestDBDDLPluginSync(t *testing.T) {
 	defer cluster.PanicHandler(t)
 	ctx := context.Background()
 	vtParams := mysql.ConnParams{
@@ -294,11 +89,28 @@ func TestInsertAllDefaults(t *testing.T) {
 	require.NoError(t, err)
 	defer conn.Close()
 
-	exec(t, conn, `insert into allDefaults () values ()`)
-	assertMatches(t, conn, `select * from allDefaults`, "[[INT64(1) NULL]]")
+	qr := exec(t, conn, `create database aaa`)
+	require.EqualValues(t, 1, qr.RowsAffected)
+
+	keyspace := &cluster.Keyspace{
+		Name: "aaa",
+	}
+	require.NoError(t,
+		clusterInstance.StartUnshardedKeyspace(*keyspace, 0, false),
+		"new database creation failed")
+
+	exec(t, conn, `use aaa`)
+
+	exec(t, conn, `create table t (id bigint primary key)`)
+	exec(t, conn, `insert into t(id) values (1),(2),(3),(4),(5)`)
+	assertMatches(t, conn, "select count(*) from t", `[[INT64(5)]]`)
+
+	exec(t, conn, `drop database aaa`)
+
+	execAssertError(t, conn, "select count(*) from t", `some error`)
 }
 
-func TestDDLUnsharded(t *testing.T) {
+func TestDBDDLPluginASync(t *testing.T) {
 	defer cluster.PanicHandler(t)
 	ctx := context.Background()
 	vtParams := mysql.ConnParams{
@@ -309,85 +121,27 @@ func TestDDLUnsharded(t *testing.T) {
 	require.NoError(t, err)
 	defer conn.Close()
 
-	exec(t, conn, `create table tempt1(c1 BIGINT NOT NULL,c2 BIGINT NOT NULL,c3 BIGINT,c4 varchar(100),PRIMARY KEY (c1), UNIQUE KEY (c2),UNIQUE KEY (c3), UNIQUE KEY (c4))`)
-	// Test that create view works and the output is as expected
-	exec(t, conn, `create view v1 as select * from tempt1`)
-	exec(t, conn, `insert into tempt1(c1, c2, c3, c4) values (300,100,300,'abc'),(30,10,30,'ac'),(3,0,3,'a')`)
-	assertMatches(t, conn, "select * from v1", `[[INT64(3) INT64(0) INT64(3) VARCHAR("a")] [INT64(30) INT64(10) INT64(30) VARCHAR("ac")] [INT64(300) INT64(100) INT64(300) VARCHAR("abc")]]`)
-	exec(t, conn, `drop view v1`)
-	exec(t, conn, `drop table tempt1`)
-	assertMatches(t, conn, "show tables", `[[VARCHAR("allDefaults")] [VARCHAR("t1")]]`)
-}
-
-func TestCallProcedure(t *testing.T) {
-	defer cluster.PanicHandler(t)
-	ctx := context.Background()
-	vtParams := mysql.ConnParams{
-		Host:   "localhost",
-		Port:   clusterInstance.VtgateMySQLPort,
-		Flags:  mysql.CapabilityClientMultiResults,
-		DbName: "@master",
-	}
-	time.Sleep(5 * time.Second)
-	conn, err := mysql.Connect(ctx, &vtParams)
-	require.NoError(t, err)
-	defer conn.Close()
-	qr := exec(t, conn, `CALL sp_insert()`)
+	qr := exec(t, conn, `create database aaa`)
 	require.EqualValues(t, 1, qr.RowsAffected)
 
-	_, err = conn.ExecuteFetch(`CALL sp_select()`, 1000, true)
-	require.Error(t, err)
-	require.Contains(t, err.Error(), "Multi-Resultset not supported in stored procedure")
+	go func() {
+		keyspace := &cluster.Keyspace{
+			Name: "aaa",
+		}
+		require.NoError(t,
+			clusterInstance.StartUnshardedKeyspace(*keyspace, 0, false),
+			"new database creation failed")
+	}()
 
-	_, err = conn.ExecuteFetch(`CALL sp_all()`, 1000, true)
-	require.Error(t, err)
-	require.Contains(t, err.Error(), "Multi-Resultset not supported in stored procedure")
+	exec(t, conn, `use aaa`)
 
-	qr = exec(t, conn, `CALL sp_delete()`)
-	require.GreaterOrEqual(t, 1, int(qr.RowsAffected))
+	exec(t, conn, `create table t (id bigint primary key)`)
+	exec(t, conn, `insert into t(id) values (1),(2),(3),(4),(5)`)
+	assertMatches(t, conn, "select count(*) from t", `[[INT64(5)]]`)
 
-	qr = exec(t, conn, `CALL sp_multi_dml()`)
-	require.EqualValues(t, 1, qr.RowsAffected)
+	exec(t, conn, `drop database aaa`)
 
-	qr = exec(t, conn, `CALL sp_variable()`)
-	require.EqualValues(t, 1, qr.RowsAffected)
-
-	qr = exec(t, conn, `CALL in_parameter(42)`)
-	require.EqualValues(t, 1, qr.RowsAffected)
-
-	_ = exec(t, conn, `SET @foo = 123`)
-	qr = exec(t, conn, `CALL in_parameter(@foo)`)
-	require.EqualValues(t, 1, qr.RowsAffected)
-	qr = exec(t, conn, "select * from allDefaults where id = 123")
-	assert.NotEmpty(t, qr.Rows)
-
-	_, err = conn.ExecuteFetch(`CALL out_parameter(@foo)`, 100, true)
-	require.Error(t, err)
-	require.Contains(t, err.Error(), "OUT and INOUT parameters are not supported")
-}
-
-func TestTempTable(t *testing.T) {
-	defer cluster.PanicHandler(t)
-	ctx := context.Background()
-	vtParams := mysql.ConnParams{
-		Host: "localhost",
-		Port: clusterInstance.VtgateMySQLPort,
-	}
-	conn1, err := mysql.Connect(ctx, &vtParams)
-	require.NoError(t, err)
-	defer conn1.Close()
-
-	_ = exec(t, conn1, `create temporary table temp_t(id bigint primary key)`)
-	_ = exec(t, conn1, `insert into temp_t(id) values (1),(2),(3)`)
-	assertMatches(t, conn1, `select id from temp_t order by id`, `[[INT64(1)] [INT64(2)] [INT64(3)]]`)
-	assertMatches(t, conn1, `select count(table_id) from information_schema.innodb_temp_table_info`, `[[INT64(1)]]`)
-
-	conn2, err := mysql.Connect(ctx, &vtParams)
-	require.NoError(t, err)
-	defer conn2.Close()
-
-	assertMatches(t, conn2, `select count(table_id) from information_schema.innodb_temp_table_info`, `[[INT64(1)]]`)
-	execAssertError(t, conn2, `show create table temp_t`, `Table 'vt_customer.temp_t' doesn't exist (errno 1146) (sqlstate 42S02)`)
+	execAssertError(t, conn, "select count(*) from t", `some error`)
 }
 
 func exec(t *testing.T, conn *mysql.Conn, query string) *sqltypes.Result {
@@ -395,20 +149,6 @@ func exec(t *testing.T, conn *mysql.Conn, query string) *sqltypes.Result {
 	qr, err := conn.ExecuteFetch(query, 1000, true)
 	require.NoError(t, err)
 	return qr
-}
-
-func execMulti(t *testing.T, conn *mysql.Conn, query string) []*sqltypes.Result {
-	t.Helper()
-	var res []*sqltypes.Result
-	qr, more, err := conn.ExecuteFetchMulti(query, 1000, true)
-	res = append(res, qr)
-	require.NoError(t, err)
-	for more == true {
-		qr, more, _, err = conn.ReadQueryResult(1000, true)
-		require.NoError(t, err)
-		res = append(res, qr)
-	}
-	return res
 }
 
 func execAssertError(t *testing.T, conn *mysql.Conn, query string, errorString string) {
