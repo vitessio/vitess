6b3608c332c94a19171a70b91b997dfa21149caa
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2021-03-04 09:12:22 +0200

    fixed logic
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- tools/check_codeowners.sh
+++ tools/check_codeowners.sh
@@ -2,14 +2,17 @@
 
 find . -type f | grep -v '^./.git' | while read -r f ; do git check-ignore "$f" || echo "$f" ; done | while read -r f ; do
   # A file not ignored by .gitignore. Let's check if it's covered by CODEOWNERS
+  # Get rid of leading dot (e.g. './web/vtctld2/src/app/index.ts' => '/web/vtctld2/src/app/index.ts')
   f="${f#"."}"
-  grep -E "^/" .github/CODEOWNERS | awk '{print $1}' | while read -r p ; do
+  grep -E "^/" .github/CODEOWNERS | awk '{print $1}' | while read -r codeowner_path ; do
     case "$f" in
-      $p) ;;       # Perfect math. All good.
-      $p/*) ;;     # Prefix (path) match. All good.
-      *) exit 1 ;; # No match: an error
+      $codeowner_path) exit 3 ;;   # Perfect match. All good.
+      $codeowner_path/*) exit 3 ;; # Prefix (path) match. All good.
+      *) ;;                        # No match: keep looping on next codeowner path
     esac
-  done && exit 0
-  # If we're stil here, then that means no CODEOWNER was found for file $f
-  echo "$f" 
+  done
+  if [ "$?" != "3" ] ; then
+    # No indication for CODEOWNER match for file $f
+    echo "$f"
+  fi
 done
