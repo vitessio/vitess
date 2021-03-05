cea7d14c99131cad54a9982eaeff5e1cf6320b6c
Vicent Marti <vmg@strn.cat>
2021-03-05 16:29:29 +0100

    sqlparser: use testify for tests
    Signed-off-by: Vicent Marti <vmg@strn.cat>

--- go/vt/sqlparser/keywords_test.go
+++ go/vt/sqlparser/keywords_test.go
@@ -1,15 +1,15 @@
 package sqlparser
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestKeywordTable(t *testing.T) {
 	for _, kw := range keywords {
 		lookup, ok := keywordLookupTable.LookupString(kw.name)
-		if !ok {
-			t.Fatalf("keyword %q failed to match", kw.name)
-		}
-		if lookup != kw.id {
-			t.Fatalf("keyword %q matched to %d (expected %d)", kw.name, lookup, kw.id)
-		}
+		require.Truef(t, ok, "keyword %q failed to match", kw.name)
+		require.Equalf(t, lookup, kw.id, "keyword %q matched to %d (expected %d)", kw.name, lookup, kw.id)
 	}
 }
