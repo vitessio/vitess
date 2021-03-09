4ef69a162e791dea4176a586a704d4ec195e5d42
Harshit Gangal <harshit@planetscale.com>
2021-03-09 19:32:00 +0530

    make e2e test to run faster
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/test/endtoend/vtgate/unsharded/main_test.go
+++ go/test/endtoend/vtgate/unsharded/main_test.go
@@ -114,6 +114,7 @@ func TestMain(m *testing.M) {
 			SchemaSQL: SchemaSQL,
 			VSchema:   VSchema,
 		}
+		clusterInstance.VtTabletExtraArgs = []string{"-queryserver-config-transaction-timeout", "3"}
 		if err := clusterInstance.StartUnshardedKeyspace(*Keyspace, 0, false); err != nil {
 			return 1
 		}
@@ -232,7 +233,7 @@ func TestReservedConnDML(t *testing.T) {
 	exec(t, conn, `insert into allDefaults () values ()`)
 	exec(t, conn, `commit`)
 
-	time.Sleep(35 * time.Second)
+	time.Sleep(6 * time.Second)
 
 	exec(t, conn, `begin`)
 	exec(t, conn, `insert into allDefaults () values ()`)
