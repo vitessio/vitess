aec340f02cc33c38f47f0172915fc7b1d904d158
Rohit Nayak <rohit@planetscale.com>
2021-03-09 10:17:14 +0100

    Don't run json test in CI for percona/mariadb flavors, which either don't have json support or have not yet been enabled
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>

--- go/vt/vttablet/tabletmanager/vreplication/vplayer_flaky_test.go
+++ go/vt/vttablet/tabletmanager/vreplication/vplayer_flaky_test.go
@@ -1271,16 +1271,9 @@ func TestPlayerRowMove(t *testing.T) {
 func TestPlayerTypes(t *testing.T) {
 	log.Errorf("TestPlayerTypes: flavor is %s", env.Flavor)
 	enableJSONColumnTesting := true
-	// some unit test
-	supportedFlavorsForJSONColumnTesting := []string{"mysql57", "mysql80"}
-	for _, flavor := range supportedFlavorsForJSONColumnTesting {
-		if strings.EqualFold(env.Flavor, flavor) {
-			log.Warningf("Enabling testing JSON columns for flavor: %s", env.Flavor)
-			enableJSONColumnTesting = true
-			break
-		} else {
-			log.Warningf("Not testing JSON columns for flavor: %s", env.Flavor)
-		}
+	flavor := strings.ToLower(env.Flavor)
+	if strings.Contains(flavor, "percona") || strings.Contains(flavor, "mariadb") {
+		enableJSONColumnTesting = false
 	}
 	defer deleteTablet(addTablet(100))
 
