72f77c7a53e0b75464da103ebc94cf8a170461eb
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2021-03-10 12:34:05 +0200

    parser support for ALTER VITESS_MIGRATION
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/sqlparser/ast.go
+++ go/vt/sqlparser/ast.go
@@ -418,6 +418,15 @@ type (
 		UUID string
 	}
 
+	// AlterMigrationType represents the type of operation in an ALTER VITESS_MIGRATION statement
+	AlterMigrationType int8
+
+	// AlterMigration represents a ALTER VITESS_MIGRATION statement
+	AlterMigration struct {
+		Type AlterMigrationType
+		UUID string
+	}
+
 	// AlterTable represents a ALTER TABLE statement.
 	AlterTable struct {
 		Table         TableName
@@ -606,6 +615,7 @@ func (*LockTables) iStatement()        {}
 func (*UnlockTables) iStatement()      {}
 func (*AlterTable) iStatement()        {}
 func (*AlterVschema) iStatement()      {}
+func (*AlterMigration) iStatement()    {}
 func (*RevertMigration) iStatement()   {}
 func (*DropTable) iStatement()         {}
 func (*DropView) iStatement()          {}
@@ -2109,6 +2119,11 @@ func (node *AlterVschema) Format(buf *TrackedBuffer) {
 	default:
 		buf.astPrintf(node, "%s table %v", node.Action.ToString(), node.Table)
 	}
+}
+
+// Format formats the node.
+func (node *AlterMigration) Format(buf *TrackedBuffer) {
+	buf.astPrintf(node, "alter vitess_migration '%s'", node.UUID) // TODO(shlomi)
 }
 
 // Format formats the node.
