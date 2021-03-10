16633d4acbaf35817bb94990b0dc1bbf5b7e05e7
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2021-03-10 15:53:16 +0200

    Format; test parsing of ALTER VITESS_MIGRATION statements
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/sqlparser/parse_test.go
+++ go/vt/sqlparser/parse_test.go
@@ -1535,6 +1535,14 @@ var (
 	}, {
 		input: "revert vitess_migration '9748c3b7_7fdb_11eb_ac2c_f875a4d24e90'",
 	}, {
+		input: "alter vitess_migration '9748c3b7_7fdb_11eb_ac2c_f875a4d24e90' retry",
+	}, {
+		input: "alter vitess_migration '9748c3b7_7fdb_11eb_ac2c_f875a4d24e90' complete",
+	}, {
+		input: "alter vitess_migration '9748c3b7_7fdb_11eb_ac2c_f875a4d24e90' cancel",
+	}, {
+		input: "alter vitess_migration cancel all",
+	}, {
 		input:  "show warnings",
 		output: "show warnings",
 	}, {
