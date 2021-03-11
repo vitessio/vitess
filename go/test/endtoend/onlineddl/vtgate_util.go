5be1535e619175c644bd69667a36b237756e2d99
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2021-03-11 07:13:01 +0200

    using ALTER VITESS_MIGRATION ... CANCEL ALL in endtoend/onlineddl/
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/test/endtoend/onlineddl/vtgate_util.go
+++ go/test/endtoend/onlineddl/vtgate_util.go
@@ -75,7 +75,7 @@ func CheckCancelMigration(t *testing.T, vtParams *mysql.ConnParams, shards []clu
 
 // CheckCancelAllMigrations cancels all pending migrations and expect number of affected rows
 func CheckCancelAllMigrations(t *testing.T, vtParams *mysql.ConnParams, expectCount int) {
-	cancelQuery := fmt.Sprintf("alter vitess_migration cancel all")
+	cancelQuery := "alter vitess_migration cancel all"
 	r := VtgateExecQuery(t, vtParams, cancelQuery, "")
 
 	assert.Equal(t, expectCount, int(r.RowsAffected))
