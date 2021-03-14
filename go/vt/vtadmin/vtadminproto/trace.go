83e3e0614b043fad1e929f8a7b8fada8e786352a
Andrew Mason <amason@slack-corp.com>
2021-03-13 20:25:01 -0500

    WIP: add spans, add error wraps
    Signed-off-by: Andrew Mason <amason@slack-corp.com>

--- go/vt/vtadmin/vtadminproto/trace.go
+++ go/vt/vtadmin/vtadminproto/trace.go
@@ -27,3 +27,15 @@ func AnnotateClusterSpan(c *vtadminpb.Cluster, span trace.Span) {
 	span.Annotate("cluster_id", c.Id)
 	span.Annotate("cluster_name", c.Name)
 }
+
+// AnnotateSpanWithGetSchemaTableSizeOptions adds the aggregate_table_sizes and
+// include_non_serving_shards_in_table_sizes to a span. It is a noop if the
+// size options object is nil.
+func AnnotateSpanWithGetSchemaTableSizeOptions(opts *vtadminpb.GetSchemaTableSizeOptions, span trace.Span) {
+	if opts == nil {
+		return
+	}
+
+	span.Annotate("aggregate_table_sizes", opts.AggregateSizes)
+	span.Annotate("include_non_serving_shards_in_table_sizes", opts.IncludeNonServingShards)
+}
