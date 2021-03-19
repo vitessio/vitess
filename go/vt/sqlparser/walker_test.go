ff8399dc1f65e085487af722ca0988b275a33239
Andres Taylor <andres@planetscale.com>
2021-03-19 15:51:24 +0100

    add benchmark
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/vt/sqlparser/walker_test.go
+++ go/vt/sqlparser/walker_test.go
@@ -38,3 +38,22 @@ func BenchmarkWalkLargeExpression(b *testing.B) {
 		})
 	}
 }
+
+func BenchmarkRewriteLargeExpression(b *testing.B) {
+	for i := 0; i < 10; i++ {
+		b.Run(fmt.Sprintf("%d", i), func(b *testing.B) {
+			exp := newGenerator(int64(i*100), 5).expression()
+			count := 0
+			for i := 0; i < b.N; i++ {
+				_, err := Rewrite(exp, func(_ *Cursor) bool {
+					count++
+					return true
+				}, func(_ *Cursor) bool {
+					count--
+					return true
+				})
+				require.NoError(b, err)
+			}
+		})
+	}
+}
