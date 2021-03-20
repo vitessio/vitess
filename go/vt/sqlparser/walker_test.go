35cddfe214c60c3764b4c48ab348b17ede49a2ca
Andres Taylor <andres@planetscale.com>
2021-03-20 11:43:34 +0100

    update make target
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/vt/sqlparser/walker_test.go
+++ go/vt/sqlparser/walker_test.go
@@ -40,9 +40,9 @@ func BenchmarkWalkLargeExpression(b *testing.B) {
 }
 
 func BenchmarkRewriteLargeExpression(b *testing.B) {
-	for i := 0; i < 10; i++ {
+	for i := 1; i < 7; i++ {
 		b.Run(fmt.Sprintf("%d", i), func(b *testing.B) {
-			exp := newGenerator(int64(i*100), 5).expression()
+			exp := newGenerator(int64(i*100), i).expression()
 			count := 0
 			for i := 0; i < b.N; i++ {
 				_, err := Rewrite(exp, func(_ *Cursor) bool {
