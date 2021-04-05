a9155b05649b00888deb6ebc6edc22361e122184
Rohit Nayak <rohit@planetscale.com>
2021-04-05 12:09:06 +0200

    Stop on sequence mismatch. Don't update tablet picker message since it overwrites the sequence error messages
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>

--- go/vt/vttablet/tabletmanager/vreplication/vplayer.go
+++ go/vt/vttablet/tabletmanager/vreplication/vplayer.go
@@ -151,10 +151,11 @@ func (vp *vplayer) fetchAndApply(ctx context.Context) (err error) {
 					vp.eventSequenceNumber++
 				}
 				if ev.SequenceNumber != vp.eventSequenceNumber {
-					err := fmt.Errorf("@@@ incorrect sequence number, got %d, expected %d, vevent %+v",
+					err := fmt.Errorf("Error: edbg: incorrect sequence number, got %d, expected %d, vevent %+v",
 						ev.SequenceNumber, vp.eventSequenceNumber, ev)
 					log.Error(err)
-					vp.vr.setState(binlogplayer.BlpError, err.Error())
+					vp.vr.setState(binlogplayer.BlpStopped, err.Error())
+					return err
 				}
 			}
 			return relay.Send(events)
@@ -370,10 +371,11 @@ func (vp *vplayer) applyEvents(ctx context.Context, relay *relayLog) error {
 					vp.relaySequenceNumber++
 				}
 				if event.SequenceNumber != vp.relaySequenceNumber {
-					err := fmt.Errorf("@@@ incorrect relay sequence number, got %d, expected %d, vevent %+v",
+					err := fmt.Errorf("Error: edbg: incorrect relay sequence number, got %d, expected %d, vevent %+v",
 						event.SequenceNumber, vp.relaySequenceNumber, event)
 					log.Error(err)
-					vp.vr.setState(binlogplayer.BlpError, err.Error())
+					vp.vr.setState(binlogplayer.BlpStopped, err.Error())
+					return err
 				}
 
 				if event.Timestamp != 0 {
