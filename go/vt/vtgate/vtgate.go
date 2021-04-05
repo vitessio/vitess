63fac67d878799b0aa0c6502c44e8fc66bec9847
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2021-04-05 10:52:48 +0300

    utilizing DDLStrategySetting to validate @@ddl_strategy
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/vtgate/vtgate.go
+++ go/vt/vtgate/vtgate.go
@@ -179,7 +179,7 @@ func Init(ctx context.Context, serv srvtopo.Server, cell string, tabletTypesToWa
 		}
 	}
 
-	if _, _, err := schema.ParseDDLStrategy(*defaultDDLStrategy); err != nil {
+	if _, err := schema.ParseDDLStrategy(*defaultDDLStrategy); err != nil {
 		log.Fatalf("Invalid value for -ddl_strategy: %v", err.Error())
 	}
 	tc := NewTxConn(gw, getTxMode())
