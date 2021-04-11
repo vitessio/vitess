b908085747d076976a2059d41b8f6e89b774999d
Sara Bee <855595+doeg@users.noreply.github.com>
2021-04-11 19:20:40 -0400

    Rewrite useTableDefinitions hook as getTableDefinitions util
    Signed-off-by: Sara Bee <855595+doeg@users.noreply.github.com>

--- web/vtadmin/src/hooks/api.ts
+++ web/vtadmin/src/hooks/api.ts
@@ -86,44 +86,6 @@ export const useWorkflows = (...args: Parameters<typeof useWorkflowsResponse>) =
     return { data: workflows, ...query };
 };
 
-export interface TableDefinition {
-    cluster?: pb.Schema['cluster'];
-    keyspace?: pb.Schema['keyspace'];
-    // The [0] index is a typescript quirk to infer the type of
-    // an entry in an array, and therefore the type of ALL entries
-    // in the array (not just the first one).
-    tableDefinition?: pb.Schema['table_definitions'][0];
-}
-
-/**
- * useTableDefinitions is a helper hook for when a flattened list
- * of table definitions (across all keyspaces and clusters) is required,
- * instead of the default vtadmin-api/Vitess grouping of schemas by keyspace.
- *
- * Under the hood, this calls the useSchemas hook and therefore uses
- * the same query cache.
- */
-export const useTableDefinitions = (...args: Parameters<typeof useSchemas>) => {
-    const { data, ...query } = useSchemas(...args);
-
-    if (!Array.isArray(data)) {
-        return { data, ...query };
-    }
-
-    const tds = data.reduce((acc: TableDefinition[], schema: pb.Schema) => {
-        (schema.table_definitions || []).forEach((td) => {
-            acc.push({
-                cluster: schema.cluster,
-                keyspace: schema.keyspace,
-                tableDefinition: td,
-            });
-        });
-        return acc;
-    }, []);
-
-    return { ...query, data: tds };
-};
-
 /**
  * useSchema is a query hook that fetches a single schema for the given parameters.
  */
