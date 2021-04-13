49c1b6898ee2c43caca93df7277f7a2240c419a9
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2021-04-13 12:00:59 +0300

    remove duplicate flag entry
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/test/endtoend/onlineddl/ghost/onlineddl_ghost_test.go
+++ go/test/endtoend/onlineddl/ghost/onlineddl_ghost_test.go
@@ -157,7 +157,6 @@ func TestMain(m *testing.M) {
 			"-heartbeat_enable",
 			"-heartbeat_interval", "250ms",
 			"-migration_check_interval", "5s",
-			"-migration_check_interval", "5s",
 			"-gh-ost-path", os.Getenv("VITESS_ENDTOEND_GH_OST_PATH"), // leave env variable empty/unset to get the default behavior. Override in Mac.
 		}
 		clusterInstance.VtGateExtraArgs = []string{
