8ef0b4acf0e1a038162198010d374dcb99f4fc6d
AdamKorcz <adam@adalogics.com>
2021-04-20 21:45:26 +0100

    Small fixes
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- go/mysql/mysql_fuzzer.go
+++ go/mysql/mysql_fuzzer.go
@@ -31,7 +31,6 @@ import (
 
 	"vitess.io/vitess/go/sqltypes"
 	querypb "vitess.io/vitess/go/vt/proto/query"
-	"vitess.io/vitess/go/vt/sqlparser"
 	"vitess.io/vitess/go/vt/tlstest"
 	"vitess.io/vitess/go/vt/vttls"
 )
@@ -302,23 +301,29 @@ func (th *fuzzTestHandler) WarningCount(c *Conn) uint16 {
 	return th.warnings
 }
 
+func (c *Conn) writeFuzzedPacket(packet []byte) {
+	c.sequence = 0
+	data, pos := c.startEphemeralPacketWithHeader(len(packet) + 1)
+	copy(data[pos:], packet)
+	_ = c.writeEphemeralPacket()
+}
+
 func FuzzTLSServer(data []byte) int {
+	if len(data) < 40 {
+		return -1
+	}
 	// totalQueries is the number of queries the fuzzer
 	// makes in each fuzz iteration
 	totalQueries := 20
-	var queries []string
+	var queries [][]byte
 	c := gofuzzheaders.NewConsumer(data)
 	for i := 0; i < totalQueries; i++ {
-		query, err := c.GetString()
+		query, err := c.GetBytes()
 		if err != nil {
 			return -1
 		}
-
-		// We parse each query now to exit if the queries
-		// are invalid
-		_, err = sqlparser.Parse(query)
-		if err != nil {
-			return -1
+		if len(query) < 40 {
+			continue
 		}
 		queries = append(queries, query)
 	}
@@ -379,7 +384,7 @@ func FuzzTLSServer(data []byte) int {
 	}
 
 	for i := 0; i < len(queries); i++ {
-		_, _ = conn.ExecuteFetch(queries[i], 1000, true)
+		conn.writeFuzzedPacket(queries[i])
 	}
 	return 1
 }
