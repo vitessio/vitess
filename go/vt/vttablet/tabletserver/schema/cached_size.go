c4f81780799b621193368a5246ff878d28670046
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2021-04-27 11:26:44 +0300

    indentation, sizegen
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/vttablet/tabletserver/schema/cached_size.go
+++ go/vt/vttablet/tabletserver/schema/cached_size.go
@@ -50,7 +50,7 @@ func (cached *Table) CachedSize(alloc bool) int64 {
 	}
 	size := int64(0)
 	if alloc {
-		size += int64(104)
+		size += int64(112)
 	}
 	// field Name vitess.io/vitess/go/vt/sqlparser.TableIdent
 	size += cached.Name.CachedSize(false)
