044b982b5bb788ebfcccb8553585b407bbb787aa
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2021-04-27 11:42:16 +0300

    fix unit test
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/binlog/binlogplayer/binlog_player_test.go
+++ go/vt/binlog/binlogplayer/binlog_player_test.go
@@ -358,7 +358,7 @@ func TestCreateVReplicationTables(t *testing.T) {
 func TestUpdateVReplicationPos(t *testing.T) {
 	gtid := mysql.MustParseGTID("MariaDB", "0-1-8283")
 	want := "update _vt.vreplication " +
-		"set pos='MariaDB/0-1-8283', time_updated=88822, message='' " +
+		"set pos='MariaDB/0-1-8283', time_updated=88822, rows_copied=0, message='' " +
 		"where id=78522"
 
 	got := GenerateUpdatePos(78522, mysql.Position{GTIDSet: gtid.GTIDSet()}, 88822, 0, 0)
