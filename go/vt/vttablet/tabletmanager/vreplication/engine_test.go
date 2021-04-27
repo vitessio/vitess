0b1968fb342e3ca08738344b564af3664b67fa09
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2021-04-27 16:45:35 +0300

    fix engine_test
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/vttablet/tabletmanager/vreplication/engine_test.go
+++ go/vt/vttablet/tabletmanager/vreplication/engine_test.go
@@ -498,6 +498,7 @@ func TestCreateDBAndTable(t *testing.T) {
 		dbClient.ExpectRequestRE("ALTER TABLE _vt.vreplication ADD COLUMN db_name.*", &sqltypes.Result{}, nil)
 		dbClient.ExpectRequestRE("ALTER TABLE _vt.vreplication MODIFY source.*", &sqltypes.Result{}, nil)
 		dbClient.ExpectRequestRE("ALTER TABLE _vt.vreplication ADD KEY.*", &sqltypes.Result{}, nil)
+		dbClient.ExpectRequestRE("ALTER TABLE _vt.vreplication ADD COLUMN rows_copied.*", &sqltypes.Result{}, nil)
 		dbClient.ExpectRequestRE("create table if not exists _vt.resharding_journal.*", &sqltypes.Result{}, nil)
 		dbClient.ExpectRequestRE("create table if not exists _vt.copy_state.*", &sqltypes.Result{}, nil)
 	}
