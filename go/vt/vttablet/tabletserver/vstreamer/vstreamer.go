7abbea9ef1101e950ff3e2694b4dae8e07a184aa
Rohit Nayak <rohit@planetscale.com>
2021-04-27 19:42:30 +0200

    Pad binlog values for binary() columns to match the value returned by a select query. This also ensures that if such columns are used as sharding keys we get the same keyspace_id
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>
    
    Signed-off-by: Rafael Chacon <rafael@slack-corp.com>

--- go/vt/vttablet/tabletserver/vstreamer/vstreamer.go
+++ go/vt/vttablet/tabletserver/vstreamer/vstreamer.go
@@ -862,7 +862,9 @@ func (vs *vstreamer) extractRowAndFilter(plan *streamerPlan, data []byte, dataCo
 			return false, nil, err
 		}
 		pos += l
+
 		values[colNum] = value
+
 		valueIndex++
 	}
 	return plan.filter(values)
