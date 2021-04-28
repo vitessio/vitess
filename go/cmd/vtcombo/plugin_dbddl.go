0eea0109e322669f35873e33f4e30fd8e531e798
Harshit Gangal <harshit@planetscale.com>
2021-04-28 18:44:46 +0530

    use context from the dbddl engine call to be used in create and drop database in the plugin
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/cmd/vtcombo/plugin_dbddl.go
+++ go/cmd/vtcombo/plugin_dbddl.go
@@ -25,26 +25,26 @@ import (
 	vttestpb "vitess.io/vitess/go/vt/proto/vttest"
 )
 
-var globalCreateDb func(ks *vttestpb.Keyspace) error
-var globalDropDb func(ksName string) error
+var globalCreateDb func(ctx context.Context, ks *vttestpb.Keyspace) error
+var globalDropDb func(ctx context.Context, ksName string) error
 
 // DBDDL doesn't need to store any state - we use the global variables above instead
 type DBDDL struct{}
 
 // CreateDatabase implements the engine.DBDDLPlugin interface
-func (plugin *DBDDL) CreateDatabase(_ context.Context, name string) error {
+func (plugin *DBDDL) CreateDatabase(ctx context.Context, name string) error {
 	ks := &vttestpb.Keyspace{
 		Name: name,
 		Shards: []*vttestpb.Shard{{
 			Name: "0",
 		}},
 	}
-	return globalCreateDb(ks)
+	return globalCreateDb(ctx, ks)
 }
 
 // DropDatabase implements the engine.DBDDLPlugin interface
-func (plugin *DBDDL) DropDatabase(_ context.Context, name string) error {
-	return globalDropDb(name)
+func (plugin *DBDDL) DropDatabase(ctx context.Context, name string) error {
+	return globalDropDb(ctx, name)
 }
 
 func init() {
