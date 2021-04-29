3fa925896b5ecd816a7bc8bcee583c7ad2e01380
Sara Bee <855595+doeg@users.noreply.github.com>
2021-04-29 19:49:42 -0400

    [vtadmin-web] Add filtering + shard counts/status to Keyspaces view (#7991)
    Signed-off-by: Sara Bee <855595+doeg@users.noreply.github.com>

--- web/vtadmin/src/components/routes/Keyspaces.tsx
+++ web/vtadmin/src/components/routes/Keyspaces.tsx
@@ -15,32 +15,81 @@
  */
 import { orderBy } from 'lodash-es';
 import * as React from 'react';
+
+import style from './Keyspaces.module.scss';
 import { useKeyspaces } from '../../hooks/api';
 import { useDocumentTitle } from '../../hooks/useDocumentTitle';
-import { vtadmin as pb } from '../../proto/vtadmin';
+import { useSyncedURLParam } from '../../hooks/useSyncedURLParam';
 import { DataCell } from '../dataTable/DataCell';
 import { DataTable } from '../dataTable/DataTable';
+import { Button } from '../Button';
+import { Icons } from '../Icon';
+import { TextInput } from '../TextInput';
+import { Pip } from '../pips/Pip';
+import { filterNouns } from '../../util/filterNouns';
+import { getShardsByState } from '../../util/keyspaces';
 
 export const Keyspaces = () => {
     useDocumentTitle('Keyspaces');
+    const { value: filter, updateValue: updateFilter } = useSyncedURLParam('filter');
+
     const { data } = useKeyspaces();
 
-    const rows = React.useMemo(() => {
-        return orderBy(data, ['cluster.name', 'keyspace.name']);
-    }, [data]);
+    const ksRows = React.useMemo(() => {
+        const mapped = (data || []).map((k) => {
+            const shardsByState = getShardsByState(k);
 
-    const renderRows = (rows: pb.Keyspace[]) =>
-        rows.map((keyspace, idx) => (
+            return {
+                clusterID: k.cluster?.id,
+                cluster: k.cluster?.name,
+                name: k.keyspace?.name,
+                servingShards: shardsByState.serving.length,
+                nonservingShards: shardsByState.nonserving.length,
+            };
+        });
+        const filtered = filterNouns(filter, mapped);
+        return orderBy(filtered, ['cluster', 'name']);
+    }, [data, filter]);
+
+    const renderRows = (rows: typeof ksRows) =>
+        rows.map((row, idx) => (
             <tr key={idx}>
-                <DataCell>{keyspace.cluster?.name}</DataCell>
-                <DataCell>{keyspace.keyspace?.name}</DataCell>
+                <DataCell>
+                    <div className="font-weight-bold">{row.name}</div>
+                    <div className="font-size-small text-color-secondary">{row.cluster}</div>
+                </DataCell>
+                <DataCell>
+                    {!!row.servingShards && (
+                        <div>
+                            <Pip state="success" /> {row.servingShards} {row.servingShards === 1 ? 'shard' : 'shards'}
+                        </div>
+                    )}
+                    {!!row.nonservingShards && (
+                        <div className="font-weight-bold">
+                            <Pip state="danger" /> {row.nonservingShards}{' '}
+                            {row.nonservingShards === 1 ? 'shard' : 'shards'} not serving
+                        </div>
+                    )}
+                </DataCell>
             </tr>
         ));
 
     return (
         <div className="max-width-content">
             <h1>Keyspaces</h1>
-            <DataTable columns={['Cluster', 'Keyspace']} data={rows} renderRows={renderRows} />
+            <div className={style.controls}>
+                <TextInput
+                    autoFocus
+                    iconLeft={Icons.search}
+                    onChange={(e) => updateFilter(e.target.value)}
+                    placeholder="Filter keyspaces"
+                    value={filter || ''}
+                />
+                <Button disabled={!filter} onClick={() => updateFilter('')} secondary>
+                    Clear filters
+                </Button>
+            </div>
+            <DataTable columns={['Keyspace', 'Shards']} data={ksRows} renderRows={renderRows} />
         </div>
     );
 };
