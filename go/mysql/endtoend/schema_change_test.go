9e1ff351b878a356faea96164a6515d8ab97b47f
Harshit Gangal <harshit@planetscale.com>
2021-04-30 19:01:01 +0530

    added schema change detection and schema copy to health streamer
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/mysql/endtoend/schema_change_test.go
+++ go/mysql/endtoend/schema_change_test.go
@@ -28,66 +28,9 @@ import (
 var ctx = context.Background()
 
 const (
-	createSchemaCopyTable = `
-CREATE TABLE _vt.schemacopy (
-	table_schema varchar(64) NOT NULL,
-	table_name varchar(64) NOT NULL,
-	column_name varchar(64) NOT NULL,
-	ordinal_position bigint(21) unsigned NOT NULL,
-	character_set_name varchar(32) DEFAULT NULL,
-	collation_name varchar(32) DEFAULT NULL,
-	column_type longtext NOT NULL,
-	column_key varchar(3) NOT NULL,
-	PRIMARY KEY (table_schema, table_name, ordinal_position)
-)`
-	createDb = `create database if not exists _vt`
-
-	insertIntoSchemaCopy = `insert _vt.schemacopy 
-select table_schema, table_name, column_name, ordinal_position, character_set_name, collation_name, column_type, column_key 
-from information_schema.columns 
-where table_schema = "vttest"`
-
-	cleanSchemaCopy = `delete from _vt.schemacopy`
-	dropTestTable   = `drop table if exists product`
-
+	createDb        = `create database if not exists _vt`
 	createUserTable = `create table vttest.product (id bigint(20) primary key, name char(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci, created bigint(20))`
-
-	detectNewColumns = `
-select 1
-from information_schema.columns as ISC
-	 left join _vt.schemacopy as c on 
-		ISC.table_name = c.table_name and 
-		ISC.table_schema=c.table_schema and 
-		ISC.ordinal_position = c.ordinal_position
-where ISC.table_schema = "vttest" AND c.table_schema is null
-`
-
-	detectChangeColumns = `
-select 1
-from information_schema.columns as ISC
-	  join _vt.schemacopy as c on 
-		ISC.table_name = c.table_name and 
-		ISC.table_schema=c.table_schema and 
-		ISC.ordinal_position = c.ordinal_position
-where ISC.table_schema = "vttest" 
-	AND (not(c.column_name <=> ISC.column_name) 
-	OR not(ISC.character_set_name <=> c.character_set_name) 
-	OR not(ISC.collation_name <=> c.collation_name) 
-	OR not(ISC.column_type <=> c.column_type) 
-	OR not(ISC.column_key <=> c.column_key))
-`
-
-	detectRemoveColumns = `
-select 1
-from information_schema.columns as ISC
-	  right join _vt.schemacopy as c on 
-		ISC.table_name = c.table_name and 
-		ISC.table_schema=c.table_schema and 
-		ISC.ordinal_position = c.ordinal_position
-where c.table_schema = "vttest" AND ISC.table_schema is null
-`
-
-	detectChange = detectChangeColumns + " UNION " + detectNewColumns + " UNION " + detectRemoveColumns
+	dropTestTable   = `drop table if exists product`
 )
 
 func TestChangeSchemaIsNoticed(t *testing.T) {
@@ -97,7 +40,7 @@ func TestChangeSchemaIsNoticed(t *testing.T) {
 
 	_, err = conn.ExecuteFetch(createDb, 1000, true)
 	require.NoError(t, err)
-	_, err = conn.ExecuteFetch(createSchemaCopyTable, 1000, true)
+	_, err = conn.ExecuteFetch(mysql.CreateSchemaCopyTable, 1000, true)
 	require.NoError(t, err)
 
 	tests := []struct {
@@ -141,18 +84,18 @@ func TestChangeSchemaIsNoticed(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			// reset schemacopy
-			_, err := conn.ExecuteFetch(cleanSchemaCopy, 1000, true)
+			_, err := conn.ExecuteFetch(mysql.ClearSchemaCopy, 1000, true)
 			require.NoError(t, err)
 			_, err = conn.ExecuteFetch(dropTestTable, 1000, true)
 			require.NoError(t, err)
 			_, err = conn.ExecuteFetch(createUserTable, 1000, true)
 			require.NoError(t, err)
-			rs, err := conn.ExecuteFetch(insertIntoSchemaCopy, 1000, true)
+			rs, err := conn.ExecuteFetch(mysql.InsertIntoSchemaCopy, 1000, true)
 			require.NoError(t, err)
 			require.NotZero(t, rs.RowsAffected)
 
 			// make sure no changes are detected
-			rs, err = conn.ExecuteFetch(detectChange, 1000, true)
+			rs, err = conn.ExecuteFetch(mysql.DetectSchemaChange, 1000, true)
 			require.NoError(t, err)
 			require.Empty(t, rs.Rows)
 
@@ -161,7 +104,7 @@ func TestChangeSchemaIsNoticed(t *testing.T) {
 			require.NoError(t, err)
 
 			// make sure the change is detected
-			rs, err = conn.ExecuteFetch(detectChange, 1000, true)
+			rs, err = conn.ExecuteFetch(mysql.DetectSchemaChange, 1000, true)
 			require.NoError(t, err)
 			require.NotEmpty(t, rs.Rows)
 		})
