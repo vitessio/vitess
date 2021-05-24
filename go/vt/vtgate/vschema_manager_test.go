5284f17f80548bf507a2c9e15c5ff6f547fc0f65
Andres Taylor <andres@planetscale.com>
2021-05-24 16:53:14 +0200

    update table information if already known by vschema
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/vt/vtgate/vschema_manager_test.go
+++ go/vt/vtgate/vschema_manager_test.go
@@ -3,12 +3,13 @@ package vtgate
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"vitess.io/vitess/go/test/utils"
 	querypb "vitess.io/vitess/go/vt/proto/query"
-	vschemapb "vitess.io/vitess/go/vt/proto/vschema"
 	"vitess.io/vitess/go/vt/sqlparser"
+
+	vschemapb "vitess.io/vitess/go/vt/proto/vschema"
 	"vitess.io/vitess/go/vt/vtgate/vindexes"
 )
 
@@ -31,21 +32,89 @@ func TestWatchSrvVSchema(t *testing.T) {
 		Name: sqlparser.NewColIdent("id"),
 		Type: querypb.Type_INT64,
 	}}
+	cols2 := []vindexes.Column{{
+		Name: sqlparser.NewColIdent("id"),
+		Type: querypb.Type_INT64,
+	}, {
+		Name: sqlparser.NewColIdent("name"),
+		Type: querypb.Type_VARCHAR,
+	}}
+	ks := &vindexes.Keyspace{Name: "ks"}
+	dual := &vindexes.Table{Type: vindexes.TypeReference, Name: sqlparser.NewTableIdent("dual"), Keyspace: ks}
+	tcases := []struct {
+		name       string
+		srvVschema *vschemapb.SrvVSchema
+		st         []SchemaTable
+		expected   map[string]*vindexes.Table
+	}{{
+		name:       "Single table known by mysql schema and not by vschema",
+		srvVschema: &vschemapb.SrvVSchema{Keyspaces: map[string]*vschemapb.Keyspace{"ks": {}}},
+		st:         []SchemaTable{{Name: "tbl", Columns: cols}},
+		expected: map[string]*vindexes.Table{
+			"dual": dual,
+			"tbl": {
+				Name:                    sqlparser.NewTableIdent("tbl"),
+				Keyspace:                ks,
+				Columns:                 cols,
+				ColumnListAuthoritative: true,
+			},
+		},
+	}, {
+		name: "Single table known by both - vschema is not authoritative",
+		srvVschema: &vschemapb.SrvVSchema{Keyspaces: map[string]*vschemapb.Keyspace{"ks": {
+			Tables: map[string]*vschemapb.Table{
+				"tbl": {}, // we know of it, but nothing else
+			},
+		}}},
+		st: []SchemaTable{{Name: "tbl", Columns: cols}},
+		expected: map[string]*vindexes.Table{
+			"dual": dual,
+			"tbl": {
+				Name:                    sqlparser.NewTableIdent("tbl"),
+				Keyspace:                ks,
+				Columns:                 cols,
+				ColumnListAuthoritative: true,
+			},
+		},
+	}, {
+		name: "Single table known by both - vschema is authoritative",
+		srvVschema: &vschemapb.SrvVSchema{Keyspaces: map[string]*vschemapb.Keyspace{"ks": {
+			Tables: map[string]*vschemapb.Table{
+				"tbl": {
+					Columns: []*vschemapb.Column{
+						{Name: "id", Type: querypb.Type_INT64},
+						{Name: "name", Type: querypb.Type_VARCHAR},
+					},
+					ColumnListAuthoritative: true},
+			},
+		}}},
+		st: []SchemaTable{{Name: "tbl", Columns: cols}},
+		expected: map[string]*vindexes.Table{
+			"dual": dual,
+			"tbl": {
+				Name:                    sqlparser.NewTableIdent("tbl"),
+				Keyspace:                ks,
+				Columns:                 cols2,
+				ColumnListAuthoritative: true,
+			},
+		},
+	}}
 
-	vm := &VSchemaManager{
-		schema: fakeSchema{t: []SchemaTable{{Name: "tbl", Columns: cols}}},
-	}
-	called := false
+	vm := &VSchemaManager{}
+	var vs *vindexes.VSchema
 	vm.subscriber = func(vschema *vindexes.VSchema, _ *VSchemaStats) {
-		called = true
-		assert.Len(t, vschema.Keyspaces, 1)
-		ks := vschema.Keyspaces["ks"]
-		require.NotNil(t, ks, "keyspace was not found")
-		require.NotNil(t, ks.Tables["tbl"])
-		assert.Len(t, ks.Tables["tbl"].Columns, 1)
+		vs = vschema
 	}
+	for _, tcase := range tcases {
+		t.Run(tcase.name, func(t *testing.T) {
+			vs = nil
+			vm.schema = fakeSchema{t: tcase.st}
+			vm.VSchemaUpdate(tcase.srvVschema, nil)
 
-	v := &vschemapb.SrvVSchema{Keyspaces: map[string]*vschemapb.Keyspace{"ks": {}}}
-	vm.VSchemaUpdate(v, nil)
-	assert.True(t, called, "assertions were never called")
+			require.NotNil(t, vs)
+			ks := vs.Keyspaces["ks"]
+			require.NotNil(t, ks, "keyspace was not found")
+			utils.MustMatch(t, tcase.expected, ks.Tables)
+		})
+	}
 }
