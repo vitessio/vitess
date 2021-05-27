3c11a1cd7b4eda72b69688e38d73f4ec6b881fe3
Andres Taylor <andres@planetscale.com>
2021-05-27 07:48:07 +0200

    clear and reload the schemacopy whenever a tablet becomes master
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/vt/vtgate/schema/tracker_test.go
+++ go/vt/vtgate/schema/tracker_test.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+    `http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -54,7 +54,7 @@ func TestTracking(t *testing.T) {
 		"prior|id|int",
 	)})
 
-	_, err := tracker.LoadKeyspace(sbc, target)
+	err := tracker.LoadKeyspace(sbc, target)
 	require.NoError(t, err)
 
 	tracker.Start()
