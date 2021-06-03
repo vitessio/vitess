c75dc3c068c614fa318673903a65330d0198456c
Andrew Mason <amason@slack-corp.com>
2021-06-02 20:13:22 -0400

    Add FixedQueryPlan implementation of QueryPlan
    Signed-off-by: Andrew Mason <amason@slack-corp.com>

--- go/vt/vtctl/workflow/vexec/query_plan.go
+++ go/vt/vtctl/workflow/vexec/query_plan.go
@@ -116,3 +116,88 @@ func (qp *FixedQueryPlan) ExecuteScatter(ctx context.Context, targets ...*topo.T
 
 	return results, rec.AggrError(vterrors.Aggregate)
 }
+
+// PerTargetQueryPlan implements the QueryPlan interface. Unlike FixedQueryPlan,
+// this implementation implements different queries, keyed by tablet alias, on
+// different targets.
+//
+// It is the callers responsibility to ensure that the shape of the QueryResult
+// (i.e. fields returned) is consistent for each target's planned query, but
+// this is not enforced.
+type PerTargetQueryPlan struct {
+	ParsedQueries map[string]*sqlparser.ParsedQuery
+
+	tmc tmclient.TabletManagerClient
+}
+
+// Execute is part of the QueryPlan interface.
+//
+// It returns ErrUnpreparedQuery if there is no ParsedQuery for the target's
+// tablet alias.
+func (qp *PerTargetQueryPlan) Execute(ctx context.Context, target *topo.TabletInfo) (qr *querypb.QueryResult, err error) {
+	if qp.ParsedQueries == nil {
+		return nil, fmt.Errorf("%w: call PlanQuery on a query planner first", ErrUnpreparedQuery)
+	}
+
+	targetAliasStr := target.AliasString()
+	query, ok := qp.ParsedQueries[targetAliasStr]
+	if !ok {
+		return nil, fmt.Errorf("%w: no prepared query for target %s", ErrUnpreparedQuery, targetAliasStr)
+	}
+
+	defer func() {
+		if err != nil {
+			log.Warningf("Result on %v: %v", targetAliasStr, err)
+			return
+		}
+	}()
+
+	qr, err = qp.tmc.VReplicationExec(ctx, target.Tablet, query.Query)
+	if err != nil {
+		return nil, err
+	}
+
+	return qr, nil
+}
+
+// ExecuteScatter is part of the QueryPlan interface.
+func (qp *PerTargetQueryPlan) ExecuteScatter(ctx context.Context, targets ...*topo.TabletInfo) (map[*topo.TabletInfo]*querypb.QueryResult, error) {
+	if qp.ParsedQueries == nil {
+		// This check is an "optimization" on error handling. We check here,
+		// even though we will check this during the individual Execute calls,
+		// so that we return one error, rather than the same error aggregated
+		// len(targets) times.
+		return nil, fmt.Errorf("%w: call PlanQuery on a query planner first", ErrUnpreparedQuery)
+	}
+
+	var (
+		m       sync.Mutex
+		wg      sync.WaitGroup
+		rec     concurrency.AllErrorRecorder
+		results = make(map[*topo.TabletInfo]*querypb.QueryResult, len(targets))
+	)
+
+	for _, target := range targets {
+		wg.Add(1)
+
+		go func(ctx context.Context, target *topo.TabletInfo) {
+			defer wg.Done()
+
+			qr, err := qp.Execute(ctx, target)
+			if err != nil {
+				rec.RecordError(err)
+
+				return
+			}
+
+			m.Lock()
+			defer m.Unlock()
+
+			results[target] = qr
+		}(ctx, target)
+	}
+
+	wg.Wait()
+
+	return results, rec.AggrError(vterrors.Aggregate)
+}
