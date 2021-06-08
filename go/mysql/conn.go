e14c19116c31cd391db6c0c46547496ac4362e30
Harshit Gangal <harshit@planetscale.com>
2021-06-08 20:49:59 +0530

    copy data before returning read buffer back to pool in COM_STMT_SEND_LONG_DATA
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/mysql/conn.go
+++ go/mysql/conn.go
@@ -1033,7 +1033,7 @@ func (c *Conn) handleNextCommand(handler Handler) error {
 			return err
 		}
 	case ComStmtSendLongData:
-		stmtID, paramID, chunkData, ok := c.parseComStmtSendLongData(data)
+		stmtID, paramID, chunk, ok := c.parseComStmtSendLongData(data)
 		c.recycleReadPacket()
 		if !ok {
 			err := fmt.Errorf("error parsing statement send long data from client %v, returning error: %v", c.ConnectionID, data)
@@ -1055,9 +1055,6 @@ func (c *Conn) handleNextCommand(handler Handler) error {
 			log.Error(err.Error())
 			return err
 		}
-
-		chunk := make([]byte, len(chunkData))
-		copy(chunk, chunkData)
 
 		key := fmt.Sprintf("v%d", paramID+1)
 		if val, ok := prepare.BindVars[key]; ok {
