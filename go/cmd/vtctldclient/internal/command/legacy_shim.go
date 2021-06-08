732a088431f12aebe81d24154fc03b8c2dd7b85a
Andrew Mason <amason@slack-corp.com>
2021-06-08 10:26:25 -0400

    [vtctldclient] Update help text for legacy shim
    Also remove the custom help func hack, because it turns out we don't
    actually need it (thank goodness)
    
    Signed-off-by: Andrew Mason <amason@slack-corp.com>

--- go/cmd/vtctldclient/internal/command/legacy_shim.go
+++ go/cmd/vtctldclient/internal/command/legacy_shim.go
@@ -23,7 +23,6 @@ import (
 	"strings"
 
 	"github.com/spf13/cobra"
-	"k8s.io/apimachinery/pkg/util/sets"
 
 	"vitess.io/vitess/go/cmd/vtctldclient/cli"
 	"vitess.io/vitess/go/vt/log"
@@ -38,13 +37,41 @@ var (
 	// RPCs. This allows users to use a single binary to make RPCs against both
 	// the new and old vtctld gRPC APIs.
 	LegacyVtctlCommand = &cobra.Command{
-		Use:   "LegacyVtctlCommand",
+		Use:   "LegacyVtctlCommand -- <command> [flags ...] [args ...]",
 		Short: "Invoke a legacy vtctlclient command. Flag parsing is best effort.",
 		Args:  cobra.ArbitraryArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			cli.FinishedParsing(cmd)
 			return runLegacyCommand(args)
 		},
+		Long: strings.TrimSpace(`
+LegacyVtctlCommand uses the legacy vtctl grpc client to make an ExecuteVtctlCommand
+rpc to a vtctld.
+
+This command exists to support a smooth transition of any scripts that relied on
+vtctlclient during the migration to the new vtctldclient, and will be removed,
+following the Vitess project's standard deprecation cycle, once all commands
+have been migrated to the new VtctldServer api.
+
+To see the list of available legacy commands, run "LegacyVtctlCommand -- help".
+Note that, as with the old client, this requires a running server, as the flag
+parsing and help/usage text generation, is done server-side.
+
+Also note that, in order to defer that flag parsing to the server side, you must
+use the double-dash ("--") after the LegacyVtctlCommand subcommand string, or
+the client-side flag parsing library we are using will attempt to parse those
+flags (and fail).
+`),
+		Example: strings.TrimSpace(`
+LegacyVtctlCommand help # displays this help message
+LegacyVtctlCommand -- help # displays help for supported legacy vtctl commands
+
+# When using legacy command that take arguments, a double dash must be used
+# before the first flag argument, like in the first example. The double dash may
+# be used, however, at any point after the "LegacyVtctlCommand" string, as in
+# the second example.
+LegacyVtctlCommand AddCellInfo -- -server_address "localhost:1234" -root "/vitess/cell1"
+LegacyVtctlCommand -- AddCellInfo -server_address "localhost:5678" -root "/vitess/cell1"`),
 	}
 )
 
@@ -73,29 +100,5 @@ func runLegacyCommand(args []string) error {
 }
 
 func init() {
-	LegacyVtctlCommand.SetHelpFunc(func(cmd *cobra.Command, args []string) {
-		// PreRun (and PersistentPreRun) do not run when a Help command is
-		// being executed, so we need to duplicate the `--server` flag check
-		// here before we attempt to invoke the legacy help command.
-		if err := ensureServerArg(); err != nil {
-			log.Error(err)
-			return
-		}
-
-		realArgs := cmd.Flags().Args()
-
-		if len(realArgs) == 0 {
-			realArgs = append(realArgs, "help")
-		}
-
-		argSet := sets.NewString(realArgs...)
-		if !argSet.HasAny("help", "-h", "--help") {
-			// Cobra tends to swallow the help flag, so we need to put it back
-			// into the arg slice that we pass to runLegacyCommand.
-			realArgs = append(realArgs, "-h")
-		}
-
-		_ = runLegacyCommand(realArgs)
-	})
 	Root.AddCommand(LegacyVtctlCommand)
 }
