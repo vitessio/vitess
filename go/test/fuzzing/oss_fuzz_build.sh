c7cd5b40bd273210563ca1fb31b80d6dbd460b89
AdamKorcz <adam@adalogics.com>
2021-06-09 23:01:52 +0100

    fuzzing: Fix coverage build
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- go/test/fuzzing/oss_fuzz_build.sh
+++ go/test/fuzzing/oss_fuzz_build.sh
@@ -14,27 +14,60 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+export WORKDIR=$SRC/vitess
 if [ "$SANITIZER" = "coverage" ]
 then
-        # As of now Vitess cannot be compiled with coverage sanitizer
-        exit 0
+
+  compile_go_fuzzer()
+  {
+  path=$1
+  function=$2
+  fuzzer=$3
+  tags="-tags gofuzz"
+  if [[ $#  -eq 4 ]]; then
+    tags="-tags $4"
+  fi
+
+  go mod download
+  fuzzed_package=`go list $tags -f '{{.Name}}' $path`
+  abspath=`go list $tags -f {{.Dir}} $path`
+  cd $abspath
+  cp $GOPATH/ossfuzz_coverage_runner.go ./"${function,,}"_test.go
+  sed -i -e 's/FuzzFunction/'$function'/' ./"${function,,}"_test.go
+  sed -i -e 's/mypackagebeingfuzzed/'$fuzzed_package'/' ./"${function,,}"_test.go
+  sed -i -e 's/TestFuzzCorpus/Test'$function'Corpus/' ./"${function,,}"_test.go
+
+  fuzzed_repo=$(go list $tags -f {{.Module}} "$path")
+  abspath_repo=`go list -m $tags -f {{.Dir}} $fuzzed_repo || go list $tags -f {{.Dir}} $fuzzed_repo`
+  echo "s=$fuzzed_repo"="$abspath_repo"= > $OUT/$fuzzer.gocovpath
+  go test -run Test${function}Corpus -v $tags -coverpkg $fuzzed_repo/... -c -o $OUT/$fuzzer $path
+  } 
+else
+  # Change name in case we are not build with coverage
+  mv $WORKDIR/go/vt/vtgate/grpcvtgateconn/fuzz_flaky_test.go \
+     $WORKDIR/go/vt/vtgate/grpcvtgateconn/fuzz.go
+  mv $WORKDIR/go/vt/vtgate/grpcvtgateconn/suite_test.go \
+     $WORKDIR/go/vt/vtgate/grpcvtgateconn/suite_fuzz.go
+
+  mv $WORKDIR/go/vt/vtgate/engine/fuzz_flaky_test.go \
+     $WORKDIR/go/vt/vtgate/engine/fuzz.go
+  mv $WORKDIR/go/vt/vtgate/engine/fake_vcursor_test.go \
+     $WORKDIR/go/vt/vtgate/engine/fake_vcursor_fuzz.go
 fi
 
-compile_go_fuzzer ./go/test/fuzzing Fuzz vtctl_fuzzer
-compile_go_fuzzer ./go/test/fuzzing FuzzIsDML is_dml_fuzzer
-compile_go_fuzzer ./go/test/fuzzing FuzzNormalizer normalizer_fuzzer
-compile_go_fuzzer ./go/test/fuzzing FuzzParser parser_fuzzer
 
-compile_go_fuzzer ./go/mysql FuzzWritePacket write_packet_fuzzer
-compile_go_fuzzer ./go/mysql FuzzHandleNextCommand handle_next_command_fuzzer
-compile_go_fuzzer ./go/mysql FuzzReadQueryResults read_query_results_fuzzer
-compile_go_fuzzer ./go/mysql FuzzTLSServer fuzz_tls
-compile_go_fuzzer ./go/vt/vtgate/grpcvtgateconn Fuzz grpc_vtgate_fuzzer
-compile_go_fuzzer ./go/vt/vtgate/planbuilder FuzzAnalyse planbuilder_fuzzer
-
-mv ./go/vt/vtgate/engine/fake_vcursor_test.go \
-	./go/vt/vtgate/engine/fake_vcursor.go
-compile_go_fuzzer ./go/vt/vtgate/engine FuzzEngine engine_fuzzer
+compile_go_fuzzer vitess.io/vitess/go/test/fuzzing Fuzz vtctl_fuzzer
+compile_go_fuzzer vitess.io/vitess/go/test/fuzzing FuzzIsDML is_dml_fuzzer
+compile_go_fuzzer vitess.io/vitess/go/test/fuzzing FuzzNormalizer normalizer_fuzzer
+compile_go_fuzzer vitess.io/vitess/go/test/fuzzing FuzzParser parser_fuzzer
+compile_go_fuzzer vitess.io/vitess/go/mysql FuzzWritePacket write_packet_fuzzer
+compile_go_fuzzer vitess.io/vitess/go/mysql FuzzHandleNextCommand handle_next_command_fuzzer
+compile_go_fuzzer vitess.io/vitess/go/mysql FuzzReadQueryResults read_query_results_fuzzer
+compile_go_fuzzer vitess.io/vitess/go/mysql FuzzTLSServer fuzz_tls
+compile_go_fuzzer vitess.io/vitess/go/vt/vtgate/grpcvtgateconn Fuzz grpc_vtgate_fuzzer
+compile_go_fuzzer vitess.io/vitess/go/vt/vtgate/planbuilder FuzzAnalyse planbuilder_fuzzer
+compile_go_fuzzer vitess.io/vitess/go/vt/vtgate/engine FuzzEngine engine_fuzzer
 
 # Build dictionaries
 cp $SRC/vitess/go/test/fuzzing/vtctl_fuzzer.dict $OUT/
+
