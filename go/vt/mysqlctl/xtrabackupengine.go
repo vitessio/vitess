6fc2b1df68b7752e30200a3770fffb961a20fe6c
deepthi <deepthi@planetscale.com>
2021-06-11 13:48:58 -0700

    workaround xtrabackup error using mysql global variable old_alter_table
    Signed-off-by: deepthi <deepthi@planetscale.com>

--- go/vt/mysqlctl/xtrabackupengine.go
+++ go/vt/mysqlctl/xtrabackupengine.go
@@ -299,6 +299,7 @@ func (be *XtrabackupEngine) backupFiles(ctx context.Context, params BackupParams
 	// the replication position. Note that if we don't read stderr as we go, the
 	// xtrabackup process gets blocked when the write buffer fills up.
 	stderrBuilder := &strings.Builder{}
+	posBuilder := &strings.Builder{}
 	stderrDone := make(chan struct{})
 	go func() {
 		defer close(stderrDone)
@@ -308,6 +309,7 @@ func (be *XtrabackupEngine) backupFiles(ctx context.Context, params BackupParams
 		for scanner.Scan() {
 			line := scanner.Text()
 			params.Logger.Infof("xtrabackup stderr: %s", line)
+			fmt.Fprintln(stderrBuilder, line)
 
 			// Wait until we see the first line of the binlog position.
 			// Then capture all subsequent lines. We need multiple lines since
@@ -318,7 +320,7 @@ func (be *XtrabackupEngine) backupFiles(ctx context.Context, params BackupParams
 				}
 				capture = true
 			}
-			fmt.Fprintln(stderrBuilder, line)
+			fmt.Fprintln(posBuilder, line)
 		}
 		if err := scanner.Err(); err != nil {
 			params.Logger.Errorf("error reading from xtrabackup stderr: %v", err)
@@ -362,7 +364,8 @@ func (be *XtrabackupEngine) backupFiles(ctx context.Context, params BackupParams
 		return replicationPosition, vterrors.Wrap(err, fmt.Sprintf("xtrabackup failed with error. Output=%s", sterrOutput))
 	}
 
-	replicationPosition, rerr := findReplicationPosition(sterrOutput, flavor, params.Logger)
+	posOutput := posBuilder.String()
+	replicationPosition, rerr := findReplicationPosition(posOutput, flavor, params.Logger)
 	if rerr != nil {
 		return replicationPosition, vterrors.Wrap(rerr, "backup failed trying to find replication position")
 	}
