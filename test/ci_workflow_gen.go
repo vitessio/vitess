1bb321785fff46fbcfbe988237d27dc5c74a75c3
Harshit Gangal <harshit@planetscale.com>
2021-06-22 09:33:40 +0530

    added percona56 and removed old workflow
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- test/ci_workflow_gen.go
+++ test/ci_workflow_gen.go
@@ -29,7 +29,7 @@ const (
 	workflowConfigDir = "../.github/workflows"
 
 	unitTestTemplate  = "templates/unit_test.tpl"
-	unitTestDatabases = "mysql57, mysql80, mariadb101, mariadb102, mariadb103"
+	unitTestDatabases = "percona56, mysql57, mysql80, mariadb101, mariadb102, mariadb103"
 
 	clusterTestTemplate = "templates/cluster_endtoend_test.tpl"
 	clusterList         = "11,12,13,14,15,16,17,18,19,20,21,22,23,24,26,27,vreplication_basic,vreplication_multicell,vreplication_cellalias,vreplication_v2"
