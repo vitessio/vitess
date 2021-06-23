d4bc85940f0e34d4c8fb4c905bd821f6aac6ec64
Vicent Marti <vmg@strn.cat>
2021-06-23 18:20:40 +0200

    grpctmclient: simplify locking & queuing
    Signed-off-by: Vicent Marti <vmg@strn.cat>

--- go/vt/vttablet/grpctmclient/cached_client.go
+++ go/vt/vttablet/grpctmclient/cached_client.go
@@ -17,10 +17,10 @@ limitations under the License.
 package grpctmclient
 
 import (
-	"container/heap"
 	"context"
 	"flag"
 	"io"
+	"sort"
 	"sync"
 	"time"
 
@@ -60,113 +60,61 @@ type cachedConn struct {
 	tabletmanagerservicepb.TabletManagerClient
 	cc *grpc.ClientConn
 
+	addr           string
 	lastAccessTime time.Time
 	refs           int
-
-	index int
-	key   string
-}
-
-// cachedConns provides a priority queue implementation for O(log n) connection
-// eviction management. It is a nearly verbatim copy of the priority queue
-// sample at https://golang.org/pkg/container/heap/#pkg-overview, with the
-// Less function changed such that connections with refs==0 get pushed to the
-// front of the queue, because those are the connections we are going to want
-// to evict.
-type cachedConns []*cachedConn
-
-var _ heap.Interface = (*cachedConns)(nil)
-
-// Len is part of the sort.Interface interface and is used by container/heap
-// functions.
-func (queue cachedConns) Len() int { return len(queue) }
-
-// Less is part of the sort.Interface interface and is used by container/heap
-// functions.
-func (queue cachedConns) Less(i, j int) bool {
-	left, right := queue[i], queue[j]
-	if left.refs == right.refs {
-		// break ties by access time.
-		// more stale connections have higher priority for removal
-		// this condition is equvalent to:
-		//		left.lastAccessTime <= right.lastAccessTime
-		return !left.lastAccessTime.After(right.lastAccessTime)
-	}
-
-	// connections with fewer refs have higher priority for removal
-	return left.refs < right.refs
-}
-
-// Swap is part of the sort.Interface interface and is used by container/heap
-// functions.
-func (queue cachedConns) Swap(i, j int) {
-	queue[i], queue[j] = queue[j], queue[i]
-	queue[i].index = i
-	queue[j].index = j
-}
-
-// Push is part of the container/heap.Interface interface.
-func (queue *cachedConns) Push(x interface{}) {
-	n := len(*queue)
-	conn := x.(*cachedConn)
-	conn.index = n
-	*queue = append(*queue, conn)
-}
-
-// Pop is part of the container/heap.Interface interface.
-func (queue *cachedConns) Pop() interface{} {
-	old := *queue
-	n := len(old)
-	conn := old[n-1]
-	old[n-1] = nil  // avoid memory leak
-	conn.index = -1 // for safety
-	*queue = old[0 : n-1]
-
-	return conn
 }
 
 type cachedConnDialer struct {
-	m            sync.RWMutex
+	m            sync.Mutex
 	conns        map[string]*cachedConn
-	qMu          sync.Mutex
-	queue        cachedConns
+	evict        []*cachedConn
+	evictSorted  bool
 	connWaitSema *sync2.Semaphore
 }
 
 var dialerStats = struct {
-	ConnReuse            *stats.Gauge
-	ConnNew              *stats.Gauge
-	DialTimeouts         *stats.Gauge
-	DialTimings          *stats.Timings
-	EvictionQueueTimings *stats.Timings
+	ConnReuse    *stats.Gauge
+	ConnNew      *stats.Gauge
+	DialTimeouts *stats.Gauge
+	DialTimings  *stats.Timings
 }{
-	ConnReuse:            stats.NewGauge("tabletmanagerclient_cachedconn_reuse", "number of times a call to dial() was able to reuse an existing connection"),
-	ConnNew:              stats.NewGauge("tabletmanagerclient_cachedconn_new", "number of times a call to dial() resulted in a dialing a new grpc clientconn"),
-	DialTimeouts:         stats.NewGauge("tabletmanagerclient_cachedconn_dial_timeouts", "number of context timeouts during dial()"),
-	DialTimings:          stats.NewTimings("tabletmanagerclient_cachedconn_dialtimings", "timings for various dial paths", "path", "rlock_fast", "sema_fast", "sema_poll"),
-	EvictionQueueTimings: stats.NewTimings("tabletmanagerclient_cachedconn_eviction_queue_timings", "timings for eviction queue management operations", "operation", "init", "push", "pop", "fix"),
+	ConnReuse:    stats.NewGauge("tabletmanagerclient_cachedconn_reuse", "number of times a call to dial() was able to reuse an existing connection"),
+	ConnNew:      stats.NewGauge("tabletmanagerclient_cachedconn_new", "number of times a call to dial() resulted in a dialing a new grpc clientconn"),
+	DialTimeouts: stats.NewGauge("tabletmanagerclient_cachedconn_dial_timeouts", "number of context timeouts during dial()"),
+	DialTimings:  stats.NewTimings("tabletmanagerclient_cachedconn_dialtimings", "timings for various dial paths", "path", "rlock_fast", "sema_fast", "sema_poll"),
 }
 
-// NewCachedConnClient returns a grpc Client using the priority queue cache
-// dialer implementation.
+// NewCachedConnClient returns a grpc Client that caches connections to the different tablets
 func NewCachedConnClient(capacity int) *Client {
 	dialer := &cachedConnDialer{
 		conns:        make(map[string]*cachedConn, capacity),
-		queue:        make(cachedConns, 0, capacity),
+		evict:        make([]*cachedConn, 0, capacity),
 		connWaitSema: sync2.NewSemaphore(capacity, 0),
 	}
-
-	dialer.heapInit()
 	return &Client{dialer}
 }
 
 var _ dialer = (*cachedConnDialer)(nil)
 
+func (dialer *cachedConnDialer) sortEvictionsLocked() {
+	if !dialer.evictSorted {
+		sort.Slice(dialer.evict, func(i, j int) bool {
+			left, right := dialer.evict[i], dialer.evict[j]
+			if left.refs == right.refs {
+				return left.lastAccessTime.After(right.lastAccessTime)
+			}
+			return left.refs > right.refs
+		})
+		dialer.evictSorted = true
+	}
+}
+
 func (dialer *cachedConnDialer) dial(ctx context.Context, tablet *topodatapb.Tablet) (tabletmanagerservicepb.TabletManagerClient, io.Closer, error) {
 	start := time.Now()
 	addr := getTabletAddr(tablet)
 
-	if client, closer, found, err := dialer.tryFromCache(addr, dialer.m.RLocker()); found {
+	if client, closer, found, err := dialer.tryFromCache(addr, &dialer.m); found {
 		dialerStats.DialTimings.Add("rlock_fast", time.Since(start))
 		return client, closer, err
 	}
@@ -175,17 +123,14 @@ func (dialer *cachedConnDialer) dial(ctx context.Context, tablet *topodatapb.Tab
 		defer func() {
 			dialerStats.DialTimings.Add("sema_fast", time.Since(start))
 		}()
-		dialer.m.Lock()
-		defer dialer.m.Unlock()
 
 		// Check if another goroutine managed to dial a conn for the same addr
 		// while we were waiting for the write lock. This is identical to the
 		// read-lock section above.
-		if client, closer, found, err := dialer.tryFromCache(addr, nil); found {
+		if client, closer, found, err := dialer.tryFromCache(addr, &dialer.m); found {
 			return client, closer, err
 		}
-
-		return dialer.newdial(addr, true /* manage queue lock */)
+		return dialer.newdial(addr)
 	}
 
 	defer func() {
@@ -223,7 +168,7 @@ func (dialer *cachedConnDialer) tryFromCache(addr string, locker sync.Locker) (c
 	}
 
 	if conn, ok := dialer.conns[addr]; ok {
-		client, closer, err := dialer.redial(conn)
+		client, closer, err := dialer.redialLocked(conn)
 		return client, closer, ok, err
 	}
 
@@ -249,37 +194,37 @@ func (dialer *cachedConnDialer) tryFromCache(addr string, locker sync.Locker) (c
 // whether the dial() poll loop should exit, and an error.
 func (dialer *cachedConnDialer) pollOnce(addr string) (client tabletmanagerservicepb.TabletManagerClient, closer io.Closer, found bool, err error) {
 	dialer.m.Lock()
-	defer dialer.m.Unlock()
 
 	if client, closer, found, err := dialer.tryFromCache(addr, nil); found {
+		dialer.m.Unlock()
 		return client, closer, found, err
 	}
 
-	dialer.qMu.Lock()
-	defer dialer.qMu.Unlock()
+	dialer.sortEvictionsLocked()
 
-	conn := dialer.queue[0]
+	conn := dialer.evict[len(dialer.evict)-1]
 	if conn.refs != 0 {
+		dialer.m.Unlock()
 		return nil, nil, false, nil
 	}
 
-	dialer.heapPop()
-	delete(dialer.conns, conn.key)
+	dialer.evict = dialer.evict[:len(dialer.evict)-1]
+	delete(dialer.conns, conn.addr)
 	conn.cc.Close()
+	dialer.m.Unlock()
 
-	client, closer, err = dialer.newdial(addr, false /* manage queue lock */)
+	client, closer, err = dialer.newdial(addr)
 	return client, closer, true, err
 }
 
 // newdial creates a new cached connection, and updates the cache and eviction
-// queue accordingly. This must be called only while holding the write lock on
-// dialer.m as well as after having successfully acquired the dialer.connWaitSema. If newdial fails to create the underlying
+// queue accordingly. If newdial fails to create the underlying
 // gRPC connection, it will make a call to Release the connWaitSema for other
 // newdial calls.
 //
 // It returns the three-tuple of client-interface, closer, and error that the
 // main dial func returns.
-func (dialer *cachedConnDialer) newdial(addr string, manageQueueLock bool) (tabletmanagerservicepb.TabletManagerClient, io.Closer, error) {
+func (dialer *cachedConnDialer) newdial(addr string) (tabletmanagerservicepb.TabletManagerClient, io.Closer, error) {
 	dialerStats.ConnNew.Add(1)
 
 	opt, err := grpcclient.SecureDialOption(*cert, *key, *ca, *name)
@@ -294,13 +239,16 @@ func (dialer *cachedConnDialer) newdial(addr string, manageQueueLock bool) (tabl
 		return nil, nil, err
 	}
 
-	// In the case where dial is evicting a connection from the cache, we
-	// already have a lock on the eviction queue. Conversely, in the case where
-	// we are able to create a new connection without evicting (because the
-	// cache is not yet full), we don't have the queue lock yet.
-	if manageQueueLock {
-		dialer.qMu.Lock()
-		defer dialer.qMu.Unlock()
+	dialer.m.Lock()
+	defer dialer.m.Unlock()
+
+	if conn, existing := dialer.conns[addr]; existing {
+		// race condition: some other goroutine has dialed our tablet before we have;
+		// this is not great, but shouldn't happen often (if at all), so we're going to
+		// close this connection and reuse the existing one. by doing this, we can keep
+		// the actual Dial out of the global lock and significantly increase throughput
+		cc.Close()
+		return dialer.redialLocked(conn)
 	}
 
 	conn := &cachedConn{
@@ -308,32 +256,27 @@ func (dialer *cachedConnDialer) newdial(addr string, manageQueueLock bool) (tabl
 		cc:                  cc,
 		lastAccessTime:      time.Now(),
 		refs:                1,
-		index:               -1, // gets set by call to Push
-		key:                 addr,
+		addr:                addr,
 	}
-	dialer.heapPush(conn)
+	dialer.evict = append(dialer.evict, conn)
+	dialer.evictSorted = false
 	dialer.conns[addr] = conn
 
 	return dialer.connWithCloser(conn)
 }
 
-// redial takes an already-dialed connection in the cache does all the work of
+// redialLocked takes an already-dialed connection in the cache does all the work of
 // lending that connection out to one more caller. this should only ever be
 // called while holding at least the RLock on dialer.m (but the write lock is
 // fine too), to prevent the connection from getting evicted out from under us.
 //
 // It returns the three-tuple of client-interface, closer, and error that the
 // main dial func returns.
-func (dialer *cachedConnDialer) redial(conn *cachedConn) (tabletmanagerservicepb.TabletManagerClient, io.Closer, error) {
+func (dialer *cachedConnDialer) redialLocked(conn *cachedConn) (tabletmanagerservicepb.TabletManagerClient, io.Closer, error) {
 	dialerStats.ConnReuse.Add(1)
-
-	dialer.qMu.Lock()
-	defer dialer.qMu.Unlock()
-
 	conn.lastAccessTime = time.Now()
 	conn.refs++
-	dialer.heapFix(conn.index)
-
+	dialer.evictSorted = false
 	return dialer.connWithCloser(conn)
 }
 
@@ -342,41 +285,12 @@ func (dialer *cachedConnDialer) redial(conn *cachedConn) (tabletmanagerservicepb
 // in the eviction queue.
 func (dialer *cachedConnDialer) connWithCloser(conn *cachedConn) (tabletmanagerservicepb.TabletManagerClient, io.Closer, error) {
 	return conn, closeFunc(func() error {
-		dialer.qMu.Lock()
-		defer dialer.qMu.Unlock()
-
+		dialer.m.Lock()
+		defer dialer.m.Unlock()
 		conn.refs--
-		dialer.heapFix(conn.index)
+		dialer.evictSorted = false
 		return nil
 	}), nil
-}
-
-// Functions to wrap queue operations to record timings for them.
-
-func (dialer *cachedConnDialer) heapInit() {
-	start := time.Now()
-	heap.Init(&dialer.queue)
-	dialerStats.EvictionQueueTimings.Add("init", time.Since(start))
-}
-
-func (dialer *cachedConnDialer) heapFix(index int) {
-	start := time.Now()
-	heap.Fix(&dialer.queue, index)
-	dialerStats.EvictionQueueTimings.Add("fix", time.Since(start))
-}
-
-func (dialer *cachedConnDialer) heapPop() interface{} {
-	start := time.Now()
-	x := heap.Pop(&dialer.queue)
-	dialerStats.EvictionQueueTimings.Add("pop", time.Since(start))
-
-	return x
-}
-
-func (dialer *cachedConnDialer) heapPush(conn *cachedConn) {
-	start := time.Now()
-	heap.Push(&dialer.queue, conn)
-	dialerStats.EvictionQueueTimings.Add("push", time.Since(start))
 }
 
 // Close closes all currently cached connections, ***regardless of whether
@@ -392,15 +306,13 @@ func (dialer *cachedConnDialer) heapPush(conn *cachedConn) {
 func (dialer *cachedConnDialer) Close() {
 	dialer.m.Lock()
 	defer dialer.m.Unlock()
-	dialer.qMu.Lock()
-	defer dialer.qMu.Unlock()
 
-	for dialer.queue.Len() > 0 {
-		conn := dialer.queue.Pop().(*cachedConn)
+	for _, conn := range dialer.evict {
 		conn.cc.Close()
-		delete(dialer.conns, conn.key)
+		delete(dialer.conns, conn.addr)
 		dialer.connWaitSema.Release()
 	}
+	dialer.evict = nil
 }
 
 func getTabletAddr(tablet *topodatapb.Tablet) string {
