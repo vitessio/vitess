3372b792a32dd6684a2d26f7bf86fedb3b75a7e6
Andrew Mason <amason@slack-corp.com>
2021-06-27 22:12:59 -0400

    Update http wrapper for getbackups with filtering
    Signed-off-by: Andrew Mason <amason@slack-corp.com>

--- go/vt/vtadmin/http/backups.go
+++ go/vt/vtadmin/http/backups.go
@@ -19,13 +19,46 @@ package http
 import (
 	"context"
 
+	"vitess.io/vitess/go/vt/concurrency"
+
 	vtadminpb "vitess.io/vitess/go/vt/proto/vtadmin"
+	vtctldatapb "vitess.io/vitess/go/vt/proto/vtctldata"
 )
 
 // GetBackups implements the http wrapper for /backups[?cluster=[&cluster=]].
 func GetBackups(ctx context.Context, r Request, api *API) *JSONResponse {
+	query := r.URL.Query()
+
+	rec := concurrency.AllErrorRecorder{} // Aggregate any BadRequest type errors
+
+	limit, err := r.ParseQueryParamAsUint32("limit", 0)
+	if err != nil {
+		rec.RecordError(err)
+	}
+
+	detailed, err := r.ParseQueryParamAsBool("detailed", false)
+	if err != nil {
+		rec.RecordError(err)
+	}
+
+	detailedLimit, err := r.ParseQueryParamAsUint32("detailed_limit", 3)
+	if err != nil {
+		rec.RecordError(err)
+	}
+
+	if rec.HasErrors() {
+		return NewJSONResponse(nil, rec.Error())
+	}
+
 	backups, err := api.server.GetBackups(ctx, &vtadminpb.GetBackupsRequest{
-		ClusterIds: r.URL.Query()["cluster"],
+		ClusterIds:     query["cluster"],
+		Keyspaces:      query["keyspace"],
+		KeyspaceShards: query["keyspace_shard"],
+		RequestOptions: &vtctldatapb.GetBackupsRequest{
+			Limit:         limit,
+			Detailed:      detailed,
+			DetailedLimit: detailedLimit,
+		},
 	})
 
 	return NewJSONResponse(backups, err)
