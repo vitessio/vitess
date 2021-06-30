7e723b10f26fc85f3c8c3a9602757bd1234607a1
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2021-06-30 16:29:44 +0300

    Online DDL/VReplication: able to read from replica
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/vttablet/onlineddl/vrepl.go
+++ go/vt/vttablet/onlineddl/vrepl.go
@@ -525,7 +525,7 @@ func (v *VRepl) analyze(ctx context.Context, conn *dbconnpool.DBConnection) erro
 // generateInsertStatement generates the INSERT INTO _vt.replication stataement that creates the vreplication workflow
 func (v *VRepl) generateInsertStatement(ctx context.Context) (string, error) {
 	ig := vreplication.NewInsertGenerator(binlogplayer.BlpStopped, v.dbName)
-	ig.AddRow(v.workflow, v.bls, v.pos, "", "MASTER")
+	ig.AddRow(v.workflow, v.bls, v.pos, "", "REPLICA,MASTER")
 
 	return ig.String(), nil
 }
