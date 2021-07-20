d0fe52197a9119e554049f8655877ba960749727
Dirkjan Bussink <d.bussink@gmail.com>
2021-07-20 16:11:00 +0200

    Refactor authentication server plugin mechanism
    This change implements a large refactor of the plugin authentication
    mechanism. This refactor needed for a number of reasons:
    
    - The current AuthServer interface is not well designed. Many of the
      AuthServer implementations implement at least one if not multiple
      methods as a panic() as they are unused or they implement something
      never really called (like the salt generation). This indicates that
      the current interface doesn't abstract things well.
    - Adding `caching_sha2_password` as a server side mechanism would make
      this interface problem worse. It would add even more methods that
      would have emply implementations / panic().
    - It is impossible right now for a plugin to support multiple
      authentication mechanisms. The caching_sha2_password plugin allows for
      caching of passwords and could be used to improve for example the LDAP
      plugin. But we can't then still keep the `mysql_clear_password` as a
      fallback for such an improvement in the LDAP plugin.
    
    Based on these issues, I'm proposing this refactor. It allows for a
    number of things that also make auth server implementations easier.
    
    - An auth server can implement multiple auth methods. This allows for
      the above mentioned optimization for the LDAP plugin and many other
      improvements. Note that these improvements are not implemented here, but
      it opens up the opportunity.
    - There are helpers for all the already supported authentication
      mechanisms plus basic support for caching_sha2_password. The last only
      supports TLS connections or Unix sockets right now, since the full
      authentication protocol is much complicated. The value of the latter
      is also debatable in the context of Vitess. It depends on a TLS
      certificate and private key also for the plain text full auth cycle
      and in that case, TLS would already be available as well anyway. For
      clients that want caching_sha2_password, switching those to enable TLS
      seems like a much simpler solution.
    - With the helpers, specific interfaces are provided that plugins can
      implement. Those interfaces provide a simpler way than the complex
      Negotiate from before which might need to read packets etc. For these
      helpers, all reading / writing of the protocol is already dealt with and
      not something auth server plugin writers need to be concerned with.
    
    Given the above advantages, I think the current setup is a better
    design and better fits what we'd want to use for authentication plugins.
    
    Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

--- go/mysql/query_benchmark_test.go
+++ go/mysql/query_benchmark_test.go
@@ -37,7 +37,7 @@ const benchmarkQueryPrefix = "benchmark "
 func benchmarkQuery(b *testing.B, threads int, query string) {
 	th := &testHandler{}
 
-	authServer := &AuthServerNone{}
+	authServer := NewAuthServerNone()
 
 	lCfg := ListenerConfig{
 		Protocol:           "tcp",
