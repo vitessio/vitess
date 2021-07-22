cf96f96a357c146fed98ac2dba2ab28ebde0f8cc
GuptaManan100 <manan@planetscale.com>
2021-07-22 12:32:53 +0530

    get primary recovery type
    Signed-off-by: GuptaManan100 <manan@planetscale.com>

--- go/vt/orchestrator/logic/reparent_functions.go
+++ go/vt/orchestrator/logic/reparent_functions.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2021 The Vitess Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package logic
 
 import (
@@ -89,4 +105,16 @@ func (vtorcReparent *VtOrcReparentFunctions) StopReplicationAndBuildStatusMaps(c
 	err := TabletDemoteMaster(vtorcReparent.analysisEntry.AnalyzedInstanceKey)
 	AuditTopologyRecovery(vtorcReparent.topologyRecovery, fmt.Sprintf("RecoverDeadMaster: TabletDemoteMaster: %v", err))
 	return err
+}
+
+// GetPrimaryRecoveryType implements the ReparentFunctions interface
+func (vtorcReparent *VtOrcReparentFunctions) GetPrimaryRecoveryType() MasterRecoveryType {
+	vtorcReparent.topologyRecovery.RecoveryType = GetMasterRecoveryType(&vtorcReparent.topologyRecovery.AnalysisEntry)
+	AuditTopologyRecovery(vtorcReparent.topologyRecovery, fmt.Sprintf("RecoverDeadMaster: masterRecoveryType=%+v", vtorcReparent.topologyRecovery.RecoveryType))
+	return vtorcReparent.topologyRecovery.RecoveryType
+}
+
+// AddError implements the ReparentFunctions interface
+func (vtorcReparent *VtOrcReparentFunctions) AddError(errorMsg string) error {
+	return vtorcReparent.topologyRecovery.AddError(log.Errorf(errorMsg))
 }
