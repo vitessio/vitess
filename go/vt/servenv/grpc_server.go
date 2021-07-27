a07c7488354b10853cfc88fb479492bb146fbd2a
Josh Chorlton <choo@stripe.com>
2021-07-27 08:35:54 -0600

    support grpc reflection on APIs
    Signed-off-by: Josh Chorlton <choo@stripe.com>

--- go/vt/servenv/grpc_server.go
+++ go/vt/servenv/grpc_server.go
@@ -31,6 +31,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/keepalive"
+	"google.golang.org/grpc/reflection"
 
 	"context"
 
@@ -212,6 +213,9 @@ func serveGRPC() {
 	if GRPCPort == nil || *GRPCPort == 0 {
 		return
 	}
+
+	// register reflection to support list calls :)
+	reflection.Register(GRPCServer)
 
 	// listen on the port
 	log.Infof("Listening for gRPC calls on port %v", *GRPCPort)
