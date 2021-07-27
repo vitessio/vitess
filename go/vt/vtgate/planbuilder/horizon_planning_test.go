fbc96dc01489e3106c24ceee04ed278e3394aba6
Florent Poinsard <florent.poinsard@outlook.fr>
2021-07-27 10:22:45 +0200

    More readable horizon planning unit test
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- go/vt/vtgate/planbuilder/horizon_planning_test.go
+++ go/vt/vtgate/planbuilder/horizon_planning_test.go
@@ -31,12 +31,42 @@ func TestCheckIfAlreadyExists(t *testing.T) {
 		sel  *sqlparser.Select
 		want int
 	}{
-		{name: "No alias, both ColName", want: 0, expr: &sqlparser.AliasedExpr{Expr: sqlparser.NewColName("id")}, sel: &sqlparser.Select{SelectExprs: []sqlparser.SelectExpr{&sqlparser.AliasedExpr{Expr: sqlparser.NewColName("id")}}}},
-		{name: "Aliased expression and ColName", want: 0, expr: &sqlparser.AliasedExpr{Expr: sqlparser.NewColName("user_id")}, sel: &sqlparser.Select{SelectExprs: []sqlparser.SelectExpr{&sqlparser.AliasedExpr{As: sqlparser.NewColIdent("user_id"), Expr: sqlparser.NewColName("id")}}}},
-		{name: "Non-ColName expressions", want: 0, expr: &sqlparser.AliasedExpr{Expr: sqlparser.NewStrLiteral("test")}, sel: &sqlparser.Select{SelectExprs: []sqlparser.SelectExpr{&sqlparser.AliasedExpr{Expr: sqlparser.NewStrLiteral("test")}}}},
-		{name: "No alias, multiple ColName in projection", want: 1, expr: &sqlparser.AliasedExpr{Expr: sqlparser.NewColName("id")}, sel: &sqlparser.Select{SelectExprs: []sqlparser.SelectExpr{&sqlparser.AliasedExpr{Expr: sqlparser.NewColName("foo")}, &sqlparser.AliasedExpr{Expr: sqlparser.NewColName("id")}}}},
-		{name: "No matching entry", want: -1, expr: &sqlparser.AliasedExpr{Expr: sqlparser.NewColName("id")}, sel: &sqlparser.Select{SelectExprs: []sqlparser.SelectExpr{&sqlparser.AliasedExpr{Expr: sqlparser.NewColName("foo")}, &sqlparser.AliasedExpr{Expr: sqlparser.NewColName("name")}}}},
-		{name: "No AliasedExpr in projection", want: -1, expr: &sqlparser.AliasedExpr{Expr: sqlparser.NewColName("id")}, sel: &sqlparser.Select{SelectExprs: []sqlparser.SelectExpr{&sqlparser.StarExpr{TableName: sqlparser.TableName{Name: sqlparser.NewTableIdent("user")}}, &sqlparser.StarExpr{TableName: sqlparser.TableName{Name: sqlparser.NewTableIdent("people")}}}}},
+		{
+			name: "No alias, both ColName",
+			want: 0,
+			expr: &sqlparser.AliasedExpr{Expr: sqlparser.NewColName("id")},
+			sel:  &sqlparser.Select{SelectExprs: []sqlparser.SelectExpr{&sqlparser.AliasedExpr{Expr: sqlparser.NewColName("id")}}},
+		},
+		{
+			name: "Aliased expression and ColName",
+			want: 0,
+			expr: &sqlparser.AliasedExpr{Expr: sqlparser.NewColName("user_id")},
+			sel:  &sqlparser.Select{SelectExprs: []sqlparser.SelectExpr{&sqlparser.AliasedExpr{As: sqlparser.NewColIdent("user_id"), Expr: sqlparser.NewColName("id")}}},
+		},
+		{
+			name: "Non-ColName expressions",
+			want: 0,
+			expr: &sqlparser.AliasedExpr{Expr: sqlparser.NewStrLiteral("test")},
+			sel:  &sqlparser.Select{SelectExprs: []sqlparser.SelectExpr{&sqlparser.AliasedExpr{Expr: sqlparser.NewStrLiteral("test")}}},
+		},
+		{
+			name: "No alias, multiple ColName in projection",
+			want: 1,
+			expr: &sqlparser.AliasedExpr{Expr: sqlparser.NewColName("id")},
+			sel:  &sqlparser.Select{SelectExprs: []sqlparser.SelectExpr{&sqlparser.AliasedExpr{Expr: sqlparser.NewColName("foo")}, &sqlparser.AliasedExpr{Expr: sqlparser.NewColName("id")}}},
+		},
+		{
+			name: "No matching entry",
+			want: -1,
+			expr: &sqlparser.AliasedExpr{Expr: sqlparser.NewColName("id")},
+			sel:  &sqlparser.Select{SelectExprs: []sqlparser.SelectExpr{&sqlparser.AliasedExpr{Expr: sqlparser.NewColName("foo")}, &sqlparser.AliasedExpr{Expr: sqlparser.NewColName("name")}}},
+		},
+		{
+			name: "No AliasedExpr in projection",
+			want: -1,
+			expr: &sqlparser.AliasedExpr{Expr: sqlparser.NewColName("id")},
+			sel:  &sqlparser.Select{SelectExprs: []sqlparser.SelectExpr{&sqlparser.StarExpr{TableName: sqlparser.TableName{Name: sqlparser.NewTableIdent("user")}}, &sqlparser.StarExpr{TableName: sqlparser.TableName{Name: sqlparser.NewTableIdent("people")}}}},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
