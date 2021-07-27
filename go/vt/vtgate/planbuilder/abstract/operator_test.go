0839d7213543b2be8d86296c82457192e3fdb2b3
GuptaManan100 <manan@planetscale.com>
2021-07-27 12:44:10 +0530

    added support for comments and SCATTER_ERRORS_AS_WARNINGS comment directive
    Signed-off-by: GuptaManan100 <manan@planetscale.com>

--- go/vt/vtgate/planbuilder/abstract/operator_test.go
+++ go/vt/vtgate/planbuilder/abstract/operator_test.go
@@ -162,7 +162,7 @@ JoinPredicates:
 		t.Run(fmt.Sprintf("%d %s", i, sql), func(t *testing.T) {
 			tree, err := sqlparser.Parse(sql)
 			require.NoError(t, err)
-			semTable, err := semantics.Analyze(tree, "", &semantics.FakeSI{})
+			semTable, err := semantics.Analyze(tree.(sqlparser.SelectStatement), "", &semantics.FakeSI{})
 			require.NoError(t, err)
 			optree, err := CreateOperatorFromSelect(tree.(*sqlparser.Select), semTable)
 			require.NoError(t, err)
