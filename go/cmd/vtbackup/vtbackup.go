f855fd192c4308214018970057717f385f4f237a
Jacques Grove <aquarapid@gmail.com>
2021-08-06 11:45:54 -0700

    Add -full_flush parameter for vtbackup, which performs a clean MySQL shutdown & startup cycle to work around xtrabackup bugs.
    Signed-off-by: Jacques Grove <aquarapid@gmail.com>

--- go/cmd/vtbackup/vtbackup.go
+++ go/cmd/vtbackup/vtbackup.go
@@ -113,6 +113,7 @@ var (
 
 	initialBackup    = flag.Bool("initial_backup", false, "Instead of restoring from backup, initialize an empty database with the provided init_db_sql_file and upload a backup of that for the shard, if the shard has no backups yet. This can be used to seed a brand new shard with an initial, empty backup. If any backups already exist for the shard, this will be considered a successful no-op. This can only be done before the shard exists in topology (i.e. before any tablets are deployed).")
 	allowFirstBackup = flag.Bool("allow_first_backup", false, "Allow this job to take the first backup of an existing shard.")
+	fullFlush        = flag.Bool("full_flush", false, "Perform a full redo & buffer flush before taking the backup, currently implemented as a clean shutdown and startup. Only makes sense to work around xtrabackup bugs.")
 
 	// vttablet-like flags
 	initDbNameOverride = flag.String("init_db_name_override", "", "(init parameter) override the name of the db used by vttablet")
@@ -390,6 +391,22 @@ func takeBackup(ctx context.Context, topoServer *topo.Server, backupStorage back
 	log.Infof("Replication caught up to %v", status.Position)
 	if !status.Position.AtLeast(primaryPos) && status.Position.Equal(restorePos) {
 		return fmt.Errorf("not taking backup: replication did not make any progress from restore point: %v", restorePos)
+	}
+
+	if *fullFlush {
+		log.Info("Proceeding with clean MySQL shutdown and startup to flush all buffers.")
+		// Prep for full/clean shutdown (not the default)
+		if err := mysqld.ExecuteSuperQuery(ctx, "SET GLOBAL innodb_fast_shutdown=0"); err != nil {
+			return fmt.Errorf("Could not prep for full shutdown: %v", err)
+		}
+		// Shutdown, waiting for it to finish
+		if err := mysqld.Shutdown(ctx, mycnf, true); err != nil {
+			return fmt.Errorf("Something went wrong during full MySQL shutdown: %v", err)
+		}
+		// Start MySQL, waiting for it to come up
+		if err := mysqld.Start(ctx, mycnf); err != nil {
+			return fmt.Errorf("Could not start MySQL after full shutdown: %v", err)
+		}
 	}
 
 	// Now we can take a new backup.
