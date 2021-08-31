a168b4dc88bf3a5471b56b8ea67997afef335934
Florent Poinsard <florent.poinsard@outlook.fr>
2021-08-31 11:14:07 +0200

    Improved 8694 E2E test and for loop in renameFields
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- go/vt/vtgate/endtoend/misc_test.go
+++ go/vt/vtgate/endtoend/misc_test.go
@@ -69,14 +69,3 @@ func TestCreateAndDropDatabase(t *testing.T) {
 		})
 	}
 }
-
-func TestRenameFieldsOnOLAP(t *testing.T) {
-	// note that this is testing vttest and not vtgate
-	conn, err := mysql.Connect(ctx, &vtParams)
-	require.NoError(t, err)
-	defer conn.Close()
-
-	_ = exec(t, conn, "set workload = olap")
-	qr := exec(t, conn, "show tables")
-	assert.Equal(t, `[[VARCHAR("aggr_test")] [VARCHAR("t1")] [VARCHAR("t1_id2_idx")] [VARCHAR("t1_last_insert_id")] [VARCHAR("t1_row_count")] [VARCHAR("t1_sharded")] [VARCHAR("t2")] [VARCHAR("t2_id4_idx")] [VARCHAR("vstream_test")]]`, fmt.Sprintf("%v", qr.Rows))
-}
