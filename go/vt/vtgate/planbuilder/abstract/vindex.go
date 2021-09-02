e8c2fea7e30f86cbe3b50cb030390421c049f726
Florent Poinsard <florent.poinsard@outlook.fr>
2021-09-02 11:32:47 +0200

    Addition of opcode and value field in the vindex operator
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- go/vt/vtgate/planbuilder/abstract/vindex.go
+++ go/vt/vtgate/planbuilder/abstract/vindex.go
@@ -17,7 +17,9 @@ limitations under the License.
 package abstract
 
 import (
+	"vitess.io/vitess/go/sqltypes"
 	"vitess.io/vitess/go/vt/sqlparser"
+	"vitess.io/vitess/go/vt/vtgate/engine"
 	"vitess.io/vitess/go/vt/vtgate/semantics"
 	"vitess.io/vitess/go/vt/vtgate/vindexes"
 )
@@ -25,10 +27,12 @@ import (
 type (
 	// Vindex stores the information about the vindex query
 	Vindex struct {
+		OpCode        engine.VindexOpcode
 		Table         VindexTable
 		Vindex        vindexes.Vindex
 		JoinPredicate []sqlparser.Expr
 		NoDeps        []sqlparser.Expr
+		Value         sqltypes.PlanValue
 	}
 
 	// VindexTable contains information about the vindex table we want to query
@@ -49,21 +53,48 @@ func (v Vindex) TableID() semantics.TableSet {
 }
 
 // PushPredicate implements the Operator interface
-func (v Vindex) PushPredicate(expr sqlparser.Expr, semTable *semantics.SemTable) error {
+func (v *Vindex) PushPredicate(expr sqlparser.Expr, semTable *semantics.SemTable) error {
 	for _, e := range sqlparser.SplitAndExpression(nil, expr) {
 		deps := semTable.BaseTableDependencies(e)
-		switch deps.NumberOfTables() {
-		case 0:
+		if deps.NumberOfTables() == 0 {
 			v.NoDeps = append(v.NoDeps, e)
-		case 1:
-			if v.TableID().IsSolvedBy(deps) {
-				v.Table.Predicates = append(v.Table.Predicates, e)
-			} else {
-				v.JoinPredicate = append(v.JoinPredicate, e)
-			}
-		default:
-			v.JoinPredicate = append(v.JoinPredicate, e)
+			continue
 		}
+		if deps.NumberOfTables() > 1 {
+			return semantics.Gen4NotSupportedF("unsupported: where clause for vindex function must be of the form id = <val> (multiple tables involved)")
+		}
+		// check if we already have a predicate
+		if v.OpCode != engine.VindexNone {
+			return semantics.Gen4NotSupportedF("unsupported: where clause for vindex function must be of the form id = <val> (multiple filters)")
+		}
+
+		// check LHS
+		comparison, ok := e.(*sqlparser.ComparisonExpr)
+		if !ok {
+			return semantics.Gen4NotSupportedF("unsupported: where clause for vindex function must be of the form id = <val> (not a comparison)")
+		}
+		if comparison.Operator != sqlparser.EqualOp {
+			return semantics.Gen4NotSupportedF("unsupported: where clause for vindex function must be of the form id = <val> (not equality)")
+		}
+		colname, ok := comparison.Left.(*sqlparser.ColName)
+		if !ok {
+			return semantics.Gen4NotSupportedF("unsupported: where clause for vindex function must be of the form id = <val> (lhs is not a column)")
+		}
+		if !colname.Name.EqualString("id") {
+			return semantics.Gen4NotSupportedF("unsupported: where clause for vindex function must be of the form id = <val> (lhs is not id)")
+		}
+
+		// check RHS
+		if !sqlparser.IsValue(comparison.Right) {
+			return semantics.Gen4NotSupportedF("unsupported: where clause for vindex function must be of the form id = <val> (rhs is not a value)")
+		}
+		var err error
+		v.Value, err = sqlparser.NewPlanValue(comparison.Right)
+		if err != nil {
+			return semantics.Gen4NotSupportedF("unsupported: where clause for vindex function must be of the form id = <val>: %v", err)
+		}
+		v.OpCode = engine.VindexMap
+		v.Table.Predicates = append(v.Table.Predicates, e)
 	}
 	return nil
 }
