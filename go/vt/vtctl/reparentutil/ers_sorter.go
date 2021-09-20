d31fd1b22197632437f291611847c46933f51247
Manan Gupta <manan@planetscale.com>
2021-09-20 17:55:41 +0530

    added sorter for ERS
    Signed-off-by: Manan Gupta <manan@planetscale.com>

---
+++ go/vt/vtctl/reparentutil/ers_sorter.go
@@ -0,0 +1,105 @@
+/*
+Copyright 2021 The Vitess Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package reparentutil
+
+import (
+	"sort"
+
+	"vitess.io/vitess/go/mysql"
+	topodatapb "vitess.io/vitess/go/vt/proto/topodata"
+	vtrpcpb "vitess.io/vitess/go/vt/proto/vtrpc"
+	"vitess.io/vitess/go/vt/vterrors"
+)
+
+// ErsSorter sorts tablets by GTID positions and Promotion rules aimed at finding the best
+// candidate for intermediate promotion in emergency reparent shard
+type ErsSorter struct {
+	tablets   []*topodatapb.Tablet
+	positions []mysql.Position
+	idealCell string
+}
+
+// NewErsSorter creates a new ErsSorter
+func NewErsSorter(tablets []*topodatapb.Tablet, positions []mysql.Position, idealCell string) *ErsSorter {
+	return &ErsSorter{
+		tablets:   tablets,
+		positions: positions,
+		idealCell: idealCell,
+	}
+}
+
+// Len implements the Interface for sorting
+func (ersSorter *ErsSorter) Len() int { return len(ersSorter.tablets) }
+
+// Swap implements the Interface for sorting
+func (ersSorter *ErsSorter) Swap(i, j int) {
+	ersSorter.tablets[i], ersSorter.tablets[j] = ersSorter.tablets[j], ersSorter.tablets[i]
+	ersSorter.positions[i], ersSorter.positions[j] = ersSorter.positions[j], ersSorter.positions[i]
+}
+
+// Less implements the Interface for sorting
+func (ersSorter *ErsSorter) Less(i, j int) bool {
+	// Returning "true" in this function means [i] is before [j],
+	// which will lead to [i] be a better candidate for promotion
+
+	// Should not happen
+	// fail-safe code
+	if ersSorter.tablets[i] == nil {
+		return false
+	}
+	if ersSorter.tablets[j] == nil {
+		return true
+	}
+
+	if !ersSorter.positions[i].AtLeast(ersSorter.positions[j]) {
+		// [i] does not have all GTIDs that [j] does
+		return false
+	}
+	if !ersSorter.positions[j].AtLeast(ersSorter.positions[i]) {
+		// [j] does not have all GTIDs that [i] does
+		return true
+	}
+
+	// at this point, both have the same GTIDs
+	// So, we will now check which one is in the same cell
+	iInSameCell := ersSorter.tablets[i].Alias.Cell == ersSorter.idealCell
+	jInSameCell := ersSorter.tablets[j].Alias.Cell == ersSorter.idealCell
+	if iInSameCell && !jInSameCell {
+		return true
+	}
+	if jInSameCell && !iInSameCell {
+		return false
+	}
+
+	// at this point, either both are in the ideal cell
+	// or neither is, so we check their promotion rules
+	jPromotionRule := PromotionRule(ersSorter.tablets[j])
+	iPromotionRule := PromotionRule(ersSorter.tablets[i])
+	return !jPromotionRule.BetterThan(iPromotionRule)
+}
+
+// sortTabletsForERS sorts the tablets, given their positions for emergency reparent shard
+func sortTabletsForERS(tablets []*topodatapb.Tablet, positions []mysql.Position, idealCell string) error {
+	// throw an error internal error in case of unequal number of tablets and positions
+	// fail-safe code prevents panic in sorting in case the lengths are unequal
+	if len(tablets) != len(positions) {
+		return vterrors.Errorf(vtrpcpb.Code_INTERNAL, "unequal number of tablets and positions")
+	}
+
+	sort.Sort(NewErsSorter(tablets, positions, idealCell))
+	return nil
+}
