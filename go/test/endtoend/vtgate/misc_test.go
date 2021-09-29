a358ad12605b05132198895b96183957bc19e9df
Florent Poinsard <florent.poinsard@outlook.fr>
2021-09-29 15:30:14 +0200

    Execute TestDerivedTableColumns using Gen4 planner since V3 does not support
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- go/test/endtoend/vtgate/misc_test.go
+++ go/test/endtoend/vtgate/misc_test.go
@@ -782,7 +782,7 @@ func TestDerivedTableColumns(t *testing.T) {
 	defer exec(t, conn, `delete from t1`)
 
 	exec(t, conn, "insert into t1(id1, id2) values (0,10),(1,9),(2,8),(3,7),(4,6),(5,5)")
-	assertMatches(t, conn, `SELECT t.id FROM (SELECT id2 FROM t1) AS t(id)`, `[[INT64(10)] [INT64(9)] [INT64(8)] [INT64(7)] [INT64(6)] [INT64(5)]]`)
+	assertMatches(t, conn, `SELECT /* GEN4_COMPARE_ONLY_GEN4 */ t.id FROM (SELECT id2 FROM t1) AS t(id) ORDER BY t.id DESC`, `[[INT64(10)] [INT64(9)] [INT64(8)] [INT64(7)] [INT64(6)] [INT64(5)]]`)
 }
 
 func assertMatches(t *testing.T, conn *mysql.Conn, query, expected string) {
