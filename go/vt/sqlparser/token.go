ddda90041e20c7a2c9a9a3329c00f049ee193e84
ritwizsinha <ritwizsinha0@gmail.com>
2021-10-15 21:00:40 +0530

    Added tokenizer support for NCHAR_STRING
    Signed-off-by: ritwizsinha <ritwizsinha0@gmail.com>

--- go/vt/sqlparser/token.go
+++ go/vt/sqlparser/token.go
@@ -160,6 +160,13 @@ func (tkn *Tokenizer) Scan() (int, string) {
 				return tkn.scanBitLiteral()
 			}
 		}
+		if ch == 'N' || ch == 'n' {
+			nxt := tkn.peek(1)
+			if nxt == '\'' || nxt == '"' {
+				tkn.skip(2)
+				return tkn.scanString(nxt, NCHAR_STRING)
+			}
+		}
 		return tkn.scanIdentifier(false)
 	case isDigit(ch):
 		return tkn.scanNumber()
