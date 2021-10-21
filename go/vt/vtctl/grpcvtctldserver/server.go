48347ac72f7608a446276e5dc133b5c0fbafaa36
Andrew Mason <amason@slack-corp.com>
2021-10-21 15:06:01 -0400

    Add Backup implementation
    Signed-off-by: Andrew Mason <andrew@planetscale.com>

--- go/vt/vtctl/grpcvtctldserver/server.go
+++ go/vt/vtctl/grpcvtctldserver/server.go
@@ -317,7 +317,48 @@ func (s *VtctldServer) ApplyVSchema(ctx context.Context, req *vtctldatapb.ApplyV
 
 // Backup is part of the vtctlservicepb.VtctldServer interface.
 func (s *VtctldServer) Backup(req *vtctldatapb.BackupRequest, stream vtctlservicepb.Vtctld_BackupServer) error {
-	panic("unimplemented!")
+	span, ctx := trace.NewSpan(stream.Context(), "VtctldServer.Backup")
+	defer span.Finish()
+
+	span.Annotate("tablet_alias", topoproto.TabletAliasString(req.TabletAlias))
+	span.Annotate("allow_primary", req.AllowPrimary)
+	span.Annotate("concurrency", req.Concurrency)
+
+	ti, err := s.ts.GetTablet(ctx, req.TabletAlias)
+	if err != nil {
+		return err
+	}
+
+	span.Annotate("keyspace", ti.Keyspace)
+	span.Annotate("shard", ti.Shard)
+
+	logStream, err := s.tmc.Backup(ctx, ti.Tablet, int(req.Concurrency), req.AllowPrimary)
+	if err != nil {
+		return err
+	}
+
+	logger := logutil.NewConsoleLogger()
+
+	for {
+		event, err := logStream.Recv()
+		switch err {
+		case nil:
+			logutil.LogEvent(logger, event)
+			resp := &vtctldatapb.BackupResponse{
+				TabletAlias: req.TabletAlias,
+				Keyspace:    ti.Keyspace,
+				Shard:       ti.Shard,
+				Event:       event,
+			}
+			if err := stream.Send(resp); err != nil {
+				logger.Errorf("failed to send stream response %+v: %v", resp, err)
+			}
+		case io.EOF:
+			return nil
+		default:
+			return err
+		}
+	}
 }
 
 // ChangeTabletType is part of the vtctlservicepb.VtctldServer interface.
