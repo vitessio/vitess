1aced3c6262f75707c4ae7bae1c89336ea5a2410
Manan Gupta <manan@planetscale.com>
2021-10-28 13:39:10 +0530

    initial support for begin stream execute in vttablet
    Signed-off-by: Manan Gupta <manan@planetscale.com>

--- go/vt/vtcombo/tablet_map.go
+++ go/vt/vtcombo/tablet_map.go
@@ -521,6 +521,13 @@ func (itc *internalTabletConn) BeginExecuteBatch(ctx context.Context, target *qu
 	return results, transactionID, alias, err
 }
 
+// BeginStreamExecute is part of queryservice.QueryService
+func (itc *internalTabletConn) BeginStreamExecute(ctx context.Context, target *querypb.Target, preQueries []string, query string, bindVars map[string]*querypb.BindVariable, reserveID int64, options *querypb.ExecuteOptions, callback func(*sqltypes.Result) error) (int64, *topodatapb.TabletAlias, error) {
+	bindVars = sqltypes.CopyBindVariables(bindVars)
+	transactionID, tabletAlias, err := itc.tablet.qsc.QueryService().BeginStreamExecute(ctx, target, preQueries, query, bindVars, reserveID, options, callback)
+	return transactionID, tabletAlias, tabletconn.ErrorFromGRPC(vterrors.ToGRPC(err))
+}
+
 // MessageStream is part of queryservice.QueryService
 func (itc *internalTabletConn) MessageStream(ctx context.Context, target *querypb.Target, name string, callback func(*sqltypes.Result) error) error {
 	err := itc.tablet.qsc.QueryService().MessageStream(ctx, target, name, callback)
--- go/vt/vttablet/queryservice/wrapped.go
+++ go/vt/vttablet/queryservice/wrapped.go
@@ -234,6 +234,16 @@ func (ws *wrappedService) BeginExecuteBatch(ctx context.Context, target *querypb
 	return qrs, transactionID, alias, err
 }
 
+func (ws *wrappedService) BeginStreamExecute(ctx context.Context, target *querypb.Target, preQueries []string, query string, bindVars map[string]*querypb.BindVariable, reservedID int64, options *querypb.ExecuteOptions, callback func(*sqltypes.Result) error) (transactionID int64, alias *topodatapb.TabletAlias, err error) {
+	inDedicatedConn := reservedID != 0
+	err = ws.wrapper(ctx, target, ws.impl, "BeginExecute", inDedicatedConn, func(ctx context.Context, target *querypb.Target, conn QueryService) (bool, error) {
+		var innerErr error
+		transactionID, alias, innerErr = conn.BeginStreamExecute(ctx, target, preQueries, query, bindVars, reservedID, options, callback)
+		return canRetry(ctx, innerErr) && !inDedicatedConn, innerErr
+	})
+	return transactionID, alias, err
+}
+
 func (ws *wrappedService) MessageStream(ctx context.Context, target *querypb.Target, name string, callback func(*sqltypes.Result) error) error {
 	return ws.wrapper(ctx, target, ws.impl, "MessageStream", false, func(ctx context.Context, target *querypb.Target, conn QueryService) (bool, error) {
 		innerErr := conn.MessageStream(ctx, target, name, callback)
