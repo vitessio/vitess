b1ccca3cc36c48c09361874486b2bef511ea1e44
Matt Lord <mattalord@gmail.com>
2021-10-29 04:29:09 -0400

    Clear tablet healthcheck cache record on EOF RPC error too
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/vt/discovery/tablet_health_check.go
+++ go/vt/discovery/tablet_health_check.go
@@ -290,8 +290,11 @@ func (thc *tabletHealthCheck) checkConn(hc *HealthCheckImpl) {
 		if err != nil {
 			hcErrorCounters.Add([]string{thc.Target.Keyspace, thc.Target.Shard, topoproto.TabletTypeLString(thc.Target.TabletType)}, 1)
 			// We have reason to suspect the tablet healthcheck record is corrupted or invalid so let's remove the tablet's record
-			// from the healthcheck cache and it will get re-added again if healthy
-			if strings.Contains(err.Error(), "health stats mismatch") || strings.HasSuffix(err.Error(), context.Canceled.Error()) {
+			// from the healthcheck cache and it will get re-added again if the tablet is reachable
+			if strings.Contains(err.Error(), "health stats mismatch") ||
+				strings.HasSuffix(err.Error(), context.Canceled.Error()) ||
+				strings.Contains(err.Error(), `"error reading from server: EOF", received prior goaway`) {
+				log.Warningf("tablet %s had a suspect healthcheck error: %s -- clearing cache record", thc.Tablet.Alias, err.Error())
 				hc.deleteTablet(thc.Tablet)
 				return
 			}
