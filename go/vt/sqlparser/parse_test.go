9b4006809d3b7e1e4b8d4173c1982a0379ba5caf
James Cor <james@dolthub.com>
2021-11-08 15:07:11 -0800

    added parse test cases

--- go/vt/sqlparser/parse_test.go
+++ go/vt/sqlparser/parse_test.go
@@ -3489,6 +3489,19 @@ func TestLoadData(t *testing.T) {
 	}
 }
 
+func TestEscape(t *testing.T) {
+	testCases := []parseTest{
+		{
+			input:                `SELECT * FROM test WHERE col LIKE "%$_%" ESCAPE "$"`,
+			output:               `select * from test where col like '%$_%' escape '$'`,
+			serializeSelectExprs: true,
+		},
+	}
+	for _, tcase := range testCases {
+		runParseTestCase(t, tcase)
+	}
+}
+
 func TestTrim(t *testing.T) {
 	testCases := []parseTest{
 		{
