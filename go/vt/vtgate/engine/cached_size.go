4c741b920a90863dcf3d58267d0761d2ba178af9
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2021-11-08 16:14:55 +0200

    Support ALTER VITESS_MIGRATION ... CLEANUP statement
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/vtgate/engine/cached_size.go
+++ go/vt/vtgate/engine/cached_size.go
@@ -343,7 +343,7 @@ func (cached *Lock) CachedSize(alloc bool) int64 {
 	}
 	size := int64(0)
 	if alloc {
-		size += int64(48)
+		size += int64(64)
 	}
 	// field Keyspace *vitess.io/vitess/go/vt/vtgate/vindexes.Keyspace
 	size += cached.Keyspace.CachedSize(true)
@@ -353,6 +353,8 @@ func (cached *Lock) CachedSize(alloc bool) int64 {
 	}
 	// field Query string
 	size += hack.RuntimeAllocSize(int64(len(cached.Query)))
+	// field FieldQuery string
+	size += hack.RuntimeAllocSize(int64(len(cached.FieldQuery)))
 	return size
 }
 func (cached *MStream) CachedSize(alloc bool) int64 {
