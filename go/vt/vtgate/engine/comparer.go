86e3abc07c58e198993dcee49abbcda71ffe07a2
Lakshya Singh <lakshay.singh1108@gmail.com>
2021-11-18 04:08:42 +0530

    refactor: simplify boolean condition
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- go/vt/vtgate/engine/comparer.go
+++ go/vt/vtgate/engine/comparer.go
@@ -44,10 +44,10 @@ func (c *comparer) compare(r1, r2 []sqltypes.Value) (int, error) {
 	if err != nil {
 		_, isComparisonErr := err.(evalengine.UnsupportedComparisonError)
 		_, isCollationErr := err.(evalengine.UnsupportedCollationError)
-		if !((isComparisonErr || isCollationErr) && c.weightString != -1) {
+		if !isComparisonErr && !isCollationErr || c.weightString == -1 {
 			return 0, err
 		}
-		// in case of a comparison error switch to using the weight string column for ordering
+		// in case of a comparison or collation error switch to using the weight string column for ordering
 		c.orderBy = c.weightString
 		c.weightString = -1
 		cmp, err = evalengine.NullsafeCompare(r1[c.orderBy], r2[c.orderBy], c.collationID)
