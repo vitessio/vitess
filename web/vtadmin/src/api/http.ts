05ff0203e4193118268ed9fd6ffe4a53879cf309
notfelineit <notfelineit@gmail.com>
2021-12-02 15:37:41 -0800

    Remove console.log
    Signed-off-by: notfelineit <notfelineit@gmail.com>

--- web/vtadmin/src/api/http.ts
+++ web/vtadmin/src/api/http.ts
@@ -33,7 +33,6 @@ import { TabletDebugVars } from '../util/tabletDebugVars';
 export const vtfetch = async (endpoint: string): Promise<HttpOkResponse> => {
     try {
         const { REACT_APP_VTADMIN_API_ADDRESS } = process.env;
-        console.log(process.env);
         const url = `${REACT_APP_VTADMIN_API_ADDRESS}${endpoint}`;
         const opts = vtfetchOpts();
 
