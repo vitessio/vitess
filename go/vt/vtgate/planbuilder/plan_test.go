9a1a56aa123fe429dd3d6bddd17542b0b0dcd77d
Andres Taylor <andres@planetscale.com>
2021-12-02 10:26:45 +0100

    test: re-insert accidentally removed test case
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/vt/vtgate/planbuilder/plan_test.go
+++ go/vt/vtgate/planbuilder/plan_test.go
@@ -398,6 +398,7 @@ func TestWithDefaultKeyspaceFromFile(t *testing.T) {
 		tabletType: topodatapb.TabletType_PRIMARY,
 	}
 
+	testFile(t, "alterVschema_cases.txt", testOutputTempDir, vschema)
 	testFile(t, "ddl_cases.txt", testOutputTempDir, vschema)
 	testFile(t, "migration_cases.txt", testOutputTempDir, vschema)
 	testFile(t, "flush_cases.txt", testOutputTempDir, vschema)
