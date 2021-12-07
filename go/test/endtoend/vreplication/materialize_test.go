7c93d32de5caac9bb0cf4911c76b668796112d10
Rohit Nayak <rohit@planetscale.com>
2021-12-07 21:21:02 +0100

    Run all materialize e2e tests from a single test
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>

--- go/test/endtoend/vreplication/materialize_test.go
+++ go/test/endtoend/vreplication/materialize_test.go
@@ -61,7 +61,7 @@ const smMaterializeSpec = `{"workflow": "wf1", "source_keyspace": "ks1", "target
 const initDataQuery = `insert into ks1.tx(id, typ, val) values (1, 1, 'abc'), (2, 1, 'def'), (3, 2, 'def'), (4, 2, 'abc'), (5, 3, 'def'), (6, 3, 'abc')`
 
 // TestShardedMaterialize tests a materialize from a sharded (single shard) using comparison filters
-func TestShardedMaterialize(t *testing.T) {
+func testShardedMaterialize(t *testing.T) {
 	defaultCellName := "zone1"
 	allCells := []string{"zone1"}
 	allCellNames = "zone1"
@@ -166,9 +166,9 @@ const smMaterializeVSchemaTarget = `
   }
 }
 `
-const smMaterializeSpec2 = `{"workflow": "wf1", "source_keyspace": "ks1", "target_keyspace": "ks2", "table_settings": [ {"target_table": "mat2", "source_expression": "select id, val, ts, dayofmonth(ts) as day, month(ts) as month, custom1(id, val) as x from mat"  }] }`
+const smMaterializeSpec2 = `{"workflow": "wf1", "source_keyspace": "source", "target_keyspace": "target", "table_settings": [ {"target_table": "mat2", "source_expression": "select id, val, ts, dayofmonth(ts) as day, month(ts) as month, custom1(id, val) as x from mat"  }] }`
 
-const materializeInitDataQuery = `insert into ks1.mat(id, val, ts) values (1, 'abc', '2021-10-9 16:17:36'), (2, 'def', '2021-11-10 16:17:36')`
+const materializeInitDataQuery = `insert into mat(id, val, ts) values (1, 'abc', '2021-10-9 16:17:36'), (2, 'def', '2021-11-10 16:17:36')`
 
 const customFunc = `
 CREATE FUNCTION custom1(id int, val varbinary(10))
@@ -178,13 +178,13 @@ RETURN id * length(val);
 `
 
 // TestMaterialize: details mentioned above
-func TestMaterialize(t *testing.T) {
+func testMaterialize(t *testing.T) {
 	defaultCellName := "zone1"
 	allCells := []string{"zone1"}
 	allCellNames = "zone1"
 	vc = NewVitessCluster(t, "TestMaterialize", allCells, mainClusterConfig)
-	ks1 := "ks1"
-	ks2 := "ks2"
+	sourceKs := "source"
+	targetKs := "target"
 	require.NotNil(t, vc)
 	defaultReplicas = 0 // because of CI resource constraints we can only run this test with primary tablets
 	defer func() { defaultReplicas = 1 }()
@@ -192,13 +192,13 @@ func TestMaterialize(t *testing.T) {
 	defer vc.TearDown(t)
 
 	defaultCell = vc.Cells[defaultCellName]
-	vc.AddKeyspace(t, []*Cell{defaultCell}, ks1, "-", smMaterializeVSchemaSource, smMaterializeSchemaSource, defaultReplicas, defaultRdonly, 100)
+	vc.AddKeyspace(t, []*Cell{defaultCell}, sourceKs, "-", smMaterializeVSchemaSource, smMaterializeSchemaSource, defaultReplicas, defaultRdonly, 300)
 	vtgate = defaultCell.Vtgates[0]
 	require.NotNil(t, vtgate)
-	vtgate.WaitForStatusOfTabletInShard(fmt.Sprintf("%s.%s.primary", ks1, "0"), 1)
+	vtgate.WaitForStatusOfTabletInShard(fmt.Sprintf("%s.%s.primary", sourceKs, "0"), 1)
 
-	vc.AddKeyspace(t, []*Cell{defaultCell}, ks2, "-", smMaterializeVSchemaTarget, smMaterializeSchemaTarget, defaultReplicas, defaultRdonly, 200)
-	vtgate.WaitForStatusOfTabletInShard(fmt.Sprintf("%s.%s.primary", ks2, "0"), 1)
+	vc.AddKeyspace(t, []*Cell{defaultCell}, targetKs, "-", smMaterializeVSchemaTarget, smMaterializeSchemaTarget, defaultReplicas, defaultRdonly, 400)
+	vtgate.WaitForStatusOfTabletInShard(fmt.Sprintf("%s.%s.primary", targetKs, "0"), 1)
 
 	vtgateConn = getConnection(t, vc.ClusterConfig.hostname, vc.ClusterConfig.vtgateMySQLPort)
 	defer vtgateConn.Close()
@@ -207,25 +207,33 @@ func TestMaterialize(t *testing.T) {
 	_, err := vtgateConn.ExecuteFetch(materializeInitDataQuery, 0, false)
 	require.NoError(t, err)
 
-	ks2Keyspace := vc.Cells[defaultCell.Name].Keyspaces["ks2"]
-	ks2Primary := ks2Keyspace.Shards["-"].Tablets["zone1-200"].Vttablet
-	_, err = ks2Primary.QueryTablet(customFunc, "ks2", true)
+	ks2Keyspace := vc.Cells[defaultCell.Name].Keyspaces[targetKs]
+	ks2Primary := ks2Keyspace.Shards["-"].Tablets["zone1-400"].Vttablet
+	_, err = ks2Primary.QueryTablet(customFunc, targetKs, true)
 	require.NoError(t, err)
 
 	materialize(t, smMaterializeSpec2)
-	tab := vc.Cells[defaultCell.Name].Keyspaces[ks2].Shards["-"].Tablets["zone1-200"].Vttablet
-	catchup(t, tab, "wf1", "Materialize")
+	catchup(t, ks2Primary, "wf1", "Materialize")
 
 	// validate data after the copy phase
-	validateCount(t, vtgateConn, ks2, "mat2", 2)
+	validateCount(t, vtgateConn, targetKs, "mat2", 2)
 	want := `[[INT64(1) VARBINARY("abc") TIMESTAMP("2021-10-09 16:17:36") INT32(9) INT32(10) INT32(3)] [INT64(2) VARBINARY("def") TIMESTAMP("2021-11-10 16:17:36") INT32(10) INT32(11) INT32(6)]]`
-	validateQuery(t, vtgateConn, "ks2:-", "select id, val, ts, day, month, x from mat2", want)
+	validateQuery(t, vtgateConn, targetKs, "select id, val, ts, day, month, x from mat2", want)
 
 	// insert data to test the replication phase
-	execVtgateQuery(t, vtgateConn, "ks1", "insert into ks1.mat(id, val, ts) values (3, 'ghi', '2021-12-11 16:17:36')")
+	execVtgateQuery(t, vtgateConn, sourceKs, "insert into mat(id, val, ts) values (3, 'ghi', '2021-12-11 16:17:36')")
 
 	// validate data after the replication phase
-	waitForQueryToExecute(t, vtgateConn, "ks2", "select count(*) from mat2", "[[INT64(3)]]")
+	waitForQueryToExecute(t, vtgateConn, targetKs, "select count(*) from mat2", "[[INT64(3)]]")
 	want = `[[INT64(1) VARBINARY("abc") TIMESTAMP("2021-10-09 16:17:36") INT32(9) INT32(10) INT32(3)] [INT64(2) VARBINARY("def") TIMESTAMP("2021-11-10 16:17:36") INT32(10) INT32(11) INT32(6)] [INT64(3) VARBINARY("ghi") TIMESTAMP("2021-12-11 16:17:36") INT32(11) INT32(12) INT32(9)]]`
-	validateQuery(t, vtgateConn, "ks2:-", "select id, val, ts, day, month, x from mat2", want)
+	validateQuery(t, vtgateConn, targetKs, "select id, val, ts, day, month, x from mat2", want)
+}
+
+func TestMaterialize(t *testing.T) {
+	t.Run("Materialize", func(t *testing.T) {
+		testMaterialize(t)
+	})
+	t.Run("ShardedMaterialize", func(t *testing.T) {
+		testShardedMaterialize(t)
+	})
 }
