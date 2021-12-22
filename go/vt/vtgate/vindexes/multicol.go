91adbbd963f07dfc8cbeec45d0c49b5675429c99
Harshit Gangal <harshit@planetscale.com>
2021-12-22 17:33:20 +0530

    feat: added column bytes parameter to multicol vindex implementation
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/vt/vtgate/vindexes/multicol.go
+++ go/vt/vtgate/vindexes/multicol.go
@@ -17,6 +17,7 @@ limitations under the License.
 package vindexes
 
 import (
+	"math"
 	"strconv"
 	"strings"
 
@@ -29,37 +30,54 @@ import (
 var _ MultiColumn = (*MultiCol)(nil)
 
 type MultiCol struct {
-	name      string
-	cost      int
-	noOfCols  int
-	columnVdx map[int]Vindex
+	name        string
+	cost        int
+	noOfCols    int
+	columnVdx   map[int]Vindex
+	columnBytes map[int]int
 }
 
 const (
 	paramColumnCount  = "column_count"
+	paramColumnBytes  = "column_bytes"
 	paramColumnVindex = "column_vindex"
 	defaultVindex     = "hash"
 )
 
 // NewMultiCol creates a new MultiCol.
 func NewMultiCol(name string, m map[string]string) (Vindex, error) {
-	colCountStr, ok := m[paramColumnCount]
-	if !ok {
-		return nil, vterrors.Errorf(vtrpcpb.Code_INVALID_ARGUMENT, "number of columns not provided in the parameter '%s'", paramColumnCount)
-	}
-	colCount, err := strconv.Atoi(colCountStr)
+	colCount, err := getColumnCount(m)
 	if err != nil {
 		return nil, err
 	}
+	columnBytes, err := getColumnBytes(m, colCount)
+	if err != nil {
+		return nil, err
+	}
+	columnVdx, vindexCost, err := getColumnVindex(m, colCount)
+	if err != nil {
+		return nil, err
+	}
+
+	return &MultiCol{
+		name:        name,
+		cost:        vindexCost,
+		noOfCols:    colCount,
+		columnVdx:   columnVdx,
+		columnBytes: columnBytes,
+	}, nil
+}
+
+func getColumnVindex(m map[string]string, colCount int) (map[int]Vindex, int, error) {
 	var colVdxs []string
 	colVdxsStr, ok := m[paramColumnVindex]
 	if ok {
 		colVdxs = strings.Split(colVdxsStr, ",")
 	}
 	if len(colVdxs) > colCount {
-		return nil, vterrors.Errorf(vtrpcpb.Code_INVALID_ARGUMENT, "number of vindex function provided are more than column count in the parameter '%s'", paramColumnVindex)
+		return nil, 0, vterrors.Errorf(vtrpcpb.Code_INVALID_ARGUMENT, "number of vindex function provided are more than column count in the parameter '%s'", paramColumnVindex)
 	}
-	columnVdx := map[int]Vindex{}
+	columnVdx := make(map[int]Vindex, colCount)
 	vindexCost := 0
 	for i := 0; i < colCount; i++ {
 		selVdx := defaultVindex
@@ -72,20 +90,75 @@ func NewMultiCol(name string, m map[string]string) (Vindex, error) {
 		// TODO: reuse vindex. avoid creating same vindex.
 		vdx, err := CreateVindex(selVdx, selVdx, m)
 		if err != nil {
-			return nil, err
+			return nil, 0, err
 		}
 		if !vdx.IsUnique() || vdx.NeedsVCursor() {
-			return nil, vterrors.Errorf(vtrpcpb.Code_INVALID_ARGUMENT, "multicol vindex supports only unique and non-vcursor vindex function, passed vindex '%s' is invalid", selVdx)
+			return nil, 0, vterrors.Errorf(vtrpcpb.Code_INVALID_ARGUMENT, "multicol vindex supports only unique and non-vcursor vindex function, passed vindex '%s' is invalid", selVdx)
 		}
 		vindexCost = vindexCost + vdx.Cost()
 		columnVdx[i] = vdx
+
 	}
-	return &MultiCol{
-		name:      name,
-		cost:      vindexCost,
-		noOfCols:  colCount,
-		columnVdx: columnVdx,
-	}, nil
+	return columnVdx, vindexCost, nil
+}
+
+func getColumnBytes(m map[string]string, colCount int) (map[int]int, error) {
+	var colByteStr []string
+	colBytesStr, ok := m[paramColumnBytes]
+	if ok {
+		colByteStr = strings.Split(colBytesStr, ",")
+	}
+	if len(colByteStr) > colCount {
+		return nil, vterrors.Errorf(vtrpcpb.Code_INVALID_ARGUMENT, "number of column bytes provided are more than column count in the parameter '%s'", paramColumnBytes)
+	}
+	// validate bytes count
+	bytesUsed := 0
+	columnBytes := make(map[int]int, colCount)
+	for idx, byteStr := range colByteStr {
+		if byteStr == "" {
+			continue
+		}
+		colByte, err := strconv.Atoi(byteStr)
+		if err != nil {
+			return nil, err
+		}
+		bytesUsed = bytesUsed + colByte
+		columnBytes[idx] = colByte
+	}
+	pendingCol := colCount - len(columnBytes)
+	remainingBytes := 8 - bytesUsed
+	if pendingCol > remainingBytes {
+		return nil, vterrors.Errorf(vtrpcpb.Code_INVALID_ARGUMENT, "column bytes count exceeds the keyspace id length (total bytes count cannot exceed 8 bytes) in the parameter '%s'", paramColumnBytes)
+	}
+	if pendingCol <= 0 {
+		return columnBytes, nil
+	}
+	colIdx := 0
+	for idx := 0; idx < colCount; idx++ {
+		if _, defined := columnBytes[colIdx]; defined {
+			continue
+		}
+		bytesToAssign := int(math.Ceil(float64(remainingBytes) / float64(pendingCol)))
+		columnBytes[idx] = bytesToAssign
+		remainingBytes = remainingBytes - bytesToAssign
+		pendingCol--
+	}
+	return columnBytes, nil
+}
+
+func getColumnCount(m map[string]string) (int, error) {
+	colCountStr, ok := m[paramColumnCount]
+	if !ok {
+		return 0, vterrors.Errorf(vtrpcpb.Code_INVALID_ARGUMENT, "number of columns not provided in the parameter '%s'", paramColumnCount)
+	}
+	colCount, err := strconv.Atoi(colCountStr)
+	if err != nil {
+		return 0, err
+	}
+	if colCount > 8 || colCount < 1 {
+		return 0, vterrors.Errorf(vtrpcpb.Code_INVALID_ARGUMENT, "number of columns should be between 1 and 8 in the parameter '%s'", paramColumnCount)
+	}
+	return colCount, nil
 }
 
 func init() {
