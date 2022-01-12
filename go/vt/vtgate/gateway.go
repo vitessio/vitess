e9caecd7b25ff55b3e9876616f6eee2e80b65d4d
deepthi <deepthi@planetscale.com>
2022-01-12 10:37:00 -0800

    fix scope for variable that was unintentionally exported
    Signed-off-by: deepthi <deepthi@planetscale.com>

--- go/vt/vtgate/gateway.go
+++ go/vt/vtgate/gateway.go
@@ -37,8 +37,8 @@ import (
 var (
 	_ = flag.String("gateway_implementation", "", "Deprecated. Only tabletgateway is now supported, discoverygateway is no longer available")
 	// We cannot reference tabletGatewayImplementation directly because it is const
-	DefaultGatewayImplementation = tabletGatewayImplementation
-	GatewayImplementation        = &DefaultGatewayImplementation
+	defaultGatewayImplementation = tabletGatewayImplementation
+	GatewayImplementation        = &defaultGatewayImplementation
 	bufferImplementation         = flag.String("buffer_implementation", "keyspace_events", "Allowed values: healthcheck (legacy implementation), keyspace_events (default)")
 	initialTabletTimeout         = flag.Duration("gateway_initial_tablet_timeout", 30*time.Second, "At startup, the gateway will wait up to that duration to get one tablet per keyspace/shard/tablettype")
 	// RetryCount is the number of times a query will be retried on error
