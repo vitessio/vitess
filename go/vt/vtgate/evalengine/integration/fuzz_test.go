c2251278529a35c1ca4f19a145e9ad0050453ac6
Vicent Marti <vmg@strn.cat>
2022-01-18 18:09:20 +0100

    evalengine: enable integration tests again
    Signed-off-by: Vicent Marti <vmg@strn.cat>

--- go/vt/vtgate/evalengine/integration/fuzz_test.go
+++ go/vt/vtgate/evalengine/integration/fuzz_test.go
@@ -75,8 +75,6 @@ func (d dummyCollation) CollationIDLookup(_ sqlparser.Expr) collations.ID {
 }
 
 func TestTypes(t *testing.T) {
-	t.Skipf("temporarily disabled because of MySQL upgrade")
-
 	var conn = mysqlconn(t)
 	defer conn.Close()
 
