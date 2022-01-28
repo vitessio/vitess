db3b82dc38fb5dbe201eb2944007a376a1344ed6
Florent Poinsard <florent.poinsard@outlook.fr>
2022-01-28 16:04:46 +0100

    Removed unrequired bash set
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- tools/do_release.sh
+++ tools/do_release.sh
@@ -14,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -euo pipefail
-
 ROOT=$(pwd)
 if [ "$VTROOT" != "" ]; then
     ROOT=$VTROOT
