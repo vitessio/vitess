d1a63457d1a4d356a2af0d832f422363f9be5f23
Florent Poinsard <florent.poinsard@outlook.fr>
2022-02-03 08:58:21 +0100

    chore: divided version.go in two parts
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- go/vt/servenv/mysql.go
+++ go/vt/servenv/mysql.go
@@ -14,9 +14,11 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// THIS FILE IS AUTO-GENERATED DURING NEW RELEASES
-// DO NOT EDIT
-
 package servenv
 
-const versionName = "13.0.0-SNAPSHOT"
+import "flag"
+
+// MySQLServerVersion is what Vitess will present as it's version during the connection handshake,
+// and as the value to the @@version system variable. If nothing is provided, Vitess will report itself as
+// a specific MySQL version with the vitess version appended to it
+var MySQLServerVersion = flag.String("mysql_server_version", "", "MySQL server version to advertise.")
