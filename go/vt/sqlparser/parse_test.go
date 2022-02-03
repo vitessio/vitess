b1e4b6d4b21f882cae82ac602ca6398ec14718db
Vicent Marti <vmg@strn.cat>
2022-02-03 16:52:09 +0100

    sqlparser: fix parens around default values
    Signed-off-by: Vicent Marti <vmg@strn.cat>

--- go/vt/sqlparser/parse_test.go
+++ go/vt/sqlparser/parse_test.go
@@ -1355,10 +1355,10 @@ var (
 		input: "create table a (\n\ta int not null default 0\n)",
 	}, {
 		input:  "create table a (\n\ta float not null default -1\n)",
-		output: "create table a (\n\ta float not null default (-1)\n)",
+		output: "create table a (\n\ta float not null default -1\n)",
 	}, {
 		input:  "create table a (\n\ta float not null default -2.1\n)",
-		output: "create table a (\n\ta float not null default (-2.1)\n)",
+		output: "create table a (\n\ta float not null default -2.1\n)",
 	}, {
 		input:  "create table a (a int not null default 0, primary key(a))",
 		output: "create table a (\n\ta int not null default 0,\n\tprimary key (a)\n)",
