928321f1466e54b12c5511738fcbdc0a88134cea
Manan Gupta <manan@planetscale.com>
2022-02-07 17:59:16 +0530

    feat: add a function to check if we have reached enough tablets to prevent a tablet from making forward progress
    Signed-off-by: Manan Gupta <manan@planetscale.com>

--- go/vt/vtctl/reparentutil/durability.go
+++ go/vt/vtctl/reparentutil/durability.go
@@ -21,6 +21,7 @@ import (
 	"vitess.io/vitess/go/vt/topo/topoproto"
 )
 
+// SemiSyncACKersForPrimary returns the list of tablets which are capable of sending Semi-Sync ACKs for the given primary tablet
 func SemiSyncACKersForPrimary(primary *topodatapb.Tablet, allTablets []*topodatapb.Tablet) (semiSyncACKers []*topodatapb.Tablet) {
 	for _, tablet := range allTablets {
 		if topoproto.TabletAliasEqual(primary.Alias, tablet.Alias) {
@@ -31,4 +32,26 @@ func SemiSyncACKersForPrimary(primary *topodatapb.Tablet, allTablets []*topodata
 		}
 	}
 	return
+}
+
+// RevokeForTablet checks whether we have reached enough tablets such that the given primary capable tablet cannot accept any new transaction
+func RevokeForTablet(primaryCapable *topodatapb.Tablet, tabletsReached []*topodatapb.Tablet, allTablets []*topodatapb.Tablet) bool {
+	// if we have reached the primaryCapable tablet and stopped its replication, then it will not
+	// accept any new transactions
+	if topoproto.TabletInList(primaryCapable, tabletsReached) {
+		return true
+	}
+
+	// semiSyncACKersReached is the list of reachable tablets capable of sending semi sync ACKs for the given primaryCapable tablet
+	semiSyncACKersReached := SemiSyncACKersForPrimary(primaryCapable, tabletsReached)
+
+	// allSemiSyncACKers is the list of reachable tablets capable of sending semi sync ACKs for the given primaryCapable tablet
+	allSemiSyncACKers := SemiSyncACKersForPrimary(primaryCapable, allTablets)
+
+	// numOfSemiSyncACKsRequired is the number of semi sync ACKs that the primaryCapable tablet requires
+	numOfSemiSyncACKsRequired := SemiSyncAckers(primaryCapable)
+
+	// if we have reached enough semi-sync ACKing tablets such that the primaryCapable cannot accept a transaction
+	// we have revoked from the tablet
+	return len(allSemiSyncACKers)-len(semiSyncACKersReached) < numOfSemiSyncACKsRequired
 }
