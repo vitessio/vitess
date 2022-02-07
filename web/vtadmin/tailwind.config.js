2d2a00a4d39c2fa6cc33fd5231b84a674771266e
Sara Bee <855595+doeg@users.noreply.github.com>
2022-02-07 13:09:25 -0500

    Update tailwind.config.js: rename 'purge' to 'content'
    Signed-off-by: Sara Bee <855595+doeg@users.noreply.github.com>

--- web/vtadmin/tailwind.config.js
+++ web/vtadmin/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false,
     theme: {
         extend: {
