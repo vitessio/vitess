da235075b69696a19140b57949340f3677453636
Andres Taylor <andres@planetscale.com>
2022-02-16 16:52:27 +0100

    refactor: remove unneeded case
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/vt/vtgate/planbuilder/builder.go
+++ go/vt/vtgate/planbuilder/builder.go
@@ -103,7 +103,7 @@ func getConfiguredPlanner(vschema plancontext.VSchema, v3planner func(string) se
 	switch planner {
 	case Gen4CompareV3:
 		return gen4CompareV3Planner(query), nil
-	case Gen4, Gen4Left2Right, Gen4GreedyOnly:
+	case Gen4Left2Right, Gen4GreedyOnly:
 		return gen4Planner(query, planner), nil
 	case Gen4WithFallback:
 		fp := &fallbackPlanner{
