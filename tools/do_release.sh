0d2562215e2fb058ef6e2f25abef1738f38a3514
Florent Poinsard <florent.poinsard@outlook.fr>
2022-02-20 18:01:34 +0100

    fixed: applied review suggestions to make the code cleaner
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- tools/do_release.sh
+++ tools/do_release.sh
@@ -42,17 +42,19 @@ if [ "$GODOC_RELEASE_VERSION" == "" ]; then
 fi
 
 function updateVersionGo () {
-  echo "/*
+
+  cat << EOF > ${ROOT}/go/vt/servenv/version.go
+/*
 Copyright 2022 The Vitess Authors.
 
-Licensed under the Apache License, Version 2.0 (the \"License\");
+Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an \"AS IS\" BASIS,
+distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
@@ -60,13 +62,16 @@ limitations under the License.
 
 package servenv
 
-const versionName = \"$1\"
+// THIS FILE IS AUTO-GENERATED DURING NEW RELEASES BY ./tools/do_releases.sh
+// DO NOT EDIT
 
-" > $ROOT/go/vt/servenv/version.go
+const versionName = "${1}"
+EOF
+
 }
 
 function updateJava () {
-  cd $ROOT/java
+  cd $ROOT/java || exit 1
   mvn versions:set -DnewVersion=$1
 }
 
@@ -86,7 +91,7 @@ git_status_output=$(git status --porcelain)
 if [ "$git_status_output" == "" ]; then
   	echo so much clean
 else
-    echo cannot do release with dirty git state
+    echo "cannot do release with dirty git state"
     exit 1
 fi
 
