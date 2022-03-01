1bff20c40faebee4af5b954eceaf4f3e48ae4d1c
Matt Lord <mattalord@gmail.com>
2022-03-01 01:12:08 -0500

    Correct GroupReplication flavor unit test
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/mysql/flavor_mysqlgr_test.go
+++ go/mysql/flavor_mysqlgr_test.go
@@ -33,8 +33,8 @@ func TestMysqlGRParsePrimaryGroupMember(t *testing.T) {
 	parsePrimaryGroupMember(&res, rows)
 	assert.Equal(t, "host1", res.SourceHost)
 	assert.Equal(t, 10, res.SourcePort)
-	assert.Equal(t, ReplicationStateStopped, res.IOState)
-	assert.Equal(t, ReplicationStateStopped, res.SQLState)
+	assert.Equal(t, ReplicationStateUnknown, res.IOState)
+	assert.Equal(t, ReplicationStateUnknown, res.SQLState)
 }
 
 func TestMysqlGRReplicationApplierLagParse(t *testing.T) {
