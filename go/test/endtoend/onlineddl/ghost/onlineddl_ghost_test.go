34f393d06f2276d96ffef97b20aebeaf70ca91d8
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2022-03-02 14:55:12 +0200

    reorder arguments
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/test/endtoend/onlineddl/ghost/onlineddl_ghost_test.go
+++ go/test/endtoend/onlineddl/ghost/onlineddl_ghost_test.go
@@ -159,8 +159,8 @@ func TestMain(m *testing.M) {
 		clusterInstance.VtctldExtraArgs = []string{
 			"-schema_change_dir", schemaChangeDirectory,
 			"-schema_change_controller", "local",
-			"-online_ddl_check_interval", "3s",
 			"-schema_change_check_interval", "1",
+			"-online_ddl_check_interval", "3s",
 		}
 
 		clusterInstance.VtTabletExtraArgs = []string{
