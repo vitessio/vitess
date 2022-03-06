d5022ad79e33e0b5eb4ca18558ff51a3920c1d33
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2022-03-06 08:57:55 +0200

    comments: clarify why we reload schema again in rowstreamer
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/vttablet/tabletserver/vstreamer/rowstreamer.go
+++ go/vt/vttablet/tabletserver/vstreamer/rowstreamer.go
@@ -125,7 +125,17 @@ func (rs *rowStreamer) buildPlan() error {
 
 	st, err := rs.se.GetTableForPos(fromTable, "")
 	if err != nil {
-		// A one-time chance of extra reload
+		// There is a scenario where vstreamer's table state can be out-of-date, and this happens
+		// with vitess migrations, based on vreplication.
+		// Vitess migrations use an elaborate cut-over flow where tables are swapped away while traffic is
+		// being blocked. The RENAME flow is such that at some point the table is renamed away, leaving a
+		// "puncture"; this is an event the is captured by vstreamer. The completion of the flow fixes the
+		// puncture, and places a new table under the original table's name, but the way it is done does not
+		// cause vstreamer to refresh schema state.
+		// there is therefore a reproducable valid sequence of events where vstreamer thinks a table does not exist,
+		// where it in fact does exist.
+		// For this reason we give vstreamer a "second chance" to review the up-to-date state of the schema.
+		// In the future, we will reduce this operation to reading a single table rather than the entire schema.
 		rs.se.ReloadAt(context.Background(), mysql.Position{})
 		st, err = rs.se.GetTableForPos(fromTable, "")
 	}
