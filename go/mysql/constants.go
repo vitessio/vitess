a8c963600face27139242025c9531101edbb2eea
Matt Lord <mattalord@gmail.com>
2022-03-11 12:56:40 -0500

    Manage MySQL Replication Status States Properly (#9853)
    * Consider repl NOT running with IO_Thread connecting and last attempt failed
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Use replication IO/SQL thread status and last IO/SQL errors in ReplicationStatus
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Minor changes & improvements after review
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Correct GroupReplication flavor unit test
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Correcting minor mistakes in func comment
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Remove unintentional behavior changes
    
    And make function names nicer (removing double context)
    
    And add some helper functions
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * One more place where we would have slightly changed behavior
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * One more spot that keep existing behavior
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Modify the replicationdata proto w/o breaking backwards compat
    
    Update vtadmin web protobufs
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/mysql/constants.go
+++ go/mysql/constants.go
@@ -680,3 +680,33 @@ func IsSchemaApplyError(err error) bool {
 	}
 	return false
 }
+
+type ReplicationState int
+
+const (
+	ReplicationStateUnknown ReplicationState = iota
+	ReplicationStateStopped
+	ReplicationStateConnecting
+	ReplicationStateRunning
+)
+
+// ReplicationStatusToState converts a value you have for the IO thread(s) or SQL
+// thread(s) or Group Replication applier thread(s) from MySQL or intermediate
+// layers to a mysql.ReplicationState.
+// on,yes,true == ReplicationStateRunning
+// off,no,false == ReplicationStateStopped
+// connecting == ReplicationStateConnecting
+// anything else == ReplicationStateUnknown
+func ReplicationStatusToState(s string) ReplicationState {
+	// Group Replication uses ON instead of Yes
+	switch strings.ToLower(s) {
+	case "yes", "on", "true":
+		return ReplicationStateRunning
+	case "no", "off", "false":
+		return ReplicationStateStopped
+	case "connecting":
+		return ReplicationStateConnecting
+	default:
+		return ReplicationStateUnknown
+	}
+}
