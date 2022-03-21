757acdf5015045f89d64b87d59bad95ef515964e
Andrew Mason <andrew@planetscale.com>
2022-03-21 09:11:30 -0400

    [endtoend/vreplication] update flags in tests (#9918)
    Signed-off-by: Andrew Mason <andrew@planetscale.com>

--- go/test/endtoend/vreplication/vreplication_test.go
+++ go/test/endtoend/vreplication/vreplication_test.go
@@ -28,20 +28,18 @@ import (
 	"time"
 
 	"github.com/buger/jsonparser"
-
-	"vitess.io/vitess/go/sqltypes"
-	"vitess.io/vitess/go/test/utils"
-	querypb "vitess.io/vitess/go/vt/proto/query"
-
-	"vitess.io/vitess/go/vt/log"
-
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"vitess.io/vitess/go/mysql"
+	"vitess.io/vitess/go/sqltypes"
 	"vitess.io/vitess/go/test/endtoend/cluster"
+	"vitess.io/vitess/go/test/utils"
+	"vitess.io/vitess/go/vt/log"
 	throttlebase "vitess.io/vitess/go/vt/vttablet/tabletserver/throttle/base"
 	"vitess.io/vitess/go/vt/wrangler"
+
+	querypb "vitess.io/vitess/go/vt/proto/query"
 )
 
 var (
@@ -987,7 +985,7 @@ func catchup(t *testing.T, vttablet *cluster.VttabletProcess, workflow, info str
 
 func moveTables(t *testing.T, cell, workflow, sourceKs, targetKs, tables string) {
 	if err := vc.VtctlClient.ExecuteCommand("MoveTables", "--", "--v1", "--cells="+cell, "--workflow="+workflow,
-		"-tablet_types="+"primary,replica,rdonly", sourceKs, targetKs, tables); err != nil {
+		"--tablet_types="+"primary,replica,rdonly", sourceKs, targetKs, tables); err != nil {
 		t.Fatalf("MoveTables command failed with %+v\n", err)
 	}
 }
