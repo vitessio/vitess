b5187dc7275b3e7c82c47bfbcd5bbbdea7c84dc6
Andrew Mason <andrew@planetscale.com>
2022-04-23 15:41:33 -0400

    more tests
    Signed-off-by: Andrew Mason <andrew@planetscale.com>

--- go/vt/vtadmin/cache/cache_test.go
+++ go/vt/vtadmin/cache/cache_test.go
@@ -18,6 +18,7 @@ package cache_test
 
 import (
 	"context"
+	"fmt"
 	"testing"
 	"time"
 
@@ -89,5 +90,104 @@ func TestBackfillDuplicates(t *testing.T) {
 
 			assert.Equal(t, tt.expectedCallCount, callcount, tt.assertionMsg)
 		})
+	}
+}
+
+func TestBackfillTTL(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name              string
+		cfg               cache.Config
+		fillSleep         time.Duration
+		enqueueCount      int
+		expectedCallCount int
+		assertionMsg      string
+	}{
+		{
+			name: "within backfill ttl",
+			cfg: cache.Config{
+				BackfillRequestTTL:               time.Millisecond * 50,
+				BackfillRequestDuplicateInterval: time.Second,
+				BackfillEnqueueWaitTime:          time.Second,
+			},
+			fillSleep:         time.Millisecond * 10,
+			enqueueCount:      2,
+			expectedCallCount: 1,
+			assertionMsg:      "both requests are within TTL and should have been filled",
+		},
+		{
+			name: "backfill ttl exceeded",
+			cfg: cache.Config{
+				BackfillRequestTTL:               time.Millisecond * 10,
+				BackfillRequestDuplicateInterval: time.Millisecond,
+				BackfillEnqueueWaitTime:          time.Second,
+			},
+			fillSleep:         time.Millisecond * 50,
+			enqueueCount:      2,
+			expectedCallCount: 1,
+			assertionMsg:      "second request exceeds TTL and should not have been filled",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			var callcount int
+			c := cache.New(func(ctx context.Context, req testkey) (any, error) {
+				time.Sleep(tt.fillSleep) // make fills take time so that requests in the queue can exceed ttl
+				callcount++
+				return nil, nil
+			}, tt.cfg)
+
+			key := testkey("testkey")
+			for i := 0; i < tt.enqueueCount; i++ {
+				if !c.EnqueueBackfill(key) {
+					assert.Fail(t, "failed to enqueue backfill for key %s", key)
+				}
+			}
+
+			time.Sleep(tt.fillSleep * time.Duration(tt.enqueueCount))
+			c.Close()
+
+			assert.Equal(t, tt.expectedCallCount, callcount, tt.assertionMsg)
+		})
+	}
+}
+
+type intkey int
+
+func (k intkey) Key() string { return fmt.Sprintf("%d", int(k)) }
+
+func TestEnqueueBackfillTimeout(t *testing.T) {
+	t.Parallel()
+
+	var callcount int
+	c := cache.New(func(ctx context.Context, req intkey) (any, error) {
+		time.Sleep(time.Millisecond * 50) // make fills take time so that the second enqueue exceeds WaitTimeout
+		callcount++
+		return nil, nil
+	}, cache.Config{
+		BackfillEnqueueWaitTime: time.Millisecond * 10,
+	})
+
+	var enqueues = []struct {
+		shouldFail bool
+		msg        string
+	}{
+		{
+			shouldFail: false,
+			msg:        "not exceed",
+		},
+		{
+			shouldFail: true,
+			msg:        "exceed",
+		},
+	}
+	for i, q := range enqueues {
+		ok := c.EnqueueBackfill(intkey(i))
+		assert.Equal(t, q.shouldFail, !ok, "enqueue should %s wait timeout", q.msg)
 	}
 }
