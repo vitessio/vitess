81eb23a361b83a09818c634d7576b19319541ed1
Sara Bee <855595+doeg@users.noreply.github.com>
2022-04-26 10:33:50 -0400

    Simplify local example
    Signed-off-by: Sara Bee <855595+doeg@users.noreply.github.com>

--- examples/local/scripts/vtadmin-down.sh
+++ examples/local/scripts/vtadmin-down.sh
@@ -2,8 +2,5 @@
 
 source ./env.sh
 
-echo "Stopping vtadmin-web..."
-kill -9 "$(cat "$VTDATAROOT/tmp/vtadmin-web.pid")"
-
-echo "Stopping vtadmin-api..."
-kill -9 "$(cat "$VTDATAROOT/tmp/vtadmin-api.pid")"
+echo "Stopping vtadmin..."
+kill -9 "$(cat "$VTDATAROOT/tmp/vtadmin.pid")"
