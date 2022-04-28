011ddf7a24571d22fd6273fd9fed229f454849bd
Harshit Gangal <harshit@planetscale.com>
2022-04-28 14:44:55 +0530

    fix: handle rconn reset when tx killer has locked the connection (#10153)
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/vt/vterrors/constants.go
+++ go/vt/vterrors/constants.go
@@ -41,3 +41,9 @@ const (
 	// PrimaryVindexNotSet is the error message to be used when there is no primary vindex found on a table
 	PrimaryVindexNotSet = "table '%s' does not have a primary vindex"
 )
+
+// TxKillerRollback purpose when acquire lock on connection for rolling back transaction.
+const TxKillerRollback = "in use: for tx killer rollback"
+
+// TxClosed regex for connection closed
+var TxClosed = regexp.MustCompile("transaction ([a-z0-9:]+) (?:ended|not found|in use: for tx killer rollback)")
