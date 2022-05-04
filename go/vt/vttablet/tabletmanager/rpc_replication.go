e5cf1762a9c4d5a91b853784ba1ccffc52a2c838
Manan Gupta <manan@planetscale.com>
2022-05-04 10:31:40 +0530

    feat: remove the deprecation notice from the enable_semi_sync flag
    Signed-off-by: Manan Gupta <manan@planetscale.com>

--- go/vt/vttablet/tabletmanager/rpc_replication.go
+++ go/vt/vttablet/tabletmanager/rpc_replication.go
@@ -39,7 +39,7 @@ import (
 )
 
 var (
-	enableSemiSync   = flag.Bool("enable_semi_sync", false, "(DEPRECATED - Set the correct durability_policy instead) Enable semi-sync when configuring replication, on primary and replica tablets only (rdonly tablets will not ack).")
+	enableSemiSync   = flag.Bool("enable_semi_sync", false, "Enable semi-sync when configuring replication, on primary and replica tablets only (rdonly tablets will not ack).")
 	setSuperReadOnly = flag.Bool("use_super_read_only", true, "Set super_read_only flag when performing planned failover.")
 )
 
