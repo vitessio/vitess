e618c1bd20946f3a4b09d93c83c4261432af0fd0
Matt Lord <mattalord@gmail.com>
2022-05-04 22:22:21 -0400

    Minor correction to new Info log msg
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/test/endtoend/cluster/vtgate_process.go
+++ go/test/endtoend/cluster/vtgate_process.go
@@ -178,7 +178,7 @@ func (vtgate *VtgateProcess) GetStatusForTabletOfShard(name string, endPointsCou
 // WaitForStatusOfTabletInShard function waits till status of a tablet in shard is 1
 // endPointsCount: how many endpoints to wait for
 func (vtgate *VtgateProcess) WaitForStatusOfTabletInShard(name string, endPointsCount int) error {
-	log.Infof("Waiting for healthy status of %d %s tablets in shard %s",
+	log.Infof("Waiting for healthy status of %d %s tablets in cell %s",
 		endPointsCount, name, vtgate.Cell)
 	timeout := time.Now().Add(30 * time.Second)
 	for time.Now().Before(timeout) {
