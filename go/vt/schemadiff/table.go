9ac8f042f8f0a448eb684a57051a98edc5767a5d
Dirkjan Bussink <d.bussink@gmail.com>
2022-05-05 10:44:22 +0200

    Fix handling of unsigned and zerofill in parser and normalization (#10220)
    In the parser when generating a query, unsigned and zerofill are always
    rendered lowercase in canonical MySQL output.
    
    Additionally, zerofill is deprecated for integer values similar to the
    length. Zerofill does still have a side effect though which is that it
    also does mark the field as unsigned.
    
    The normalization now removes the zerofill attribute, but sets unsigned
    to true if it was used to keep this side effect.
    
    Also see
    https://dev.mysql.com/doc/refman/8.0/en/numeric-type-attributes.html for
    the reference documentation on how this works.
    
    Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

--- go/vt/schemadiff/table.go
+++ go/vt/schemadiff/table.go
@@ -282,6 +282,12 @@ func (c *CreateTableEntity) normalizeColumnOptions() {
 		// doesn't mean anything anymore.
 		if _, ok := integralTypes[col.Type.Type]; ok {
 			col.Type.Length = nil
+			// Remove zerofill for integral types but keep the behavior that this marks the value
+			// as unsigned
+			if col.Type.Zerofill {
+				col.Type.Zerofill = false
+				col.Type.Unsigned = true
+			}
 		}
 
 		if _, ok := charsetTypes[col.Type.Type]; ok {
