4469316394d6076eba8c3e1db79ac0c562f60cf3
Manan Gupta <manan@planetscale.com>
2022-05-09 10:58:12 +0530

    test: wait for schema tracking to run before running the test queries in TestFoundRows
    Signed-off-by: Manan Gupta <manan@planetscale.com>

--- go/test/endtoend/vtgate/queries/foundrows/found_rows_test.go
+++ go/test/endtoend/vtgate/queries/foundrows/found_rows_test.go
@@ -18,14 +18,15 @@ package foundrows
 
 import (
 	"context"
+	"fmt"
 	"testing"
-
-	"vitess.io/vitess/go/test/endtoend/vtgate/utils"
+	"time"
 
 	"github.com/stretchr/testify/require"
 
 	"vitess.io/vitess/go/mysql"
 	"vitess.io/vitess/go/test/endtoend/cluster"
+	"vitess.io/vitess/go/test/endtoend/vtgate/utils"
 )
 
 func TestFoundRows(t *testing.T) {
@@ -40,6 +41,11 @@ func TestFoundRows(t *testing.T) {
 
 	utils.Exec(t, conn, "insert into t2(id3,id4) values(1,2), (2,2), (3,3), (4,3), (5,3)")
 
+	// Wait for schema tracking to run and mark t2 as authoritative before we try out the queries.
+	// Some of the queries depend on schema tracking to run successfully to be able to replace the StarExpr
+	// in the select clause with the definitive column list.
+	err = waitForT2Authoritative(t)
+	require.NoError(t, err)
 	runTests := func(workload string) {
 		utils.AssertFoundRowsValue(t, conn, "select * from t2", workload, 5)
 		utils.AssertFoundRowsValue(t, conn, "select * from t2 order by id3 limit 2", workload, 2)
@@ -57,4 +63,39 @@ func TestFoundRows(t *testing.T) {
 	utils.Exec(t, conn, "set workload = oltp")
 	utils.Exec(t, conn, "delete from t2")
 	utils.Exec(t, conn, "delete from t2_id4_idx")
+}
+
+// waitForT2Authoritative waits for the table t2 to become authoritative
+func waitForT2Authoritative(t *testing.T) error {
+	timeout := time.After(10 * time.Second)
+	for {
+		select {
+		case <-timeout:
+			return fmt.Errorf("schema tracking didn't mark table t2 as authoritative until timeout")
+		default:
+			time.Sleep(1 * time.Second)
+			res, err := clusterInstance.VtgateProcess.ReadVSchema()
+			require.NoError(t, err, res)
+			t2Map := getTableT2Map(res)
+			authoritative, fieldPresent := t2Map["column_list_authoritative"]
+			if !fieldPresent {
+				continue
+			}
+			authoritativeBool, isBool := authoritative.(bool)
+			if !isBool || !authoritativeBool {
+				continue
+			}
+			return nil
+		}
+	}
+}
+
+func getTableT2Map(res *interface{}) map[string]interface{} {
+	t2Map := convertToMap(convertToMap(convertToMap(convertToMap(convertToMap(*res)["keyspaces"])[KeyspaceName])["tables"])["t2"])
+	return t2Map
+}
+
+func convertToMap(input interface{}) map[string]interface{} {
+	output, _ := input.(map[string]interface{})
+	return output
 }
