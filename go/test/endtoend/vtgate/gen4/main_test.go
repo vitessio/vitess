c0b1f2ea320025c2d0458d688d459f9355575e38
Andres Taylor <andres@planetscale.com>
2022-05-10 08:03:37 +0200

    test: make sure schema tracking is enabled for the end to end tests
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/test/endtoend/vtgate/gen4/main_test.go
+++ go/test/endtoend/vtgate/gen4/main_test.go
@@ -220,6 +220,9 @@ func TestMain(m *testing.M) {
 			SchemaSQL: shardedSchemaSQL,
 			VSchema:   shardedVSchema,
 		}
+
+		clusterInstance.VtGateExtraArgs = []string{"--schema_change_signal"}
+		clusterInstance.VtTabletExtraArgs = []string{"--queryserver-config-schema-change-signal", "--queryserver-config-schema-change-signal-interval", "0.1"}
 		err = clusterInstance.StartKeyspace(*sKs, shardedKsShards, 0, false)
 		if err != nil {
 			return 1
