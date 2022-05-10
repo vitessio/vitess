b96ebd4e7aefeb88a7d7d7401f2dc37ad9b3b3ad
Kushal Kumar <59891164+K-Kumar-01@users.noreply.github.com>
2022-05-10 13:14:49 +0530

    Update the parsing support for JSON_VALUE (#10242)
    * feat: update the parser for JSON_VALUE
    
    add parsing support for optional attributes
    add test cases
    
    Signed-off-by: Kushal Kumar <kushalkumargupta4@gmail.com>
    
    * test: add a few more parser tests
    
    Signed-off-by: Manan Gupta <manan@planetscale.com>
    
    * test: add more tests to verify json_value fixes work in create table indexes too
    
    Signed-off-by: Manan Gupta <manan@planetscale.com>
    
    Co-authored-by: Manan Gupta <manan@planetscale.com>

--- go/vt/sqlparser/cached_size.go
+++ go/vt/sqlparser/cached_size.go
@@ -1716,7 +1716,7 @@ func (cached *JSONValueExpr) CachedSize(alloc bool) int64 {
 	}
 	size := int64(0)
 	if alloc {
-		size += int64(32)
+		size += int64(64)
 	}
 	// field JSONDoc vitess.io/vitess/go/vt/sqlparser.Expr
 	if cc, ok := cached.JSONDoc.(cachedObject); ok {
@@ -1726,6 +1726,12 @@ func (cached *JSONValueExpr) CachedSize(alloc bool) int64 {
 	if cc, ok := cached.Path.(cachedObject); ok {
 		size += cc.CachedSize(true)
 	}
+	// field ReturningType *vitess.io/vitess/go/vt/sqlparser.ConvertType
+	size += cached.ReturningType.CachedSize(true)
+	// field EmptyOnResponse *vitess.io/vitess/go/vt/sqlparser.JtOnResponse
+	size += cached.EmptyOnResponse.CachedSize(true)
+	// field ErrorOnResponse *vitess.io/vitess/go/vt/sqlparser.JtOnResponse
+	size += cached.ErrorOnResponse.CachedSize(true)
 	return size
 }
 func (cached *JSONValueMergeExpr) CachedSize(alloc bool) int64 {
