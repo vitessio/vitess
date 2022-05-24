cba9d2e2cc1812e93e7c10d19576887d70aeed0b
Dirkjan Bussink <d.bussink@gmail.com>
2022-05-24 17:25:29 +0200

    Add support for parsing ARRAY in cast / convert operations
    This adds the parser logic to allow ARRAY to be at the end of the
    convert type. This is described at https://dev.mysql.com/doc/refman/8.0/en/cast-functions.html#function_cast
    
    We're here more flexible than MySQL though which only allows this at the
    `CREATE TABLE`, `CREATE INDEX` or `ALTER TABLE` level but in that case
    it will still error in MySQL if this is used.
    
    Adding this makes it work for those statements then though.
    
    Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

--- go/vt/sqlparser/ast_format.go
+++ go/vt/sqlparser/ast_format.go
@@ -1457,7 +1457,11 @@ func (node *SubstrExpr) Format(buf *TrackedBuffer) {
 
 // Format formats the node.
 func (node *ConvertExpr) Format(buf *TrackedBuffer) {
-	buf.astPrintf(node, "convert(%v, %v)", node.Expr, node.Type)
+	buf.astPrintf(node, "convert(%v, %v", node.Expr, node.Type)
+	if node.Array {
+		buf.astPrintf(node, " %s", keywordStrings[ARRAY])
+	}
+	buf.astPrintf(node, ")")
 }
 
 // Format formats the node.
