c79d15e13a29452db37277005fcbe4e257c54c2a
Vicent Martí <42793+vmg@users.noreply.github.com>
2022-05-25 23:32:53 +0200

    plancache: Lazy sysvar planner functions (#10248) (#10368)
    * planbuilder: make sysvar plan cache lazy
    
    Signed-off-by: Vicent Marti <vmg@strn.cat>
    
    * collations: check that Local() is not called too early
    
    Signed-off-by: Vicent Marti <vmg@strn.cat>
    
    * integration: fix early collation inits
    
    Signed-off-by: Vicent Marti <vmg@strn.cat>

--- go/mysql/collations/integration/collations_test.go
+++ go/mysql/collations/integration/collations_test.go
@@ -32,6 +32,7 @@ import (
 	"golang.org/x/text/encoding/unicode/utf32"
 
 	"vitess.io/vitess/go/mysql"
+	"vitess.io/vitess/go/mysql/collations"
 	"vitess.io/vitess/go/mysql/collations/remote"
 	"vitess.io/vitess/go/sqltypes"
 	"vitess.io/vitess/go/vt/sqlparser"
@@ -104,7 +105,7 @@ func parseWeightString(b []byte) []byte {
 }
 
 func (u *uca900CollationTest) Test(t *testing.T, result *sqltypes.Result) {
-	coll := defaultenv.LookupByName(u.collation)
+	coll := collations.Local().LookupByName(u.collation)
 	if coll == nil {
 		t.Fatalf("unknown collation %q", u.collation)
 	}
@@ -214,7 +215,7 @@ func TestCollationWithSpace(t *testing.T) {
 
 	for _, collName := range []string{"utf8mb4_0900_ai_ci", "utf8mb4_unicode_ci", "utf8mb4_unicode_520_ci"} {
 		t.Run(collName, func(t *testing.T) {
-			local := defaultenv.LookupByName(collName)
+			local := collations.Local().LookupByName(collName)
 			remote := remote.NewCollation(conn, collName)
 
 			for _, size := range []int{0, codepoints, codepoints + 1, codepoints + 2, 20, 32} {
