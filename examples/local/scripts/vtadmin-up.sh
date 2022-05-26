dae2913a20ca287a468c3c633e7f65f26bef2ced
Andrew Mason <andrew@planetscale.com>
2022-05-26 14:08:03 -0400

    Allow vtadmin-up to override hostname to support vscode container port forwarding
    Signed-off-by: Andrew Mason <andrew@planetscale.com>

--- examples/local/scripts/vtadmin-up.sh
+++ examples/local/scripts/vtadmin-up.sh
@@ -5,12 +5,13 @@ source ./env.sh
 log_dir="${VTDATAROOT}/tmp"
 web_dir="../../web/vtadmin"
 
+host=${HOST:-localhost}
 vtadmin_api_port=14200
 vtadmin_web_port=14201
 
 vtadmin \
   --addr ":${vtadmin_api_port}" \
-  --http-origin "http://localhost:${vtadmin_web_port}" \
+  --http-origin "http://${host}:${vtadmin_web_port}" \
   --http-tablet-url-tmpl "http://{{ .Tablet.Hostname }}:15{{ .Tablet.Alias.Uid }}" \
   --tracer "opentracing-jaeger" \
   --grpc-tracing \
@@ -27,7 +28,7 @@ echo ${vtadmin_api_pid} > "${log_dir}/vtadmin-api.pid"
 
 echo "\
 vtadmin-api is running!
-  - API: http://localhost:${vtadmin_api_port}
+  - API: http://${host}:${vtadmin_api_port}
   - Logs: ${log_dir}/vtadmin-api.out
   - PID: ${vtadmin_api_pid}
 "
@@ -37,7 +38,7 @@ vtadmin-api is running!
 # other Vitess components.)
 npm --prefix $web_dir --silent install
 
-REACT_APP_VTADMIN_API_ADDRESS="http://localhost:${vtadmin_api_port}" \
+REACT_APP_VTADMIN_API_ADDRESS="http://${host}:${vtadmin_api_port}" \
   REACT_APP_ENABLE_EXPERIMENTAL_TABLET_DEBUG_VARS="true" \
   npm run --prefix $web_dir build
 
@@ -49,7 +50,7 @@ echo ${vtadmin_web_pid} > "${log_dir}/vtadmin-web.pid"
 
 echo "\
 vtadmin-web is running!
-  - Browser: http://localhost:${vtadmin_web_port}
+  - Browser: http://${host}:${vtadmin_web_port}
   - Logs: ${log_dir}/vtadmin-web.out
   - PID: ${vtadmin_web_pid}
 "
