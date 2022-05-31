defc2011f5ceb606670f1570bcbf240c38d7bcfa
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2022-05-31 14:58:40 +0300

    OnlineDDL/vrepl: more error codes, spatial types (#10394)
    * more vreplication unrecoverable errors
    
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
    
    * error code 1416
    
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
    
    * add geometry cast tests
    
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/mysql/constants.go
+++ go/mysql/constants.go
@@ -536,6 +536,7 @@ const (
 	ERQueryInterrupted             = 1317
 	ERTruncatedWrongValueForField  = 1366
 	ERDataTooLong                  = 1406
+	ErrWrongValueForType           = 1411
 	ERWarnDataTruncated            = 1265
 	ERForbidSchemaChange           = 1450
 	ERDataOutOfRange               = 1690
@@ -546,6 +547,12 @@ const (
 	ERInvalidCastToJSON            = 3147
 	ERJSONValueTooBig              = 3150
 	ERJSONDocumentTooDeep          = 3157
+
+	ErrCantCreateGeometryObject      = 1416
+	ErrGISDataWrongEndianess         = 3055
+	ErrNotImplementedForCartesianSRS = 3704
+	ErrNotImplementedForProjectedSRS = 3705
+	ErrNonPositiveRadius             = 3706
 
 	// server not available
 	ERServerIsntAvailable = 3168
