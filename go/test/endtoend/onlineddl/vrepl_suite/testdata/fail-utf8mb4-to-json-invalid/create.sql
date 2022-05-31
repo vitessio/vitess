b85a76e7eee7e7c52faf8683209cd9f4876cd3dc
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2022-05-31 10:27:57 +0300

    OnlineDDL/vitess fix: convert data type to JSON  (#10390)
    * Adding JSON related error codes
    
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
    
    * JSON related error codes are unrecoverable
    
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
    
    * when converting any type to JSON, we encode via 'convert(%s using utf8mb4)'
    
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
    
    * adding vrepl suite tests for converting values to JSON
    
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

---
+++ go/test/endtoend/onlineddl/vrepl_suite/testdata/fail-utf8mb4-to-json-invalid/create.sql
@@ -0,0 +1,21 @@
+drop table if exists onlineddl_test;
+create table onlineddl_test (
+  id int auto_increment,
+  t varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  primary key(id)
+) auto_increment=1;
+
+drop event if exists onlineddl_test;
+delimiter ;;
+create event onlineddl_test
+  on schedule every 1 second
+  starts current_timestamp
+  ends current_timestamp + interval 60 second
+  on completion not preserve
+  enable
+  do
+begin
+  insert into onlineddl_test values (null, '{}');
+  insert into onlineddl_test values (null, '"a"');
+  insert into onlineddl_test values (null, '{"'); -- invalid JSON value, should error
+end ;;
