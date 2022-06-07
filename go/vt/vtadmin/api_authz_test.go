5a496b5ce359f7381a4cc72826c039fefe9510ae
Andrew Mason <andrew@planetscale.com>
2022-06-07 06:47:41 -0400

    Add authz tests for RefreshState
    Signed-off-by: Andrew Mason <andrew@planetscale.com>

--- go/vt/vtadmin/api_authz_test.go
+++ go/vt/vtadmin/api_authz_test.go
@@ -1814,6 +1814,75 @@ func TestPingTablet(t *testing.T) {
 	})
 }
 
+func TestRefreshState(t *testing.T) {
+	t.Parallel()
+
+	opts := vtadmin.Options{
+		RBAC: &rbac.Config{
+			Rules: []*struct {
+				Resource string
+				Actions  []string
+				Subjects []string
+				Clusters []string
+			}{
+				{
+					Resource: "Tablet",
+					Actions:  []string{"put"},
+					Subjects: []string{"user:allowed"},
+					Clusters: []string{"*"},
+				},
+			},
+		},
+	}
+	err := opts.RBAC.Reify()
+	require.NoError(t, err, "failed to reify authorization rules: %+v", opts.RBAC.Rules)
+
+	api := vtadmin.NewAPI(testClusters(t), opts)
+	t.Cleanup(func() {
+		if err := api.Close(); err != nil {
+			t.Logf("api did not close cleanly: %s", err.Error())
+		}
+	})
+
+	t.Run("unauthorized actor", func(t *testing.T) {
+		t.Parallel()
+		actor := &rbac.Actor{Name: "other"}
+
+		ctx := context.Background()
+		if actor != nil {
+			ctx = rbac.NewContext(ctx, actor)
+		}
+
+		resp, err := api.RefreshState(ctx, &vtadminpb.RefreshStateRequest{
+			Alias: &topodatapb.TabletAlias{
+				Cell: "zone1",
+				Uid:  100,
+			},
+		})
+		assert.Error(t, err, "actor %+v should not be permitted to RefreshState", actor)
+		assert.Nil(t, resp, "actor %+v should not be permitted to RefreshState", actor)
+	})
+
+	t.Run("authorized actor", func(t *testing.T) {
+		t.Parallel()
+		actor := &rbac.Actor{Name: "allowed"}
+
+		ctx := context.Background()
+		if actor != nil {
+			ctx = rbac.NewContext(ctx, actor)
+		}
+
+		resp, err := api.RefreshState(ctx, &vtadminpb.RefreshStateRequest{
+			Alias: &topodatapb.TabletAlias{
+				Cell: "zone1",
+				Uid:  100,
+			},
+		})
+		require.NoError(t, err)
+		assert.NotNil(t, resp, "actor %+v should be permitted to RefreshState", actor)
+	})
+}
+
 func testClusters(t testing.TB) []*cluster.Cluster {
 	configs := []testutil.TestClusterConfig{
 		{
@@ -1941,6 +2010,9 @@ func testClusters(t testing.TB) []*cluster.Cluster {
 						}},
 				},
 				PingTabletResults: map[string]error{
+					"zone1-0000000100": nil,
+				},
+				RefreshStateResults: map[string]error{
 					"zone1-0000000100": nil,
 				},
 			},
