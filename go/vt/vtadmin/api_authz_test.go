ed1d3bcc6742ab9bae90fdc1cf178b89721fd2f6
Andrew Mason <andrew@planetscale.com>
2022-06-07 09:19:15 -0400

    Add authz tests for RefreshTabletReplicationSource
    Signed-off-by: Andrew Mason <andrew@planetscale.com>

--- go/vt/vtadmin/api_authz_test.go
+++ go/vt/vtadmin/api_authz_test.go
@@ -1883,6 +1883,75 @@ func TestRefreshState(t *testing.T) {
 	})
 }
 
+func TestRefreshTabletReplicationSource(t *testing.T) {
+	t.Parallel()
+
+	opts := vtadmin.Options{
+		RBAC: &rbac.Config{
+			Rules: []*struct {
+				Resource string
+				Actions  []string
+				Subjects []string
+				Clusters []string
+			}{
+				{
+					Resource: "Tablet",
+					Actions:  []string{"refresh_tablet_replication_source"},
+					Subjects: []string{"user:allowed"},
+					Clusters: []string{"*"},
+				},
+			},
+		},
+	}
+	err := opts.RBAC.Reify()
+	require.NoError(t, err, "failed to reify authorization rules: %+v", opts.RBAC.Rules)
+
+	api := vtadmin.NewAPI(testClusters(t), opts)
+	t.Cleanup(func() {
+		if err := api.Close(); err != nil {
+			t.Logf("api did not close cleanly: %s", err.Error())
+		}
+	})
+
+	t.Run("unauthorized actor", func(t *testing.T) {
+		t.Parallel()
+		actor := &rbac.Actor{Name: "other"}
+
+		ctx := context.Background()
+		if actor != nil {
+			ctx = rbac.NewContext(ctx, actor)
+		}
+
+		resp, err := api.RefreshTabletReplicationSource(ctx, &vtadminpb.RefreshTabletReplicationSourceRequest{
+			Alias: &topodatapb.TabletAlias{
+				Cell: "zone1",
+				Uid:  100,
+			},
+		})
+		assert.Error(t, err, "actor %+v should not be permitted to RefreshTabletReplicationSource", actor)
+		assert.Nil(t, resp, "actor %+v should not be permitted to RefreshTabletReplicationSource", actor)
+	})
+
+	t.Run("authorized actor", func(t *testing.T) {
+		t.Parallel()
+		actor := &rbac.Actor{Name: "allowed"}
+
+		ctx := context.Background()
+		if actor != nil {
+			ctx = rbac.NewContext(ctx, actor)
+		}
+
+		resp, err := api.RefreshTabletReplicationSource(ctx, &vtadminpb.RefreshTabletReplicationSourceRequest{
+			Alias: &topodatapb.TabletAlias{
+				Cell: "zone1",
+				Uid:  100,
+			},
+		})
+		require.NoError(t, err)
+		assert.NotNil(t, resp, "actor %+v should be permitted to RefreshTabletReplicationSource", actor)
+	})
+}
+
 func testClusters(t testing.TB) []*cluster.Cluster {
 	configs := []testutil.TestClusterConfig{
 		{
@@ -2014,6 +2083,14 @@ func testClusters(t testing.TB) []*cluster.Cluster {
 				},
 				RefreshStateResults: map[string]error{
 					"zone1-0000000100": nil,
+				},
+				ReparentTabletResults: map[string]struct {
+					Response *vtctldatapb.ReparentTabletResponse
+					Error    error
+				}{
+					"zone1-0000000100": {
+						Response: &vtctldatapb.ReparentTabletResponse{},
+					},
 				},
 			},
 			Tablets: []*vtadminpb.Tablet{
