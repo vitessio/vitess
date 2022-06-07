9b7384ee13488ec7b636848e7ca65b1fad53d286
Andrew Mason <andrew@planetscale.com>
2022-06-07 09:49:26 -0400

    Add authz tests for StartReplication
    Signed-off-by: Andrew Mason <andrew@planetscale.com>

--- go/vt/vtadmin/api_authz_test.go
+++ go/vt/vtadmin/api_authz_test.go
@@ -2159,6 +2159,75 @@ func TestSetReadWrite(t *testing.T) {
 	})
 }
 
+func TestStartReplication(t *testing.T) {
+	t.Parallel()
+
+	opts := vtadmin.Options{
+		RBAC: &rbac.Config{
+			Rules: []*struct {
+				Resource string
+				Actions  []string
+				Subjects []string
+				Clusters []string
+			}{
+				{
+					Resource: "Tablet",
+					Actions:  []string{"manage_tablet_replication"},
+					Subjects: []string{"user:allowed"},
+					Clusters: []string{"*"},
+				},
+			},
+		},
+	}
+	err := opts.RBAC.Reify()
+	require.NoError(t, err, "failed to reify authorization rules: %+v", opts.RBAC.Rules)
+
+	api := vtadmin.NewAPI(testClusters(t), opts)
+	t.Cleanup(func() {
+		if err := api.Close(); err != nil {
+			t.Logf("api did not close cleanly: %s", err.Error())
+		}
+	})
+
+	t.Run("unauthorized actor", func(t *testing.T) {
+		t.Parallel()
+		actor := &rbac.Actor{Name: "other"}
+
+		ctx := context.Background()
+		if actor != nil {
+			ctx = rbac.NewContext(ctx, actor)
+		}
+
+		resp, err := api.StartReplication(ctx, &vtadminpb.StartReplicationRequest{
+			Alias: &topodatapb.TabletAlias{
+				Cell: "zone1",
+				Uid:  100,
+			},
+		})
+		assert.Error(t, err, "actor %+v should not be permitted to StartReplication", actor)
+		assert.Nil(t, resp, "actor %+v should not be permitted to StartReplication", actor)
+	})
+
+	t.Run("authorized actor", func(t *testing.T) {
+		t.Parallel()
+		actor := &rbac.Actor{Name: "allowed"}
+
+		ctx := context.Background()
+		if actor != nil {
+			ctx = rbac.NewContext(ctx, actor)
+		}
+
+		resp, err := api.StartReplication(ctx, &vtadminpb.StartReplicationRequest{
+			Alias: &topodatapb.TabletAlias{
+				Cell: "zone1",
+				Uid:  100,
+			},
+		})
+		require.NoError(t, err)
+		assert.NotNil(t, resp, "actor %+v should be permitted to StartReplication", actor)
+	})
+}
+
 func testClusters(t testing.TB) []*cluster.Cluster {
 	configs := []testutil.TestClusterConfig{
 		{
@@ -2303,6 +2372,9 @@ func testClusters(t testing.TB) []*cluster.Cluster {
 					"zone1-0000000100": nil,
 				},
 				SetWritableResults: map[string]error{
+					"zone1-0000000100": nil,
+				},
+				StartReplicationResults: map[string]error{
 					"zone1-0000000100": nil,
 				},
 			},
