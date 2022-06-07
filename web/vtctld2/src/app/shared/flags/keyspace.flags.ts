35b7e60e6f5b5f7305076927a3d83074525d7cda
notfelineit <notfelineit@gmail.com>
2022-06-07 10:55:55 -0700

    Remove sharding_column_name and sharding_column_type
    Signed-off-by: notfelineit <notfelineit@gmail.com>

--- web/vtctld2/src/app/shared/flags/keyspace.flags.ts
+++ web/vtctld2/src/app/shared/flags/keyspace.flags.ts
@@ -16,11 +16,6 @@ export class EditKeyspaceFlags {
   constructor(keyspace) {
     this.flags['keyspace_name'] = new KeyspaceNameFlag(0, 'keyspace_name', keyspace.name, false);
     this.flags['keyspace_name']['positional'] = true;
-    this.flags['sharding_column_name'] = new ShardingColumnNameFlag(1, 'sharding_column_name', keyspace.shardingColumnName);
-    this.flags['sharding_column_name']['positional'] = true;
-    let shardingColumnType = Proto.SHARDING_COLUMN_TYPES[keyspace.shardingColumnType];
-    this.flags['sharding_column_type'] = new ShardingColumnTypeFlag(2, 'sharding_column_type', shardingColumnType);
-    this.flags['sharding_column_type']['positional'] = true;
     this.flags['force'] = new ForceFlag(3, 'force');
   }
 }
@@ -30,8 +25,6 @@ export class NewKeyspaceFlags {
   constructor() {
     this.flags['keyspace_name'] = new KeyspaceNameFlag(0, 'keyspace_name');
     this.flags['keyspace_name']['positional'] = true;
-    this.flags['sharding_column_name'] = new ShardingColumnNameFlag(1, 'sharding_column_name');
-    this.flags['sharding_column_type'] = new ShardingColumnTypeFlag(2, 'sharding_column_type');
     this.flags['force'] = new ForceFlag(3, 'force');
   }
 }
@@ -150,25 +143,5 @@ export class RKCForceFlag extends CheckBoxFlag {
 export class RKCRecursiveFlag extends CheckBoxFlag {
   constructor(position: number, id: string, value= false) {
     super(position, id, 'Recursive', 'Also delete all tablets in that cell belonging to the specified keyspace.', value);
-  }
-}
-
-export class ShardingColumnNameFlag extends InputFlag {
-  constructor(position: number, id: string, value= '') {
-    super(position, id, 'Sharding Column Name', 'Specifies the column to use for sharding operations.', value);
-  }
-}
-
-export class ShardingColumnTypeFlag extends DropDownFlag {
-  constructor(position: number, id: string, value= 'UINT64') {
-    super(position, id, 'Sharding Column Type', 'Specifies the type of the column to use for sharding operations.', value);
-    let options = [];
-    Proto.SHARDING_COLUMN_NAMES.forEach(name => {
-        if (name !== '') {
-          options.push({label: name, value: Proto.SHARDING_COLUMN_NAME_TO_TYPE[name]});
-        }
-    });
-    this.setOptions(options);
-    this.setBlockOnEmptyList(['sharding_column_name']);
   }
 }
