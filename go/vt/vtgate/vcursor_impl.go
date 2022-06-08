7b51d62609a50b8b1aba545048be4d84af86403d
Andres Taylor <andres@planetscale.com>
2022-06-08 11:15:57 +0200

    fix: change planner_version to planner-version everywhere (#10453)
    * fix: change planner_version to planner-version everywhere
    
    Signed-off-by: Andres Taylor <andres@planetscale.com>
    
    * fix: actually change the planner version on vtgate after checking
    
    Signed-off-by: Andres Taylor <andres@planetscale.com>
    
    * fix: move CheckPlannerVersionFlag out from vtgate
    
    Signed-off-by: Andres Taylor <andres@planetscale.com>
    
    * fix: move the global plannerVersion to be a field on the executor. think global, act local, yo
    
    Signed-off-by: Andres Taylor <andres@planetscale.com>
    
    * fix: use the planner in the session first
    
    Signed-off-by: Andres Taylor <andres@planetscale.com>
    
    * test: use DEFAULT instead of 0
    
    Signed-off-by: Andres Taylor <andres@planetscale.com>
    
    * fix: re-add the planner-version flag to vtcombo
    
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/vt/vtgate/vcursor_impl.go
+++ go/vt/vtgate/vcursor_impl.go
@@ -36,7 +36,6 @@ import (
 	"vitess.io/vitess/go/vt/callerid"
 	"vitess.io/vitess/go/vt/discovery"
 	"vitess.io/vitess/go/vt/key"
-	"vitess.io/vitess/go/vt/log"
 	binlogdatapb "vitess.io/vitess/go/vt/proto/binlogdata"
 	querypb "vitess.io/vitess/go/vt/proto/query"
 	topodatapb "vitess.io/vitess/go/vt/proto/topodata"
@@ -51,7 +50,6 @@ import (
 	"vitess.io/vitess/go/vt/vterrors"
 	"vitess.io/vitess/go/vt/vtgate/buffer"
 	"vitess.io/vitess/go/vt/vtgate/engine"
-	"vitess.io/vitess/go/vt/vtgate/planbuilder"
 	"vitess.io/vitess/go/vt/vtgate/semantics"
 	"vitess.io/vitess/go/vt/vtgate/vindexes"
 	"vitess.io/vitess/go/vt/vtgate/vschemaacl"
@@ -113,6 +111,7 @@ type vcursorImpl struct {
 	warnShardedOnly     bool // when using sharded only features, a warning will be warnings field
 
 	warnings []*querypb.QueryWarning // any warnings that are accumulated during the planning phase are stored here
+	pv       plancontext.PlannerVersion
 }
 
 // newVcursorImpl creates a vcursorImpl. Before creating this object, you have to separate out any marginComments that came with
@@ -130,6 +129,7 @@ func newVCursorImpl(
 	resolver *srvtopo.Resolver,
 	serv srvtopo.Server,
 	warnShardedOnly bool,
+	pv plancontext.PlannerVersion,
 ) (*vcursorImpl, error) {
 	keyspace, tabletType, destination, err := parseDestinationTarget(safeSession.TargetString, vschema)
 	if err != nil {
@@ -175,6 +175,7 @@ func newVCursorImpl(
 		vm:              vm,
 		topoServer:      ts,
 		warnShardedOnly: warnShardedOnly,
+		pv:              pv,
 	}, nil
 }
 
@@ -413,13 +414,7 @@ func (vc *vcursorImpl) Planner() plancontext.PlannerVersion {
 		vc.safeSession.Options.PlannerVersion != querypb.ExecuteOptions_DEFAULT_PLANNER {
 		return vc.safeSession.Options.PlannerVersion
 	}
-	version, done := plancontext.PlannerNameToVersion(*plannerVersion)
-	if done {
-		return version
-	}
-
-	log.Warning("unknown planner version configured. using the default")
-	return planbuilder.V3
+	return vc.pv
 }
 
 // GetSemTable implements the ContextVSchema interface
