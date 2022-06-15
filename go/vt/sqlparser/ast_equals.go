425feff1e986f4193abab26b7fbd23eba8418393
Dirkjan Bussink <d.bussink@gmail.com>
2022-06-15 15:48:46 +0200

    Fix parsing of CAST() statements (#10512)
    * Fix parsing of CAST() statements
    
    CAST() was treated as an alias for CONVERT() but with slightly different
    syntax.
    
    This is also described in the documentation at
    https://dev.mysql.com/doc/refman/8.0/en/cast-functions.html,
    specifically:
    
    With CAST(expr AS type syntax, the CAST() function takes an expression of
    any type and produces a result value of the specified type. This operation
    may also be expressed as CONVERT(expr, type), which is equivalent. If expr
    is NULL, CAST() returns NULL.
    
    This is wrong sadly. CAST() is not equivalent to CONVERT(), specifically
    in the context of a CREATE TABLE. For JSON keys, the ARRAY attribute is
    possible on a CAST(), but that is not accepted for a CONVERT().
    
    The difference in parsing also shows in MySQL:
    
    ```
    mysql> select convert(json_keys(c), char(64) array) from t;
    ERROR 1064 (42000): You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'array) from t' at line 1
    mysql> select cast(json_keys(c) as char(64) array) from t;
    ERROR 1235 (42000): This version of MySQL doesn't yet support 'Use of CAST( .. AS .. ARRAY) outside of functional index in CREATE(non-SELECT)/ALTER TABLE or in general expressions'
    ```
    
    Here the first statement can't be parsed at all. The second is properly
    parsed, but ARRAY is not allowed in the context of CAST() in this
    situation (and only in a CREATE TABLE).
    
    This means we should really treat these as two separate expressions and
    don't store them both in the same structure. The change here creates a
    separate CAST structure, removes the ARRAY option from CONVERT and
    updates the grammar and all tests accordingly.
    
    Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>
    
    * Handle new cast expression in evalengine and planbuilder
    
    Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>
    
    * evalengine: do not duplicate CAST/CONVERT translation
    
    Signed-off-by: Vicent Marti <vmg@strn.cat>
    
    Co-authored-by: Vicent Marti <vmg@strn.cat>

--- go/vt/sqlparser/ast_equals.go
+++ go/vt/sqlparser/ast_equals.go
@@ -206,6 +206,12 @@ func EqualsSQLNode(inA, inB SQLNode) bool {
 			return false
 		}
 		return EqualsRefOfCaseExpr(a, b)
+	case *CastExpr:
+		b, ok := inB.(*CastExpr)
+		if !ok {
+			return false
+		}
+		return EqualsRefOfCastExpr(a, b)
 	case *ChangeColumn:
 		b, ok := inB.(*ChangeColumn)
 		if !ok {
@@ -1791,6 +1797,19 @@ func EqualsRefOfCaseExpr(a, b *CaseExpr) bool {
 		EqualsExpr(a.Else, b.Else)
 }
 
+// EqualsRefOfCastExpr does deep equals between the two objects.
+func EqualsRefOfCastExpr(a, b *CastExpr) bool {
+	if a == b {
+		return true
+	}
+	if a == nil || b == nil {
+		return false
+	}
+	return a.Array == b.Array &&
+		EqualsExpr(a.Expr, b.Expr) &&
+		EqualsRefOfConvertType(a.Type, b.Type)
+}
+
 // EqualsRefOfChangeColumn does deep equals between the two objects.
 func EqualsRefOfChangeColumn(a, b *ChangeColumn) bool {
 	if a == b {
@@ -1949,8 +1968,7 @@ func EqualsRefOfConvertExpr(a, b *ConvertExpr) bool {
 	if a == nil || b == nil {
 		return false
 	}
-	return a.Array == b.Array &&
-		EqualsExpr(a.Expr, b.Expr) &&
+	return EqualsExpr(a.Expr, b.Expr) &&
 		EqualsRefOfConvertType(a.Type, b.Type)
 }
 
@@ -5203,6 +5221,12 @@ func EqualsExpr(inA, inB Expr) bool {
 			return false
 		}
 		return EqualsRefOfCaseExpr(a, b)
+	case *CastExpr:
+		b, ok := inB.(*CastExpr)
+		if !ok {
+			return false
+		}
+		return EqualsRefOfCastExpr(a, b)
 	case *ColName:
 		b, ok := inB.(*ColName)
 		if !ok {
@@ -5779,6 +5803,12 @@ func EqualsJSONPathParam(inA, inB JSONPathParam) bool {
 			return false
 		}
 		return EqualsRefOfCaseExpr(a, b)
+	case *CastExpr:
+		b, ok := inB.(*CastExpr)
+		if !ok {
+			return false
+		}
+		return EqualsRefOfCastExpr(a, b)
 	case *ColName:
 		b, ok := inB.(*ColName)
 		if !ok {
