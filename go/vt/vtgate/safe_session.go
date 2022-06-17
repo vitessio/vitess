d5d4ef3ca7c158302ffad7ddb285145e7bc7ce76
Harshit Gangal <harshit@planetscale.com>
2022-06-17 15:16:45 +0530

    feat: claim a vindex only session if other query uses it
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/vt/vtgate/safe_session.go
+++ go/vt/vtgate/safe_session.go
@@ -299,9 +299,16 @@ func (session *SafeSession) Find(keyspace, shard string, tabletType topodatapb.T
 	}
 	for _, shardSession := range sessions {
 		if keyspace == shardSession.Target.Keyspace && tabletType == shardSession.Target.TabletType && shard == shardSession.Target.Shard {
-			if shardSession.VindexOnly && !session.queryFromVindex {
-				session.mustRollback = true
-				return 0, 0, nil, vterrors.Errorf(vtrpcpb.Code_ABORTED, "multi-db transaction attempted: %v", session.ShardSessions)
+			if getTxMode() == vtgatepb.TransactionMode_SINGLE && shardSession.VindexOnly && !session.queryFromVindex {
+				count := actualNoOfShardSession(session.ShardSessions)
+				// If the count of shard session which are non vindex only is greater than 0, then it is a
+				if count > 0 {
+					session.mustRollback = true
+					return 0, 0, nil, vterrors.Errorf(vtrpcpb.Code_ABORTED, "multi-db transaction attempted: %v", session.ShardSessions)
+				}
+				// the shard session is now used by non-vindex query as well,
+				// so it is not an exclusive vindex only shard session anymore.
+				shardSession.VindexOnly = false
 			}
 			return shardSession.TransactionId, shardSession.ReservedId, shardSession.TabletAlias, nil
 		}
