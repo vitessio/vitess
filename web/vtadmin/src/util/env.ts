a5b7df390d41efae78ca75171c6131749c96dcf0
Léopold Jacquot <leopold.jacquot@infomaniak.com>
2022-06-18 23:47:35 +0200

    add-vtadmin-docker-image
    Signed-off-by: Léopold Jacquot <leopold.jacquot@infomaniak.com>

--- web/vtadmin/src/util/env.ts
+++ web/vtadmin/src/util/env.ts
@@ -13,8 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+export var env: NodeJS.ProcessEnv = { ...window.env, ...process.env };
 
 // process.env variables are always strings, hence this tiny helper function
 // to transmute it into a boolean. It is a function, rather than a constant,
 // to support dynamic updates to process.env in tests.
-export const isReadOnlyMode = (): boolean => process.env.REACT_APP_READONLY_MODE === 'true';
+export const isReadOnlyMode = (): boolean => env.REACT_APP_READONLY_MODE === 'true';
