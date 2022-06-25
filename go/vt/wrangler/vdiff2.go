2ac130c808572c02acc451231a1bad649fd9c9bc
Matt Lord <mattalord@gmail.com>
2022-06-25 11:17:56 -0400

    Remove lingering %w verb in log.*
    Turns out that log does not support this verb. So we can stick to
    using it with fmt instead.
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/vt/wrangler/vdiff2.go
+++ go/vt/wrangler/vdiff2.go
@@ -68,7 +68,7 @@ func (wr *Wrangler) VDiff2(ctx context.Context, keyspace, workflowName string, c
 		return err
 	})
 	if output.Err != nil {
-		log.Errorf("Error in command %s: %w", command, output.Err)
+		log.Errorf("Error in command %s: %v", command, output.Err)
 		return nil, output.Err
 	}
 
