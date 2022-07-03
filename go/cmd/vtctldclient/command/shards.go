2a58893db64f5372970290a2b826c6910bcd70f8
Matt Lord <mattalord@gmail.com>
2022-07-03 04:42:59 -0400

    Update throttlerservice protobuf
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/cmd/vtctldclient/command/shards.go
+++ go/cmd/vtctldclient/command/shards.go
@@ -83,8 +83,8 @@ that shard.`,
 		Short: "Sets the TabletControl record for a shard and tablet type. Only use this for an emergency fix or after a finished MoveTables.",
 		Long: `Sets the TabletControl record for a shard and tablet type.
 
-Only use this for an emergency fix or after a finished MoveTables. The MigrateServedFrom
-and MigrateServedType commands set this record appropriately already.
+Only use this for an emergency fix or after a finished MoveTables. The MigrateServedType
+commands set this record appropriately already.
 
 Always specify the denied-tables flag for MoveTables, but never for Reshard operations.
 
