3cf9ff1627c8207c5ad65648fc8c3363d8174e56
FlorentP <35779988+frouioui@users.noreply.github.com>
2022-07-08 17:22:24 +0200

    Remove legacy healthcheck files and structures (#10542)
    * Changed the txthrottler to use the new healthcheck implementation
    
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>
    
    * Removed the use of the legacy healthcheck in the tx_throttler
    
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>
    
    * Started the removal of the legacy healthcheck in vtworker
    
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>
    
    * Remove the legacy healthcheck from vtworker
    
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>
    
    * Remove the legacy healthcheck from vtworker
    
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>
    
    * Remove the legacy healthcheck from the wrangler
    
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>
    
    * Remove the legacy healthcheck and al. files
    
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>
    
    * Applied goimports and linter on the throttler package
    
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>
    
    * Fixed build issue and code comments
    
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>
    
    * Fixed comment of GetTabletStats
    
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>
    
    * Removed duplicated function GetTabletStats
    
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>
    
    * Reverted the changes to throttler.Result
    
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>
    
    * Use GetHealthyTabletStats in waitForDrainInCell and removed unrequired target.shard checks
    
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>
    
    * Addition of a NewCellTabletsWatcher to waitForDrainInCell
    
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>
    
    * Moved TabletRecorded methods inside the HealthCheck interface
    
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- go/vt/discovery/utils_test.go
+++ go/vt/discovery/utils_test.go
@@ -27,66 +27,68 @@ import (
 func TestRemoveUnhealthyTablets(t *testing.T) {
 	var testcases = []struct {
 		desc  string
-		input []LegacyTabletStats
-		want  []LegacyTabletStats
+		input []TabletHealth
+		want  []TabletHealth
 	}{{
 		desc:  "tablets missing Stats",
-		input: []LegacyTabletStats{replica(1), replica(2)},
-		want:  []LegacyTabletStats{},
+		input: []TabletHealth{replica(1), replica(2)},
+		want:  []TabletHealth{replica(1), replica(2)},
 	}, {
 		desc:  "all tablets healthy",
-		input: []LegacyTabletStats{healthy(replica(1)), healthy(replica(2))},
-		want:  []LegacyTabletStats{healthy(replica(1)), healthy(replica(2))},
+		input: []TabletHealth{healthy(replica(1)), healthy(replica(2))},
+		want:  []TabletHealth{healthy(replica(1)), healthy(replica(2))},
 	}, {
 		desc:  "one unhealthy tablet (error)",
-		input: []LegacyTabletStats{healthy(replica(1)), unhealthyError(replica(2))},
-		want:  []LegacyTabletStats{healthy(replica(1))},
+		input: []TabletHealth{healthy(replica(1)), unhealthyError(replica(2))},
+		want:  []TabletHealth{healthy(replica(1))},
 	}, {
 		desc:  "one error tablet",
-		input: []LegacyTabletStats{healthy(replica(1)), unhealthyLastError(replica(2))},
-		want:  []LegacyTabletStats{healthy(replica(1))},
+		input: []TabletHealth{healthy(replica(1)), unhealthyLastError(replica(2))},
+		want:  []TabletHealth{healthy(replica(1))},
 	}, {
 		desc:  "one unhealthy tablet (lag)",
-		input: []LegacyTabletStats{healthy(replica(1)), unhealthyLag(replica(2))},
-		want:  []LegacyTabletStats{healthy(replica(1))},
+		input: []TabletHealth{healthy(replica(1)), unhealthyLag(replica(2))},
+		want:  []TabletHealth{healthy(replica(1))},
 	}, {
 		desc:  "no filtering by tablet type",
-		input: []LegacyTabletStats{healthy(primary(1)), healthy(replica(2)), healthy(rdonly(3))},
-		want:  []LegacyTabletStats{healthy(primary(1)), healthy(replica(2)), healthy(rdonly(3))},
+		input: []TabletHealth{healthy(primary(1)), healthy(replica(2)), healthy(rdonly(3))},
+		want:  []TabletHealth{healthy(primary(1)), healthy(replica(2)), healthy(rdonly(3))},
 	}, {
 		desc:  "non-serving tablets won't be removed",
-		input: []LegacyTabletStats{notServing(healthy(replica(1)))},
-		want:  []LegacyTabletStats{notServing(healthy(replica(1)))},
+		input: []TabletHealth{notServing(healthy(replica(1)))},
+		want:  []TabletHealth{notServing(healthy(replica(1)))},
 	}}
 
 	for _, tc := range testcases {
-		got := RemoveUnhealthyTablets(tc.input)
-		if len(got) != len(tc.want) {
-			t.Errorf("test case '%v' failed: RemoveUnhealthyTablets(%v) = %#v, want: %#v", tc.desc, tc.input, got, tc.want)
-		} else {
-			for i := range tc.want {
-				if !got[i].DeepEqual(&tc.want[i]) {
-					t.Errorf("test case '%v' failed: RemoveUnhealthyTablets(%v) = %#v, want: %#v", tc.desc, tc.input, got, tc.want)
+		t.Run(tc.desc, func(t *testing.T) {
+			got := RemoveUnhealthyTablets(tc.input)
+			if len(got) != len(tc.want) {
+				t.Errorf("test case '%v' failed: RemoveUnhealthyTablets(%v) = %#v, want: %#v", tc.desc, tc.input, got, tc.want)
+			} else {
+				for i := range tc.want {
+					if !got[i].DeepEqual(&tc.want[i]) {
+						t.Errorf("test case '%v' failed: RemoveUnhealthyTablets(%v) = %#v, want: %#v", tc.desc, tc.input, got, tc.want)
+					}
 				}
 			}
-		}
+		})
 	}
 }
 
-func primary(uid uint32) LegacyTabletStats {
+func primary(uid uint32) TabletHealth {
 	return minimalTabletStats(uid, topodatapb.TabletType_PRIMARY)
 }
 
-func replica(uid uint32) LegacyTabletStats {
+func replica(uid uint32) TabletHealth {
 	return minimalTabletStats(uid, topodatapb.TabletType_REPLICA)
 }
 
-func rdonly(uid uint32) LegacyTabletStats {
+func rdonly(uid uint32) TabletHealth {
 	return minimalTabletStats(uid, topodatapb.TabletType_RDONLY)
 }
 
-func minimalTabletStats(uid uint32, tabletType topodatapb.TabletType) LegacyTabletStats {
-	return LegacyTabletStats{
+func minimalTabletStats(uid uint32, tabletType topodatapb.TabletType) TabletHealth {
+	return TabletHealth{
 		Tablet: &topodatapb.Tablet{
 			Alias: &topodatapb.TabletAlias{
 				Uid: uid},
@@ -100,33 +102,33 @@ func minimalTabletStats(uid uint32, tabletType topodatapb.TabletType) LegacyTabl
 	}
 }
 
-func healthy(ts LegacyTabletStats) LegacyTabletStats {
+func healthy(ts TabletHealth) TabletHealth {
 	ts.Stats = &querypb.RealtimeStats{
 		ReplicationLagSeconds: uint32(1),
 	}
 	return ts
 }
 
-func unhealthyLag(ts LegacyTabletStats) LegacyTabletStats {
+func unhealthyLag(ts TabletHealth) TabletHealth {
 	ts.Stats = &querypb.RealtimeStats{
 		ReplicationLagSeconds: uint32(3600),
 	}
 	return ts
 }
 
-func unhealthyError(ts LegacyTabletStats) LegacyTabletStats {
+func unhealthyError(ts TabletHealth) TabletHealth {
 	ts.Stats = &querypb.RealtimeStats{
 		HealthError: "unhealthy",
 	}
 	return ts
 }
 
-func unhealthyLastError(ts LegacyTabletStats) LegacyTabletStats {
+func unhealthyLastError(ts TabletHealth) TabletHealth {
 	ts.LastError = errors.New("err")
 	return ts
 }
 
-func notServing(ts LegacyTabletStats) LegacyTabletStats {
+func notServing(ts TabletHealth) TabletHealth {
 	ts.Serving = false
 	return ts
 }
