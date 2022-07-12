1385debaabf68c264dcda6b6f53fa59467af7c82
Andrew Mason <andrew@planetscale.com>
2022-07-12 14:13:56 -0400

    update flags in java test code
    Signed-off-by: Andrew Mason <andrew@planetscale.com>

--- java/grpc-client/src/test/java/io/client/grpc/GrpcClientWithRetriesTest.java
+++ java/grpc-client/src/test/java/io/client/grpc/GrpcClientWithRetriesTest.java
@@ -49,10 +49,10 @@ public class GrpcClientWithRetriesTest extends RpcClientTest {
         new ProcessBuilder(
             Arrays.asList(
                 vtRoot + "/bin/vtgateclienttest",
-                "-logtostderr",
-                "-grpc_port",
+                "--logtostderr",
+                "--grpc_port",
                 Integer.toString(port),
-                "-service_map",
+                "--service_map",
                 "grpc-vtgateservice"))
             .inheritIO()
             .start();
