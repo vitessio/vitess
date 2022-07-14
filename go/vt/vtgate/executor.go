6868042e26cb74c50d99101377fe6b99f72b66b1
Rameez Sajwani <rameezwazirali@hotmail.com>
2022-07-13 19:09:30 -0700

    Removing nil check for logstats
    Signed-off-by: Rameez Sajwani <rameezwazirali@hotmail.com>

--- go/vt/vtgate/executor.go
+++ go/vt/vtgate/executor.go
@@ -999,10 +999,8 @@ func (e *Executor) getPlan(vcursor *vcursorImpl, sql string, comments sqlparser.
 		query = sqlparser.String(statement)
 	}
 
-	if logStats != nil {
-		logStats.SQL = comments.Leading + query + comments.Trailing
-		logStats.BindVariables = sqltypes.CopyBindVariables(bindVars)
-	}
+	logStats.SQL = comments.Leading + query + comments.Trailing
+	logStats.BindVariables = sqltypes.CopyBindVariables(bindVars)
 
 	planHash := sha256.New()
 	_, _ = planHash.Write([]byte(vcursor.planPrefixKey()))
@@ -1012,9 +1010,7 @@ func (e *Executor) getPlan(vcursor *vcursorImpl, sql string, comments sqlparser.
 
 	if sqlparser.CachePlan(statement) && qo.cachePlan() {
 		if plan, ok := e.plans.Get(planKey); ok {
-			if logStats != nil {
-				logStats.CachedPlan = true
-			}
+			logStats.CachedPlan = true
 			return plan.(*engine.Plan), nil
 		}
 	}
