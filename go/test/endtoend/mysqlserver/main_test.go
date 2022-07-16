02e8305ca0d6d022dd8555d4bb515f54ffbcda09
Matt Lord <mattalord@gmail.com>
2022-07-16 05:07:27 +0000

    minimize diff
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/test/endtoend/mysqlserver/main_test.go
+++ go/test/endtoend/mysqlserver/main_test.go
@@ -113,8 +113,8 @@ func TestMain(m *testing.M) {
 			"--mysql_server_query_timeout", "1s",
 			"--mysql_auth_server_impl", "static",
 			"--mysql_auth_server_static_file", clusterInstance.TmpDirectory + mysqlAuthServerStatic,
-			"--warn_sharded_only=true",
 			"--mysql_server_version", "8.0.16-7",
+			"--warn_sharded_only=true",
 		}
 
 		clusterInstance.VtTabletExtraArgs = []string{
