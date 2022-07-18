a36423008132838c6e7c91a99cfac9b5de5e7036
Matt Lord <mattalord@gmail.com>
2022-07-18 09:22:18 -0400

    Auto Detect MySQL Version and Use in vtgate mysql_server_version Flag (#10701)
    * Auto detect MySQL version and use in vtgate --mysql_server_version flag
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Remove any explicit static usage of mysql_server_version
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * The reservedconns test needs to override the mysql_server_version
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Explicitly allow overriding the mysql_system_version
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * minimize diff
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Fix https://github.com/vitessio/vitess/issues/10712
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Use more robust string comparisons
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Fix collation handling in tests
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Fix tm_init_test
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Fix more unit tests
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Another test fix
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Fix another test that requires 8.0 collations
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/vt/vtgate/engine/route_test.go
+++ go/vt/vtgate/engine/route_test.go
@@ -27,6 +27,7 @@ import (
 
 	"vitess.io/vitess/go/mysql/collations"
 	vtrpcpb "vitess.io/vitess/go/vt/proto/vtrpc"
+	"vitess.io/vitess/go/vt/servenv"
 	"vitess.io/vitess/go/vt/vterrors"
 
 	"vitess.io/vitess/go/vt/sqlparser"
@@ -48,6 +49,13 @@ var defaultSelectResult = sqltypes.MakeTestResult(
 	),
 	"1",
 )
+
+func init() {
+	// We require MySQL 8.0 collations for the comparisons in the tests
+	mySQLVersion := "8.0.0"
+	servenv.MySQLServerVersion = &mySQLVersion
+	collationEnv = collations.NewEnvironment(mySQLVersion)
+}
 
 func TestSelectUnsharded(t *testing.T) {
 	sel := NewRoute(
