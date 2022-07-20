a9ce80cd8ac763e34c06ff4bb7118ece352dba43
Matt Lord <mattalord@gmail.com>
2022-07-19 21:24:23 -0400

    [release-14.0] Auto Detect MySQL Version and Use in vtgate mysql_server_version Flag (#10733)
    * Auto Detect MySQL Version and Use in vtgate mysql_server_version Flag (#10701)
    
    * Auto detect MySQL version and use in vtgate --mysql_server_version flag
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Remove any explicit static usage of mysql_server_version
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * The reservedconns test needs to override the mysql_server_version
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Explicitly allow overriding the mysql_system_version
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * minimize diff
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Fix https://github.com/vitessio/vitess/issues/10712
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Use more robust string comparisons
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Fix collation handling in tests
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Fix tm_init_test
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Fix more unit tests
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Another test fix
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Fix another test that requires 8.0 collations
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Kick the CI :-|
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/vt/vtgate/engine/memory_sort_test.go
+++ go/vt/vtgate/engine/memory_sort_test.go
@@ -19,6 +19,7 @@ package engine
 import (
 	"testing"
 
+	"vitess.io/vitess/go/vt/servenv"
 	"vitess.io/vitess/go/vt/vtgate/evalengine"
 
 	"vitess.io/vitess/go/mysql/collations"
@@ -29,6 +30,13 @@ import (
 	"vitess.io/vitess/go/sqltypes"
 	querypb "vitess.io/vitess/go/vt/proto/query"
 )
+
+func init() {
+	// We require MySQL 8.0 collations for the comparisons in the tests
+	mySQLVersion := "8.0.0"
+	servenv.MySQLServerVersion = &mySQLVersion
+	collationEnv = collations.NewEnvironment(mySQLVersion)
+}
 
 func TestMemorySortExecute(t *testing.T) {
 	fields := sqltypes.MakeTestFields(
