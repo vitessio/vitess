f173307459d37828712403d6fd7754a253af4a39
Manan Gupta <35839558+GuptaManan100@users.noreply.github.com>
2022-07-23 11:19:03 +0530

    BugFix: `--queryserver-config-terse-errors` shouldn't redact `Row count exceeded` error and shouldn't affect ignoring `Duplicate entry` error in lookup vindexes (#10762)
    * test: refactor test to use start function for cleaning and add failing test for row limit exceeded not shown to the user
    
    Signed-off-by: Manan Gupta <manan@planetscale.com>
    
    * feat: remove bindvariables check from terse error because bindVariables are optional and normalization can be turned off
    
    Signed-off-by: Manan Gupta <manan@planetscale.com>
    
    * feat: move rows exceeded error from MySQL error to vterror
    
    Signed-off-by: Manan Gupta <manan@planetscale.com>
    
    * feat: remove duplicated test
    
    Signed-off-by: Manan Gupta <manan@planetscale.com>
    
    * feat: instead of checking explicitly for the error message, instead check for the error code to also work with sanitized errors
    
    Signed-off-by: Manan Gupta <manan@planetscale.com>
    
    * test: fix test expectation with queryserver-terse-errors on
    
    Signed-off-by: Manan Gupta <manan@planetscale.com>
    
    * test: fix expectations of remaining tests
    
    Signed-off-by: Manan Gupta <manan@planetscale.com>
    
    * test: fix tests to pass correct error with error numbers and all
    
    Signed-off-by: Manan Gupta <manan@planetscale.com>

--- go/mysql/query_test.go
+++ go/mysql/query_test.go
@@ -651,13 +651,7 @@ func checkQueryInternal(t *testing.T, query string, sConn, cConn *Conn, result *
 		}
 		got, gotWarnings, err := cConn.ExecuteFetchWithWarningCount(query, maxrows, wantfields)
 		if !allRows && len(result.Rows) > 1 {
-			if err == nil {
-				t.Errorf("ExecuteFetch should have failed but got: %v", got)
-			}
-			sqlErr, ok := err.(*SQLError)
-			if !ok || sqlErr.Number() != ERVitessMaxRowsExceeded {
-				t.Errorf("Expected ERVitessMaxRowsExceeded %v, got %v", ERVitessMaxRowsExceeded, sqlErr.Number())
-			}
+			require.ErrorContains(t, err, "Row count exceeded")
 			return
 		}
 		if err != nil {
