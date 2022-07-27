27046176f1434ebd42e3b85ffbb5f9282acbfc6a
Andrew Mason <andrew@planetscale.com>
2022-07-27 06:05:42 -0400

    [cli] [mysqlctl] Scope all backupstorage implementation flags to `pflag` and relevant binaries (#10844)
    * Migrate `azblobbackupstorage` flags to `pflag`
    
    Closes #10811.
    
    Signed-off-by: Andrew Mason <andrew@planetscale.com>
    
    * Migrate `cephbackupstorage` flags to `pflag`
    
    Closes #10813.
    
    Signed-off-by: Andrew Mason <andrew@planetscale.com>
    
    * Migrate `filebackupstorage` flags to `pflag`
    
    Closes #10814.
    
    Signed-off-by: Andrew Mason <andrew@planetscale.com>
    
    * Migrate `gcsbackupstorage` flags to `pflag`
    
    Closes #10815.
    
    Signed-off-by: Andrew Mason <andrew@planetscale.com>
    
    * Remove copy-pasta from original ceph implementation
    
    Signed-off-by: Andrew Mason <andrew@planetscale.com>
    
    * Migrate `s3backupstorage` flags to `pflag`
    
    Closes #10816.
    
    Signed-off-by: Andrew Mason <andrew@planetscale.com>

--- go/vt/mysqlctl/azblobbackupstorage/azblob.go
+++ go/vt/mysqlctl/azblobbackupstorage/azblob.go
@@ -20,7 +20,6 @@ package azblobbackupstorage
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"io"
 	"net/url"
@@ -31,27 +30,44 @@ import (
 
 	"github.com/Azure/azure-pipeline-go/pipeline"
 	"github.com/Azure/azure-storage-blob-go/azblob"
+	"github.com/spf13/pflag"
 
 	"vitess.io/vitess/go/vt/concurrency"
 	"vitess.io/vitess/go/vt/log"
 	"vitess.io/vitess/go/vt/mysqlctl/backupstorage"
+	"vitess.io/vitess/go/vt/servenv"
 )
 
 var (
 	// This is the account name
-	accountName = flag.String("azblob_backup_account_name", "", "Azure Storage Account name for backups; if this flag is unset, the environment variable VT_AZBLOB_ACCOUNT_NAME will be used")
+	accountName string
 
 	// This is the private access key
-	accountKeyFile = flag.String("azblob_backup_account_key_file", "", "Path to a file containing the Azure Storage account key; if this flag is unset, the environment variable VT_AZBLOB_ACCOUNT_KEY will be used as the key itself (NOT a file path)")
+	accountKeyFile string
 
 	// This is the name of the container that will store the backups
-	containerName = flag.String("azblob_backup_container_name", "", "Azure Blob Container Name")
+	containerName string
 
 	// This is an optional prefix to prepend to all files
-	storageRoot = flag.String("azblob_backup_storage_root", "", "Root prefix for all backup-related Azure Blobs; this should exclude both initial and trailing '/' (e.g. just 'a/b' not '/a/b/')")
+	storageRoot string
 
-	azBlobParallelism = flag.Int("azblob_backup_parallelism", 1, "Azure Blob operation parallelism (requires extra memory when increased)")
+	azBlobParallelism int
 )
+
+func registerFlags(fs *pflag.FlagSet) {
+	fs.StringVar(&accountName, "azblob_backup_account_name", "", "Azure Storage Account name for backups; if this flag is unset, the environment variable VT_AZBLOB_ACCOUNT_NAME will be used.")
+	fs.StringVar(&accountKeyFile, "azblob_backup_account_key_file", "", "Path to a file containing the Azure Storage account key; if this flag is unset, the environment variable VT_AZBLOB_ACCOUNT_KEY will be used as the key itself (NOT a file path).")
+	fs.StringVar(&containerName, "azblob_backup_container_name", "", "Azure Blob Container Name.")
+	fs.StringVar(&storageRoot, "azblob_backup_storage_root", "", "Root prefix for all backup-related Azure Blobs; this should exclude both initial and trailing '/' (e.g. just 'a/b' not '/a/b/').")
+	fs.IntVar(&azBlobParallelism, "azblob_backup_parallelism", 1, "Azure Blob operation parallelism (requires extra memory when increased).")
+}
+
+func init() {
+	servenv.OnParseFor("vtbackup", registerFlags)
+	servenv.OnParseFor("vtctl", registerFlags)
+	servenv.OnParseFor("vtctld", registerFlags)
+	servenv.OnParseFor("vttablet", registerFlags)
+}
 
 const (
 	defaultRetryCount = 5
@@ -63,16 +79,16 @@ const (
 // 1. Direct Command Line Flag (azblob_backup_account_name, azblob_backup_account_key)
 // 2. Environment variables
 func azInternalCredentials() (string, string, error) {
-	actName := *accountName
+	actName := accountName
 	if actName == "" {
 		// Check the Environmental Value
 		actName = os.Getenv("VT_AZBLOB_ACCOUNT_NAME")
 	}
 
 	var actKey string
-	if *accountKeyFile != "" {
-		log.Infof("Getting Azure Storage Account key from file: %s", *accountKeyFile)
-		dat, err := os.ReadFile(*accountKeyFile)
+	if accountKeyFile != "" {
+		log.Infof("Getting Azure Storage Account key from file: %s", accountKeyFile)
+		dat, err := os.ReadFile(accountKeyFile)
 		if err != nil {
 			return "", "", err
 		}
@@ -203,7 +219,7 @@ func (bh *AZBlobBackupHandle) AddFile(ctx context.Context, filename string, file
 		defer bh.waitGroup.Done()
 		_, err := azblob.UploadStreamToBlockBlob(bh.ctx, reader, blockBlobURL, azblob.UploadStreamToBlockBlobOptions{
 			BufferSize: azblob.BlockBlobMaxStageBlockBytes,
-			MaxBuffers: *azBlobParallelism,
+			MaxBuffers: azBlobParallelism,
 		})
 		if err != nil {
 			reader.CloseWithError(err)
@@ -255,7 +271,7 @@ func (bh *AZBlobBackupHandle) ReadFile(ctx context.Context, filename string) (io
 	return resp.Body(azblob.RetryReaderOptions{
 		MaxRetryRequests: defaultRetryCount,
 		NotifyFailedRead: func(failureCount int, lastError error, offset int64, count int64, willRetry bool) {
-			log.Warningf("ReadFile: [azblob] container: %s, directory: %s, filename: %s, error: %v", *containerName, objName(bh.dir, ""), filename, lastError)
+			log.Warningf("ReadFile: [azblob] container: %s, directory: %s, filename: %s, error: %v", containerName, objName(bh.dir, ""), filename, lastError)
 		},
 		TreatEarlyCloseAsError: true,
 	}), nil
@@ -270,7 +286,7 @@ func (bs *AZBlobBackupStorage) containerURL() (*azblob.ContainerURL, error) {
 	if err != nil {
 		return nil, err
 	}
-	u := azServiceURL(credentials).NewContainerURL(*containerName)
+	u := azServiceURL(credentials).NewContainerURL(containerName)
 	return &u, nil
 }
 
@@ -283,7 +299,7 @@ func (bs *AZBlobBackupStorage) ListBackups(ctx context.Context, dir string) ([]b
 		searchPrefix = objName(dir, "")
 	}
 
-	log.Infof("ListBackups: [azblob] container: %s, directory: %v", *containerName, searchPrefix)
+	log.Infof("ListBackups: [azblob] container: %s, directory: %v", containerName, searchPrefix)
 
 	containerURL, err := bs.containerURL()
 	if err != nil {
@@ -343,7 +359,7 @@ func (bs *AZBlobBackupStorage) StartBackup(ctx context.Context, dir, name string
 
 // RemoveBackup implements BackupStorage.
 func (bs *AZBlobBackupStorage) RemoveBackup(ctx context.Context, dir, name string) error {
-	log.Infof("ListBackups: [azblob] container: %s, directory: %s", *containerName, objName(dir, ""))
+	log.Infof("ListBackups: [azblob] container: %s, directory: %s", containerName, objName(dir, ""))
 
 	containerURL, err := bs.containerURL()
 	if err != nil {
@@ -404,8 +420,8 @@ func (bs *AZBlobBackupStorage) Close() error {
 // Unlike path.Join, it doesn't collapse ".." or strip trailing slashes.
 // It also adds the value of the -azblob_backup_storage_root flag if set.
 func objName(parts ...string) string {
-	if *storageRoot != "" {
-		return *storageRoot + "/" + strings.Join(parts, "/")
+	if storageRoot != "" {
+		return storageRoot + "/" + strings.Join(parts, "/")
 	}
 	return strings.Join(parts, "/")
 }
