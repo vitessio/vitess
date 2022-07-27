f4fef393cc896bf7217884a08639b64a0c789796
Andrew Mason <andrew@planetscale.com>
2022-07-27 07:55:24 -0400

    Add `pflag.Value` implementation to `topoproto.TabletTypeFlag`, migrate only instance
    Partially fixes #10861.
    
    Signed-off-by: Andrew Mason <andrew@planetscale.com>

--- go/vt/topo/topoproto/flag.go
+++ go/vt/topo/topoproto/flag.go
@@ -29,13 +29,6 @@ func TabletTypeListVar(p *[]topodatapb.TabletType, name string, usage string) {
 	flag.Var((*TabletTypeListValue)(p), name, usage)
 }
 
-// TabletTypeVar defines a TabletType flag with the specified name, default value and usage
-// string. The argument 'p' points to a tabletType in which to store the value of the flag.
-func TabletTypeVar(p *topodatapb.TabletType, name string, defaultValue topodatapb.TabletType, usage string) {
-	*p = defaultValue
-	flag.Var((*TabletTypeFlag)(p), name, usage)
-}
-
 // TabletTypeListValue implements the flag.Value interface, for parsing a command-line comma-separated
 // list of value into a slice of TabletTypes.
 type TabletTypeListValue []topodatapb.TabletType
@@ -56,20 +49,23 @@ func (ttlv *TabletTypeListValue) Get() any {
 	return *ttlv
 }
 
-// TabletTypeFlag implements the flag.Value interface, for parsing a command-line value into a TabletType.
+// TabletTypeFlag implements the flag.Value and pflag.Value interfaces, for parsing a command-line value into a TabletType.
 type TabletTypeFlag topodatapb.TabletType
 
-// String is part of the flag.Value interface.
+// String is part of the flag.Value and pflag.Value interfaces.
 func (ttf *TabletTypeFlag) String() string {
 	return topodatapb.TabletType(*ttf).String()
 }
 
-// Set is part of the flag.Value interface.
+// Set is part of the flag.Value and pflag.Value interfaces.
 func (ttf *TabletTypeFlag) Set(v string) error {
 	t, err := ParseTabletType(v)
 	*ttf = TabletTypeFlag(t)
 	return err
 }
+
+// Type is part of the pflag.Value interface.
+func (*TabletTypeFlag) Type() string { return "topodatapb.TabletType" }
 
 // Get is for satisflying the internal flag interface.
 func (ttf *TabletTypeFlag) Get() any {
