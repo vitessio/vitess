51dcabd2a85b084d0f4dc47e63b96fd95aacb7b2
Harshit Gangal <harshit@planetscale.com>
2022-07-31 23:24:10 +0530

    test: benchmarking the new pool
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/pools/rp_bench_test.go
+++ go/pools/rp_bench_test.go
@@ -18,32 +18,80 @@ package pools
 
 import (
 	"context"
-	"strconv"
+	"fmt"
+	"sync"
 	"testing"
 )
 
 func BenchmarkGetPut(b *testing.B) {
+	tcases := []struct {
+		name string
+		pool func(int, int) IResourcePool
+	}{
+		{
+			name: "static",
+			pool: getResourcePool,
+		},
+		{
+			name: "dynamic",
+			pool: getDynamicPool,
+		},
+	}
+
+	b.ReportAllocs()
 	for _, size := range []int{1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024} {
 		parallelism := (size + 1) / 2
 		var pool IResourcePool
-		for _, f := range []func(int, int) IResourcePool{getResourcePool} {
-			pool = f(size, parallelism)
-			b.Run(pool.Name()+"size="+strconv.Itoa(size), func(b *testing.B) {
+		for _, tc := range tcases {
+			rName := fmt.Sprintf("%s:%d", tc.name, size)
+			b.Run(rName, func(b *testing.B) {
+				pool = tc.pool(size, parallelism)
 				for i := 0; i < b.N; i++ {
-					ctx := context.Background()
-					if _, err := pool.Get(ctx); err != nil {
-						b.Error(err)
-					}
-					pool.Put(nil)
+					execTest(b, pool)
 				}
+				pool.Close()
 			})
-			pool.Close()
 		}
 	}
 }
 
+func execTest(b *testing.B, pool IResourcePool) {
+	ctx := context.Background()
+	maxProcs, numReqs := 2, 50
+
+	var wg sync.WaitGroup
+	wg.Add(numReqs)
+
+	reqs := make(chan bool)
+	defer close(reqs)
+
+	for i := 0; i < maxProcs*2; i++ {
+		go func() {
+			for range reqs {
+				if conn, err := pool.Get(ctx); err != nil {
+					b.Error(err)
+				} else {
+					pool.Put(conn)
+				}
+				wg.Done()
+			}
+		}()
+	}
+
+	for i := 0; i < numReqs; i++ {
+		reqs <- true
+	}
+
+	wg.Wait()
+
+}
+
 func getResourcePool(size, parallelism int) IResourcePool {
 	return NewResourcePool(testResourceFactory, size, size, 0, parallelism, nil, nil, 0)
+}
+
+func getDynamicPool(size, _ int) IResourcePool {
+	return NewDynamicResourcePool(testResourceFactory, size)
 }
 
 func testResourceFactory(context.Context) (Resource, error) {
