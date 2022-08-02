123a565fbd3fb2704d1ac56f38175fe6d4f0f689
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2022-08-02 10:20:37 +0300

    proto: BackupRequest.incremental_from_pos
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/proto/tabletmanagerdata/tabletmanagerdata_vtproto.pb.go
+++ go/vt/proto/tabletmanagerdata/tabletmanagerdata_vtproto.pb.go
@@ -4001,6 +4001,13 @@ func (m *BackupRequest) MarshalToSizedBufferVT(dAtA []byte) (int, error) {
 		i -= len(m.unknownFields)
 		copy(dAtA[i:], m.unknownFields)
 	}
+	if len(m.IncrementalFromPos) > 0 {
+		i -= len(m.IncrementalFromPos)
+		copy(dAtA[i:], m.IncrementalFromPos)
+		i = encodeVarint(dAtA, i, uint64(len(m.IncrementalFromPos)))
+		i--
+		dAtA[i] = 0x1a
+	}
 	if m.AllowPrimary {
 		i--
 		if m.AllowPrimary {
@@ -6197,6 +6204,10 @@ func (m *BackupRequest) SizeVT() (n int) {
 	}
 	if m.AllowPrimary {
 		n += 2
+	}
+	l = len(m.IncrementalFromPos)
+	if l > 0 {
+		n += 1 + l + sov(uint64(l))
 	}
 	if m.unknownFields != nil {
 		n += len(m.unknownFields)
@@ -14671,6 +14682,38 @@ func (m *BackupRequest) UnmarshalVT(dAtA []byte) error {
 				}
 			}
 			m.AllowPrimary = bool(v != 0)
+		case 3:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field IncrementalFromPos", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflow
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLength
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLength
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.IncrementalFromPos = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
 			skippy, err := skip(dAtA[iNdEx:])
