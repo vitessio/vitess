7edd1b3cbaabd17d747fa0ca33a829b61ea3df47
Andrew Mason <andrew@planetscale.com>
2022-08-04 17:09:30 -0400

    [cli] [vtgate] Migrate `vtgate/buffer` flags to `pflag` (#10939)
    * Migrate `vtgate/buffer` flags to `pflag`
    
    Signed-off-by: Andrew Mason <andrew@planetscale.com>
    
    * set defaults
    
    Signed-off-by: Andrew Mason <andrew@planetscale.com>
    
    * Remove no-op flag.Set
    
    Signed-off-by: Andrew Mason <andrew@planetscale.com>
    
    * include vtgate/buffer flags in vtcombo
    
    Signed-off-by: Andrew Mason <andrew@planetscale.com>

--- go/vt/vtgate/buffer/test_util.go
+++ go/vt/vtgate/buffer/test_util.go
@@ -18,5 +18,5 @@ package buffer
 
 // SetBufferingModeInTestingEnv should only be used from testing code to change the flag (enable_buffer) default value
 func SetBufferingModeInTestingEnv(enabled bool) {
-	*bufferEnabled = enabled
+	bufferEnabled = enabled
 }
