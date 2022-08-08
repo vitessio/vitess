211d33357e97794f7338dedb3d8d080c82dce79e
Andrew Mason <andrew@planetscale.com>
2022-08-08 06:20:35 -0400

    Tidy imports, make logErrStacks private
    Signed-off-by: Andrew Mason <andrew@planetscale.com>

--- go/vt/vterrors/vterrors.go
+++ go/vt/vterrors/vterrors.go
@@ -86,21 +86,20 @@ limitations under the License.
 package vterrors
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"io"
 
-	"context"
-
 	vtrpcpb "vitess.io/vitess/go/vt/proto/vtrpc"
 )
 
-// LogErrStacks controls whether or not printing errors includes the
+// logErrStacks controls whether or not printing errors includes the
 // embedded stack trace in the output.
-var LogErrStacks bool
+var logErrStacks bool
 
 func init() {
-	flag.BoolVar(&LogErrStacks, "log_err_stacks", false, "log stack traces for errors")
+	flag.BoolVar(&logErrStacks, "log_err_stacks", false, "log stack traces for errors")
 }
 
 // New returns an error with the supplied message.
@@ -153,7 +152,7 @@ func (f *fundamental) Format(s fmt.State, verb rune) {
 	case 'v':
 		panicIfError(io.WriteString(s, "Code: "+f.code.String()+"\n"))
 		panicIfError(io.WriteString(s, f.msg+"\n"))
-		if LogErrStacks {
+		if logErrStacks {
 			f.stack.Format(s, verb)
 		}
 		return
@@ -249,7 +248,7 @@ func (w *wrapping) Format(s fmt.State, verb rune) {
 	if rune('v') == verb {
 		panicIfError(fmt.Fprintf(s, "%v\n", w.Cause()))
 		panicIfError(io.WriteString(s, w.msg))
-		if LogErrStacks {
+		if logErrStacks {
 			w.stack.Format(s, verb)
 		}
 		return
