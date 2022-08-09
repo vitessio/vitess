1b17836c2a9b30b281ef6d1c058c2bbe0d16d7cc
Vicent Marti <vmg@strn.cat>
2022-08-09 12:57:18 +0200

    pools: improve GetPut benchmarks
    Signed-off-by: Vicent Marti <vmg@strn.cat>

--- go/pools/rp_bench_test.go
+++ go/pools/rp_bench_test.go
@@ -19,81 +19,54 @@ package pools
 import (
 	"context"
 	"fmt"
-	"sync"
 	"testing"
 )
 
 func BenchmarkGetPut(b *testing.B) {
+	testResourceFactory := func(context.Context) (Resource, error) {
+		return &TestResource{}, nil
+	}
+
 	tcases := []struct {
 		name string
-		pool func(int, int) IResourcePool
+		pool func(int) IResourcePool
 	}{
 		{
 			name: "static",
-			pool: getResourcePool,
+			pool: func(cap int) IResourcePool {
+				return NewResourcePool(testResourceFactory, cap, cap, 0, 0, nil, nil, 0)
+			},
 		},
 		{
 			name: "dynamic",
-			pool: getDynamicPool,
+			pool: func(cap int) IResourcePool {
+				return NewDynamicResourcePool(testResourceFactory, cap, 0)
+			},
 		},
 	}
 
-	b.ReportAllocs()
-	for _, size := range []int{1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024} {
-		parallelism := (size + 1) / 2
-		var pool IResourcePool
-		for _, tc := range tcases {
-			rName := fmt.Sprintf("%s:%d", tc.name, size)
-			b.Run(rName, func(b *testing.B) {
-				pool = tc.pool(size, parallelism)
-				for i := 0; i < b.N; i++ {
-					execTest(b, pool)
-				}
-				pool.Close()
-			})
+	for _, size := range []int{8, 64, 512} {
+		for _, parallelism := range []int{1, 4, 8, 32, 128} {
+			for _, tc := range tcases {
+				rName := fmt.Sprintf("%s/x%d-cap%d", tc.name, parallelism, size)
+				b.Run(rName, func(b *testing.B) {
+					pool := tc.pool(size)
+					defer pool.Close()
+
+					b.ReportAllocs()
+					b.SetParallelism(parallelism)
+					b.RunParallel(func(pb *testing.PB) {
+						var ctx = context.Background()
+						for pb.Next() {
+							if conn, err := pool.Get(ctx); err != nil {
+								b.Error(err)
+							} else {
+								pool.Put(conn)
+							}
+						}
+					})
+				})
+			}
 		}
 	}
-}
-
-func execTest(b *testing.B, pool IResourcePool) {
-	ctx := context.Background()
-	maxProcs, numReqs := 2, 50
-
-	var wg sync.WaitGroup
-	wg.Add(numReqs)
-
-	reqs := make(chan bool)
-	defer close(reqs)
-
-	for i := 0; i < maxProcs*2; i++ {
-		go func() {
-			for range reqs {
-				if conn, err := pool.Get(ctx); err != nil {
-					b.Error(err)
-				} else {
-					pool.Put(conn)
-				}
-				wg.Done()
-			}
-		}()
-	}
-
-	for i := 0; i < numReqs; i++ {
-		reqs <- true
-	}
-
-	wg.Wait()
-
-}
-
-func getResourcePool(size, parallelism int) IResourcePool {
-	return NewResourcePool(testResourceFactory, size, size, 0, parallelism, nil, nil, 0)
-}
-
-func getDynamicPool(size, _ int) IResourcePool {
-	return NewDynamicResourcePool(testResourceFactory, size, 0)
-}
-
-func testResourceFactory(context.Context) (Resource, error) {
-	return &TestResource{}, nil
 }
