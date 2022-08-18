6f0dae9d76c8919b3431a34728946b0faedee8e0
Harshit Gangal <harshit@planetscale.com>
2022-08-18 21:22:25 +0530

    Remove prefill logic from resource pool (#11002)
    * refactor: removed prefill parallelism, removed unnecessary tracing, added pool benchmark
    
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>
    
    * updated doc and mark flag deprecation
    
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>
    
    * test: fix test expectation
    
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>
    
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/pools/resource_pool.go
+++ go/pools/resource_pool.go
@@ -27,7 +27,6 @@ import (
 
 	"vitess.io/vitess/go/sync2"
 	"vitess.io/vitess/go/timer"
-	"vitess.io/vitess/go/trace"
 	"vitess.io/vitess/go/vt/log"
 	"vitess.io/vitess/go/vt/vterrors"
 
@@ -103,8 +102,6 @@ var (
 
 	// ErrCtxTimeout is returned if a ctx is already expired by the time the resource pool is used
 	ErrCtxTimeout = vterrors.New(vtrpcpb.Code_DEADLINE_EXCEEDED, "resource pool context already expired")
-
-	prefillTimeout = 30 * time.Second
 )
 
 // NewResourcePool creates a new ResourcePool pool.
@@ -120,7 +117,7 @@ var (
 // The value specifies how many resources can be opened in parallel.
 // refreshCheck is a function we consult at refreshInterval
 // intervals to determine if the pool should be drained and reopened
-func NewResourcePool(factory Factory, capacity, maxCap int, idleTimeout time.Duration, prefillParallelism int, logWait func(time.Time), refreshCheck RefreshCheck, refreshInterval time.Duration) *ResourcePool {
+func NewResourcePool(factory Factory, capacity, maxCap int, idleTimeout time.Duration, logWait func(time.Time), refreshCheck RefreshCheck, refreshInterval time.Duration) *ResourcePool {
 	if capacity <= 0 || maxCap <= 0 || capacity > maxCap {
 		panic(errors.New("invalid/out of range capacity"))
 	}
@@ -134,35 +131,6 @@ func NewResourcePool(factory Factory, capacity, maxCap int, idleTimeout time.Dur
 	}
 	for i := 0; i < capacity; i++ {
 		rp.resources <- resourceWrapper{}
-	}
-
-	ctx, cancel := context.WithTimeout(context.TODO(), prefillTimeout)
-	defer cancel()
-	if prefillParallelism != 0 {
-		sem := sync2.NewSemaphore(prefillParallelism, 0 /* timeout */)
-		var wg sync.WaitGroup
-		for i := 0; i < capacity; i++ {
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				_ = sem.Acquire()
-				defer sem.Release()
-
-				// If context has expired, give up.
-				select {
-				case <-ctx.Done():
-					return
-				default:
-				}
-
-				r, err := rp.Get(ctx)
-				if err != nil {
-					return
-				}
-				rp.Put(r)
-			}()
-		}
-		wg.Wait()
 	}
 
 	if idleTimeout != 0 {
@@ -238,21 +206,13 @@ func (rp *ResourcePool) reopen() {
 // it will wait till the next resource becomes available or a timeout.
 // A timeout of 0 is an indefinite wait.
 func (rp *ResourcePool) Get(ctx context.Context) (resource Resource, err error) {
-	span, ctx := trace.NewSpan(ctx, "ResourcePool.Get")
-	span.Annotate("capacity", rp.capacity.Get())
-	span.Annotate("in_use", rp.inUse.Get())
-	span.Annotate("available", rp.available.Get())
-	span.Annotate("active", rp.active.Get())
-	defer span.Finish()
 	return rp.get(ctx)
 }
 
 func (rp *ResourcePool) get(ctx context.Context) (resource Resource, err error) {
 	// If ctx has already expired, avoid racing with rp's resource channel.
-	select {
-	case <-ctx.Done():
+	if ctx.Err() != nil {
 		return nil, ErrCtxTimeout
-	default:
 	}
 
 	// Fetch
@@ -275,9 +235,7 @@ func (rp *ResourcePool) get(ctx context.Context) (resource Resource, err error) 
 
 	// Unwrap
 	if wrapper.resource == nil {
-		span, _ := trace.NewSpan(ctx, "ResourcePool.factory")
 		wrapper.resource, err = rp.factory(ctx)
-		span.Finish()
 		if err != nil {
 			rp.resources <- resourceWrapper{}
 			return nil, err
--- go/pools/rp_bench_test.go
+++ go/pools/rp_bench_test.go
@@ -1,12 +1,9 @@
 /*
 Copyright 2022 The Vitess Authors.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,34 +15,47 @@ package pools
 
 import (
 	"context"
-	"strconv"
+	"fmt"
 	"testing"
 )
 
 func BenchmarkGetPut(b *testing.B) {
-	for _, size := range []int{1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024} {
-		parallelism := (size + 1) / 2
-		var pool IResourcePool
-		for _, f := range []func(int, int) IResourcePool{getResourcePool} {
-			pool = f(size, parallelism)
-			b.Run(pool.Name()+"size="+strconv.Itoa(size), func(b *testing.B) {
-				for i := 0; i < b.N; i++ {
-					ctx := context.Background()
-					if _, err := pool.Get(ctx); err != nil {
-						b.Error(err)
-					}
-					pool.Put(nil)
-				}
-			})
-			pool.Close()
+	testResourceFactory := func(context.Context) (Resource, error) {
+		return &TestResource{}, nil
+	}
+
+	tcases := []struct {
+		name string
+		pool func(int) IResourcePool
+	}{{
+		name: "static",
+		pool: func(cap int) IResourcePool {
+			return NewResourcePool(testResourceFactory, cap, cap, 0, nil, nil, 0)
+		},
+	}}
+
+	for _, size := range []int{64, 128, 512} {
+		for _, parallelism := range []int{1, 8, 32, 128} {
+			for _, tc := range tcases {
+				rName := fmt.Sprintf("%s/x%d-cap%d", tc.name, parallelism, size)
+				b.Run(rName, func(b *testing.B) {
+					pool := tc.pool(size)
+					defer pool.Close()
+
+					b.ReportAllocs()
+					b.SetParallelism(parallelism)
+					b.RunParallel(func(pb *testing.PB) {
+						var ctx = context.Background()
+						for pb.Next() {
+							if conn, err := pool.Get(ctx); err != nil {
+								b.Error(err)
+							} else {
+								pool.Put(conn)
+							}
+						}
+					})
+				})
+			}
 		}
 	}
-}
-
-func getResourcePool(size, parallelism int) IResourcePool {
-	return NewResourcePool(testResourceFactory, size, size, 0, parallelism, nil, nil, 0)
-}
-
-func testResourceFactory(context.Context) (Resource, error) {
-	return &TestResource{}, nil
 }
