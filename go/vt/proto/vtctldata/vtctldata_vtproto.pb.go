9219316ea8891a9c3242c2cd29efbd10eefce2d4
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2022-08-31 11:33:48 +0300

    RestoreFromBackupRequest: RestoreToPos
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/proto/vtctldata/vtctldata_vtproto.pb.go
+++ go/vt/proto/vtctldata/vtctldata_vtproto.pb.go
@@ -6432,6 +6432,13 @@ func (m *RestoreFromBackupRequest) MarshalToSizedBufferVT(dAtA []byte) (int, err
 		i -= len(m.unknownFields)
 		copy(dAtA[i:], m.unknownFields)
 	}
+	if len(m.RestoreToPos) > 0 {
+		i -= len(m.RestoreToPos)
+		copy(dAtA[i:], m.RestoreToPos)
+		i = encodeVarint(dAtA, i, uint64(len(m.RestoreToPos)))
+		i--
+		dAtA[i] = 0x1a
+	}
 	if m.BackupTime != nil {
 		size, err := m.BackupTime.MarshalToSizedBufferVT(dAtA[:i])
 		if err != nil {
@@ -11695,6 +11702,10 @@ func (m *RestoreFromBackupRequest) SizeVT() (n int) {
 	}
 	if m.BackupTime != nil {
 		l = m.BackupTime.SizeVT()
+		n += 1 + l + sov(uint64(l))
+	}
+	l = len(m.RestoreToPos)
+	if l > 0 {
 		n += 1 + l + sov(uint64(l))
 	}
 	if m.unknownFields != nil {
@@ -27657,6 +27668,38 @@ func (m *RestoreFromBackupRequest) UnmarshalVT(dAtA []byte) error {
 			if err := m.BackupTime.UnmarshalVT(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
+			iNdEx = postIndex
+		case 3:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field RestoreToPos", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflow
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLength
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLength
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.RestoreToPos = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
