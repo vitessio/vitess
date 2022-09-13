a4eed07d8f341bb518cc146423ff2a3bee46915f
Matt Lord <mattalord@gmail.com>
2022-09-13 15:21:45 -0400

    VReplication: Handle DECIMAL 0 Value Edge Case (#11212)
    * Handle DECIMAL 0 val edge case
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Add testing
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Add vplayer/vcopier test for decimal with 0 scale. Use different logic for handling zero values with 0 scale
    
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>
    
    * Minor changes after self review
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>
    Co-authored-by: Rohit Nayak <rohit@planetscale.com>

--- go/test/endtoend/vreplication/config_test.go
+++ go/test/endtoend/vreplication/config_test.go
@@ -36,7 +36,7 @@ var (
 create table product(pid int, description varbinary(128), date1 datetime not null default '0000-00-00 00:00:00', date2 datetime not null default '2021-00-01 00:00:00', primary key(pid)) CHARSET=utf8mb4;
 create table customer(cid int, name varchar(128) collate utf8mb4_bin, meta json default null, typ enum('individual','soho','enterprise'), sport set('football','cricket','baseball'),
 	ts timestamp not null default current_timestamp, bits bit(2) default b'11', date1 datetime not null default '0000-00-00 00:00:00', 
-	date2 datetime not null default '2021-00-01 00:00:00', primary key(cid,typ)) CHARSET=utf8mb4;
+	date2 datetime not null default '2021-00-01 00:00:00', dec80 decimal(8,0), primary key(cid,typ)) CHARSET=utf8mb4;
 create table customer_seq(id int, next_id bigint, cache bigint, primary key(id)) comment 'vitess_sequence';
 create table merchant(mname varchar(128), category varchar(128), primary key(mname)) CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 create table orders(oid int, cid int, pid int, mname varchar(128), price int, qty int, total int as (qty * price), total2 int as (qty * price) stored, primary key(oid)) CHARSET=utf8;
