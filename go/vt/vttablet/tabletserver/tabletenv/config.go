3a496ec9fdfd2060e42866f393edfff0b0acb256
Harshit Gangal <harshit@planetscale.com>
2022-09-15 11:18:09 +0530

    addressing review comments from #11088 (#11221)
    * refactor: move pool unsafe query test to query_engine test
    
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>
    
    * fix: changed the new flag to newer convention of dashes than underscore
    
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>
    
    * added release notes
    
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>
    
    * test: fix test expectation
    
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>
    
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/vt/vttablet/tabletserver/tabletenv/config.go
+++ go/vt/vttablet/tabletserver/tabletenv/config.go
@@ -173,7 +173,7 @@ func registerTabletEnvFlags(fs *pflag.FlagSet) {
 	fs.BoolVar(&enableReplicationReporter, "enable_replication_reporter", false, "Use polling to track replication lag.")
 	fs.BoolVar(&currentConfig.EnableOnlineDDL, "queryserver_enable_online_ddl", true, "Enable online DDL.")
 	fs.BoolVar(&currentConfig.SanitizeLogMessages, "sanitize_log_messages", false, "Remove potentially sensitive information in tablet INFO, WARNING, and ERROR log messages such as query parameters.")
-	fs.BoolVar(&currentConfig.EnableSettingsPool, "queryserver_enable_settings_pool", false, "Enable pooling of connections with modified system settings")
+	fs.BoolVar(&currentConfig.EnableSettingsPool, "queryserver-enable-settings-pool", false, "Enable pooling of connections with modified system settings")
 
 	fs.Int64Var(&currentConfig.RowStreamer.MaxInnoDBTrxHistLen, "vreplication_copy_phase_max_innodb_history_list_length", 1000000, "The maximum InnoDB transaction history that can exist on a vstreamer (source) before starting another round of copying rows. This helps to limit the impact on the source tablet.")
 	fs.Int64Var(&currentConfig.RowStreamer.MaxMySQLReplLagSecs, "vreplication_copy_phase_max_mysql_replication_lag", 43200, "The maximum MySQL replication lag (in seconds) that can exist on a vstreamer (source) before starting another round of copying rows. This helps to limit the impact on the source tablet.")
