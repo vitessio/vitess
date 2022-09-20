c88ef86c0943b4404854f5ccc50a19339ea312d9
Matt Lord <mattalord@gmail.com>
2022-09-20 16:35:46 -0400

    Prevent VStreamer engine deadlocks during state transitions
    The VStreamer engine is somewhat unusual in two ways:
      1. It is open and running on replica tablets rather than only
         running on primary tablets.
      2. It has no controllers so the main engine mutex is widely shared.
    
    Because of this, when a tablet has open vstreams (direct binary log
    streams) performing work and a state transition starts, it can
    deadlock with the tabletmanager's state lock when checking if
    the engine is open or not.
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/vt/vttablet/tabletserver/vstreamer/vstreamer.go
+++ go/vt/vttablet/tabletserver/vstreamer/vstreamer.go
@@ -28,6 +28,7 @@ import (
 	"vitess.io/vitess/go/mysql/collations"
 	"vitess.io/vitess/go/timer"
 	vtschema "vitess.io/vitess/go/vt/schema"
+	"vitess.io/vitess/go/vt/vterrors"
 
 	"vitess.io/vitess/go/mysql"
 	"vitess.io/vitess/go/sqltypes"
@@ -40,6 +41,7 @@ import (
 
 	binlogdatapb "vitess.io/vitess/go/vt/proto/binlogdata"
 	querypb "vitess.io/vitess/go/vt/proto/query"
+	vtrpcpb "vitess.io/vitess/go/vt/proto/vtrpc"
 )
 
 const (
@@ -284,13 +286,18 @@ func (vs *vstreamer) parseEvents(ctx context.Context, events <-chan mysql.Binlog
 
 	injectHeartbeat := func(throttled bool) error {
 		now := time.Now().UnixNano()
-		err := bufferAndTransmit(&binlogdatapb.VEvent{
-			Type:        binlogdatapb.VEventType_HEARTBEAT,
-			Timestamp:   now / 1e9,
-			CurrentTime: now,
-			Throttled:   throttled,
-		})
-		return err
+		select {
+		case <-ctx.Done():
+			return vterrors.Errorf(vtrpcpb.Code_CANCELED, "context has expired")
+		default:
+			err := bufferAndTransmit(&binlogdatapb.VEvent{
+				Type:        binlogdatapb.VEventType_HEARTBEAT,
+				Timestamp:   now / 1e9,
+				CurrentTime: now,
+				Throttled:   throttled,
+			})
+			return err
+		}
 	}
 
 	throttleEvents := func(throttledEvents chan mysql.BinlogEvent) {
@@ -367,11 +374,16 @@ func (vs *vstreamer) parseEvents(ctx context.Context, events <-chan mysql.Binlog
 				}
 			}
 		case vs.vschema = <-vs.vevents:
-			if err := vs.rebuildPlans(); err != nil {
-				return err
+			select {
+			case <-ctx.Done():
+				return nil
+			default:
+				if err := vs.rebuildPlans(); err != nil {
+					return err
+				}
+				// Increment this counter for testing.
+				vschemaUpdateCount.Add(1)
 			}
-			// Increment this counter for testing.
-			vschemaUpdateCount.Add(1)
 		case <-ctx.Done():
 			return nil
 		case <-hbTimer.C:
