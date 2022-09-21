48cd261881676da30e657086e4b2c9579cf45307
Matt Lord <mattalord@gmail.com>
2022-09-21 16:08:47 -0400

    Address review comments
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/mysql/auth_server_static.go
+++ go/mysql/auth_server_static.go
@@ -41,7 +41,7 @@ var (
 	mysqlAuthServerStaticFile           string
 	mysqlAuthServerStaticString         string
 	mysqlAuthServerStaticReloadInterval time.Duration
-	mysqlServerFlushDelay               time.Duration
+	mysqlServerFlushDelay               = 100 * time.Millisecond
 )
 
 func init() {
@@ -49,7 +49,7 @@ func init() {
 		fs.StringVar(&mysqlAuthServerStaticFile, "mysql_auth_server_static_file", "", "JSON File to read the users/passwords from.")
 		fs.StringVar(&mysqlAuthServerStaticString, "mysql_auth_server_static_string", "", "JSON representation of the users/passwords config.")
 		fs.DurationVar(&mysqlAuthServerStaticReloadInterval, "mysql_auth_static_reload_interval", 0, "Ticker to reload credentials")
-		fs.DurationVar(&mysqlServerFlushDelay, "mysql_server_flush_delay", 100*time.Millisecond, "Delay after which buffered response will be flushed to the client.")
+		fs.DurationVar(&mysqlServerFlushDelay, "mysql_server_flush_delay", mysqlServerFlushDelay, "Delay after which buffered response will be flushed to the client.")
 	})
 }
 
