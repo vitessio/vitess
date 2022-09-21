39b3726549e9fef381d48b6d1b53ee1cb5cc4e47
notfelineit <notfelineit@gmail.com>
2022-09-21 10:46:34 -0700

    Setting up page for shard actions
    Signed-off-by: notfelineit <notfelineit@gmail.com>

---
+++ web/vtadmin/src/components/routes/shard/Advanced.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Advanced: React.FC = () => {
+  return (
+    <>
+      <div className="pt-4">
+        <div className="my-8">
+          <h3 className="mb-4">Status</h3>
+        </div>
+      </div>
+      <div className="pt-4">
+        <div className="my-8">
+          <h3 className="mb-4">Change</h3>
+        </div>
+      </div>
+      <div className="pt-4">
+        <div className="my-8">
+          <h3 className="mb-4">Reparent</h3>
+        </div>
+      </div>
+    </>
+
+  )
+}
+
+export default Advanced
--- web/vtadmin/src/components/routes/shard/Shard.tsx
+++ web/vtadmin/src/components/routes/shard/Shard.tsx
@@ -29,105 +29,109 @@ import { useDocumentTitle } from '../../../hooks/useDocumentTitle';
 import { KeyspaceLink } from '../../links/KeyspaceLink';
 import { useKeyspace } from '../../../hooks/api';
 import { ShardTablets } from './ShardTablets';
+import Advanced from './Advanced';
 
 interface RouteParams {
-    clusterID: string;
-    keyspace: string;
-    shard: string;
+  clusterID: string;
+  keyspace: string;
+  shard: string;
 }
 
 export const Shard = () => {
-    const params = useParams<RouteParams>();
-    const { path, url } = useRouteMatch();
+  const params = useParams<RouteParams>();
+  const { path, url } = useRouteMatch();
 
-    const shardName = `${params.keyspace}/${params.shard}`;
+  const shardName = `${params.keyspace}/${params.shard}`;
 
-    useDocumentTitle(`${shardName} (${params.clusterID})`);
+  useDocumentTitle(`${shardName} (${params.clusterID})`);
 
-    const { data: keyspace, ...kq } = useKeyspace({ clusterID: params.clusterID, name: params.keyspace });
+  const { data: keyspace, ...kq } = useKeyspace({ clusterID: params.clusterID, name: params.keyspace });
 
-    if (kq.error) {
-        return (
-            <div className={style.placeholder}>
-                <span className={style.errorEmoji}>😰</span>
-                <h1>An error occurred</h1>
-                <code>{(kq.error as any).response?.error?.message || kq.error?.message}</code>
-                <p>
-                    <Link to="/keyspaces">← All keyspaces</Link>
-                </p>
-            </div>
-        );
-    }
-
-    if (!kq.isLoading && !keyspace) {
-        return (
-            <div className={style.placeholder}>
-                <span className={style.errorEmoji}>😖</span>
-                <h1>Keyspace not found</h1>
-                <p>
-                    <Link to="/keyspaces">← All keyspaces</Link>
-                </p>
-            </div>
-        );
-    }
-
-    let shard = null;
-    if (keyspace?.shards && params.shard in keyspace.shards) {
-        shard = keyspace.shards[params.shard];
-    }
-
-    if (!kq.isLoading && !shard) {
-        return (
-            <div className={style.placeholder}>
-                <span className={style.errorEmoji}>😖</span>
-                <h1>Shard not found</h1>
-                <p>
-                    <KeyspaceLink clusterID={params.clusterID} name={params.keyspace}>
-                        ← All shards in {params.keyspace}
-                    </KeyspaceLink>
-                </p>
-            </div>
-        );
-    }
-
+  if (kq.error) {
     return (
-        <div>
-            <WorkspaceHeader>
-                <NavCrumbs>
-                    <Link to="/keyspaces">Keyspaces</Link>
-                    <KeyspaceLink clusterID={params.clusterID} name={params.keyspace}>
-                        {params.keyspace}
-                    </KeyspaceLink>
-                </NavCrumbs>
-
-                <WorkspaceTitle className="font-mono">{shardName}</WorkspaceTitle>
-
-                <div className={style.headingMeta}>
-                    <span>
-                        Cluster: <code>{params.clusterID}</code>
-                    </span>
-                </div>
-            </WorkspaceHeader>
-
-            <ContentContainer>
-                <TabContainer>
-                    <Tab text="Tablets" to={`${url}/tablets`} />
-                    <Tab text="JSON" to={`${url}/json`} />
-                </TabContainer>
-
-                <Switch>
-                    <Route path={`${path}/tablets`}>
-                        <ShardTablets {...params} />
-                    </Route>
-
-                    <Route path={`${path}/json`}>{shard && <Code code={JSON.stringify(shard, null, 2)} />}</Route>
-
-                    <Redirect from={path} to={`${path}/tablets`} />
-                </Switch>
-            </ContentContainer>
-
-            {/* TODO skeleton placeholder */}
-            {!!kq.isLoading && <div className={style.placeholder}>Loading</div>}
-        </div>
+      <div className={style.placeholder}>
+        <span className={style.errorEmoji}>😰</span>
+        <h1>An error occurred</h1>
+        <code>{(kq.error as any).response?.error?.message || kq.error?.message}</code>
+        <p>
+          <Link to="/keyspaces">← All keyspaces</Link>
+        </p>
+      </div>
     );
+  }
+
+  if (!kq.isLoading && !keyspace) {
+    return (
+      <div className={style.placeholder}>
+        <span className={style.errorEmoji}>😖</span>
+        <h1>Keyspace not found</h1>
+        <p>
+          <Link to="/keyspaces">← All keyspaces</Link>
+        </p>
+      </div>
+    );
+  }
+
+  let shard = null;
+  if (keyspace?.shards && params.shard in keyspace.shards) {
+    shard = keyspace.shards[params.shard];
+  }
+
+  if (!kq.isLoading && !shard) {
+    return (
+      <div className={style.placeholder}>
+        <span className={style.errorEmoji}>😖</span>
+        <h1>Shard not found</h1>
+        <p>
+          <KeyspaceLink clusterID={params.clusterID} name={params.keyspace}>
+            ← All shards in {params.keyspace}
+          </KeyspaceLink>
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <WorkspaceHeader>
+        <NavCrumbs>
+          <Link to="/keyspaces">Keyspaces</Link>
+          <KeyspaceLink clusterID={params.clusterID} name={params.keyspace}>
+            {params.keyspace}
+          </KeyspaceLink>
+        </NavCrumbs>
+
+        <WorkspaceTitle className="font-mono">{shardName}</WorkspaceTitle>
+
+        <div className={style.headingMeta}>
+          <span>
+            Cluster: <code>{params.clusterID}</code>
+          </span>
+        </div>
+      </WorkspaceHeader>
+
+      <ContentContainer>
+        <TabContainer>
+          <Tab text="Tablets" to={`${url}/tablets`} />
+          <Tab text="JSON" to={`${url}/json`} />
+          <Tab text="Advanced" to={`${url}/advanced`} />
+        </TabContainer>
+
+        <Switch>
+          <Route path={`${path}/tablets`}>
+            <ShardTablets {...params} />
+          </Route>
+
+          <Route path={`${path}/json`}>{shard && <Code code={JSON.stringify(shard, null, 2)} />}</Route>
+          <Route path={`${path}/advanced`}>
+            <Advanced />
+          </Route>
+          <Redirect from={path} to={`${path}/tablets`} />
+        </Switch>
+      </ContentContainer>
+
+      {/* TODO skeleton placeholder */}
+      {!!kq.isLoading && <div className={style.placeholder}>Loading</div>}
+    </div>
+  );
 };
