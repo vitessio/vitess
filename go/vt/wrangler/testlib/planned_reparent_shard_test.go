3fd027eaee33d986e38440299fc1021834565dee
Deepthi Sigireddi <deepthi@planetscale.com>
2022-09-26 19:37:03 -0700

    Delete deprecated flags (#11360)
    * flags: remove deprecated from test comment
    
    Signed-off-by: deepthi <deepthi@planetscale.com>
    
    * flags: remove deprecated planner_version flag
    
    Signed-off-by: deepthi <deepthi@planetscale.com>
    
    * flags: remove deprecated master_connect_retry flag
    
    Signed-off-by: deepthi <deepthi@planetscale.com>
    
    Signed-off-by: deepthi <deepthi@planetscale.com>

--- go/vt/wrangler/testlib/planned_reparent_shard_test.go
+++ go/vt/wrangler/testlib/planned_reparent_shard_test.go
@@ -130,7 +130,6 @@ func TestPlannedReparentShardNoPrimaryProvided(t *testing.T) {
 	defer goodReplica1.StopActionLoop(t)
 
 	// run PlannedReparentShard
-	// using deprecated flag until it is removed completely. at that time this should be replaced with -wait_replicas_timeout
 	err := vp.Run([]string{"PlannedReparentShard", "--wait_replicas_timeout", "10s", "--keyspace_shard", newPrimary.Tablet.Keyspace + "/" + newPrimary.Tablet.Shard})
 	require.NoError(t, err)
 
