c8dcac134ba754b21711cf3ec4134f9642824e9d
Andres Taylor <andres@planetscale.com>
2022-09-27 09:09:26 +0200

    [vtexplain] Migrate to pflags
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/cmd/vtexplain/vtexplain.go
+++ go/cmd/vtexplain/vtexplain.go
@@ -17,7 +17,6 @@ limitations under the License.
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 
@@ -28,61 +27,46 @@ import (
 	"vitess.io/vitess/go/vt/vtexplain"
 	"vitess.io/vitess/go/vt/vtgate/planbuilder/plancontext"
 
+	"github.com/spf13/pflag"
+
 	querypb "vitess.io/vitess/go/vt/proto/query"
-	// Include deprecation warnings for soon-to-be-unsupported flag invocations.
-	_flag "vitess.io/vitess/go/internal/flag"
 )
 
 var (
-	sqlFlag            = flag.String("sql", "", "A list of semicolon-delimited SQL commands to analyze")
-	sqlFileFlag        = flag.String("sql-file", "", "Identifies the file that contains the SQL commands to analyze")
-	schemaFlag         = flag.String("schema", "", "The SQL table schema")
-	schemaFileFlag     = flag.String("schema-file", "", "Identifies the file that contains the SQL table schema")
-	vschemaFlag        = flag.String("vschema", "", "Identifies the VTGate routing schema")
-	vschemaFileFlag    = flag.String("vschema-file", "", "Identifies the VTGate routing schema file")
-	ksShardMapFlag     = flag.String("ks-shard-map", "", "JSON map of keyspace name -> shard name -> ShardReference object. The inner map is the same as the output of FindAllShardsInKeyspace")
-	ksShardMapFileFlag = flag.String("ks-shard-map-file", "", "File containing json blob of keyspace name -> shard name -> ShardReference object")
-	numShards          = flag.Int("shards", 2, "Number of shards per keyspace. Passing --ks-shard-map/--ks-shard-map-file causes this flag to be ignored.")
-	executionMode      = flag.String("execution-mode", "multi", "The execution mode to simulate -- must be set to multi, legacy-autocommit, or twopc")
-	replicationMode    = flag.String("replication-mode", "ROW", "The replication mode to simulate -- must be set to either ROW or STATEMENT")
-	normalize          = flag.Bool("normalize", false, "Whether to enable vtgate normalization")
-	outputMode         = flag.String("output-mode", "text", "Output in human-friendly text or json")
-	dbName             = flag.String("dbname", "", "Optional database target to override normal routing")
-	plannerVersionStr  = flag.String("planner-version", "", "Sets the query planner version to use when generating the explain output. Valid values are V3 and Gen4")
-
-	// vtexplainFlags lists all the flags that should show in usage
-	vtexplainFlags = []string{
-		"output-mode",
-		"planner-version",
-		"normalize",
-		"shards",
-		"replication-mode",
-		"schema",
-		"schema-file",
-		"sql",
-		"sql-file",
-		"vschema",
-		"vschema-file",
-		"ks-shard-map",
-		"ks-shard-map-file",
-		"dbname",
-		"queryserver-config-passthrough-dmls",
-	}
+	sqlFlag            string
+	sqlFileFlag        string
+	schemaFlag         string
+	schemaFileFlag     string
+	vschemaFlag        string
+	vschemaFileFlag    string
+	ksShardMapFlag     string
+	ksShardMapFileFlag string
+	numShards          int
+	executionMode      string
+	replicationMode    string
+	normalize          bool
+	outputMode         string
+	dbName             string
+	plannerVersionStr  string
 )
 
 func init() {
-	logger := logutil.NewConsoleLogger()
-	flag.CommandLine.SetOutput(logutil.NewLoggerWriter(logger))
-	_flag.SetUsage(flag.CommandLine, _flag.UsageOptions{
-		FlagFilter: func(f *flag.Flag) bool {
-			for _, name := range vtexplainFlags {
-				if f.Name == name {
-					return true
-				}
-			}
-
-			return false
-		},
+	servenv.OnParse(func(fs *pflag.FlagSet) {
+		fs.StringVar(&sqlFlag, "sql", "", "A list of semicolon-delimited SQL commands to analyze")
+		fs.StringVar(&sqlFileFlag, "sql-file", "", "Identifies the file that contains the SQL commands to analyze")
+		fs.StringVar(&schemaFlag, "schema", "", "The SQL table schema")
+		fs.StringVar(&schemaFileFlag, "schema-file", "", "Identifies the file that contains the SQL table schema")
+		fs.StringVar(&vschemaFlag, "vschema", "", "Identifies the VTGate routing schema")
+		fs.StringVar(&vschemaFileFlag, "vschema-file", "", "Identifies the VTGate routing schema file")
+		fs.StringVar(&ksShardMapFlag, "ks-shard-map", "", "JSON map of keyspace name -> shard name -> ShardReference object. The inner map is the same as the output of FindAllShardsInKeyspace")
+		fs.StringVar(&ksShardMapFileFlag, "ks-shard-map-file", "", "File containing json blob of keyspace name -> shard name -> ShardReference object")
+		fs.IntVar(&numShards, "shards", 2, "Number of shards per keyspace. Passing --ks-shard-map/--ks-shard-map-file causes this flag to be ignored.")
+		fs.StringVar(&executionMode, "execution-mode", "multi", "The execution mode to simulate -- must be set to multi, legacy-autocommit, or twopc")
+		fs.StringVar(&replicationMode, "replication-mode", "ROW", "The replication mode to simulate -- must be set to either ROW or STATEMENT")
+		fs.BoolVar(&normalize, "normalize", false, "Whether to enable vtgate normalization")
+		fs.StringVar(&outputMode, "output-mode", "text", "Output in human-friendly text or json")
+		fs.StringVar(&dbName, "dbname", "", "Optional database target to override normal routing")
+		fs.StringVar(&plannerVersionStr, "planner-version", "", "Sets the query planner version to use when generating the explain output. Valid values are V3 and Gen4")
 	})
 }
 
@@ -123,38 +107,38 @@ func main() {
 }
 
 func parseAndRun() error {
-	plannerVersion, _ := plancontext.PlannerNameToVersion(*plannerVersionStr)
+	plannerVersion, _ := plancontext.PlannerNameToVersion(plannerVersionStr)
 	if plannerVersion != querypb.ExecuteOptions_V3 && plannerVersion != querypb.ExecuteOptions_Gen4 {
-		return fmt.Errorf("invalid value specified for planner-version of '%s' -- valid values are V3 and Gen4", *plannerVersionStr)
+		return fmt.Errorf("invalid value specified for planner-version of '%s' -- valid values are V3 and Gen4", plannerVersionStr)
 	}
 
-	sql, err := getFileParam(*sqlFlag, *sqlFileFlag, "sql", true)
+	sql, err := getFileParam(sqlFlag, sqlFileFlag, "sql", true)
 	if err != nil {
 		return err
 	}
 
-	schema, err := getFileParam(*schemaFlag, *schemaFileFlag, "schema", true)
+	schema, err := getFileParam(schemaFlag, schemaFileFlag, "schema", true)
 	if err != nil {
 		return err
 	}
 
-	vschema, err := getFileParam(*vschemaFlag, *vschemaFileFlag, "vschema", true)
+	vschema, err := getFileParam(vschemaFlag, vschemaFileFlag, "vschema", true)
 	if err != nil {
 		return err
 	}
 
-	ksShardMap, err := getFileParam(*ksShardMapFlag, *ksShardMapFileFlag, "ks-shard-map", false)
+	ksShardMap, err := getFileParam(ksShardMapFlag, ksShardMapFileFlag, "ks-shard-map", false)
 	if err != nil {
 		return err
 	}
 
 	opts := &vtexplain.Options{
-		ExecutionMode:   *executionMode,
+		ExecutionMode:   executionMode,
 		PlannerVersion:  plannerVersion,
-		ReplicationMode: *replicationMode,
-		NumShards:       *numShards,
-		Normalize:       *normalize,
-		Target:          *dbName,
+		ReplicationMode: replicationMode,
+		NumShards:       numShards,
+		Normalize:       normalize,
+		Target:          dbName,
 	}
 
 	log.V(100).Infof("sql %s\n", sql)
@@ -172,7 +156,7 @@ func parseAndRun() error {
 		return err
 	}
 
-	if *outputMode == "text" {
+	if outputMode == "text" {
 		fmt.Print(vte.ExplainsAsText(plans))
 	} else {
 		fmt.Print(vtexplain.ExplainsAsJSON(plans))
