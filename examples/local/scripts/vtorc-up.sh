67c0eaaa099a253e7cd348f71c3985755847779d
Manan Gupta <manan@planetscale.com>
2022-09-28 15:02:08 +0530

    feat: fix all the examples to not pass in the ListenAddress and not export port for the old UI
    Signed-off-by: Manan Gupta <manan@planetscale.com>

--- examples/local/scripts/vtorc-up.sh
+++ examples/local/scripts/vtorc-up.sh
@@ -3,13 +3,10 @@
 source ./env.sh
 
 log_dir="${VTDATAROOT}/tmp"
-web_dir="../../web/vtorc"
-vtorc_web_port=16000
-port=16001
+port=16000
 
 vtorc \
   $TOPOLOGY_FLAGS \
-  --orc_web_dir "${web_dir}" \
   --logtostderr \
   --alsologtostderr \
   --config="./vtorc/config.json" \
@@ -21,8 +18,7 @@ echo ${vtorc_pid} > "${log_dir}/vtorc.pid"
 
 echo "\
 vtorc is running!
-  - UI: http://localhost:${vtorc_web_port}
-  - Debug UI: http://localhost:${port}
+  - UI: http://localhost:${port}
   - Logs: ${log_dir}/vtorc.out
   - PID: ${vtorc_pid}
 "
