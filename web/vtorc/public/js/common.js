c884b813cc484e1f07bd191b08843d0b93e34283
Manan Gupta <manan@planetscale.com>
2022-09-28 14:53:42 +0530

    feat: remove web/vtorc files and fix Dockerfiles to not try to copy them
    Signed-off-by: Manan Gupta <manan@planetscale.com>

--- web/vtorc/public/js/common.js
+++
@@ -1,6 +0,0 @@
-function Instance(){
-
-}
-Instance.createElement = function (instance){
-    return $('<div class="instance"><div></div><h3 class="instance-title"></h3><div class="instance-content"></div></div>');
-}
