a8806829a46dfbc25d584de3b46a23d5b088cee3
notfelineit <notfelineit@gmail.com>
2022-09-30 14:37:28 -0700

    Add validate to cluster page
    Signed-off-by: notfelineit <notfelineit@gmail.com>

--- web/vtadmin/src/components/dialog/Dialog.tsx
+++ web/vtadmin/src/components/dialog/Dialog.tsx
@@ -3,24 +3,24 @@ import { Dialog as HUDialog, Transition } from '@headlessui/react';
 import { Icon, Icons } from '../Icon';
 
 interface DialogProps {
-    icon?: Icons;
-    isOpen: boolean;
-    title?: string;
-    description?: string;
-    content?: React.ReactElement;
-    onCancel?: () => void;
-    onConfirm?: Function;
-    onClose?: () => void;
-    loading?: boolean;
-    loadingText?: string;
-    confirmText?: string;
-    cancelText?: string;
-    footer?: React.ReactElement;
-    hideFooter?: boolean;
-    children?: React.ReactElement;
-    hideConfirm?: boolean;
-    hideCancel?: boolean;
-    className?: string;
+  icon?: Icons;
+  isOpen: boolean;
+  title?: string;
+  description?: string;
+  content?: React.ReactElement;
+  onCancel?: () => void;
+  onConfirm?: Function;
+  onClose?: () => void;
+  loading?: boolean;
+  loadingText?: string;
+  confirmText?: string;
+  cancelText?: string;
+  footer?: React.ReactElement;
+  hideFooter?: boolean;
+  children?: React.ReactElement;
+  hideConfirm?: boolean;
+  hideCancel?: boolean;
+  className?: string;
 }
 
 /**
@@ -28,127 +28,126 @@ interface DialogProps {
  * User should pass in a function to set isOpen to the onClose property.
  */
 const Dialog: React.FC<DialogProps> = ({
-    icon,
-    title,
-    description,
-    children,
-    isOpen,
-    loading,
-    loadingText,
-    confirmText,
-    cancelText,
-    footer,
-    hideFooter,
-    hideCancel,
-    hideConfirm,
-    onCancel,
-    onConfirm,
-    onClose,
-    className,
+  icon,
+  title,
+  description,
+  children,
+  isOpen,
+  loading,
+  loadingText,
+  confirmText,
+  cancelText,
+  footer,
+  hideFooter,
+  hideCancel,
+  hideConfirm,
+  onCancel,
+  onConfirm,
+  onClose,
+  className,
 }) => {
-    const cancelButtonRef = useRef(null);
+  const cancelButtonRef = useRef(null);
 
-    return (
-        <Transition.Root show={isOpen} as={Fragment}>
-            <HUDialog
-                as="div"
-                className="fixed z-10 inset-0 overflow-y-auto"
-                initialFocus={cancelButtonRef}
-                onClose={(_) => {
-                    onClose && onClose();
-                }}
+  return (
+    <Transition.Root show={isOpen} as={Fragment}>
+      <HUDialog
+        as="div"
+        className="fixed z-10 inset-0 overflow-y-auto"
+        initialFocus={cancelButtonRef}
+        onClose={(_) => {
+          onClose && onClose();
+        }}
+      >
+        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <HUDialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enterTo="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <div
+              className={`inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-2xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full ${className}`}
             >
-                <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-                    <Transition.Child
-                        as={Fragment}
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                    >
-                        <HUDialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-                    </Transition.Child>
-
-                    {/* This element is to trick the browser into centering the modal contents. */}
-                    <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
-                        &#8203;
-                    </span>
-                    <Transition.Child
-                        as={Fragment}
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                        enterTo="opacity-100 translate-y-0 sm:scale-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                        leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                    >
-                        <div
-                            className={`inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-2xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full ${className}`}
-                        >
-                            <div className="bg-white px-4 py-5 sm:py-8 sm:px-6 w-full">
-                                <div className="sm:flex sm:items-start">
-                                    {icon && (
-                                        <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                                            <Icon icon={icon} />
-                                        </div>
-                                    )}
-                                    <div className="text-center sm:mt-0 sm:text-left w-full">
-                                        {title && (
-                                            <HUDialog.Title
-                                                as="h2"
-                                                className="text-xl leading-6 font-medium text-primary"
-                                            >
-                                                {title}
-                                            </HUDialog.Title>
-                                        )}
-                                        {description && (
-                                            <div className="mt-2">
-                                                <p className="text-sm text-secondary">{description}</p>
-                                            </div>
-                                        )}
-                                        {Boolean(children) && children}
-                                    </div>
-                                </div>
-                            </div>
-                            {!footer && !hideFooter && (
-                                <div className="px-4 py-3 flex gap-2 sm:px-6 sm:flex-row-reverse">
-                                    {!hideConfirm && (
-                                        <button
-                                            disabled={loading}
-                                            type="button"
-                                            className="btn"
-                                            onClick={() => {
-                                                onConfirm && onConfirm();
-                                                onClose && onClose();
-                                            }}
-                                        >
-                                            {loading ? loadingText : confirmText || 'Confirm'}
-                                        </button>
-                                    )}
-                                    {!hideCancel && (
-                                        <button
-                                            type="button"
-                                            className="btn btn-secondary"
-                                            onClick={() => {
-                                                onCancel && onCancel();
-                                                onClose && onClose();
-                                            }}
-                                            ref={cancelButtonRef}
-                                        >
-                                            {cancelText || 'Cancel'}
-                                        </button>
-                                    )}
-                                </div>
-                            )}
-                            {Boolean(footer) && footer}
-                        </div>
-                    </Transition.Child>
+              <div className="bg-white px-4 py-5 sm:py-8 sm:px-6 w-full">
+                <div className="sm:flex sm:items-start">
+                  {icon && (
+                    <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <Icon icon={icon} />
+                    </div>
+                  )}
+                  <div className="text-center sm:mt-0 sm:text-left w-full">
+                    {title && (
+                      <HUDialog.Title
+                        as="h2"
+                        className="text-xl leading-6 font-medium text-primary"
+                      >
+                        {title}
+                      </HUDialog.Title>
+                    )}
+                    {description && (
+                      <div className="mt-2">
+                        <p className="text-sm text-secondary">{description}</p>
+                      </div>
+                    )}
+                    {Boolean(children) && children}
+                  </div>
                 </div>
-            </HUDialog>
-        </Transition.Root>
-    );
+              </div>
+              {!footer && !hideFooter && (
+                <div className="px-4 py-3 flex gap-2 sm:px-6 sm:flex-row-reverse">
+                  {!hideConfirm && (
+                    <button
+                      disabled={loading}
+                      type="button"
+                      className="btn"
+                      onClick={() => {
+                        onConfirm && onConfirm();
+                      }}
+                    >
+                      {loading ? loadingText : confirmText || 'Confirm'}
+                    </button>
+                  )}
+                  {!hideCancel && (
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      onClick={() => {
+                        onCancel && onCancel();
+                        onClose && onClose();
+                      }}
+                      ref={cancelButtonRef}
+                    >
+                      {cancelText || 'Cancel'}
+                    </button>
+                  )}
+                </div>
+              )}
+              {Boolean(footer) && footer}
+            </div>
+          </Transition.Child>
+        </div>
+      </HUDialog>
+    </Transition.Root>
+  );
 };
 
 export default Dialog;
--- web/vtadmin/src/proto/vtadmin.d.ts
+++ web/vtadmin/src/proto/vtadmin.d.ts
@@ -9879,77 +9879,6 @@ export namespace vtadmin {
         public toJSON(): { [k: string]: any };
     }
 
-<<<<<<< HEAD
-    /** Properties of a ValidateRequest. */
-    interface IValidateRequest {
-
-        /** ValidateRequest cluster_id */
-        cluster_id?: (string|null);
-
-        /** ValidateRequest ping_tablets */
-        ping_tablets?: (boolean|null);
-    }
-
-    /** Represents a ValidateRequest. */
-    class ValidateRequest implements IValidateRequest {
-
-        /**
-         * Constructs a new ValidateRequest.
-         * @param [properties] Properties to set
-         */
-        constructor(properties?: vtadmin.IValidateRequest);
-
-        /** ValidateRequest cluster_id. */
-        public cluster_id: string;
-
-        /** ValidateRequest ping_tablets. */
-        public ping_tablets: boolean;
-
-        /**
-         * Creates a new ValidateRequest instance using the specified properties.
-         * @param [properties] Properties to set
-         * @returns ValidateRequest instance
-         */
-        public static create(properties?: vtadmin.IValidateRequest): vtadmin.ValidateRequest;
-
-        /**
-         * Encodes the specified ValidateRequest message. Does not implicitly {@link vtadmin.ValidateRequest.verify|verify} messages.
-         * @param message ValidateRequest message or plain object to encode
-         * @param [writer] Writer to encode to
-         * @returns Writer
-         */
-        public static encode(message: vtadmin.IValidateRequest, writer?: $protobuf.Writer): $protobuf.Writer;
-
-        /**
-         * Encodes the specified ValidateRequest message, length delimited. Does not implicitly {@link vtadmin.ValidateRequest.verify|verify} messages.
-         * @param message ValidateRequest message or plain object to encode
-         * @param [writer] Writer to encode to
-         * @returns Writer
-         */
-        public static encodeDelimited(message: vtadmin.IValidateRequest, writer?: $protobuf.Writer): $protobuf.Writer;
-
-        /**
-         * Decodes a ValidateRequest message from the specified reader or buffer.
-         * @param reader Reader or buffer to decode from
-         * @param [length] Message length if known beforehand
-         * @returns ValidateRequest
-         * @throws {Error} If the payload is not a reader or valid buffer
-         * @throws {$protobuf.util.ProtocolError} If required fields are missing
-         */
-        public static decode(reader: ($protobuf.Reader|Uint8Array), length?: number): vtadmin.ValidateRequest;
-
-        /**
-         * Decodes a ValidateRequest message from the specified reader or buffer, length delimited.
-         * @param reader Reader or buffer to decode from
-         * @returns ValidateRequest
-         * @throws {Error} If the payload is not a reader or valid buffer
-         * @throws {$protobuf.util.ProtocolError} If required fields are missing
-         */
-        public static decodeDelimited(reader: ($protobuf.Reader|Uint8Array)): vtadmin.ValidateRequest;
-
-        /**
-         * Verifies a ValidateRequest message.
-=======
     /** Properties of a TabletExternallyReparentedRequest. */
     interface ITabletExternallyReparentedRequest {
 
@@ -10019,31 +9948,12 @@ export namespace vtadmin {
 
         /**
          * Verifies a TabletExternallyReparentedRequest message.
->>>>>>> 812a3efe81d6decc12bf641bd78bc8a18e5d5e88
          * @param message Plain object to verify
          * @returns `null` if valid, otherwise the reason why it is not
          */
         public static verify(message: { [k: string]: any }): (string|null);
 
         /**
-<<<<<<< HEAD
-         * Creates a ValidateRequest message from a plain object. Also converts values to their respective internal types.
-         * @param object Plain object
-         * @returns ValidateRequest
-         */
-        public static fromObject(object: { [k: string]: any }): vtadmin.ValidateRequest;
-
-        /**
-         * Creates a plain object from a ValidateRequest message. Also converts values to other types if specified.
-         * @param message ValidateRequest
-         * @param [options] Conversion options
-         * @returns Plain object
-         */
-        public static toObject(message: vtadmin.ValidateRequest, options?: $protobuf.IConversionOptions): { [k: string]: any };
-
-        /**
-         * Converts this ValidateRequest to JSON.
-=======
          * Creates a TabletExternallyReparentedRequest message from a plain object. Also converts values to their respective internal types.
          * @param object Plain object
          * @returns TabletExternallyReparentedRequest
@@ -10060,7 +9970,102 @@ export namespace vtadmin {
 
         /**
          * Converts this TabletExternallyReparentedRequest to JSON.
->>>>>>> 812a3efe81d6decc12bf641bd78bc8a18e5d5e88
+         * @returns JSON object
+         */
+        public toJSON(): { [k: string]: any };
+    }
+
+    /** Properties of a ValidateRequest. */
+    interface IValidateRequest {
+
+        /** ValidateRequest cluster_id */
+        cluster_id?: (string|null);
+
+        /** ValidateRequest ping_tablets */
+        ping_tablets?: (boolean|null);
+    }
+
+    /** Represents a ValidateRequest. */
+    class ValidateRequest implements IValidateRequest {
+
+        /**
+         * Constructs a new ValidateRequest.
+         * @param [properties] Properties to set
+         */
+        constructor(properties?: vtadmin.IValidateRequest);
+
+        /** ValidateRequest cluster_id. */
+        public cluster_id: string;
+
+        /** ValidateRequest ping_tablets. */
+        public ping_tablets: boolean;
+
+        /**
+         * Creates a new ValidateRequest instance using the specified properties.
+         * @param [properties] Properties to set
+         * @returns ValidateRequest instance
+         */
+        public static create(properties?: vtadmin.IValidateRequest): vtadmin.ValidateRequest;
+
+        /**
+         * Encodes the specified ValidateRequest message. Does not implicitly {@link vtadmin.ValidateRequest.verify|verify} messages.
+         * @param message ValidateRequest message or plain object to encode
+         * @param [writer] Writer to encode to
+         * @returns Writer
+         */
+        public static encode(message: vtadmin.IValidateRequest, writer?: $protobuf.Writer): $protobuf.Writer;
+
+        /**
+         * Encodes the specified ValidateRequest message, length delimited. Does not implicitly {@link vtadmin.ValidateRequest.verify|verify} messages.
+         * @param message ValidateRequest message or plain object to encode
+         * @param [writer] Writer to encode to
+         * @returns Writer
+         */
+        public static encodeDelimited(message: vtadmin.IValidateRequest, writer?: $protobuf.Writer): $protobuf.Writer;
+
+        /**
+         * Decodes a ValidateRequest message from the specified reader or buffer.
+         * @param reader Reader or buffer to decode from
+         * @param [length] Message length if known beforehand
+         * @returns ValidateRequest
+         * @throws {Error} If the payload is not a reader or valid buffer
+         * @throws {$protobuf.util.ProtocolError} If required fields are missing
+         */
+        public static decode(reader: ($protobuf.Reader|Uint8Array), length?: number): vtadmin.ValidateRequest;
+
+        /**
+         * Decodes a ValidateRequest message from the specified reader or buffer, length delimited.
+         * @param reader Reader or buffer to decode from
+         * @returns ValidateRequest
+         * @throws {Error} If the payload is not a reader or valid buffer
+         * @throws {$protobuf.util.ProtocolError} If required fields are missing
+         */
+        public static decodeDelimited(reader: ($protobuf.Reader|Uint8Array)): vtadmin.ValidateRequest;
+
+        /**
+         * Verifies a ValidateRequest message.
+         * @param message Plain object to verify
+         * @returns `null` if valid, otherwise the reason why it is not
+         */
+        public static verify(message: { [k: string]: any }): (string|null);
+
+        /**
+         * Creates a ValidateRequest message from a plain object. Also converts values to their respective internal types.
+         * @param object Plain object
+         * @returns ValidateRequest
+         */
+        public static fromObject(object: { [k: string]: any }): vtadmin.ValidateRequest;
+
+        /**
+         * Creates a plain object from a ValidateRequest message. Also converts values to other types if specified.
+         * @param message ValidateRequest
+         * @param [options] Conversion options
+         * @returns Plain object
+         */
+        public static toObject(message: vtadmin.ValidateRequest, options?: $protobuf.IConversionOptions): { [k: string]: any };
+
+        /**
+         * Converts this ValidateRequest to JSON.
          * @returns JSON object
          */
         public toJSON(): { [k: string]: any };
