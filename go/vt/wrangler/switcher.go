8f0f9abdd06791276ddfe14701aa1ab2fdaf027a
Matt Lord <mattalord@gmail.com>
2022-10-05 17:37:10 -0400

    For partial MoveTables, setup reverse shard routing rules on workflow creation (#11415)
    * Testing....
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Setup reverse routing rules at the start of the first partial movetables. Enhance e2e test to test for this as well as switch the remaining shard so that all shards are switched
    
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>
    
    * Update display representation of state for partially moved keyspaces
    
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>
    
    * Test improvements
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Cleanup shard routing rules on cancel/complete
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Cleanup intermediate changes
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Fix DryRun TrafficSwitcher and its tests
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Support concurrent partial movetables for N keyspaces
    
    And other minor changes after self review
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Avoid unnecessary save + rebuild steps
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Add secondary ordering for shard routing rules on to_keyspace
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Minor changes after self review
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>
    Co-authored-by: Rohit Nayak <rohit@planetscale.com>

--- go/vt/wrangler/switcher.go
+++ go/vt/wrangler/switcher.go
@@ -40,6 +40,10 @@ func (r *switcher) deleteRoutingRules(ctx context.Context) error {
 	return r.ts.deleteRoutingRules(ctx)
 }
 
+func (r *switcher) deleteShardRoutingRules(ctx context.Context) error {
+	return r.ts.deleteShardRoutingRules(ctx)
+}
+
 func (r *switcher) dropSourceDeniedTables(ctx context.Context) error {
 	return r.ts.dropSourceDeniedTables(ctx)
 }
