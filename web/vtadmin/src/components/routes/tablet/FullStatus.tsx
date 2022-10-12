7b9ba65c61a993d9ee5f93d5313ac1a82938fad5
Manan Gupta <manan@planetscale.com>
2022-10-12 14:19:58 +0530

    refactor: prettify the changed file
    Signed-off-by: Manan Gupta <manan@planetscale.com>

--- web/vtadmin/src/components/routes/tablet/FullStatus.tsx
+++ web/vtadmin/src/components/routes/tablet/FullStatus.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useGetFullStatus } from '../../../hooks/api';
-import {topodata, vtadmin} from '../../../proto/vtadmin';
+import { topodata, vtadmin } from '../../../proto/vtadmin';
 import { formatAlias } from '../../../util/tablets';
 import { Code } from '../../Code';
 import style from './Tablet.module.scss';
-import {isNil} from "lodash-es";
+import { isNil } from 'lodash-es';
 
 interface Props {
     tablet: vtadmin.Tablet;
 }
 
 function stateReplacer(key: any, val: any) {
-    if ( key === 'io_state' || key == 'sql_state' ) {
+    if (key === 'io_state' || key === 'sql_state') {
         if (val === 3) {
-            return "Replication Running"
+            return 'Replication Running';
         } else if (val === 2) {
-            return "Replication Connected"
+            return 'Replication Connected';
         } else if (val === 1) {
-            return "Replication Stopped"
+            return 'Replication Stopped';
         }
     }
     return val;
@@ -46,16 +46,16 @@ const FullStatus: React.FC<Props> = ({ tablet }) => {
     }
     if (!isNil(data) && !isNil(data.status)) {
         if (data.status.semi_sync_primary_enabled !== true) {
-            data.status.semi_sync_primary_enabled = false
+            data.status.semi_sync_primary_enabled = false;
         }
         if (data.status.semi_sync_replica_enabled !== true) {
-            data.status.semi_sync_replica_enabled = false
+            data.status.semi_sync_replica_enabled = false;
         }
         if (data.status.semi_sync_primary_status !== true) {
-            data.status.semi_sync_primary_status = false
+            data.status.semi_sync_primary_status = false;
         }
         if (data.status.semi_sync_replica_status !== true) {
-            data.status.semi_sync_replica_status = false
+            data.status.semi_sync_replica_status = false;
         }
     }
 
