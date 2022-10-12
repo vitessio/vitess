322e71531f805fd5f3e908546b3ae0172b44c51b
Manan Gupta <manan@planetscale.com>
2022-10-12 13:47:25 +0530

    feat: add replacer and code to always print semi-sync fields and decode the io and sql state from integers to strings
    Signed-off-by: Manan Gupta <manan@planetscale.com>

--- web/vtadmin/src/components/routes/tablet/FullStatus.tsx
+++ web/vtadmin/src/components/routes/tablet/FullStatus.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useGetFullStatus } from '../../../hooks/api';
-import { vtadmin } from '../../../proto/vtadmin';
+import {topodata, vtadmin} from '../../../proto/vtadmin';
 import { formatAlias } from '../../../util/tablets';
 import { Code } from '../../Code';
 import style from './Tablet.module.scss';
+import {isNil} from "lodash-es";
 
 interface Props {
     tablet: vtadmin.Tablet;
+}
+
+function stateReplacer(key: any, val: any) {
+    if ( key === 'io_state' || key == 'sql_state' ) {
+        if (val === 3) {
+            return "Replication Running"
+        } else if (val === 2) {
+            return "Replication Connected"
+        } else if (val === 1) {
+            return "Replication Stopped"
+        }
+    }
+    return val;
 }
 
 const FullStatus: React.FC<Props> = ({ tablet }) => {
@@ -30,8 +44,22 @@ const FullStatus: React.FC<Props> = ({ tablet }) => {
             </div>
         );
     }
+    if (!isNil(data) && !isNil(data.status)) {
+        if (data.status.semi_sync_primary_enabled !== true) {
+            data.status.semi_sync_primary_enabled = false
+        }
+        if (data.status.semi_sync_replica_enabled !== true) {
+            data.status.semi_sync_replica_enabled = false
+        }
+        if (data.status.semi_sync_primary_status !== true) {
+            data.status.semi_sync_primary_status = false
+        }
+        if (data.status.semi_sync_replica_status !== true) {
+            data.status.semi_sync_replica_status = false
+        }
+    }
 
-    return <Code code={JSON.stringify(data, null, 2)} />;
+    return <Code code={JSON.stringify(data, stateReplacer, 2)} />;
 };
 
 export default FullStatus;
