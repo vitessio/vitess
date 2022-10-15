ff3a41bcde2970f03ffb0eebb1873fe2a34062e2
Rameez Sajwani <rameezwazirali@hotmail.com>
2022-10-14 19:08:38 -0700

    removing unncessary flags across binaries (#11495)
    * removing unncessary flags across binaries
    
    Signed-off-by: Rameez Sajwani <rameezwazirali@hotmail.com>
    
    * code review feedback
    
    Signed-off-by: Rameez Sajwani <rameezwazirali@hotmail.com>
    
    * fixing blank space
    
    Signed-off-by: Rameez Sajwani <rameezwazirali@hotmail.com>
    
    Signed-off-by: Rameez Sajwani <rameezwazirali@hotmail.com>

--- go/vt/mysqlctl/compression.go
+++ go/vt/mysqlctl/compression.go
@@ -65,7 +65,7 @@ var (
 )
 
 func init() {
-	for _, cmd := range []string{"mysqlctl", "mysqlctld", "vtcombo", "vttablet", "vttestserver", "vtctld", "vtctldclient", "vtexplain"} {
+	for _, cmd := range []string{"mysqlctl", "mysqlctld", "vtcombo", "vttablet", "vttestserver", "vtctld", "vtctldclient"} {
 		servenv.OnParseFor(cmd, registerBackupCompressionFlags)
 	}
 }
