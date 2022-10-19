f68d92bbb7178220508729625bf23fdd6754d6e6
Jimmy Kim <jimmy.kim@sendbird.com>
2022-10-19 17:36:19 +0900

    pass maxLifetime to vttablet dbapool
    Signed-off-by: Jimmy Kim <jimmy.kim@sendbird.com>

--- go/vt/vttablet/tabletserver/connpool/pool.go
+++ go/vt/vttablet/tabletserver/connpool/pool.go
@@ -86,7 +86,7 @@ func NewPool(env tabletenv.Env, name string, cfg tabletenv.ConnPoolConfig) *Pool
 		idleTimeout:        idleTimeout,
 		maxLifetime:        maxLifetime,
 		waiterCap:          int64(cfg.MaxWaiters),
-		dbaPool:            dbconnpool.NewConnectionPool("", 1, idleTimeout, 0, 0),
+		dbaPool:            dbconnpool.NewConnectionPool("", 1, idleTimeout, maxLifetime, 0),
 	}
 	if name == "" {
 		return cp
