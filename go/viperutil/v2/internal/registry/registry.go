57366b0b53c7e472f14c309e44eb2f58c513f199
Andrew Mason <andrew@planetscale.com>
2022-11-10 15:31:41 -0500

    starting to work on doc comments
    Signed-off-by: Andrew Mason <andrew@planetscale.com>

--- go/viperutil/v2/internal/registry/registry.go
+++ go/viperutil/v2/internal/registry/registry.go
@@ -8,13 +8,23 @@ import (
 )
 
 var (
-	Static  = viper.New()
+	// Static is the registry for static config variables. These variables will
+	// never be affected by a Watch-ed config, and maintain their original
+	// values for the lifetime of the process.
+	Static = viper.New()
+	// Dynamic is the registry for dynamic config variables. If a config file is
+	// found by viper, it will be watched by a threadsafe wrapper around a
+	// second viper (see sync.Viper), and variables registered to it will pick
+	// up changes to that config file throughout the lifetime of the process.
 	Dynamic = sync.New()
 
 	_ Bindable = (*viper.Viper)(nil)
 	_ Bindable = (*sync.Viper)(nil)
 )
 
+// Bindable represents the methods needed to bind a value.Value to a given
+// registry. It exists primarly to allow us to treat a sync.Viper as a
+// viper.Viper for configuration registration purposes.
 type Bindable interface {
 	BindEnv(vars ...string) error
 	BindPFlag(key string, flag *pflag.Flag) error
--- go/viperutil/v2/internal/value/value.go
+++ go/viperutil/v2/internal/value/value.go
@@ -11,12 +11,20 @@ import (
 	"vitess.io/vitess/go/viperutil/v2/internal/sync"
 )
 
+// Registerable is the subset of the interface exposed by Values (which is
+// declared in the public viperutil package).
+//
+// We need a separate interface type because Go generics do not let you define
+// a function that takes Value[T] for many, different T's, which we want to do
+// for BindFlags.
 type Registerable interface {
 	Key() string
 	Registry() registry.Bindable
 	Flag(fs *pflag.FlagSet) (*pflag.Flag, error)
 }
 
+// Base is the base functionality shared by Static and Dynamic values. It
+// implements viperutil.Value.
 type Base[T any] struct {
 	KeyName    string
 	DefaultVal T
@@ -33,6 +41,8 @@ func (val *Base[T]) Key() string { return val.KeyName }
 func (val *Base[T]) Default() T  { return val.DefaultVal }
 func (val *Base[T]) Get() T      { return val.BoundGetFunc(val.Key()) }
 
+// ErrNoFlagDefined is returned when a Value has a FlagName set, but the given
+// FlagSet does not define a flag with that name.
 var ErrNoFlagDefined = errors.New("flag not defined")
 
 func (val *Base[T]) Flag(fs *pflag.FlagSet) (*pflag.Flag, error) {
