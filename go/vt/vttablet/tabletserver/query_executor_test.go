67cb5d1497e038e78cc9f62de56ce19ef070bca2
Andrew Mason <andrew@planetscale.com>
2022-11-21 14:03:29 -0500

    Fix some linter errors (#11773)
    Noticed these when resolving merge conflicts on a separate PR, so just
    fixing them quickly:
    
    ```
    go/mysql/fakesqldb/server.go:424:14: var-declaration: should omit type func() from declaration of var afterFn; it will be inferred from the right-hand side (revive)
            var afterFn func() = func() {}
                        ^
    go/test/endtoend/vtorc/utils/utils.go:39:2: blank-imports: a blank import should be only in a main or test package, or have a comment justifying it (revive)
            _ "vitess.io/vitess/go/vt/topo/consultopo"
            ^
    go/test/endtoend/vtorc/utils/utils.go:43:2: blank-imports: a blank import should be only in a main or test package, or have a comment justifying it (revive)
            _ "vitess.io/vitess/go/vt/topo/zk2topo"
            ^
    go/vt/vttablet/tabletserver/query_executor_test.go:1266:3: `query` is unused (structcheck)
                    query  string
                    ^
    go/vt/vttablet/tabletserver/query_executor_test.go:1267:3: `result` is unused (structcheck)
                    result *sqltypes.Result
                    ^
    ```
    
    Signed-off-by: Andrew Mason <andrew@planetscale.com>
    
    Signed-off-by: Andrew Mason <andrew@planetscale.com>

--- go/vt/vttablet/tabletserver/query_executor_test.go
+++ go/vt/vttablet/tabletserver/query_executor_test.go
@@ -1260,11 +1260,6 @@ func TestReplaceSchemaName(t *testing.T) {
 }
 
 func TestQueryExecutorShouldConsolidate(t *testing.T) {
-	type dbResponse struct {
-		query  string
-		result *sqltypes.Result
-	}
-
 	testcases := []struct {
 		consolidates  []bool
 		executorFlags executorFlags
