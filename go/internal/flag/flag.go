def9aec2f0906f605035d94a60a7cbb4a45e4c3a
Manan Gupta <manan@planetscale.com>
2022-12-02 15:06:58 +0530

    feat: add a new lock-timeout flag and use that instead of remote-operation-timeout
    Signed-off-by: Manan Gupta <manan@planetscale.com>

--- go/internal/flag/flag.go
+++ go/internal/flag/flag.go
@@ -70,6 +70,17 @@ func Parse(fs *flag.FlagSet) {
 	flag.Parse()
 }
 
+// IsFlagPassed returns if the given flag has been provided by the user explicitly or not
+func IsFlagPassed(name string) bool {
+	found := false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == name {
+			found = true
+		}
+	})
+	return found
+}
+
 // TrickGlog tricks glog into understanding that flags have been parsed.
 //
 // N.B. Do not delete this function. `glog` is a persnickity package and wants
