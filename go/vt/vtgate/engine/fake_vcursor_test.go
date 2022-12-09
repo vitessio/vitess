2fceb403b4fc7ce171077b2a309c8c767fd2c1d3
FlorentP <35779988+frouioui@users.noreply.github.com>
2022-12-09 19:25:46 +0100

    Revert "Better clone of the VCursor (#11926)" (#11931)
    This reverts commit 035d6b7124e4ff4a99aa87aa549cc82acc05044a.
    
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>
    
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- go/vt/vtgate/engine/fake_vcursor_test.go
+++ go/vt/vtgate/engine/fake_vcursor_test.go
@@ -51,7 +51,7 @@ type noopVCursor struct {
 }
 
 func (t *noopVCursor) SetTransactionIsolation(isolation querypb.ExecuteOptions_TransactionIsolation) {
-	// TODO implement me
+	//TODO implement me
 	panic("implement me")
 }
 
@@ -60,7 +60,7 @@ func (t *noopVCursor) InTransaction() bool {
 }
 
 func (t *noopVCursor) SetCommitOrder(co vtgatepb.CommitOrder) {
-	// TODO implement me
+	//TODO implement me
 	panic("implement me")
 }
 
@@ -317,10 +317,6 @@ func (t *noopVCursor) ResolveDestinationsMultiCol(ctx context.Context, keyspace 
 
 func (t *noopVCursor) GetDBDDLPluginName() string {
 	panic("unimplemented")
-}
-
-func (t *noopVCursor) DeepClone() VCursor {
-	return t
 }
 
 var _ VCursor = (*loggingVCursor)(nil)
@@ -713,10 +709,6 @@ func (f *loggingVCursor) CanUseSetVar() bool {
 		f.log = append(f.log, "SET_VAR can be used")
 	}
 	return useSetVar
-}
-
-func (f *loggingVCursor) DeepClone() VCursor {
-	return f
 }
 
 func (t *noopVCursor) VtExplainLogging() {}
