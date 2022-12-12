5e9d92eaec97ebf03cc1299f806b4199e138c840
Florent Poinsard <florent.poinsard@outlook.fr>
2022-12-12 10:05:33 +0100

    add missing error in planbuilder
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- go/vt/vtgate/planbuilder/throttler.go
+++ go/vt/vtgate/planbuilder/throttler.go
@@ -19,7 +19,6 @@ package planbuilder
 import (
 	"vitess.io/vitess/go/vt/key"
 	topodatapb "vitess.io/vitess/go/vt/proto/topodata"
-	vtrpcpb "vitess.io/vitess/go/vt/proto/vtrpc"
 	"vitess.io/vitess/go/vt/vterrors"
 	"vitess.io/vitess/go/vt/vtgate/engine"
 	"vitess.io/vitess/go/vt/vtgate/planbuilder/plancontext"
@@ -55,7 +54,7 @@ func buildShowThrottlerStatusPlan(query string, vschema plancontext.VSchema) (*p
 		return nil, err
 	}
 	if ks == nil {
-		return nil, vterrors.NewErrorf(vtrpcpb.Code_FAILED_PRECONDITION, vterrors.NoDB, "No database selected: use keyspace<:shard> or keyspace<[range]> (<> are optional)")
+		return nil, vterrors.VT09005()
 	}
 
 	if tabletType != topodatapb.TabletType_PRIMARY {
