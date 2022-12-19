6e2db660b7ee6abd7c92d603192f6e299042d88f
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2022-12-19 19:41:34 +0200

    indentation
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/sqlparser/utils_test.go
+++ go/vt/sqlparser/utils_test.go
@@ -58,86 +58,88 @@ func TestQueryMatchesTemplates(t *testing.T) {
 		q    string
 		tmpl []string
 		out  bool
-	}{{
-		q: "select id from tbl",
-		tmpl: []string{
-			"select id from tbl",
+	}{
+		{
+			q: "select id from tbl",
+			tmpl: []string{
+				"select id from tbl",
+			},
+			out: true,
+		}, {
+			q: "select id from tbl",
+			tmpl: []string{
+				"select name from tbl",
+				"select id from tbl",
+			},
+			out: true,
+		}, {
+			q: "select id from tbl where a=3",
+			tmpl: []string{
+				"select id from tbl",
+			},
+			out: false,
+		}, {
+			// int value
+			q: "select id from tbl where a=3",
+			tmpl: []string{
+				"select name from tbl where a=17",
+				"select id from tbl where a=5",
+			},
+			out: true,
+		}, {
+			// string value
+			q: "select id from tbl where a='abc'",
+			tmpl: []string{
+				"select name from tbl where a='x'",
+				"select id from tbl where a='y'",
+			},
+			out: true,
+		}, {
+			// two params
+			q: "select id from tbl where a='abc' and b='def'",
+			tmpl: []string{
+				"select name from tbl where a='x' and b = 'y'",
+				"select id from tbl where a='x' and b = 'y'",
+			},
+			out: true,
+		}, {
+			// no match
+			q: "select id from tbl where a='abc' and b='def'",
+			tmpl: []string{
+				"select name from tbl where a='x' and b = 'y'",
+				"select id from tbl where a='x' and c = 'y'",
+			},
+			out: false,
+		}, {
+			// reorder AND params
+			q: "select id from tbl where a='abc' and b='def'",
+			tmpl: []string{
+				"select id from tbl where b='x' and a = 'y'",
+			},
+			out: true,
+		}, {
+			// no reorder OR params
+			q: "select id from tbl where a='abc' or b='def'",
+			tmpl: []string{
+				"select id from tbl where b='x' or a = 'y'",
+			},
+			out: false,
+		}, {
+			// strict reorder OR params
+			q: "select id from tbl where a='abc' or b='def'",
+			tmpl: []string{
+				"select id from tbl where a='x' or b = 'y'",
+			},
+			out: true,
+		}, {
+			// reorder AND params, range test
+			q: "select id from tbl where a >'abc' and b<3",
+			tmpl: []string{
+				"select id from tbl where b<17 and a > 'y'",
+			},
+			out: true,
 		},
-		out: true,
-	}, {
-		q: "select id from tbl",
-		tmpl: []string{
-			"select name from tbl",
-			"select id from tbl",
-		},
-		out: true,
-	}, {
-		q: "select id from tbl where a=3",
-		tmpl: []string{
-			"select id from tbl",
-		},
-		out: false,
-	}, {
-		// int value
-		q: "select id from tbl where a=3",
-		tmpl: []string{
-			"select name from tbl where a=17",
-			"select id from tbl where a=5",
-		},
-		out: true,
-	}, {
-		// string value
-		q: "select id from tbl where a='abc'",
-		tmpl: []string{
-			"select name from tbl where a='x'",
-			"select id from tbl where a='y'",
-		},
-		out: true,
-	}, {
-		// two params
-		q: "select id from tbl where a='abc' and b='def'",
-		tmpl: []string{
-			"select name from tbl where a='x' and b = 'y'",
-			"select id from tbl where a='x' and b = 'y'",
-		},
-		out: true,
-	}, {
-		// no match
-		q: "select id from tbl where a='abc' and b='def'",
-		tmpl: []string{
-			"select name from tbl where a='x' and b = 'y'",
-			"select id from tbl where a='x' and c = 'y'",
-		},
-		out: false,
-	}, {
-		// reorder AND params
-		q: "select id from tbl where a='abc' and b='def'",
-		tmpl: []string{
-			"select id from tbl where b='x' and a = 'y'",
-		},
-		out: true,
-	}, {
-		// no reorder OR params
-		q: "select id from tbl where a='abc' or b='def'",
-		tmpl: []string{
-			"select id from tbl where b='x' or a = 'y'",
-		},
-		out: false,
-	}, {
-		// strict reorder OR params
-		q: "select id from tbl where a='abc' or b='def'",
-		tmpl: []string{
-			"select id from tbl where a='x' or b = 'y'",
-		},
-		out: true,
-	}, {
-		// reorder AND params, range test
-		q: "select id from tbl where a >'abc' and b<3",
-		tmpl: []string{
-			"select id from tbl where b<17 and a > 'y'",
-		},
-		out: true,
-	}}
+	}
 	for _, tc := range testcases {
 		match, err := QueryMatchesTemplates(tc.q, tc.tmpl)
 		assert.NoError(t, err)
