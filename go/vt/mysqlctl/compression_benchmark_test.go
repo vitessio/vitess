51d7a8fcbb675b5cefdfd3f883c49b052bfb3b0e
Max Englander <max@planetscale.com>
2022-12-27 15:30:47 -0500

    address pr comments: fetch realistic data to compress
    Signed-off-by: Max Englander <max@planetscale.com>

--- go/vt/mysqlctl/compression_benchmark_test.go
+++ go/vt/mysqlctl/compression_benchmark_test.go
@@ -1,10 +1,15 @@
 package mysqlctl
 
 import (
+	"compress/gzip"
 	"context"
+	"crypto/md5"
+	"errors"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
+	"path"
 	"strings"
 	"testing"
 
@@ -22,32 +27,24 @@ type (
 
 	benchmarkCompressEnv struct {
 		benchmarkCompressArgs
+		dataLocalPath string
+		dataURL       string
 	}
 )
 
 const (
-	usage = `
-This benchmark tests the performance of built-in and external compressors,
-using mysqlctl.newBuiltinDecompressor and mysqlctl.newExternalCompressor
-to obtain compressors.
+	// This is the default file which will be downloaded, gunzipped, and used
+	// by the compression benchmarks in this suite. It's a ~60GB gzipped InnoDB
+	// file, which was built from this Wikipedia dataset:
+	//
+	// https://dumps.wikimedia.org/enwiki/20221220/enwiki-20221220-externallinks.sql.gz
+	defaultDataURL = "https://archive.org/download/enwiki-20221220-externallinks.ibd/enwiki-20221220-externallinks.ibd.gz"
 
-You must supply a file to compress by setting this environment variable:
-
-    export VT_MYSQLCTL_COMPRESSION_BENCHMARK_DATA_PATH=path/to/data.xml
-
-We recommend using a data set that compresses well, such as:
-
- - The enwik9 dataset: http://mattmahoney.net/dc/textdata
- - A larger Wikipedia dataset from: https://en.wikipedia.org/wiki/Wikipedia:Database_download
-
-Once you've set that environment variable, you can run the tests like this:
-
-    go test -bench=BenchmarkCompressZstd ./go/vt/mysqlctl -run=NONE -timeout=2h -benchtime=300s
-
-Set -timeout and -benchtime to taste (or don't set them at all).
-`
-
-	userDefinedDataPathEnvVar = "VT_MYSQLCTL_COMPRESSION_BENCHMARK_DATA_PATH"
+	// Users may specify an alternate gzipped URL. This option is intended
+	// purely for development and debugging purposes. For example:
+	//
+	//     export VT_MYSQLCTL_COMPRESSION_BENCHMARK_DATA_URL=https://wiki.mozilla.org/images/f/ff/Example.json.gz
+	userDefinedDataURLEnvVar = "VT_MYSQLCTL_COMPRESSION_BENCHMARK_DATA_URL"
 )
 
 func setupBenchmarkCompressEnv(args benchmarkCompressArgs) benchmarkCompressEnv {
@@ -64,18 +61,18 @@ func (bce *benchmarkCompressEnv) benchmark() {
 
 	for i := 0; i < bce.b.N; i++ {
 		logger := logutil.NewMemoryLogger()
-
 		compressor := bce.compressor(logger)
-		defer compressor.Close()
-
-		reader := bce.reader()
-		defer reader.Close()
+		reader, err := bce.reader()
+		require.Nil(bce.b, err, "Failed to get data reader.")
 
 		// Only time the part we care about.
 		bce.b.StartTimer()
-		_, err := io.Copy(compressor, reader)
+		_, err = io.Copy(compressor, reader)
 		bce.b.StopTimer()
 
+		// Don't defer closing, otherwise we can exhaust open file limit.
+		reader.Close()
+		compressor.Close()
 		require.Nil(bce.b, err, logger.Events)
 	}
 }
@@ -94,11 +91,55 @@ func (bce *benchmarkCompressEnv) compressor(logger logutil.Logger) io.WriteClose
 	return compressor
 }
 
-func (bce *benchmarkCompressEnv) reader() io.ReadCloser {
-	dataPath := os.Getenv(userDefinedDataPathEnvVar)
-	f, err := os.Open(dataPath)
-	require.Nil(bce.b, err, "Failed to open data path: %s", dataPath)
-	return f
+func (bce *benchmarkCompressEnv) reader() (io.ReadCloser, error) {
+	url := defaultDataURL
+
+	// Use user-defined URL, if specified.
+	if udURL := os.Getenv(userDefinedDataURLEnvVar); udURL != "" {
+		url = udURL
+	}
+
+	// Compute a local path for a file by hashing the URL.
+	localPath := path.Join(os.TempDir(), fmt.Sprintf("%x.dat", md5.Sum([]byte(url))))
+
+	if _, err := os.Stat(localPath); errors.Is(err, os.ErrNotExist) {
+		bce.b.Logf("File not found at local path: %s\n", localPath)
+
+		// If the local path does not exist, download the file from the URL.
+		httpClient := http.Client{
+			CheckRedirect: func(r *http.Request, via []*http.Request) error {
+				r.URL.Opaque = r.URL.Path
+				return nil
+			},
+		}
+
+		resp, err := httpClient.Get(url)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get data at URL %q: %v", url, err)
+		}
+		defer resp.Body.Close()
+
+		// Assume the data we're downloading is gzipped.
+		gzr, err := gzip.NewReader(resp.Body)
+		if err != nil {
+			return nil, fmt.Errorf("failed to gunzip data at URL %q: %v", url, err)
+		}
+		defer gzr.Close()
+
+		// Create a local file to write the HTTP response to.
+		file, err := os.OpenFile(localPath, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+		if err != nil {
+			return nil, err
+		}
+		defer file.Close()
+
+		// Write the gunzipped  HTTP response to local path.
+		if _, err := io.Copy(file, gzr); err != nil {
+			return nil, err
+		}
+	}
+
+	return os.Open(localPath)
 }
 
 func (bce *benchmarkCompressEnv) validate() {
@@ -113,15 +154,6 @@ func (bce *benchmarkCompressEnv) validate() {
 
 	if bce.builtin == "" && bce.external == "" {
 		require.Fail(bce.b, "Either builtin or external compressor must be specified.")
-	}
-
-	dataPath := os.Getenv(userDefinedDataPathEnvVar)
-	if dataPath == "" {
-		require.Fail(bce.b, fmt.Sprintf(
-			"Required environment variable %q is not set.\n\nUSAGE:\n%s",
-			userDefinedDataPathEnvVar,
-			usage,
-		))
 	}
 }
 
