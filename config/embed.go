cf81c136b55f8f1fc11c5be0f82dda5378391a40
Matt Lord <mattalord@gmail.com>
2023-01-04 12:31:25 -0500

    VReplication: Test Migrations From MariaDB to MySQL (#12036)
    * Add MariaDB Migration e2e Test
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Consolidate MariaDB Version Configs
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Minor changes after self review
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Additional MariaDB cleanup from unit test generation
    
    Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>
    
    * Small additional cleanup
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Use latest MariaDB LTS release as default guess from ENV
    
    https://mariadb.com/kb/en/changes-improvements-in-mariadb-106/
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>
    Co-authored-by: Dirkjan Bussink <d.bussink@gmail.com>

--- config/embed.go
+++ config/embed.go
@@ -8,20 +8,8 @@ var DefaultInitDB string
 //go:embed mycnf/default.cnf
 var MycnfDefault string
 
-//go:embed mycnf/mariadb100.cnf
-var MycnfMariaDB100 string
-
-//go:embed mycnf/mariadb101.cnf
-var MycnfMariaDB101 string
-
-//go:embed mycnf/mariadb102.cnf
-var MycnfMariaDB102 string
-
-//go:embed mycnf/mariadb103.cnf
-var MycnfMariaDB103 string
-
-//go:embed mycnf/mariadb104.cnf
-var MycnfMariaDB104 string
+//go:embed mycnf/mariadb10.cnf
+var MycnfMariaDB10 string
 
 //go:embed mycnf/mysql57.cnf
 var MycnfMySQL57 string
