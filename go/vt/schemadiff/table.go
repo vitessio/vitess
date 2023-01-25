bfef5325922a0873d945b5ffcefcd191596e8097
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2023-01-25 10:24:30 +0200

    schemadiff: TableCharsetCollateStrategy (#12137)
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
    
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/schemadiff/table.go
+++ go/vt/schemadiff/table.go
@@ -863,6 +863,12 @@ func (c *CreateTableEntity) diffOptions(alterTable *sqlparser.AlterTable,
 				// skip
 			case "AVG_ROW_LENGTH":
 				// skip. MyISAM only, not interesting
+			case "CHARSET":
+				switch hints.TableCharsetCollateStrategy {
+				case TableCharsetCollateStrict:
+					tableOption = &sqlparser.TableOption{String: ""}
+					// in all other strategies we ignore the charset
+				}
 			case "CHECKSUM":
 				tableOption = &sqlparser.TableOption{Value: sqlparser.NewIntLiteral("0")}
 			case "COLLATE":
@@ -930,6 +936,18 @@ func (c *CreateTableEntity) diffOptions(alterTable *sqlparser.AlterTable,
 				// options are different.
 				// However, we don't automatically apply these changes. It depends on the option!
 				switch strings.ToUpper(t1Option.Name) {
+				case "CHARSET", "COLLATE":
+					switch hints.TableCharsetCollateStrategy {
+					case TableCharsetCollateStrict:
+						alterTableOptions = append(alterTableOptions, t2Option)
+					case TableCharsetCollateIgnoreEmpty:
+						if t1Option.String != "" && t2Option.String != "" {
+							alterTableOptions = append(alterTableOptions, t2Option)
+						}
+						// if one is empty, we ignore
+					case TableCharsetCollateIgnoreAlways:
+						// ignore always
+					}
 				case "AUTO_INCREMENT":
 					switch hints.AutoIncrementStrategy {
 					case AutoIncrementApplyAlways:
@@ -961,6 +979,12 @@ func (c *CreateTableEntity) diffOptions(alterTable *sqlparser.AlterTable,
 	for _, t2Option := range t2Options {
 		if _, ok := t1OptionsMap[t2Option.Name]; !ok {
 			switch strings.ToUpper(t2Option.Name) {
+			case "CHARSET", "COLLATE":
+				switch hints.TableCharsetCollateStrategy {
+				case TableCharsetCollateStrict:
+					alterTableOptions = append(alterTableOptions, t2Option)
+					// in all other strategies we ignore the charset
+				}
 			case "AUTO_INCREMENT":
 				switch hints.AutoIncrementStrategy {
 				case AutoIncrementApplyAlways, AutoIncrementApplyHigher:
