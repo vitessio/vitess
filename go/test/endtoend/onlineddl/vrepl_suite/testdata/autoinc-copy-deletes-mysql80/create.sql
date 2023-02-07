0e8693a7feb87cc932aee853582898dd6a6c726e
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2023-02-07 14:24:16 +0200

    onlineddl_vrepl suite: fix auto_increment flakyness (#12246)
    * onlineddl_vrepl suite: fix auto_increment flakyness
    
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
    
    * delete all rows before events begin
    
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/test/endtoend/onlineddl/vrepl_suite/testdata/autoinc-copy-deletes-mysql80/create.sql
+++ go/test/endtoend/onlineddl/vrepl_suite/testdata/autoinc-copy-deletes-mysql80/create.sql
@@ -5,7 +5,7 @@ create table onlineddl_test (
   id int auto_increment,
   i int not null,
   primary key(id)
-) auto_increment=1;
+) auto_increment=8;
 
 insert into onlineddl_test values (NULL, 11);
 insert into onlineddl_test values (NULL, 13);
@@ -13,5 +13,18 @@ insert into onlineddl_test values (NULL, 17);
 insert into onlineddl_test values (NULL, 23);
 insert into onlineddl_test values (NULL, 29);
 insert into onlineddl_test values (NULL, 31);
-insert into onlineddl_test values (NULL, 37);
-delete from onlineddl_test where id>=5;
+delete from onlineddl_test where id>=0;
+
+drop event if exists onlineddl_test;
+delimiter ;;
+create event onlineddl_test
+  on schedule every 1 second
+  starts current_timestamp
+  ends current_timestamp + interval 60 second
+  on completion not preserve
+  enable
+  do
+begin
+  insert into onlineddl_test values (NULL, 37);
+  delete from onlineddl_test where id>=0;
+end ;;
