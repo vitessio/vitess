03f81b3fdb5c2826e6ee20dc0feaa32df5648407
Dirkjan Bussink <d.bussink@gmail.com>
2023-02-10 17:22:20 +0100

    Move to independent sets package (#12251)
    This moves to a separate sets package inside Vitess so we don't break
    version pinning for k8s like in the Vitess operator.
    
    It mostly copies the existing implementation, but simplifies and cleans
    it up to only copy what we actually use.
    
    Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

--- go/vt/topo/topoproto/tablet.go
+++ go/vt/topo/topoproto/tablet.go
@@ -28,9 +28,8 @@ import (
 
 	"google.golang.org/protobuf/proto"
 
-	"k8s.io/apimachinery/pkg/util/sets"
-
 	"vitess.io/vitess/go/netutil"
+	"vitess.io/vitess/go/sets"
 	"vitess.io/vitess/go/vt/vterrors"
 
 	topodatapb "vitess.io/vitess/go/vt/proto/topodata"
