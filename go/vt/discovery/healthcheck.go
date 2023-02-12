e42b71d467131c4c4465f6d4f3dc35c61c72a709
jun <johnwang.wangjun@gmail.com>
2023-02-12 04:10:15 +0000

    update 2 more tables with id
    Signed-off-by: jun <johnwang.wangjun@gmail.com>

--- go/vt/discovery/healthcheck.go
+++ go/vt/discovery/healthcheck.go
@@ -113,7 +113,7 @@ const (
     padding: 0.2rem;
   }
 </style>
-<table id="HealthCheckCache">
+<table id="HealthCheckTabletCache">
   <tr>
     <th colspan="5">HealthCheck Tablet Cache</th>
   </tr>
