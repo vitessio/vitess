de58a4e462914ee3a1b09ddf891f28c0b55831ce
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2023-02-21 17:26:19 +0200

    update test to match new UnwrapAll() behavior
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/mysql/server_flaky_test.go
+++ go/mysql/server_flaky_test.go
@@ -1270,7 +1270,7 @@ func TestErrorCodes(t *testing.T) {
 			text:     "resource exhausted",
 		},
 		{
-			err:      vterrors.Wrap(vterrors.Errorf(vtrpcpb.Code_ABORTED, "Row count exceeded 10000"), "wrapped"),
+			err:      vterrors.Errorf(vtrpcpb.Code_ABORTED, "Row count exceeded 10000"),
 			code:     ERQueryInterrupted,
 			sqlState: SSQueryInterrupted,
 			text:     "aborted",
