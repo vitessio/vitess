9b74634585f49c15ab8e7f7606769b41ec78fef2
Harshit Gangal <harshit@planetscale.com>
2023-02-23 16:46:23 +0530

    GetSchema rpc to streaming api
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/vt/vttablet/endtoend/framework/client.go
+++ go/vt/vttablet/endtoend/framework/client.go
@@ -410,5 +410,13 @@ func (client *QueryClient) UpdateContext(ctx context.Context) {
 }
 
 func (client *QueryClient) GetSchema(tableType querypb.SchemaTableType, tableNames ...string) (map[string]string, error) {
-	return client.server.GetSchema(client.ctx, client.target, tableType, tableNames)
+	schemaDef := map[string]string{}
+	err := client.server.GetSchema(client.ctx, client.target, tableType, tableNames, func(schemaRes *querypb.GetSchemaResponse) error {
+		schemaDef = schemaRes.TableDefinition
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return schemaDef, nil
 }
