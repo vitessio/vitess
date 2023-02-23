e429768d1b78fad9ffa3414f6ab618b4065c53d5
Rohit Nayak <57520317+rohit-nayak-ps@users.noreply.github.com>
2023-02-23 13:11:24 +0100

    Sidecar schema init: use COPY algorithm while altering sidecardb tables (#12436)
    * schemadiff: AlterTableAlgorithmStratgy
    
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>
    
    * minor rename
    
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>
    
    * Use COPY algorithm while altering sidecardb tables
    
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>
    
    ---------
    
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>
    Co-authored-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/sidecardb/sidecardb.go
+++ go/vt/sidecardb/sidecardb.go
@@ -306,6 +306,7 @@ func (si *schemaInit) getCurrentSchema(tableName string) (string, error) {
 func (si *schemaInit) findTableSchemaDiff(tableName, current, desired string) (string, error) {
 	hints := &schemadiff.DiffHints{
 		TableCharsetCollateStrategy: schemadiff.TableCharsetCollateIgnoreAlways,
+		AlterTableAlgorithmStrategy: schemadiff.AlterTableAlgorithmStrategyCopy,
 	}
 	diff, err := schemadiff.DiffCreateTablesQueries(current, desired, hints)
 	if err != nil {
