58d9a94d65939b952bd676337d4ed53980bf01e5
Dirkjan Bussink <d.bussink@gmail.com>
2023-02-23 13:50:50 +0100

    Add 128 bit hashing for the evalengine (#12428)
    * evalengine: 128-bit hashes
    
    This adds support for 128 bit hashes. With 128 bit hashes, we have
    enough entropy so we can remove the need to compare values after
    hashing.
    
    Signed-off-by: Vicent Marti <vmg@strn.cat>
    Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>
    
    * evalengine: Add support for 128 bit hashing
    
    This adds a null safe hashing function for 128 bit hashing and tests it.
    There's no usage of this yet, but that will be extended in the future.
    
    Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>
    
    * Address review feedback
    
    Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>
    
    * Ensure to always initialize hasher
    
    Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>
    
    ---------
    
    Signed-off-by: Vicent Marti <vmg@strn.cat>
    Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>
    Co-authored-by: Vicent Martí <42793+vmg@users.noreply.github.com>

--- go/vt/vtgate/evalengine/internal/decimal/helpers.go
+++ go/vt/vtgate/evalengine/internal/decimal/helpers.go
@@ -1,7 +1,5 @@
-//go:build 386 || arm || mips || mipsle || wasm
-
 /*
-Copyright 2021 The Vitess Authors.
+Copyright 2023 The Vitess Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -16,17 +14,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package collations
+package decimal
 
-// 4-byte constants for hashing in 32-bit systems
-const (
-	m1 = 0x78bd642f
-	m2 = 0xa0b428db
-	m3 = 0x9c88c6e3
-	m4 = 0x75374cc3
-	m5 = 0xc47d124f
-)
+import "vitess.io/vitess/go/vt/vthash"
 
-// Generate all the metadata used for collations from the JSON data dumped from MySQL
-// In 32 bit systems, do not use memory embedding, it's not worth it
-//go:generate go run ./tools/makecolldata/ --embed=false
+func (d *Decimal) Hash(hasher *vthash.Hasher) {
+	_, _ = hasher.Write(d.formatFast(0, false, true))
+}
