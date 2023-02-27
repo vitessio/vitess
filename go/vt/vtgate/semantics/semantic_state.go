8aad3871310ffcc11b2cf82a9a124fbf72f892fa
Manan Gupta <35839558+GuptaManan100@users.noreply.github.com>
2023-02-27 21:23:06 +0530

    BugFix: Unsharded query using a derived table and a dual table (#12484) (#12493)
    * test: add failing test for a query in an unsharded keyspace using a derived table and reference table
    
    
    
    * feat: fix logic to check if a single unsharded keyspace is used
    
    
    
    * feat: fix test output
    
    
    
    ---------
    
    Signed-off-by: Manan Gupta <manan@planetscale.com>

--- go/vt/vtgate/semantics/semantic_state.go
+++ go/vt/vtgate/semantics/semantic_state.go
@@ -370,11 +370,19 @@ func (st *SemTable) SingleUnshardedKeyspace() *vindexes.Keyspace {
 	var ks *vindexes.Keyspace
 	for _, table := range st.Tables {
 		vindexTable := table.GetVindexTable()
-		if vindexTable == nil || vindexTable.Type != "" {
+
+		if vindexTable == nil {
 			_, isDT := table.getExpr().Expr.(*sqlparser.DerivedTable)
 			if isDT {
 				// derived tables are ok, as long as all real tables are from the same unsharded keyspace
 				// we check the real tables inside the derived table as well for same unsharded keyspace.
+				continue
+			}
+			return nil
+		}
+		if vindexTable.Type != "" {
+			// A reference table is not an issue when seeing if a query is going to an unsharded keyspace
+			if vindexTable.Type == vindexes.TypeReference {
 				continue
 			}
 			return nil
