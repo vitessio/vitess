618a1a5fa7f4c0488ad8a1c9c91b737eb939b0ae
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2023-03-08 10:35:29 +0200

    refactor: renaming
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/vtgate/semantics/errors.go
+++ go/vt/vtgate/semantics/errors.go
@@ -39,11 +39,11 @@ const (
 func printf(e SemanticsError, msg string, args ...any) string {
 	format := msg
 
-	if e.Classify().id != "" {
-		format = fmt.Sprintf("%s: %s", e.Classify().id, format)
+	if e.Info().id != "" {
+		format = fmt.Sprintf("%s: %s", e.Info().id, format)
 	}
 
-	switch e.Classify().typ {
+	switch e.Info().typ {
 	case UnsupportedErrorType:
 		format = "VT12001: unsupported: " + format
 	case BugErrorType:
@@ -52,19 +52,21 @@ func printf(e SemanticsError, msg string, args ...any) string {
 	return fmt.Sprintf(format, args...)
 }
 
+// SemanticsError should be implemented by all errors in this package that arise from a semantic problem
 type SemanticsError interface {
 	Error() string
-	Classify() *SemanticsErrorClassification
+	Info() *SemanticsErrorInfo
 }
 
-type SemanticsErrorClassification struct {
+// SemanticsErrorInfo provides additional information about a semantic error
+type SemanticsErrorInfo struct {
 	code  vtrpcpb.Code
 	state vterrors.State
 	typ   ErrType
 	id    string
 }
 
-func (c *SemanticsErrorClassification) ErrorCode() vtrpcpb.Code {
+func (c *SemanticsErrorInfo) ErrorCode() vtrpcpb.Code {
 	if c.code == UndefinedErrorCode {
 		return vtrpcpb.Code_UNKNOWN
 	}
@@ -79,17 +81,19 @@ func (c *SemanticsErrorClassification) ErrorCode() vtrpcpb.Code {
 	}
 }
 
-func (c *SemanticsErrorClassification) State() vterrors.State {
+func (c *SemanticsErrorInfo) State() vterrors.State {
 	return c.state
 }
 
-func (c *SemanticsErrorClassification) ErrorType() ErrType {
+func (c *SemanticsErrorInfo) ErrorType() ErrType {
 	return c.typ
 }
 
-func (c *SemanticsErrorClassification) Id() string {
+func (c *SemanticsErrorInfo) Id() string {
 	return c.id
 }
+
+// Specific error implementations follow
 
 // UnionColumnsDoNotMatchError
 type UnionColumnsDoNotMatchError struct {
@@ -101,8 +105,8 @@ func (e *UnionColumnsDoNotMatchError) Error() string {
 	return printf(e, "The used SELECT statements have a different number of columns: %v, %v", e.FirstProj, e.SecondProj)
 }
 
-func (e *UnionColumnsDoNotMatchError) Classify() *SemanticsErrorClassification {
-	return &SemanticsErrorClassification{code: vtrpcpb.Code_FAILED_PRECONDITION, state: vterrors.WrongNumberOfColumnsInSelect}
+func (e *UnionColumnsDoNotMatchError) Info() *SemanticsErrorInfo {
+	return &SemanticsErrorInfo{code: vtrpcpb.Code_FAILED_PRECONDITION, state: vterrors.WrongNumberOfColumnsInSelect}
 }
 
 // TODO: untested
@@ -121,8 +125,8 @@ func (e *UnsupportedMultiTablesInUpdateError) Error() string {
 	}
 }
 
-func (e *UnsupportedMultiTablesInUpdateError) Classify() *SemanticsErrorClassification {
-	return &SemanticsErrorClassification{typ: UnsupportedErrorType}
+func (e *UnsupportedMultiTablesInUpdateError) Info() *SemanticsErrorInfo {
+	return &SemanticsErrorInfo{typ: UnsupportedErrorType}
 }
 
 // UnsupportedNaturalJoinError
@@ -134,8 +138,8 @@ func (e *UnsupportedNaturalJoinError) Error() string {
 	return printf(e, "", e.Join)
 }
 
-func (e *UnsupportedNaturalJoinError) Classify() *SemanticsErrorClassification {
-	return &SemanticsErrorClassification{typ: UnsupportedErrorType}
+func (e *UnsupportedNaturalJoinError) Info() *SemanticsErrorInfo {
+	return &SemanticsErrorInfo{typ: UnsupportedErrorType}
 }
 
 // UnionWithSQLCalcFoundRowsError
@@ -146,8 +150,8 @@ func (e *UnionWithSQLCalcFoundRowsError) Error() string {
 	return printf(e, "SQL_CALC_FOUND_ROWS not supported with union")
 }
 
-func (e *UnionWithSQLCalcFoundRowsError) Classify() *SemanticsErrorClassification {
-	return &SemanticsErrorClassification{typ: UnsupportedErrorType}
+func (e *UnionWithSQLCalcFoundRowsError) Info() *SemanticsErrorInfo {
+	return &SemanticsErrorInfo{typ: UnsupportedErrorType}
 }
 
 // TODO: untested
@@ -160,8 +164,8 @@ func (e *TableNotUpdatableError) Error() string {
 	return printf(e, "The target table %s of the UPDATE is not updatable", e.Table)
 }
 
-func (e *TableNotUpdatableError) Classify() *SemanticsErrorClassification {
-	return &SemanticsErrorClassification{state: vterrors.NonUpdateableTable, code: vtrpcpb.Code_INVALID_ARGUMENT}
+func (e *TableNotUpdatableError) Info() *SemanticsErrorInfo {
+	return &SemanticsErrorInfo{state: vterrors.NonUpdateableTable, code: vtrpcpb.Code_INVALID_ARGUMENT}
 }
 
 // SQLCalcFoundRowsUsageError
@@ -172,8 +176,8 @@ func (e *SQLCalcFoundRowsUsageError) Error() string {
 	return printf(e, "Incorrect usage/placement of 'SQL_CALC_FOUND_ROWS'")
 }
 
-func (e *SQLCalcFoundRowsUsageError) Classify() *SemanticsErrorClassification {
-	return &SemanticsErrorClassification{code: vtrpcpb.Code_INVALID_ARGUMENT}
+func (e *SQLCalcFoundRowsUsageError) Info() *SemanticsErrorInfo {
+	return &SemanticsErrorInfo{code: vtrpcpb.Code_INVALID_ARGUMENT}
 }
 
 // TODO: untested
@@ -186,8 +190,8 @@ func (e *CantUseOptionHereError) Error() string {
 	return printf(e, "Incorrect usage/placement of '%s'", e.Msg)
 }
 
-func (e *CantUseOptionHereError) Classify() *SemanticsErrorClassification {
-	return &SemanticsErrorClassification{state: vterrors.CantUseOptionHere, code: vtrpcpb.Code_INVALID_ARGUMENT}
+func (e *CantUseOptionHereError) Info() *SemanticsErrorInfo {
+	return &SemanticsErrorInfo{state: vterrors.CantUseOptionHere, code: vtrpcpb.Code_INVALID_ARGUMENT}
 }
 
 // TODO: untested
@@ -199,8 +203,8 @@ func (e *MissingInVSchemaError) Error() string {
 	return printf(e, "Table information is not provided in vschema")
 }
 
-func (e *MissingInVSchemaError) Classify() *SemanticsErrorClassification {
-	return &SemanticsErrorClassification{code: vtrpcpb.Code_INVALID_ARGUMENT}
+func (e *MissingInVSchemaError) Info() *SemanticsErrorInfo {
+	return &SemanticsErrorInfo{code: vtrpcpb.Code_INVALID_ARGUMENT}
 }
 
 // TODO: untested
@@ -213,8 +217,8 @@ func (e *NotSequenceTableError) Error() string {
 	return printf(e, "NEXT used on a non-sequence table %s", e.Table)
 }
 
-func (e *NotSequenceTableError) Classify() *SemanticsErrorClassification {
-	return &SemanticsErrorClassification{code: vtrpcpb.Code_INVALID_ARGUMENT}
+func (e *NotSequenceTableError) Info() *SemanticsErrorInfo {
+	return &SemanticsErrorInfo{code: vtrpcpb.Code_INVALID_ARGUMENT}
 }
 
 // TODO: untested
@@ -227,8 +231,8 @@ func (e *NextWithMultipleTablesError) Error() string {
 	return printf(e, "Next statement should not contain multiple tables: found %d tables", e.CountTables)
 }
 
-func (e *NextWithMultipleTablesError) Classify() *SemanticsErrorClassification {
-	return &SemanticsErrorClassification{typ: BugErrorType}
+func (e *NextWithMultipleTablesError) Info() *SemanticsErrorInfo {
+	return &SemanticsErrorInfo{typ: BugErrorType}
 }
 
 // LockOnlyWithDualError
@@ -240,8 +244,8 @@ func (e *LockOnlyWithDualError) Error() string {
 	return printf(e, "%v allowed only with dual", sqlparser.String(e.Node))
 }
 
-func (e *LockOnlyWithDualError) Classify() *SemanticsErrorClassification {
-	return &SemanticsErrorClassification{code: vtrpcpb.Code_UNIMPLEMENTED}
+func (e *LockOnlyWithDualError) Info() *SemanticsErrorInfo {
+	return &SemanticsErrorInfo{code: vtrpcpb.Code_UNIMPLEMENTED}
 }
 
 // QualifiedOrderInUnionError
@@ -253,8 +257,8 @@ func (e *QualifiedOrderInUnionError) Error() string {
 	return printf(e, "Table %s from one of the SELECTs cannot be used in global ORDER clause", e.Table)
 }
 
-func (e *QualifiedOrderInUnionError) Classify() *SemanticsErrorClassification {
-	return &SemanticsErrorClassification{}
+func (e *QualifiedOrderInUnionError) Info() *SemanticsErrorInfo {
+	return &SemanticsErrorInfo{}
 }
 
 // JSONTablesError
@@ -266,8 +270,8 @@ func (e *JSONTablesError) Error() string {
 	return printf(e, "json_table expressions")
 }
 
-func (e *JSONTablesError) Classify() *SemanticsErrorClassification {
-	return &SemanticsErrorClassification{typ: UnsupportedErrorType}
+func (e *JSONTablesError) Info() *SemanticsErrorInfo {
+	return &SemanticsErrorInfo{typ: UnsupportedErrorType}
 }
 
 // TODO: untested
@@ -280,8 +284,8 @@ func (e *BuggyError) Error() string {
 	return printf(e, e.Msg)
 }
 
-func (e *BuggyError) Classify() *SemanticsErrorClassification {
-	return &SemanticsErrorClassification{typ: BugErrorType}
+func (e *BuggyError) Info() *SemanticsErrorInfo {
+	return &SemanticsErrorInfo{typ: BugErrorType}
 }
 
 // ColumnNotFoundError
@@ -293,8 +297,8 @@ func (e *ColumnNotFoundError) Error() string {
 	return printf(e, "symbol %s not found", e.Column)
 }
 
-func (e *ColumnNotFoundError) Classify() *SemanticsErrorClassification {
-	return &SemanticsErrorClassification{state: vterrors.BadFieldError, code: vtrpcpb.Code_INVALID_ARGUMENT}
+func (e *ColumnNotFoundError) Info() *SemanticsErrorInfo {
+	return &SemanticsErrorInfo{state: vterrors.BadFieldError, code: vtrpcpb.Code_INVALID_ARGUMENT}
 }
 
 // AmbiguousColumnError
@@ -306,6 +310,6 @@ func (e *AmbiguousColumnError) Error() string {
 	return printf(e, "Column '%s' in field list is ambiguous", e.Column)
 }
 
-func (e *AmbiguousColumnError) Classify() *SemanticsErrorClassification {
-	return &SemanticsErrorClassification{state: vterrors.BadFieldError, code: vtrpcpb.Code_INVALID_ARGUMENT}
+func (e *AmbiguousColumnError) Info() *SemanticsErrorInfo {
+	return &SemanticsErrorInfo{state: vterrors.BadFieldError, code: vtrpcpb.Code_INVALID_ARGUMENT}
 }
