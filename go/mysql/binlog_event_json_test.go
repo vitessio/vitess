2b43fd7dfb804b421f71705eedab7c043675b648
Rohit Nayak <57520317+rohit-nayak-ps@users.noreply.github.com>
2023-03-16 17:41:19 +0100

    VStreamer: improve representation of integers in json data types (#12630)
    * Add signed and unsigned integer types to forked ason library to fix issue of integers being parsed as float64 by the source binlog parser. This results in larger integers being stored as floats on the target and sent with scientific notation in vstream events.
    
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>
    
    * Use go.mod reference to the ajson module to make it easier to switch to the upstream module once changes are made there
    
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>
    
    ---------
    
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>

--- go/mysql/binlog_event_json_test.go
+++ go/mysql/binlog_event_json_test.go
@@ -18,50 +18,65 @@ package mysql
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestJSONTypes(t *testing.T) {
+	// most of these test cases have been taken from open source java/python adapters
+	// like https://github.com/shyiko/mysql-binlog-connector-java/pull/119/files
 	testcases := []struct {
+		name     string
 		data     []byte
 		expected string
 		isMap    bool
 	}{{
+		name:     "null",
 		data:     []byte{},
 		expected: `null`,
 	}, {
+		name:     "map, string value",
 		data:     []byte{0, 1, 0, 14, 0, 11, 0, 1, 0, 12, 12, 0, 97, 1, 98},
 		expected: `{"a":"b"}`,
 	}, {
+		name:     "map, int value",
 		data:     []byte{0, 1, 0, 12, 0, 11, 0, 1, 0, 5, 2, 0, 97},
 		expected: `{"a":2}`,
 	}, {
+		name:     "map, object value",
 		data:     []byte{0, 1, 0, 29, 0, 11, 0, 4, 0, 0, 15, 0, 97, 115, 100, 102, 1, 0, 14, 0, 11, 0, 3, 0, 5, 123, 0, 102, 111, 111},
 		expected: `{"asdf":{"foo":123}}`,
 	}, {
+		name:     "list of ints",
 		data:     []byte{2, 2, 0, 10, 0, 5, 1, 0, 5, 2, 0},
 		expected: `[1,2]`,
 	}, {
+		name:     "list of maps",
 		data:     []byte{0, 4, 0, 60, 0, 32, 0, 1, 0, 33, 0, 1, 0, 34, 0, 2, 0, 36, 0, 2, 0, 12, 38, 0, 12, 40, 0, 12, 42, 0, 2, 46, 0, 97, 99, 97, 98, 98, 99, 1, 98, 1, 100, 3, 97, 98, 99, 2, 0, 14, 0, 12, 10, 0, 12, 12, 0, 1, 120, 1, 121},
 		expected: `{"a":"b","c":"d","ab":"abc","bc":["x","y"]}`,
 		isMap:    true,
 	}, {
+		name:     "list with one string",
 		data:     []byte{2, 1, 0, 37, 0, 12, 8, 0, 0, 4, 104, 101, 114, 101},
 		expected: `["here"]`,
 	}, {
+		name:     "list varied",
 		data:     []byte{2, 3, 0, 37, 0, 12, 13, 0, 2, 18, 0, 12, 33, 0, 4, 104, 101, 114, 101, 2, 0, 15, 0, 12, 10, 0, 12, 12, 0, 1, 73, 2, 97, 109, 3, 33, 33, 33},
 		expected: `["here",["I","am"],"!!!"]`,
 	}, {
+		name:     "string",
 		data:     []byte{12, 13, 115, 99, 97, 108, 97, 114, 32, 115, 116, 114, 105, 110, 103},
 		expected: `"scalar string"`,
 	}, {
+		name:     "map, long string value",
 		data:     []byte{0, 1, 0, 149, 0, 11, 0, 6, 0, 12, 17, 0, 115, 99, 111, 112, 101, 115, 130, 1, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 66, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 66, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 69, 65, 65, 65, 65, 65, 65, 69, 65, 65, 65, 65, 65, 65, 56, 65, 65, 65, 66, 103, 65, 65, 65, 65, 65, 65, 66, 65, 65, 65, 65, 67, 65, 65, 65, 65, 65, 65, 65, 65, 65, 84, 216, 142, 184},
 		expected: `{"scopes":"AAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAEAAAAAAEAAAAAA8AAABgAAAAAABAAAACAAAAAAAAA"}`,
 	}, {
 		// repeat the same string 10 times, to test the case where length of string
 		// requires 2 bytes to store
+		name: "long string",
 		data: []byte{12, 130, 1,
 			115, 99, 97, 108, 97, 114, 32, 115, 116, 114, 105, 110, 103,
 			115, 99, 97, 108, 97, 114, 32, 115, 116, 114, 105, 110, 103,
@@ -75,89 +90,139 @@ func TestJSONTypes(t *testing.T) {
 			115, 99, 97, 108, 97, 114, 32, 115, 116, 114, 105, 110, 103},
 		expected: `"scalar stringscalar stringscalar stringscalar stringscalar stringscalar stringscalar stringscalar stringscalar stringscalar string"`,
 	}, {
+		name:     "bool true",
 		data:     []byte{4, 1},
 		expected: `true`,
 	}, {
+		name:     "bool false",
 		data:     []byte{4, 2},
 		expected: `false`,
 	}, {
+		name:     "bool null",
 		data:     []byte{4, 0},
 		expected: `null`,
 	}, {
+		name:     "uint16 max",
+		data:     []byte{6, 255, 255},
+		expected: `65535`,
+	}, {
+		name:     "uint32 max",
+		data:     []byte{8, 255, 255, 255, 255},
+		expected: `4294967295`,
+	}, {
+		name:     "uint64 max",
+		data:     []byte{10, 255, 255, 255, 255, 255, 255, 255, 255},
+		expected: `18446744073709551615`,
+	}, {
+		name:     "int16 -1",
 		data:     []byte{5, 255, 255},
 		expected: `-1`,
 	}, {
-		data:     []byte{6, 1, 0},
-		expected: `1`,
+		name:     "int32 -1",
+		data:     []byte{7, 255, 255, 255, 255},
+		expected: `-1`,
 	}, {
+		name:     "int64 -1",
+		data:     []byte{9, 255, 255, 255, 255, 255, 255, 255, 255},
+		expected: `-1`,
+	}, {
+		name:     "int16 max",
 		data:     []byte{5, 255, 127},
 		expected: `32767`,
 	}, {
+		name:     "int32 max",
+		data:     []byte{7, 255, 255, 255, 127},
+		expected: `2147483647`,
+	}, {
+		name:     "int64 max",
+		data:     []byte{9, 255, 255, 255, 255, 255, 255, 255, 127},
+		expected: `9223372036854775807`,
+	}, {
+		name:     "uint16/1",
+		data:     []byte{6, 1, 0},
+		expected: `1`,
+	}, {
+		name:     "int32/32768",
 		data:     []byte{7, 0, 128, 0, 0},
 		expected: `32768`,
 	}, {
+		name:     "int16/neg",
 		data:     []byte{5, 0, 128},
 		expected: `-32768`,
 	}, {
+		name:     "int32/neg",
 		data:     []byte{7, 255, 127, 255, 255},
 		expected: `-32769`,
 	}, {
-		data:     []byte{7, 255, 255, 255, 127},
-		expected: `2.147483647e+09`,
-	}, {
+		name:     "uint32",
 		data:     []byte{8, 0, 128, 0, 0},
 		expected: `32768`,
 	}, {
+		name:     "int64",
 		data:     []byte{9, 0, 0, 0, 128, 0, 0, 0, 0},
-		expected: `2.147483648e+09`,
+		expected: `2147483648`,
 	}, {
+		name:     "int32/neg",
 		data:     []byte{7, 0, 0, 0, 128},
-		expected: `-2.147483648e+09`,
+		expected: `-2147483648`,
 	}, {
+		name:     "int64/neg",
 		data:     []byte{9, 255, 255, 255, 127, 255, 255, 255, 255},
-		expected: `-2.147483649e+09`,
+		expected: `-2147483649`,
 	}, {
+		name:     "uint64",
 		data:     []byte{10, 255, 255, 255, 255, 255, 255, 255, 255},
-		expected: `1.8446744073709552e+19`,
+		expected: `18446744073709551615`,
 	}, {
+		name:     "int64/neg",
 		data:     []byte{9, 0, 0, 0, 0, 0, 0, 0, 128},
-		expected: `-9.223372036854776e+18`,
+		expected: `-9223372036854775808`,
 	}, {
+		name:     "double",
 		data:     []byte{11, 110, 134, 27, 240, 249, 33, 9, 64},
 		expected: `3.14159`,
 	}, {
+		name:     "empty map",
 		data:     []byte{0, 0, 0, 4, 0},
 		expected: `{}`,
 	}, {
+		name:     "empty list",
 		data:     []byte{2, 0, 0, 4, 0},
 		expected: `[]`,
 	}, {
 		// opaque, datetime
+		name:     "datetime",
 		data:     []byte{15, 12, 8, 0, 0, 0, 25, 118, 31, 149, 25},
 		expected: `"2015-01-15 23:24:25.000000"`,
 	}, {
 		// opaque, time
+		name:     "time",
 		data:     []byte{15, 11, 8, 0, 0, 0, 25, 118, 1, 0, 0},
 		expected: `"23:24:25.000000"`,
 	}, {
 		// opaque, time
+		name:     "time2",
 		data:     []byte{15, 11, 8, 192, 212, 1, 25, 118, 1, 0, 0},
 		expected: `"23:24:25.120000"`,
 	}, {
 		// opaque, date
+		name:     "date",
 		data:     []byte{15, 10, 8, 0, 0, 0, 0, 0, 30, 149, 25},
 		expected: `"2015-01-15"`,
 	}, {
 		// opaque, decimal
+		name:     "decimal",
 		data:     []byte{15, 246, 8, 13, 4, 135, 91, 205, 21, 4, 210},
 		expected: `1.234567891234e+08`,
 	}, {
 		// opaque, bit field. Not yet implemented.
+		name:     "bitfield: unimplemented",
 		data:     []byte{15, 16, 2, 202, 254},
 		expected: `opaque type 16 is not supported yet, data [2 202 254]`,
 	}}
 	for _, tc := range testcases {
-		t.Run(tc.expected, func(t *testing.T) {
+		name := fmt.Sprintf("%s (%s)", tc.name, tc.expected)
+		t.Run(name, func(t *testing.T) {
 			val, err := getJSONValue(tc.data)
 			if err != nil {
 				require.Equal(t, tc.expected, err.Error())
