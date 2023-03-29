39f83b9cb40d1911dec1f91c2408ba96475f5cfa
Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
2023-03-29 11:47:46 +0200

    Fix transaction throttler ignoring the initial rate (#12618)
    * Fix transaction throttler ignoring the initial rate
    
    This addresses the issue reported in https://github.com/vitessio/vitess/issues/12549
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
    
    * Add missing override of max replication lag in `throttler.newThrottler()`
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
    
    * Reorder functions to make diff easier to read
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
    
    * Fix check for maxRate in `newThrottlerFromConfig()`
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
    
    * Fix some CI pipeline issues
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
    
    * Address PR comment.
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
    
    * Fix typo
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
    Signed-off-by: Eduardo J. Ortega U. <5791035+ejortegau@users.noreply.github.com>

--- go/vt/vttablet/tabletserver/txthrottler/tx_throttler.go
+++ go/vt/vttablet/tabletserver/txthrottler/tx_throttler.go
@@ -191,7 +191,7 @@ type txThrottlerState struct {
 // in tests to generate mocks.
 type healthCheckFactoryFunc func(topoServer *topo.Server, cell string, cellsToWatch []string) discovery.HealthCheck
 type topologyWatcherFactoryFunc func(topoServer *topo.Server, hc discovery.HealthCheck, cell, keyspace, shard string, refreshInterval time.Duration, topoReadConcurrency int) TopologyWatcherInterface
-type throttlerFactoryFunc func(name, unit string, threadCount int, maxRate, maxReplicationLag int64) (ThrottlerInterface, error)
+type throttlerFactoryFunc func(name, unit string, threadCount int, maxRate int64, maxReplicationLagConfig throttler.MaxReplicationLagModuleConfig) (ThrottlerInterface, error)
 
 var (
 	healthCheckFactory     healthCheckFactoryFunc
@@ -210,8 +210,8 @@ func resetTxThrottlerFactories() {
 	topologyWatcherFactory = func(topoServer *topo.Server, hc discovery.HealthCheck, cell, keyspace, shard string, refreshInterval time.Duration, topoReadConcurrency int) TopologyWatcherInterface {
 		return discovery.NewCellTabletsWatcher(context.Background(), topoServer, hc, discovery.NewFilterByKeyspace([]string{keyspace}), cell, refreshInterval, true, topoReadConcurrency)
 	}
-	throttlerFactory = func(name, unit string, threadCount int, maxRate, maxReplicationLag int64) (ThrottlerInterface, error) {
-		return throttler.NewThrottler(name, unit, threadCount, maxRate, maxReplicationLag)
+	throttlerFactory = func(name, unit string, threadCount int, maxRate int64, maxReplicationLagConfig throttler.MaxReplicationLagModuleConfig) (ThrottlerInterface, error) {
+		return throttler.NewThrottlerFromConfig(name, unit, threadCount, maxRate, maxReplicationLagConfig, time.Now)
 	}
 }
 
@@ -285,12 +285,15 @@ func (t *TxThrottler) Throttle() (result bool) {
 }
 
 func newTxThrottlerState(config *txThrottlerConfig, keyspace, shard, cell string) (*txThrottlerState, error) {
+	maxReplicationLagModuleConfig := throttler.MaxReplicationLagModuleConfig{Configuration: config.throttlerConfig}
+
 	t, err := throttlerFactory(
 		TxThrottlerName,
 		"TPS",                           /* unit */
 		1,                               /* threadCount */
 		throttler.MaxRateModuleDisabled, /* maxRate */
-		config.throttlerConfig.MaxReplicationLagSec /* maxReplicationLag */)
+		maxReplicationLagModuleConfig,
+	)
 	if err != nil {
 		return nil, err
 	}
