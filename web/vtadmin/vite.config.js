c6571f72ac9dce0fdd5dd17646b4386dcb1338f4
notfelineit <notfelineit@gmail.com>
2023-03-29 14:05:44 -0700

    Add typescript vite support
    Signed-off-by: notfelineit <notfelineit@gmail.com>

---
+++ web/vtadmin/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: 'build',
+    },
+    plugins: [react()],
+  };
+});
