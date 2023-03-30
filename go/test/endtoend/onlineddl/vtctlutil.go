1a0c3fe777948e5d14e9c5144701547da4115f30
Matt Lord <mattalord@gmail.com>
2023-03-30 13:53:07 -0400

    Throttler: Expose Tablet's Config & Leverage to Deflake Tests (#12737)
    * Flakes: effectively disable vtorc for deterministic behavior
    
    For example, we stop replication, wait a few seconds, then expect
    there to be lag. But vtorc could repair replication during that
    wait and then the lag is gone.
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Wait for the throttler to be up and running everywhere
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Expose tablet's throttler config and leverage to deflake tests
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Apply various corrections
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Be more explicit about VTOrc behavior changes
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Note received throttler response when it is unexpected
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Fixes from local testing
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Nits from self review
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Use assert.Equalf on failed assertions
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Ummm, duh.
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Try to get rid of last bit of flakiness
    
    Which seemed to revolve around NOT sleeping long enough
    after starting all the sleep queries.
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Nits from self review
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Address review comments
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Adjust test for behavior and comment it
    
    And adjust timing
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Align both stale hearbeat checks
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Remove no longer needed flag
    
    This is because enabling heartbeats with --heartbeat_enable
    also results in the replication reporter being enabled:
    https://github.com/vitessio/vitess/blob/3d9ef871e42bd20a60ec95997c97ecf0694c1e78/go/vt/vttablet/tabletserver/tabletenv/config.go#L235-L237
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Correct comment
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Correct comment part II: electric boogaloo
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Revert one other minor unnecessary change.
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    ---------
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/test/endtoend/onlineddl/vtctlutil.go
+++ go/test/endtoend/onlineddl/vtctlutil.go
@@ -17,8 +17,6 @@ limitations under the License.
 package onlineddl
 
 import (
-	"context"
-	"fmt"
 	"testing"
 	"time"
 
@@ -35,52 +33,4 @@ func CheckCancelAllMigrationsViaVtctl(t *testing.T, vtctlclient *cluster.VtctlCl
 
 	_, err := vtctlclient.ApplySchemaWithOutput(keyspace, cancelQuery, cluster.VtctlClientParams{})
 	assert.NoError(t, err)
-}
-
-// UpdateThrottlerTopoConfig runs vtctlclient UpdateThrottlerConfig.
-// This retries the command until it succeeds or times out as the
-// SrvKeyspace record may not yet exist for a newly created
-// Keyspace that is still initializing before it becomes serving.
-func UpdateThrottlerTopoConfig(clusterInstance *cluster.LocalProcessCluster, enable bool, disable bool, threshold float64, metricsQuery string, viaVtctldClient bool) (result string, err error) {
-	args := []string{}
-	clientfunc := clusterInstance.VtctldClientProcess.ExecuteCommandWithOutput
-	if !viaVtctldClient {
-		args = append(args, "--")
-		clientfunc = clusterInstance.VtctlclientProcess.ExecuteCommandWithOutput
-	}
-	args = append(args, "UpdateThrottlerConfig")
-	if enable {
-		args = append(args, "--enable")
-	}
-	if disable {
-		args = append(args, "--disable")
-	}
-	if threshold > 0 {
-		args = append(args, "--threshold", fmt.Sprintf("%f", threshold))
-	}
-	if metricsQuery != "" {
-		args = append(args, "--custom-query", metricsQuery)
-		args = append(args, "--check-as-check-self")
-	} else {
-		args = append(args, "--check-as-check-shard")
-	}
-	args = append(args, clusterInstance.Keyspaces[0].Name)
-
-	ctx, cancel := context.WithTimeout(context.Background(), throttlerConfigTimeout)
-	defer cancel()
-
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
-
-	for {
-		result, err = clientfunc(args...)
-		if err == nil {
-			return result, nil
-		}
-		select {
-		case <-ctx.Done():
-			return "", fmt.Errorf("timed out waiting for UpdateThrottlerConfig to succeed after %v. Last seen value: %+v, error: %v", throttlerConfigTimeout, result, err)
-		case <-ticker.C:
-		}
-	}
 }
