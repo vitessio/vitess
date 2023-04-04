194968df5eecaa2d19052e64c84e9c746a8b623d
Rameez Sajwani <rameezwazirali@hotmail.com>
2023-04-04 12:50:08 -0700

    adding test
    Signed-off-by: Rameez Sajwani <rameezwazirali@hotmail.com>

--- go/vt/mysqlctl/builtinbackupengine_test.go
+++ go/vt/mysqlctl/builtinbackupengine_test.go
@@ -38,8 +38,6 @@ import (
 	"vitess.io/vitess/go/vt/proto/vttime"
 	"vitess.io/vitess/go/vt/topo"
 	"vitess.io/vitess/go/vt/topo/memorytopo"
-	"vitess.io/vitess/go/vt/vttablet/faketmclient"
-	"vitess.io/vitess/go/vt/vttablet/tmclient"
 )
 
 func setBuiltinBackupMysqldDeadline(t time.Duration) time.Duration {
@@ -113,12 +111,6 @@ func TestExecuteBackup(t *testing.T) {
 	})
 	require.NoError(t, err)
 
-	// Set up tm client
-	// Note that using faketmclient.NewFakeTabletManagerClient will cause infinite recursion :shrug:
-	tmclient.RegisterTabletManagerClientFactory("grpc",
-		func() tmclient.TabletManagerClient { return &faketmclient.FakeTabletManagerClient{} },
-	)
-
 	be := &mysqlctl.BuiltinBackupEngine{}
 
 	// Configure a tight deadline to force a timeout
@@ -171,9 +163,8 @@ func TestExecuteBackup(t *testing.T) {
 	assert.False(t, ok)
 }
 
-// TestExecuteBackupWithCancelledContext test the ability of backup to gracefully
-// handle cases where we encounter error due to any reasons for e.g context cancel etc.
-// Process should not panic in these situations.
+// TestExecuteBackupWithCancelledContext tests the ability of the backup function to gracefully handle cases where errors
+// occur due to various reasons, such as context time cancel. The process should not panic in these situations.
 func TestExecuteBackupWithCancelledContext(t *testing.T) {
 	// Set up local backup directory
 	id := fmt.Sprintf("%d", time.Now().UnixNano())
@@ -189,7 +180,7 @@ func TestExecuteBackupWithCancelledContext(t *testing.T) {
 	require.NoError(t, createBackupFiles(path.Join(dataDir, "test2"), 2, "ibd"))
 	defer os.RemoveAll(backupRoot)
 
-	// cancel the context deliberately
+	// Cancel the context deliberately
 	ctx, cancel := context.WithCancel(context.Background())
 	cancel()
 	needIt, err := needInnoDBRedoLogSubdir()
@@ -225,12 +216,6 @@ func TestExecuteBackupWithCancelledContext(t *testing.T) {
 	})
 	require.NoError(t, err)
 
-	// Set up tm client.
-	// Note that using faketmclient.NewFakeTabletManagerClient will cause infinite recursion :shrug:
-	tmclient.RegisterTabletManagerClientFactory("grpc2",
-		func() tmclient.TabletManagerClient { return &faketmclient.FakeTabletManagerClient{} },
-	)
-
 	be := &mysqlctl.BuiltinBackupEngine{}
 	bh := filebackupstorage.NewBackupHandle(nil, "", "", false)
 	// Spin up a fake daemon to be used in backups. It needs to be allowed to receive:
@@ -258,6 +243,135 @@ func TestExecuteBackupWithCancelledContext(t *testing.T) {
 	// all four files will fail
 	require.ErrorContains(t, err, "context canceled;context canceled;context canceled;context canceled")
 	assert.False(t, ok)
+}
+
+// TestExecuteRestoreWithCancelledContext tests the ability of the restore function to gracefully handle cases where errors
+// occur due to various reasons, such as context time-outs. The process should not panic in these situations.
+func TestExecuteRestoreWithTimedOutContext(t *testing.T) {
+	// Set up local backup directory
+	id := fmt.Sprintf("%d", time.Now().UnixNano())
+	backupRoot := fmt.Sprintf("testdata/builtinbackup_test_%s", id)
+	filebackupstorage.FileBackupStorageRoot = backupRoot
+	require.NoError(t, createBackupDir(backupRoot, "innodb", "log", "datadir"))
+	dataDir := path.Join(backupRoot, "datadir")
+	// Add some files under data directory to force backup to execute semaphore acquire inside
+	// backupFiles() method (https://github.com/vitessio/vitess/blob/main/go/vt/mysqlctl/builtinbackupengine.go#L483).
+	require.NoError(t, createBackupDir(dataDir, "test1"))
+	require.NoError(t, createBackupDir(dataDir, "test2"))
+	require.NoError(t, createBackupFiles(path.Join(dataDir, "test1"), 2, "ibd"))
+	require.NoError(t, createBackupFiles(path.Join(dataDir, "test2"), 2, "ibd"))
+	defer os.RemoveAll(backupRoot)
+
+	ctx := context.Background()
+	needIt, err := needInnoDBRedoLogSubdir()
+	require.NoError(t, err)
+	if needIt {
+		fpath := path.Join("log", mysql.DynamicRedoLogSubdir)
+		if err := createBackupDir(backupRoot, fpath); err != nil {
+			require.Failf(t, err.Error(), "failed to create directory: %s", fpath)
+		}
+	}
+
+	// Set up topo
+	keyspace, shard := "mykeyspace", "-80"
+	ts := memorytopo.NewServer("cell1")
+	defer ts.Close()
+
+	require.NoError(t, ts.CreateKeyspace(ctx, keyspace, &topodata.Keyspace{}))
+	require.NoError(t, ts.CreateShard(ctx, keyspace, shard))
+
+	tablet := topo.NewTablet(100, "cell1", "mykeyspace-00-80-0100")
+	tablet.Keyspace = keyspace
+	tablet.Shard = shard
+
+	require.NoError(t, ts.CreateTablet(ctx, tablet))
+
+	_, err = ts.UpdateShardFields(ctx, keyspace, shard, func(si *topo.ShardInfo) error {
+		si.PrimaryAlias = &topodata.TabletAlias{Uid: 100, Cell: "cell1"}
+
+		now := time.Now()
+		si.PrimaryTermStartTime = &vttime.Time{Seconds: int64(now.Second()), Nanoseconds: int32(now.Nanosecond())}
+
+		return nil
+	})
+	require.NoError(t, err)
+
+	be := &mysqlctl.BuiltinBackupEngine{}
+	bh := filebackupstorage.NewBackupHandle(nil, "", "", false)
+	// Spin up a fake daemon to be used in backups. It needs to be allowed to receive:
+	// "STOP SLAVE", "START SLAVE", in that order.
+	mysqld := mysqlctl.NewFakeMysqlDaemon(fakesqldb.New(t))
+	mysqld.ExpectedExecuteSuperQueryList = []string{"STOP SLAVE", "START SLAVE"}
+
+	ok, err := be.ExecuteBackup(ctx, mysqlctl.BackupParams{
+		Logger: logutil.NewConsoleLogger(),
+		Mysqld: mysqld,
+		Cnf: &mysqlctl.Mycnf{
+			InnodbDataHomeDir:     path.Join(backupRoot, "innodb"),
+			InnodbLogGroupHomeDir: path.Join(backupRoot, "log"),
+			DataDir:               path.Join(backupRoot, "datadir"),
+		},
+		Stats:        backupstats.NewFakeStats(),
+		Concurrency:  2,
+		HookExtraEnv: map[string]string{},
+		TopoServer:   ts,
+		Keyspace:     keyspace,
+		Shard:        shard,
+	}, bh)
+
+	require.NoError(t, err)
+	assert.True(t, ok)
+
+	// Now try to restore the above backup.
+	bh = filebackupstorage.NewBackupHandle(nil, "", "", true)
+	mysqld = mysqlctl.NewFakeMysqlDaemon(fakesqldb.New(t))
+	mysqld.ExpectedExecuteSuperQueryList = []string{"STOP SLAVE", "START SLAVE"}
+	restoreParams := mysqlctl.RestoreParams{
+		Cnf: &mysqlctl.Mycnf{
+			InnodbDataHomeDir:     path.Join(backupRoot, "innodb"),
+			InnodbLogGroupHomeDir: path.Join(backupRoot, "log"),
+			DataDir:               path.Join(backupRoot, "datadir"),
+			BinLogPath:            path.Join(backupRoot, "binlog"),
+			RelayLogPath:          path.Join(backupRoot, "relaylog"),
+			RelayLogIndexPath:     path.Join(backupRoot, "relaylogindex"),
+			RelayLogInfoPath:      path.Join(backupRoot, "relayloginfo"),
+		},
+		Logger:              logutil.NewConsoleLogger(),
+		Mysqld:              mysqld,
+		Concurrency:         2,
+		HookExtraEnv:        map[string]string{},
+		DeleteBeforeRestore: false,
+		DbName:              "test",
+		Keyspace:            "test",
+		Shard:               "-",
+		StartTime:           time.Now(),
+		RestoreToPos:        mysql.Position{},
+		DryRun:              false,
+		Stats:               backupstats.NewFakeStats(),
+	}
+
+	// Successful restore.
+	bm, err := be.ExecuteRestore(ctx, restoreParams, bh)
+	assert.NoError(t, err)
+	assert.True(t, ok)
+	assert.NotNil(t, bm)
+
+	// Restore using timed-out context
+	mysqld = mysqlctl.NewFakeMysqlDaemon(fakesqldb.New(t))
+	mysqld.ExpectedExecuteSuperQueryList = []string{"STOP SLAVE", "START SLAVE"}
+	restoreParams.Mysqld = mysqld
+	timedOutCtx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
+	// Let the context timed out.
+	time.Sleep(1 * time.Second)
+	bm, err = be.ExecuteRestore(timedOutCtx, restoreParams, bh)
+	// ExecuteRestore should fail but since we are passing context.backgroun() in builtinbackupengine.go
+	// @executeRestoreFullBackup (https://github.com/vitessio/vitess/blob/main/go/vt/mysqlctl/builtinbackupengine.go#L806),
+	// therefore, ExecuteRestore will still pass. Once fixed (https://github.com/vitessio/vitess/issues/12830)
+	// the below asserts will start failing.
+	assert.NoError(t, err)
+	assert.True(t, ok)
+	assert.NotNil(t, bm)
 }
 
 // needInnoDBRedoLogSubdir indicates whether we need to create a redo log subdirectory.
