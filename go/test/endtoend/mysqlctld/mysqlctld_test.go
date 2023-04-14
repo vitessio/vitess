30c36eb6f24affc00138d140c77391bc79e351eb
Rameez Sajwani <rameezwazirali@hotmail.com>
2023-04-14 12:25:56 -0700

    test: fix flaky test
    Signed-off-by: Manan Gupta <manan@planetscale.com>

--- go/test/endtoend/mysqlctld/mysqlctld_test.go
+++ go/test/endtoend/mysqlctld/mysqlctld_test.go
@@ -133,6 +133,7 @@ func TestRestart(t *testing.T) {
 	defer cluster.PanicHandler(t)
 	err := primaryTablet.MysqlctldProcess.Stop()
 	require.Nil(t, err)
+	require.Truef(t, primaryTablet.MysqlctldProcess.WaitForMysqlCtldShutdown(), "Mysqlctld has not stopped...")
 	primaryTablet.MysqlctldProcess.CleanupFiles(primaryTablet.TabletUID)
 	err = primaryTablet.MysqlctldProcess.Start()
 	require.Nil(t, err)
