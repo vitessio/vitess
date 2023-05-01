4d750ea28091ec79f83409aeef2143c300c2e0c7
Dirkjan Bussink <d.bussink@gmail.com>
2023-05-01 10:40:38 +0200

    mysqlctl: Remove unused schema version
    We were only every writing to this, but never reading it anywhere. This
    means it's unused and we can deprecate and remove this.
    
    Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

--- web/vtadmin/src/proto/vtadmin.d.ts
+++ web/vtadmin/src/proto/vtadmin.d.ts
@@ -16290,9 +16290,6 @@ export namespace tabletmanagerdata {
 
         /** SchemaDefinition table_definitions */
         table_definitions?: (tabletmanagerdata.ITableDefinition[]|null);
-
-        /** SchemaDefinition version */
-        version?: (string|null);
     }
 
     /** Represents a SchemaDefinition. */
@@ -16309,9 +16306,6 @@ export namespace tabletmanagerdata {
 
         /** SchemaDefinition table_definitions. */
         public table_definitions: tabletmanagerdata.ITableDefinition[];
-
-        /** SchemaDefinition version. */
-        public version: string;
 
         /**
          * Creates a new SchemaDefinition instance using the specified properties.
