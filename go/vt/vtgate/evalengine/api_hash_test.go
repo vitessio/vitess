vitessio/vitess

commit 37a47e4cc3ad968d9fb1eda93604d918ca775aa2

mysql/evalengine: Unify float parsing (#12979)
We had three different variations of how we parsed floats in a MySQL
compatible way. The one in the `fastparse` package, the `strconv` hack
import & a version in the `evalengine`.

This all unifies things onto a single way that we parse it. It's in the
`fastparse` package but then uses the hack. But it's the only place that
calls it and all other callsites are converted to use the version in the
`fastparse` package, since the one from the `hack` package doesn't
immediately suffice.

While adding this, also additional tests were added to ensure that we
cover this properly and associated small fixes are included as well.

The main thing is that we weren't properly ignore leading and trailing
whitespaces everywhere that we should.

Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

go/vt/vtgate/evalengine/eval.go
@@ -23,6 +23,7 @@ import (
 	"vitess.io/vitess/go/hack"
 	"vitess.io/vitess/go/mysql/collations"
 	"vitess.io/vitess/go/mysql/decimal"
+	"vitess.io/vitess/go/mysql/fastparse"
 	"vitess.io/vitess/go/mysql/format"
 	"vitess.io/vitess/go/mysql/json"
 	"vitess.io/vitess/go/sqltypes"
@@ -151,7 +152,8 @@ func evalIsTruthy(e eval) boolean {
 			}
 			return makeboolean(hex.u != 0)
 		}
-		return makeboolean(parseStringToFloat(e.string()) != 0.0)
+		f, _ := fastparse.ParseFloat64(e.string())
+		return makeboolean(f != 0.0)
 	case *evalJSON:
 		return makeboolean(e.ToBoolean())
 	case *evalTemporal:
@@ -216,7 +218,8 @@ func valueToEvalCast(v sqltypes.Value, typ sqltypes.Type) (eval, error) {
 			fval, err := v.ToFloat64()
 			return newEvalFloat(fval), err
 		case v.IsText() || v.IsBinary():
-			return newEvalFloat(parseStringToFloat(v.RawStr())), nil
+			fval, _ := fastparse.ParseFloat64(v.RawStr())
+			return newEvalFloat(fval), nil
 		default:
 			return nil, vterrors.Errorf(vtrpcpb.Code_INTERNAL, "coercion should not try to coerce this value to a float: %v", v)
 		}
@@ -237,8 +240,8 @@ func valueToEvalCast(v sqltypes.Value, typ sqltypes.Type) (eval, error) {
 			}
 			dec = decimal.NewFromFloat(fval)
 		case v.IsText() || v.IsBinary():
-			fval := parseStringToFloat(v.RawStr())
-			dec = decimal.NewFromFloat(fval)
+			fval, _ := fastparse.ParseFloat64(v.RawStr())
+			return newEvalFloat(fval), nil
 		default:
 			return nil, vterrors.Errorf(vtrpcpb.Code_INTERNAL, "coercion should not try to coerce this value to a decimal: %v", v)
 		}

go/mysql/datetime/parse.go
@@ -18,9 +18,10 @@ package datetime
 
 import (
 	"math"
+	"strings"
 
 	"vitess.io/vitess/go/mysql/decimal"
-	"vitess.io/vitess/go/mysql/json/fastparse"
+	"vitess.io/vitess/go/mysql/fastparse"
 )
 
 func parsetimeHours(tp *timeparts, in string) (out string, ok bool) {
@@ -73,17 +74,24 @@ func parsetimeSeconds(tp *timeparts, in string) (out string, ok bool) {
 }
 
 func parsetimeAny(tp *timeparts, in string) (out string, ok bool) {
+	orig := in
 	for i := 0; i < len(in); i++ {
 		switch r := in[i]; {
 		case isSpace(r):
 			tp.day, in, ok = getnum(in, false)
-			if !ok || tp.day > 34 || !isSpace(in[0]) {
+			if !ok || !isSpace(in[0]) {
 				tp.day = 0
-				return "", false
+				return parsetimeNoDelimiters(tp, orig)
 			}
-
 			for len(in) > 0 && isSpace(in[0]) {
 				in = in[1:]
+			}
+			if !isDigit(in, 0) {
+				tp.day = 0
+				return parsetimeNoDelimiters(tp, orig)
+			}
+			if tp.day > 34 {
+				return "", clampTimeparts(tp)
 			}
 			return parsetimeHours(tp, in)
 		case r == ':':
@@ -148,6 +156,10 @@ func parsetimeNoDelimiters(tp *timeparts, in string) (out string, ok bool) {
 		in = in[n:]
 	}
 
+	return in, clampTimeparts(tp) && ok
+}
+
+func clampTimeparts(tp *timeparts) bool {
 	// Maximum time is 838:59:59, so we have to clamp
 	// it to that value here if we otherwise successfully
 	// parser the time.
@@ -156,13 +168,13 @@ func parsetimeNoDelimiters(tp *timeparts, in string) (out string, ok bool) {
 		tp.hour = 22
 		tp.min = 59
 		tp.sec = 59
-		ok = false
+		return false
 	}
-
-	return in, ok
+	return true
 }
 
 func ParseTime(in string, prec int) (t Time, l int, ok bool) {
+	in = strings.Trim(in, " \t\r\n")
 	if len(in) == 0 {
 		return Time{}, 0, false
 	}
@@ -174,6 +186,7 @@ func ParseTime(in string, prec int) (t Time, l int, ok bool) {
 
 	var tp timeparts
 	in, ok = parsetimeAny(&tp, in)
+	ok = clampTimeparts(&tp) && ok
 
 	hours := uint16(24*tp.day + tp.hour)
 	if !tp.isZero() && neg {

go/mysql/datetime/parse_test.go
@@ -112,14 +112,15 @@ func TestParseTime(t *testing.T) {
 		{input: "3 11:12:13foo", norm: "83:12:13.000000", output: testTime{3*24 + 11, 12, 13, 0, false}, err: true},
 		{input: "3 41:12:13", norm: "113:12:13.000000", output: testTime{3*24 + 41, 12, 13, 0, false}},
 		{input: "3 41:12:13foo", norm: "113:12:13.000000", output: testTime{3*24 + 41, 12, 13, 0, false}, err: true},
-		{input: "35 11:12:13", norm: "00:00:00.000000", err: true},
+		{input: "34 23:12:13", norm: "838:59:59.000000", output: testTime{838, 59, 59, 0, false}, err: true},
+		{input: "35 11:12:13", norm: "838:59:59.000000", output: testTime{838, 59, 59, 0, false}, err: true},
 		{input: "11:12", norm: "11:12:00.000000", output: testTime{11, 12, 0, 0, false}},
 		{input: "5 11:12", norm: "131:12:00.000000", output: testTime{5*24 + 11, 12, 0, 0, false}},
 		{input: "-2 11:12", norm: "-59:12:00.000000", output: testTime{2*24 + 11, 12, 0, 0, true}},
 		{input: "--2 11:12", norm: "00:00:00.000000", err: true},
 		{input: "nonsense", norm: "00:00:00.000000", err: true},
 		{input: "2 11", norm: "59:00:00.000000", output: testTime{2*24 + 11, 0, 0, 0, false}},
-		{input: "2 -11", norm: "48:00:00.000000", output: testTime{2 * 24, 0, 0, 0, false}, err: true},
+		{input: "2 -11", norm: "00:00:02.000000", output: testTime{0, 0, 2, 0, false}, err: true},
 		{input: "13", norm: "00:00:13.000000", output: testTime{0, 0, 13, 0, false}},
 		{input: "111213", norm: "11:12:13.000000", output: testTime{11, 12, 13, 0, false}},
 		{input: "111213.123456", norm: "11:12:13.123456", output: testTime{11, 12, 13, 123456000, false}, l: 6},
@@ -148,6 +149,7 @@ func TestParseTime(t *testing.T) {
 		{input: "-11111", norm: "-01:11:11.000000", output: testTime{1, 11, 11, 0, true}},
 		{input: "-111111", norm: "-11:11:11.000000", output: testTime{11, 11, 11, 0, true}},
 		{input: "-1111111", norm: "-111:11:11.000000", output: testTime{111, 11, 11, 0, true}},
+		{input: "0", norm: "00:00:00.000000", output: testTime{0, 0, 0, 0, false}},
 		{input: "1", norm: "00:00:01.000000", output: testTime{0, 0, 1, 0, false}},
 		{input: "11", norm: "00:00:11.000000", output: testTime{0, 0, 11, 0, false}},
 		{input: "111", norm: "00:01:11.000000", output: testTime{0, 1, 11, 0, false}},
@@ -169,7 +171,6 @@ func TestParseTime(t *testing.T) {
 		{input: "11111.1", norm: "01:11:11.100000", output: testTime{1, 11, 11, 100000000, false}, l: 1},
 		{input: "111111.1", norm: "11:11:11.100000", output: testTime{11, 11, 11, 100000000, false}, l: 1},
 		{input: "1111111.1", norm: "111:11:11.100000", output: testTime{111, 11, 11, 100000000, false}, l: 1},
-		{input: "2000-01-01 12:34:58.000000", norm: "00:00:00.000000", err: true},
 		{input: "20000101", norm: "838:59:59.000000", output: testTime{838, 59, 59, 0, false}, err: true},
 		{input: "-20000101", norm: "-838:59:59.000000", output: testTime{838, 59, 59, 0, true}, err: true},
 		{input: "999995959", norm: "838:59:59.000000", output: testTime{838, 59, 59, 0, false}, err: true},
@@ -178,6 +179,13 @@ func TestParseTime(t *testing.T) {
 		{input: "-4294965959", norm: "-838:59:59.000000", output: testTime{838, 59, 59, 0, true}, err: true},
 		{input: "4294975959", norm: "00:00:00.000000", err: true},
 		{input: "-4294975959", norm: "00:00:00.000000", err: true},
+		{input: "\t34 foo\t", norm: "00:00:34.000000", output: testTime{0, 0, 34, 0, false}, err: true},
+		{input: "\t34 1foo\t", norm: "817:00:00.000000", output: testTime{817, 0, 0, 0, false}, err: true},
+		{input: "\t34 23foo\t", norm: "838:59:59.000000", output: testTime{838, 59, 59, 0, false}, err: true},
+		{input: "\t35 foo\t", norm: "00:00:35.000000", output: testTime{0, 0, 35, 0, false}, err: true},
+		{input: "\t35 1foo\t", norm: "838:59:59.000000", output: testTime{838, 59, 59, 0, false}, err: true},
+		{input: " 255 foo", norm: "00:02:55.000000", output: testTime{0, 2, 55, 0, false}, err: true},
+		{input: "255", norm: "00:02:55.000000", output: testTime{0, 2, 55, 0, false}},
 	}
 
 	for _, test := range tests {

go/mysql/decimal/decimal_test.go
@@ -340,6 +340,10 @@ func TestNewFromStringDeepEquals(t *testing.T) {
 		{"10", "10.0", false},
 		{"1.1", "1.10", false},
 		{"1.001", "1.01", false},
+		{" 0 ", "0", true},
+		{" 0.0 ", "0.0", true},
+		{" 1 ", "1", true},
+		{" 0.1 ", "0.1", true},
 	}
 
 	for _, cmp := range tests {

go/mysql/decimal/scan.go
@@ -24,7 +24,7 @@ import (
 	"math/big"
 	"math/bits"
 
-	"vitess.io/vitess/go/mysql/json/fastparse"
+	"vitess.io/vitess/go/mysql/fastparse"
 )
 
 var errOverflow = errors.New("overflow")
@@ -161,6 +161,13 @@ func NewFromString(s string) (d Decimal, err error) {
 	i := 0
 	var num bool
 	var exp int64
+
+	for i < maxLen {
+		if !isSpace(s[i]) {
+			break
+		}
+		i++
+	}
 next:
 	for i < maxLen {
 		switch {
@@ -208,7 +215,8 @@ next:
 	}
 
 	if len(si) <= 18 {
-		v, _ := fastparse.ParseInt64(si, 10)
+		var v int64
+		v, err = fastparse.ParseInt64(si, 10)
 		d.value = big.NewInt(v)
 	} else {
 		d.value = new(big.Int)
@@ -230,6 +238,14 @@ next:
 	}
 
 	d.exp = int32(exp)
+
+	for i < maxLen {
+		if !isSpace(s[i]) {
+			break
+		}
+		i++
+	}
+
 	if !num || i < maxLen || expOverflow {
 		err = fmt.Errorf("invalid decimal string: %q", s)
 	}
@@ -344,4 +360,13 @@ func parseLargeDecimal(integral, fractional []byte) (*big.Int, error) {
 		z = mulAddWW(z, z, pow(b1, i), di)
 	}
 	return new(big.Int).SetBits(z), nil
+}
+
+func isSpace(c byte) bool {
+	switch c {
+	case ' ', '\t', '\n', '\r':
+		return true
+	default:
+		return false
+	}
 }

go/mysql/fastparse/fastparse.go
@@ -1,0 +1,238 @@
+/*
+Copyright 2018 Aliaksandr Valialkin
+Copyright 2023 The Vitess Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package fastparse
+
+import (
+	"errors"
+	"fmt"
+	"math"
+	"strconv"
+
+	"vitess.io/vitess/go/hack"
+)
+
+// ParseUint64 parses uint64 from s.
+//
+// It is equivalent to strconv.ParseUint(s, base, 64) in case it succeeds,
+// but on error it will return the best effort value of what it has parsed so far.
+func ParseUint64(s string, base int) (uint64, error) {
+	if len(s) == 0 {
+		return 0, fmt.Errorf("cannot parse uint64 from empty string")
+	}
+	if base < 2 || base > 36 {
+		return 0, fmt.Errorf("invalid base %d; must be in [2, 36]", base)
+	}
+	i := uint(0)
+	for i < uint(len(s)) {
+		if !isSpace(s[i]) {
+			break
+		}
+		i++
+	}
+
+	d := uint64(0)
+	j := i
+next:
+	for i < uint(len(s)) {
+		var b byte
+		switch {
+		case s[i] >= '0' && s[i] <= '9':
+			b = s[i] - '0'
+		case s[i] >= 'a' && s[i] <= 'z':
+			b = s[i] - 'a' + 10
+		case s[i] >= 'A' && s[i] <= 'Z':
+			b = s[i] - 'A' + 10
+		default:
+			break next
+		}
+
+		if b >= byte(base) {
+			break next
+		}
+
+		v := d*uint64(base) + uint64(b)
+		if v < d {
+			return math.MaxUint64, fmt.Errorf("cannot parse uint64 from %q: %w", s, ErrOverflow)
+		}
+		d = v
+		i++
+	}
+	if i <= j {
+		return d, fmt.Errorf("cannot parse uint64 from %q", s)
+	}
+
+	for i < uint(len(s)) {
+		if !isSpace(s[i]) {
+			break
+		}
+		i++
+	}
+
+	if i < uint(len(s)) {
+		// Unparsed tail left.
+		return d, fmt.Errorf("unparsed tail left after parsing uint64 from %q: %q", s, s[i:])
+	}
+	return d, nil
+}
+
+var ErrOverflow = errors.New("overflow")
+
+// ParseInt64 parses int64 number s.
+//
+// It is equivalent to strconv.ParseInt(s, base, 64) in case it succeeds,
+// but on error it will return the best effort value of what it has parsed so far.
+func ParseInt64(s string, base int) (int64, error) {
+	if len(s) == 0 {
+		return 0, fmt.Errorf("cannot parse int64 from empty string")
+	}
+	if base < 2 || base > 36 {
+		return 0, fmt.Errorf("invalid base %d; must be in [2, 36]", base)
+	}
+	i := uint(0)
+	for i < uint(len(s)) {
+		if !isSpace(s[i]) {
+			break
+		}
+		i++
+	}
+
+	minus := s[i] == '-'
+	if minus {
+		i++
+		if i >= uint(len(s)) {
+			return 0, fmt.Errorf("cannot parse int64 from %q", s)
+		}
+	}
+
+	d := uint64(0)
+	j := i
+next:
+	for i < uint(len(s)) {
+		var b byte
+		switch {
+		case s[i] >= '0' && s[i] <= '9':
+			b = s[i] - '0'
+		case s[i] >= 'a' && s[i] <= 'z':
+			b = s[i] - 'a' + 10
+		case s[i] >= 'A' && s[i] <= 'Z':
+			b = s[i] - 'A' + 10
+		default:
+			break next
+		}
+
+		if b >= byte(base) {
+			break next
+		}
+
+		v := d*uint64(base) + uint64(b)
+		if v < d {
+			if minus {
+				return math.MinInt64, fmt.Errorf("cannot parse int64 from %q: %w", s, ErrOverflow)
+			}
+			return math.MaxInt64, fmt.Errorf("cannot parse int64 from %q: %w", s, ErrOverflow)
+		}
+		d = v
+		i++
+	}
+
+	v := int64(d)
+	if d > math.MaxInt64 && !minus {
+		return math.MaxInt64, fmt.Errorf("cannot parse int64 from %q: %w", s, ErrOverflow)
+	} else if d > math.MaxInt64+1 && minus {
+		return math.MinInt64, fmt.Errorf("cannot parse int64 from %q: %w", s, ErrOverflow)
+	}
+
+	if minus {
+		v = -v
+		if d == math.MaxInt64+1 {
+			v = math.MinInt64
+		}
+	}
+
+	if i <= j {
+		return v, fmt.Errorf("cannot parse int64 from %q", s)
+	}
+
+	for i < uint(len(s)) {
+		if !isSpace(s[i]) {
+			break
+		}
+		i++
+	}
+
+	if i < uint(len(s)) {
+		// Unparsed tail left.
+		return v, fmt.Errorf("unparsed tail left after parsing int64 from %q: %q", s, s[i:])
+	}
+	if d == math.MaxInt64+1 && minus {
+		v = math.MinInt64
+	}
+
+	return v, nil
+}
+
+// ParseFloat64 parses floating-point number s.
+//
+// It is equivalent to strconv.ParseFloat(s, 64) in case it succeeds,
+// but on error it will return the best effort value of what it has parsed so far.
+func ParseFloat64(s string) (float64, error) {
+	if len(s) == 0 {
+		return 0.0, fmt.Errorf("cannot parse float64 from empty string")
+	}
+	i := uint(0)
+	for i < uint(len(s)) {
+		if !isSpace(s[i]) {
+			break
+		}
+		i++
+	}
+	ws := i
+
+	// We only care to parse as many of the initial float characters of the
+	// string as possible. This functionality is implemented in the `strconv` package
+	// of the standard library, but not exposed, so we hook into it.
+	val, l, err := hack.ParseFloatPrefix(s[ws:], 64)
+	for l < len(s[ws:]) {
+		if !isSpace(s[ws+uint(l)]) {
+			break
+		}
+		l++
+	}
+
+	if l < len(s[ws:]) {
+		return val, fmt.Errorf("unparsed tail left after parsing float64 from %q: %q", s, s[ws+uint(l):])
+	}
+	if errors.Is(err, strconv.ErrRange) {
+		if val < 0 {
+			val = -math.MaxFloat64
+		} else {
+			val = math.MaxFloat64
+		}
+	}
+
+	return val, err
+}
+
+func isSpace(c byte) bool {
+	switch c {
+	case ' ', '\t':
+		return true
+	default:
+		return false
+	}
+}

go/mysql/fastparse/fastparse_test.go
@@ -16,6 +16,7 @@ limitations under the License.
 package fastparse
 
 import (
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -47,6 +48,33 @@ func TestParseInt64(t *testing.T) {
 			input:    "10",
 			base:     2,
 			expected: 2,
+		},
+		{
+			input:    " 10",
+			base:     10,
+			expected: 10,
+		},
+		{
+			input:    " 10 ",
+			base:     10,
+			expected: 10,
+		},
+		{
+			input:    " 10 1",
+			base:     10,
+			expected: 10,
+			err:      `unparsed tail left after parsing int64 from " 10 1": "1"`,
+		},
+		{
+			input:    " -10 ",
+			base:     10,
+			expected: -10,
+		},
+		{
+			input:    " -10 1",
+			base:     10,
+			expected: -10,
+			err:      `unparsed tail left after parsing int64 from " -10 1": "1"`,
 		},
 		{
 			input:    "9223372036854775807",
@@ -145,6 +173,17 @@ func TestParseInt64(t *testing.T) {
 			expected: -1,
 			err:      `unparsed tail left after parsing int64 from "-1.1": ".1"`,
 		},
+		{
+			input:    "\t 42 \t",
+			base:     10,
+			expected: 42,
+		},
+		{
+			input:    "\t 42 \n",
+			base:     10,
+			expected: 42,
+			err:      `unparsed tail left after parsing int64 from "\t 42 \n": "\n"`,
+		},
 	}
 	for _, tc := range testcases {
 		t.Run(tc.input, func(t *testing.T) {
@@ -186,6 +225,22 @@ func TestParseUint64(t *testing.T) {
 			input:    "10",
 			base:     2,
 			expected: 2,
+		},
+		{
+			input:    " 10",
+			base:     10,
+			expected: 10,
+		},
+		{
+			input:    " 10 ",
+			base:     10,
+			expected: 10,
+		},
+		{
+			input:    " 10 1",
+			base:     10,
+			expected: 10,
+			err:      `unparsed tail left after parsing uint64 from " 10 1": "1"`,
 		},
 		{
 			input:    "9223372036854775807",
@@ -248,6 +303,17 @@ func TestParseUint64(t *testing.T) {
 			expected: 1,
 			err:      `unparsed tail left after parsing uint64 from "1.1": ".1"`,
 		},
+		{
+			input:    "\t 42 \t",
+			base:     10,
+			expected: 42,
+		},
+		{
+			input:    "\t 42 \n",
+			base:     10,
+			expected: 42,
+			err:      `unparsed tail left after parsing uint64 from "\t 42 \n": "\n"`,
+		},
 	}
 	for _, tc := range testcases {
 		t.Run(tc.input, func(t *testing.T) {
@@ -259,6 +325,217 @@ func TestParseUint64(t *testing.T) {
 				require.Equal(t, tc.expected, val)
 				require.EqualError(t, err, tc.err)
 			}
+		})
+	}
+}
+
+func TestParseFloat64(t *testing.T) {
+	testcases := []struct {
+		input    string
+		expected float64
+		err      string
+	}{
+		{
+			input:    "0",
+			expected: 0,
+		},
+		{
+			input:    "1",
+			expected: 1,
+		},
+		{
+			input:    "1",
+			expected: 1,
+		},
+		{
+			input:    "10",
+			expected: 10,
+		},
+		{
+			input:    "-",
+			expected: 0.0,
+			err:      `strconv.ParseFloat: parsing "-": invalid syntax`,
+		},
+		{
+			input:    " 10",
+			expected: 10,
+		},
+		{
+			input:    " 10 ",
+			expected: 10,
+		},
+		{
+			input:    " 10 1",
+			expected: 10,
+			err:      `unparsed tail left after parsing float64 from " 10 1": "1"`,
+		},
+		{
+			input:    "9223372036854775807",
+			expected: 9223372036854775807,
+		},
+		{
+			input:    "80.1",
+			expected: 80.1,
+		},
+		{
+			input:    "9223372036854775807trailing",
+			expected: 9223372036854775807,
+			err:      `unparsed tail left after parsing float64 from "9223372036854775807trailing": "trailing"`,
+		},
+		{
+			input:    " 9223372036854775807trailing",
+			expected: 9223372036854775807,
+			err:      `unparsed tail left after parsing float64 from " 9223372036854775807trailing": "trailing"`,
+		},
+		{
+			input:    " 9223372036854775807",
+			expected: 9223372036854775807,
+		},
+		{
+			input:    "9223372036854775808",
+			expected: 9223372036854775808,
+		},
+		{
+			input:    "9223372036854775808trailing",
+			expected: 9223372036854775808,
+			err:      `unparsed tail left after parsing float64 from "9223372036854775808trailing": "trailing"`,
+		},
+		{
+			input:    "-9223372036854775807",
+			expected: -9223372036854775807,
+		},
+		{
+			input:    "-9223372036854775807.1",
+			expected: -9223372036854775807.1,
+		},
+		{
+			input:    "-9223372036854775808",
+			expected: -9223372036854775808,
+		},
+		{
+			input:    "-9223372036854775808.1",
+			expected: -9223372036854775808.1,
+		},
+		{
+			input:    "-9223372036854775809",
+			expected: -9223372036854775809,
+		},
+		{
+			input:    "18446744073709551615",
+			expected: 18446744073709551615,
+		},
+		{
+			input:    "18446744073709551616",
+			expected: 18446744073709551616,
+		},
+		{
+			input:    "1.1",
+			expected: 1.1,
+		},
+		{
+			input:    "-1.1",
+			expected: -1.1,
+		},
+		{
+			input:    "1e100",
+			expected: 1e+100,
+		},
+		{
+			input:    "1e+100",
+			expected: 1e+100,
+		},
+		{
+			input:    "1e22",
+			expected: 1e22,
+		},
+		{
+			input:    "1e-22",
+			expected: 1e-22,
+		},
+		{
+			input:    "1e-100",
+			expected: 1e-100,
+		},
+		{
+			input:    "1e308",
+			expected: 1e308,
+		},
+		{
+			input:    "-1e308",
+			expected: -1e308,
+		},
+		{
+			input:    "1e408",
+			expected: math.MaxFloat64,
+			err:      `strconv.ParseFloat: parsing "1e408": value out of range`,
+		},
+		{
+			input:    "-1e408",
+			expected: -math.MaxFloat64,
+			err:      `strconv.ParseFloat: parsing "-1e408": value out of range`,
+		},
+		{
+			input:    "1e-308",
+			expected: 1e-308,
+		},
+		{
+			input:    "0.1.99",
+			expected: 0.1,
+			err:      `unparsed tail left after parsing float64 from "0.1.99": ".99"`,
+		},
+		{
+			input:    "\t 42.10 \t",
+			expected: 42.10,
+		},
+		{
+			input:    "\t 42.10 \n",
+			expected: 42.10,
+			err:      `unparsed tail left after parsing float64 from "\t 42.10 \n": "\n"`,
+		},
+	}
+	for _, tc := range testcases {
+		t.Run(tc.input, func(t *testing.T) {
+			val, err := ParseFloat64(tc.input)
+			if tc.err == "" {
+				require.NoError(t, err)
+				require.Equal(t, tc.expected, val)
+			} else {
+				require.Equal(t, tc.expected, val)
+				require.EqualError(t, err, tc.err)
+			}
+		})
+	}
+}
+
+func TestParseStringToFloat(t *testing.T) {
+	tcs := []struct {
+		str string
+		val float64
+	}{
+		{str: ""},
+		{str: " "},
+		{str: "1", val: 1},
+		{str: "1.10", val: 1.10},
+		{str: "    6.87", val: 6.87},
+		{str: "93.66  ", val: 93.66},
+		{str: "\t 42.10 \n ", val: 42.10},
+		{str: "1.10aa", val: 1.10},
+		{str: ".", val: 0.00},
+		{str: ".99", val: 0.99},
+		{str: "..99", val: 0},
+		{str: "1.", val: 1},
+		{str: "0.1.99", val: 0.1},
+		{str: "0.", val: 0},
+		{str: "8794354", val: 8794354},
+		{str: "    10  ", val: 10},
+		{str: "2266951196291479516", val: 2266951196291479516},
+		{str: "abcd123", val: 0},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.str, func(t *testing.T) {
+			got, _ := ParseFloat64(tc.str)
+			require.EqualValues(t, tc.val, got)
 		})
 	}
 }

commit 94f610cf27dd67e4521d0aeb86194a9fc4d6ba70

evalengine: Fix wrong coercion into float
This was a likely copy-paste bug introduced in
https://github.com/vitessio/vitess/pull/12979/files#diff-4e6fe1f65ff81fe967668629b6d6b978309e3ad20ba5f81ae95612c98b7c3cfcR244

Instead of converting properly into a decimal, it was returning a float
instance. This adds some additional test to ensure we catch this case
and improves the random test generator to cover decimals too.

Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

go/vt/vtgate/evalengine/eval.go
@@ -241,7 +241,7 @@ func valueToEvalCast(v sqltypes.Value, typ sqltypes.Type) (eval, error) {
 			dec = decimal.NewFromFloat(fval)
 		case v.IsText() || v.IsBinary():
 			fval, _ := fastparse.ParseFloat64(v.RawStr())
-			return newEvalFloat(fval), nil
+			dec = decimal.NewFromFloat(fval)
 		default:
 			return nil, vterrors.Errorf(vtrpcpb.Code_INTERNAL, "coercion should not try to coerce this value to a decimal: %v", v)
 		}

go/vt/vtgate/evalengine/api_hash_test.go
@@ -31,6 +31,33 @@ import (
 	"vitess.io/vitess/go/sqltypes"
 )
 
+func TestHashCodes(t *testing.T) {
+	var cases = []struct {
+		static, dynamic sqltypes.Value
+		equal           bool
+		err             error
+	}{
+		{sqltypes.NewInt64(-1), sqltypes.NewUint64(^uint64(0)), true, nil},
+		{sqltypes.NewUint64(^uint64(0)), sqltypes.NewInt64(-1), true, nil},
+		{sqltypes.NewFloat64(-1), sqltypes.NewVarChar("-1"), true, nil},
+		{sqltypes.NewDecimal("-1"), sqltypes.NewVarChar("-1"), true, nil},
+	}
+
+	for _, tc := range cases {
+		cmp, err := NullsafeCompare(tc.static, tc.dynamic, collations.CollationUtf8mb4ID)
+		require.NoError(t, err)
+		require.Equalf(t, tc.equal, cmp == 0, "got %v %s %v (expected %s)", tc.static, equality(cmp == 0).Operator(), tc.dynamic, equality(tc.equal))
+
+		h1, err := NullsafeHashcode(tc.static, collations.CollationUtf8mb4ID, tc.static.Type())
+		require.NoError(t, err)
+
+		h2, err := NullsafeHashcode(tc.dynamic, collations.CollationUtf8mb4ID, tc.static.Type())
+		require.ErrorIs(t, err, tc.err)
+
+		assert.Equalf(t, tc.equal, h1 == h2, "HASH(%v) %s HASH(%v) (expected %s)", tc.static, equality(h1 == h2).Operator(), tc.dynamic, equality(tc.equal))
+	}
+}
+
 // The following test tries to produce lots of different values and compares them both using hash code and compare,
 // to make sure that these two methods agree on what values are equal
 func TestHashCodesRandom(t *testing.T) {
@@ -179,6 +206,7 @@ var randomGenerators = []func() sqltypes.Value{
 	randomUint32,
 	randomVarChar,
 	randomComplexVarChar,
+	randomDecimal,
 }
 
 func randomValue() sqltypes.Value {
@@ -192,7 +220,9 @@ func randomInt32() sqltypes.Value   { return sqltypes.NewInt32(rand.Int31()) }
 func randomInt64() sqltypes.Value   { return sqltypes.NewInt64(rand.Int63()) }
 func randomUint32() sqltypes.Value  { return sqltypes.NewUint32(rand.Uint32()) }
 func randomUint64() sqltypes.Value  { return sqltypes.NewUint64(rand.Uint64()) }
+func randomDecimal() sqltypes.Value { return sqltypes.NewDecimal(fmt.Sprintf("%d", rand.Int63())) }
 func randomVarChar() sqltypes.Value { return sqltypes.NewVarChar(fmt.Sprintf("%d", rand.Int63())) }
+
 func randomComplexVarChar() sqltypes.Value {
 	return sqltypes.NewVarChar(fmt.Sprintf(" \t %f apa", float64(rand.Intn(1000))*1.10))
 }