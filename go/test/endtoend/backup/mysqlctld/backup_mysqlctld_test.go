d5bc7170cda459382abed82b63f28cc6b5718337
Olga Shestopalova <oshestopalova@hubspot.com>
2023-05-18 09:50:39 -0400

    try running this in isolation
    Signed-off-by: Olga Shestopalova <oshestopalova@hubspot.com>

--- go/test/endtoend/backup/mysqlctld/backup_mysqlctld_test.go
+++ go/test/endtoend/backup/mysqlctld/backup_mysqlctld_test.go
@@ -38,6 +38,10 @@ func TestBackupMysqlctldWithlz4Compression(t *testing.T) {
 	backup.TestBackup(t, backup.Mysqlctld, "xbstream", 0, cDetails, []string{"TestReplicaBackup", "TestPrimaryBackup"})
 }
 
+func TestDoNotDemoteNewlyPromotedPrimaryIfReparentingDuringBackup(t *testing.T) {
+	backup.TestBackup(t, backup.Mysqlctld, "xbstream", 0, nil, []string{"DoNotDemoteNewlyPromotedPrimaryIfReparentingDuringBackup"})
+}
+
 func setDefaultCompressionFlag() {
 	mysqlctl.CompressionEngineName = "pgzip"
 	mysqlctl.ExternalCompressorCmd = ""
