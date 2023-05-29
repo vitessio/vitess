02b430740ffb5dca3cf66e6e5c736531b914678e
Andres Taylor <andres@planetscale.com>
2023-05-29 10:44:44 +0200

    tests: stop tests not supported by older versions from running
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/test/endtoend/utils/utils.go
+++ go/test/endtoend/utils/utils.go
@@ -185,6 +185,16 @@ func SkipIfBinaryIsBelowVersion(t *testing.T, majorVersion int, binary string) {
 	}
 }
 
+// BinaryIsAtVersion returns true if this binary is at or above the required version
+func BinaryIsAtVersion(majorVersion int, binary string) bool {
+	version, err := cluster.GetMajorVersion(binary)
+	if err != nil {
+		return false
+	}
+	return version >= majorVersion
+
+}
+
 // AssertMatchesWithTimeout asserts that the given query produces the expected result.
 // The query will be executed every 'r' duration until it matches the expected result.
 // If after 'd' duration we still did not find the expected result, the test will be marked as failed.
