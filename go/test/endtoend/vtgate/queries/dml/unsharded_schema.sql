44e5a590fea727bc4d666b123b8fa98198fb6219
Harshit Gangal <harshit@planetscale.com>
2023-05-31 23:48:07 +0530

    added additional test for lookup column as auto increment column
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/test/endtoend/vtgate/queries/dml/unsharded_schema.sql
+++ go/test/endtoend/vtgate/queries/dml/unsharded_schema.sql
@@ -14,6 +14,14 @@ create table auto_seq
     primary key (id)
 ) comment 'vitess_sequence' Engine = InnoDB;
 
+create table mixed_seq
+(
+    id      int    default 0,
+    next_id bigint default null,
+    cache   bigint default null,
+    primary key (id)
+) comment 'vitess_sequence' Engine = InnoDB;
+
 create table u_tbl
 (
     id  bigint,
@@ -25,3 +33,5 @@ insert into user_seq(id, next_id, cache)
 values (0, 1, 1000);
 insert into auto_seq(id, next_id, cache)
 values (0, 666, 1000);
+insert into mixed_seq(id, next_id, cache)
+values (0, 1, 1000);
