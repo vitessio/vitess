21df39694877cb4659aedb674e29e6bccdc82abd
Harshit Gangal <harshit@planetscale.com>
2023-06-06 18:16:51 +0530

    on aggregate count and sum splittling create new aggr and update the column offset based on where it is pushed
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/vt/vtgate/planbuilder/operators/aggregation_pushing.go
+++ go/vt/vtgate/planbuilder/operators/aggregation_pushing.go
@@ -349,13 +349,12 @@ func splitAggrColumnsToLeftAndRight(
 outer:
 	// we prefer adding the aggregations in the same order as the columns are declared
 	for colIdx, col := range aggregator.Columns {
-		for aggrIdx, aggr := range aggregator.Aggregations {
+		for _, aggr := range aggregator.Aggregations {
 			if aggr.ColOffset == colIdx {
-				aggrToKeep, err := builder.handleAggr(ctx, aggr)
+				err := builder.handleAggr(ctx, aggr)
 				if err != nil {
 					return nil, nil, err
 				}
-				aggregator.Aggregations[aggrIdx] = aggrToKeep
 				continue outer
 			}
 		}
@@ -426,19 +425,19 @@ func (p *joinPusher) countStar(ctx *plancontext.PlanningContext) (*sqlparser.Ali
 	return p.csAE, true
 }
 
-func (ab *aggBuilder) handleAggr(ctx *plancontext.PlanningContext, aggr Aggr) (Aggr, error) {
+func (ab *aggBuilder) handleAggr(ctx *plancontext.PlanningContext, aggr Aggr) error {
 	switch aggr.OpCode {
 	case opcode.AggregateCountStar:
-		return ab.handleCountStar(ctx, aggr)
+		ab.handleCountStar(ctx, aggr)
+		return nil
 	case opcode.AggregateMax, opcode.AggregateMin, opcode.AggregateRandom:
 		return ab.handlePushThroughAggregation(ctx, aggr)
 	case opcode.AggregateCount, opcode.AggregateSum:
 		return ab.handleAggrWithCountStarMultiplier(ctx, aggr)
-
 	case opcode.AggregateUnassigned:
-		return Aggr{}, vterrors.VT12001(fmt.Sprintf("in scatter query: aggregation function '%s'", sqlparser.String(aggr.Original)))
+		return vterrors.VT12001(fmt.Sprintf("in scatter query: aggregation function '%s'", sqlparser.String(aggr.Original)))
 	default:
-		return Aggr{}, errHorizonNotPlanned()
+		return errHorizonNotPlanned()
 	}
 }
 
@@ -460,23 +459,22 @@ func (ab *aggBuilder) pushThroughRight(aggr Aggr) {
 	})
 }
 
-func (ab *aggBuilder) handlePushThroughAggregation(ctx *plancontext.PlanningContext, aggr Aggr) (Aggr, error) {
+func (ab *aggBuilder) handlePushThroughAggregation(ctx *plancontext.PlanningContext, aggr Aggr) error {
 	ab.proj.addUnexploredExpr(aggr.Original, aggr.Original.Expr)
 
 	deps := ctx.SemTable.RecursiveDeps(aggr.Original.Expr)
 	switch {
 	case deps.IsSolvedBy(ab.lhs.tableID):
 		ab.pushThroughLeft(aggr)
-		return aggr, nil
 	case deps.IsSolvedBy(ab.rhs.tableID):
 		ab.pushThroughRight(aggr)
-		return aggr, nil
 	default:
-		return Aggr{}, vterrors.VT12001("aggregation on columns from different sources: " + sqlparser.String(aggr.Original.Expr))
+		return vterrors.VT12001("aggregation on columns from different sources: " + sqlparser.String(aggr.Original.Expr))
 	}
+	return nil
 }
 
-func (ab *aggBuilder) handleCountStar(ctx *plancontext.PlanningContext, aggr Aggr) (Aggr, error) {
+func (ab *aggBuilder) handleCountStar(ctx *plancontext.PlanningContext, aggr Aggr) {
 	// Projection is necessary since we are going to need to do arithmetics to summarize the aggregates
 	ab.projectionRequired = true
 
@@ -484,6 +482,35 @@ func (ab *aggBuilder) handleCountStar(ctx *plancontext.PlanningContext, aggr Agg
 	lhsAE := ab.leftCountStar(ctx)
 	rhsAE := ab.rightCountStar(ctx)
 
+	ab.buildProjectionForAggr(lhsAE, rhsAE, aggr)
+}
+
+func (ab *aggBuilder) handleAggrWithCountStarMultiplier(ctx *plancontext.PlanningContext, aggr Aggr) error {
+	ab.projectionRequired = true
+
+	deps := ctx.SemTable.RecursiveDeps(aggr.Original.Expr)
+
+	var lhsAE, rhsAE *sqlparser.AliasedExpr
+	switch {
+	case deps.IsSolvedBy(ab.lhs.tableID):
+		ab.pushThroughLeft(aggr)
+		lhsAE = aggr.Original
+		rhsAE = ab.rightCountStar(ctx)
+
+	case deps.IsSolvedBy(ab.rhs.tableID):
+		ab.pushThroughRight(aggr)
+		lhsAE = ab.leftCountStar(ctx)
+		rhsAE = aggr.Original
+
+	default:
+		return errHorizonNotPlanned()
+	}
+
+	ab.buildProjectionForAggr(lhsAE, rhsAE, aggr)
+	return nil
+}
+
+func (ab *aggBuilder) buildProjectionForAggr(lhsAE *sqlparser.AliasedExpr, rhsAE *sqlparser.AliasedExpr, aggr Aggr) {
 	// We expect the expressions to be different on each side of the join, otherwise it's an error.
 	if lhsAE.Expr == rhsAE.Expr {
 		panic(fmt.Sprintf("Need the two produced expressions to be different. %T %T", lhsAE, rhsAE))
@@ -513,45 +540,6 @@ func (ab *aggBuilder) handleCountStar(ctx *plancontext.PlanningContext, aggr Agg
 	}
 
 	ab.proj.addUnexploredExpr(projAE, projExpr)
-	return aggr, nil
-}
-
-func (ab *aggBuilder) handleAggrWithCountStarMultiplier(ctx *plancontext.PlanningContext, aggr Aggr) (Aggr, error) {
-	ab.projectionRequired = true
-
-	expr := aggr.Original.Expr
-	deps := ctx.SemTable.RecursiveDeps(expr)
-	var otherSide sqlparser.Expr
-
-	switch {
-	case deps.IsSolvedBy(ab.lhs.tableID):
-		ab.pushThroughLeft(aggr)
-		ae := ab.rightCountStar(ctx)
-		otherSide = ae.Expr
-
-	case deps.IsSolvedBy(ab.rhs.tableID):
-		ab.pushThroughRight(aggr)
-		ae := ab.leftCountStar(ctx)
-		otherSide = ae.Expr
-
-	default:
-		return Aggr{}, errHorizonNotPlanned()
-	}
-
-	if ab.outerJoin {
-		otherSide = coalesceFunc(otherSide)
-	}
-
-	projAE := &sqlparser.AliasedExpr{
-		Expr: aggr.Original.Expr,
-		As:   sqlparser.NewIdentifierCI(aggr.Original.ColumnName()),
-	}
-	ab.proj.addUnexploredExpr(projAE, &sqlparser.BinaryExpr{
-		Operator: sqlparser.MultOp,
-		Left:     expr,
-		Right:    otherSide,
-	})
-	return aggr, nil
 }
 
 func coalesceFunc(e sqlparser.Expr) sqlparser.Expr {
@@ -584,8 +572,10 @@ func (p *joinPusher) addAggr(ctx *plancontext.PlanningContext, aggr Aggr) sqlpar
 // pushThroughAggr pushes through an aggregation without changing dependencies.
 // Can be used for aggregations we can push in one piece
 func (p *joinPusher) pushThroughAggr(aggr Aggr) {
-	p.pushed.Columns = append(p.pushed.Columns, aggr.Original)
-	p.pushed.Aggregations = append(p.pushed.Aggregations, aggr)
+	newAggr := NewAggr(aggr.OpCode, aggr.Func, aggr.Original, aggr.Alias)
+	newAggr.ColOffset = len(p.pushed.Columns)
+	p.pushed.Columns = append(p.pushed.Columns, newAggr.Original)
+	p.pushed.Aggregations = append(p.pushed.Aggregations, newAggr)
 }
 
 // addGrouping creates a copy of the given GroupBy, updates its column offset to point to the correct location in the new Aggregator,
