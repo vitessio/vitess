3bd0f4d77658f21c6091547ad0b9b4d73e9d512d
Andres Taylor <andres@planetscale.com>
2023-06-16 16:33:27 +0200

    bugfix: use the incoming type fields to calculate the outgoing types
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/test/endtoend/vtgate/queries/aggregation/aggregation_test.go
+++ go/test/endtoend/vtgate/queries/aggregation/aggregation_test.go
@@ -440,6 +440,8 @@ func TestBuggyQueries(t *testing.T) {
 		"[[NULL NULL INT64(3) NULL NULL INT64(3)] "+
 			"[INT32(100) DECIMAL(300) INT64(3) INT32(100) DECIMAL(300) INT64(3)] "+
 			"[INT32(200) DECIMAL(600) INT64(3) INT32(200) DECIMAL(600) INT64(3)]]")
+
+	mcmp.Exec("select /*vt+ PLANNER=gen4 */sum(tbl1.a), min(tbl0.b) from t10 as tbl0, t10 as tbl1 left join t10 as tbl2 on tbl1.a = tbl2.a and tbl1.b = tbl2.k")
 }
 
 func TestMinMaxAcrossJoins(t *testing.T) {
