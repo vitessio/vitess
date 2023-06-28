3ac82604d36403348dfcca1372779e55054f0464
Harshit Gangal <harshit@planetscale.com>
2023-06-28 23:48:34 +0530

    added more e2e test
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/test/endtoend/vtgate/queries/kill/kill_test.go
+++ go/test/endtoend/vtgate/queries/kill/kill_test.go
@@ -43,9 +43,6 @@ func TestKillOwnConnection(t *testing.T) {
 	// the connection should be closed.
 	_, err = utils.ExecAllowError(t, conn, "select 1")
 	require.ErrorContains(t, err, "EOF (errno 2013) (sqlstate HY000)")
-
-	err = conn.Ping()
-	require.ErrorContains(t, err, "EOF (errno 2013) (sqlstate HY000)")
 }
 
 // TestKillDifferentConnection kills different connection and check relevant error messages.
@@ -116,6 +113,124 @@ func TestKillDifferentConnectionQuery(t *testing.T) {
 			_, err = utils.ExecAllowError(t, killConn, fmt.Sprintf("kill query %d", conn.ConnectionID))
 			require.NoError(t, err)
 		case <-time.After(5 * time.Second):
+			t.Fatal("test did not complete in 5 seconds.")
+		}
+	}
+}
+
+// TestKillOnHungQuery test that any hung query should return.
+func TestKillOnHungQuery(t *testing.T) {
+
+	t.Run("connection close", func(t *testing.T) {
+		testHungQuery(t, func(conn1 *mysql.Conn, conn2 *mysql.Conn) {
+			// closing the hung query connection.
+			conn2.Close()
+		}, "(errno 2013) (sqlstate HY000)")
+	})
+
+	t.Run("connection kill", func(t *testing.T) {
+		testHungQuery(t, func(conn1 *mysql.Conn, conn2 *mysql.Conn) {
+			// kill conn2 connection
+			utils.Exec(t, conn1, fmt.Sprintf("kill %d", conn2.ConnectionID))
+		}, "(errno 2013) (sqlstate HY000)")
+	})
+
+	t.Run("query kill", func(t *testing.T) {
+		testHungQuery(t, func(conn1 *mysql.Conn, conn2 *mysql.Conn) {
+			// kill conn2 query
+			utils.Exec(t, conn1, fmt.Sprintf("kill query %d", conn2.ConnectionID))
+		}, "context canceled")
+	})
+}
+
+func testHungQuery(t *testing.T, killFunc func(*mysql.Conn, *mysql.Conn), errMsg string) {
+	conn1, err := mysql.Connect(context.Background(), &vtParams)
+	require.NoError(t, err)
+	defer conn1.Close()
+
+	utils.Exec(t, conn1, "begin")
+	utils.Exec(t, conn1, "insert into test(id, msg, extra) values (1, 'a', 'e')")
+
+	conn2, err := mysql.Connect(context.Background(), &vtParams)
+	require.NoError(t, err)
+	defer conn2.Close()
+
+	ch := make(chan error)
+	go func() {
+		ch <- nil
+		utils.Exec(t, conn2, "begin")
+		_, err := utils.ExecAllowError(t, conn2, "insert into test(id, msg, extra) values (1, 'a', 'e')")
+		ch <- err
+	}()
+	<-ch                        // started
+	time.Sleep(1 * time.Second) // just little more time
+
+	killFunc(conn1, conn2)
+
+	select {
+	case execErr := <-ch:
+		require.ErrorContains(t, execErr, errMsg)
+	case <-time.After(5 * time.Second):
+		t.Fatal("test did not complete in 5 seconds.")
+	}
+}
+
+// TestKillStmtOnHugeData tests different kill scenario on huge data.
+func TestKillStmtOnHugeData(t *testing.T) {
+	setupData(t, true)
+	defer dropData(t)
+
+	t.Run("oltp - kill conn", func(t *testing.T) {
+		testHugeData(t, "oltp", func(conn *mysql.Conn, killConn *mysql.Conn) {
+			utils.Exec(t, killConn, fmt.Sprintf("kill query %d", conn.ConnectionID))
+		}, "(errno 1317) (sqlstate 70100)")
+	})
+
+	t.Run("oltp - kill query", func(t *testing.T) {
+		testHugeData(t, "oltp", func(conn *mysql.Conn, killConn *mysql.Conn) {
+			utils.Exec(t, killConn, fmt.Sprintf("kill query %d", conn.ConnectionID))
+		}, "(errno 1317) (sqlstate 70100)")
+	})
+
+	t.Run("olap - kill conn", func(t *testing.T) {
+		testHugeData(t, "olap", func(conn *mysql.Conn, killConn *mysql.Conn) {
+			utils.Exec(t, killConn, fmt.Sprintf("kill query %d", conn.ConnectionID))
+		}, "(errno 2013) (sqlstate HY000)")
+	})
+
+	t.Run("olap - kill query", func(t *testing.T) {
+		testHugeData(t, "olap", func(conn *mysql.Conn, killConn *mysql.Conn) {
+			utils.Exec(t, killConn, fmt.Sprintf("kill query %d", conn.ConnectionID))
+		}, "(errno 2013) (sqlstate HY000)")
+	})
+}
+
+func testHugeData(t *testing.T, workload string, killFunc func(*mysql.Conn, *mysql.Conn), errMsg string) {
+	conn, err := mysql.Connect(context.Background(), &vtParams)
+	require.NoError(t, err)
+	defer conn.Close()
+	utils.Exec(t, conn, fmt.Sprintf("set workload = %s", workload))
+
+	killConn, err := mysql.Connect(context.Background(), &vtParams)
+	require.NoError(t, err)
+	defer killConn.Close()
+	utils.Exec(t, killConn, fmt.Sprintf("set workload = %s", workload))
+
+	ch := make(chan error)
+	go func() {
+		_, err := utils.ExecWithRowCount(t, conn, "select * from test", 640000)
+		ch <- err
+	}()
+
+	totalTime := time.After(5 * time.Second)
+	for {
+		select {
+		case execErr := <-ch:
+			require.ErrorContains(t, execErr, errMsg)
+			return
+		case <-time.After(100 * time.Millisecond):
+			killFunc(conn, killConn)
+		case <-totalTime:
 			t.Fatal("test did not complete in 5 seconds.")
 		}
 	}
