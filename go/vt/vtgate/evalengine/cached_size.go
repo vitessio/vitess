c9c9b8ef90c2a759c62d12d52c17266e346e5cf2
Dirkjan Bussink <d.bussink@gmail.com>
2023-06-29 18:50:35 +0200

    Update sizegen
    Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

--- go/vt/vtgate/evalengine/cached_size.go
+++ go/vt/vtgate/evalengine/cached_size.go
@@ -1257,6 +1257,18 @@ func (cached *builtinRandomBytes) CachedSize(alloc bool) int64 {
 	size += cached.CallExpr.CachedSize(false)
 	return size
 }
+func (cached *builtinRegexpLike) CachedSize(alloc bool) int64 {
+	if cached == nil {
+		return int64(0)
+	}
+	size := int64(0)
+	if alloc {
+		size += int64(48)
+	}
+	// field CallExpr vitess.io/vitess/go/vt/vtgate/evalengine.CallExpr
+	size += cached.CallExpr.CachedSize(false)
+	return size
+}
 func (cached *builtinRepeat) CachedSize(alloc bool) int64 {
 	if cached == nil {
 		return int64(0)
