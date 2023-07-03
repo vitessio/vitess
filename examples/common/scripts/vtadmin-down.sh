0487e79f8f409f0e57c43d8b3c3b8a150d5f03a4
Jean-François Gagné <jfg956@users.noreply.github.com>
2023-07-03 16:09:50 -0400

    Get rid of code duplication in vtadmin-down.sh.
    Signed-off-by: Jean-François Gagné <jfg956@users.noreply.github.com>

--- examples/common/scripts/vtadmin-down.sh
+++ examples/common/scripts/vtadmin-down.sh
@@ -2,16 +2,17 @@
 
 source "$(dirname "${BASH_SOURCE[0]:-$0}")/../env.sh"
 
-if test -e "$VTDATAROOT/tmp/vtadmin-web.pid"; then
-  echo "Stopping vtadmin-web..."
-  kill -9 "$(cat "$VTDATAROOT/tmp/vtadmin-web.pid")"
-else
-  echo "Skipping stopping vtadmin-web because no pid file."
-fi
+function stop_vtadmin() {
+  local name="$1"
+  local file="$2"
 
-if test -e "$VTDATAROOT/tmp/vtadmin-api.pid"; then
-  echo "Stopping vtadmin-api..."
-  kill -9 "$(cat "$VTDATAROOT/tmp/vtadmin-api.pid")"
-else
-  echo "Skipping stopping vtadmin-web because no pid file."
-fi
+  if test -e "$file"; then
+    echo "Stopping $name..."
+    kill -9 "$(cat "$file")"
+  else
+    echo "Skipping stopping $name because no pid file."
+  fi
+}
+
+stop_vtadmin "vtadmin-web" "$VTDATAROOT/tmp/vtadmin-web.pid"
+stop_vtadmin "vtadmin-api" "$VTDATAROOT/tmp/vtadmin-api.pid"
