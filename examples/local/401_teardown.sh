09ecf952d0ad6c075dde3eedb3ed4dfbf9abbb5f
Jean-François Gagné <jfg956@users.noreply.github.com>
2023-07-07 16:50:13 -0400

    Stop mysqlctls in parallel in 401_teardown.sh.
    MySQL takes time to stop, and we have at least 3 to stop, maybe more.
    Instead of waiting n times for MySQL to stop, we stop in the background and wait for them at the same time.
    This makes 401_teardown.sh significantly quicker to run.
    
    Signed-off-by: Jean-François Gagné <jfg956@users.noreply.github.com>

--- examples/local/401_teardown.sh
+++ examples/local/401_teardown.sh
@@ -33,8 +33,15 @@ for tablet in 100 200 300 400; do
 			printf -v alias '%s-%010d' 'zone1' $uid
 			echo "Shutting down tablet $alias"
 			CELL=zone1 TABLET_UID=$uid ../common/scripts/vttablet-down.sh
+   			# because MySQL takes time to stop, we do this in parallel
 			CELL=zone1 TABLET_UID=$uid ../common/scripts/mysqlctl-down.sh
 		done
+
+  		# without a sleep below, we can have the echo happen before the echo of mysqlctl-down.sh
+                sleep 2
+                echo "Waiting mysqlctl to stop..."
+                wait
+                echo "mysqlctls are stopped!"
 	fi
 done
 
