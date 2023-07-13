vitessio/vitess

commit 40979b6b437483f208829c2cd5cd71aac2e76211

backup: Allow for upgrade safe backups (#13449)
* backup: Allow for upgrade safe backups

This adds functionality so that it's possible to use `Backup` and
`BackupShard` and enforce that the backup created is safe to be used for
upgrading MySQL.

Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

* Fix test output

Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

* backup: Add version number to manifest

This adds the version number and upgrade safe state to the manifest. If
these values are set, they are also validated on restore.

For backwards compability we always allow still empty version numbers.

Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

* Move restart logic into xtrabackupengine

This already happens correctly with the shutdown for the builtin engine,
where we're shutdown twice for `vtbackup` then. With this move we also
properly honor the `UpgradeSafe` flag if the path is called through
`Backup` or `BackupShard` vtctld commands.

Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

* backup: Rework version detection

It's not guaranteed that MySQL will always be up when doing a restore,
this is shown in some of the failing tests with the changes so far.

This means we need a way to get the MySQL version without having it
running. We can though rely in `mysqlctld` being up. So this adds a new
RPC to retrieve the version number through invoking the binary. This
ensures we still get the right version also if we're running from the
tablet as we'd execute the RPC.

We also have less of a versioning issue here, as the vttablet + mysql
are always upgraded together and the new mysqlctl is injected into the
mysql container if needed so we don't have a version mismatch. So it's
safe to do this in one step.

Also decided to make the upgrade safe an explicit flag for `vtbackup` as
well since overloading the existing flag was wonky and not really what
we'd want. This makes it more explicit and safe.

go/vt/mysqlctl/grpcmysqlctlclient/client.go
@@ -111,6 +111,17 @@ func (c *client) RefreshConfig(ctx context.Context) error {
 	})
 }
 
+// VersionString is part of the MysqlctlClient interface.
+func (c *client) VersionString(ctx context.Context) (string, error) {
+	var version string
+	err := c.withRetry(ctx, func() error {
+		r, err := c.c.VersionString(ctx, &mysqlctlpb.VersionStringRequest{})
+		version = r.Version
+		return err
+	})
+	return version, err
+}
+
 // Close is part of the MysqlctlClient interface.
 func (c *client) Close() {
 	c.cc.Close()

go/vt/vtctl/grpcvtctldserver/server.go
@@ -455,7 +455,7 @@ func (s *VtctldServer) BackupShard(req *vtctldatapb.BackupShardRequest, stream v
 
 	span.Annotate("tablet_alias", topoproto.TabletAliasString(backupTablet.Alias))
 
-	r := &vtctldatapb.BackupRequest{Concurrency: req.Concurrency, AllowPrimary: req.AllowPrimary}
+	r := &vtctldatapb.BackupRequest{Concurrency: req.Concurrency, AllowPrimary: req.AllowPrimary, UpgradeSafe: req.UpgradeSafe}
 	err = s.backupTablet(ctx, backupTablet, r, stream)
 	return err
 }
@@ -467,6 +467,7 @@ func (s *VtctldServer) backupTablet(ctx context.Context, tablet *topodatapb.Tabl
 		Concurrency:        int64(req.Concurrency),
 		AllowPrimary:       req.AllowPrimary,
 		IncrementalFromPos: req.IncrementalFromPos,
+		UpgradeSafe:        req.UpgradeSafe,
 	}
 	logStream, err := s.tmc.Backup(ctx, tablet, r)
 	if err != nil {

go/cmd/vtbackup/vtbackup.go
@@ -107,6 +107,7 @@ var (
 	initialBackup       bool
 	allowFirstBackup    bool
 	restartBeforeBackup bool
+	upgradeSafe         bool
 	// vttablet-like flags
 	initDbNameOverride string
 	initKeyspace       string
@@ -135,6 +136,7 @@ func registerFlags(fs *pflag.FlagSet) {
 	fs.BoolVar(&initialBackup, "initial_backup", initialBackup, "Instead of restoring from backup, initialize an empty database with the provided init_db_sql_file and upload a backup of that for the shard, if the shard has no backups yet. This can be used to seed a brand new shard with an initial, empty backup. If any backups already exist for the shard, this will be considered a successful no-op. This can only be done before the shard exists in topology (i.e. before any tablets are deployed).")
 	fs.BoolVar(&allowFirstBackup, "allow_first_backup", allowFirstBackup, "Allow this job to take the first backup of an existing shard.")
 	fs.BoolVar(&restartBeforeBackup, "restart_before_backup", restartBeforeBackup, "Perform a mysqld clean/full restart after applying binlogs, but before taking the backup. Only makes sense to work around xtrabackup bugs.")
+	fs.BoolVar(&upgradeSafe, "upgrade_safe", upgradeSafe, "Whether to use innodb_fast_shutdown=0 for the backup so it is safe to use for MySQL upgrades.")
 	// vttablet-like flags
 	fs.StringVar(&initDbNameOverride, "init_db_name_override", initDbNameOverride, "(init parameter) override the name of the db used by vttablet")
 	fs.StringVar(&initKeyspace, "init_keyspace", initKeyspace, "(init parameter) keyspace to use for this tablet")
@@ -301,6 +303,7 @@ func takeBackup(ctx context.Context, topoServer *topo.Server, backupStorage back
 		Shard:              initShard,
 		TabletAlias:        topoproto.TabletAliasString(tabletAlias),
 		Stats:              backupstats.BackupStats(),
+		UpgradeSafe:        upgradeSafe,
 	}
 	// In initial_backup mode, just take a backup of this empty database.
 	if initialBackup {

go/test/endtoend/backup/vtctlbackup/backup_utils.go
@@ -810,6 +810,7 @@ func terminatedRestore(t *testing.T) {
 }
 
 func checkTabletType(t *testing.T, alias string, tabletType topodata.TabletType) {
+	t.Helper()
 	// for loop for 15 seconds to check if tablet type is correct
 	for i := 0; i < 15; i++ {
 		output, err := localCluster.VtctldClientProcess.ExecuteCommandWithOutput("GetTablet", alias)
@@ -1060,8 +1061,8 @@ func terminateRestore(t *testing.T) {
 				assert.Fail(t, "restore in progress file missing")
 			}
 			tmpProcess.Process.Signal(syscall.SIGTERM)
-			found = true //nolint
-			return
+			found = true
+			break
 		}
 	}
 	assert.True(t, found, "Restore message not found")

go/vt/mysqlctl/backup_test.go
@@ -20,6 +20,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"os"
 	"path"
@@ -419,6 +420,134 @@ func TestRestoreTriesToParameterizeBackupStorage(t *testing.T) {
 	require.NotNil(t, scopedStats)
 }
 
+// TestRestoreManifestMySQLVersionValidation tests that Restore tries to validate
+// the MySQL version and safe upgrade attribute.
+func TestRestoreManifestMySQLVersionValidation(t *testing.T) {
+	testCases := []struct {
+		fromVersion, toVersion string
+		upgradeSafe            bool
+		wantErr                bool
+	}{
+		{
+			fromVersion: "mysqld  Ver 5.6.42",
+			toVersion:   "mysqld  Ver 5.7.40",
+			upgradeSafe: false,
+			wantErr:     true,
+		},
+		{
+			fromVersion: "mysqld  Ver 5.6.42",
+			toVersion:   "mysqld  Ver 5.7.40",
+			upgradeSafe: true,
+			wantErr:     false,
+		},
+		{
+			fromVersion: "mysqld  Ver 5.7.42",
+			toVersion:   "mysqld  Ver 8.0.32",
+			upgradeSafe: true,
+			wantErr:     false,
+		},
+		{
+			fromVersion: "mysqld  Ver 5.7.42",
+			toVersion:   "mysqld  Ver 8.0.32",
+			upgradeSafe: false,
+			wantErr:     true,
+		},
+		{
+			fromVersion: "mysqld  Ver 5.7.42",
+			toVersion:   "mysqld  Ver 8.0.32",
+			upgradeSafe: true,
+			wantErr:     false,
+		},
+		{
+			fromVersion: "mysqld  Ver 8.0.32",
+			toVersion:   "mysqld  Ver 8.0.32",
+			upgradeSafe: false,
+			wantErr:     false,
+		},
+		{
+			fromVersion: "mysqld  Ver 8.0.32",
+			toVersion:   "mysqld  Ver 8.0.32",
+			upgradeSafe: true,
+			wantErr:     false,
+		},
+		{
+			fromVersion: "mysqld  Ver 8.0.32",
+			toVersion:   "mysqld  Ver 8.0.31",
+			upgradeSafe: false,
+			wantErr:     true,
+		},
+		{
+			fromVersion: "mysqld  Ver 8.0.32",
+			toVersion:   "mysqld  Ver 8.0.31",
+			upgradeSafe: true,
+			wantErr:     true,
+		},
+		{
+			fromVersion: "mysqld  Ver 8.0.32",
+			toVersion:   "mysqld  Ver 8.0.33",
+			upgradeSafe: false,
+			wantErr:     true,
+		},
+		{
+			fromVersion: "mysqld  Ver 8.0.32",
+			toVersion:   "mysqld  Ver 8.0.33",
+			upgradeSafe: true,
+			wantErr:     false,
+		},
+		{
+			fromVersion: "",
+			toVersion:   "mysqld  Ver 8.0.33",
+			upgradeSafe: false,
+			wantErr:     false,
+		},
+		{
+			fromVersion: "",
+			toVersion:   "mysqld  Ver 8.0.33",
+			upgradeSafe: true,
+			wantErr:     false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%s->%s upgradeSafe=%t", tc.fromVersion, tc.toVersion, tc.upgradeSafe), func(t *testing.T) {
+			env, closer := createFakeBackupRestoreEnv(t)
+			defer closer()
+			env.mysqld.Version = tc.toVersion
+
+			manifest := BackupManifest{
+				BackupTime:   time.Now().Add(-1 * time.Hour).Format(time.RFC3339),
+				BackupMethod: "fake",
+				Keyspace:     "test",
+				Shard:        "-",
+				MySQLVersion: tc.fromVersion,
+				UpgradeSafe:  tc.upgradeSafe,
+			}
+
+			manifestBytes, err := json.Marshal(manifest)
+			require.Nil(t, err)
+
+			env.backupEngine.ExecuteRestoreReturn = FakeBackupEngineExecuteRestoreReturn{&manifest, nil}
+			env.backupStorage.ListBackupsReturn = FakeBackupStorageListBackupsReturn{
+				BackupHandles: []backupstorage.BackupHandle{
+					&FakeBackupHandle{
+						ReadFileReturnF: func(context.Context, string) (io.ReadCloser, error) {
+							return io.NopCloser(bytes.NewBuffer(manifestBytes)), nil
+						},
+					},
+				},
+			}
+
+			_, err = Restore(env.ctx, env.restoreParams)
+			if tc.wantErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+
+}
+
 type forTest []FileEntry
 
 func (f forTest) Len() int           { return len(f) }
@@ -490,6 +619,7 @@ func createFakeBackupRestoreEnv(t *testing.T) (*fakeBackupRestoreEnv, func()) {
 		BackupMethod: "fake",
 		Keyspace:     "test",
 		Shard:        "-",
+		MySQLVersion: "8.0.32",
 	}
 
 	manifestBytes, err := json.Marshal(manifest)

go/vt/mysqlctl/backupengine.go
@@ -73,22 +73,25 @@ type BackupParams struct {
 	IncrementalFromPos string
 	// Stats let's backup engines report detailed backup timings.
 	Stats backupstats.Stats
+	// UpgradeSafe indicates whether the backup is safe for upgrade and created with innodb_fast_shutdown=0
+	UpgradeSafe bool
 }
 
-func (b BackupParams) Copy() BackupParams {
+func (b *BackupParams) Copy() BackupParams {
 	return BackupParams{
-		b.Cnf,
-		b.Mysqld,
-		b.Logger,
-		b.Concurrency,
-		b.HookExtraEnv,
-		b.TopoServer,
-		b.Keyspace,
-		b.Shard,
-		b.TabletAlias,
-		b.BackupTime,
-		b.IncrementalFromPos,
-		b.Stats,
+		Cnf:                b.Cnf,
+		Mysqld:             b.Mysqld,
+		Logger:             b.Logger,
+		Concurrency:        b.Concurrency,
+		HookExtraEnv:       b.HookExtraEnv,
+		TopoServer:         b.TopoServer,
+		Keyspace:           b.Keyspace,
+		Shard:              b.Shard,
+		TabletAlias:        b.TabletAlias,
+		BackupTime:         b.BackupTime,
+		IncrementalFromPos: b.IncrementalFromPos,
+		Stats:              b.Stats,
+		UpgradeSafe:        b.UpgradeSafe,
 	}
 }
 
@@ -123,21 +126,21 @@ type RestoreParams struct {
 	Stats backupstats.Stats
 }
 
-func (p RestoreParams) Copy() RestoreParams {
+func (p *RestoreParams) Copy() RestoreParams {
 	return RestoreParams{
-		p.Cnf,
-		p.Mysqld,
-		p.Logger,
-		p.Concurrency,
-		p.HookExtraEnv,
-		p.DeleteBeforeRestore,
-		p.DbName,
-		p.Keyspace,
-		p.Shard,
-		p.StartTime,
-		p.RestoreToPos,
-		p.DryRun,
-		p.Stats,
+		Cnf:                 p.Cnf,
+		Mysqld:              p.Mysqld,
+		Logger:              p.Logger,
+		Concurrency:         p.Concurrency,
+		HookExtraEnv:        p.HookExtraEnv,
+		DeleteBeforeRestore: p.DeleteBeforeRestore,
+		DbName:              p.DbName,
+		Keyspace:            p.Keyspace,
+		Shard:               p.Shard,
+		StartTime:           p.StartTime,
+		RestoreToPos:        p.RestoreToPos,
+		DryRun:              p.DryRun,
+		Stats:               p.Stats,
 	}
 }
 
@@ -273,6 +276,12 @@ type BackupManifest struct {
 	Keyspace string
 
 	Shard string
+
+	// MySQLversion is the version of MySQL when the backup was taken.
+	MySQLVersion string
+
+	// UpgradeSafe indicates whether the backup is safe to use for an upgrade to a newer MySQL version
+	UpgradeSafe bool
 }
 
 func (m *BackupManifest) HashKey() string {
@@ -392,6 +401,11 @@ func FindBackupToRestore(ctx context.Context, params RestoreParams, bhs []backup
 	manifests := make([]*BackupManifest, len(bhs))
 	manifestHandleMap := NewManifestHandleMap()
 
+	mysqlVersion, err := params.Mysqld.GetVersionString(ctx)
+	if err != nil {
+		return nil, err
+	}
+
 	fullBackupIndex := func() int {
 		for index := len(bhs) - 1; index >= 0; index-- {
 			bh := bhs[index]
@@ -407,6 +421,14 @@ func FindBackupToRestore(ctx context.Context, params RestoreParams, bhs []backup
 			if bm.Incremental {
 				// We're looking for a full backup
 				continue
+			}
+
+			// check if the backup can be used with this MySQL version.
+			if bm.MySQLVersion != "" {
+				if err := validateMySQLVersionUpgradeCompatible(mysqlVersion, bm.MySQLVersion, bm.UpgradeSafe); err != nil {
+					params.Logger.Warningf("Skipping backup %v/%v with incompatible MySQL version %v (upgrade safe: %v): %v", backupDir, bh.Name(), bm.MySQLVersion, bm.UpgradeSafe, err)
+					continue
+				}
 			}
 
 			var backupTime time.Time
@@ -461,12 +483,46 @@ func FindBackupToRestore(ctx context.Context, params RestoreParams, bhs []backup
 	// restore to a position (using incremental backups):
 	// we calculate a possible restore path based on the manifests. The resulting manifests are
 	// a sorted subsequence, with the full backup first, and zero or more incremental backups to follow.
-	manifests, err := FindPITRPath(params.RestoreToPos.GTIDSet, manifests)
+	restorePath.manifests, err = FindPITRPath(params.RestoreToPos.GTIDSet, manifests)
 	if err != nil {
 		return nil, err
 	}
-	restorePath.manifests = manifests
 	return restorePath, nil
+}
+
+func validateMySQLVersionUpgradeCompatible(to string, from string, upgradeSafe bool) error {
+	// It's always safe to use the same version.
+	if to == from {
+		return nil
+	}
+
+	flavorTo, parsedTo, err := ParseVersionString(to)
+	if err != nil {
+		return err
+	}
+
+	flavorFrom, parsedFrom, err := ParseVersionString(from)
+	if err != nil {
+		return err
+	}
+
+	if flavorTo != flavorFrom {
+		return fmt.Errorf("cannot use backup between different flavors: %q vs. %q", from, to)
+	}
+
+	if parsedTo == parsedFrom {
+		return nil
+	}
+
+	if !parsedTo.atLeast(parsedFrom) {
+		return fmt.Errorf("running MySQL version %q is older than backup MySQL version %q", to, from)
+	}
+
+	if upgradeSafe {
+		return nil
+	}
+
+	return fmt.Errorf("running MySQL version %q is newer than backup MySQL version %q which is not safe to upgrade", to, from)
 }
 
 func prepareToRestore(ctx context.Context, cnf *Mycnf, mysqld MysqlDaemon, logger logutil.Logger) error {

go/vt/mysqlctl/builtinbackupengine.go
@@ -218,6 +218,11 @@ func (be *BuiltinBackupEngine) executeIncrementalBackup(ctx context.Context, par
 	if err != nil {
 		return false, vterrors.Wrap(err, "can't get server uuid")
 	}
+	mysqlVersion, err := params.Mysqld.GetVersionString(ctx)
+	if err != nil {
+		return false, vterrors.Wrap(err, "can't get MySQL version")
+	}
+
 	// @@gtid_purged
 	getPurgedGTIDSet := func() (mysql.Position, mysql.Mysql56GTIDSet, error) {
 		gtidPurged, err := params.Mysqld.GetGTIDPurged(ctx)
@@ -328,7 +333,7 @@ func (be *BuiltinBackupEngine) executeIncrementalBackup(ctx context.Context, par
 	// incrementalBackupFromGTID is the "previous GTIDs" of the first binlog file we back up.
 	// It is a fact that incrementalBackupFromGTID is earlier or equal to params.IncrementalFromPos.
 	// In the backup manifest file, we document incrementalBackupFromGTID, not the user's requested position.
-	if err := be.backupFiles(ctx, params, bh, incrementalBackupToPosition, gtidPurged, incrementalBackupFromPosition, binaryLogsToBackup, serverUUID); err != nil {
+	if err := be.backupFiles(ctx, params, bh, incrementalBackupToPosition, gtidPurged, incrementalBackupFromPosition, binaryLogsToBackup, serverUUID, mysqlVersion); err != nil {
 		return false, err
 	}
 	return true, nil
@@ -418,6 +423,18 @@ func (be *BuiltinBackupEngine) executeFullBackup(ctx context.Context, params Bac
 		return false, vterrors.Wrap(err, "can't get server uuid")
 	}
 
+	mysqlVersion, err := params.Mysqld.GetVersionString(ctx)
+	if err != nil {
+		return false, vterrors.Wrap(err, "can't get MySQL version")
+	}
+
+	// check if we need to set innodb_fast_shutdown=0 for a backup safe for upgrades
+	if params.UpgradeSafe {
+		if _, err := params.Mysqld.FetchSuperQuery(ctx, "SET GLOBAL innodb_fast_shutdown=0"); err != nil {
+			return false, vterrors.Wrapf(err, "failed to disable fast shutdown")
+		}
+	}
+
 	// shutdown mysqld
 	shutdownCtx, cancel := context.WithTimeout(ctx, BuiltinBackupMysqldTimeout)
 	err = params.Mysqld.Shutdown(shutdownCtx, params.Cnf, true)
@@ -427,7 +444,7 @@ func (be *BuiltinBackupEngine) executeFullBackup(ctx context.Context, params Bac
 	}
 
 	// Backup everything, capture the error.
-	backupErr := be.backupFiles(ctx, params, bh, replicationPosition, gtidPurgedPosition, mysql.Position{}, nil, serverUUID)
+	backupErr := be.backupFiles(ctx, params, bh, replicationPosition, gtidPurgedPosition, mysql.Position{}, nil, serverUUID, mysqlVersion)
 	usable := backupErr == nil
 
 	// Try to restart mysqld, use background context in case we timed out the original context
@@ -512,6 +529,7 @@ func (be *BuiltinBackupEngine) backupFiles(
 	fromPosition mysql.Position,
 	binlogFiles []string,
 	serverUUID string,
+	mysqlVersion string,
 ) (finalErr error) {
 	// Get the files to backup.
 	// We don't care about totalSize because we add each file separately.
@@ -606,6 +624,8 @@ func (be *BuiltinBackupEngine) backupFiles(
 			Shard:          params.Shard,
 			BackupTime:     params.BackupTime.UTC().Format(time.RFC3339),
 			FinishedTime:   time.Now().UTC().Format(time.RFC3339),
+			MySQLVersion:   mysqlVersion,
+			UpgradeSafe:    params.UpgradeSafe,
 		},
 
 		// Builtin-specific fields

go/vt/mysqlctl/builtinbackupengine_test.go
@@ -29,6 +29,8 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"vitess.io/vitess/go/sqltypes"
+
 	"vitess.io/vitess/go/mysql"
 	"vitess.io/vitess/go/mysql/fakesqldb"
 	"vitess.io/vitess/go/vt/logutil"
@@ -213,6 +215,91 @@ func TestExecuteBackup(t *testing.T) {
 
 	assert.Error(t, err)
 	assert.False(t, ok)
+}
+
+func TestExecuteBackupWithSafeUpgrade(t *testing.T) {
+	// Set up local backup directory
+	backupRoot := "testdata/builtinbackup_test"
+	filebackupstorage.FileBackupStorageRoot = backupRoot
+	require.NoError(t, createBackupDir(backupRoot, "innodb", "log", "datadir"))
+	dataDir := path.Join(backupRoot, "datadir")
+	// Add some files under data directory to force backup to actually backup files.
+	require.NoError(t, createBackupDir(dataDir, "test1"))
+	require.NoError(t, createBackupDir(dataDir, "test2"))
+	require.NoError(t, createBackupFiles(path.Join(dataDir, "test1"), 2, "ibd"))
+	require.NoError(t, createBackupFiles(path.Join(dataDir, "test2"), 2, "ibd"))
+	defer os.RemoveAll(backupRoot)
+
+	ctx := context.Background()
+
+	needIt, err := needInnoDBRedoLogSubdir()
+	require.NoError(t, err)
+	if needIt {
+		fpath := path.Join("log", mysql.DynamicRedoLogSubdir)
+		if err := createBackupDir(backupRoot, fpath); err != nil {
+			require.Failf(t, err.Error(), "failed to create directory: %s", fpath)
+		}
+	}
+
+	// Set up topo
+	keyspace, shard := "mykeyspace", "-80"
+	ts := memorytopo.NewServer("cell1")
+	defer ts.Close()
+
+	require.NoError(t, ts.CreateKeyspace(ctx, keyspace, &topodata.Keyspace{}))
+	require.NoError(t, ts.CreateShard(ctx, keyspace, shard))
+
+	tablet := topo.NewTablet(100, "cell1", "mykeyspace-00-80-0100")
+	tablet.Keyspace = keyspace
+	tablet.Shard = shard
+
+	require.NoError(t, ts.CreateTablet(ctx, tablet))
+
+	_, err = ts.UpdateShardFields(ctx, keyspace, shard, func(si *topo.ShardInfo) error {
+		si.PrimaryAlias = &topodata.TabletAlias{Uid: 100, Cell: "cell1"}
+
+		now := time.Now()
+		si.PrimaryTermStartTime = &vttime.Time{Seconds: int64(now.Second()), Nanoseconds: int32(now.Nanosecond())}
+
+		return nil
+	})
+	require.NoError(t, err)
+
+	be := &mysqlctl.BuiltinBackupEngine{}
+
+	// Configure a tight deadline to force a timeout
+	oldDeadline := setBuiltinBackupMysqldDeadline(time.Second)
+	defer setBuiltinBackupMysqldDeadline(oldDeadline)
+
+	bh := filebackupstorage.NewBackupHandle(nil, "", "", false)
+
+	// Spin up a fake daemon to be used in backups. It needs to be allowed to receive:
+	//  "STOP SLAVE", "START SLAVE", in that order.
+	// It also needs to be allowed to receive the query to disable the innodb_fast_shutdown flag.
+	mysqld := mysqlctl.NewFakeMysqlDaemon(fakesqldb.New(t))
+	mysqld.ExpectedExecuteSuperQueryList = []string{"STOP SLAVE", "START SLAVE"}
+	mysqld.FetchSuperQueryMap = map[string]*sqltypes.Result{
+		"SET GLOBAL innodb_fast_shutdown=0": {},
+	}
+
+	ok, err := be.ExecuteBackup(ctx, mysqlctl.BackupParams{
+		Logger: logutil.NewConsoleLogger(),
+		Mysqld: mysqld,
+		Cnf: &mysqlctl.Mycnf{
+			InnodbDataHomeDir:     path.Join(backupRoot, "innodb"),
+			InnodbLogGroupHomeDir: path.Join(backupRoot, "log"),
+			DataDir:               path.Join(backupRoot, "datadir"),
+		},
+		Concurrency: 2,
+		TopoServer:  ts,
+		Keyspace:    keyspace,
+		Shard:       shard,
+		Stats:       backupstats.NewFakeStats(),
+		UpgradeSafe: true,
+	}, bh)
+
+	require.NoError(t, err)
+	assert.True(t, ok)
 }
 
 // TestExecuteBackupWithCanceledContext tests the ability of the backup function to gracefully handle cases where errors

commit 42585fd40c6161618468147bc00209cd263924ee

Fix remote VersionString API (#13484)
* Fix potential nil crash

If this errors, we can't derefence v and read this value. This was
broken in https://github.com/vitessio/vitess/pull/13449

Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

* muysqlctl: Ensure to implement server side API

Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

* mysqlctld: Add endtoend test to verify the version string works

Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

---------

Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

go/vt/mysqlctl/grpcmysqlctlclient/client.go
@@ -116,8 +116,11 @@ func (c *client) VersionString(ctx context.Context) (string, error) {
 	var version string
 	err := c.withRetry(ctx, func() error {
 		r, err := c.c.VersionString(ctx, &mysqlctlpb.VersionStringRequest{})
+		if err != nil {
+			return err
+		}
 		version = r.Version
-		return err
+		return nil
 	})
 	return version, err
 }

go/vt/mysqlctl/grpcmysqlctlserver/server.go
@@ -71,6 +71,15 @@ func (s *server) RefreshConfig(ctx context.Context, request *mysqlctlpb.RefreshC
 	return &mysqlctlpb.RefreshConfigResponse{}, s.mysqld.RefreshConfig(ctx, s.cnf)
 }
 
+// VersionString registers the Server for RPCs.
+func (s *server) VersionString(ctx context.Context, request *mysqlctlpb.VersionStringRequest) (*mysqlctlpb.VersionStringResponse, error) {
+	version, err := s.mysqld.GetVersionString(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &mysqlctlpb.VersionStringResponse{Version: version}, nil
+}
+
 // StartServer registers the Server for RPCs.
 func StartServer(s *grpc.Server, cnf *mysqlctl.Mycnf, mysqld *mysqlctl.Mysqld) {
 	mysqlctlpb.RegisterMysqlCtlServer(s, &server{cnf: cnf, mysqld: mysqld})

go/test/endtoend/cluster/mysqlctld_process.go
@@ -43,17 +43,24 @@ type MysqlctldProcess struct {
 	process            *exec.Cmd
 	exit               chan error
 	InitMysql          bool
+	SocketFile         string
 	exitSignalReceived bool
 }
 
 // InitDb executes mysqlctld command to add cell info
 func (mysqlctld *MysqlctldProcess) InitDb() (err error) {
-	tmpProcess := exec.Command(
-		mysqlctld.Binary,
+	args := []string{
 		"--log_dir", mysqlctld.LogDirectory,
 		"--tablet_uid", fmt.Sprintf("%d", mysqlctld.TabletUID),
 		"--mysql_port", fmt.Sprintf("%d", mysqlctld.MySQLPort),
 		"--init_db_sql_file", mysqlctld.InitDBFile,
+	}
+	if mysqlctld.SocketFile != "" {
+		args = append(args, "--socket_file", mysqlctld.SocketFile)
+	}
+	tmpProcess := exec.Command(
+		mysqlctld.Binary,
+		args...,
 	)
 	return tmpProcess.Run()
 }
@@ -64,11 +71,17 @@ func (mysqlctld *MysqlctldProcess) Start() error {
 		return fmt.Errorf("process is already running")
 	}
 	_ = createDirectory(mysqlctld.LogDirectory, 0700)
-	tempProcess := exec.Command(
-		mysqlctld.Binary,
+	args := []string{
 		"--log_dir", mysqlctld.LogDirectory,
 		"--tablet_uid", fmt.Sprintf("%d", mysqlctld.TabletUID),
 		"--mysql_port", fmt.Sprintf("%d", mysqlctld.MySQLPort),
+	}
+	if mysqlctld.SocketFile != "" {
+		args = append(args, "--socket_file", mysqlctld.SocketFile)
+	}
+	tempProcess := exec.Command(
+		mysqlctld.Binary,
+		args...,
 	)
 
 	tempProcess.Args = append(tempProcess.Args, mysqlctld.ExtraArgs...)

go/test/endtoend/mysqlctld/mysqlctld_test.go
@@ -17,12 +17,16 @@ limitations under the License.
 package mysqlctld
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"os"
+	"path"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+
+	"vitess.io/vitess/go/vt/mysqlctl/mysqlctlclient"
 
 	"vitess.io/vitess/go/test/endtoend/cluster"
 	"vitess.io/vitess/go/vt/sidecardb"
@@ -101,6 +105,7 @@ func initCluster(shardNames []string, totalTabletsRequired int) error {
 			if err != nil {
 				return err
 			}
+			mysqlctldProcess.SocketFile = path.Join(clusterInstance.TmpDirectory, fmt.Sprintf("mysqlctld_%d.sock", tablet.TabletUID))
 			tablet.MysqlctldProcess = *mysqlctldProcess
 			err = tablet.MysqlctldProcess.Start()
 			if err != nil {
@@ -155,4 +160,12 @@ func TestAutoDetect(t *testing.T) {
 	// Reparent tablets, which requires flavor detection
 	err = clusterInstance.VtctlclientProcess.InitializeShard(keyspaceName, shardName, cell, primaryTablet.TabletUID)
 	require.Nil(t, err, "error should be nil")
+}
+
+func TestVersionString(t *testing.T) {
+	client, err := mysqlctlclient.New("unix", primaryTablet.MysqlctldProcess.SocketFile)
+	require.NoError(t, err)
+	version, err := client.VersionString(context.Background())
+	require.NoError(t, err)
+	require.NotEmpty(t, version)
 }