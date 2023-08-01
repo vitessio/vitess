b71f6df3130eb3e1269109391adf72b627c1709a
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2023-08-01 10:58:10 +0300

    removed irrelevant comment
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/ioutil/timeout_closer_test.go
+++ go/ioutil/timeout_closer_test.go
@@ -14,11 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-/*
-MeteredReadCloser and MeteredReader are time-and-byte-tracking wrappers around
-ReadCloser and Reader.
-*/
-
 package ioutil
 
 import (
