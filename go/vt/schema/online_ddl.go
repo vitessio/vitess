1485b956a5711f37528497abff9350eb5d4f062b
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2023-08-03 16:00:22 +0300

    Solve RevertMigration.Comment read/write concurrency issue
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/schema/online_ddl.go
+++ go/vt/schema/online_ddl.go
@@ -269,7 +269,10 @@ func OnlineDDLFromCommentedStatement(stmt sqlparser.Statement) (onlineDDL *Onlin
 	case sqlparser.DDLStatement:
 		comments = stmt.GetParsedComments()
 	case *sqlparser.RevertMigration:
-		comments = stmt.Comments
+		// We clone the comments because they will end up being cached by the query planner. Then, the Directive() function actually modifies the comments.
+		// If comments are shared in cache, and Directive() modifies it, then we have a concurrency issue when someone else wants to read the comments.
+		// By cloning the comments we remove the concurrency problem.
+		comments = sqlparser.CloneRefOfParsedComments(stmt.Comments)
 	default:
 		return nil, vterrors.Errorf(vtrpcpb.Code_INVALID_ARGUMENT, "unsupported statement for Online DDL: %v", sqlparser.String(stmt))
 	}
