c7afb7e45836235d5ea9bce46ac8c846f1ccc25b
Harshit Gangal <harshit@planetscale.com>
2023-08-17 22:44:46 +0530

    test: updated the unit test
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/vt/vtgate/engine/fk_cascade_test.go
+++ go/vt/vtgate/engine/fk_cascade_test.go
@@ -20,29 +20,57 @@ import (
 	"context"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"vitess.io/vitess/go/sqltypes"
 	querypb "vitess.io/vitess/go/vt/proto/query"
+	"vitess.io/vitess/go/vt/vtgate/vindexes"
 )
 
 func TestDeleteCascade(t *testing.T) {
 	fakeRes := sqltypes.MakeTestResult(sqltypes.MakeTestFields("cola|colb", "int64|varchar"), "1|a", "2|b")
 
-	inputP := &fakePrimitive{results: []*sqltypes.Result{fakeRes}}
-	childP := &fakePrimitive{results: []*sqltypes.Result{fakeRes}}
-	parentP := &fakePrimitive{results: []*sqltypes.Result{fakeRes}}
+	inputP := &Route{
+		Query: "select cola, colb from parent where foo = 48",
+		RoutingParameters: &RoutingParameters{
+			Opcode:   Unsharded,
+			Keyspace: &vindexes.Keyspace{Name: "ks"},
+		},
+	}
+	childP := &Delete{
+		DML: &DML{
+			Query: "delete from child where (cola, colb) in ::__vals",
+			RoutingParameters: &RoutingParameters{
+				Opcode:   Unsharded,
+				Keyspace: &vindexes.Keyspace{Name: "ks"},
+			},
+		},
+	}
+	parentP := &Delete{
+		DML: &DML{
+			Query: "delete from parent where foo = 48",
+			RoutingParameters: &RoutingParameters{
+				Opcode:   Unsharded,
+				Keyspace: &vindexes.Keyspace{Name: "ks"},
+			},
+		},
+	}
 	fkc := &FK_Cascade{
 		Input:  inputP,
 		Child:  []Child{{BVName: "__vals", Cols: []int{0, 1}, P: childP}},
 		Parent: parentP,
 	}
 
-	vc := &loggingVCursor{}
+	vc := newDMLTestVCursor("0")
+	vc.results = []*sqltypes.Result{fakeRes}
 	_, err := fkc.TryExecute(context.Background(), vc, map[string]*querypb.BindVariable{}, true)
 	require.NoError(t, err)
-	assert.Equal(t, []string{`Execute  true`}, inputP.log)
-	assert.Equal(t, []string{`Execute __vals: type:TUPLE values:{type:TUPLE values:{type:INT64 value:"1"} values:{type:VARCHAR value:"a"}} values:{type:TUPLE values:{type:INT64 value:"2"} values:{type:VARCHAR value:"b"}} true`}, childP.log)
-	assert.Equal(t, []string{`Execute  true`}, parentP.log)
+	vc.ExpectLog(t, []string{
+		`ResolveDestinations ks [] Destinations:DestinationAllShards()`,
+		`ExecuteMultiShard ks.0: select cola, colb from parent where foo = 48 {} false false`,
+		`ResolveDestinations ks [] Destinations:DestinationAllShards()`,
+		`ExecuteMultiShard ks.0: delete from child where (cola, colb) in ::__vals {__vals: type:TUPLE values:{type:TUPLE values:{type:INT64 value:"1"} values:{type:VARCHAR value:"a"}} values:{type:TUPLE values:{type:INT64 value:"2"} values:{type:VARCHAR value:"b"}}} true true`,
+		`ResolveDestinations ks [] Destinations:DestinationAllShards()`,
+		`ExecuteMultiShard ks.0: delete from parent where foo = 48 {} true true`,
+	})
 }
