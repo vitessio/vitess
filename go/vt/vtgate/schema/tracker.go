63c5d0c95af85f10418392cc80cfc96f76bf0e2c
Dirkjan Bussink <d.bussink@gmail.com>
2023-08-29 13:14:20 +0200

    Fix setup order to avoid races (#13871)
    Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

--- go/vt/vtgate/schema/tracker.go
+++ go/vt/vtgate/schema/tracker.go
@@ -151,6 +151,10 @@ func (t *Tracker) Start() {
 		for {
 			select {
 			case th := <-t.ch:
+				if th == nil {
+					// channel closed
+					return
+				}
 				ksUpdater := t.getKeyspaceUpdateController(th)
 				ksUpdater.add(th)
 			case <-ctx.Done():
