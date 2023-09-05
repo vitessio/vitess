920d9e7f94832b27ad6aac6e1b4d3c27d631d096
Andrew Mason <andrew@planetscale.com>
2023-09-05 14:43:28 -0400

    remove unused shim code
    Signed-off-by: Andrew Mason <andrew@planetscale.com>

--- go/cmd/vtorc/main.go
+++ go/cmd/vtorc/main.go
@@ -19,11 +19,9 @@ package main
 import (
 	"flag"
 	"fmt"
-	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
 	_ "modernc.org/sqlite"
 
 	"vitess.io/vitess/go/acl"
@@ -37,63 +35,6 @@ import (
 	"vitess.io/vitess/go/vt/vtorc/logic"
 	"vitess.io/vitess/go/vt/vtorc/server"
 )
-
-// transformArgsForPflag turns a slice of raw args passed on the command line,
-// possibly incompatible with pflag (because the user is expecting stdlib flag
-// parsing behavior) and transforms them into the arguments that should have
-// been passed to conform to pflag parsing behavior.
-//
-// the primary function is to catch any cases where the user specified a longopt
-// with only a single hyphen (e.g. `-myflag`) and correct it to be
-// double-hyphenated.
-//
-// note that this transformation does _not_ actually validate the arguments; for
-// example if the user specifies `--myflag`, but the FlagSet has no such flag
-// defined, that will still appear in the returned result and will (correctly)
-// cause a parse error later on in `main`, at which point the CLI usage will
-// be printed.
-//
-// note also that this transformation is incomplete. pflag allows interspersing
-// of flag and positional arguments, whereas stdlib flag does not. however, for
-// vtorc specifically, with the exception of `vtorc help <topic>`, the CLI only
-// consumes flag arguments (in other words, there are no supported subcommands),
-// so this is a non-issue, and is not implemented here in order to make this
-// function a bit simpler.
-func transformArgsForPflag(fs *pflag.FlagSet, args []string) (result []string) {
-	for i, arg := range args {
-		switch {
-		case arg == "--":
-			// pflag stops parsing at `--`, so we're done transforming the CLI
-			// arguments. Just append everything remaining and be done.
-			result = append(result, args[i:]...)
-			return result
-		case strings.HasPrefix(arg, "--"):
-			// Long-hand flag. Append it and continue.
-			result = append(result, arg)
-		case strings.HasPrefix(arg, "-"):
-			// Most complex case. This is either:
-			// 1. A legacy long-hand flag that needs a double-dash (e.g. `-myflag` => `--myflag`).
-			// 2. One _or more_ pflag shortopts all shoved together (think `rm -rf` as `rm -r -f`).
-			//
-			// In the latter case, we don't need to do any transformations, but
-			// in the former, we do.
-			name := strings.SplitN(arg[1:], "=", 2)[0] // discard any potential value (`-myflag` and `-myflag=10` both have the name of `myflag`)
-			if fs.Lookup(name) != nil || name == "help" {
-				// Case 1: We have a long opt with this name, so we need to
-				// prepend an additional hyphen.
-				result = append(result, "-"+arg)
-			} else {
-				// Case 2: No transformation needed.
-				result = append(result, arg)
-			}
-		default:
-			// Just a flag argument. Nothing to transform.
-			result = append(result, arg)
-		}
-	}
-
-	return result
-}
 
 var (
 	configFile string
