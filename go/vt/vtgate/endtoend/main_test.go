63e69528aff9cc844cb80d222774895977781843
pbibra <pbibra@slack-corp.com>
2023-09-12 12:05:40 -0700

    apply vcopy patch 11103 (#128)
    Signed-off-by: Priya Bibra <pbibra@slack-corp.com>

--- go/vt/vtgate/endtoend/main_test.go
+++ go/vt/vtgate/endtoend/main_test.go
@@ -45,6 +45,12 @@ create table t1(
 	primary key(id1)
 ) Engine=InnoDB;
 
+create table t1_copy_resume(
+	id1 bigint,
+	id2 bigint,
+	primary key(id1)
+) Engine=InnoDB;
+
 create table t1_id2_idx(
 	id2 bigint,
 	keyspace_id varbinary(10),
@@ -131,6 +137,12 @@ create table t1_sharded(
 				}, {
 					Column: "id2",
 					Name:   "t1_id2_vdx",
+				}},
+			},
+			"t1_copy_resume": {
+				ColumnVindexes: []*vschemapb.ColumnVindex{{
+					Column: "id1",
+					Name:   "hash",
 				}},
 			},
 			"t1_sharded": {
