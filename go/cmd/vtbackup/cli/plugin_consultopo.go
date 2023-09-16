2c969f2b950dd94233effff702f8ce71db1d7f57
Andrew Mason <andrew@planetscale.com>
2023-09-16 12:36:20 -0400

    migrate vtbackup to cobra
    Signed-off-by: Andrew Mason <andrew@planetscale.com>

--- go/cmd/vtbackup/cli/plugin_consultopo.go
+++ go/cmd/vtbackup/cli/plugin_consultopo.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 The Vitess Authors.
+Copyright 2019 The Vitess Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,8 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package main
+package cli
 
 import (
-	_ "vitess.io/vitess/go/vt/mysqlctl/azblobbackupstorage"
+	_ "vitess.io/vitess/go/vt/topo/consultopo"
 )
--- go/cmd/vtbackup/cli/plugin_s3backupstorage.go
+++ go/cmd/vtbackup/cli/plugin_s3backupstorage.go
@@ -13,8 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-package main
+package cli
 
 import (
-	_ "vitess.io/vitess/go/vt/mysqlctl/gcsbackupstorage"
+	_ "vitess.io/vitess/go/vt/mysqlctl/s3backupstorage"
 )
