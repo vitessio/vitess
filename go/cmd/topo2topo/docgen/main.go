5e634afc2648d25ae332fce3bace98154f2736c1
Andrew Mason <andrew@planetscale.com>
2023-09-21 10:03:28 -0400

    migrate topo2topo to cobra
    Signed-off-by: Andrew Mason <andrew@planetscale.com>

--- go/cmd/topo2topo/docgen/main.go
+++ go/cmd/topo2topo/docgen/main.go
@@ -1,11 +1,11 @@
 /*
-Copyright 2019 The Vitess Authors.
+Copyright 2023 The Vitess Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,15 +17,21 @@ limitations under the License.
 package main
 
 import (
+	"github.com/spf13/cobra"
+
+	"vitess.io/vitess/go/cmd/internal/docgen"
 	"vitess.io/vitess/go/cmd/topo2topo/cli"
-	"vitess.io/vitess/go/exit"
-	"vitess.io/vitess/go/vt/log"
 )
 
 func main() {
-	defer exit.RecoverAll()
-
-	if err := cli.Main.Execute(); err != nil {
-		log.Exitf("%s", err)
+	var dir string
+	cmd := cobra.Command{
+		Use: "docgen [-d <dir>]",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return docgen.GenerateMarkdownTree(cli.Main, dir)
+		},
 	}
+
+	cmd.Flags().StringVarP(&dir, "dir", "d", "doc", "output directory to write documentation")
+	_ = cmd.Execute()
 }
