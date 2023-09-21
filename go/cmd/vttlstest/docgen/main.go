8054456927872e15ceca0caf211eb0519998327e
Andrew Mason <andrew@planetscale.com>
2023-09-21 15:40:27 -0400

    migrate vttlstest to cobra
    Signed-off-by: Andrew Mason <andrew@planetscale.com>

--- go/cmd/vttlstest/docgen/main.go
+++ go/cmd/vttlstest/docgen/main.go
@@ -1,13 +1,13 @@
 /*
-Copyright 2019 The Vitess Authors.
+Copyright 2023 The Vitess Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreedto in writing, software
+Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
@@ -19,14 +19,19 @@ package main
 import (
 	"github.com/spf13/cobra"
 
+	"vitess.io/vitess/go/cmd/internal/docgen"
 	"vitess.io/vitess/go/cmd/vttlstest/cli"
-	"vitess.io/vitess/go/exit"
-	"vitess.io/vitess/go/vt/logutil"
 )
 
 func main() {
-	defer exit.Recover()
-	defer logutil.Flush()
+	var dir string
+	cmd := cobra.Command{
+		Use: "docgen [-d <dir>]",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return docgen.GenerateMarkdownTree(cli.Root, dir)
+		},
+	}
 
-	cobra.CheckErr(cli.Root.Execute())
+	cmd.Flags().StringVarP(&dir, "dir", "d", "doc", "output directory to write documentation")
+	_ = cmd.Execute()
 }
