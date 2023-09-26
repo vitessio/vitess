d874a73f2d937a193c6cc55fed9e3bff4432ab0c
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2023-09-26 12:43:48 +0300

    adding a onlineddl_vrepl_suite test
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
    Signed-off-by: Olga Shestopalova <oshestopalova@hubspot.com>

--- go/test/endtoend/onlineddl/vrepl_suite/onlineddl_vrepl_suite_test.go
+++ go/test/endtoend/onlineddl/vrepl_suite/onlineddl_vrepl_suite_test.go
@@ -58,7 +58,7 @@ var (
 )
 
 const (
-	testDataPath   = "testdata"
+	testDataPath   = "tmptestdata"
 	defaultSQLMode = "ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION"
 )
 
