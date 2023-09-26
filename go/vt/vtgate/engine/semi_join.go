ba8ea1009f22fd87594d4729a38ba33bd46143b3
Andres Taylor <andres@planetscale.com>
2023-09-26 17:45:36 +0200

    make sure the engine primitive handles the new plans correctly
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/vt/vtgate/engine/semi_join.go
+++ go/vt/vtgate/engine/semi_join.go
@@ -151,6 +151,9 @@ func projectFields(lfields []*querypb.Field, cols []int) []*querypb.Field {
 	if lfields == nil {
 		return nil
 	}
+	if len(cols) == 0 {
+		return lfields
+	}
 	fields := make([]*querypb.Field, len(cols))
 	for i, index := range cols {
 		fields[i] = lfields[-index-1]
@@ -159,6 +162,9 @@ func projectFields(lfields []*querypb.Field, cols []int) []*querypb.Field {
 }
 
 func projectRows(lrow []sqltypes.Value, cols []int) []sqltypes.Value {
+	if len(cols) == 0 {
+		return lrow
+	}
 	row := make([]sqltypes.Value, len(cols))
 	for i, index := range cols {
 		if index < 0 {
