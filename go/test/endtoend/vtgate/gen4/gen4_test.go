7ab4d8c81ba7abb80c840086f692cf98a99f9f4b
Andres Taylor <andres@planetscale.com>
2023-09-27 07:13:11 +0200

    remove another query with subq in outer join condition
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/test/endtoend/vtgate/gen4/gen4_test.go
+++ go/test/endtoend/vtgate/gen4/gen4_test.go
@@ -162,13 +162,6 @@ func TestSubQueries(t *testing.T) {
 	// inserting some data in u_a
 	utils.Exec(t, mcmp.VtConn, `insert into u_a(id, a) values (1, 1)`)
 
-	// execute same query again.
-	qr := utils.Exec(t, mcmp.VtConn, `select u_a.a from u_a left join t2 on t2.id IN (select id from t2)`)
-	assert.EqualValues(t, 8, len(qr.Rows))
-	for index, row := range qr.Rows {
-		assert.EqualValues(t, `[INT64(1)]`, fmt.Sprintf("%v", row), "does not match for row: %d", index+1)
-	}
-
 	// fail as projection subquery is not scalar
 	_, err := utils.ExecAllowError(t, mcmp.VtConn, `select (select id from t2) from t2 order by id`)
 	assert.EqualError(t, err, "subquery returned more than one row (errno 1105) (sqlstate HY000) during query: select (select id from t2) from t2 order by id")
