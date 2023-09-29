aa92d9acb8d16d2d241470e9dd83c41c48f6203b
Florent Poinsard <florent.poinsard@outlook.fr>
2023-09-29 10:17:16 -0600

    remove --vtctld_addr usages as it is deprecated
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- examples/common/scripts/vttablet-up.sh
+++ examples/common/scripts/vttablet-up.sh
@@ -53,7 +53,6 @@ vttablet \
  --grpc_port $grpc_port \
  --service_map 'grpc-queryservice,grpc-tabletmanager,grpc-updatestream' \
  --pid_file $VTDATAROOT/$tablet_dir/vttablet.pid \
- --vtctld_addr http://$hostname:$vtctld_web_port/ \
  --heartbeat_enable --heartbeat_interval=250ms --heartbeat_on_demand_duration=5s \
  > $VTDATAROOT/$tablet_dir/vttablet.out 2>&1 &
 
