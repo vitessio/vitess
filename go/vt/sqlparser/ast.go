314ebcf13923f98945595208d5099eca4a7184ea
Andrés Taylor <andres@planetscale.com>
2023-10-14 10:17:41 +0200

    bugfix: use the proper interface for comment directives (#14267)

--- go/vt/sqlparser/ast.go
+++ go/vt/sqlparser/ast.go
@@ -1388,6 +1388,14 @@ func (node *ExplainStmt) GetParsedComments() *ParsedComments {
 
 // GetParsedComments implements Commented interface.
 func (node *VExplainStmt) GetParsedComments() *ParsedComments {
+	if node.Comments == nil {
+		cmt, ok := node.Statement.(Commented)
+		if !ok {
+			return nil
+		}
+		return cmt.GetParsedComments()
+	}
+
 	return node.Comments
 }
 
