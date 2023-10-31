eddb7da44636994d1064906ca20d06543e6044a0
Andrés Taylor <andres@planetscale.com>
2023-10-31 07:47:43 +0100

    Add support for common table expressions (#14321)

--- go/vt/vtgate/planbuilder/select.go
+++ go/vt/vtgate/planbuilder/select.go
@@ -38,17 +38,6 @@ func gen4SelectStmtPlanner(
 	reservedVars *sqlparser.ReservedVars,
 	vschema plancontext.VSchema,
 ) (*planResult, error) {
-	switch node := stmt.(type) {
-	case *sqlparser.Select:
-		if node.With != nil {
-			return nil, vterrors.VT12001("WITH expression in SELECT statement")
-		}
-	case *sqlparser.Union:
-		if node.With != nil {
-			return nil, vterrors.VT12001("WITH expression in UNION statement")
-		}
-	}
-
 	sel, isSel := stmt.(*sqlparser.Select)
 	if isSel {
 		// handle dual table for processing at vtgate.
