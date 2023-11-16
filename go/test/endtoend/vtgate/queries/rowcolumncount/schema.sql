fb8abb525d686b951768cc059f831360181df94b
Prakhar Gurunani <prakhargurunani@gmail.com>
2023-11-16 21:39:54 +0000

    add dml tests
    Signed-off-by: Prakhar Gurunani <prakhargurunani@gmail.com>

--- go/test/endtoend/vtgate/queries/rowcolumncount/schema.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists a(
-                   one int,
-                   two int,
-                   three int,
-                   four int,
-                   five int,
-                   primary key(one)
-) Engine=InnoDB;
