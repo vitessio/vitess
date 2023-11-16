b1518d4ddf1b4895c37ea92211f2c6ce9cdcec66
Vicent Marti <vmg@strn.cat>
2023-11-16 18:06:56 +0100

    evalengine: hide the fields for Type
    Signed-off-by: Vicent Marti <vmg@strn.cat>

--- go/vt/vtgate/evalengine/api_coerce.go
+++ go/vt/vtgate/evalengine/api_coerce.go
@@ -36,63 +36,63 @@ func CoerceTypes(v1, v2 Type) (out Type, err error) {
 	if v1 == v2 {
 		return v1, nil
 	}
-	if sqltypes.IsNull(v1.Type) || sqltypes.IsNull(v2.Type) {
-		return Type{Type: sqltypes.Null, Coll: collations.CollationBinaryID}, nil
-	}
-	fail := func() error {
-		return vterrors.Errorf(vtrpcpb.Code_UNIMPLEMENTED, "types does not support hashcode yet: %v vs %v", v1.Type, v2.Type)
+	if sqltypes.IsNull(v1.Type()) || sqltypes.IsNull(v2.Type()) {
+		return NewType(sqltypes.Null, collations.CollationBinaryID), nil
 	}
 
-	out = Type{NotNullable: v1.NotNullable && v2.NotNullable}
+	out = Type{
+		init:     true,
+		nullable: v1.Nullable() || v2.Nullable(),
+	}
 
 	switch {
-	case sqltypes.IsTextOrBinary(v1.Type) && sqltypes.IsTextOrBinary(v2.Type):
-		out.Type = sqltypes.VarChar
-		mergedCollation, _, _, ferr := mergeCollations(typedCoercionCollation(v1.Type, v1.Coll), typedCoercionCollation(v2.Type, v2.Coll), v1.Type, v2.Type)
+	case sqltypes.IsTextOrBinary(v1.Type()) && sqltypes.IsTextOrBinary(v2.Type()):
+		mergedCollation, _, _, ferr := mergeCollations(typedCoercionCollation(v1.Type(), v1.Collation()), typedCoercionCollation(v2.Type(), v2.Collation()), v1.Type(), v2.Type())
 		if err != nil {
 			return Type{}, ferr
 		}
-		out.Coll = mergedCollation.Collation
+		out.collation = mergedCollation.Collation
+		out.typ = sqltypes.VarChar
 		return
 
-	case sqltypes.IsDateOrTime(v1.Type):
-		out.Coll = collations.CollationBinaryID
-		out.Type = v1.Type
+	case sqltypes.IsDateOrTime(v1.Type()):
+		out.collation = collations.CollationBinaryID
+		out.typ = v1.Type()
 		return
 
-	case sqltypes.IsDateOrTime(v2.Type):
-		out.Coll = collations.CollationBinaryID
-		out.Type = v2.Type
+	case sqltypes.IsDateOrTime(v2.Type()):
+		out.collation = collations.CollationBinaryID
+		out.typ = v2.Type()
 		return
 
-	case sqltypes.IsNumber(v1.Type) || sqltypes.IsNumber(v2.Type):
-		out.Coll = collations.CollationBinaryID
+	case sqltypes.IsNumber(v1.Type()) || sqltypes.IsNumber(v2.Type()):
+		out.collation = collations.CollationBinaryID
 		switch {
-		case sqltypes.IsTextOrBinary(v1.Type) || sqltypes.IsFloat(v1.Type) || sqltypes.IsDecimal(v1.Type) ||
-			sqltypes.IsTextOrBinary(v2.Type) || sqltypes.IsFloat(v2.Type) || sqltypes.IsDecimal(v2.Type):
-			out.Type = sqltypes.Float64
+		case sqltypes.IsTextOrBinary(v1.Type()) || sqltypes.IsFloat(v1.Type()) || sqltypes.IsDecimal(v1.Type()) ||
+			sqltypes.IsTextOrBinary(v2.Type()) || sqltypes.IsFloat(v2.Type()) || sqltypes.IsDecimal(v2.Type()):
+			out.typ = sqltypes.Float64
 			return
-		case sqltypes.IsSigned(v1.Type):
+		case sqltypes.IsSigned(v1.Type()):
 			switch {
-			case sqltypes.IsUnsigned(v2.Type):
-				out.Type = sqltypes.Uint64
+			case sqltypes.IsUnsigned(v2.Type()):
+				out.typ = sqltypes.Uint64
 				return
-			case sqltypes.IsSigned(v2.Type):
-				out.Type = sqltypes.Int64
+			case sqltypes.IsSigned(v2.Type()):
+				out.typ = sqltypes.Int64
 				return
 			default:
-				return Type{}, fail()
+				return Type{}, vterrors.Errorf(vtrpcpb.Code_UNIMPLEMENTED, "cannot coerce SIGNED into %v", v2.Type())
 			}
-		case sqltypes.IsUnsigned(v1.Type):
+		case sqltypes.IsUnsigned(v1.Type()):
 			switch {
-			case sqltypes.IsSigned(v2.Type) || sqltypes.IsUnsigned(v2.Type):
-				out.Type = sqltypes.Uint64
+			case sqltypes.IsSigned(v2.Type()) || sqltypes.IsUnsigned(v2.Type()):
+				out.typ = sqltypes.Uint64
 				return
 			default:
-				return Type{}, fail()
+				return Type{}, vterrors.Errorf(vtrpcpb.Code_UNIMPLEMENTED, "cannot coerce UNSIGNED into %v", v2.Type())
 			}
 		}
 	}
 
-	return Type{}, fail()
+	return Type{}, vterrors.Errorf(vtrpcpb.Code_UNIMPLEMENTED, "cannot coerce %v into %v", v1.Type(), v2.Type())
 }
