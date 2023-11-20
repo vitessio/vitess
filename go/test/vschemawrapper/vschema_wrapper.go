bda4f9a508240051047bd9f117a2831921ea5fa9
Manan Gupta <35839558+GuptaManan100@users.noreply.github.com>
2023-11-20 14:05:17 +0530

    Make `Foreign_key_checks` a Vitess Aware variable (#14484)
    Signed-off-by: Manan Gupta <manan@planetscale.com>

--- go/test/vschemawrapper/vschema_wrapper.go
+++ go/test/vschemawrapper/vschema_wrapper.go
@@ -40,14 +40,15 @@ import (
 var _ plancontext.VSchema = (*VSchemaWrapper)(nil)
 
 type VSchemaWrapper struct {
-	V             *vindexes.VSchema
-	Keyspace      *vindexes.Keyspace
-	TabletType_   topodatapb.TabletType
-	Dest          key.Destination
-	SysVarEnabled bool
-	Version       plancontext.PlannerVersion
-	EnableViews   bool
-	TestBuilder   func(query string, vschema plancontext.VSchema, keyspace string) (*engine.Plan, error)
+	V                     *vindexes.VSchema
+	Keyspace              *vindexes.Keyspace
+	TabletType_           topodatapb.TabletType
+	Dest                  key.Destination
+	SysVarEnabled         bool
+	ForeignKeyChecksState *bool
+	Version               plancontext.PlannerVersion
+	EnableViews           bool
+	TestBuilder           func(query string, vschema plancontext.VSchema, keyspace string) (*engine.Plan, error)
 }
 
 func (vw *VSchemaWrapper) GetPrepareData(stmtName string) *vtgatepb.PrepareData {
@@ -138,6 +139,10 @@ func (vw *VSchemaWrapper) ForeignKeyMode(keyspace string) (vschemapb.Keyspace_Fo
 
 func (vw *VSchemaWrapper) KeyspaceError(keyspace string) error {
 	return nil
+}
+
+func (vw *VSchemaWrapper) GetForeignKeyChecksState() *bool {
+	return vw.ForeignKeyChecksState
 }
 
 func (vw *VSchemaWrapper) AllKeyspace() ([]*vindexes.Keyspace, error) {
