e1f03ef086f81b57eeaf00ef28c5eb50d3acad4c
Andrew Mason <andrew@planetscale.com>
2023-12-08 12:21:30 -0500

    fixup! fix test flags
    Signed-off-by: Andrew Mason <andrew@planetscale.com>

--- go/test/endtoend/reparent/prscomplex/main_test.go
+++ go/test/endtoend/reparent/prscomplex/main_test.go
@@ -67,8 +67,8 @@ func TestMain(m *testing.M) {
 			"--queryserver-config-pool-size=3",
 			"--queryserver-config-stream-pool-size=3",
 			"--queryserver-config-transaction-cap=2",
-			"--queryserver-config-transaction-timeout=20",
-			"--shutdown_grace_period=3",
+			"--queryserver-config-transaction-timeout=20s",
+			"--shutdown_grace_period=3s",
 			"--queryserver-config-schema-change-signal=false")
 		err = clusterInstance.StartUnshardedKeyspace(*keyspace, 1, false)
 		if err != nil {
