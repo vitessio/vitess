9d4c726a116ae7733cf4d356573c719505399197
Manan Gupta <35839558+GuptaManan100@users.noreply.github.com>
2023-12-14 14:21:10 +0530

    Enable verbose logging for some more RPCs (#14770)
    Signed-off-by: Manan Gupta <manan@planetscale.com>

--- go/vt/vttablet/tmrpctest/test_tm_rpc.go
+++ go/vt/vttablet/tmrpctest/test_tm_rpc.go
@@ -1039,7 +1039,12 @@ func tmRPCTestPopulateReparentJournal(ctx context.Context, t *testing.T, client 
 
 func tmRPCTestPopulateReparentJournalPanic(ctx context.Context, t *testing.T, client tmclient.TabletManagerClient, tablet *topodatapb.Tablet) {
 	err := client.PopulateReparentJournal(ctx, tablet, testTimeCreatedNS, testActionName, testPrimaryAlias, testReplicationPosition)
-	expectHandleRPCPanic(t, "PopulateReparentJournal", false /*verbose*/, err)
+	expectHandleRPCPanic(t, "PopulateReparentJournal", true /*verbose*/, err)
+}
+
+func tmRPCTestWaitForPositionPanic(ctx context.Context, t *testing.T, client tmclient.TabletManagerClient, tablet *topodatapb.Tablet) {
+	err := client.WaitForPosition(ctx, tablet, testReplicationPosition)
+	expectHandleRPCPanic(t, "WaitForPosition", true /*verbose*/, err)
 }
 
 var testInitReplicaCalled = false
@@ -1447,6 +1452,7 @@ func Run(t *testing.T, client tmclient.TabletManagerClient, tablet *topodatapb.T
 	tmRPCTestResetReplicationPanic(ctx, t, client, tablet)
 	tmRPCTestInitPrimaryPanic(ctx, t, client, tablet)
 	tmRPCTestPopulateReparentJournalPanic(ctx, t, client, tablet)
+	tmRPCTestWaitForPositionPanic(ctx, t, client, tablet)
 	tmRPCTestDemotePrimaryPanic(ctx, t, client, tablet)
 	tmRPCTestUndoDemotePrimaryPanic(ctx, t, client, tablet)
 	tmRPCTestSetReplicationSourcePanic(ctx, t, client, tablet)
