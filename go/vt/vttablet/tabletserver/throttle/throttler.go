f8968e77f75016a19dd55d0f655f3a7bc84aeec7
Rohit Nayak <57520317+rohit-nayak-ps@users.noreply.github.com>
2023-12-20 14:18:14 +0100

    Remove some logs that are logging excessively on large clusters (#14825)
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>

--- go/vt/vttablet/tabletserver/throttle/throttler.go
+++ go/vt/vttablet/tabletserver/throttle/throttler.go
@@ -355,7 +355,6 @@ func (throttler *Throttler) normalizeThrottlerConfig(throttlerConfig *topodatapb
 }
 
 func (throttler *Throttler) WatchSrvKeyspaceCallback(srvks *topodatapb.SrvKeyspace, err error) bool {
-	log.Infof("Throttler: WatchSrvKeyspaceCallback called with: %+v", srvks)
 	if err != nil {
 		log.Errorf("WatchSrvKeyspaceCallback error: %v", err)
 		return false
@@ -366,7 +365,6 @@ func (throttler *Throttler) WatchSrvKeyspaceCallback(srvks *topodatapb.SrvKeyspa
 		// Throttler is enabled and we should apply the config change
 		// through Operate() or else we get into race conditions.
 		go func() {
-			log.Infof("Throttler: submitting a throttler config apply message with: %+v", throttlerConfig)
 			throttler.throttlerConfigChan <- throttlerConfig
 		}()
 	} else {
