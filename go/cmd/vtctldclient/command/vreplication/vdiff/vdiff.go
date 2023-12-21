9ebc9014576adf72ba3bf2ce1804b605dded2267
Matt Lord <mattalord@gmail.com>
2023-12-21 16:05:13 -0500

    Address review comments
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/cmd/vtctldclient/command/vreplication/vdiff/vdiff.go
+++ go/cmd/vtctldclient/command/vreplication/vdiff/vdiff.go
@@ -885,7 +885,7 @@ func registerCommands(root *cobra.Command) {
 	create.Flags().DurationVar(&createOptions.WaitUpdateInterval, "wait-update-interval", time.Duration(1*time.Minute), "When waiting on a vdiff to finish, check and display the current status this often.")
 	create.Flags().BoolVar(&createOptions.AutoRetry, "auto-retry", true, "Should this vdiff automatically retry and continue in case of recoverable errors.")
 	create.Flags().BoolVar(&createOptions.UpdateTableStats, "update-table-stats", false, "Update the table statistics, using ANALYZE TABLE, on each table involved in the VDiff during initialization. This will ensure that progress estimates are as accurate as possible -- but it does involve locks and can potentially impact query processing on the target keyspace.")
-	create.Flags().DurationVar(&createOptions.MaxDiffDuration, "max-diff-duration", time.Duration(0), "How long should an individual table diff run before being stopped and restarted in order to lessen the impact on tablets due to holding open database snapshots for long periods of time (0 is the default and means no time limit).")
+	create.Flags().DurationVar(&createOptions.MaxDiffDuration, "max-diff-duration", 0, "How long should an individual table diff run before being stopped and restarted in order to lessen the impact on tablets due to holding open database snapshots for long periods of time (0 is the default and means no time limit).")
 	base.AddCommand(create)
 
 	base.AddCommand(delete)
