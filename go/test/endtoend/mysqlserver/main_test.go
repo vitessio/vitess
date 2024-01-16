100357432e1905852a5a9d599c1244e1db1bee5f
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2024-01-16 09:14:47 +0200

    useDB rather than 'USE ...' statement
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/test/endtoend/mysqlserver/main_test.go
+++ go/test/endtoend/mysqlserver/main_test.go
@@ -144,7 +144,7 @@ func TestMain(m *testing.M) {
 		}
 
 		primaryTabletProcess := clusterInstance.Keyspaces[0].Shards[0].PrimaryTablet().VttabletProcess
-		if _, err := primaryTabletProcess.QueryTablet(createProcSQL, keyspaceName, false); err != nil {
+		if _, err := primaryTabletProcess.QueryTablet(createProcSQL, keyspaceName, true); err != nil {
 			return 1, err
 		}
 
