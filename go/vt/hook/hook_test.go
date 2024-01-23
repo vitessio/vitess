4b106a296f835be21de5fd1f513a68ad8d8c47f4
Manik Rana <manikrana54@gmail.com>
2024-01-23 12:28:27 +0530

    tests: add test for func String
    Signed-off-by: Manik Rana <manikrana54@gmail.com>

--- go/vt/hook/hook_test.go
+++ go/vt/hook/hook_test.go
@@ -38,3 +38,69 @@ func TestExecuteContext(t *testing.T) {
 	hr = h.Execute()
 	assert.Equal(t, HOOK_SUCCESS, hr.ExitStatus)
 }
+
+func TestString(t *testing.T) {
+	tt := []struct {
+		name     string
+		input    HookResult
+		expected string
+	}{
+		{
+			name:     "HOOK_SUCCESS",
+			input:    HookResult{ExitStatus: HOOK_SUCCESS, Stdout: "output"},
+			expected: "result: HOOK_SUCCESS\nstdout:\noutput",
+		},
+		{
+			name:     "HOOK_DOES_NOT_EXIST",
+			input:    HookResult{ExitStatus: HOOK_DOES_NOT_EXIST},
+			expected: "result: HOOK_DOES_NOT_EXIST",
+		},
+		{
+			name:     "HOOK_STAT_FAILED",
+			input:    HookResult{ExitStatus: HOOK_STAT_FAILED},
+			expected: "result: HOOK_STAT_FAILED",
+		},
+		{
+			name:     "HOOK_CANNOT_GET_EXIT_STATUS",
+			input:    HookResult{ExitStatus: HOOK_CANNOT_GET_EXIT_STATUS},
+			expected: "result: HOOK_CANNOT_GET_EXIT_STATUS",
+		},
+		{
+			name:     "HOOK_INVALID_NAME",
+			input:    HookResult{ExitStatus: HOOK_INVALID_NAME},
+			expected: "result: HOOK_INVALID_NAME",
+		},
+		{
+			name:     "HOOK_VTROOT_ERROR",
+			input:    HookResult{ExitStatus: HOOK_VTROOT_ERROR},
+			expected: "result: HOOK_VTROOT_ERROR",
+		},
+		{
+			name:     "case default",
+			input:    HookResult{ExitStatus: 42},
+			expected: "result: exit(42)",
+		},
+		{
+			name:     "WithStderr",
+			input:    HookResult{ExitStatus: HOOK_SUCCESS, Stderr: "error"},
+			expected: "result: HOOK_SUCCESS\nstderr:\nerror",
+		},
+		{
+			name:     "WithStderr",
+			input:    HookResult{ExitStatus: HOOK_SUCCESS, Stderr: "error"},
+			expected: "result: HOOK_SUCCESS\nstderr:\nerror",
+		},
+		{
+			name:     "WithStdoutAndStderr",
+			input:    HookResult{ExitStatus: HOOK_SUCCESS, Stdout: "output", Stderr: "error"},
+			expected: "result: HOOK_SUCCESS\nstdout:\noutput\nstderr:\nerror",
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			result := tc.input.String()
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
