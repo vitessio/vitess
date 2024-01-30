ef565e678d9c5c44c5ffba8174904de55b7e9185
Harshit Gangal <harshit@planetscale.com>
2024-01-30 23:45:06 +0530

    addressed todo
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/vt/vtgate/planbuilder/delete.go
+++ go/vt/vtgate/planbuilder/delete.go
@@ -140,9 +140,8 @@ func checkIfDeleteSupported(del *sqlparser.Delete, semTable *semantics.SemTable)
 	}
 
 	// Delete is only supported for single Target.
-	// TODO: Fix error message based on whether sharded keyspace or foreign keys.
 	if len(del.Targets) > 1 {
-		return vterrors.VT12001("multi-table DELETE statement in a sharded keyspace")
+		return vterrors.VT12001("multi-table DELETE statement with multi-target")
 	}
 
 	err := sqlparser.Walk(func(node sqlparser.SQLNode) (kontinue bool, err error) {
