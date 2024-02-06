fdf38b2c203968650e58e245ea429fc063cb44a7
Harshit Gangal <harshit@planetscale.com>
2024-02-06 20:43:47 +0530

    fix: ignore internal tables in schema tracking (#15141)
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/vt/vtgate/vschema_manager.go
+++ go/vt/vtgate/vschema_manager.go
@@ -22,6 +22,7 @@ import (
 
 	"vitess.io/vitess/go/vt/log"
 	topodatapb "vitess.io/vitess/go/vt/proto/topodata"
+	"vitess.io/vitess/go/vt/schema"
 	"vitess.io/vitess/go/vt/sqlparser"
 	"vitess.io/vitess/go/vt/srvtopo"
 	"vitess.io/vitess/go/vt/topo"
@@ -206,6 +207,10 @@ func (vm *VSchemaManager) updateFromSchema(vschema *vindexes.VSchema) {
 		// in the tables.
 		for tblName, tblInfo := range m {
 			for _, fkDef := range tblInfo.ForeignKeys {
+				// Ignore internal tables as part of foreign key references.
+				if schema.IsInternalOperationTableName(fkDef.ReferenceDefinition.ReferencedTable.Name.String()) {
+					continue
+				}
 				parentTbl, err := vschema.FindRoutedTable(ksName, fkDef.ReferenceDefinition.ReferencedTable.Name.String(), topodatapb.TabletType_PRIMARY)
 				if err != nil {
 					log.Errorf("error finding parent table %s: %v", fkDef.ReferenceDefinition.ReferencedTable.Name.String(), err)
