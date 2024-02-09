91481b966313e8a4a824041d847ee3c7026f4e11
Rohit Nayak <rohit@planetscale.com>
2024-02-09 21:28:18 +0100

    Protect map access with mutexes in e2e test. Make function scope private to tests.
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>

--- go/test/endtoend/vreplication/multi_tenant_test.go
+++ go/test/endtoend/vreplication/multi_tenant_test.go
@@ -3,6 +3,7 @@ package vreplication
 import (
 	"encoding/json"
 	"fmt"
+	"sync"
 	"testing"
 
 	"google.golang.org/protobuf/encoding/protojson"
@@ -30,15 +31,16 @@ const (
 
 type multiTenantMigration struct {
 	t                     *testing.T
+	mu                    sync.Mutex
 	tenantMigrationStatus map[int]tenantMigrationStatus
 	activeMoveTables      map[int]*VtctldMoveTables
 
 	targetKeyspace     string
 	tables             string
 	tenantIdColumnName string
-}
 
-var lastIDs = make(map[int]int)
+	lastIDs map[int]int
+}
 
 const (
 	mtSchema  = "create table t1(id int, tenant_id int, primary key(id, tenant_id)) Engine=InnoDB"
@@ -57,28 +59,6 @@ func getSourceKeyspace(tenantId int) string {
 
 func getUserKeyspace(tenantId int) string {
 	return fmt.Sprintf(userKeyspaceTemplate, tenantId)
-}
-
-func initTenantData(t *testing.T, tenantId int, userKeyspace string) {
-	insertSomeData(t, tenantId, userKeyspace, 10)
-}
-
-func newMultiTenantMigration(t *testing.T) *multiTenantMigration {
-	_, err := vc.AddKeyspace(t, []*Cell{vc.Cells["zone1"]}, targetKeyspaceName, "0", mtVSchema, mtSchema, 1, 0, 200, nil)
-	require.NoError(t, err)
-	mtm := &multiTenantMigration{
-		t:                     t,
-		tenantMigrationStatus: make(map[int]tenantMigrationStatus),
-		activeMoveTables:      make(map[int]*VtctldMoveTables),
-		targetKeyspace:        targetKeyspaceName,
-		tables:                "t1",
-		tenantIdColumnName:    "tenant_id",
-	}
-	for i := 1; i <= numTenants; i++ {
-		mtm.tenantMigrationStatus[i] = tenantMigrationStatusNotMigrated
-	}
-
-	return mtm
 }
 
 func printKeyspaceRoutingRules(t *testing.T, vc *VitessCluster, msg string) {
@@ -114,16 +94,74 @@ func updateKeyspaceRoutingRules(t *testing.T, vc *VitessCluster, fromKeyspace, t
 	printKeyspaceRoutingRules(t, vc, "After creating unmatched keyspace")
 }
 
+func newMultiTenantMigration(t *testing.T) *multiTenantMigration {
+	_, err := vc.AddKeyspace(t, []*Cell{vc.Cells["zone1"]}, targetKeyspaceName, "0", mtVSchema, mtSchema, 1, 0, 200, nil)
+	require.NoError(t, err)
+	mtm := &multiTenantMigration{
+		t:                     t,
+		tenantMigrationStatus: make(map[int]tenantMigrationStatus),
+		activeMoveTables:      make(map[int]*VtctldMoveTables),
+		targetKeyspace:        targetKeyspaceName,
+		tables:                "t1",
+		tenantIdColumnName:    "tenant_id",
+		lastIDs:               make(map[int]int),
+	}
+	for i := 1; i <= numTenants; i++ {
+		mtm.setTenantMigrationStatus(i, tenantMigrationStatusNotMigrated)
+	}
+	return mtm
+}
+
+func (mtm *multiTenantMigration) getTenantMigrationStatus(tenantId int) tenantMigrationStatus {
+	mtm.mu.Lock()
+	defer mtm.mu.Unlock()
+	return mtm.tenantMigrationStatus[tenantId]
+}
+
+func (mtm *multiTenantMigration) setTenantMigrationStatus(tenantId int, status tenantMigrationStatus) {
+	mtm.mu.Lock()
+	defer mtm.mu.Unlock()
+	mtm.tenantMigrationStatus[tenantId] = status
+}
+
+func (mtm *multiTenantMigration) getActiveMoveTables(tenantId int) *VtctldMoveTables {
+	mtm.mu.Lock()
+	defer mtm.mu.Unlock()
+	return mtm.activeMoveTables[tenantId]
+}
+
+func (mtm *multiTenantMigration) setActiveMoveTables(tenantId int, mt *VtctldMoveTables) {
+	mtm.mu.Lock()
+	defer mtm.mu.Unlock()
+	mtm.activeMoveTables[tenantId] = mt
+}
+
+func (mtm *multiTenantMigration) setLastID(tenantId, lastID int) {
+	mtm.mu.Lock()
+	defer mtm.mu.Unlock()
+	mtm.lastIDs[tenantId] = lastID
+}
+
+func (mtm *multiTenantMigration) getLastID(tenantId int) int {
+	mtm.mu.Lock()
+	defer mtm.mu.Unlock()
+	return mtm.lastIDs[tenantId]
+}
+
+func (mtm *multiTenantMigration) initTenantData(t *testing.T, tenantId int, userKeyspace string) {
+	mtm.insertSomeData(t, tenantId, userKeyspace, 10)
+}
+
 func (mtm *multiTenantMigration) create(tenantId int) {
-	lastIDs[tenantId] = 0
+	mtm.setLastID(tenantId, 0)
 	sourceKeyspace := getSourceKeyspace(tenantId)
 	userKeyspace := getUserKeyspace(tenantId)
 	_, err := vc.AddKeyspace(mtm.t, []*Cell{vc.Cells["zone1"]}, sourceKeyspace, "0", mtVSchema, mtSchema, 1, 0, 1000+tenantId*100, nil)
 	require.NoError(mtm.t, err)
 	updateKeyspaceRoutingRules(mtm.t, vc, userKeyspace, sourceKeyspace)
 	log.Flush()
-	initTenantData(mtm.t, tenantId, userKeyspace)
-	mtm.tenantMigrationStatus[tenantId] = tenantMigrationStatusMigrating
+	mtm.initTenantData(mtm.t, tenantId, userKeyspace)
+	mtm.setTenantMigrationStatus(tenantId, tenantMigrationStatusMigrating)
 	mt := newVtctldMoveTables(&moveTablesWorkflow{
 		workflowInfo: &workflowInfo{
 			vc:             vc,
@@ -138,8 +176,37 @@ func (mtm *multiTenantMigration) create(tenantId int) {
 			"--source-keyspace-alias", userKeyspace,
 		},
 	})
-	mtm.activeMoveTables[tenantId] = mt
+	mtm.setActiveMoveTables(tenantId, mt)
 	mt.Create()
+}
+
+func (mtm *multiTenantMigration) insertSomeData(t *testing.T, tenantId int, userKeyspace string, numRows int) {
+	vtgateConn, closeConn := getVTGateConn()
+	defer closeConn()
+	idx := mtm.getLastID(tenantId)
+	for i := idx + 1; i <= idx+numRows; i++ {
+		execVtgateQuery(t, vtgateConn, "",
+			fmt.Sprintf("insert into %s.t1(id, tenant_id) values(%d, %d)", userKeyspace, i, tenantId))
+	}
+	mtm.setLastID(tenantId, idx+numRows)
+}
+
+func (mtm *multiTenantMigration) startTenantMigration(t *testing.T, tenantId int) {
+	userKeyspace := getUserKeyspace(tenantId)
+	mtm.create(tenantId)
+	printKeyspaceRoutingRules(t, vc, "After MoveTables Create")
+	mt := mtm.activeMoveTables[tenantId]
+	ksWorkflow := fmt.Sprintf("%s.%s", mtm.targetKeyspace, mt.workflowName)
+	waitForWorkflowState(t, vc, ksWorkflow, binlogdatapb.VReplicationWorkflowState_Running.String())
+	mtm.insertSomeData(t, tenantId, userKeyspace, 10)
+	mt.SwitchReadsAndWrites()
+	printKeyspaceRoutingRules(t, vc, "After MoveTables SwitchTraffic")
+	mtm.insertSomeData(t, tenantId, userKeyspace, 10)
+	mt.Complete()
+	printKeyspaceRoutingRules(t, vc, "After MoveTables Complete")
+	vtgateConn := vc.GetVTGateConn(t)
+	defer vtgateConn.Close()
+	waitForRowCount(t, vtgateConn, targetKeyspaceName, "t1", mtm.lastIDs[tenantId])
 }
 
 func TestMultiTenant(t *testing.T) {
@@ -156,38 +223,10 @@ func TestMultiTenant(t *testing.T) {
 	numMigrated := 0
 	tenantsToMigrate := 1 //numTenants
 	for i := 1; i <= tenantsToMigrate; i++ {
-		migrateTenant(t, mtm, i)
+		mtm.startTenantMigration(t, i)
 		numMigrated++
 	}
 	vtgateConn, closeConn := getVTGateConn()
 	defer closeConn()
 	require.Equal(t, numMigrated*10*3, getRowCount(t, vtgateConn, fmt.Sprintf("%s.%s", mtm.targetKeyspace, "t1")))
-}
-
-func insertSomeData(t *testing.T, tenantId int, userKeyspace string, numRows int) {
-	vtgateConn, closeConn := getVTGateConn()
-	defer closeConn()
-	for i := lastIDs[tenantId] + 1; i <= lastIDs[tenantId]+numRows; i++ {
-		execVtgateQuery(t, vtgateConn, "",
-			fmt.Sprintf("insert into %s.t1(id, tenant_id) values(%d, %d)", userKeyspace, i, tenantId))
-	}
-	lastIDs[tenantId] = lastIDs[tenantId] + numRows
-}
-
-func migrateTenant(t *testing.T, mtm *multiTenantMigration, tenantId int) {
-	userKeyspace := getUserKeyspace(tenantId)
-	mtm.create(tenantId)
-	printKeyspaceRoutingRules(t, vc, "After MoveTables Create")
-	mt := mtm.activeMoveTables[tenantId]
-	ksWorkflow := fmt.Sprintf("%s.%s", mtm.targetKeyspace, mt.workflowName)
-	waitForWorkflowState(t, vc, ksWorkflow, binlogdatapb.VReplicationWorkflowState_Running.String())
-	insertSomeData(t, tenantId, userKeyspace, 10)
-	mt.SwitchReadsAndWrites()
-	printKeyspaceRoutingRules(t, vc, "After MoveTables SwitchTraffic")
-	insertSomeData(t, tenantId, userKeyspace, 10)
-	mt.Complete()
-	printKeyspaceRoutingRules(t, vc, "After MoveTables Complete")
-	vtgateConn := vc.GetVTGateConn(t)
-	defer vtgateConn.Close()
-	waitForRowCount(t, vtgateConn, targetKeyspaceName, "t1", lastIDs[tenantId])
 }
