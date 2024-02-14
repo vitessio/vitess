cc4f8ae778ddc33a40eb1a4d439104f522fc73a0
Harshit Gangal <harshit@planetscale.com>
2024-02-14 11:24:35 +0530

    Add support for Update Multi Table (#15211)
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>
    Signed-off-by: Andres Taylor <andres@planetscale.com>
    Co-authored-by: Andres Taylor <andres@planetscale.com>

--- go/vt/vtgate/planbuilder/operators/update.go
+++ go/vt/vtgate/planbuilder/operators/update.go
@@ -103,6 +103,8 @@ func (u *Update) ShortDescription() string {
 }
 
 func createOperatorFromUpdate(ctx *plancontext.PlanningContext, updStmt *sqlparser.Update) (op Operator) {
+	errIfUpdateNotSupported(ctx, updStmt)
+
 	var updClone *sqlparser.Update
 	var vTbl *vindexes.Table
 
@@ -134,6 +136,31 @@ func createOperatorFromUpdate(ctx *plancontext.PlanningContext, updStmt *sqlpars
 	return buildFkOperator(ctx, op, updClone, parentFks, childFks, vTbl)
 }
 
+func errIfUpdateNotSupported(ctx *plancontext.PlanningContext, stmt *sqlparser.Update) {
+	var vTbl *vindexes.Table
+	for _, ue := range stmt.Exprs {
+		tblInfo, err := ctx.SemTable.TableInfoForExpr(ue.Name)
+		if err != nil {
+			panic(err)
+		}
+		if _, isATable := tblInfo.(*semantics.RealTable); !isATable {
+			var tblName string
+			ate := tblInfo.GetAliasedTableExpr()
+			if ate != nil {
+				tblName = sqlparser.String(ate)
+			}
+			panic(vterrors.VT03032(tblName))
+		}
+
+		if vTbl == nil {
+			vTbl = tblInfo.GetVindexTable()
+		}
+		if vTbl != tblInfo.GetVindexTable() {
+			panic(vterrors.VT12001("multi-table UPDATE statement with multi-target column update"))
+		}
+	}
+}
+
 func createUpdateOperator(ctx *plancontext.PlanningContext, updStmt *sqlparser.Update) (Operator, *vindexes.Table, *sqlparser.Update) {
 	op := crossJoin(ctx, updStmt.TableExprs)
 
@@ -148,6 +175,8 @@ func createUpdateOperator(ctx *plancontext.PlanningContext, updStmt *sqlparser.U
 	// If we encounter subqueries, we want to fix the updClone to use the replaced expression, so that the pulled out subquery's
 	// result is used everywhere instead of running the subquery multiple times, which is wasteful.
 	updClone := sqlparser.CloneRefOfUpdate(updStmt)
+	var tblInfo semantics.TableInfo
+	var err error
 	for idx, updExpr := range updStmt.Exprs {
 		expr, subqs := sqc.pullOutValueSubqueries(ctx, updExpr.Expr, outerID, true)
 		if len(subqs) == 0 {
@@ -164,19 +193,13 @@ func createUpdateOperator(ctx *plancontext.PlanningContext, updStmt *sqlparser.U
 			Name: updExpr.Name,
 			Expr: proj,
 		}
+		tblInfo, err = ctx.SemTable.TableInfoForExpr(updExpr.Name)
+		if err != nil {
+			panic(err)
+		}
 	}
 
-	target := updStmt.TableExprs[0]
-	atbl, ok := target.(*sqlparser.AliasedTableExpr)
-	if !ok {
-		panic(vterrors.VT12001("multi table update"))
-	}
-	tblID := ctx.SemTable.TableSetFor(atbl)
-	tblInfo, err := ctx.SemTable.TableInfoFor(tblID)
-	if err != nil {
-		panic(err)
-	}
-
+	tblID := ctx.SemTable.TableSetFor(tblInfo.GetAliasedTableExpr())
 	vTbl := tblInfo.GetVindexTable()
 	// Reference table should update the source table.
 	if vTbl.Type == vindexes.TypeReference && vTbl.Source != nil {
@@ -194,7 +217,7 @@ func createUpdateOperator(ctx *plancontext.PlanningContext, updStmt *sqlparser.U
 		Name:   name,
 	}
 
-	_, cvv, ovq, subQueriesArgOnChangedVindex := getUpdateVindexInformation(ctx, updStmt, targetTbl, assignments)
+	_, cvv, ovq, subQueriesArgOnChangedVindex := getUpdateVindexInformation(ctx, updStmt, targetTbl, tblInfo.GetAliasedTableExpr(), assignments)
 
 	updOp := &Update{
 		DMLCommon: &DMLCommon{
@@ -226,6 +249,7 @@ func getUpdateVindexInformation(
 	ctx *plancontext.PlanningContext,
 	updStmt *sqlparser.Update,
 	table TargetTable,
+	ate *sqlparser.AliasedTableExpr,
 	assignments []SetExpr,
 ) ([]*VindexPlusPredicates, map[string]*engine.VindexValues, *sqlparser.Select, []string) {
 	if !table.VTable.Keyspace.Sharded {
@@ -233,7 +257,7 @@ func getUpdateVindexInformation(
 	}
 
 	primaryVindex, vindexAndPredicates := getVindexInformation(table.ID, table.VTable)
-	changedVindexValues, ownedVindexQuery, subQueriesArgOnChangedVindex := buildChangedVindexesValues(ctx, updStmt, table.VTable, primaryVindex.Columns, assignments)
+	changedVindexValues, ownedVindexQuery, subQueriesArgOnChangedVindex := buildChangedVindexesValues(ctx, updStmt, table.VTable, ate, primaryVindex.Columns, assignments)
 	return vindexAndPredicates, changedVindexValues, ownedVindexQuery, subQueriesArgOnChangedVindex
 }
 
