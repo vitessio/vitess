939de976abaa318cc641eaca5cd6e3ac99533aeb
Dirkjan Bussink <d.bussink@gmail.com>
2024-02-17 15:13:48 +0100

    vtexplain: Fix setting up the column information
    In https://github.com/vitessio/vitess/pull/13312 we fixed how we encode
    names in information_schema queries. The problem there though is that
    the `GetColumnNamesQuery` string was also used inside `vtexplain`. It's
    usage there was not updated for the new escaping logic.
    
    This in turn leads to broken queries during `vtexplain` which then leads
    to a lot of logs. The issue here is not that we log a lot, we log that
    we actually do have a problem here which is the case.
    
    This fixes the underlying issue of not correctly generating the query as
    we should.
    
    Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

--- go/vt/mysqlctl/schema.go
+++ go/vt/mysqlctl/schema.go
@@ -66,7 +66,7 @@ func (mysqld *Mysqld) executeSchemaCommands(ctx context.Context, sql string) err
 	return mysqld.executeMysqlScript(ctx, params, sql)
 }
 
-func encodeEntityName(name string) string {
+func EncodeEntityName(name string) string {
 	var buf strings.Builder
 	sqltypes.NewVarChar(name).EncodeSQL(&buf)
 	return buf.String()
@@ -80,7 +80,7 @@ func tableListSQL(tables []string) (string, error) {
 
 	encodedTables := make([]string, len(tables))
 	for i, tableName := range tables {
-		encodedTables[i] = encodeEntityName(tableName)
+		encodedTables[i] = EncodeEntityName(tableName)
 	}
 
 	return "(" + strings.Join(encodedTables, ", ") + ")", nil
@@ -307,13 +307,13 @@ func GetColumnsList(dbName, tableName string, exec func(string, int, bool) (*sql
 	if dbName == "" {
 		dbName2 = "database()"
 	} else {
-		dbName2 = encodeEntityName(dbName)
+		dbName2 = EncodeEntityName(dbName)
 	}
 	sanitizedTableName, err := sqlescape.UnescapeID(tableName)
 	if err != nil {
 		return "", err
 	}
-	query := fmt.Sprintf(GetColumnNamesQuery, dbName2, encodeEntityName(sanitizedTableName))
+	query := fmt.Sprintf(GetColumnNamesQuery, dbName2, EncodeEntityName(sanitizedTableName))
 	qr, err := exec(query, -1, true)
 	if err != nil {
 		return "", err
@@ -407,7 +407,7 @@ func (mysqld *Mysqld) getPrimaryKeyColumns(ctx context.Context, dbName string, t
             FROM information_schema.STATISTICS
             WHERE TABLE_SCHEMA = %s AND TABLE_NAME IN %s AND LOWER(INDEX_NAME) = 'primary'
             ORDER BY table_name, SEQ_IN_INDEX`
-	sql = fmt.Sprintf(sql, encodeEntityName(dbName), tableList)
+	sql = fmt.Sprintf(sql, EncodeEntityName(dbName), tableList)
 	qr, err := conn.Conn.ExecuteFetch(sql, len(tables)*100, true)
 	if err != nil {
 		return nil, err
@@ -631,8 +631,8 @@ func GetPrimaryKeyEquivalentColumns(ctx context.Context, exec func(string, int, 
             ) AS pke ON index_cols.INDEX_NAME = pke.INDEX_NAME
             WHERE index_cols.TABLE_SCHEMA = %s AND index_cols.TABLE_NAME = %s AND NON_UNIQUE = 0 AND NULLABLE != 'YES'
             ORDER BY SEQ_IN_INDEX ASC`
-	encodedDbName := encodeEntityName(dbName)
-	encodedTable := encodeEntityName(table)
+	encodedDbName := EncodeEntityName(dbName)
+	encodedTable := EncodeEntityName(table)
 	sql = fmt.Sprintf(sql, encodedDbName, encodedTable, encodedDbName, encodedTable, encodedDbName, encodedTable)
 	qr, err := exec(sql, 1000, true)
 	if err != nil {
