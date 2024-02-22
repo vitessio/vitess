bd83ca161bd9e4a0d0fe4e27ea120c977731de10
Manik Rana <Manikrana54@gmail.com>
2024-02-22 19:19:15 +0530

    chore: modernize tests (#15244)
    Signed-off-by: Manik Rana <manikrana54@gmail.com>
    Signed-off-by: Manik Rana <Manikrana54@gmail.com>

--- go/streamlog/streamlog_test.go
+++ go/streamlog/streamlog_test.go
@@ -315,7 +315,6 @@ func TestShouldEmitLog(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.sql, func(t *testing.T) {
 			SetQueryLogFilterTag(tt.qLogFilterTag)
 			SetQueryLogRowThreshold(tt.qLogRowThreshold)
@@ -360,7 +359,6 @@ func TestGetFormatter(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			var buffer bytes.Buffer
 			logFormatterFunc := GetFormatter[string](tt.logger)
