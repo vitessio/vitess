f0fb98ed1b477b9274af6f18d2c289eb25d65133
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2024-02-28 11:01:03 +0200

    simplify test
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/test/endtoend/onlineddl/vrepl_suite/testdata/fk-child-modify-not-null/create.sql
+++ go/test/endtoend/onlineddl/vrepl_suite/testdata/fk-child-modify-not-null/create.sql
@@ -5,13 +5,11 @@ drop table if exists onlineddl_test_parent;
 set session foreign_key_checks=1;
 create table onlineddl_test_parent (
   id int auto_increment,
-  ts timestamp,
   primary key(id)
 );
 create table onlineddl_test (
   id int auto_increment,
-  i int not null,
   parent_id int null,
   primary key(id),
   constraint test_fk foreign key (parent_id) references onlineddl_test_parent (id) on delete no action
-) auto_increment=1;
+);
