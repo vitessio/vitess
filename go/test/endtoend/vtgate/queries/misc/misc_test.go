604503e997b8151f41b2c186291b216c374f1343
Florent Poinsard <35779988+frouioui@users.noreply.github.com>
2024-03-04 10:24:04 -0600

    Update go/test/endtoend/vtgate/queries/misc/misc_test.go
    Signed-off-by: Florent Poinsard <35779988+frouioui@users.noreply.github.com>

--- go/test/endtoend/vtgate/queries/misc/misc_test.go
+++ go/test/endtoend/vtgate/queries/misc/misc_test.go
@@ -364,7 +364,7 @@ func TestAliasesInOuterJoinQueries(t *testing.T) {
 	mcmp.Exec("insert into t1(id1, id2) values (1,2), (42,5), (5, 42)")
 	mcmp.Exec("insert into tbl(id, unq_col, nonunq_col) values (1,2,3), (2,5,3), (3, 42, 2)")
 
-	// Check that the select query works as intended and then run it agin verifying the column names as well.
+	// Check that the select query works as intended and then run it again verifying the column names as well.
 	mcmp.AssertMatches("select t1.id1 as t0, t1.id1 as t1, tbl.unq_col as col from t1 left outer join tbl on t1.id2 = tbl.nonunq_col", `[[INT64(1) INT64(1) INT64(42)] [INT64(5) INT64(5) NULL] [INT64(42) INT64(42) NULL]]`)
 	mcmp.ExecWithColumnCompare("select t1.id1 as t0, t1.id1 as t1, tbl.unq_col as col from t1 left outer join tbl on t1.id2 = tbl.nonunq_col")
 
