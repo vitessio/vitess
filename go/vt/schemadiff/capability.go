103917a142c4ad1c0b751aa72368bd0d5ee8d2a9
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2024-04-16 14:09:30 +0300

    update comment
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/schemadiff/capability.go
+++ go/vt/schemadiff/capability.go
@@ -10,7 +10,7 @@ import (
 // alterOptionAvailableViaInstantDDL checks if the specific alter option is eligible to run via ALGORITHM=INSTANT
 // reference: https://dev.mysql.com/doc/refman/8.0/en/innodb-online-ddl-operations.html
 func alterOptionCapableOfInstantDDL(alterOption sqlparser.AlterOption, createTable *sqlparser.CreateTable, capableOf capabilities.CapableOf) (bool, error) {
-	// A table with FULLTEXT index can't have any sort of INSTANT DDL
+	// A table with FULLTEXT index won't support adding/removing columns instantly.
 	tableHasFulltextIndex := false
 	for _, key := range createTable.TableSpec.Indexes {
 		if key.Info.Type == sqlparser.IndexTypeFullText {
