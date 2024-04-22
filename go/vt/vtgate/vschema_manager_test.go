480216861b2b268df58e586a9dac65cab204d85e
Harshit Gangal <harshit@planetscale.com>
2024-04-22 12:09:34 +0530

    add udfs to vschema on update (#15771)
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/vt/vtgate/vschema_manager_test.go
+++ go/vt/vtgate/vschema_manager_test.go
@@ -438,6 +438,38 @@ func TestRebuildVSchema(t *testing.T) {
 	}
 }
 
+// TestVSchemaUDFsUpdate tests that the UDFs are updated in the VSchema.
+func TestVSchemaUDFsUpdate(t *testing.T) {
+	ks := &vindexes.Keyspace{Name: "ks", Sharded: true}
+
+	vm := &VSchemaManager{}
+	var vs *vindexes.VSchema
+	vm.subscriber = func(vschema *vindexes.VSchema, _ *VSchemaStats) {
+		vs = vschema
+		vs.ResetCreated()
+	}
+	vm.schema = &fakeSchema{udfs: []string{"udf1", "udf2"}}
+	vm.VSchemaUpdate(&vschemapb.SrvVSchema{
+		Keyspaces: map[string]*vschemapb.Keyspace{
+			"ks": {Sharded: true},
+		},
+	}, nil)
+
+	utils.MustMatchFn(".globalTables", ".uniqueVindexes")(t, &vindexes.VSchema{
+		RoutingRules: map[string]*vindexes.RoutingRule{},
+		Keyspaces: map[string]*vindexes.KeyspaceSchema{
+			"ks": {
+				Keyspace:       ks,
+				ForeignKeyMode: vschemapb.Keyspace_unmanaged,
+				Tables:         map[string]*vindexes.Table{},
+				Vindexes:       map[string]vindexes.Vindex{},
+				AggregateUDFs:  []string{"udf1", "udf2"},
+			},
+		},
+	}, vs)
+	utils.MustMatch(t, vs, vm.currentVschema, "currentVschema does not match Vschema")
+}
+
 func TestMarkErrorIfCyclesInFk(t *testing.T) {
 	ksName := "ks"
 	keyspace := &vindexes.Keyspace{
