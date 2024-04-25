2829b797d84e9e75266dcb0c953f13e4a4bc787d
Manan Gupta <35839558+GuptaManan100@users.noreply.github.com>
2024-04-25 14:20:49 +0530

    Store Decimal precision and size while normalising (#15785)
    Signed-off-by: Manan Gupta <manan@planetscale.com>
    Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>
    Co-authored-by: Dirkjan Bussink <d.bussink@gmail.com>

--- go/test/endtoend/vtgate/queries/tpch/tpch_test.go
+++ go/test/endtoend/vtgate/queries/tpch/tpch_test.go
@@ -174,6 +174,28 @@ where l_partkey = p_partkey
   and l_shipdate >= '1996-12-01'
   and l_shipdate < date_add('1996-12-01', interval '1' month);`,
 		},
+		{
+			name: "Q14 without case",
+			query: `select 100.00 * sum(l_extendedprice * (1 - l_discount)) / sum(l_extendedprice * (1 - l_discount)) as promo_revenue
+from lineitem,
+     part
+where l_partkey = p_partkey
+  and l_shipdate >= '1996-12-01'
+  and l_shipdate < date_add('1996-12-01', interval '1' month);`,
+		},
+		{
+			name: "Q14",
+			query: `select 100.00 * sum(case
+                        when p_type like 'PROMO%'
+                            then l_extendedprice * (1 - l_discount)
+                        else 0
+    end) / sum(l_extendedprice * (1 - l_discount)) as promo_revenue
+from lineitem,
+     part
+where l_partkey = p_partkey
+  and l_shipdate >= '1996-12-01'
+  and l_shipdate < date_add('1996-12-01', interval '1' month);`,
+		},
 	}
 
 	for _, testcase := range testcases {
