d1c396bf022a77aec051f55a6beda4554ffae9f0
Harshit Gangal <harshit@planetscale.com>
2024-04-26 13:09:39 +0530

    split the rowalias table collector method to smaller methods
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/vt/vtgate/semantics/table_collector.go
+++ go/vt/vtgate/semantics/table_collector.go
@@ -109,86 +109,6 @@ func (etc *earlyTableCollector) handleTableName(tbl sqlparser.TableName, aet *sq
 	etc.Tables = append(etc.Tables, tableInfo)
 }
 
-func (tc *tableCollector) visitRowAlias(ins *sqlparser.Insert, rowAlias *sqlparser.RowAlias) error {
-	origTableInfo := tc.Tables[0]
-
-	var colNames []string
-	var types []evalengine.Type
-	switch {
-	case len(rowAlias.Columns) > 0 && len(ins.Columns) > 0:
-		// we have explicit column list on the row alias and the insert statement
-		if len(rowAlias.Columns) != len(ins.Columns) {
-			return vterrors.VT03033()
-		}
-		origCols := origTableInfo.getColumns(false /* ignoreInvisbleCol */)
-	for1:
-		for idx, column := range rowAlias.Columns {
-			colNames = append(colNames, column.String())
-			col := ins.Columns[idx]
-			for _, origCol := range origCols {
-				if col.EqualString(origCol.Name) {
-					types = append(types, origCol.Type)
-					continue for1
-				}
-			}
-			return vterrors.NewErrorf(vtrpcpb.Code_NOT_FOUND, vterrors.BadFieldError, "Unknown column '%s' in 'field list'", col)
-		}
-	case len(rowAlias.Columns) > 0:
-		if !origTableInfo.authoritative() {
-			return vterrors.VT09015()
-		}
-		origCols := origTableInfo.getColumns(true /* ignoreInvisibleCol */)
-		if len(rowAlias.Columns) != len(origCols) {
-			return vterrors.VT03033()
-		}
-		for idx, column := range rowAlias.Columns {
-			colNames = append(colNames, column.String())
-			types = append(types, origCols[idx].Type)
-		}
-	case len(ins.Columns) > 0:
-		origCols := origTableInfo.getColumns(false /* ignoreInvisbleCol */)
-	for2:
-		for _, column := range ins.Columns {
-			colNames = append(colNames, column.String())
-			for _, origCol := range origCols {
-				if column.EqualString(origCol.Name) {
-					types = append(types, origCol.Type)
-					continue for2
-				}
-			}
-			types = append(types, evalengine.NewType(sqltypes.Unknown, collations.Unknown))
-		}
-	default:
-		if !origTableInfo.authoritative() {
-			return vterrors.VT09015()
-		}
-		for _, column := range origTableInfo.getColumns(true /* ignoreInvisibleCol */) {
-			colNames = append(colNames, column.Name)
-			types = append(types, column.Type)
-		}
-	}
-	deps := make([]TableSet, len(colNames))
-	for i := range colNames {
-		deps[i] = SingleTableSet(0)
-	}
-
-	derivedTable := &DerivedTable{
-		tableName: rowAlias.TableName.String(),
-		ASTNode: &sqlparser.AliasedTableExpr{
-			Expr: sqlparser.NewTableName(rowAlias.TableName.String()),
-		},
-		columnNames:     colNames,
-		tables:          SingleTableSet(0),
-		recursive:       deps,
-		isAuthoritative: true,
-		types:           types,
-	}
-
-	tc.Tables = append(tc.Tables, derivedTable)
-	current := tc.scoper.currentScope()
-	return current.addTable(derivedTable)
-}
-
 func (tc *tableCollector) up(cursor *sqlparser.Cursor) error {
 	switch node := cursor.Node().(type) {
 	case *sqlparser.AliasedTableExpr:
@@ -204,6 +124,16 @@ func (tc *tableCollector) up(cursor *sqlparser.Cursor) error {
 	default:
 		return nil
 	}
+}
+
+func (tc *tableCollector) visitAliasedTableExpr(node *sqlparser.AliasedTableExpr) error {
+	switch t := node.Expr.(type) {
+	case *sqlparser.DerivedTable:
+		return tc.handleDerivedTable(node, t)
+	case sqlparser.TableName:
+		return tc.handleTableName(node, t)
+	}
+	return nil
 }
 
 func (tc *tableCollector) visitUnion(union *sqlparser.Union) error {
@@ -248,15 +178,126 @@ func (tc *tableCollector) visitUnion(union *sqlparser.Union) error {
 	return nil
 }
 
-func (tc *tableCollector) visitAliasedTableExpr(node *sqlparser.AliasedTableExpr) error {
-	switch t := node.Expr.(type) {
-	case *sqlparser.DerivedTable:
-		return tc.handleDerivedTable(node, t)
+func (tc *tableCollector) visitRowAlias(ins *sqlparser.Insert, rowAlias *sqlparser.RowAlias) error {
+	origTableInfo := tc.Tables[0]
 
-	case sqlparser.TableName:
-		return tc.handleTableName(node, t)
+	colNames, types, err := tc.getColumnNamesAndTypes(ins, rowAlias, origTableInfo)
+	if err != nil {
+		return err
 	}
-	return nil
+
+	derivedTable := buildDerivedTable(colNames, rowAlias, types)
+	tc.Tables = append(tc.Tables, derivedTable)
+	current := tc.scoper.currentScope()
+	return current.addTable(derivedTable)
+}
+
+func (tc *tableCollector) getColumnNamesAndTypes(ins *sqlparser.Insert, rowAlias *sqlparser.RowAlias, origTableInfo TableInfo) (colNames []string, types []evalengine.Type, err error) {
+	switch {
+	case len(rowAlias.Columns) > 0 && len(ins.Columns) > 0:
+		return tc.handleExplicitColumns(ins, rowAlias, origTableInfo)
+	case len(rowAlias.Columns) > 0:
+		return tc.handleRowAliasColumns(origTableInfo, rowAlias)
+	case len(ins.Columns) > 0:
+		colNames, types = tc.handleInsertColumns(ins, origTableInfo)
+		return colNames, types, nil
+	default:
+		return tc.handleDefaultColumns(origTableInfo)
+	}
+}
+
+// handleDefaultColumns have no explicit column list on the insert statement and no column list on the row alias
+func (tc *tableCollector) handleDefaultColumns(origTableInfo TableInfo) ([]string, []evalengine.Type, error) {
+	if !origTableInfo.authoritative() {
+		return nil, nil, vterrors.VT09015()
+	}
+	var colNames []string
+	var types []evalengine.Type
+	for _, column := range origTableInfo.getColumns(true /* ignoreInvisibleCol */) {
+		colNames = append(colNames, column.Name)
+		types = append(types, column.Type)
+	}
+	return colNames, types, nil
+}
+
+// handleInsertColumns have explicit column list on the insert statement and no column list on the row alias
+func (tc *tableCollector) handleInsertColumns(ins *sqlparser.Insert, origTableInfo TableInfo) ([]string, []evalengine.Type) {
+	var colNames []string
+	var types []evalengine.Type
+	origCols := origTableInfo.getColumns(false /* ignoreInvisbleCol */)
+for2:
+	for _, column := range ins.Columns {
+		colNames = append(colNames, column.String())
+		for _, origCol := range origCols {
+			if column.EqualString(origCol.Name) {
+				types = append(types, origCol.Type)
+				continue for2
+			}
+		}
+		types = append(types, evalengine.NewType(sqltypes.Unknown, collations.Unknown))
+	}
+	return colNames, types
+}
+
+// handleRowAliasColumns have explicit column list on the row alias and no column list on the insert statement
+func (tc *tableCollector) handleRowAliasColumns(origTableInfo TableInfo, rowAlias *sqlparser.RowAlias) ([]string, []evalengine.Type, error) {
+	if !origTableInfo.authoritative() {
+		return nil, nil, vterrors.VT09015()
+	}
+	origCols := origTableInfo.getColumns(true /* ignoreInvisibleCol */)
+	if len(rowAlias.Columns) != len(origCols) {
+		return nil, nil, vterrors.VT03033()
+	}
+	var colNames []string
+	var types []evalengine.Type
+	for idx, column := range rowAlias.Columns {
+		colNames = append(colNames, column.String())
+		types = append(types, origCols[idx].Type)
+	}
+	return colNames, types, nil
+}
+
+// handleExplicitColumns have explicit column list on the row alias and the insert statement
+func (tc *tableCollector) handleExplicitColumns(ins *sqlparser.Insert, rowAlias *sqlparser.RowAlias, origTableInfo TableInfo) ([]string, []evalengine.Type, error) {
+	if len(rowAlias.Columns) != len(ins.Columns) {
+		return nil, nil, vterrors.VT03033()
+	}
+	var colNames []string
+	var types []evalengine.Type
+	origCols := origTableInfo.getColumns(false /* ignoreInvisbleCol */)
+for1:
+	for idx, column := range rowAlias.Columns {
+		colNames = append(colNames, column.String())
+		col := ins.Columns[idx]
+		for _, origCol := range origCols {
+			if col.EqualString(origCol.Name) {
+				types = append(types, origCol.Type)
+				continue for1
+			}
+		}
+		return nil, nil, vterrors.NewErrorf(vtrpcpb.Code_NOT_FOUND, vterrors.BadFieldError, "Unknown column '%s' in 'field list'", col)
+	}
+	return colNames, types, nil
+}
+
+func buildDerivedTable(colNames []string, rowAlias *sqlparser.RowAlias, types []evalengine.Type) *DerivedTable {
+	deps := make([]TableSet, len(colNames))
+	for i := range colNames {
+		deps[i] = SingleTableSet(0)
+	}
+
+	derivedTable := &DerivedTable{
+		tableName: rowAlias.TableName.String(),
+		ASTNode: &sqlparser.AliasedTableExpr{
+			Expr: sqlparser.NewTableName(rowAlias.TableName.String()),
+		},
+		columnNames:     colNames,
+		tables:          SingleTableSet(0),
+		recursive:       deps,
+		isAuthoritative: true,
+		types:           types,
+	}
+	return derivedTable
 }
 
 func (tc *tableCollector) handleTableName(node *sqlparser.AliasedTableExpr, t sqlparser.TableName) (err error) {
