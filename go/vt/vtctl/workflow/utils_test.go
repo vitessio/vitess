682b00c9d6bccdd914cfc1bc29415e17ac6f7b87
Rohit Nayak <rohit@planetscale.com>
2024-05-02 16:39:39 +0200

    Add concurrency tests for a specific topo to confirm that it works for a 'real' topo implementation in addition to memtopo
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>

--- go/vt/vtctl/workflow/utils_test.go
+++ go/vt/vtctl/workflow/utils_test.go
@@ -5,13 +5,18 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"os/exec"
 	"sync"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
+	clientv3 "go.etcd.io/etcd/client/v3"
 
+	"vitess.io/vitess/go/testfiles"
+	"vitess.io/vitess/go/vt/log"
 	"vitess.io/vitess/go/vt/topo"
+	"vitess.io/vitess/go/vt/topo/etcd2topo"
 	"vitess.io/vitess/go/vt/topo/memorytopo"
 	"vitess.io/vitess/go/vt/topotools"
 )
@@ -36,11 +41,27 @@ func TestUpdateKeyspaceRoutingRule(t *testing.T) {
 // TestConcurrentKeyspaceRoutingRulesUpdates runs multiple keyspace routing rules updates concurrently to test
 // the locking mechanism.
 func TestConcurrentKeyspaceRoutingRulesUpdates(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	ctx := context.Background()
+
 	ts := memorytopo.NewServer(ctx, "zone1")
 	defer ts.Close()
+	t.Run("memtopo", func(t *testing.T) {
+		testConcurrentKeyspaceRoutingRulesUpdates(t, ctx, ts)
+	})
 
+	etcdServerAddress := startEtcd(t)
+	log.Infof("Successfully started etcd server at %s", etcdServerAddress)
+	topoName := "etcd2_test" // "etcd2" is already registered on init(), so using a different name
+	topo.RegisterFactory(topoName, etcd2topo.Factory{})
+	ts, err := topo.OpenServer(topoName, etcdServerAddress, "/vitess")
+	require.NoError(t, err)
+	t.Run("etcd", func(t *testing.T) {
+		testConcurrentKeyspaceRoutingRulesUpdates(t, ctx, ts)
+		ts.Close()
+	})
+}
+
+func testConcurrentKeyspaceRoutingRulesUpdates(t *testing.T, ctx context.Context, ts *topo.Server) {
 	concurrency := 100
 	duration := 3 * time.Second
 
@@ -48,7 +69,7 @@ func TestConcurrentKeyspaceRoutingRulesUpdates(t *testing.T) {
 	wg.Add(concurrency)
 
 	stop := make(chan struct{})
-
+	log.Infof("Starting %d concurrent updates", concurrency)
 	for i := 0; i < concurrency; i++ {
 		go func(id int) {
 			defer wg.Done()
@@ -65,6 +86,10 @@ func TestConcurrentKeyspaceRoutingRulesUpdates(t *testing.T) {
 	<-time.After(duration)
 	close(stop)
 	wg.Wait()
+	log.Infof("All updates completed")
+	rules, err := ts.GetKeyspaceRoutingRules(ctx)
+	require.NoError(t, err)
+	require.LessOrEqual(t, concurrency, len(rules.Rules))
 }
 
 func update(t *testing.T, ts *topo.Server, id int) {
@@ -84,4 +109,60 @@ func update(t *testing.T, ts *topo.Server, id int) {
 		from := fmt.Sprintf("from%s%s", s, tabletType)
 		require.Equal(t, s+tabletType, got[from])
 	}
+}
+
+// startEtcd starts an etcd subprocess, and waits for it to be ready.
+func startEtcd(t *testing.T) string {
+	// Create a temporary directory.
+	dataDir := t.TempDir()
+
+	// Get our two ports to listen to.
+	port := testfiles.GoVtTopoEtcd2topoPort
+	name := "vitess_unit_test"
+	clientAddr := fmt.Sprintf("http://localhost:%v", port)
+	peerAddr := fmt.Sprintf("http://localhost:%v", port+1)
+	initialCluster := fmt.Sprintf("%v=%v", name, peerAddr)
+	cmd := exec.Command("etcd",
+		"-name", name,
+		"-advertise-client-urls", clientAddr,
+		"-initial-advertise-peer-urls", peerAddr,
+		"-listen-client-urls", clientAddr,
+		"-listen-peer-urls", peerAddr,
+		"-initial-cluster", initialCluster,
+		"-data-dir", dataDir)
+	err := cmd.Start()
+	if err != nil {
+		t.Fatalf("failed to start etcd: %v", err)
+	}
+
+	// Create a client to connect to the created etcd.
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{clientAddr},
+		DialTimeout: 5 * time.Second,
+	})
+	if err != nil {
+		t.Fatalf("newCellClient(%v) failed: %v", clientAddr, err)
+	}
+	defer cli.Close()
+
+	// Wait until we can list "/", or timeout.
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	start := time.Now()
+	for {
+		if _, err := cli.Get(ctx, "/"); err == nil {
+			break
+		}
+		if time.Since(start) > 10*time.Second {
+			t.Fatalf("Failed to start etcd daemon in time")
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
+	t.Cleanup(func() {
+		if cmd.Process.Kill() != nil {
+			log.Infof("cmd.Process.Kill() failed : %v", err)
+		}
+	})
+
+	return clientAddr
 }
