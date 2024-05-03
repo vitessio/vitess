4b66d39eaaae95a952e2d970e085f13f2bd74f1f
Manan Gupta <35839558+GuptaManan100@users.noreply.github.com>
2024-05-03 11:28:47 +0530

    Fix derived table bug (#15831)

--- go/test/endtoend/vtgate/queries/tpch/tpch_test.go
+++ go/test/endtoend/vtgate/queries/tpch/tpch_test.go
@@ -196,6 +196,37 @@ where l_partkey = p_partkey
   and l_shipdate >= '1996-12-01'
   and l_shipdate < date_add('1996-12-01', interval '1' month);`,
 		},
+		{
+			name: "Q8",
+			query: `select o_year, sum(case when nation = 'BRAZIL' then volume else 0 end) / sum(volume) as mkt_share
+from (select extract(year from o_orderdate) as o_year, l_extendedprice * (1 - l_discount) as volume, n2.n_name as nation
+      from part,
+           supplier,
+           lineitem,
+           orders,
+           customer,
+           nation n1,
+           nation n2,
+           region
+      where p_partkey = l_partkey
+        and s_suppkey = l_suppkey
+        and l_orderkey = o_orderkey
+        and o_custkey = c_custkey
+        and c_nationkey = n1.n_nationkey
+        and n1.n_regionkey = r_regionkey
+        and r_name = 'AMERICA'
+        and s_nationkey = n2.n_nationkey
+        and o_orderdate between date '1995-01-01' and date ('1996-12-31') and p_type = 'ECONOMY ANODIZED STEEL' ) as all_nations
+group by o_year
+order by o_year`,
+		},
+		{
+			name: "simple derived table",
+			query: `select *
+from (select l.l_extendedprice * o.o_totalprice
+      from lineitem l
+               join orders o) as dt`,
+		},
 	}
 
 	for _, testcase := range testcases {
