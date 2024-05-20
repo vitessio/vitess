f16d8b23d284ccd06701eea8da13ad841806bc9c
Florent Poinsard <35779988+frouioui@users.noreply.github.com>
2024-05-20 00:44:40 -0600

    Upgrade the golang version used `vitess-mixin` (#15972)
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- go/tools/go-upgrade/go-upgrade.go
+++ go/tools/go-upgrade/go-upgrade.go
@@ -415,13 +415,16 @@ func replaceGoVersionInCodebase(old, new *version.Version, workflowUpdate bool) 
 	}
 
 	if !isSameMajorMinorVersion(old, new) {
-		err = replaceInFile(
-			[]*regexp.Regexp{regexp.MustCompile(regexpReplaceGoModGoVersion)},
-			[]string{fmt.Sprintf("go %d.%d", new.Segments()[0], new.Segments()[1])},
-			"./go.mod",
-		)
-		if err != nil {
-			return err
+		goModFiles := []string{"./go.mod", "./vitess-mixin/go.mod"}
+		for _, file := range goModFiles {
+			err = replaceInFile(
+				[]*regexp.Regexp{regexp.MustCompile(regexpReplaceGoModGoVersion)},
+				[]string{fmt.Sprintf("go %d.%d", new.Segments()[0], new.Segments()[1])},
+				file,
+			)
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return nil
