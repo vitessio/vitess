9010477f0efd791ef2d4afa38da0868600c51b71
Rohit Nayak <rohit@planetscale.com>
2024-05-27 11:41:43 +0200

    Comment logs to remove noise while unit testing
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>

--- go/vt/vttest/local_cluster.go
+++ go/vt/vttest/local_cluster.go
@@ -600,7 +600,7 @@ func (db *LocalCluster) Execute(sql []string, dbname string) error {
 	}
 
 	for _, cmd := range sql {
-		log.Infof("Execute(%s): \"%s\"", dbname, cmd)
+		//		log.Infof("Execute(%s): \"%s\"", dbname, cmd)
 		_, err := conn.ExecuteFetch(cmd, -1, false)
 		if err != nil {
 			return err
@@ -621,7 +621,7 @@ func (db *LocalCluster) ExecuteFetch(sql string, dbname string) (*sqltypes.Resul
 	}
 	defer conn.Close()
 
-	log.Infof("ExecuteFetch(%s): \"%s\"", dbname, sql)
+	//	log.Infof("ExecuteFetch(%s): \"%s\"", dbname, sql)
 	rs, err := conn.ExecuteFetch(sql, -1, true)
 	return rs, err
 }
