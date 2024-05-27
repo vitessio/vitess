5ae9c113fd23dbceef3574ef1e0236e055777741
Manan Gupta <manan@planetscale.com>
2024-05-27 13:47:54 +0530

    test: print the pod status of vttablet to see what the problem is
    Signed-off-by: Manan Gupta <manan@planetscale.com>

--- test/vtop_example.sh
+++ test/vtop_example.sh
@@ -68,6 +68,8 @@ function checkPodStatusWithTimeout() {
     sleep 1
   done
   echo -e "ERROR: checkPodStatusWithTimeout timeout to find pod matching:\ngot:\n$out\nfor regex: $regex"
+  vttabletPod=$(kubectl get pods | grep -E "vttablet" | head -n 1 | awk '{print $1}')
+  kubectl describe pod $vttabletPod
   exit 1
 }
 
