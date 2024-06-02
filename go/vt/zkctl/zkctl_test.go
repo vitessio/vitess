68318b4f289549b6237198e4ca97d5b50b26c32d
Ari1009 <arihant0pal@gmail.com>
2024-06-02 12:45:20 +0530

    test Signed-off-by: Arihant Pal<arihant0pal@gmail.com>

--- go/vt/zkctl/zkctl_test.go
+++ go/vt/zkctl/zkctl_test.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,13 +13,14 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
 package zkctl
 
 import (
 	"fmt"
-	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 // This test depend on starting and stopping a ZK instance,
@@ -39,29 +40,21 @@ func TestLifeCycle(t *testing.T) {
 	adminServerCfg := "admin.serverPort=8081"
 	zkConf.Extra = []string{tpcKeepAliveCfg, adminServerCfg}
 
-	if zkObservedConf, err := MakeZooCfg([]string{zkConf.ConfigFile()}, zkConf, "header"); err != nil {
-		t.Fatalf("MakeZooCfg err: %v", err)
-	} else if !strings.Contains(zkObservedConf, fmt.Sprintf("\n%s\n", tpcKeepAliveCfg)) {
-		t.Fatalf("Expected tpcKeepAliveCfg in zkObservedConf")
-	} else if !strings.Contains(zkObservedConf, fmt.Sprintf("\n%s\n", adminServerCfg)) {
-		t.Fatalf("Expected adminServerCfg in zkObservedConf")
-	}
+	zkObservedConf, err := MakeZooCfg([]string{zkConf.ConfigFile()}, zkConf, "header")
+	require.NoError(t, err, "MakeZooCfg err: %v", err)
+	assert.Contains(t, zkObservedConf, fmt.Sprintf("\n%s\n", tpcKeepAliveCfg), "Expected tpcKeepAliveCfg in zkObservedConf")
+	assert.Contains(t, zkObservedConf, fmt.Sprintf("\n%s\n", adminServerCfg), "Expected adminServerCfg in zkObservedConf")
 
 	zkd := NewZkd(zkConf)
-	if err := zkd.Init(); err != nil {
-		t.Fatalf("Init() err: %v", err)
-	}
+	err = zkd.Init()
+	require.NoError(t, err, "Init() err: %v", err)
 
-	if err := zkd.Shutdown(); err != nil {
-		t.Fatalf("Shutdown() err: %v", err)
-	}
+	err = zkd.Shutdown()
+	require.NoError(t, err, "Shutdown() err: %v", err)
 
-	if err := zkd.Start(); err != nil {
-		t.Fatalf("Start() err: %v", err)
-	}
+	err = zkd.Start()
+	require.NoError(t, err, "Start() err: %v", err)
 
-	if err := zkd.Teardown(); err != nil {
-		t.Fatalf("Teardown() err: %v", err)
-	}
-
+	err = zkd.Teardown()
+	require.NoError(t, err, "Teardown() err: %v", err)
 }
