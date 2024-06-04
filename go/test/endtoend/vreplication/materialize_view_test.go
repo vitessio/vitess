c9baeb68169e7db89b218c05271f10bfe5ae8afc
Rohit Nayak <rohit@planetscale.com>
2024-06-04 12:24:03 -0700

    Working e2e with insert, update and delete
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>

--- go/test/endtoend/vreplication/materialize_view_test.go
+++ go/test/endtoend/vreplication/materialize_view_test.go
@@ -2,7 +2,11 @@ package vreplication
 
 import (
 	"fmt"
+	"strings"
 	"testing"
+	"time"
+
+	"vitess.io/vitess/go/mysql"
 	"vitess.io/vitess/go/sqltypes"
 	binlogdatapb "vitess.io/vitess/go/vt/proto/binlogdata"
 
@@ -45,7 +49,7 @@ func TestMaterializeView(t *testing.T) {
 	workflow := "wf1"
 	sourceKeyspace := "product"
 	targetKeyspace := "product"
-	viewQuery := "select /*vt+ view=orders_view */ o.oid, c.cid, p.pid, o.mname, o.qty, o.price, c.name, p.description from orders o join customer c on o.cid = c.cid join product p on p.pid = o.pid;"
+	viewQuery := "select /*vt+ view=orders_view */ orders.oid, customer.cid, product.pid, orders.mname, orders.qty, orders.price, customer.name, product.description from orders join customer on orders.cid = customer.cid join product on product.pid = orders.pid;"
 	tableSettings := fmt.Sprintf("[ {\"target_table\": \"orders_view\", \"source_expression\": \"%s\"  }]", viewQuery)
 
 	output, err := vc.VtctldClient.ExecuteCommandWithOutput("materialize", "--workflow", workflow, "--target-keyspace", targetKeyspace,
@@ -54,18 +58,52 @@ func TestMaterializeView(t *testing.T) {
 	require.NoError(t, err, "Materialize")
 
 	waitForWorkflowState(t, vc, fmt.Sprintf("%s.%s", targetKeyspace, workflow), binlogdatapb.VReplicationWorkflowState_Running.String())
+	diffViews(t, vtgateConn, viewQuery, "After copy phase")
 
-	rsMaterializedView := execVtgateQuery(t, vtgateConn, "product", "select * from orders_view")
-	rsViewQuery := execVtgateQuery(t, vtgateConn, "product", viewQuery)
-	diffResults(t, rsMaterializedView, rsViewQuery)
+	queries := []string{
+		"insert into orders (oid, cid, pid, mname, qty, price) values (11, 1, 1, 'mname1', 1, 1100)",
+		"update orders set qty = 2 where oid = 11",
+		"insert into orders (oid, cid, pid, mname, qty, price) values (12, 1, 1, 'mname1', 1, 1200)",
+	}
+	for _, query := range queries {
+		execVtgateQuery(t, vtgateConn, "product", query)
+	}
+	time.Sleep(3 * time.Second)
+	diffViews(t, vtgateConn, viewQuery, "After DMLs")
 
+	queries = []string{
+		"delete from orders where oid = 11",
+	}
+	for _, query := range queries {
+		execVtgateQuery(t, vtgateConn, "product", query)
+	}
+	time.Sleep(3 * time.Second)
+	diffViews(t, vtgateConn, viewQuery, "After Delete")
+
+	queries = []string{
+		"update orders set qty = 3 where oid = 12",
+	}
+	for _, query := range queries {
+		execVtgateQuery(t, vtgateConn, "product", query)
+	}
+	time.Sleep(3 * time.Second)
+	diffViews(t, vtgateConn, viewQuery, "After Update")
 }
 
-func diffResults(t *testing.T, rs1, rs2 *sqltypes.Result) {
+func diffViews(t *testing.T, vtgateConn *mysql.Conn, viewQuery, message string) {
+	rsMaterializedView := execVtgateQuery(t, vtgateConn, "product", "select * from orders_view order by oid")
+	rsViewQuery := execVtgateQuery(t, vtgateConn, "product", strings.Replace(viewQuery, ";", " order by oid;", 1))
+	diffResults(t, rsViewQuery, rsMaterializedView, message)
+}
+
+func diffResults(t *testing.T, rs1, rs2 *sqltypes.Result, message string) {
+	if len(rs1.Rows) != len(rs2.Rows) {
+		t.Fatalf("%s:: Row count mismatch: %d != %d", message, len(rs1.Rows), len(rs2.Rows))
+	}
 	for i := range rs1.Rows {
 		for j := range rs1.Rows[i] {
 			if rs1.Rows[i][j].RawStr() != rs2.Rows[i][j].RawStr() {
-				t.Fatalf("Row %d, Column %d: %v != %v", i, j, rs1.Rows[i][j].RawStr(), rs2.Rows[i][j].RawStr())
+				t.Fatalf("%s:: Row %d, Column %d: %v != %v", message, i, j, rs1.Rows[i][j].RawStr(), rs2.Rows[i][j].RawStr())
 			}
 		}
 	}
