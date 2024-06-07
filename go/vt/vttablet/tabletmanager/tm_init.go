d5b53d064263ed29a9901d828f66e573746861cb
Vitaliy Mogilevskiy <oracle.ux@gmail.com>
2024-06-07 12:03:57 -0700

    forward ports slack specific v14 PRS fix (#404)
    Co-authored-by: Tim Vaillancourt <tim@timvaillancourt.com>

--- go/vt/vttablet/tabletmanager/tm_init.go
+++ go/vt/vttablet/tabletmanager/tm_init.go
@@ -211,6 +211,8 @@ type TabletManager struct {
 	_lockTablesTimer      *time.Timer
 	// _isBackupRunning tells us whether there is a backup that is currently running
 	_isBackupRunning bool
+	// _isSetReplicationSourceLockedRunning indicates we are actively running setReplicationSourceLocked
+	_isSetReplicationSourceLockedRunning bool
 }
 
 // BuildTabletFromInput builds a tablet record from input parameters.
