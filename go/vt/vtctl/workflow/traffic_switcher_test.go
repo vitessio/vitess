695539890fa6ec6122e5cbc8ec9d381cbe355980
Matt Lord <mattalord@gmail.com>
2024-06-18 14:10:04 -0400

    VReplication: handle escaped identifiers in vschema when initializing sequence tables (#16169)
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/vt/vtctl/workflow/traffic_switcher_test.go
+++ go/vt/vtctl/workflow/traffic_switcher_test.go
@@ -17,10 +17,17 @@ limitations under the License.
 package workflow
 
 import (
+	"context"
+	"fmt"
+	"reflect"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
+	"vitess.io/vitess/go/vt/proto/vschema"
+	"vitess.io/vitess/go/vt/topo"
 	"vitess.io/vitess/go/vt/vtgate/vindexes"
 )
 
@@ -54,5 +61,303 @@ func TestReverseWorkflowName(t *testing.T) {
 	for _, test := range tests {
 		got := ReverseWorkflowName(test.in)
 		assert.Equal(t, test.out, got)
+	}
+}
+
+func TestGetTargetSequenceMetadata(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+	cell := "cell1"
+	workflow := "wf1"
+	table := "`t1`"
+	unescapedTable := "t1"
+	sourceKeyspace := &testKeyspace{
+		KeyspaceName: "source-ks",
+		ShardNames:   []string{"0"},
+	}
+	targetKeyspace := &testKeyspace{
+		KeyspaceName: "targetks",
+		ShardNames:   []string{"-80", "80-"},
+	}
+	vindexes := map[string]*vschema.Vindex{
+		"xxhash": {
+			Type: "xxhash",
+		},
+	}
+	env := newTestEnv(t, ctx, cell, sourceKeyspace, targetKeyspace)
+	defer env.close()
+
+	type testCase struct {
+		name          string
+		sourceVSchema *vschema.Keyspace
+		targetVSchema *vschema.Keyspace
+		want          map[string]*sequenceMetadata
+		err           string
+	}
+	tests := []testCase{
+		{
+			name: "no sequences",
+			want: nil,
+		},
+		{
+			name: "sequences with backticks and qualified table",
+			sourceVSchema: &vschema.Keyspace{
+				Vindexes: vindexes,
+				Tables: map[string]*vschema.Table{
+					"`my-seq1`": {
+						Type: "sequence",
+					},
+				},
+			},
+			targetVSchema: &vschema.Keyspace{
+				Vindexes: vindexes,
+				Tables: map[string]*vschema.Table{
+					table: {
+						ColumnVindexes: []*vschema.ColumnVindex{
+							{
+								Name:   "xxhash",
+								Column: "`my-col`",
+							},
+						},
+						AutoIncrement: &vschema.AutoIncrement{
+							Column:   "`my-col`",
+							Sequence: fmt.Sprintf("`%s`.`my-seq1`", sourceKeyspace.KeyspaceName),
+						},
+					},
+				},
+			},
+			want: map[string]*sequenceMetadata{
+				"my-seq1": {
+					backingTableName:     "my-seq1",
+					backingTableKeyspace: "source-ks",
+					backingTableDBName:   "vt_source-ks",
+					usingTableName:       unescapedTable,
+					usingTableDBName:     "vt_targetks",
+					usingTableDefinition: &vschema.Table{
+						ColumnVindexes: []*vschema.ColumnVindex{
+							{
+								Column: "my-col",
+								Name:   "xxhash",
+							},
+						},
+						AutoIncrement: &vschema.AutoIncrement{
+							Column:   "my-col",
+							Sequence: fmt.Sprintf("%s.my-seq1", sourceKeyspace.KeyspaceName),
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "sequences with backticks",
+			sourceVSchema: &vschema.Keyspace{
+				Vindexes: vindexes,
+				Tables: map[string]*vschema.Table{
+					"`my-seq1`": {
+						Type: "sequence",
+					},
+				},
+			},
+			targetVSchema: &vschema.Keyspace{
+				Vindexes: vindexes,
+				Tables: map[string]*vschema.Table{
+					table: {
+						ColumnVindexes: []*vschema.ColumnVindex{
+							{
+								Name:   "xxhash",
+								Column: "`my-col`",
+							},
+						},
+						AutoIncrement: &vschema.AutoIncrement{
+							Column:   "`my-col`",
+							Sequence: "`my-seq1`",
+						},
+					},
+				},
+			},
+			want: map[string]*sequenceMetadata{
+				"my-seq1": {
+					backingTableName:     "my-seq1",
+					backingTableKeyspace: "source-ks",
+					backingTableDBName:   "vt_source-ks",
+					usingTableName:       unescapedTable,
+					usingTableDBName:     "vt_targetks",
+					usingTableDefinition: &vschema.Table{
+						ColumnVindexes: []*vschema.ColumnVindex{
+							{
+								Column: "my-col",
+								Name:   "xxhash",
+							},
+						},
+						AutoIncrement: &vschema.AutoIncrement{
+							Column:   "my-col",
+							Sequence: "my-seq1",
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "invalid table name",
+			sourceVSchema: &vschema.Keyspace{
+				Vindexes: vindexes,
+				Tables: map[string]*vschema.Table{
+					"`my-`seq1`": {
+						Type: "sequence",
+					},
+				},
+			},
+			targetVSchema: &vschema.Keyspace{
+				Vindexes: vindexes,
+				Tables: map[string]*vschema.Table{
+					table: {
+						ColumnVindexes: []*vschema.ColumnVindex{
+							{
+								Name:   "xxhash",
+								Column: "`my-col`",
+							},
+						},
+						AutoIncrement: &vschema.AutoIncrement{
+							Column:   "`my-col`",
+							Sequence: "`my-seq1`",
+						},
+					},
+				},
+			},
+			err: "invalid table name `my-`seq1` in keyspace source-ks: UnescapeID err: unexpected single backtick at position 3 in 'my-`seq1'",
+		},
+		{
+			name: "invalid keyspace name",
+			sourceVSchema: &vschema.Keyspace{
+				Vindexes: vindexes,
+				Tables: map[string]*vschema.Table{
+					"`my-seq1`": {
+						Type: "sequence",
+					},
+				},
+			},
+			targetVSchema: &vschema.Keyspace{
+				Vindexes: vindexes,
+				Tables: map[string]*vschema.Table{
+					table: {
+						ColumnVindexes: []*vschema.ColumnVindex{
+							{
+								Name:   "xxhash",
+								Column: "`my-col`",
+							},
+						},
+						AutoIncrement: &vschema.AutoIncrement{
+							Column:   "`my-col`",
+							Sequence: "`ks`1`.`my-seq1`",
+						},
+					},
+				},
+			},
+			err: "invalid keyspace in qualified sequence table name `ks`1`.`my-seq1` defined in sequence table column_vindexes:{column:\"`my-col`\" name:\"xxhash\"} auto_increment:{column:\"`my-col`\" sequence:\"`ks`1`.`my-seq1`\"}: UnescapeID err: unexpected single backtick at position 2 in 'ks`1'",
+		},
+		{
+			name: "invalid auto-inc column name",
+			sourceVSchema: &vschema.Keyspace{
+				Vindexes: vindexes,
+				Tables: map[string]*vschema.Table{
+					"`my-seq1`": {
+						Type: "sequence",
+					},
+				},
+			},
+			targetVSchema: &vschema.Keyspace{
+				Vindexes: vindexes,
+				Tables: map[string]*vschema.Table{
+					table: {
+						ColumnVindexes: []*vschema.ColumnVindex{
+							{
+								Name:   "xxhash",
+								Column: "`my-col`",
+							},
+						},
+						AutoIncrement: &vschema.AutoIncrement{
+							Column:   "`my`-col`",
+							Sequence: "`my-seq1`",
+						},
+					},
+				},
+			},
+			err: "invalid auto-increment column name `my`-col` defined in sequence table column_vindexes:{column:\"my-col\" name:\"xxhash\"} auto_increment:{column:\"`my`-col`\" sequence:\"my-seq1\"}: UnescapeID err: unexpected single backtick at position 2 in 'my`-col'",
+		},
+		{
+			name: "invalid sequence name",
+			sourceVSchema: &vschema.Keyspace{
+				Vindexes: vindexes,
+				Tables: map[string]*vschema.Table{
+					"`my-seq1`": {
+						Type: "sequence",
+					},
+				},
+			},
+			targetVSchema: &vschema.Keyspace{
+				Vindexes: vindexes,
+				Tables: map[string]*vschema.Table{
+					table: {
+						ColumnVindexes: []*vschema.ColumnVindex{
+							{
+								Name:   "xxhash",
+								Column: "`my-col`",
+							},
+						},
+						AutoIncrement: &vschema.AutoIncrement{
+							Column:   "`my-col`",
+							Sequence: "`my-`seq1`",
+						},
+					},
+				},
+			},
+			err: "invalid sequence table name `my-`seq1` defined in sequence table column_vindexes:{column:\"`my-col`\" name:\"xxhash\"} auto_increment:{column:\"`my-col`\" sequence:\"`my-`seq1`\"}: UnescapeID err: unexpected single backtick at position 3 in 'my-`seq1'",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			err := env.ts.SaveVSchema(ctx, sourceKeyspace.KeyspaceName, tc.sourceVSchema)
+			require.NoError(t, err)
+			err = env.ts.SaveVSchema(ctx, targetKeyspace.KeyspaceName, tc.targetVSchema)
+			require.NoError(t, err)
+			err = env.ts.RebuildSrvVSchema(ctx, nil)
+			require.NoError(t, err)
+			sources := make(map[string]*MigrationSource, len(sourceKeyspace.ShardNames))
+			targets := make(map[string]*MigrationTarget, len(targetKeyspace.ShardNames))
+			for i, shard := range sourceKeyspace.ShardNames {
+				tablet := env.tablets[sourceKeyspace.KeyspaceName][startingSourceTabletUID+(i*tabletUIDStep)]
+				sources[shard] = &MigrationSource{
+					primary: &topo.TabletInfo{
+						Tablet: tablet,
+					},
+				}
+			}
+			for i, shard := range targetKeyspace.ShardNames {
+				tablet := env.tablets[targetKeyspace.KeyspaceName][startingTargetTabletUID+(i*tabletUIDStep)]
+				targets[shard] = &MigrationTarget{
+					primary: &topo.TabletInfo{
+						Tablet: tablet,
+					},
+				}
+			}
+			ts := &trafficSwitcher{
+				id:             1,
+				ws:             env.ws,
+				workflow:       workflow,
+				tables:         []string{table},
+				sourceKeyspace: sourceKeyspace.KeyspaceName,
+				targetKeyspace: targetKeyspace.KeyspaceName,
+				sources:        sources,
+				targets:        targets,
+			}
+			got, err := ts.getTargetSequenceMetadata(ctx)
+			if tc.err != "" {
+				require.EqualError(t, err, tc.err)
+			} else {
+				require.NoError(t, err)
+			}
+			require.True(t, reflect.DeepEqual(tc.want, got), "want: %v, got: %v", tc.want, got)
+		})
 	}
 }
