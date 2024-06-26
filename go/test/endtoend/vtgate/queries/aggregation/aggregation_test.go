d05f9827e3f3a95dda0451bfeb68bd1814fe97dc
Florent Poinsard <35779988+frouioui@users.noreply.github.com>
2024-06-26 08:59:37 -0600

    Update go/test/endtoend/vtgate/queries/aggregation/aggregation_test.go
    Signed-off-by: Florent Poinsard <35779988+frouioui@users.noreply.github.com>
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- go/test/endtoend/vtgate/queries/aggregation/aggregation_test.go
+++ go/test/endtoend/vtgate/queries/aggregation/aggregation_test.go
@@ -71,7 +71,6 @@ func start(t *testing.T) (utils.MySQLCompare, func()) {
 }
 
 func TestAggrWithLimit(t *testing.T) {
-	utils.SkipIfBinaryIsBelowVersion(t, 20, "vtgate")
 	mcmp, closer := start(t)
 	defer closer()
 
