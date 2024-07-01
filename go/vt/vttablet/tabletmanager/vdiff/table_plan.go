a22ffbdb388059115243bdc790b5486e0359ff43
Matt Lord <mattalord@gmail.com>
2024-07-01 15:12:25 -0400

    Copy non in_keyrange filters to target query
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/vt/vttablet/tabletmanager/vdiff/table_plan.go
+++ go/vt/vttablet/tabletmanager/vdiff/table_plan.go
@@ -174,8 +174,14 @@ func (td *tableDiffer) buildTablePlan(dbClient binlogplayer.DBClient, dbName str
 		return nil, err
 	}
 
-	// Remove in_keyrange. It's not understood by mysql.
-	sourceSelect.Where = sel.Where // removeKeyrange(sel.Where)
+	// Copy all filters for the source.
+	sourceSelect.Where = sel.Where
+
+	// Copy all non-in_keyrange filters from the source to the target.
+	// This is important for things like multi-tenant migrations where
+	// an additional tenant_id filter is applied in the workflow.
+	targetSelect.Where = copyNonKeyRangeExpressions(sel.Where)
+
 	// The source should also perform the group by.
 	sourceSelect.GroupBy = sel.GroupBy
 	sourceSelect.OrderBy = tp.orderBy
