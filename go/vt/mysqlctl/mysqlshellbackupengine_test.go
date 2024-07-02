58c21c451d4f8abd4a90a793cc03aeabdd2f1f00
Renan Rangel <rrangel@slack-corp.com>
2024-07-02 01:23:03 -0700

    change flags
    Signed-off-by: 'Renan Rangel' <rrangel@slack-corp.com>

--- go/vt/mysqlctl/mysqlshellbackupengine_test.go
+++ go/vt/mysqlctl/mysqlshellbackupengine_test.go
@@ -4,12 +4,71 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
 	tabletmanagerdatapb "vitess.io/vitess/go/vt/proto/tabletmanagerdata"
 )
 
+func TestMySQLShellBackupBackupPreCheck(t *testing.T) {
+	originalLocation := mysqlShellBackupLocation
+	originalFlags := mysqlShellFlags
+	defer func() {
+		mysqlShellBackupLocation = originalLocation
+		mysqlShellFlags = originalFlags
+	}()
+
+	engine := MySQLShellBackupEngine{}
+	tests := []struct {
+		name     string
+		location string
+		flags    string
+		err      error
+	}{
+		{
+			"empty flags",
+			"",
+			`{}`,
+			MySQLShellPreCheckError,
+		},
+		{
+			"only location",
+			"/dev/null",
+			"",
+			MySQLShellPreCheckError,
+		},
+		{
+			"only flags",
+			"",
+			"--js",
+			MySQLShellPreCheckError,
+		},
+		{
+			"both values present but without --js",
+			"",
+			"-h localhost",
+			MySQLShellPreCheckError,
+		},
+		{
+			"supported values",
+			"/tmp/backup/",
+			"--js -h localhost",
+			nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			mysqlShellBackupLocation = tt.location
+			mysqlShellFlags = tt.flags
+			assert.ErrorIs(t, engine.backupPreCheck(), tt.err)
+		})
+	}
+
+}
+
 func TestMySQLShellBackupRestorePreCheck(t *testing.T) {
-	original := *mysqlShellLoadFlags
-	defer func() { *mysqlShellLoadFlags = original }()
+	original := mysqlShellLoadFlags
+	defer func() { mysqlShellLoadFlags = original }()
 
 	engine := MySQLShellBackupEngine{}
 	tests := []struct {
@@ -45,24 +104,26 @@ func TestMySQLShellBackupRestorePreCheck(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		*mysqlShellLoadFlags = tt.flags
-		assert.ErrorIs(t, engine.restorePreCheck(), tt.err)
+		t.Run(tt.name, func(t *testing.T) {
+			mysqlShellLoadFlags = tt.flags
+			assert.ErrorIs(t, engine.restorePreCheck(), tt.err)
+		})
 	}
 
 }
 
 func TestShouldDrainForBackupMySQLShell(t *testing.T) {
-	original := *mysqlShellBackupShouldDrain
-	defer func() { *mysqlShellBackupShouldDrain = original }()
+	original := mysqlShellBackupShouldDrain
+	defer func() { mysqlShellBackupShouldDrain = original }()
 
 	engine := MySQLShellBackupEngine{}
 
-	*mysqlShellBackupShouldDrain = false
+	mysqlShellBackupShouldDrain = false
 
 	assert.False(t, engine.ShouldDrainForBackup(nil))
 	assert.False(t, engine.ShouldDrainForBackup(&tabletmanagerdatapb.BackupRequest{}))
 
-	*mysqlShellBackupShouldDrain = true
+	mysqlShellBackupShouldDrain = true
 
 	assert.True(t, engine.ShouldDrainForBackup(nil))
 	assert.True(t, engine.ShouldDrainForBackup(&tabletmanagerdatapb.BackupRequest{}))
