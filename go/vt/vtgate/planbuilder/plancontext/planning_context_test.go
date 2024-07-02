172490ffcd395acde55cacc7aa13f47d7264b366
Andres Taylor <andres@planetscale.com>
2024-07-02 12:01:30 +0200

    feat: use the evalengine typer to get types for most expressions
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/vt/vtgate/planbuilder/plancontext/planning_context_test.go
+++ go/vt/vtgate/planbuilder/plancontext/planning_context_test.go
@@ -17,6 +17,8 @@ limitations under the License.
 package plancontext
 
 import (
+	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -24,11 +26,79 @@ import (
 
 	"vitess.io/vitess/go/mysql/collations"
 	"vitess.io/vitess/go/sqltypes"
+	"vitess.io/vitess/go/vt/key"
+	querypb "vitess.io/vitess/go/vt/proto/query"
+	topodatapb "vitess.io/vitess/go/vt/proto/topodata"
+	vschemapb "vitess.io/vitess/go/vt/proto/vschema"
+	vtgatepb "vitess.io/vitess/go/vt/proto/vtgate"
+	"vitess.io/vitess/go/vt/vtenv"
+	"vitess.io/vitess/go/vt/vtgate/engine"
 	"vitess.io/vitess/go/vt/vtgate/evalengine"
+	"vitess.io/vitess/go/vt/vtgate/vindexes"
 
 	"vitess.io/vitess/go/vt/sqlparser"
 	"vitess.io/vitess/go/vt/vtgate/semantics"
 )
+
+func TestTyping(t *testing.T) {
+	// this test checks that PlanningContext can take an expression with only columns typed and
+	// return the type of the full expression
+	// col1 is a bigint, and col2 is a varchar
+	expr, err := sqlparser.NewTestParser().ParseExpr("sum(length(col1)) + avg(acos(col2))")
+	require.NoError(t, err)
+	semTable := semantics.EmptySemTable()
+	var sum, avg, col1, col2, length, acos sqlparser.Expr
+
+	// here we walk the expression tree and fetch the two aggregate functions, and set the types for the columns
+	_ = sqlparser.Walk(func(node sqlparser.SQLNode) (kontinue bool, err error) {
+		switch node := node.(type) {
+		case *sqlparser.ColName:
+			switch node.Name.String() {
+			case "col1":
+				semTable.ExprTypes[node] = evalengine.NewType(sqltypes.Int64, collations.Unknown)
+				col1 = node
+			case "col2":
+				semTable.ExprTypes[node] = evalengine.NewType(sqltypes.VarChar, collations.Unknown)
+				col2 = node
+			}
+		case *sqlparser.FuncExpr:
+			switch node.Name.Lowered() {
+			case "length":
+				length = node
+			case "acos":
+				acos = node
+			}
+
+		case *sqlparser.Sum:
+			sum = node
+		case *sqlparser.Avg:
+			avg = node
+		}
+
+		return true, nil
+	}, expr)
+
+	ctx := createPlanContext(semTable)
+
+	expectations := map[sqlparser.Expr]sqltypes.Type{
+		sum:    sqltypes.Decimal,
+		avg:    sqltypes.Float64,
+		col1:   sqltypes.Int64,
+		col2:   sqltypes.VarChar,
+		expr:   sqltypes.Float64,
+		length: sqltypes.Int64,
+		acos:   sqltypes.Float64,
+	}
+	fmt.Println(sum, avg, expr, acos, col1, col2)
+
+	for expr, expected := range expectations {
+		t.Run(sqlparser.String(expr), func(t *testing.T) {
+			typ, found := ctx.TypeForExpr(expr)
+			require.True(t, found)
+			require.Equal(t, expected, typ.Type())
+		})
+	}
+}
 
 func TestOuterTableNullability(t *testing.T) {
 	// Tests that columns from outer tables are nullable,
@@ -96,13 +166,202 @@ func prepareContextAndFindColumns(t *testing.T, query string) (ctx *PlanningCont
 		return false, nil
 	}, nil, expr)
 
-	ctx = &PlanningContext{
-		SemTable:          semTable,
+	ctx = createPlanContext(semTable)
+	ctx.Statement = stmt
+	ctx.OuterTables = t2
+
+	return
+}
+
+func createPlanContext(st *semantics.SemTable) *PlanningContext {
+	return &PlanningContext{
+		SemTable:          st,
 		joinPredicates:    map[sqlparser.Expr][]sqlparser.Expr{},
 		skipPredicates:    map[sqlparser.Expr]any{},
 		ReservedArguments: map[sqlparser.Expr]string{},
-		Statement:         stmt,
-		OuterTables:       t2, // t2 is the outer table.
+		VSchema:           &vschema{},
 	}
-	return
 }
+
+type vschema struct{}
+
+func (v *vschema) FindTable(tablename sqlparser.TableName) (*vindexes.Table, string, topodatapb.TabletType, key.Destination, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) FindView(name sqlparser.TableName) sqlparser.SelectStatement {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) FindTableOrVindex(tablename sqlparser.TableName) (*vindexes.Table, vindexes.Vindex, string, topodatapb.TabletType, key.Destination, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) DefaultKeyspace() (*vindexes.Keyspace, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) TargetString() string {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) Destination() key.Destination {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) TabletType() topodatapb.TabletType {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) TargetDestination(qualifier string) (key.Destination, *vindexes.Keyspace, topodatapb.TabletType, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) AnyKeyspace() (*vindexes.Keyspace, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) FirstSortedKeyspace() (*vindexes.Keyspace, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) SysVarSetEnabled() bool {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) KeyspaceExists(keyspace string) bool {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) AllKeyspace() ([]*vindexes.Keyspace, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) FindKeyspace(keyspace string) (*vindexes.Keyspace, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) GetSemTable() *semantics.SemTable {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) Planner() PlannerVersion {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) SetPlannerVersion(pv PlannerVersion) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) ConnCollation() collations.ID {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) Environment() *vtenv.Environment {
+	return vtenv.NewTestEnv()
+}
+
+func (v *vschema) ErrorIfShardedF(keyspace *vindexes.Keyspace, warn, errFmt string, params ...any) error {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) WarnUnshardedOnly(format string, params ...any) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) PlannerWarning(message string) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) ForeignKeyMode(keyspace string) (vschemapb.Keyspace_ForeignKeyMode, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) KeyspaceError(keyspace string) error {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) GetForeignKeyChecksState() *bool {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) GetVSchema() *vindexes.VSchema {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) GetSrvVschema() *vschemapb.SrvVSchema {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) FindRoutedShard(keyspace, shard string) (string, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) IsShardRoutingEnabled() bool {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) IsViewsEnabled() bool {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) GetUDV(name string) *querypb.BindVariable {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) PlanPrepareStatement(ctx context.Context, query string) (*engine.Plan, sqlparser.Statement, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) ClearPrepareData(stmtName string) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) GetPrepareData(stmtName string) *vtgatepb.PrepareData {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) StorePrepareData(name string, pd *vtgatepb.PrepareData) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (v *vschema) GetAggregateUDFs() []string {
+	// TODO implement me
+	panic("implement me")
+}
+
+var _ VSchema = (*vschema)(nil)
