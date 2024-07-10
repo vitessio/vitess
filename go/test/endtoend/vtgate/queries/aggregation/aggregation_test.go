bce13f7887dcd999524454b1856f8a95152c52b1
Andres Taylor <andres@planetscale.com>
2024-07-10 09:54:16 +0200

    add e2e test
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/test/endtoend/vtgate/queries/aggregation/aggregation_test.go
+++ go/test/endtoend/vtgate/queries/aggregation/aggregation_test.go
@@ -433,7 +433,8 @@ func TestOrderByCount(t *testing.T) {
 
 	mcmp.Exec("insert into t9(id1, id2, id3) values(1, '1', '1'), (2, '2', '2'), (3, '2', '2'), (4, '3', '3'), (5, '3', '3'), (6, '3', '3')")
 
-	mcmp.AssertMatches("SELECT t9.id2 FROM t9 GROUP BY t9.id2 ORDER BY COUNT(t9.id2) DESC", `[[VARCHAR("3")] [VARCHAR("2")] [VARCHAR("1")]]`)
+	mcmp.Exec("SELECT t9.id2 FROM t9 GROUP BY t9.id2 ORDER BY COUNT(t9.id2) DESC")
+	mcmp.Exec("select COUNT(*) from (select 1 as one FROM t9 WHERE id3 = 3 ORDER BY id1 DESC LIMIT 3 OFFSET 0) subquery_for_count")
 }
 
 func TestAggregateAnyValue(t *testing.T) {
