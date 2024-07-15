2b3e796e1a234d84938cb51f98c90ef662fea5f0
Florent Poinsard <florent.poinsard@outlook.fr>
2024-07-15 17:29:20 -0600

    Add tbl table in sql schema
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- go/test/endtoend/vtgate/queries/misc/schema.sql
+++ go/test/endtoend/vtgate/queries/misc/schema.sql
@@ -3,3 +3,10 @@ create table if not exists t1(
                    id2 bigint,
                    primary key(id1)
 ) Engine=InnoDB;
+
+create table if not exists tbl(
+                    id bigint,
+                    unq_col bigint,
+                    nonunq_col bigint,
+                    primary key(id)
+) Engine=InnoDB;
