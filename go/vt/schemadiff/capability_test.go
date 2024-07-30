e90c3c2a402a3e544cd2b7359ef40f33d37e92d9
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2024-07-30 14:55:20 +0300

    schemadiff: support INSTANT DDL for changing column visibility
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/schemadiff/capability_test.go
+++ go/vt/schemadiff/capability_test.go
@@ -19,6 +19,7 @@ func TestAlterTableCapableOfInstantDDL(t *testing.T) {
 			capabilities.InstantAddDropVirtualColumnFlavorCapability,
 			capabilities.InstantAddDropColumnFlavorCapability,
 			capabilities.InstantChangeColumnDefaultFlavorCapability,
+			capabilities.InstantChangeColumnVisibilityCapability,
 			capabilities.InstantExpandEnumCapability:
 			return true, nil
 		}
@@ -285,10 +286,22 @@ func TestAlterTableCapableOfInstantDDL(t *testing.T) {
 			expectCapableOfInstantDDL: true,
 		},
 		{
-			name:                      "make a column invisible via SET, unsupported",
+			name:                      "make a column visible with rename",
+			create:                    "create table t1 (id int, i1 int)",
+			alter:                     "alter table t1 change column i1 i2 int visible",
+			expectCapableOfInstantDDL: false,
+		},
+		{
+			name:                      "make a column invisible via SET",
 			create:                    "create table t1 (id int, i1 int)",
 			alter:                     "alter table t1 alter column i1 set invisible",
-			expectCapableOfInstantDDL: false,
+			expectCapableOfInstantDDL: true,
+		},
+		{
+			name:                      "drop column default",
+			create:                    "create table t1 (id int, i1 int)",
+			alter:                     "alter table t1 alter column i1 drop default",
+			expectCapableOfInstantDDL: true,
 		},
 	}
 	for _, tcase := range tcases {
