ddf86019ebb1bec7a020cfdc7fda10ef284d64b6
Andrés Taylor <andres@planetscale.com>
2024-08-01 14:55:51 +0200

    bugfix: Allow cross-keyspace joins (#16520)
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/vt/vtgate/planbuilder/operators/join_merging.go
+++ go/vt/vtgate/planbuilder/operators/join_merging.go
@@ -68,7 +68,7 @@ func mergeJoinInputs(ctx *plancontext.PlanningContext, lhs, rhs ops.Operator, jo
 
 	// sharded routing is complex, so we handle it in a separate method
 	case a == sharded && b == sharded:
-		return tryMergeJoinShardedRouting(ctx, lhsRoute, rhsRoute, m, joinPredicates)
+		return tryMergeShardedRouting(ctx, lhsRoute, rhsRoute, m, joinPredicates, false /*isSubquery*/)
 
 	default:
 		return nil, nil
