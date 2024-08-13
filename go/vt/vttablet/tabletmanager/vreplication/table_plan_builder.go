9dd061a120da5111cd0d0b67f1acfdd2db421ce5
Matt Lord <mattalord@gmail.com>
2024-08-12 22:14:03 -0400

    Improve table plan logs
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/vt/vttablet/tabletmanager/vreplication/table_plan_builder.go
+++ go/vt/vttablet/tabletmanager/vreplication/table_plan_builder.go
@@ -598,10 +598,12 @@ func (tpb *tablePlanBuilder) analyzePK(cols []*ColumnInfo) error {
 			// TODO(shlomi): at some point in the futue we want to make this check stricter.
 			// We could be reading a generated column c1 which in turn selects some other column c2.
 			// We will want t oensure that `c2` is found in select list...
-			return fmt.Errorf("primary key column %v not found in select list for table %s", col, tpb.name.String())
+			return fmt.Errorf("primary key column %v not found in %s table's select filter or the TableMap event in the GTID",
+				col, tpb.name)
 		}
 		if cexpr.operation != opExpr {
-			return fmt.Errorf("primary key column %v is not allowed to reference an aggregate expression for table %s", col, tpb.name.String())
+			return fmt.Errorf("primary key column %v in table %s is not allowed to reference an aggregate expression",
+				col, tpb.name)
 		}
 		cexpr.isPK = true
 		cexpr.dataType = col.DataType
