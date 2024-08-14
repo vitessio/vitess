731799a5f8ba00d77aeafa7fb022bb5d14ae71ad
Rohit Nayak <rohit@planetscale.com>
2024-08-14 10:16:34 +0200

    Fix vstream call for new signature in vdiff framework
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>

--- go/vt/vttablet/tabletmanager/vdiff/framework_test.go
+++ go/vt/vttablet/tabletmanager/vdiff/framework_test.go
@@ -213,7 +213,7 @@ func shortCircuitTestAfterQuery(query string, dbClient *binlogplayer.MockDBClien
 	dbClient.ExpectRequest("insert into _vt.vdiff_log(vdiff_id, message) values (1, 'Error: Short circuiting test')", singleRowAffected, nil)
 }
 
-//--------------------------------------
+// --------------------------------------
 // Topos and tablets
 
 // fakeTabletConn implement TabletConn interface. We only care about the
@@ -249,7 +249,7 @@ func (ftc *fakeTabletConn) VStream(ctx context.Context, request *binlogdatapb.VS
 	if vstreamHook != nil {
 		vstreamHook(ctx)
 	}
-	return vdiffenv.vse.Stream(ctx, request.Position, request.TableLastPKs, request.Filter, throttlerapp.VStreamerName, send)
+	return vdiffenv.vse.Stream(ctx, request.Position, request.TableLastPKs, request.Filter, throttlerapp.VStreamerName, send, nil)
 }
 
 // vstreamRowsHook allows you to do work just before calling VStreamRows.
@@ -283,7 +283,7 @@ func (ftc *fakeTabletConn) Close(ctx context.Context) error {
 	return nil
 }
 
-//--------------------------------------
+// --------------------------------------
 // Binlog Client to TabletManager
 
 // fakeBinlogClient satisfies binlogplayer.Client.
@@ -344,7 +344,7 @@ func (bts *btStream) Recv() (*binlogdatapb.BinlogTransaction, error) {
 	return nil, bts.ctx.Err()
 }
 
-//--------------------------------------
+// --------------------------------------
 // DBCLient wrapper
 
 func realDBClientFactory() binlogplayer.DBClient {
@@ -427,7 +427,7 @@ func (dbc *realDBClient) SupportsCapability(capability capabilities.FlavorCapabi
 	return dbc.conn.SupportsCapability(capability)
 }
 
-//----------------------------------------------
+// ----------------------------------------------
 // fakeTMClient
 
 type fakeTMClient struct {
