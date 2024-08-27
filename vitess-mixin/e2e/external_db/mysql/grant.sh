a8381d22845305235458f7004c9a6d4eed2f87a3
Dirkjan Bussink <d.bussink@gmail.com>
2024-08-27 09:18:51 +0200

    Remove the vitess-mixin
    Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

--- vitess-mixin/e2e/external_db/mysql/grant.sh
+++
@@ -1,7 +0,0 @@
-
-echo '**********GRANTING PRIVILEGES START*******************'
-echo ${mysql[@]}
-# PURGE BINARY LOGS BEFORE DATE(NOW());
-mysql --protocol=socket -uroot -hlocalhost --socket=/var/run/mysqld/mysqld.sock -p$MYSQL_ROOT_PASSWORD -e \
-"GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
-echo '*************GRANTING PRIVILEGES END****************'
--- vitess-mixin/e2e/tables/create_dinosaurs.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE dinosaurs (
-  id BIGINT UNSIGNED,
-  time_created_ns BIGINT UNSIGNED,
-  name VARCHAR(255),
-  PRIMARY KEY (id)
-) ENGINE=InnoDB;
