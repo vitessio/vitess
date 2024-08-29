e799299a67bad23ce56ef17115e3b2b059765c8b
Michael Demmer <mdemmer@slack-corp.com>
2024-08-29 08:47:25 -0700

    flow based tablet load balancer (#16351)
    Signed-off-by: Michael Demmer <mdemmer@slack-corp.com>
    Signed-off-by: Venkatraju V <venkatraju@slack-corp.com>
    Signed-off-by: Venkatraju <venkatraju@gmail.com>
    Co-authored-by: Venkatraju V <venkatraju@slack-corp.com>
    Co-authored-by: Venkatraju <venkatraju@gmail.com>

--- go/vt/vtgate/vtgate.go
+++ go/vt/vtgate/vtgate.go
@@ -377,6 +377,7 @@ func Init(
 	})
 	vtgateInst.registerDebugHealthHandler()
 	vtgateInst.registerDebugEnvHandler()
+	vtgateInst.registerDebugBalancerHandler()
 
 	initAPI(gw.hc)
 	return vtgateInst
@@ -438,6 +439,12 @@ func (vtg *VTGate) registerDebugHealthHandler() {
 			return
 		}
 		w.Write([]byte("ok"))
+	})
+}
+
+func (vtg *VTGate) registerDebugBalancerHandler() {
+	http.HandleFunc("/debug/balancer", func(w http.ResponseWriter, r *http.Request) {
+		vtg.Gateway().DebugBalancerHandler(w, r)
 	})
 }
 
