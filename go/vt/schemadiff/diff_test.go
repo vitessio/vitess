a10cacb3d0555d8b7b51cdc019964deff2add186
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2024-09-03 12:47:08 +0300

    Optimize: convert to ALTER INDEX where appropriate
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/schemadiff/diff_test.go
+++ go/vt/schemadiff/diff_test.go
@@ -916,10 +916,10 @@ func TestDiffSchemas(t *testing.T) {
 			from: `create table parent (id int primary key); create table t (id int primary key, i int, key i_idx (i), constraint f foreign key (i) references parent(id))`,
 			to:   `create table parent (id int primary key); create table t (id int primary key, i int, key i_alternative (i), constraint f foreign key (i) references parent(id))`,
 			diffs: []string{
-				"alter table t drop key i_idx, add key i_alternative (i)",
+				"alter table t rename index i_idx to i_alternative",
 			},
 			cdiffs: []string{
-				"ALTER TABLE `t` DROP KEY `i_idx`, ADD KEY `i_alternative` (`i`)",
+				"ALTER TABLE `t` RENAME INDEX `i_idx` TO `i_alternative`",
 			},
 		},
 		// Views
