02598ab50bf15a078904fa4dd44f6bb816ebe01a
Florent Poinsard <florent.poinsard@outlook.fr>
2024-09-12 17:10:46 -0600

    Small fix
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- go/vt/mysqlctl/s3backupstorage/s3.go
+++ go/vt/mysqlctl/s3backupstorage/s3.go
@@ -181,7 +181,6 @@ func (bh *S3BackupHandle) AddFile(ctx context.Context, filename string, filesize
 			u.PartSize = partSizeBytes
 		})
 		object := objName(bh.dir, bh.name, filename)
-		log.Infof("objName: %v", object)
 		sendStats := bh.bs.params.Stats.Scope(stats.Operation("AWS:Request:Send"))
 		// Using UploadWithContext breaks uploading to Minio and Ceph https://github.com/vitessio/vitess/issues/14188
 		_, err := uploader.Upload(context.Background(), &s3.PutObjectInput{
