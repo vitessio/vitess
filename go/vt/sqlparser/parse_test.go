2bc5b326e87a778ff8bf9d840670215138af7085
Andres Taylor <andres@planetscale.com>
2024-09-12 08:36:44 +0200

    add parsing & ast for vexplain trace
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/vt/sqlparser/parse_test.go
+++ go/vt/sqlparser/parse_test.go
@@ -2568,6 +2568,9 @@ var (
 		input:  "vexplain select * from t",
 		output: "vexplain plan select * from t",
 	}, {
+		input:  "vexplain trace select * from t",
+		output: "vexplain trace select * from t",
+	}, {
 		input: "explain analyze select * from t",
 	}, {
 		input: "explain format = tree select * from t",
