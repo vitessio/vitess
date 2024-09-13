47dc753d6c50244ad044a4b74c7cf5f07e7fe7fb
Renan Rangel <rrangel@slack-corp.com>
2024-09-13 05:36:59 -0700

    make sure vitess default matches mysqlshell's
    Signed-off-by: Renan Rangel <rrangel@slack-corp.com>

--- go/vt/mysqlctl/mysqlshellbackupengine.go
+++ go/vt/mysqlctl/mysqlshellbackupengine.go
@@ -49,7 +49,7 @@ var (
 	// flags passed to the mysql shell utility, used both on dump/restore
 	mysqlShellFlags = "--defaults-file=/dev/null --js -h localhost"
 	// flags passed to the Dump command, as a JSON string
-	mysqlShellDumpFlags = `{"threads": 2}`
+	mysqlShellDumpFlags = `{"threads": 4}`
 	// flags passed to the Load command, as a JSON string
 	mysqlShellLoadFlags = `{"threads": 4, "loadUsers": true, "excludeUsers": ["'root'@'localhost'"], "updateGtidSet": "replace", "skipBinlog": true, "progressFile": ""}`
 	// drain a tablet when taking a backup
