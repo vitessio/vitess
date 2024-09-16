8b57e7c9bc2c0368ff90bbf889643325b1606118
Harshit Gangal <harshit@planetscale.com>
2024-09-16 11:19:13 +0530

    Add conclude transaction command to vtctld service (#16693)
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/vt/vttablet/tabletservermock/controller.go
+++ go/vt/vttablet/tabletservermock/controller.go
@@ -91,6 +91,8 @@ type Controller struct {
 
 	// queryRulesMap has the latest query rules.
 	queryRulesMap map[string]*rules.Rules
+
+	MethodCalled map[string]bool
 }
 
 // NewController returns a mock of tabletserver.Controller
@@ -101,6 +103,7 @@ func NewController() *Controller {
 		BroadcastData:       make(chan *BroadcastData, 10),
 		StateChanges:        make(chan *StateChange, 10),
 		queryRulesMap:       make(map[string]*rules.Rules),
+		MethodCalled:        make(map[string]bool),
 	}
 }
 
@@ -235,7 +238,20 @@ func (tqsc *Controller) SetTwoPCAllowed(bool) {
 
 // UnresolvedTransactions is part of the tabletserver.Controller interface
 func (tqsc *Controller) UnresolvedTransactions(context.Context, *querypb.Target) ([]*querypb.TransactionMetadata, error) {
+	tqsc.MethodCalled["UnresolvedTransactions"] = true
 	return nil, nil
+}
+
+// ConcludeTransaction is part of the tabletserver.Controller interface
+func (tqsc *Controller) ConcludeTransaction(context.Context, *querypb.Target, string) error {
+	tqsc.MethodCalled["ConcludeTransaction"] = true
+	return nil
+}
+
+// RollbackPrepared is part of the tabletserver.Controller interface
+func (tqsc *Controller) RollbackPrepared(context.Context, *querypb.Target, string, int64) error {
+	tqsc.MethodCalled["RollbackPrepared"] = true
+	return nil
 }
 
 // EnterLameduck implements tabletserver.Controller.
