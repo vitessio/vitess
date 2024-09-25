c5fcf862a541d8a3d7087174e99333abd6aed03b
Tim Vaillancourt <tim@timvaillancourt.com>
2024-09-26 01:08:14 +0200

    Support passing filters to `discovery.NewHealthCheck(...)` (#16170)
    Signed-off-by: Tim Vaillancourt <tim@timvaillancourt.com>

--- go/vt/discovery/topology_watcher.go
+++ go/vt/discovery/topology_watcher.go
@@ -274,6 +274,19 @@ type TabletFilter interface {
 	IsIncluded(tablet *topodata.Tablet) bool
 }
 
+// TabletFilters contains filters for tablets.
+type TabletFilters []TabletFilter
+
+// IsIncluded returns true if a tablet passes all filters.
+func (tf TabletFilters) IsIncluded(tablet *topodata.Tablet) bool {
+	for _, filter := range tf {
+		if !filter.IsIncluded(tablet) {
+			return false
+		}
+	}
+	return true
+}
+
 // FilterByShard is a filter that filters tablets by
 // keyspace/shard.
 type FilterByShard struct {
