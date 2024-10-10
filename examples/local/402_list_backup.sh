8ecc7ecd8abd78093bfb6fe87862cf92194a3026
Shail Pujan <shailsing977@gmail.com>
2024-10-10 23:59:05 +0530

    Add copyright header and backup and restore scripts in local example script.
    Signed-off-by: Shail Pujan <shailsing977@gmail.com>

--- examples/local/402_list_backup.sh
+++ examples/local/402_list_backup.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 # Copyright 2024 The Vitess Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -7,6 +6,12 @@
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Load common environment variables and functions
 source ../common/env.sh  # Import necessary environment variables and functions from a common script
 
