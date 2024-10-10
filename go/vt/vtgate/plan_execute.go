cd0a3924f17273b0c6b0071e2c4a7eb30fda9639
Rohit Nayak <rohit@planetscale.com>
2024-10-10 21:10:10 +0200

    Address code review
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>

--- go/vt/vtgate/plan_execute.go
+++ go/vt/vtgate/plan_execute.go
@@ -105,7 +105,7 @@ func (e *Executor) newExecute(
 			// within the given window of time for most queries and we should not end up waiting too long
 			// after the traffic switch fails or the buffer window has ended, retrying old queries.
 			timeout := 30 * time.Second
-			if e.resolver.scatterConn.gateway.buffer != nil {
+			if e.resolver.scatterConn.gateway.buffer != nil && e.resolver.scatterConn.gateway.buffer.GetConfig() != nil {
 				timeout = e.resolver.scatterConn.gateway.buffer.GetConfig().MaxFailoverDuration / (MaxBufferingRetries - 1)
 			}
 			if waitForNewerVSchema(ctx, e, lastVSchemaCreated, timeout) {
