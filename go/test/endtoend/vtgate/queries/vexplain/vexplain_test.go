ac11a8c1f4570844aacc9f5880eb4127d8eb48c2
Dirkjan Bussink <d.bussink@gmail.com>
2024-10-22 18:29:59 +0200

    Relax vexplain test for upcoming changes (#17035)
    Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

--- go/test/endtoend/vtgate/queries/vexplain/vexplain_test.go
+++ go/test/endtoend/vtgate/queries/vexplain/vexplain_test.go
@@ -53,6 +53,9 @@ func start(t *testing.T) (*mysql.Conn, func()) {
 }
 
 func TestVtGateVExplain(t *testing.T) {
+	if utils.BinaryIsAtLeastAtVersion(22, "vtgate") {
+		t.Skip("v22 changes the output of vexplain queries because of binary bind vars which breaks this test.")
+	}
 	conn, closer := start(t)
 	defer closer()
 
