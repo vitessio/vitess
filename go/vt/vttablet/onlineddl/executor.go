bed84fa8aa87a4f490da417cc3b5055f7b9b0b0d
Matt Lord <mattalord@gmail.com>
2024-10-25 01:01:25 -0400

    Be less aggressive in timeout extension
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/vt/vttablet/onlineddl/executor.go
+++ go/vt/vttablet/onlineddl/executor.go
@@ -954,7 +954,7 @@ func (e *Executor) cutOverVReplMigration(ctx context.Context, s *VReplStream, sh
 		// impacts query serving so we wait for a multiple of the cutover threshold here, with
 		// that variable primarily serving to limit the max time we later spend waiting for
 		// a position again AFTER we've taken the locks and table access is blocked.
-		if err := waitForPos(s, postSentryPos, migrationCutOverThreshold*5); err != nil {
+		if err := waitForPos(s, postSentryPos, migrationCutOverThreshold*3); err != nil {
 			return err
 		}
 		e.updateMigrationStage(ctx, onlineDDL.UUID, "post-sentry pos reached")
