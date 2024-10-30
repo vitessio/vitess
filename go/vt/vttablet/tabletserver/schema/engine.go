f04958ce23af674e03db2c7648f6fce99339105d
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2024-10-30 10:36:14 +0200

    use BaseShowInnodbTableSize()
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/vttablet/tabletserver/schema/engine.go
+++ go/vt/vttablet/tabletserver/schema/engine.go
@@ -31,6 +31,7 @@ import (
 	"vitess.io/vitess/go/acl"
 	"vitess.io/vitess/go/constants/sidecar"
 	"vitess.io/vitess/go/mysql"
+	"vitess.io/vitess/go/mysql/collations/charset"
 	"vitess.io/vitess/go/mysql/replication"
 	"vitess.io/vitess/go/mysql/sqlerror"
 	"vitess.io/vitess/go/sqltypes"
@@ -424,10 +425,54 @@ func (se *Engine) reload(ctx context.Context, includeStats bool) error {
 		return err
 	}
 
+	innodbTablesStats := make(map[string]*Table)
+	if includeStats {
+		if innodbTableSizesQuery := conn.Conn.BaseShowInnodbTableSizes(); innodbTableSizesQuery != "" {
+			// Since the InnoDB table size query is available to us on this MySQL version, we should use it.
+			// We therefore don't want to query for table sizes in getTableData()
+			includeStats = false
+
+			innodbResults, err := conn.Conn.Exec(ctx, innodbTableSizesQuery, maxTableCount, false)
+			if err != nil {
+				return vterrors.Wrapf(err, "in Engine.reload(), reading innodb tables")
+			}
+			for _, row := range innodbResults.Rows {
+				innodbTableName := row[0].ToString() // In the form of encoded `schema/table`
+				fileSize, _ := row[1].ToCastUint64()
+				allocatedSize, _ := row[2].ToCastUint64()
+
+				if _, ok := innodbTablesStats[innodbTableName]; !ok {
+					innodbTablesStats[innodbTableName] = &Table{}
+				}
+				// There could be multiple appearances of the same table in the result set:
+				// A table that has FULLTEXT indexes will appear once for the table itself,
+				// with total size of row data, and once for the aggregates size of all
+				// FULLTEXT indexes. We aggregate the sizes of all appearances of the same table.
+				table := innodbTablesStats[innodbTableName]
+				table.FileSize += fileSize
+				table.AllocatedSize += allocatedSize
+
+				if strings.Contains(innodbTableName, "#p#") {
+					// innodbTableName is encoded any special characters are turned into some @0-f0-f0-f value.
+					// Therefore this "#p#" here is a clear indication that we are looking at a partitioned table.
+					originalTableName := strings.Split(innodbTableName, "#p#")[0]
+					// We turn `my@002ddb/tbl_part#p#p0` into `my@002ddb/tbl_part`
+					// and aggregate the total partition sizes.
+					if _, ok := innodbTablesStats[originalTableName]; !ok {
+						innodbTablesStats[originalTableName] = &Table{}
+						originalTable := innodbTablesStats[originalTableName]
+						originalTable.FileSize += fileSize
+						originalTable.AllocatedSize += allocatedSize
+					}
+				}
+			}
+		}
+	}
 	tableData, err := getTableData(ctx, conn.Conn, includeStats)
 	if err != nil {
 		return vterrors.Wrapf(err, "in Engine.reload(), reading tables")
 	}
+
 	// On the primary tablet, we also check the data we have stored in our schema tables to see what all needs reloading.
 	shouldUseDatabase := se.isServingPrimary && se.schemaCopy
 
@@ -462,8 +507,11 @@ func (se *Engine) reload(ctx context.Context, includeStats bool) error {
 	changedTables := make(map[string]*Table)
 	// created and altered contain the names of created and altered tables for broadcast.
 	var created, altered []*Table
+	databaseName := se.cp.DBName()
 	for _, row := range tableData.Rows {
 		tableName := row[0].ToString()
+		innodbTableName := fmt.Sprintf("%s/%s", charset.TablenameToFilename(databaseName), charset.TablenameToFilename(tableName))
+		innodbTable := innodbTablesStats[innodbTableName]
 		curTables[tableName] = true
 		createTime, _ := row[2].ToCastInt64()
 		var fileSize, allocatedSize uint64
@@ -474,6 +522,9 @@ func (se *Engine) reload(ctx context.Context, includeStats bool) error {
 			// publish the size metrics
 			se.tableFileSizeGauge.Set(tableName, int64(fileSize))
 			se.tableAllocatedSizeGauge.Set(tableName, int64(allocatedSize))
+		} else if innodbTable != nil {
+			se.tableFileSizeGauge.Set(tableName, int64(innodbTable.FileSize))
+			se.tableAllocatedSizeGauge.Set(tableName, int64(innodbTable.AllocatedSize))
 		}
 
 		// Table schemas are cached by tabletserver. For each table we cache `information_schema.tables.create_time` (`tbl.CreateTime`).
@@ -501,6 +552,9 @@ func (se *Engine) reload(ctx context.Context, includeStats bool) error {
 			if includeStats {
 				tbl.FileSize = fileSize
 				tbl.AllocatedSize = allocatedSize
+			} else if innodbTable != nil {
+				tbl.FileSize = innodbTable.FileSize
+				tbl.AllocatedSize = innodbTable.AllocatedSize
 			}
 			continue
 		}
@@ -520,6 +574,9 @@ func (se *Engine) reload(ctx context.Context, includeStats bool) error {
 		if includeStats {
 			table.FileSize = fileSize
 			table.AllocatedSize = allocatedSize
+		} else if innodbTable != nil {
+			table.FileSize = innodbTable.FileSize
+			table.AllocatedSize = innodbTable.AllocatedSize
 		}
 		table.CreateTime = createTime
 		changedTables[tableName] = table
