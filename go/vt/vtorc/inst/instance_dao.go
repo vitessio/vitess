a90de33abcf5f49d9c2e2c1e7992f45e9db81a7d
Tim Vaillancourt <tim@timvaillancourt.com>
2024-11-05 23:20:52 +0100

    `vtorc`: make SQL formatting consistent
    Signed-off-by: Tim Vaillancourt <tim@timvaillancourt.com>

--- go/vt/vtorc/inst/instance_dao.go
+++ go/vt/vtorc/inst/instance_dao.go
@@ -115,7 +115,12 @@ func ExecDBWriteFunc(f func() error) error {
 func ExpireTableData(tableName string, timestampColumn string) error {
 	writeFunc := func() error {
 		_, err := db.ExecVTOrc(
-			fmt.Sprintf("delete from %s where %s < datetime('now', printf('-%%d DAY', ?))", tableName, timestampColumn),
+			fmt.Sprintf(`DELETE
+				FROM %s
+				WHERE %s < DATETIME('now', PRINTF('-%%d DAY', ?))`,
+				tableName,
+				timestampColumn,
+			),
 			config.Config.AuditPurgeDays,
 		)
 		return err
@@ -439,16 +444,16 @@ func ReadInstanceClusterAttributes(instance *Instance) (err error) {
 	var primaryExecutedGtidSet string
 	primaryDataFound := false
 
-	query := `
-			select
-					replication_depth,
-					source_host,
-					source_port,
-					ancestry_uuid,
-					executed_gtid_set
-				from database_instance
-				where hostname=? and port=?
-	`
+	query := `SELECT
+		replication_depth,
+		source_host,
+		source_port,
+		ancestry_uuid,
+		executed_gtid_set
+	FROM database_instance
+	WHERE
+		hostname = ?
+		AND port = ?`
 	primaryHostname := instance.SourceHost
 	primaryPort := instance.SourcePort
 	args := sqlutils.Args(primaryHostname, primaryPort)
@@ -580,20 +585,22 @@ func readInstancesByCondition(condition string, args []any, sort string) ([](*In
 		if sort == "" {
 			sort = `alias`
 		}
-		query := fmt.Sprintf(`
-		select
-			*,
-			strftime('%%s', 'now') - strftime('%%s', last_checked) as seconds_since_last_checked,
-			ifnull(last_checked <= last_seen, 0) as is_last_check_valid,
-			strftime('%%s', 'now') - strftime('%%s', last_seen) as seconds_since_last_seen
-		from
-			vitess_tablet
-			left join database_instance using (alias, hostname, port)
-		where
-			%s
-		order by
-			%s
-			`, condition, sort)
+		query := fmt.Sprintf(`SELECT
+				*,
+				STRFTIME('%%s', 'now') - STRFTIME('%%s', last_checked) AS seconds_since_last_checked,
+				IFNULL(last_checked <= last_seen, 0) AS is_last_check_valid,
+				STRFTIME('%%s', 'now') - STRFTIME('%%s', last_seen) AS seconds_since_last_seen
+			FROM
+				vitess_tablet
+				LEFT JOIN database_instance USING (alias, hostname, port)
+			WHERE
+				%s
+			ORDER BY
+				%s
+			`,
+			condition,
+			sort,
+		)
 
 		err := db.QueryVTOrc(query, args, func(m sqlutils.RowMap) error {
 			instance := readInstanceRow(m)
@@ -614,9 +621,7 @@ func readInstancesByCondition(condition string, args []any, sort string) ([](*In
 
 // ReadInstance reads an instance from the vtorc backend database
 func ReadInstance(tabletAlias string) (*Instance, bool, error) {
-	condition := `
-			alias = ?
-		`
+	condition := `alias = ?`
 	instances, err := readInstancesByCondition(condition, sqlutils.Args(tabletAlias), "")
 	// We know there will be at most one (alias is the PK).
 	// And we expect to find one.
@@ -633,18 +638,17 @@ func ReadInstance(tabletAlias string) (*Instance, bool, error) {
 // ReadProblemInstances reads all instances with problems
 func ReadProblemInstances(keyspace string, shard string) ([](*Instance), error) {
 	condition := `
-			keyspace LIKE (CASE WHEN ? = '' THEN '%' ELSE ? END)
-			and shard LIKE (CASE WHEN ? = '' THEN '%' ELSE ? END)
-			and (
-				(last_seen < last_checked)
-				or (strftime('%%s', 'now') - strftime('%%s', last_checked) > ?)
-				or (replication_sql_thread_state not in (-1 ,1))
-				or (replication_io_thread_state not in (-1 ,1))
-				or (abs(cast(replication_lag_seconds as integer) - cast(sql_delay as integer)) > ?)
-				or (abs(cast(replica_lag_seconds as integer) - cast(sql_delay as integer)) > ?)
-				or (gtid_errant != '')
-			)
-		`
+		keyspace LIKE (CASE WHEN ? = '' THEN '%' ELSE ? END)
+		AND shard LIKE (CASE WHEN ? = '' THEN '%' ELSE ? END)
+		AND (
+			(last_seen < last_checked)
+			OR (STRFTIME('%%s', 'now') - STRFTIME('%%s', last_checked) > ?)
+			OR (replication_sql_thread_state NOT IN (-1 ,1))
+			OR (replication_io_thread_state NOT IN (-1 ,1))
+			OR (ABS(CAST(replication_lag_seconds AS integer) - CAST(sql_delay AS integer)) > ?)
+			OR (ABS(CAST(replica_lag_seconds AS integer) - CAST(sql_delay AS integer)) > ?)
+			OR (gtid_errant != '')
+		)`
 
 	args := sqlutils.Args(keyspace, keyspace, shard, shard, config.Config.InstancePollSeconds*5, config.Config.ReasonableReplicationLagSeconds, config.Config.ReasonableReplicationLagSeconds)
 	return readInstancesByCondition(condition, args, "")
@@ -653,10 +657,9 @@ func ReadProblemInstances(keyspace string, shard string) ([](*Instance), error) 
 // ReadInstancesWithErrantGTIds reads all instances with errant GTIDs
 func ReadInstancesWithErrantGTIds(keyspace string, shard string) ([]*Instance, error) {
 	condition := `
-			keyspace LIKE (CASE WHEN ? = '' THEN '%' ELSE ? END)
-			and shard LIKE (CASE WHEN ? = '' THEN '%' ELSE ? END)
-			and gtid_errant != ''
-		`
+		keyspace LIKE (CASE WHEN ? = '' THEN '%' ELSE ? END)
+		AND shard LIKE (CASE WHEN ? = '' THEN '%' ELSE ? END)
+		AND gtid_errant != ''`
 
 	args := sqlutils.Args(keyspace, keyspace, shard, shard)
 	return readInstancesByCondition(condition, args, "")
@@ -664,15 +667,14 @@ func ReadInstancesWithErrantGTIds(keyspace string, shard string) ([]*Instance, e
 
 // GetKeyspaceShardName gets the keyspace shard name for the given instance key
 func GetKeyspaceShardName(tabletAlias string) (keyspace string, shard string, err error) {
-	query := `
-		select
-			keyspace,
-			shard
-		from
-			vitess_tablet
-		where
-			alias = ?
-			`
+	query := `SELECT
+		keyspace,
+		shard
+	FROM
+		vitess_tablet
+	WHERE
+		alias = ?
+	`
 	err = db.QueryVTOrc(query, sqlutils.Args(tabletAlias), func(m sqlutils.RowMap) error {
 		keyspace = m.GetString("keyspace")
 		shard = m.GetString("shard")
@@ -695,27 +697,26 @@ func GetKeyspaceShardName(tabletAlias string) (keyspace string, shard string, er
 // the instance.
 func ReadOutdatedInstanceKeys() ([]string, error) {
 	var res []string
-	query := `
-		SELECT
-			alias
-		FROM
-			database_instance
-		WHERE
-			CASE
-				WHEN last_attempted_check <= last_checked
-				THEN last_checked < datetime('now', printf('-%d second', ?))
-				ELSE last_checked < datetime('now', printf('-%d second', ?))
-			END
-		UNION
-		SELECT
-			vitess_tablet.alias
-		FROM
-			vitess_tablet LEFT JOIN database_instance ON (
-			vitess_tablet.alias = database_instance.alias
-		)
-		WHERE
-			database_instance.alias IS NULL
-			`
+	query := `SELECT
+		alias
+	FROM
+		database_instance
+	WHERE
+		CASE
+			WHEN last_attempted_check <= last_checked
+			THEN last_checked < DATETIME('now', PRINTF('-%d SECOND', ?))
+			ELSE last_checked < DATETIME('now', PRINTF('-%d SECOND', ?))
+		END
+	UNION
+	SELECT
+		vitess_tablet.alias
+	FROM
+		vitess_tablet LEFT JOIN database_instance ON (
+		vitess_tablet.alias = database_instance.alias
+	)
+	WHERE
+		database_instance.alias IS NULL
+	`
 	args := sqlutils.Args(config.Config.InstancePollSeconds, 2*config.Config.InstancePollSeconds)
 
 	err := db.QueryVTOrc(query, args, func(m sqlutils.RowMap) error {
@@ -759,10 +760,10 @@ func mkInsert(table string, columns []string, values []string, nrRows int, inser
 
 	col := strings.Join(columns, ", ")
 	q.WriteString(fmt.Sprintf(`%s %s
-                (%s)
-        VALUES
-                %s
-        `,
+			(%s)
+		VALUES
+			%s
+		`,
 		insertStr, table, col, val.String()))
 
 	return q.String(), nil
@@ -849,13 +850,13 @@ func mkInsertForInstances(instances []*Instance, instanceWasActuallyFound bool, 
 	for i := range columns {
 		values[i] = "?"
 	}
-	values[3] = "datetime('now')" // last_checked
-	values[4] = "datetime('now')" // last_attempted_check
+	values[3] = "DATETIME('now')" // last_checked
+	values[4] = "DATETIME('now')" // last_attempted_check
 	values[5] = "1"               // last_check_partial_success
 
 	if updateLastSeen {
 		columns = append(columns, "last_seen")
-		values = append(values, "datetime('now')")
+		values = append(values, "DATETIME('now')")
 	}
 
 	var args []any
@@ -971,14 +972,13 @@ func WriteInstance(instance *Instance, instanceWasActuallyFound bool, lastError 
 // for a given instance
 func UpdateInstanceLastChecked(tabletAlias string, partialSuccess bool) error {
 	writeFunc := func() error {
-		_, err := db.ExecVTOrc(`
-        	update
-        		database_instance
-        	set
-						last_checked = datetime('now'),
-						last_check_partial_success = ?
-			where
-				alias = ?`,
+		_, err := db.ExecVTOrc(`UPDATE database_instance
+			SET
+				last_checked = DATETIME('now'),
+				last_check_partial_success = ?
+			WHERE
+				alias = ?
+			`,
 			partialSuccess,
 			tabletAlias,
 		)
@@ -1000,13 +1000,12 @@ func UpdateInstanceLastChecked(tabletAlias string, partialSuccess bool) error {
 // we have a "hanging" issue.
 func UpdateInstanceLastAttemptedCheck(tabletAlias string) error {
 	writeFunc := func() error {
-		_, err := db.ExecVTOrc(`
-    	update
-    		database_instance
-    	set
-    		last_attempted_check = datetime('now')
-			where
-				alias = ?`,
+		_, err := db.ExecVTOrc(`UPDATE database_instance
+			SET
+				last_attempted_check = DATETIME('now')
+			WHERE
+				alias = ?
+			`,
 			tabletAlias,
 		)
 		if err != nil {
@@ -1037,11 +1036,11 @@ func ForgetInstance(tabletAlias string) error {
 	currentErrantGTIDCount.Reset(tabletAlias)
 
 	// Delete from the 'vitess_tablet' table.
-	_, err := db.ExecVTOrc(`
-					delete
-						from vitess_tablet
-					where
-						alias = ?`,
+	_, err := db.ExecVTOrc(`DELETE
+		FROM vitess_tablet
+		WHERE
+			alias = ?
+		`,
 		tabletAlias,
 	)
 	if err != nil {
@@ -1050,11 +1049,11 @@ func ForgetInstance(tabletAlias string) error {
 	}
 
 	// Also delete from the 'database_instance' table.
-	sqlResult, err := db.ExecVTOrc(`
-			delete
-				from database_instance
-			where
-				alias = ?`,
+	sqlResult, err := db.ExecVTOrc(`DELETE
+		FROM database_instance
+		WHERE
+			alias = ?
+		`,
 		tabletAlias,
 	)
 	if err != nil {
@@ -1078,11 +1077,11 @@ func ForgetInstance(tabletAlias string) error {
 
 // ForgetLongUnseenInstances will remove entries of all instances that have long since been last seen.
 func ForgetLongUnseenInstances() error {
-	sqlResult, err := db.ExecVTOrc(`
-			delete
-				from database_instance
-			where
-				last_seen < datetime('now', printf('-%d hour', ?))`,
+	sqlResult, err := db.ExecVTOrc(`DELETE
+		FROM database_instance
+		WHERE
+			last_seen < DATETIME('now', PRINTF('-%d HOUR', ?))
+		`,
 		config.UnseenInstanceForgetHours,
 	)
 	if err != nil {
@@ -1103,18 +1102,26 @@ func ForgetLongUnseenInstances() error {
 // SnapshotTopologies records topology graph for all existing topologies
 func SnapshotTopologies() error {
 	writeFunc := func() error {
-		_, err := db.ExecVTOrc(`
-        	insert or ignore into
-        		database_instance_topology_history (snapshot_unix_timestamp,
-        			alias, hostname, port, source_host, source_port, keyspace, shard, version)
-        	select
-        		strftime('%s', 'now'),
-				vitess_tablet.alias, vitess_tablet.hostname, vitess_tablet.port, 
-				database_instance.source_host, database_instance.source_port, 
+		_, err := db.ExecVTOrc(`INSERT OR IGNORE
+			INTO database_instance_topology_history (
+				snapshot_unix_timestamp,
+				alias,
+				hostname,
+				port,
+				source_host,
+				source_port,
+				keyspace,
+				shard,
+				version
+			)
+			SELECT
+				STRFTIME('%s', 'now'),
+				vitess_tablet.alias, vitess_tablet.hostname, vitess_tablet.port,
+				database_instance.source_host, database_instance.source_port,
 				vitess_tablet.keyspace, vitess_tablet.shard, database_instance.version
-			from
-				vitess_tablet left join database_instance using (alias, hostname, port)
-				`,
+			FROM
+				vitess_tablet LEFT JOIN database_instance USING (alias, hostname, port)
+			`,
 		)
 		if err != nil {
 			log.Error(err)
@@ -1132,10 +1139,12 @@ func ExpireStaleInstanceBinlogCoordinates() error {
 		expireSeconds = config.StaleInstanceCoordinatesExpireSeconds
 	}
 	writeFunc := func() error {
-		_, err := db.ExecVTOrc(`
-					delete from database_instance_stale_binlog_coordinates
-					where first_seen < datetime('now', printf('-%d second', ?))
-					`, expireSeconds,
+		_, err := db.ExecVTOrc(`DELETE
+			FROM database_instance_stale_binlog_coordinates
+			WHERE
+				first_seen < DATETIME('now', PRINTF('-%d SECOND', ?))
+			`,
+			expireSeconds,
 		)
 		if err != nil {
 			log.Error(err)
@@ -1157,7 +1166,7 @@ func GetDatabaseState() (string, error) {
 		ts := tableState{
 			TableName: tableName,
 		}
-		err := db.QueryVTOrc("select * from "+tableName, nil, func(rowMap sqlutils.RowMap) error {
+		err := db.QueryVTOrc("SELECT * FROM "+tableName, nil, func(rowMap sqlutils.RowMap) error {
 			ts.Rows = append(ts.Rows, rowMap)
 			return nil
 		})
