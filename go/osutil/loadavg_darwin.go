8adc7eb62b03311f7807f8208037a8ed3f57199c
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2024-11-12 07:18:22 +0200

    OS-specific build files
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

---
+++ go/osutil/loadavg_darwin.go
@@ -0,0 +1,40 @@
+//go:build darwin
+
+/*
+Copyright 2024 The Vitess Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package osutil
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+// LoadAvg returns the past 1 minute system load average. This works on linux and darwin systems.
+// On other systems, it returns 0 with no error.
+func LoadAvg() (float64, error) {
+	cmd := exec.Command("sysctl", "-n", "vm.loadavg")
+	// Sample output: `{ 2.83 3.01 3.36 }`
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return 0, err
+	}
+	if len(output) < 1 {
+		return 0, fmt.Errorf("unexpected sysctl output: %q", output)
+	}
+	output = output[1:] // Remove the leading `{ `
+	return parseLoadAvg(string(output))
+}
