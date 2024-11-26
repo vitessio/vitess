7de1507b52848ce6d4cd96d93fed1b38f56ec16b
Rohit Nayak <rohit@planetscale.com>
2024-11-27 00:00:02 +0100

    New tool to get the shard which would be routed to, for a specific vindex and shard ranges
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>

---
+++ tools/shard-from-id/shard-from-id.go
@@ -0,0 +1,184 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"encoding/hex"
+	"fmt"
+	flag "github.com/spf13/pflag"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"vitess.io/vitess/go/sqltypes"
+	"vitess.io/vitess/go/vt/key"
+	"vitess.io/vitess/go/vt/proto/topodata"
+	"vitess.io/vitess/go/vt/vtgate/vindexes"
+)
+
+/*
+ * This is a simple tool that reads a list of values from stdin and prints the
+ * corresponding keyspace ID and shard for each value. It uses the given vindex
+ * and shard ranges to determine the shard. The vindex is expected to be a
+ * single-column vindex. The shard ranges are specified as a comma-separated
+ * list of key ranges, example "-80,80-".
+ * If you have uniformly distributed shards, you can specify the total number
+ * of shards using the -total_shards flag, and the tool will generate the shard ranges.
+ *
+ * Example usage:
+ * echo "1\n2\n3" | go run shard-from-id.go -vindex=hash -shards=-80,80-
+ *
+ * Currently tested only for integer values and hash/xxhash vindexes.
+ */
+
+func mapShard(shards map[string]*topodata.KeyRange, ksid key.DestinationKeyspaceID) (string, error) {
+	foundShard := ""
+	addShard := func(shard string) error {
+		foundShard = shard
+		return nil
+	}
+	allShards := make([]*topodata.ShardReference, 0, len(shards))
+	for shard, keyRange := range shards {
+		allShards = append(allShards, &topodata.ShardReference{
+			Name:     shard,
+			KeyRange: keyRange,
+		})
+	}
+	if err := ksid.Resolve(allShards, addShard); err != nil {
+		return "", fmt.Errorf("failed to resolve keyspace ID: %v:: %s", ksid.String(), err)
+	}
+
+	if foundShard == "" {
+		return "", fmt.Errorf("no shard found for keyspace ID: %v", ksid)
+	}
+	return foundShard, nil
+}
+
+func selectShard(vindex vindexes.Vindex, value sqltypes.Value, shards map[string]*topodata.KeyRange) (string, key.DestinationKeyspaceID, error) {
+	ctx := context.Background()
+
+	destinations, err := vindexes.Map(ctx, vindex, nil, [][]sqltypes.Value{{value}})
+	if err != nil {
+		return "", nil, fmt.Errorf("failed to map value to keyspace ID: %w", err)
+	}
+
+	if len(destinations) != 1 {
+		return "", nil, fmt.Errorf("unexpected number of destinations: %d", len(destinations))
+	}
+
+	ksid, ok := destinations[0].(key.DestinationKeyspaceID)
+	if !ok {
+		return "", nil, fmt.Errorf("unexpected destination type: %T", destinations[0])
+	}
+
+	foundShard, err := mapShard(shards, ksid)
+	if err != nil {
+		return "", nil, fmt.Errorf("failed to map shard: %w", err)
+	}
+	return foundShard, ksid, nil
+}
+
+func getValue(valueStr string) (sqltypes.Value, int64, error) {
+	var value sqltypes.Value
+	valueInt, err := strconv.ParseInt(valueStr, 10, 64)
+	if err == nil {
+		value = sqltypes.NewInt64(int64(valueInt))
+	} else {
+		valueUint, err := strconv.ParseUint(valueStr, 10, 64)
+		if err == nil {
+			value = sqltypes.NewUint64(valueUint)
+		} else {
+			value = sqltypes.NewVarChar(valueStr)
+		}
+	}
+	return value, valueInt, err
+}
+
+func getShardMap(shardsCSV *string) map[string]*topodata.KeyRange {
+	shards := make(map[string]*topodata.KeyRange)
+	for _, shard := range strings.Split(*shardsCSV, ",") {
+		parts := strings.Split(shard, "-")
+		var start, end []byte
+		if len(parts) > 0 && parts[0] != "" {
+			start = []byte(parts[0])
+		}
+		if len(parts) > 1 && parts[1] != "" {
+			end = []byte(parts[1])
+		}
+		shards[shard] = &topodata.KeyRange{Start: start, End: end}
+	}
+	return shards
+}
+
+// Assumes shard digits can be specified by two hex digits each. It won't work for > 256 shards.
+func generateUniformShardRanges(totalShards int) string {
+	shardRanges := make([]string, totalShards)
+	for i := 0; i < totalShards; i++ {
+		start := fmt.Sprintf("%x", i*256/totalShards)
+		end := fmt.Sprintf("%x", (i+1)*256/totalShards)
+		if i == totalShards-1 {
+			end = ""
+		}
+		if start == "0" {
+			start = ""
+		}
+		shardRanges[i] = fmt.Sprintf("%s-%s", start, end)
+	}
+	return strings.Join(shardRanges, ",")
+}
+
+func main() {
+	vindexName := flag.String("vindex", "hash", "name of the vindex")
+	shardsCSV := flag.String("shards", "", "comma-separated list of shard ranges")
+	totalShards := flag.Int("total_shards", 0, "total number of uniformly distributed shards")
+	flag.Parse()
+
+	if *totalShards > 0 {
+		if *shardsCSV != "" {
+			log.Fatalf("cannot specify both total_shards and shards")
+		}
+		*shardsCSV = generateUniformShardRanges(*totalShards)
+	}
+
+	if *shardsCSV == "" {
+		*shardsCSV = "-80,80-" // default for testing //FIXME: remove before merging
+		// log.Fatalf("shards or total_shards must be specified")
+	}
+	log.Printf("shardsCSV: %s\n", *shardsCSV)
+	shards := getShardMap(shardsCSV)
+
+	vindex, err := vindexes.CreateVindex(*vindexName, *vindexName, nil)
+	if err != nil {
+		log.Fatalf("failed to create vindex: %v", err)
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	first := true
+	for scanner.Scan() {
+		valueStr := scanner.Text()
+		if valueStr == "" {
+			continue
+		}
+		value, valueInt, err := getValue(valueStr)
+
+		shard, ksid, err := selectShard(vindex, value, shards)
+		if err != nil {
+			log.Printf("failed to select shard for value %d: %v", valueInt, err)
+			continue
+		}
+
+		if first {
+			// print header
+			fmt.Println("value,ksid,shard")
+			first = false
+		}
+
+		ksidStr := hex.EncodeToString([]byte(ksid))
+		fmt.Printf("%s,%s,%s\n", valueStr, ksidStr, shard)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("error reading from stdin: %v", err)
+	}
+}
