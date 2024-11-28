3af6cc8a49a6c70886e69857f7279e3b8efa7453
Rohit Nayak <rohit@planetscale.com>
2024-11-28 10:15:00 +0100

    Self-review
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>

--- tools/map-shard-for-value/map-shard-for-value.go
+++ tools/map-shard-for-value/map-shard-for-value.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2024 The Vitess Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package main
 
 import (
@@ -18,13 +34,14 @@ import (
 )
 
 /*
- * This is a simple tool that reads a list of values from stdin and prints the
+ * This tool reads a list of values from stdin and prints the
  * corresponding keyspace ID and shard for each value. It uses the given vindex
  * and shard ranges to determine the shard. The vindex is expected to be a
  * single-column vindex. The shard ranges are specified as a comma-separated
  * list of key ranges, example "-80,80-".
  * If you have uniformly distributed shards, you can specify the total number
- * of shards using the -total_shards flag, and the tool will generate the shard ranges.
+ * of shards using the -total_shards flag, and the tool will generate the shard ranges
+ * using the same logic as the Vitess operator does (using the key.GenerateShardRanges() function).
  *
  * Example usage:
  * echo "1\n2\n3" | go run shard-from-id.go -vindex=hash -shards=-80,80-
