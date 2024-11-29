7864e3adb0e54d62a7bd5aabb3e697d25faf8e60
Harshit Gangal <harshit@planetscale.com>
2024-11-29 17:35:52 +0530

    add plan equality with tracing
    Signed-off-by: Harshit Gangal <harshit@planetscale.com>

--- go/test/endtoend/vtgate/queries/plan_tests/plan_test.go
+++ go/test/endtoend/vtgate/queries/plan_tests/plan_test.go
@@ -17,184 +17,23 @@ limitations under the License.
 package plan_tests
 
 import (
-	_ "embed"
-	"encoding/json"
-	"fmt"
-	"os"
 	"testing"
 
-	"github.com/stretchr/testify/require"
-
-	"vitess.io/vitess/go/mysql"
-	"vitess.io/vitess/go/test/endtoend/cluster"
 	"vitess.io/vitess/go/test/endtoend/utils"
-	"vitess.io/vitess/go/vt/vtgate/planbuilder"
 )
 
-func readJSONTests(filename string) []planbuilder.PlanTest {
-	var output []planbuilder.PlanTest
-	file, err := os.Open(locateFile(filename))
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-	dec := json.NewDecoder(file)
-	err = dec.Decode(&output)
-	if err != nil {
-		panic(err)
-	}
-	return output
-}
-
-func locateFile(name string) string {
-	return "../../../../../vt/vtgate/planbuilder/testdata/" + name
-}
-
-func TestPlan(t *testing.T) {
+func TestSelectCases(t *testing.T) {
 	mcmp, closer := start(t)
 	defer closer()
-	tests := readJSONTests("select_cases_new.json")
+	tests := readJSONTests("select_cases.json")
 	for _, test := range tests {
-		if test.SkipE2E {
-			continue
-		}
 		mcmp.Run(test.Query, func(mcmp *utils.MySQLCompare) {
+			if test.SkipE2E {
+				mcmp.AsT().Skip(test.Query)
+			}
 			mcmp.Exec(test.Query)
-			// if mcmp.AsT().Failed() {
-			// 	tests[i].SkipE2E = true
-			// }
+			pd := utils.ExecTrace(mcmp.AsT(), mcmp.VtConn, test.Query)
+			verifyTestExpectations(mcmp.AsT(), pd, test)
 		})
 	}
-
-	// file, err := os.Create(locateFile("select_cases_new.json"))
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer file.Close()
-	//
-	// enc := json.NewEncoder(file)
-	// enc.SetEscapeHTML(false)
-	// enc.SetIndent("", "  ")
-	// err = enc.Encode(tests)
-	// require.NoError(t, err)
-}
-
-var (
-	clusterInstance *cluster.LocalProcessCluster
-	vtParams        mysql.ConnParams
-	mysqlParams     mysql.ConnParams
-	keyspaceName    = "user"
-	cell            = "test_aggr"
-)
-
-func readFile(filename string) string {
-	schema, err := os.ReadFile(locateFile(filename))
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	return string(schema)
-}
-
-func start(t *testing.T) (utils.MySQLCompare, func()) {
-	mcmp, err := utils.NewMySQLCompare(t, vtParams, mysqlParams)
-	require.NoError(t, err)
-	return mcmp, func() {
-		mcmp.Close()
-	}
-}
-
-func extractUserKS(jsonString string) string {
-	var result map[string]any
-	if err := json.Unmarshal([]byte(jsonString), &result); err != nil {
-		panic(err.Error())
-	}
-
-	keyspaces, ok := result["keyspaces"].(map[string]any)
-	if !ok {
-		panic("Keyspaces not found")
-	}
-
-	user, ok := keyspaces["user"].(map[string]any)
-	if !ok {
-		panic("User keyspaces not found")
-	}
-
-	tables, ok := user["tables"].(map[string]any)
-	if !ok {
-		panic("Tables not found")
-	}
-
-	userTbl, ok := tables["user"].(map[string]any)
-	if !ok {
-		panic("User table not found")
-	}
-
-	delete(userTbl, "auto_increment") // TODO: we should have an unsharded keyspace where this could live
-
-	// Marshal the inner part back to JSON string
-	userJson, err := json.Marshal(user)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	return string(userJson)
-}
-
-func TestMain(m *testing.M) {
-	defer cluster.PanicHandler(nil)
-
-	schemaSQL := readFile("vschemas/schema.sql")
-	vschema := extractUserKS(readFile("vschemas/schema.json"))
-
-	exitCode := func() int {
-		clusterInstance = cluster.NewCluster(cell, "localhost")
-		defer clusterInstance.Teardown()
-
-		// Start topo server
-		err := clusterInstance.StartTopo()
-		if err != nil {
-			fmt.Println(err.Error())
-			return 1
-		}
-
-		// Start keyspace
-		keyspace := &cluster.Keyspace{
-			Name:      keyspaceName,
-			SchemaSQL: schemaSQL,
-			VSchema:   vschema,
-		}
-		err = clusterInstance.StartKeyspace(*keyspace, []string{"-80", "80-"}, 0, false)
-		if err != nil {
-			fmt.Println(err.Error())
-			return 1
-		}
-
-		// TODO: (@GuptaManan100/@systay): Also run the tests with normalizer on.
-		clusterInstance.VtGateExtraArgs = append(clusterInstance.VtGateExtraArgs,
-			"--normalize_queries=false",
-			"--schema_change_signal=false",
-		)
-
-		// Start vtgate
-		err = clusterInstance.StartVtgate()
-		if err != nil {
-			fmt.Println(err.Error())
-			return 1
-		}
-
-		vtParams = clusterInstance.GetVTParams(keyspaceName)
-
-		// create mysql instance and connection parameters
-		conn, closer, err := utils.NewMySQL(clusterInstance, keyspaceName, schemaSQL)
-		if err != nil {
-			fmt.Println(err.Error())
-			return 1
-		}
-		defer closer()
-		mysqlParams = conn
-
-		return m.Run()
-	}()
-	os.Exit(exitCode)
 }
