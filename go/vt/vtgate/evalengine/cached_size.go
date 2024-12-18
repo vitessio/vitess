323cf2b4a3edcb36482152aaf7be550baa0724f0
Andres Taylor <andres@planetscale.com>
2024-12-18 12:46:48 +0100

    codegen
    Signed-off-by: Andres Taylor <andres@planetscale.com>

--- go/vt/vtgate/evalengine/cached_size.go
+++ go/vt/vtgate/evalengine/cached_size.go
@@ -1181,6 +1181,18 @@ func (cached *builtinLastDay) CachedSize(alloc bool) int64 {
 	size += cached.CallExpr.CachedSize(false)
 	return size
 }
+func (cached *builtinLastInsertID) CachedSize(alloc bool) int64 {
+	if cached == nil {
+		return int64(0)
+	}
+	size := int64(0)
+	if alloc {
+		size += int64(48)
+	}
+	// field CallExpr vitess.io/vitess/go/vt/vtgate/evalengine.CallExpr
+	size += cached.CallExpr.CachedSize(false)
+	return size
+}
 func (cached *builtinLeftRight) CachedSize(alloc bool) int64 {
 	if cached == nil {
 		return int64(0)
