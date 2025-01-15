276790e3ea3247da2aeb06e2908f68d170beed6d
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2025-01-15 18:15:30 +0200

    undo changes to flavor_mysqlgr_test.go
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/mysql/flavor_mysqlgr_test.go
+++ go/mysql/flavor_mysqlgr_test.go
@@ -142,10 +142,6 @@ func TestMysqlGRSupportCapability(t *testing.T) {
 			version:    "8.0.20",
 			capability: capabilities.PerformanceSchemaDataLocksTableCapability,
 			isCapable:  true,
-		}, {
-			version:    "8.0.20",
-			capability: capabilities.PerformanceSchemaMetadataLocksTableCapability,
-			isCapable:  true,
 		},
 		{
 			// What happens if server version is unspecified
