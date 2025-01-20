c259558a0b9080941eb4905e689467fcb8935baf
Dirkjan Bussink <d.bussink@gmail.com>
2025-01-20 17:11:06 +0100

    Fix bootstrap and clean up legacy install logic
    Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

--- docker/utils/install_dependencies.sh
+++ docker/utils/install_dependencies.sh
@@ -71,25 +71,6 @@ apt-get install -y --no-install-recommends "${BASE_PACKAGES[@]}"
 
 # Packages specific to certain flavors.
 case "${FLAVOR}" in
-mysql57)
-    if [ -z "$VERSION" ]; then
-        VERSION=5.7.31
-    fi
-    do_fetch https://repo.mysql.com/apt/debian/pool/mysql-5.7/m/mysql-community/libmysqlclient20_${VERSION}-1debian10_amd64.deb /tmp/libmysqlclient20_${VERSION}-1debian10_amd64.deb
-    do_fetch https://repo.mysql.com/apt/debian/pool/mysql-5.7/m/mysql-community/mysql-community-client_${VERSION}-1debian10_amd64.deb /tmp/mysql-community-client_${VERSION}-1debian10_amd64.deb
-    do_fetch https://repo.mysql.com/apt/debian/pool/mysql-5.7/m/mysql-community/mysql-client_${VERSION}-1debian10_amd64.deb /tmp/mysql-client_${VERSION}-1debian10_amd64.deb
-    do_fetch https://repo.mysql.com/apt/debian/pool/mysql-5.7/m/mysql-community/mysql-community-server_${VERSION}-1debian10_amd64.deb /tmp/mysql-community-server_${VERSION}-1debian10_amd64.deb
-    do_fetch https://repo.mysql.com/apt/debian/pool/mysql-5.7/m/mysql-community/mysql-server_${VERSION}-1debian10_amd64.deb /tmp/mysql-server_${VERSION}-1debian10_amd64.deb
-    PACKAGES=(
-        /tmp/libmysqlclient20_${VERSION}-1debian10_amd64.deb
-        /tmp/mysql-community-client_${VERSION}-1debian10_amd64.deb
-        /tmp/mysql-client_${VERSION}-1debian10_amd64.deb
-        /tmp/mysql-community-server_${VERSION}-1debian10_amd64.deb
-        /tmp/mysql-server_${VERSION}-1debian10_amd64.deb
-        mysql-shell
-        percona-xtrabackup-24
-    )
-    ;;
 mysql80)
     if [ -z "$VERSION" ]; then
         VERSION=8.0.40
@@ -117,20 +98,6 @@ mysql80)
         percona-xtrabackup-80
     )
     ;;
-percona)
-    PACKAGES=(
-        libcurl3
-        percona-server-server-5.6
-        percona-xtrabackup
-    )
-    ;;
-percona57)
-    PACKAGES=(
-        libperconaserverclient20
-        percona-server-server-5.7
-        percona-xtrabackup-24
-    )
-    ;;
 percona80)
     PACKAGES=(
         libperconaserverclient21
@@ -155,9 +122,6 @@ add_apt_key 9334A25F8507EFA5
 
 # Add extra apt repositories for MySQL.
 case "${FLAVOR}" in
-mysql57)
-    echo 'deb http://repo.mysql.com/apt/debian/ buster mysql-5.7' > /etc/apt/sources.list.d/mysql.list
-    ;;
 mysql80)
     echo 'deb http://repo.mysql.com/apt/debian/ bookworm mysql-8.0' > /etc/apt/sources.list.d/mysql.list
     ;;
@@ -165,10 +129,7 @@ esac
 
 # Add extra apt repositories for Percona Server and/or Percona XtraBackup.
 case "${FLAVOR}" in
-mysql57)
-    echo 'deb http://repo.percona.com/apt buster main' > /etc/apt/sources.list.d/percona.list
-    ;;
-mysql80|percona57)
+mysql80)
     echo 'deb http://repo.percona.com/apt bookworm main' > /etc/apt/sources.list.d/percona.list
     ;;
 percona80)
@@ -179,15 +140,6 @@ esac
 
 # Pre-fill values for installation prompts that are normally interactive.
 case "${FLAVOR}" in
-percona57)
-    debconf-set-selections <<EOF
-debconf debconf/frontend select Noninteractive
-percona-server-server-5.7 percona-server-server/root_password password 'unused'
-percona-server-server-5.7 percona-server-server/root_password_again password 'unused'
-percona-server-server-5.7 percona-server-server-5.7/root-pass password 'unused'
-percona-server-server-5.7 percona-server-server-5.7/re-root-pass password 'unused'
-EOF
-    ;;
 percona80)
     debconf-set-selections <<EOF
 debconf debconf/frontend select Noninteractive
