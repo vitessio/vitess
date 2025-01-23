433d7ffb752b1199ea3ae759d5d50745e836c1ef
Matt Lord <mattalord@gmail.com>
2025-01-23 04:31:53 +0000

    Improve comments
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/vt/vtctl/workflow/traffic_switcher.go
+++ go/vt/vtctl/workflow/traffic_switcher.go
@@ -1147,10 +1147,11 @@ func (ts *trafficSwitcher) cancelMigration(ctx context.Context, sm *StreamMigrat
 	}
 
 	// We create a new context while canceling the migration, so that we are independent of the original
-	// context being cancelled prior to or during the cancel operation.
-	// Create a child context that cannot be canceled by the parent, so that we maintain the locks.
+	// context being canceled prior to or during the cancel operation itself.
+	// First we create a copy of the parent context, so that we maintain the locks, but which cannot be
+	// canceled by the parent context.
 	cctx := context.WithoutCancel(ctx)
-	// Now create a child context from that which has a timeout.
+	// Now we create a child context from that which has a timeout.
 	cmTimeout := 60 * time.Second
 	cmCtx, cmCancel := context.WithTimeout(cctx, cmTimeout)
 	defer cmCancel()
