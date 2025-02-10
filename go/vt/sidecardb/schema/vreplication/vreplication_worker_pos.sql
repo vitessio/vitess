28e8716a4e22c6a86d43f736993f625c4f2b8999
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2025-02-10 16:20:00 +0200

    store explicit GTID. not pos
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/sidecardb/schema/vreplication/vreplication_worker_pos.sql
+++ go/vt/sidecardb/schema/vreplication/vreplication_worker_pos.sql
@@ -18,6 +18,6 @@ CREATE TABLE IF NOT EXISTS vreplication_worker_pos
 (
     `id`                    int              NOT NULL AUTO_INCREMENT,
     `worker`                int unsigned     NOT NULL,
-    `pos`                   varbinary(10000) NOT NULL,
+    `gtid`                  varbinary(10000) NOT NULL,
     PRIMARY KEY (`id`, `worker`)
 ) ENGINE = InnoDB CHARSET = utf8mb4
