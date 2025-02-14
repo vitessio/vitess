bf9b845ee8524fccdebcc2d775c5c0195146a4a2
Manan Gupta <manan@planetscale.com>
2025-02-14 15:24:21 +0530

    feat: address review comments
    Signed-off-by: Manan Gupta <manan@planetscale.com>

--- go/vt/vttablet/tabletserver/semisyncmonitor/monitor.go
+++ go/vt/vttablet/tabletserver/semisyncmonitor/monitor.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package semisyncwatcher
+package semisyncmonitor
 
 import (
 	"context"
@@ -33,16 +33,16 @@ import (
 const (
 	semiSyncWaitSessionsRead = "SHOW STATUS LIKE 'Rpl_semi_sync_%_wait_sessions'"
 	semiSyncRecoverWrite     = "INSERT INTO semisync_recover (ts) VALUES (NOW())"
-	semiSyncRecoverClear     = "DELETE FROM semisync_recover"
+	semiSyncRecoverClear     = "TRUNCATE TABLE semisync_recover"
 )
 
-// Watcher is a watcher that checks if the primary tablet
+// Monitor is a monitor that checks if the primary tablet
 // is blocked on a semi-sync ack from the replica.
 // If the semi-sync ACK is lost in the network,
 // it is possible that the primary is indefinitely stuck,
-// blocking PRS. The watcher looks for this situation and manufactures a write
+// blocking PRS. The monitor looks for this situation and manufactures a write
 // periodically to unblock the primary.
-type Watcher struct {
+type Monitor struct {
 	// env is used to get the connection parameters.
 	env tabletenv.Env
 	// ticks is the ticker on which we'll check
@@ -56,7 +56,7 @@ type Watcher struct {
 	mu      sync.Mutex
 	appPool *dbconnpool.ConnectionPool
 	isOpen  bool
-	// isWriting stores if the watcher is currently writing to the DB.
+	// isWriting stores if the monitor is currently writing to the DB.
 	// We don't want two different threads initiating writes, so we use this
 	// for synchronization.
 	isWriting bool
@@ -66,82 +66,84 @@ type Watcher struct {
 	waiters []chan any
 }
 
-// NewWatcher creates a new Watcher.
-func NewWatcher(env tabletenv.Env) *Watcher {
+// NewMonitor creates a new Monitor.
+func NewMonitor(env tabletenv.Env) *Monitor {
 	// TODO (@GuptaManan100): Parameterize the watch interval.
 	watchInterval := 30 * time.Second
-	return &Watcher{
+	return &Monitor{
 		env:   env,
 		ticks: timer.NewTimer(watchInterval),
 		// We clear the data every day. We can make it configurable in the future,
 		// but this seams fine for now.
 		clearTicks: timer.NewTimer(24 * time.Hour),
-		appPool:    dbconnpool.NewConnectionPool("SemiSyncWatcherAppPool", env.Exporter(), 20, mysqlctl.DbaIdleTimeout, 0, mysqlctl.PoolDynamicHostnameResolution),
+		appPool:    dbconnpool.NewConnectionPool("SemiSyncMonitorAppPool", env.Exporter(), 20, mysqlctl.DbaIdleTimeout, 0, mysqlctl.PoolDynamicHostnameResolution),
 		waiters:    make([]chan any, 0),
 	}
 }
 
-// Open starts the watcher.
-func (w *Watcher) Open() {
-	w.mu.Lock()
-	defer w.mu.Unlock()
-	if w.isOpen {
+// Open starts the monitor.
+func (m *Monitor) Open() {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if m.isOpen {
 		// If we are already open, then there is nothing to do
 		return
 	}
-	// Set the watcher to be open.
-	w.isOpen = true
-	log.Info("SemiSync Watcher: opening")
+	// Set the monitor to be open.
+	m.isOpen = true
+	log.Info("SemiSync Monitor: opening")
 
 	// This function could be running from within a unit test scope, in which case we use
 	// mock pools that are already open. This is why we test for the pool being open.
-	if !w.appPool.IsOpen() {
-		w.appPool.Open(w.env.Config().DB.AppWithDB())
+	if !m.appPool.IsOpen() {
+		m.appPool.Open(m.env.Config().DB.AppWithDB())
 	}
-	w.clearTicks.Start(w.clearAllData)
-	w.ticks.Start(w.checkAndFixSemiSyncBlocked)
+	m.clearTicks.Start(m.clearAllData)
+	m.ticks.Start(m.checkAndFixSemiSyncBlocked)
 }
 
-// Close stops the watcher.
-func (w *Watcher) Close() {
-	w.mu.Lock()
-	defer w.mu.Unlock()
-	if !w.isOpen {
+// Close stops the monitor.
+func (m *Monitor) Close() {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if !m.isOpen {
 		// If we are already closed, then there is nothing to do
 		return
 	}
-	w.isOpen = false
-	log.Info("SemiSync Watcher: closing")
-	w.clearTicks.Stop()
-	w.ticks.Stop()
-	w.appPool.Close()
+	m.isOpen = false
+	log.Info("SemiSync Monitor: closing")
+	m.clearTicks.Stop()
+	m.ticks.Stop()
+	m.appPool.Close()
 }
 
 // checkAndFixSemiSyncBlocked checks if the primary is blocked on semi-sync ack
 // and manufactures a write to unblock the primary. This function is safe to
 // be called multiple times in parallel.
-func (w *Watcher) checkAndFixSemiSyncBlocked() {
+func (m *Monitor) checkAndFixSemiSyncBlocked() {
 	// Check if semi-sync is blocked or not
-	isBlocked, err := w.isSemiSyncBlocked(context.Background())
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+	isBlocked, err := m.isSemiSyncBlocked(ctx)
 	if err != nil {
 		// If we are unable to determine whether the primary is blocked or not,
 		// then we can just abort the function and try again later.
-		log.Errorf("SemiSync Watcher: failed to check if primary is blocked on semi-sync: %v", err)
+		log.Errorf("SemiSync Monitor: failed to check if primary is blocked on semi-sync: %v", err)
 		return
 	}
 	// Set the isBlocked state.
-	w.setIsBlocked(isBlocked)
+	m.setIsBlocked(isBlocked)
 	if isBlocked {
 		// If we are blocked, then we want to start the writes.
 		// That function is re-entrant. If we are already writing, then it will just return.
-		w.startWrites()
+		m.startWrites()
 	}
 }
 
 // isSemiSyncBlocked checks if the primary is blocked on semi-sync.
-func (w *Watcher) isSemiSyncBlocked(ctx context.Context) (bool, error) {
+func (m *Monitor) isSemiSyncBlocked(ctx context.Context) (bool, error) {
 	// Get a connection from the pool
-	conn, err := w.appPool.Get(ctx)
+	conn, err := m.appPool.Get(ctx)
 	if err != nil {
 		return false, err
 	}
@@ -168,115 +170,124 @@ func (w *Watcher) isSemiSyncBlocked(ctx context.Context) (bool, error) {
 
 // waitUntilSemiSyncUnblocked waits until the primary is not blocked
 // on semi-sync.
-func (w *Watcher) waitUntilSemiSyncUnblocked() {
+func (m *Monitor) waitUntilSemiSyncUnblocked() {
 	// run one iteration of checking if semi-sync is blocked or not.
-	w.checkAndFixSemiSyncBlocked()
-	if !w.stillBlocked() {
+	m.checkAndFixSemiSyncBlocked()
+	if !m.stillBlocked() {
 		// If we find that the primary isn't blocked, we're good,
 		// we don't need to wait for anything.
 		return
 	}
 	// The primary is blocked. We need to wait for it to be unblocked.
-	ch := w.addWaiter()
+	ch := m.addWaiter()
 	<-ch
 }
 
-// stillBlocked returns true if the watcher should continue writing to the DB
-// because the watcher is still open, and the primary is still blocked.
-func (w *Watcher) stillBlocked() bool {
-	w.mu.Lock()
-	defer w.mu.Unlock()
-	return w.isOpen && w.isBlocked
+// stillBlocked returns true if the monitor should continue writing to the DB
+// because the monitor is still open, and the primary is still blocked.
+func (m *Monitor) stillBlocked() bool {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	return m.isOpen && m.isBlocked
 }
 
-// checkAndSetIsWriting checks if the watcher is already writing to the DB.
+// checkAndSetIsWriting checks if the monitor is already writing to the DB.
 // If it is not, then it sets the isWriting field and signals the caller.
-func (w *Watcher) checkAndSetIsWriting() bool {
-	w.mu.Lock()
-	defer w.mu.Unlock()
-	if w.isWriting {
+func (m *Monitor) checkAndSetIsWriting() bool {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if m.isWriting {
 		return false
 	}
-	w.isWriting = true
+	m.isWriting = true
 	return true
 }
 
 // clearIsWriting clears the isWriting field.
-func (w *Watcher) clearIsWriting() {
-	w.mu.Lock()
-	defer w.mu.Unlock()
-	w.isWriting = false
+func (m *Monitor) clearIsWriting() {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	m.isWriting = false
 }
 
 // startWrites starts writing to the DB.
 // It is re-entrant and will return if we are already writing.
-func (w *Watcher) startWrites() {
+func (m *Monitor) startWrites() {
 	// If we are already writing, then we can just return.
-	if !w.checkAndSetIsWriting() {
+	if !m.checkAndSetIsWriting() {
 		return
 	}
 	// We defer the clear of the isWriting field.
-	defer w.clearIsWriting()
+	defer m.clearIsWriting()
 
 	// We start writing to the DB with a backoff.
 	backoff := 1 * time.Second
 	maxBackoff := 1 * time.Minute
 	// Check if we need to continue writing or not.
-	for w.stillBlocked() {
-		go w.write()
+	for m.stillBlocked() {
+		// We do the writes in a go-routine because if the network disruption
+		// is somewhat long-lived, then the writes themselves can also block.
+		// By doing them in a go-routine we give the system more time to recover while
+		// exponentially backing off. We will eventually run out of the connections in the pool
+		// at which point, there would be nothing that we can do.
+		go m.write()
 		<-time.After(backoff)
 		backoff = time.Duration(math.Min(float64(backoff*2), float64(maxBackoff)))
 	}
 }
 
 // write writes a heartbeat to unblock semi-sync being stuck.
-func (w *Watcher) write() {
+func (m *Monitor) write() {
 	// Get a connection from the pool
-	conn, err := w.appPool.Get(context.Background())
+	conn, err := m.appPool.Get(context.Background())
 	if err != nil {
+		log.Errorf("SemiSync Monitor: failed to write to semisync_recovery table: %v", err)
 		return
 	}
 	defer conn.Recycle()
-	_, _ = conn.Conn.ExecuteFetch(semiSyncRecoverWrite, 0, false)
+	_, err = conn.Conn.ExecuteFetch(semiSyncRecoverWrite, 0, false)
+	if err != nil {
+		log.Errorf("SemiSync Monitor: failed to write to semisync_recovery table: %v", err)
+	}
 }
 
 // setIsBlocked sets the isBlocked field.
-func (w *Watcher) setIsBlocked(val bool) {
-	w.mu.Lock()
-	defer w.mu.Unlock()
-	w.isBlocked = val
+func (m *Monitor) setIsBlocked(val bool) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	m.isBlocked = val
 	if !val {
 		// If we are unblocked, then we need to signal all the waiters.
-		for _, ch := range w.waiters {
+		for _, ch := range m.waiters {
 			close(ch)
 		}
 		// We also empty the list of current waiters.
-		w.waiters = nil
+		m.waiters = nil
 	}
 }
 
 // clearAllData clears all the data in the table so that it never
 // consumes too much space on the MySQL instance.
-func (w *Watcher) clearAllData() {
+func (m *Monitor) clearAllData() {
 	// Get a connection from the pool
-	conn, err := w.appPool.Get(context.Background())
+	conn, err := m.appPool.Get(context.Background())
 	if err != nil {
-		log.Errorf("SemiSync Watcher: failed to clear semisync_recovery table: %v", err)
+		log.Errorf("SemiSync Monitor: failed to clear semisync_recovery table: %v", err)
 		return
 	}
 	defer conn.Recycle()
 	_, err = conn.Conn.ExecuteFetch(semiSyncRecoverClear, 0, false)
 	if err != nil {
-		log.Errorf("SemiSync Watcher: failed to clear semisync_recovery table: %v", err)
+		log.Errorf("SemiSync Monitor: failed to clear semisync_recovery table: %v", err)
 	}
 }
 
 // addWaiter adds a waiter to the list of waiters
 // that will be unblocked when the primary is no longer blocked.
-func (w *Watcher) addWaiter() chan any {
-	w.mu.Lock()
-	defer w.mu.Unlock()
-	ch := make(chan any, 1)
-	w.waiters = append(w.waiters, ch)
+func (m *Monitor) addWaiter() chan any {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	ch := make(chan any)
+	m.waiters = append(m.waiters, ch)
 	return ch
 }
