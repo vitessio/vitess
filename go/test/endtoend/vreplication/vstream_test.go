83e2a4f5b117641c76fd43723ad62375edc65b14
Noble Mittal <62551163+beingnoble03@users.noreply.github.com>
2025-02-19 20:14:51 +0530

    VReplication: Support for `BETWEEN`/`NOT BETWEEN` filter in VStream (#17721)
    Signed-off-by: Noble Mittal <noblemittal@outlook.com>
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>
    Co-authored-by: Rohit Nayak <rohit@planetscale.com>

--- go/test/endtoend/vreplication/vstream_test.go
+++ go/test/endtoend/vreplication/vstream_test.go
@@ -1130,7 +1130,7 @@ func TestVStreamPushdownFilters(t *testing.T) {
 	require.NoError(t, err)
 
 	// Coordinate go-routines.
-	streamCtx, streamCancel := context.WithTimeout(context.Background(), 1*time.Minute)
+	streamCtx, streamCancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer streamCancel()
 	done := make(chan struct{})
 
@@ -1176,19 +1176,31 @@ func TestVStreamPushdownFilters(t *testing.T) {
 			Filter: "select * from customer where name = 'påul'",
 		}},
 	}
-	flags := &vtgatepb.VStreamFlags{}
 	vstreamConn, err := vtgateconn.Dial(ctx, fmt.Sprintf("%s:%d", vc.ClusterConfig.hostname, vc.ClusterConfig.vtgateGrpcPort))
 	require.NoError(t, err)
 	defer vstreamConn.Close()
 
-	// So we should have at least one paul row event in the copy phase.
-	copyPhaseRowEvents := 0
-	// And we should have many paul row events in the running phase.
-	runningPhaseRowEvents := 0
-	copyPhase := true
+	// So we should have at least one paul row event in the copy phase, and
+	// we should have many paul row events in the running phase.
+	copyPhaseRowEvents, runningPhaseRowEvents := runVStreamAndGetNumOfRowEvents(t, ctx, vstreamConn, vgtid, filter, done)
 
+	require.NotZero(t, createdPauls)
+	require.NotZero(t, createdNonPauls)
+	require.Greater(t, createdNonPauls, createdPauls)
+	require.NotZero(t, copyPhaseRowEvents)
+	require.NotZero(t, runningPhaseRowEvents)
+
+	t.Logf("Created pauls: %d, pauls copied: %d, pauls replicated: %d", createdPauls, copyPhaseRowEvents, runningPhaseRowEvents)
+	require.Equal(t, createdPauls, copyPhaseRowEvents+runningPhaseRowEvents)
+}
+
+// runVStreamAndGetNumOfRowEvents runs VStream with the specified filter and
+// returns number of copy phase and running phase row events.
+func runVStreamAndGetNumOfRowEvents(t *testing.T, ctx context.Context, vstreamConn *vtgateconn.VTGateConn,
+	vgtid *binlogdatapb.VGtid, filter *binlogdatapb.Filter, done chan struct{}) (copyPhaseRowEvents int, runningPhaseRowEvents int) {
+	copyPhase := true
 	func() {
-		reader, err := vstreamConn.VStream(ctx, topodatapb.TabletType_PRIMARY, vgtid, filter, flags)
+		reader, err := vstreamConn.VStream(ctx, topodatapb.TabletType_PRIMARY, vgtid, filter, &vtgatepb.VStreamFlags{})
 		require.NoError(t, err)
 		for {
 			evs, err := reader.Recv()
@@ -1217,13 +1229,5 @@ func TestVStreamPushdownFilters(t *testing.T) {
 			}
 		}
 	}()
-
-	require.NotZero(t, createdPauls)
-	require.NotZero(t, createdNonPauls)
-	require.Greater(t, createdNonPauls, createdPauls)
-	require.NotZero(t, copyPhaseRowEvents)
-	require.NotZero(t, runningPhaseRowEvents)
-
-	t.Logf("Created pauls: %d, pauls copied: %d, pauls replicated: %d", createdPauls, copyPhaseRowEvents, runningPhaseRowEvents)
-	require.Equal(t, createdPauls, copyPhaseRowEvents+runningPhaseRowEvents)
+	return
 }
