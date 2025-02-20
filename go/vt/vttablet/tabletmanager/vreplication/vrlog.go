2d99400580589bfd664255108361ce195182ea2f
Matt Lord <mattalord@gmail.com>
2025-02-20 10:18:11 -0500

    Don't enable the http endpoint unless used
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/vt/vttablet/tabletmanager/vreplication/vrlog.go
+++ go/vt/vttablet/tabletmanager/vreplication/vrlog.go
@@ -23,6 +23,7 @@ package vreplication
 import (
 	"net/http"
 	"strconv"
+	"sync"
 	"text/template"
 	"time"
 
@@ -35,6 +36,7 @@ var (
 	vrLogStatsLogger   = streamlog.New[*VrLogStats]("VReplication", 50)
 	vrLogStatsTemplate = template.Must(template.New("vrlog").
 				Parse("{{.Type}} Event	{{.Detail}}	{{.LogTime}}	{{.DurationNs}}\n"))
+	addEndpointOnce sync.Once
 )
 
 // VrLogStats collects attributes of a vreplication event for logging
@@ -47,6 +49,9 @@ type VrLogStats struct {
 }
 
 func NewVrLogStats(eventType string, startTime time.Time) *VrLogStats {
+	addEndpointOnce.Do(func() {
+		addHttpEndpoint()
+	})
 	return &VrLogStats{Type: eventType, StartTime: startTime}
 }
 
@@ -62,7 +67,7 @@ func (stats *VrLogStats) Send(detail string) {
 	vrLogStatsLogger.Send(stats)
 }
 
-func init() {
+func addHttpEndpoint() {
 	servenv.HTTPHandleFunc("/debug/vrlog", func(w http.ResponseWriter, r *http.Request) {
 		ch := vrLogStatsLogger.Subscribe("vrlogstats")
 		defer vrLogStatsLogger.Unsubscribe(ch)
