94291350390c1e67d6aba95d1f3de670d7927214
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2025-02-25 10:11:07 +0200

    Vreplication: DATETIME correctness in non-UTC timezone
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/test/endtoend/onlineddl/vrepl_suite/testdata/tz-datetime-ts-system/create.sql
+++ go/test/endtoend/onlineddl/vrepl_suite/testdata/tz-datetime-ts-system/create.sql
@@ -1,3 +1,5 @@
+set @original_timezone=@@global.time_zone;
+
 drop table if exists onlineddl_test;
 create table onlineddl_test (
   id int auto_increment,
@@ -24,7 +26,11 @@ create event onlineddl_test
   enable
   do
 begin
+  set @@global.TIME_ZONE='+05:00';
+
+  set session time_zone='+02:00';
   insert into onlineddl_test values (null, 7, null, now(), now(), '2010-10-20 10:20:30', 0);
+  insert into onlineddl_test values (null, 8, null, now(), now(), now(), 0);
 
   insert into onlineddl_test values (null, 11, null, now(), now(), '2010-10-20 10:20:30', 0);
   update onlineddl_test set ts2=now() + interval 1 minute, updated = 1 where i = 11 order by id desc limit 1;
@@ -32,10 +38,14 @@ begin
   set session time_zone='system';
   insert into onlineddl_test values (null, 13, null, now(), now(), '2010-10-20 10:20:30', 0);
   update onlineddl_test set ts2=now() + interval 1 minute, updated = 1 where i = 13 order by id desc limit 1;
+  insert into onlineddl_test values (null, 14, null, now(), now(), now(), 0);
+  update onlineddl_test set ts2=now() + interval 1 minute, updated = 1 where i = 14 order by id desc limit 1;
 
   set session time_zone='+00:00';
   insert into onlineddl_test values (null, 17, null, now(), now(), '2010-10-20 10:20:30', 0);
   update onlineddl_test set ts2=now() + interval 1 minute, updated = 1 where i = 17 order by id desc limit 1;
+  insert into onlineddl_test values (null, 18, null, now(), now(), now(), 0);
+  update onlineddl_test set ts2=now() + interval 1 minute, updated = 1 where i = 18 order by id desc limit 1;
 
   set session time_zone='-03:00';
   insert into onlineddl_test values (null, 19, null, now(), now(), '2010-10-20 10:20:30', 0);
@@ -44,4 +54,8 @@ begin
   set session time_zone='+05:00';
   insert into onlineddl_test values (null, 23, null, now(), now(), '2010-10-20 10:20:30', 0);
   update onlineddl_test set ts2=now() + interval 1 minute, updated = 1 where i = 23 order by id desc limit 1;
+  insert into onlineddl_test values (null, 24, null, now(), now(), now(), 0);
+  update onlineddl_test set ts2=now() + interval 1 minute, updated = 1 where i = 24 order by id desc limit 1;
+
+  set @@global.TIME_ZONE=@original_timezone;
 end ;;
