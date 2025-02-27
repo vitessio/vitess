599851338d8d2f117edfc485443eb68b50e87a01
twthorn <thomaswilliamthornton@gmail.com>
2025-02-27 13:35:43 -0500

    Use reset instead of adding zero
    Signed-off-by: twthorn <thomaswilliamthornton@gmail.com>

--- go/vt/vtgate/vstream_manager.go
+++ go/vt/vtgate/vstream_manager.go
@@ -397,7 +397,7 @@ func (vs *vstream) startOneStream(ctx context.Context, sgtid *binlogdatapb.Shard
 		defer vs.wg.Done()
 
 		labelValues := []string{sgtid.Keyspace, sgtid.Shard, vs.tabletType.String()}
-		vs.vsm.vstreamsEndedWithErrors.Add(labelValues, 0)
+		vs.vsm.vstreamsEndedWithErrors.Reset(labelValues)
 		vs.vsm.vstreamsCreated.Add(labelValues, 1)
 		vs.vsm.vstreamsCount.Add(labelValues, 1)
 
