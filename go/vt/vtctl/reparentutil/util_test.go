1719cead3de59a28da004c3931e2dcbcd17c384d
Florent Poinsard <florent.poinsard@outlook.fr>
2025-03-21 13:34:19 -0600

    improve new primary election by takig into account NULL repl lag
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- go/vt/vtctl/reparentutil/util_test.go
+++ go/vt/vtctl/reparentutil/util_test.go
@@ -141,6 +141,114 @@ func TestElectNewPrimary(t *testing.T) {
 			errContains: nil,
 		},
 		{
+			name: "more advanced replica has an unknown replication lag",
+			tmc: &chooseNewPrimaryTestTMClient{
+				// zone1-101 is behind zone1-102 bug zone1-102 has an unknown replication lag, hence picking zone1-101
+				replicationStatuses: map[string]*replicationdatapb.Status{
+					"zone1-0000000101": {
+						Position:              "MySQL56/3E11FA47-71CA-11E1-9E33-C80AA9429562:1",
+						ReplicationLagSeconds: 10,
+					},
+					"zone1-0000000102": {
+						Position:              "MySQL56/3E11FA47-71CA-11E1-9E33-C80AA9429562:1-5",
+						ReplicationLagUnknown: true,
+					},
+				},
+			},
+			tolerableReplLag: 50 * time.Second,
+			shardInfo: topo.NewShardInfo("testkeyspace", "-", &topodatapb.Shard{
+				PrimaryAlias: &topodatapb.TabletAlias{
+					Cell: "zone1",
+					Uid:  100,
+				},
+			}, nil),
+			tabletMap: map[string]*topo.TabletInfo{
+				"primary": {
+					Tablet: &topodatapb.Tablet{
+						Alias: &topodatapb.TabletAlias{
+							Cell: "zone1",
+							Uid:  100,
+						},
+						Type: topodatapb.TabletType_PRIMARY,
+					},
+				},
+				"replica1": {
+					Tablet: &topodatapb.Tablet{
+						Alias: &topodatapb.TabletAlias{
+							Cell: "zone1",
+							Uid:  101,
+						},
+						Type: topodatapb.TabletType_REPLICA,
+					},
+				},
+				"replica2": {
+					Tablet: &topodatapb.Tablet{
+						Alias: &topodatapb.TabletAlias{
+							Cell: "zone1",
+							Uid:  102,
+						},
+						Type: topodatapb.TabletType_REPLICA,
+					},
+				},
+			},
+			avoidPrimaryAlias: &topodatapb.TabletAlias{
+				Cell: "zone1",
+				Uid:  0,
+			},
+			expected: &topodatapb.TabletAlias{
+				Cell: "zone1",
+				Uid:  101,
+			},
+			errContains: nil,
+		},
+		{
+			name: "replica with unknown replication lag",
+			tmc: &chooseNewPrimaryTestTMClient{
+				// zone1-101 is behind zone1-102 bug zone1-102 has an unknown replication lag, hence picking zone1-101
+				replicationStatuses: map[string]*replicationdatapb.Status{
+					"zone1-0000000101": {
+						Position:              "MySQL56/3E11FA47-71CA-11E1-9E33-C80AA9429562:1",
+						ReplicationLagUnknown: true,
+					},
+				},
+			},
+			tolerableReplLag: 50 * time.Second,
+			shardInfo: topo.NewShardInfo("testkeyspace", "-", &topodatapb.Shard{
+				PrimaryAlias: &topodatapb.TabletAlias{
+					Cell: "zone1",
+					Uid:  100,
+				},
+			}, nil),
+			tabletMap: map[string]*topo.TabletInfo{
+				"primary": {
+					Tablet: &topodatapb.Tablet{
+						Alias: &topodatapb.TabletAlias{
+							Cell: "zone1",
+							Uid:  100,
+						},
+						Type: topodatapb.TabletType_PRIMARY,
+					},
+				},
+				"replica1": {
+					Tablet: &topodatapb.Tablet{
+						Alias: &topodatapb.TabletAlias{
+							Cell: "zone1",
+							Uid:  101,
+						},
+						Type: topodatapb.TabletType_REPLICA,
+					},
+				},
+			},
+			avoidPrimaryAlias: &topodatapb.TabletAlias{
+				Cell: "zone1",
+				Uid:  0,
+			},
+			expected: nil,
+			errContains: []string{
+				"zone1-0000000101 position known but unknown replication status",
+			},
+		},
+		{
 			name: "Two good replicas, but one of them is taking a backup so we pick the other one",
 			tmc: &chooseNewPrimaryTestTMClient{
 				// both zone1-101 and zone1-102 are equivalent from a replicaiton PoV, but zone1-102 is taking a backup
@@ -1049,13 +1157,14 @@ func TestFindPositionForTablet(t *testing.T) {
 	ctx := context.Background()
 	logger := logutil.NewMemoryLogger()
 	tests := []struct {
-		name                 string
-		tmc                  *testutil.TabletManagerClient
-		tablet               *topodatapb.Tablet
-		expectedPosition     string
-		expectedLag          time.Duration
-		expectedErr          string
-		expectedTakingBackup bool
+		name                   string
+		tmc                    *testutil.TabletManagerClient
+		tablet                 *topodatapb.Tablet
+		expectedPosition       string
+		expectedLag            time.Duration
+		expectedErr            string
+		expectedTakingBackup   bool
+		expectedUnknownReplLag bool
 	}{
 		{
 			name: "executed gtid set",
@@ -1170,12 +1279,36 @@ func TestFindPositionForTablet(t *testing.T) {
 				},
 			},
 			expectedErr: `parse error: unknown GTIDSet flavor ""`,
+		}, {
+			name: "unknown replication lag",
+			tmc: &testutil.TabletManagerClient{
+				ReplicationStatusResults: map[string]struct {
+					Position *replicationdatapb.Status
+					Error    error
+				}{
+					"zone1-0000000100": {
+						Position: &replicationdatapb.Status{
+							RelayLogPosition:      "MySQL56/3e11fa47-71ca-11e1-9e33-c80aa9429562:1-5",
+							ReplicationLagUnknown: true,
+						},
+					},
+				},
+			},
+			tablet: &topodatapb.Tablet{
+				Alias: &topodatapb.TabletAlias{
+					Cell: "zone1",
+					Uid:  100,
+				},
+			},
+			expectedLag:            0,
+			expectedPosition:       "MySQL56/3e11fa47-71ca-11e1-9e33-c80aa9429562:1-5",
+			expectedUnknownReplLag: true,
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			pos, lag, takingBackup, err := findTabletPositionLagBackupStatus(ctx, test.tablet, logger, test.tmc, 10*time.Second)
+			pos, lag, takingBackup, replUnknown, err := findTabletPositionLagBackupStatus(ctx, test.tablet, logger, test.tmc, 10*time.Second)
 			if test.expectedErr != "" {
 				require.EqualError(t, err, test.expectedErr)
 				return
@@ -1185,6 +1318,7 @@ func TestFindPositionForTablet(t *testing.T) {
 			require.Equal(t, test.expectedPosition, posString)
 			require.Equal(t, test.expectedLag, lag)
 			require.Equal(t, test.expectedTakingBackup, takingBackup)
+			require.Equal(t, test.expectedUnknownReplLag, replUnknown)
 		})
 	}
 }
