e4b0c4a22789764804d953bd75692f7393a8759d
mounicasruthi <mounicasruthi30@gmail.com>
2025-03-22 00:35:46 +0530

    add missing import to fix errors
    Signed-off-by: mounicasruthi <mounicasruthi30@gmail.com>

--- go/vt/utils/flags.go
+++ go/vt/utils/flags.go
@@ -81,6 +81,14 @@ func SetFlagDurationVar(fs *pflag.FlagSet, p *time.Duration, name string, def ti
 	setFlagVar(fs, p, name, def, usage, (*pflag.FlagSet).DurationVar)
 }
 
+// SetFlagVariants adds both underscored and dashed versions of the flag to maintain compatibility during the transition.
+// Underscores will be fully deprecated in v25.
+func SetFlagVariants(m map[string]string, key, value string) {
+	underscored, dashed := flagVariants(key)
+	m[underscored] = value
+	m[dashed] = value
+}
+
 // SetFlagVariantsForTests randomly assigns either the underscored or dashed version of the flag name to the map.
 // This is designed to help catch cases where code does not properly handle both formats during testing.
 func SetFlagVariantsForTests(m map[string]string, key, value string) {
@@ -92,6 +100,7 @@ func SetFlagVariantsForTests(m map[string]string, key, value string) {
 	}
 }
 
+// GetFlagVariantForTests randomly returns either the underscored or dashed version of the flag name.
 func GetFlagVariantForTests(flagName string) string {
 	underscored, dashed := flagVariants(flagName)
 	if rand.Int()%2 == 0 {
