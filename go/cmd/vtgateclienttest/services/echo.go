0533b0971e61fbb5ce57ff16ae03d6d0d2246b2c
Manan Gupta <manan@planetscale.com>
2025-04-08 09:59:47 +0300

    feat: remove test code that isn't used
    Signed-off-by: Manan Gupta <manan@planetscale.com>

--- go/cmd/vtgateclienttest/services/echo.go
+++ go/cmd/vtgateclienttest/services/echo.go
@@ -27,7 +27,6 @@ import (
 	"vitess.io/vitess/go/mysql/collations"
 	"vitess.io/vitess/go/sqltypes"
 	"vitess.io/vitess/go/vt/callerid"
-	"vitess.io/vitess/go/vt/sqlparser"
 	"vitess.io/vitess/go/vt/vtgate/vtgateservice"
 
 	binlogdatapb "vitess.io/vitess/go/vt/proto/binlogdata"
@@ -133,39 +132,14 @@ func (c *echoClient) StreamExecute(ctx context.Context, mysqlCtx vtgateservice.M
 
 // ExecuteMulti is part of the VTGateService interface
 func (c *echoClient) ExecuteMulti(ctx context.Context, mysqlCtx vtgateservice.MySQLConnection, session *vtgatepb.Session, sqlString string) (newSession *vtgatepb.Session, qrs []*sqltypes.Result, err error) {
-	queries, err := sqlparser.NewTestParser().SplitStatementToPieces(sqlString)
-	if err != nil {
-		return session, nil, err
-	}
-	var result *sqltypes.Result
-	for _, query := range queries {
-		session, result, err = c.Execute(ctx, mysqlCtx, session, query, nil, false)
-		if err != nil {
-			return session, qrs, err
-		}
-		qrs = append(qrs, result)
-	}
-	return session, qrs, nil
+	// Look at https://github.com/vitessio/vitess/pull/18059 for details on how to implement this.
+	panic("unimplemented")
 }
 
 // StreamExecuteMulti is part of the VTGateService interface
 func (c *echoClient) StreamExecuteMulti(ctx context.Context, mysqlCtx vtgateservice.MySQLConnection, session *vtgatepb.Session, sqlString string, callback func(qr sqltypes.QueryResponse, more bool, firstPacket bool) error) (*vtgatepb.Session, error) {
-	queries, err := sqlparser.NewTestParser().SplitStatementToPieces(sqlString)
-	if err != nil {
-		return session, err
-	}
-	for idx, query := range queries {
-		firstPacket := true
-		session, err = c.StreamExecute(ctx, mysqlCtx, session, query, nil, func(result *sqltypes.Result) error {
-			err = callback(sqltypes.QueryResponse{QueryResult: result}, idx < len(queries)-1, firstPacket)
-			firstPacket = false
-			return err
-		})
-		if err != nil {
-			return session, err
-		}
-	}
-	return session, nil
+	// Look at https://github.com/vitessio/vitess/pull/18059 for details on how to implement this.
+	panic("unimplemented")
 }
 
 func (c *echoClient) ExecuteBatch(ctx context.Context, session *vtgatepb.Session, sqlList []string, bindVariablesList []map[string]*querypb.BindVariable) (*vtgatepb.Session, []sqltypes.QueryResponse, error) {
