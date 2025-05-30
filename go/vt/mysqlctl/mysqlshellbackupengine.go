e258d22681bea4dc89bebc42333fa0475cbc4e0b
Renan Rangel <rrangel@slack-corp.com>
2025-05-30 05:35:53 -0700

    fix version issue when using --mysql-shell-speedup-restore=true
    Signed-off-by: Renan Rangel <rrangel@slack-corp.com>

--- go/vt/mysqlctl/mysqlshellbackupengine.go
+++ go/vt/mysqlctl/mysqlshellbackupengine.go
@@ -458,7 +458,14 @@ func (be *MySQLShellBackupEngine) restorePreCheck(ctx context.Context, params Re
 			return false, fmt.Errorf("%w: failed to fetch MySQL version: %v", ErrMySQLShellPreCheck, err)
 		}
 
-		capableOf := mysql.ServerVersionCapableOf(version)
+		_, sv, err := ParseVersionString(version)
+		if err != nil {
+			return false, fmt.Errorf("%w: failed to parse MySQL version (version: %s): %v", ErrMySQLShellPreCheck, version, err)
+		}
+
+		versionStr := fmt.Sprintf("%d.%d.%d", sv.Major, sv.Minor, sv.Patch)
+
+		capableOf := mysql.ServerVersionCapableOf(versionStr)
 		capable, err := capableOf(capabilities.DisableRedoLogFlavorCapability)
 		if err != nil {
 			return false, fmt.Errorf("%w: error checking if server supports disabling redo log: %v", ErrMySQLShellPreCheck, err)
