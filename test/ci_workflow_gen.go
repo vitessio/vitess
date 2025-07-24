f4a735b27faf16be25888229fe9cebb0cb721f40
Jeffrey Sica <me@jeefy.dev>
2025-07-24 11:47:24 -0500

    Update runners to use cncf oracle vms (#18425)
    Signed-off-by: Jeffrey Sica <me@jeefy.dev>

--- test/ci_workflow_gen.go
+++ test/ci_workflow_gen.go
@@ -47,9 +47,9 @@ var (
 )
 
 const (
-	oracleCloudRunner = "oracle-16cpu-64gb-x86-64"
+	oracleCloudRunner = "oracle-vm-16cpu-64gb-x86-64"
 	githubRunner      = "gh-hosted-runners-16cores-1-24.04"
-	cores16RunnerName = githubRunner
+	cores16RunnerName = oracleCloudRunner
 	defaultRunnerName = "ubuntu-24.04"
 )
 
