27041d2ab8b0f8512e354bec66bc7f0ed5b8c8e8
Mohamed Hamza <mhamza15@github.com>
2025-08-29 19:04:03 -0400

    Simplify e2e test
    Signed-off-by: Mohamed Hamza <mhamza15@github.com>

--- go/test/endtoend/vtgate/queries/transaction_timeout/transaction_timeout_test.go
+++ go/test/endtoend/vtgate/queries/transaction_timeout/transaction_timeout_test.go
@@ -17,6 +17,7 @@ limitations under the License.
 package transactiontimeout
 
 import (
+	"context"
 	_ "embed"
 	"testing"
 	"time"
@@ -31,28 +32,21 @@ import (
 var (
 	clusterInstance *cluster.LocalProcessCluster
 	vtParams        mysql.ConnParams
-	mysqlParams     mysql.ConnParams
-	keyspaceName    = "ks_misc"
 	uks             = "uks"
 	cell            = "test_misc"
 
 	//go:embed uschema.sql
 	uschemaSQL string
-
-	//go:embed schema.sql
-	schemaSQL string
 )
 
 func createCluster(t *testing.T, vttabletArgs ...string) func() {
 	clusterInstance = cluster.NewCluster(cell, "localhost")
 
-	// Start topo server
 	err := clusterInstance.StartTopo()
 	require.NoError(t, err)
 
 	clusterInstance.VtTabletExtraArgs = append(clusterInstance.VtTabletExtraArgs, vttabletArgs...)
 
-	// Start unsharded keyspace
 	ukeyspace := &cluster.Keyspace{
 		Name:      uks,
 		SchemaSQL: uschemaSQL,
@@ -60,40 +54,17 @@ func createCluster(t *testing.T, vttabletArgs ...string) func() {
 	err = clusterInstance.StartUnshardedKeyspace(*ukeyspace, 0, false)
 	require.NoError(t, err)
 
-	// Start vtgate
 	err = clusterInstance.StartVtgate()
 	require.NoError(t, err)
 
-	vtParams = clusterInstance.GetVTParams(keyspaceName)
+	vtParams = clusterInstance.GetVTParams(uks)
 
-	// create mysql instance and connection parameters
-	conn, closer, err := utils.NewMySQL(clusterInstance, keyspaceName, schemaSQL)
+	_, closer, err := utils.NewMySQL(clusterInstance, uks, uschemaSQL)
 	require.NoError(t, err)
-
-	mysqlParams = conn
 
 	return func() {
 		clusterInstance.Teardown()
 		closer()
-	}
-}
-
-func start(t *testing.T) (utils.MySQLCompare, func()) {
-	mcmp, err := utils.NewMySQLCompare(t, vtParams, mysqlParams)
-	require.NoError(t, err)
-
-	deleteAll := func() {
-		tables := []string{"t1", "uks.unsharded"}
-		for _, table := range tables {
-			_, _ = mcmp.ExecAndIgnore("delete from " + table)
-		}
-	}
-
-	deleteAll()
-
-	return mcmp, func() {
-		deleteAll()
-		mcmp.Close()
 	}
 }
 
@@ -104,42 +75,42 @@ func TestTransactionTimeout(t *testing.T) {
 	teardown := createCluster(t)
 	defer teardown()
 
-	mcmp, closer := start(t)
-	defer closer()
+	conn, err := mysql.Connect(context.Background(), &vtParams)
+	require.NoError(t, err)
+	defer conn.Close()
 
 	// No timeout set, transaction shouldn't timeout
-	utils.Exec(t, mcmp.VtConn, "begin")
-	utils.Exec(t, mcmp.VtConn, "insert into uks.unsharded(id1) values (1),(2),(3),(4),(sleep(0.5))")
-	utils.Exec(t, mcmp.VtConn, "commit")
+	utils.Exec(t, conn, "begin")
+	utils.Exec(t, conn, "insert into uks.unsharded(id1) values (1),(2),(3),(4),(sleep(0.5))")
+	utils.Exec(t, conn, "commit")
 
 	// Set session transaction timeout
-	utils.Exec(t, mcmp.VtConn, "set transaction_timeout=100")
+	utils.Exec(t, conn, "set transaction_timeout=100")
 
 	// Sleeping outside of query will allow the transaction killer to kill the transaction
-	utils.Exec(t, mcmp.VtConn, "begin")
-	utils.Exec(t, mcmp.VtConn, "insert into uks.unsharded(id1) values (1),(2),(3),(4),(5)")
+	utils.Exec(t, conn, "begin")
+	utils.Exec(t, conn, "insert into uks.unsharded(id1) values (1),(2),(3),(4),(5)")
 	time.Sleep(3 * time.Second)
-	_, err := utils.ExecAllowError(t, mcmp.VtConn, "insert into uks.unsharded(id1) values (1),(2),(3),(4),(5)")
+	_, err = utils.ExecAllowError(t, conn, "insert into uks.unsharded(id1) values (1),(2),(3),(4),(5)")
 	require.ErrorContains(t, err, "Aborted")
 
 	// Sleeping in MySQL will cause a context timeout instead (different error)
-	utils.Exec(t, mcmp.VtConn, "begin")
-	utils.Exec(t, mcmp.VtConn, "insert into uks.unsharded(id1) values (1),(2),(3),(4),(5)")
-	_, err = utils.ExecAllowError(t, mcmp.VtConn, "insert into uks.unsharded(id1) values (1),(2),(3),(4),(sleep(0.5))")
+	utils.Exec(t, conn, "begin")
+	utils.Exec(t, conn, "insert into uks.unsharded(id1) values (1),(2),(3),(4),(5)")
+	_, err = utils.ExecAllowError(t, conn, "insert into uks.unsharded(id1) values (1),(2),(3),(4),(sleep(0.5))")
 	require.ErrorContains(t, err, "Query execution was interrupted")
 
 	// Get new connection
-	mcmp, closer = start(t)
-	defer closer()
+	conn, err = mysql.Connect(context.Background(), &vtParams)
 
 	// Set session transaction timeout to 0
-	utils.Exec(t, mcmp.VtConn, "set transaction_timeout=0")
+	utils.Exec(t, conn, "set transaction_timeout=0")
 
 	// Should time out using tablet transaction timeout
-	utils.Exec(t, mcmp.VtConn, "begin")
-	utils.Exec(t, mcmp.VtConn, "insert into uks.unsharded(id1) values (1),(2),(3),(4),(5)")
-	utils.Exec(t, mcmp.VtConn, "insert into uks.unsharded(id1) values (1),(2),(3),(4),(sleep(2))")
-	utils.Exec(t, mcmp.VtConn, "commit")
+	utils.Exec(t, conn, "begin")
+	utils.Exec(t, conn, "insert into uks.unsharded(id1) values (1),(2),(3),(4),(5)")
+	utils.Exec(t, conn, "insert into uks.unsharded(id1) values (1),(2),(3),(4),(sleep(2))")
+	utils.Exec(t, conn, "commit")
 }
 
 func TestSmallerTimeout(t *testing.T) {
@@ -149,26 +120,26 @@ func TestSmallerTimeout(t *testing.T) {
 	teardown := createCluster(t, "--queryserver-config-transaction-timeout", "1s")
 	defer teardown()
 
-	mcmp, closer := start(t)
-	defer closer()
+	conn, err := mysql.Connect(context.Background(), &vtParams)
+	require.NoError(t, err)
 
 	// Set session transaction timeout larger than tablet transaction timeout
-	utils.Exec(t, mcmp.VtConn, "set transaction_timeout=2000")
+	utils.Exec(t, conn, "set transaction_timeout=2000")
 
 	// Transaction should get killed with lower timeout
-	utils.Exec(t, mcmp.VtConn, "begin")
-	utils.Exec(t, mcmp.VtConn, "insert into uks.unsharded(id1) values (1),(2),(3),(4),(5)")
+	utils.Exec(t, conn, "begin")
+	utils.Exec(t, conn, "insert into uks.unsharded(id1) values (1),(2),(3),(4),(5)")
 	time.Sleep(1500 * time.Millisecond)
-	_, err := utils.ExecAllowError(t, mcmp.VtConn, "insert into uks.unsharded(id1) values (1),(2),(3),(4),(5)")
+	_, err = utils.ExecAllowError(t, conn, "insert into uks.unsharded(id1) values (1),(2),(3),(4),(5)")
 	require.ErrorContains(t, err, "Aborted")
 
 	// Set session transaction timeout smaller than tablet transaction timeout
-	utils.Exec(t, mcmp.VtConn, "set transaction_timeout=250")
+	utils.Exec(t, conn, "set transaction_timeout=250")
 
 	// Session timeout should be used this time
-	utils.Exec(t, mcmp.VtConn, "begin")
-	utils.Exec(t, mcmp.VtConn, "insert into uks.unsharded(id1) values (1),(2),(3),(4),(5)")
+	utils.Exec(t, conn, "begin")
+	utils.Exec(t, conn, "insert into uks.unsharded(id1) values (1),(2),(3),(4),(5)")
 	time.Sleep(500 * time.Millisecond)
-	_, err = utils.ExecAllowError(t, mcmp.VtConn, "insert into uks.unsharded(id1) values (1),(2),(3),(4),(5)")
+	_, err = utils.ExecAllowError(t, conn, "insert into uks.unsharded(id1) values (1),(2),(3),(4),(5)")
 	require.ErrorContains(t, err, "Aborted")
 }
