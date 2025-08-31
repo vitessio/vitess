24e2a2e824af87331e23827018b0a7f6897c1903
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2025-08-31 14:05:13 +0300

    use AppendGTID
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/vttablet/tabletserver/vstreamer/vstreamer.go
+++ go/vt/vttablet/tabletserver/vstreamer/vstreamer.go
@@ -491,9 +491,7 @@ func (vs *vstreamer) parseEvent(ev mysql.BinlogEvent, bufferAndTransmit func(vev
 				SequenceNumber: sequenceNumber,
 			})
 		}
-		vs.se.MutexProtected(func() {
-			vs.pos = replication.AppendGTIDInPlace(vs.pos, gtid)
-		})
+		vs.pos = replication.AppendGTID(vs.pos, gtid)
 		vs.commitParent = commitParent
 		vs.sequenceNumber = sequenceNumber
 		vs.eventGTID = gtid
