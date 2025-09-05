28c2048833cdc56282a2daf02cdf9e9a2a3470fa
Rohit Nayak <rohit@planetscale.com>
2025-09-05 22:27:23 +0200

    Migrate timeout flags to dash format
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>

--- go/vt/vtctld/action_repository.go
+++ go/vt/vtctld/action_repository.go
@@ -28,6 +28,7 @@ import (
 	"vitess.io/vitess/go/acl"
 	"vitess.io/vitess/go/vt/logutil"
 	"vitess.io/vitess/go/vt/servenv"
+	"vitess.io/vitess/go/vt/utils"
 	"vitess.io/vitess/go/vt/topo"
 	"vitess.io/vitess/go/vt/topo/topoproto"
 	"vitess.io/vitess/go/vt/utils"
