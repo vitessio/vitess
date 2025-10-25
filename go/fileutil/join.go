ae318d4ae32ff6903ede8e03ec4c148738a83f61
Tim Vaillancourt <tim@timvaillancourt.com>
2025-10-25 22:44:25 +0200

    fix err again
    Signed-off-by: Tim Vaillancourt <tim@timvaillancourt.com>

--- go/fileutil/join.go
+++ go/fileutil/join.go
@@ -36,11 +36,11 @@ func SafePathJoin(rootPath string, joinPaths ...string) (string, error) {
 	p := path.Join(allPaths...)
 	absPath, err := filepath.Abs(p)
 	if err != nil {
-		return p, fmt.Errorf("failed to parse backup path %q: %w", p, err)
+		return p, fmt.Errorf("failed to parse path %q: %w", p, err)
 	}
 	absRootPath, err := filepath.Abs(rootPath)
 	if err != nil {
-		return p, fmt.Errorf("failed to parse backup root path %q: %w", rootPath, err)
+		return p, fmt.Errorf("failed to parse root path %q: %w", rootPath, err)
 	}
 	if absPath != absRootPath && !strings.HasPrefix(absPath, absRootPath+string(os.PathSeparator)) {
 		return p, ErrInvalidJoinedPath
