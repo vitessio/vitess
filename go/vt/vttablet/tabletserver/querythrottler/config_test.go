e150703ae22296175edce8373678e443b19378e0
siddharth16396 <siddharth16396@gmail.com>
2025-11-18 13:10:11 +0530

    Fix import lint of config_test.go
    Signed-off-by: siddharth16396 <siddharth16396@gmail.com>

--- go/vt/vttablet/tabletserver/querythrottler/config_test.go
+++ go/vt/vttablet/tabletserver/querythrottler/config_test.go
@@ -20,6 +20,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+
 	"vitess.io/vitess/go/vt/proto/querythrottler"
 	"vitess.io/vitess/go/vt/vttablet/tabletserver/querythrottler/registry"
 )
