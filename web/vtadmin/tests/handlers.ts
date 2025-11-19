884debdf66ddc930e616c6188fabcb4f22fbec42
Tim Vaillancourt <tim@timvaillancourt.com>
2025-11-19 05:56:09 +0100

    `vtadmin`: remove `graphql` and `rest` imports (#18909)
    Signed-off-by: Tim Vaillancourt <tim@timvaillancourt.com>
    Signed-off-by: siddharth16396 <siddharth16396@gmail.com>

--- web/vtadmin/tests/handlers.ts
+++ web/vtadmin/tests/handlers.ts
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { graphql, rest } from 'msw'
 
 // Add handlers here as needed for mocking the test server
 export const handlers = [
