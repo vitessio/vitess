9e59a16bcc6c631549be20a8eadfafde1f73d8e6
Mohamed Hamza <mhamza@fastmail.com>
2026-01-08 17:11:10 -0500

    use gotestsum for e2e tests too
    Signed-off-by: Mohamed Hamza <mhamza@fastmail.com>

--- tools/e2e_go_test.sh
+++ tools/e2e_go_test.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 source build.env
 echo "running tests for " "$@"
-go test -v "$@" -alsologtostderr -count=1
+
+GOTESTSUM_ARGS="--format github-actions --format-hide-empty-pkg --hide-summary=skipped"
+if [[ -n "${JUNIT_OUTPUT:-}" ]]; then
+	GOTESTSUM_ARGS="$GOTESTSUM_ARGS --junitfile $JUNIT_OUTPUT"
+fi
+if [[ -n "${JSON_OUTPUT:-}" ]]; then
+	GOTESTSUM_ARGS="$GOTESTSUM_ARGS --jsonfile $JSON_OUTPUT"
+fi
+
+go tool gotestsum $GOTESTSUM_ARGS -- "$@" -count=1
