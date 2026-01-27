c1a8c99f9477e6726ab35b47a7c31974cd734cd9
Mohamed Hamza <mhamza@fastmail.com>
2026-01-27 16:42:07 -0500

    fix conflicts
    Signed-off-by: Mohamed Hamza <mhamza@fastmail.com>

--- go/vt/vtcombo/tablet_map.go
+++ go/vt/vtcombo/tablet_map.go
@@ -18,6 +18,7 @@ package vtcombo
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
 	"path"
@@ -1017,13 +1018,8 @@ func (itmc *internalTabletManagerClient) ReadReparentJournalInfo(ctx context.Con
 	return 0, fmt.Errorf("not implemented in vtcombo")
 }
 
-<<<<<<< HEAD
-func (itmc *internalTabletManagerClient) DemotePrimary(context.Context, *topodatapb.Tablet) (*replicationdatapb.PrimaryStatus, error) {
-	return nil, fmt.Errorf("not implemented in vtcombo")
-=======
 func (itmc *internalTabletManagerClient) DemotePrimary(context.Context, *topodatapb.Tablet, bool) (*replicationdatapb.PrimaryStatus, error) {
 	return nil, errors.New("not implemented in vtcombo")
->>>>>>> 1f49de43ad (Add new `force` flag to `DemotePrimary` to force a demotion even when blocked on waiting for semi-sync acks (#18714))
 }
 
 func (itmc *internalTabletManagerClient) UndoDemotePrimary(context.Context, *topodatapb.Tablet, bool) error {
@@ -1108,6 +1104,7 @@ func (itmc *internalTabletManagerClient) ResetReplicationParameters(context.Cont
 func (itmc *internalTabletManagerClient) ReplicaWasRestarted(context.Context, *topodatapb.Tablet, *topodatapb.TabletAlias) error {
 	return fmt.Errorf("not implemented in vtcombo")
 }
+
 func (itmc *internalTabletManagerClient) ResetSequences(ctx context.Context, tablet *topodatapb.Tablet, tables []string) error {
 	return fmt.Errorf("not implemented in vtcombo")
 }
--- go/vt/vtctl/reparentutil/emergency_reparenter_test.go
+++ go/vt/vtctl/reparentutil/emergency_reparenter_test.go
@@ -18,11 +18,7 @@ package reparentutil
 
 import (
 	"context"
-<<<<<<< HEAD
-	"fmt"
-=======
 	"errors"
->>>>>>> 1f49de43ad (Add new `force` flag to `DemotePrimary` to force a demotion even when blocked on waiting for semi-sync acks (#18714))
 	"slices"
 	"testing"
 	"time"
@@ -1871,7 +1867,6 @@ func TestEmergencyReparenter_reparentShardLocked(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-
 			ctx, cancel := context.WithCancel(context.Background())
 			defer cancel()
 
@@ -2021,7 +2016,7 @@ func TestEmergencyReparenter_promotionOfNewPrimary(t *testing.T) {
 					Error  error
 				}{
 					"zone1-0000000100": {
-						Error: fmt.Errorf("primary position error"),
+						Error: errors.New("primary position error"),
 					},
 				},
 			},
@@ -3205,7 +3200,7 @@ func TestEmergencyReparenter_reparentReplicas(t *testing.T) {
 					Error  error
 				}{
 					"zone1-0000000100": {
-						Error: fmt.Errorf("primary position error"),
+						Error: errors.New("primary position error"),
 					},
 				},
 			},
@@ -3439,7 +3434,8 @@ func TestEmergencyReparenter_reparentReplicas(t *testing.T) {
 			keyspace:  "testkeyspace",
 			shard:     "-",
 			shouldErr: false,
-		}, {
+		},
+		{
 			name:                 "single replica failing to SetReplicationSource does not fail the promotion",
 			emergencyReparentOps: EmergencyReparentOptions{},
 			tmc: &testutil.TabletManagerClient{
@@ -3582,7 +3578,7 @@ func TestEmergencyReparenter_reparentReplicas(t *testing.T) {
 					Shard: &topodatapb.Shard{
 						PrimaryAlias: &topodatapb.TabletAlias{
 							Cell: "zone1",
-							Uid:  000,
+							Uid:  0o00,
 						},
 					},
 				},
@@ -3723,7 +3719,8 @@ func TestEmergencyReparenter_promoteIntermediateSource(t *testing.T) {
 						Uid:  100,
 					},
 					Hostname: "primary-elect",
-				}, {
+				},
+				{
 					Alias: &topodatapb.TabletAlias{
 						Cell: "zone1",
 						Uid:  101,
@@ -3744,7 +3741,8 @@ func TestEmergencyReparenter_promoteIntermediateSource(t *testing.T) {
 						Uid:  100,
 					},
 					Hostname: "primary-elect",
-				}, {
+				},
+				{
 					Alias: &topodatapb.TabletAlias{
 						Cell: "zone1",
 						Uid:  101,
@@ -3853,7 +3851,8 @@ func TestEmergencyReparenter_promoteIntermediateSource(t *testing.T) {
 					},
 				},
 			},
-		}, {
+		},
+		{
 			name:                 "success - only 2 tablets and they error",
 			emergencyReparentOps: EmergencyReparentOptions{},
 			tmc: &testutil.TabletManagerClient{
@@ -3861,7 +3860,7 @@ func TestEmergencyReparenter_promoteIntermediateSource(t *testing.T) {
 					"zone1-0000000100": nil,
 				},
 				SetReplicationSourceResults: map[string]error{
-					"zone1-0000000101": fmt.Errorf("An error"),
+					"zone1-0000000101": errors.New("An error"),
 				},
 			},
 			newSourceTabletAlias: "zone1-0000000100",
@@ -3968,7 +3967,8 @@ func TestEmergencyReparenter_promoteIntermediateSource(t *testing.T) {
 						Uid:  100,
 					},
 					Hostname: "primary-elect",
-				}, {
+				},
+				{
 					Alias: &topodatapb.TabletAlias{
 						Cell: "zone1",
 						Uid:  101,
@@ -4038,7 +4038,8 @@ func TestEmergencyReparenter_promoteIntermediateSource(t *testing.T) {
 						Uid:  100,
 					},
 					Hostname: "primary-elect",
-				}, {
+				},
+				{
 					Alias: &topodatapb.TabletAlias{
 						Cell: "zone1",
 						Uid:  101,
