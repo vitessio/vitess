4326dd580bfbe33754e4b0daf3eb6a54531f06af
Tim Vaillancourt <tim@timvaillancourt.com>
2026-02-04 01:10:32 +0100

    rename for new major version of opentracing
    Signed-off-by: Tim Vaillancourt <tim@timvaillancourt.com>

--- java/grpc-client/src/main/java/io/vitess/client/grpc/GrpcClientFactory.java
+++ java/grpc-client/src/main/java/io/vitess/client/grpc/GrpcClientFactory.java
@@ -27,7 +27,7 @@ import io.grpc.netty.NegotiationType;
 import io.grpc.netty.NettyChannelBuilder;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
-import io.opentracing.contrib.grpc.ClientTracingInterceptor;
+import io.opentracing.contrib.grpc.TracingClientInterceptor;
 import io.vitess.client.Context;
 import io.vitess.client.RpcClient;
 import io.vitess.client.RpcClientFactory;
@@ -115,7 +115,7 @@ public class GrpcClientFactory implements RpcClientFactory {
     RetryingInterceptor retryingInterceptor = new RetryingInterceptor(config);
     ClientInterceptor[] interceptors;
     if (useTracing) {
-      ClientTracingInterceptor tracingInterceptor = new ClientTracingInterceptor();
+      TracingClientInterceptor tracingInterceptor = new TracingClientInterceptor();
       interceptors = new ClientInterceptor[]{retryingInterceptor, tracingInterceptor};
     } else {
       interceptors = new ClientInterceptor[]{retryingInterceptor};
