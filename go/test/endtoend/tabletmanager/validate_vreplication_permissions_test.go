d494939152c462867381fdeaa84f82aba9c1d25a
Matt Lord <mattalord@gmail.com>
2026-02-11 23:12:44 +0000

    More deflaking
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/test/endtoend/tabletmanager/validate_vreplication_permissions_test.go
+++ go/test/endtoend/tabletmanager/validate_vreplication_permissions_test.go
@@ -60,66 +60,102 @@ func TestValidateVReplicationPermissions_FailsWithoutInsertPermissions(t *testin
 	tablet := getTablet(primaryTablet.GrpcPort)
 
 	// Revoke INSERT permission on the _vt.vreplication table
-	conn, err := mysql.Connect(t.Context(), &primaryTabletParams)
+	ctx, cancel := context.WithTimeout(t.Context(), vreplicationPermissionTimeout)
+	defer cancel()
+	conn, err := mysql.Connect(ctx, &primaryTabletParams)
 	require.NoError(t, err)
 	t.Cleanup(func() { conn.Close() })
 
-	_, err = conn.ExecuteFetch("revoke insert on *.* from vt_filtered@localhost", 0, false)
-	require.NoError(t, err)
+	require.EventuallyWithT(t, func(c *assert.CollectT) {
+		_, err = conn.ExecuteFetch("revoke insert on *.* from vt_filtered@localhost", 0, false)
+		require.NoError(c, err)
+	}, 10*time.Second, 200*time.Millisecond)
 	t.Cleanup(func() {
 		// Restore the permission for other tests
-		_, err = conn.ExecuteFetch("grant insert on *.* to vt_filtered@localhost", 0, false)
-		require.NoError(t, err)
+		require.EventuallyWithT(t, func(c *assert.CollectT) {
+			_, err = conn.ExecuteFetch("grant insert on *.* to vt_filtered@localhost", 0, false)
+			require.NoError(c, err)
+		}, 10*time.Second, 200*time.Millisecond)
 	})
 
 	req := &tmdatapb.ValidateVReplicationPermissionsRequest{}
-	res, err := tmClient.ValidateVReplicationPermissions(t.Context(), tablet, req)
-	require.NoError(t, err)
-	require.False(t, res.Ok)
+	var res *tmdatapb.ValidateVReplicationPermissionsResponse
+	require.EventuallyWithT(t, func(c *assert.CollectT) {
+		ctx, cancel = context.WithTimeout(t.Context(), vreplicationPermissionTimeout)
+		defer cancel()
+		res, err = tmClient.ValidateVReplicationPermissions(ctx, tablet, req)
+		require.NoError(c, err)
+		require.NotNil(c, res)
+		require.False(c, res.Ok)
+	}, 10*time.Second, 200*time.Millisecond)
 }
 
 func TestValidateVReplicationPermissions_FailsWithoutUpdatePermissions(t *testing.T) {
 	tablet := getTablet(primaryTablet.GrpcPort)
 
 	// Revoke UPDATE permission on the _vt.vreplication table
-	conn, err := mysql.Connect(t.Context(), &primaryTabletParams)
+	ctx, cancel := context.WithTimeout(t.Context(), vreplicationPermissionTimeout)
+	defer cancel()
+	conn, err := mysql.Connect(ctx, &primaryTabletParams)
 	require.NoError(t, err)
 	t.Cleanup(func() { conn.Close() })
 
-	_, err = conn.ExecuteFetch("revoke update on *.* from vt_filtered@localhost", 0, false)
-	require.NoError(t, err)
+	require.EventuallyWithT(t, func(c *assert.CollectT) {
+		_, err = conn.ExecuteFetch("revoke update on *.* from vt_filtered@localhost", 0, false)
+		require.NoError(c, err)
+	}, 10*time.Second, 200*time.Millisecond)
 	t.Cleanup(func() {
 		// Restore the permission for other tests
-		_, err = conn.ExecuteFetch("grant update on *.* to vt_filtered@localhost", 0, false)
-		require.NoError(t, err)
+		require.EventuallyWithT(t, func(c *assert.CollectT) {
+			_, err = conn.ExecuteFetch("grant update on *.* to vt_filtered@localhost", 0, false)
+			require.NoError(c, err)
+		}, 10*time.Second, 200*time.Millisecond)
 	})
 
 	req := &tmdatapb.ValidateVReplicationPermissionsRequest{}
-	res, err := tmClient.ValidateVReplicationPermissions(t.Context(), tablet, req)
-	require.NoError(t, err)
-	require.False(t, res.Ok)
+	var res *tmdatapb.ValidateVReplicationPermissionsResponse
+	require.EventuallyWithT(t, func(c *assert.CollectT) {
+		ctx, cancel = context.WithTimeout(t.Context(), vreplicationPermissionTimeout)
+		defer cancel()
+		res, err = tmClient.ValidateVReplicationPermissions(ctx, tablet, req)
+		require.NoError(c, err)
+		require.NotNil(c, res)
+		require.False(c, res.Ok)
+	}, 10*time.Second, 200*time.Millisecond)
 }
 
 func TestValidateVReplicationPermissions_FailsWithoutDeletePermissions(t *testing.T) {
 	tablet := getTablet(primaryTablet.GrpcPort)
 
 	// Revoke DELETE permission on the _vt.vreplication table
-	conn, err := mysql.Connect(t.Context(), &primaryTabletParams)
+	ctx, cancel := context.WithTimeout(t.Context(), vreplicationPermissionTimeout)
+	defer cancel()
+	conn, err := mysql.Connect(ctx, &primaryTabletParams)
 	require.NoError(t, err)
 	t.Cleanup(func() { conn.Close() })
 
-	_, err = conn.ExecuteFetch("revoke delete on *.* from vt_filtered@localhost", 0, false)
-	require.NoError(t, err)
+	require.EventuallyWithT(t, func(c *assert.CollectT) {
+		_, err = conn.ExecuteFetch("revoke delete on *.* from vt_filtered@localhost", 0, false)
+		require.NoError(c, err)
+	}, 10*time.Second, 200*time.Millisecond)
 	t.Cleanup(func() {
 		// Restore the permission for other tests
-		_, err = conn.ExecuteFetch("grant delete on *.* to vt_filtered@localhost", 0, false)
-		require.NoError(t, err)
+		require.EventuallyWithT(t, func(c *assert.CollectT) {
+			_, err = conn.ExecuteFetch("grant delete on *.* to vt_filtered@localhost", 0, false)
+			require.NoError(c, err)
+		}, 10*time.Second, 200*time.Millisecond)
 	})
 
 	req := &tmdatapb.ValidateVReplicationPermissionsRequest{}
-	res, err := tmClient.ValidateVReplicationPermissions(t.Context(), tablet, req)
-	require.NoError(t, err)
-	require.False(t, res.Ok)
+	var res *tmdatapb.ValidateVReplicationPermissionsResponse
+	require.EventuallyWithT(t, func(c *assert.CollectT) {
+		ctx, cancel = context.WithTimeout(t.Context(), vreplicationPermissionTimeout)
+		defer cancel()
+		res, err = tmClient.ValidateVReplicationPermissions(ctx, tablet, req)
+		require.NoError(c, err)
+		require.NotNil(c, res)
+		require.False(c, res.Ok)
+	}, 10*time.Second, 200*time.Millisecond)
 }
 
 func TestValidateVReplicationPermissions_FailsIfUserCantLogin(t *testing.T) {
