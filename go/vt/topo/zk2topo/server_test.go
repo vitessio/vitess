6cd43fd173cfbf0ab2d9a3ff0490461ad489d6bc
Matt Lord <mattalord@gmail.com>
2026-02-15 02:07:45 +0000

    Skip TestZk2Topo in CI too
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/vt/topo/zk2topo/server_test.go
+++ go/vt/topo/zk2topo/server_test.go
@@ -19,6 +19,7 @@ package zk2topo
 import (
 	"context"
 	"fmt"
+	"os"
 	"path"
 	"testing"
 	"time"
@@ -34,6 +35,9 @@ import (
 )
 
 func TestZk2Topo(t *testing.T) {
+	if testing.Short() || os.Getenv("CI") == "true" {
+		t.Skip("skipping integration test in short mode and in CI (it's too flaky).")
+	}
 	// Start a real single ZK daemon, and close it after all tests are done.
 	zkd, serverAddr := zkctl.StartLocalZk(testfiles.GoVtTopoZk2topoZkID, testfiles.GoVtTopoZk2topoPort)
 	defer func() {
