d5ac0d71c472f0689b2a7773bb070b352be20412
Derek Perkins <derek@nozzle.io>
2026-03-02 22:52:30 -0700

    Revert "vitessdriver: return string for binary result values (#19527)"
    Signed-off-by: Derek Perkins <derek@nozzle.io>
    This reverts commit 2deac14bc3074eee71214acd7c1802df313fa36f.
    Signed-off-by: Derek Perkins <derek@nozzle.io>

--- go/vt/vitessdriver/convert.go
+++ go/vt/vitessdriver/convert.go
@@ -45,10 +45,6 @@ func (cv *converter) ToNative(v sqltypes.Value) (any, error) {
 		return v.ToFloat64()
 	case v.Type() == sqltypes.Datetime, v.Type() == sqltypes.Timestamp, v.Type() == sqltypes.Date:
 		return v.ToTime(cv.location)
-	case v.IsBinary():
-		// sending through string values matches the behavior in go-sql-driver and
-		// is necessary for json values to be handled without erroring in vttablet.
-		out = v.ToString()
 	case v.IsQuoted() || v.Type() == sqltypes.Bit || v.Type() == sqltypes.Decimal:
 		out, err = v.ToBytes()
 	case v.Type() == sqltypes.Expression:
