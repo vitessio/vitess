f090392fa4d2784501a0457de6b4cd451460184d
Derek Perkins <derek@nozzle.io>
2026-04-10 22:29:15 -0600

    vstreamclient: skip vgtid update when value hasn't changed
    Without CLIENT_FOUND_ROWS, MySQL returns RowsAffected=0 for an UPDATE
    that matches but doesn't change any columns. When buffered rows trigger
    a flush but the vgtid hasn't advanced, the RowsAffected!=1 check would
    erroneously fail. Guard the update with a proto.Equal check.
    
    Signed-off-by: Derek Perkins <derek@nozzle.io>

--- go/vt/vstreamclient/run.go
+++ go/vt/vstreamclient/run.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2025 The Vitess Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package vstreamclient
 
 import (
@@ -487,9 +503,13 @@ func (v *VStreamClient) flush(ctx context.Context, isCopyCompleted bool) error {
 	}
 
 	// always store the latest vgtid, even if there are no rows to store
-	err := updateLatestVGtid(ctx, v.session, v.cfg.name, v.cfg.vgtidStateKeyspace, v.cfg.vgtidStateTable, v.latestVgtid, isCopyCompleted)
-	if err != nil {
-		return err
+	// but skip the update if the vgtid hasn't changed and we're not setting copy_completed,
+	// because MySQL will return RowsAffected=0 for an UPDATE that doesn't change any columns
+	if isCopyCompleted || !proto.Equal(v.lastFlushedVgtid, v.latestVgtid) {
+		err := updateLatestVGtid(ctx, v.session, v.cfg.name, v.cfg.vgtidStateKeyspace, v.cfg.vgtidStateTable, v.latestVgtid, isCopyCompleted)
+		if err != nil {
+			return err
+		}
 	}
 
 	v.stats.FlushCount++
