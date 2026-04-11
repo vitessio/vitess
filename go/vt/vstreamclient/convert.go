8cf4eb28be3a454d5239eede9417d0d0c63fd911
Derek Perkins <derek@nozzle.io>
2026-04-10 22:29:07 -0600

    vstreamclient: add missing Uint64 case in sqlValueToDriverValue
    BIGINT UNSIGNED columns fell through to the ToBool() fallback, returning
    true for any nonzero value. Add an explicit Uint64 case that returns
    int64 for values that fit, and a string for values above math.MaxInt64.
    
    Signed-off-by: Derek Perkins <derek@nozzle.io>

--- go/vt/vstreamclient/convert.go
+++ go/vt/vstreamclient/convert.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2025 The Vitess Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package vstreamclient
 
 import (
@@ -6,6 +22,7 @@ import (
 	"encoding"
 	"encoding/json"
 	"fmt"
+	"math"
 	"reflect"
 	"time"
 
@@ -233,6 +250,16 @@ func sqlValueToDriverValue(rowValue sqltypes.Value, loc *time.Location) (driver.
 		rowVal, err := rowValue.ToUint64()
 		if err != nil {
 			return nil, err
+		}
+		return int64(rowVal), nil
+
+	case sqltypes.Uint64:
+		rowVal, err := rowValue.ToUint64()
+		if err != nil {
+			return nil, err
+		}
+		if rowVal > math.MaxInt64 {
+			return rowValue.ToString(), nil
 		}
 		return int64(rowVal), nil
 
