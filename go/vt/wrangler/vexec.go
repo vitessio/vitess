947a0a5819746c23807dd8ea69f954355d17bba5
Tim Vaillancourt <tim@timvaillancourt.com>
2026-05-12 22:06:50 +0200

    stragglers
    Signed-off-by: Tim Vaillancourt <tim@timvaillancourt.com>

--- go/vt/wrangler/vexec.go
+++ go/vt/wrangler/vexec.go
@@ -116,14 +116,13 @@ func (wr *Wrangler) QueryResultForTabletResults(results map[*topo.TabletInfo]*sq
 		Name: "Tablet",
 		Type: sqltypes.VarBinary,
 	}}
-	var row2 []sqltypes.Value
 	for tablet, result := range results {
 		if qr.Fields == nil {
 			qr.Fields = append(qr.Fields, defaultFields...)
 			qr.Fields = append(qr.Fields, result.Fields...)
 		}
 		for _, row := range result.Rows {
-			row2 = nil
+			row2 := make([]sqltypes.Value, 0, 1+len(row))
 			row2 = append(row2, sqltypes.NewVarBinary(tablet.AliasString()))
 			row2 = append(row2, row...)
 			qr.Rows = append(qr.Rows, row2)
