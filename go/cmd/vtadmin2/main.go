1c0e1e61e601894b9c6454c1431187a0deb25f4f
Matt Lord <mattalord@gmail.com>
2026-05-14 01:17:06 +0000

    Address codex review comments
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/cmd/vtadmin2/main.go
+++ go/cmd/vtadmin2/main.go
@@ -22,8 +22,11 @@ import (
 	"flag"
 	"fmt"
 	"log/slog"
+	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -63,22 +66,32 @@ var (
 	}
 )
 
+type runtimeConfig struct {
+	rbac *rbac.Config
+	ui   vtadmin2.Options
+}
+
 func preRun(cmd *cobra.Command, args []string) error {
 	_flag.TrickGlog()
 	if err := log.Init(cmd.Flags()); err != nil {
 		return err
 	}
 	logutil.PurgeLogs()
-	return validateRBACFlags()
+	return validateFlags()
 }
 
 func run(cmd *cobra.Command, args []string) error {
-	api, err := buildAPI(cmd.Context())
+	cfg, err := buildRuntimeConfig()
 	if err != nil {
 		return err
 	}
 
-	server, err := vtadmin2.NewServer(api, buildVTAdmin2Options())
+	api, err := buildAPI(cmd.Context(), cfg.rbac)
+	if err != nil {
+		return err
+	}
+
+	server, err := vtadmin2.NewServer(api, cfg.ui)
 	if err != nil {
 		return err
 	}
@@ -88,15 +101,9 @@ func run(cmd *cobra.Command, args []string) error {
 		addr = ":15001"
 	}
 	log.Info("starting vtadmin2", slog.String("addr", addr))
-	httpServer := buildHTTPServer(addr, server)
-	servenv.OnTermSync(func() {
-		ctx, cancel := context.WithTimeout(context.WithoutCancel(cmd.Context()), 30*time.Second)
-		defer cancel()
-		if err := httpServer.Shutdown(ctx); err != nil {
-			log.Error("failed to shut down vtadmin2", slog.Any("error", err))
-		}
-	})
-	return normalizeListenAndServeError(httpServer.ListenAndServe())
+	ctx, stop := signal.NotifyContext(cmd.Context(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+	return serveHTTPServer(ctx, buildHTTPServer(addr, server))
 }
 
 func buildHTTPServer(addr string, handler http.Handler) *http.Server {
@@ -110,6 +117,33 @@ func buildHTTPServer(addr string, handler http.Handler) *http.Server {
 	}
 }
 
+func serveHTTPServer(ctx context.Context, server *http.Server) error {
+	listener, err := net.Listen("tcp", server.Addr)
+	if err != nil {
+		return err
+	}
+	return serveHTTPServerWithListener(ctx, server, listener)
+}
+
+func serveHTTPServerWithListener(ctx context.Context, server *http.Server, listener net.Listener) error {
+	errCh := make(chan error, 1)
+	go func() {
+		errCh <- normalizeListenAndServeError(server.Serve(listener))
+	}()
+
+	select {
+	case err := <-errCh:
+		return err
+	case <-ctx.Done():
+		shutdownCtx, cancel := context.WithTimeout(context.WithoutCancel(ctx), 30*time.Second)
+		defer cancel()
+		if err := server.Shutdown(shutdownCtx); err != nil {
+			return err
+		}
+		return <-errCh
+	}
+}
+
 func normalizeListenAndServeError(err error) error {
 	if errors.Is(err, http.ErrServerClosed) {
 		return nil
@@ -117,15 +151,10 @@ func normalizeListenAndServeError(err error) error {
 	return err
 }
 
-func buildAPI(ctx context.Context) (*vtadmin.API, error) {
+func buildAPI(ctx context.Context, rbacConfig *rbac.Config) (*vtadmin.API, error) {
 	configs := clusterFileConfig.Combine(defaultClusterConfig, clusterConfigs)
-	if len(configs) == 0 && !enableDynamicClusters {
+	if len(configs) == 0 {
 		return nil, errors.New("must specify at least one cluster")
-	}
-
-	rbacConfig, err := buildRBACConfig()
-	if err != nil {
-		return nil, err
 	}
 
 	clusters := make([]*cluster.Cluster, 0, len(configs))
@@ -152,9 +181,18 @@ func buildAPI(ctx context.Context) (*vtadmin.API, error) {
 	}
 
 	return vtadmin.NewAPI(env, clusters, vtadmin.Options{
-		RBAC:                  rbacConfig,
-		EnableDynamicClusters: enableDynamicClusters,
+		RBAC: rbacConfig,
 	}), nil
+}
+
+func validateFlags() error {
+	if err := validateRBACFlags(); err != nil {
+		return err
+	}
+	if enableDynamicClusters {
+		return errors.New("vtadmin2 does not support dynamic clusters")
+	}
+	return nil
 }
 
 func validateRBACFlags() error {
@@ -174,16 +212,24 @@ func buildRBACConfig() (*rbac.Config, error) {
 	return rbac.LoadConfig(rbacConfigPath)
 }
 
-func buildVTAdmin2Options() vtadmin2.Options {
+func buildRuntimeConfig() (*runtimeConfig, error) {
+	rbacConfig, err := buildRBACConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	return &runtimeConfig{
+		rbac: rbacConfig,
+		ui:   buildVTAdmin2Options(rbacConfig),
+	}, nil
+}
+
+func buildVTAdmin2Options(rbacConfig *rbac.Config) vtadmin2.Options {
 	opts := uiOpts
 	if opts.DocumentTitle == "" {
 		opts.DocumentTitle = "VTAdmin2"
 	}
-	if enableRBAC {
-		rbacConfig, err := buildRBACConfig()
-		if err != nil {
-			return opts
-		}
+	if rbacConfig != nil {
 		opts.Authenticator = rbacConfig.GetAuthenticator()
 	}
 	return opts
