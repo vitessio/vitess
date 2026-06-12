30e63f82c6e84d753d836474da4ebd6ca85a409c
Max Englander <max@planetscale.com>
2026-06-12 16:43:31 -0400

    Revert "[debug] print proto diff in check_make_proto"
    This reverts commit fabfb99a634abf4696d9fe7d16ba0ff0565029b4.
    
    Signed-off-by: Max Englander <max@planetscale.com>

--- tools/check_make_proto.sh
+++ tools/check_make_proto.sh
@@ -13,10 +13,5 @@ diff=$(diff <( echo "$first_output") <( echo "$second_output"))
 if [[ "$diff" != "" ]]; then
   echo "ERROR: Regenerated proto files do not match the current version."
   echo -e "List of files containing differences:\n$diff"
-  echo "DEBUG: protoc-gen-go-grpc version info:"
-  bin/protoc-gen-go-grpc --version 2>&1 || true
-  go version
-  echo "DEBUG: full content diff for each modified file:"
-  git diff --no-color --text -- go/vt/proto/ | head -400
   exit 1
 fi
