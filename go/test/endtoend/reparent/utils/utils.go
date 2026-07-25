786ec70789fdf5dd7b8599f6e9d72a9e534b974b
Tim Vaillancourt <tim@timvaillancourt.com>
2026-07-25 02:39:15 +0200

    test: remove and consolidate ERS end-to-end tests
    Signed-off-by: Tim Vaillancourt <tim@timvaillancourt.com>

--- go/test/endtoend/reparent/utils/utils.go
+++ go/test/endtoend/reparent/utils/utils.go
@@ -425,12 +425,6 @@ func ErsIgnoreTablet(clusterInstance *cluster.LocalProcessCluster, tab *cluster.
 	return clusterInstance.VtctldClientProcess.ExecuteCommandWithOutput(args...)
 }
 
-// ErsWithVtctldClient runs ERS via a vtctldclient binary.
-func ErsWithVtctldClient(clusterInstance *cluster.LocalProcessCluster) (string, error) {
-	args := []string{"EmergencyReparentShard", fmt.Sprintf("%s/%s", KeyspaceName, ShardName)}
-	return clusterInstance.VtctldClientProcess.ExecuteCommandWithOutput(args...)
-}
-
 // endregion
 
 // region validations
