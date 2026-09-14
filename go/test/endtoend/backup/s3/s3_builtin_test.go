f4e00343a088f14dcabd539c7c165ba64fcc1c08
Matt Lord <mattalord@gmail.com>
2026-09-14 13:47:50 +0000

    test/endtoend/backup/s3: use errors.As so the file backports to Go 1.24
    errors.AsType is Go 1.26. This change needs to go to release-22.0 and
    release-23.0, whose CI is broken the same way and which build with Go
    1.24 and 1.25.
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/test/endtoend/backup/s3/s3_builtin_test.go
+++ go/test/endtoend/backup/s3/s3_builtin_test.go
@@ -151,7 +151,8 @@ func ensureBucket(ctx context.Context, env s3Env) error {
 		if err == nil {
 			return nil
 		}
-		if _, ok := errors.AsType[*types.NotFound](err); ok {
+		var notFound *types.NotFound
+		if errors.As(err, &notFound) { //nolint:modernize // errors.AsType is Go 1.26; this file is backported to branches on Go 1.24 and 1.25.
 			_, err = client.CreateBucket(ctx, &s3.CreateBucketInput{Bucket: aws.String(env.bucket)})
 			if err == nil {
 				return nil
