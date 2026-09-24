ea4a61357ad9e6cfec098f7eb2ea10d070d41cb6
Matt Lord <mattalord@gmail.com>
2026-09-24 16:59:45 -0400

    VTTablet: Discard the pooled connection after CALL so procedure session state cannot leak (#21062)
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/pools/smartconnpool/pool.go
+++ go/pools/smartconnpool/pool.go
@@ -57,6 +57,7 @@ type Metrics struct {
 	idleClosed           atomic.Int64
 	diffSetting          atomic.Int64
 	resetSetting         atomic.Int64
+	discardedByCaller    atomic.Int64
 	waiterCapRejected    atomic.Int64
 }
 
@@ -90,6 +91,12 @@ func (m *Metrics) DiffSettingCount() int64 {
 
 func (m *Metrics) ResetSettingCount() int64 {
 	return m.resetSetting.Load()
+}
+
+// DiscardedByCallerCount is the number of pooled connections a borrower
+// discarded (see Pooled.Discard) instead of returning them to the pool.
+func (m *Metrics) DiscardedByCallerCount() int64 {
+	return m.discardedByCaller.Load()
 }
 
 func (m *Metrics) WaiterCapRejected() int64 {
@@ -1076,5 +1083,8 @@ func (pool *ConnPool[C]) RegisterStats(stats *servenv.Exporter, name string) {
 	})
 	stats.NewCounterFunc(name+"ResetSetting", "Number of times pool reset the setting", func() int64 {
 		return pool.Metrics.ResetSettingCount()
+	})
+	stats.NewCounterFunc(name+"DiscardedByCaller", "Number of connections a borrower closed instead of returning them to the pool", func() int64 {
+		return pool.Metrics.DiscardedByCallerCount()
 	})
 }
